6b1c2635049aa9968df49dafeac6e66ed5adfea2
William Stein <wstein@ucsd.edu>
2006-03-07 11:01:49 +0000

    [project @ added new files for interfaces; added tornaria's extra pari declarations.]

---
+++ src/sage/interfaces/lcalc.py
@@ -0,0 +1,316 @@
+r"""
+Rubinstein's $L$-function Calculator
+
+This a standard part of \sage.  This interface provides complete
+access to Rubinstein's lcalc calculator with extra PARI functionality
+compiled in.
+
+\note{Each call to \code{lcalc} runs a complete \code{lcalc} process.
+On a typical Linux system, this entails about 0.3 seconds overhead.}
+
+AUTHOR:
+    -- Michael Rubinstein (2005): released under GPL the C++ program lcalc
+    -- William Stein (2006-03-05): wrote SAGE interface to lcalc
+"""
+
+########################################################################
+#       Copyright (C) 2006 William Stein <wstein@ucsd.edu>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#                  http://www.gnu.org/licenses/
+########################################################################
+
+import os, weakref
+from sage.ext.sage_object import SageObject
+from sage.misc.all import pager
+import sage.rings.all
+
+prec = 32
+
+class LCalc(SageObject):
+    r"""
+    Rubinstein's $L$-functions Calculator
+
+    Type \code{lcalc.[tab]} for a list of useful commands that are
+    implemented using the command line interface, but return objects
+    that make sense in SAGE.  For each command the possible inputs
+    for the L-function are:
+    \begin{itemize}
+       \item   \code{ ''} -- (default) the Riemann zeta function
+       \item   \code{ 'tau'} -- the L function of the Ramanujan delta function
+       \item   elliptic curve E -- where E is an elliptic curve over $\QQ$; defines $L(E,s)$
+    \end{itemize}
+
+    You can also use the complete command-line interface of
+    Rubinstein's $L$-functions calculations program via this class.
+    Type \code{lcalc.help()} for a list of commands and how to call
+    them.
+    """
+    def _repr_(self):
+        return "Rubinsteins L-function Calculator"
+
+    def __call__(self, args):
+        cmd = 'lcalc %s'%args
+        return os.popen(cmd).read().strip()
+
+    def _compute_L(self, L):
+        if isinstance(L, str):
+            if L == 'tau':
+                return '--tau'
+            return L
+        import sage.schemes.all
+        if sage.schemes.all.is_EllipticCurve(L):
+            if L.base_ring() == sage.rings.all.RationalField():
+                L = L.minimal_model()
+                return '-e --a1 %s --a2 %s --a3 %s --a4 %s --a6 %s'%tuple(L.a_invariants())
+        raise TypeError, "$L$-function of %s not known"%L
+
+    def help(self):
+        try:
+            h = self.__help
+        except AttributeError:
+            h = "-"*70 + '\n'
+            h += "   Call lcalc with one argument, e.g., \n"
+            h += "      sage: lcalc('--tau -z 1000')\n"
+            h += "   is translated into the command line\n"
+            h += "      $ lcalc --tau -z 1000\n"
+            h += "-"*70 + '\n'
+            h += '\n' + self('--help')
+            self.__help = h
+        pager(h)
+
+    def zeros(self, n, L=''):
+        """
+        Return the imaginary parts of the first $n$ nontrivial zeros
+        of the $L$-function in the upper half plane, as 32-bit reals.
+
+        INPUT:
+            n -- integer
+            L -- defines $L$-function (default: Riemann zeta function)
+
+        This function also checks the Riemann Hypothesis and makes
+        sure no zeros are missed.  This means it looks for several
+        dozen zeros to make sure none have been missed before
+        outputting any zeros at all, so takes longer than
+        \code{self.zeros_of_zeta_in_interval(...)}.
+
+        EXAMPLES:
+            sage: lcalc.zeros(4)                           # long
+            [14.134725139, 21.022039637, 25.010857575, 30.424876124]
+            sage: lcalc.zeros(5, L='--tau')                # long
+            [9.2223793976, 13.907549858, 17.442776978, 19.656513140, 22.336103633]
+            sage: lcalc.zeros(3, EllipticCurve('37a'))     # long
+            [0.00000000000, 5.0031700134, 6.8703912161]
+        """
+        L = self._compute_L(L)
+        RR = sage.rings.all.RealField(prec)
+        X = self('-z %s %s'%(int(n), L))
+        return [RR(z) for z in X.split()]
+
+    def zeros_in_interval(self, x, y, stepsize, L=''):
+        r"""
+        Return the imaginary parts of (most of) the nontrivial zeros
+        of the $L$-function on the line $\Re(s)=1/2$ with
+        positive imaginary part between $x$ and $y$, along with a
+        technical quantity for each.
+
+        INPUT:
+            x, y, stepsize -- positive floating point numbers
+            L -- defines $L$-function (default: Riemann zeta function)
+
+        OUTPUT:
+            list of pairs (zero, S(T)).
+
+        Rubinstein writes: The first column outputs the imaginary part
+        of the zero, the second column a quantity related to $S(T)$ (it
+        increases roughly by 2 whenever a sign change, i.e. pair of
+        zeros, is missed). Higher up the critical strip you should use
+        a smaller stepsize so as not to miss zeros.
+
+        EXAMPLES:
+            sage: lcalc.zeros_in_interval(10, 30, 0.1)
+            [(14.134725139, 0.18467291561), (21.022039637, -0.067789328954), (25.010857575, -0.055587278126)]
+        """
+        L = self._compute_L(L)
+        RR = sage.rings.all.RealField(prec)
+        X = self('--zeros-interval -x %s -y %s --stepsize=%s %s'%(
+            float(x), float(y), float(stepsize), L))
+        return [tuple([RR(z) for z in t.split()]) for t in X.split('\n')]
+
+    def value(self, s, L=''):
+        r"""
+        Return $L(s)$ for $s$ a complex number.
+
+        INPUT:
+            s -- complex number
+            L -- defines $L$-function (default: Riemann zeta function)
+
+        EXAMPLES:
+            sage: I = CC.0
+            sage: lcalc.value(0.5 + 100*I)
+            2.6926198853 - 0.020386029602*I
+
+        Note, SAGE can also compute zeta at complex numbers (using
+        the PARI C library):
+            sage: (0.5 + 100*I).zeta()
+            2.6926198856813239 - 0.020386029602598159*I
+        """
+        L = self._compute_L(L)
+        CC = sage.rings.all.ComplexField(prec)
+        s = CC(s)
+        x, y = self('-v -x %s -y %s %s'%(s.real(), s.imag(), L)).split()
+        return CC((float(x), float(y)))
+
+    def values_along_line(self, s0, s1, number_samples, L=''):
+        r"""
+        Return values of $L(s)$ at \code{number_samples}
+        equally-spaced sample points along the line from $s_0$ to
+        $s_1$ in the complex plane.
+
+        INPUT:
+            s0, s1 -- complex numbers
+            number_samples -- integer
+            L -- defines $L$-function (default: Riemann zeta function)
+
+        OUTPUT:
+            list -- list of pairs (s, zeta(s)), where the s are
+                    equally spaced sampled points on the line from
+                    s0 to s1.
+
+        EXAMPLES:
+            sage: I = CC.0
+            sage: lcalc.values_along_line(0.5, 0.5+20*I, 5)
+            [(0.50000000000, -1.4603545084),
+             (0.50000000000 + 4.0000000000*I, 0.60678376444 + 0.091112139955*I),
+             (0.50000000000 + 8.0000000000*I, 1.2416151054 + 0.36004758836*I),
+             (0.50000000000 + 12.000000000*I, 1.0159366503 - 0.74511247221*I),
+             (0.50000000000 + 16.000000000*I, 0.93854540843 + 1.2165878159*I)]
+        """
+        L = self._compute_L(L)
+        CC = sage.rings.all.ComplexField(prec)
+        s0 = CC(s0)
+        s1 = CC(s1)
+        v = self('--value-line-segment -x %s -y %s -X %s -Y %s --number-samples %s %s'%(
+            (s0.real(), s0.imag(), s1.real(), s1.imag(), int(number_samples), L)))
+        w = []
+        for a in v.split('\n'):
+            x0,y0,x1,y1 = a.split()
+            w.append((CC(x0,y0), CC(x1,y1)))
+        return w
+
+    def twist_values(self, s, dmin, dmax, L=''):
+        r"""
+        Return values of $L(s, \chi_d)$ for each quadratic
+        character $\chi_d$ for $d_{\min} \leq d \leq d_{\max}$.
+
+        INPUT:
+            s -- complex numbers
+            dmin -- integer
+            dmax -- integer
+            L -- defines $L$-function (default: Riemann zeta function)
+
+        OUTPUT:
+            list -- list of pairs (d, L(s,chi_d))
+
+        EXAMPLES:
+            sage: lcalc.twist_values(0.5, -10, 10)
+            [(-8, 1.1004214091),
+             (-7, 1.1465856666),
+             (-4, 0.66769145709),
+             (-3, 0.48086755769),
+             (5, 0.23175094748),
+             (8, 0.37369171286)]
+        """
+        L = self._compute_L(L)
+        CC = sage.rings.all.ComplexField(prec)
+        Z = sage.rings.all.Integer
+        s = CC(s)
+        typ = '--twist-quadratic'
+        dmin = int(dmin)
+        dmax = int(dmax)
+        v = self('-v -x %s -y %s %s --start %s --finish %s %s'%(
+            (s.real(), s.imag(), typ, dmin, dmax, L)))
+        w = []
+        if len(v) == 0:
+            return w
+        if len(v) == 0:
+            return w
+        for a in v.split('\n'):
+            d,x,y = a.split()
+            w.append((Z(d), CC(x,y)))
+        return w
+
+    def twist_zeros(self, n, dmin, dmax, L=''):
+        r"""
+        Return first $n$ real parts of nontrivial zeros for each
+        quadratic character $\chi_d$ for $d_{\min} \leq d \leq
+        d_{\max}$.
+
+        INPUT:
+            n -- integer
+            dmin -- integer
+            dmax -- integer
+            L -- defines $L$-function (default: Riemann zeta function)
+
+        OUTPUT:
+            dict -- keys are the discriminants $d$, and
+                    values are list of corresponding zeros.
+
+        EXAMPLES:
+            sage: lcalc.twist_zeros(3, -3, 6)
+            {-3: [8.0397371538, 11.249206208, 15.704619177],
+              5: [6.6484533437, 9.8314444311, 11.958845623]}
+        """
+        L = self._compute_L(L)
+        RR = sage.rings.all.RealField(prec)
+        Z = sage.rings.all.Integer
+        typ = '--twist-quadratic'
+        n = int(n)
+        v = self('-z %s %s --start %s --finish %s %s'%(
+            (n, typ, dmin, dmax, L)))
+        w = {}
+        if len(v) == 0:
+            return w
+        for a in v.split('\n'):
+            d, x = a.split()
+            x = RR(x)
+            d = Z(d)
+            if w.has_key(d):
+                w[d].append(x)
+            else:
+                w[d] = [x]
+        return w
+
+    def analytic_rank(self, L=''):
+        r"""
+        Return the analytic rank of the $L$-function at the central
+        critical point.
+
+        INPUT:
+            L -- defines $L$-function (default: Riemann zeta function)
+
+        OUTPUT:
+            integer
+
+        \note{Of course this is not provably correct in general, since
+        it is an open problem to compute analytic ranks provably
+        correctly in general.}
+
+        EXAMPLES:
+            sage: E = EllipticCurve('37a')
+            sage: lcalc.analytic_rank(E)
+            1
+        """
+        L = self._compute_L(L)
+        Z = sage.rings.all.Integer
+        s = self('--rank-compute %s'%L)
+        i = s.find('equals')
+        return Z(s[i+6:])
+
+
+
+# An instance
+lcalc = LCalc()
+
+
---
+++ src/sage/interfaces/sympow.py
@@ -0,0 +1,326 @@
+r"""
+Watkins Symmetric Power $L$-function Calculator
+
+SYMPOW is a package to compute special values of symmetric power
+elliptic curve L-functions. It can compute up to about 64 digits of
+precision.  This interface provides complete access to sympow, which
+is a standard part of \sage (and includes the extra data files).
+
+\note{Each call to \code{sympow} runs a complete \code{sympow}
+process.}
+
+AUTHOR:
+    -- Mark Watkins  (2005-2006): wrote and released sympow
+    -- William Stein (2006-03-05): wrote SAGE interface
+
+ACKNOWLEDGEMENT (from sympow readme):
+\begin{itemize}
+\item The quad-double package was modified from David Bailey's package:
+\url{http://crd.lbl.gov/~dhbailey/mpdist/}
+
+\item The \code{squfof} implementation was modified from Allan Steel's
+version of Arjen Lenstra's original LIP-based code.
+
+\item The \code{ec_ap} code was originally written for the kernel of
+MAGMA, but was modified to use small integers when possible.
+
+\item SYMPOW was originally developed using PARI, but due to licensing
+difficulties, this was eliminated. SYMPOW also does not use the
+standard math libraries unless Configure is run with the -lm option.
+SYMPOW still uses GP to compute the meshes of inverse Mellin
+transforms (this is done when a new symmetric power is added to
+datafiles).
+
+\end{itemize}
+"""
+
+########################################################################
+#       Copyright (C) 2006 William Stein <wstein@ucsd.edu>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#                  http://www.gnu.org/licenses/
+########################################################################
+
+import os, weakref
+
+from sage.ext.sage_object import SageObject
+from sage.misc.all import pager, verbose
+import sage.rings.all
+
+class Sympow(SageObject):
+    r"""
+    Watkins Symmetric Power $L$-function Calculator
+
+    Type \code{sympow.[tab]} for a list of useful commands that are
+    implemented using the command line interface, but return objects
+    that make sense in SAGE.
+
+    You can also use the complete command-line interface of sympow via
+    this class.  Type \code{sympow.help()} for a list of commands and
+    how to call them.
+    """
+    def _repr_(self):
+        return "Watkins Symmetric Power L-function Calculator"
+
+    def __call__(self, args):
+        cmd = 'sympow %s'%args
+        v = os.popen(cmd).read().strip()
+        verbose(v, level=2)
+        return v
+
+    def _fix_err(self, err):
+        w = err
+        j = w.rfind('./sympow')
+        if j != -1:
+            w = w[:j-1] + "sympow('" + w[j+9:] + ')'
+        return w
+
+    def _curve_str(self, E):
+        return '-curve "%s"'%(str(E.minimal_model().a_invariants()).replace(' ',''))
+
+    def L(self, E, n, prec):
+        r"""
+        Return $L(\Sym^{(n)}(E, \text{edge}))$ to prec digits
+        of precision.
+
+        INPUT:
+            E -- elliptic curve
+            n -- integer
+            prec -- integer
+
+        OUTPUT:
+            string -- real number to prec digits of precision as a string.
+
+        \note{Before using this function for the first time for
+        a given $n$, you may have to type \code{sympow('-new_data <n>')},
+        where \code{<n>} is replaced by your value of $n$.}
+
+        If you would like to see the extensive output sympow prints
+        when running this function, just type \code{set_verbose(2)}.
+
+        EXAMPLES:
+            sage.: a = sympow.L(EllipticCurve('11a'), 2, 16); a
+            '1.057599244590958E+00'
+            sage.: RR(a)
+            1.0575992445909579
+        """
+        v = self('-sp %sp%s %s'%(n, prec, self._curve_str(E)))
+        i = v.rfind(': ')
+        if i == -1:
+            print self._fix_err(v)
+            raise RuntimeError, "failed to compute symmetric power"
+        x = v[i+2:]
+        return x
+
+    def Lderivs(self, E, n, prec, d):
+        r"""
+        Return $0$th to $d$th derivatives of $L(\Sym^{(n)}(E,
+        \text{edge}))$ to prec digits of precision.
+
+        INPUT:
+            E -- elliptic curve
+            n -- integer
+            prec -- integer
+            d -- integer
+
+        OUTPUT:
+            a string, exactly as output by sympow
+
+        \note{To use this function you may have to run a few commands
+        like \code{sympow('-new_data 1d2')}, each which takes a few
+        minutes.  If this function fails it will indicate what
+        commands have to be run.}
+
+        EXAMPLES:
+            sage.: print sympow.Lderivs(EllipticCurve('11a'), 1, 16, 2)
+            ...
+             1n0: 2.538418608559107E-01
+             1w0: 2.538418608559108E-01
+             1n1: 1.032321840884568E-01
+             1w1: 1.059251499158892E-01
+             1n2: 3.238743180659171E-02
+             1w2: 3.414818600982502E-02
+        """
+        v = self('-sp %sp%sd%s %s'%(n, prec, d, self._curve_str(E)))
+        return self._fix_err(v)
+
+    def modular_degree(self, E):
+        """
+        Return the modular degree of the elliptic curve E, assuming
+        the Stevens conjecture.
+
+        INPUT:
+            E -- elliptic curve over Q
+
+        OUTPUT:
+            integer -- modular degree
+
+        EXAMPLES:
+        We compute the modular degrees of the lowest known conductor
+        curves of the first few ranks:
+
+            sage: sympow.modular_degree(EC('11a'))
+            1
+            sage: sympow.modular_degree(EC('37a'))
+            2
+            sage: sympow.modular_degree(EC('389a'))
+            40
+            sage: sympow.modular_degree(EC('5077a'))
+            1984
+            sage: sympow.modular_degree(EllipticCurve([1, -1, 0, -79, 289]))
+            334976
+        """
+        v = self('%s -moddeg'%self._curve_str(E))
+        s = 'Modular Degree is '
+        i = v.find(s)
+        if i == -1:
+            print self._fix_err(v)
+            raise RuntimeError, "failed to compute modular degree"
+        return sage.rings.all.Integer(v[i+len(s):])
+
+    def analytic_rank(self, E):
+        r"""
+        Return the analytic rank and leading $L$-value of the elliptic
+        curve $E$.
+
+        INPUT:
+            E -- elliptic curve over Q
+
+        OUTPUT:
+            integer -- analytic rank
+            string -- leading coefficient (as string)
+
+        \note{The analytic rank is \emph{not} computed provably
+        correctly in general.}
+
+        EXAMPLES:
+        We compute the analytic ranks of the lowest known conductor
+        curves of the first few ranks:
+
+            sage: sympow.analytic_rank(EllipticCurve('11a'))
+            (0, '2.53842e-01')
+            sage: sympow.analytic_rank(EllipticCurve('37a'))
+            (1, '3.06000e-01')
+            sage: sympow.analytic_rank(EllipticCurve('389a'))
+            (2, '7.59317e-01')
+            sage: sympow.analytic_rank(EllipticCurve('5077a'))
+            (3, '1.73185e+00')
+            sage: sympow.analytic_rank(EllipticCurve([1, -1, 0, -79, 289]))
+            (4, '8.94385e+00')
+            sage: sympow.analytic_rank(EllipticCurve([0, 0, 1, -79, 342]))  # long
+            (5, '3.02857e+01')
+            sage: sympow.analytic_rank(EllipticCurve([1, 1, 0, -2582, 48720]))  # long
+            (6, '3.20781e+02')
+            sage: sympow.analytic_rank(EllipticCurve([0, 0, 0, -10012, 346900]))  # long
+            (7, '1.32517e+03')
+        """
+        v = self('%s -analrank'%self._curve_str(E))
+        s = 'Analytic Rank is '
+        i = v.rfind(s)
+        if i == -1:
+            print self._fix_err(v)
+            raise RuntimeError, "failed to compute analytic rank"
+        j = v.rfind(':')
+        r = sage.rings.all.Integer(v[i+len(s):j])
+        i = v.rfind(' ')
+        L = v[i+1:]
+        return r, L
+
+    def new_data(self, n):
+        """
+        Pre-compute data files needed for computation of
+        n-th symmetric powers.
+        """
+        print self('-new_data %s'%n)
+
+    def help(self):
+        h = """
+     sympow('-sp 2p16 -curve "[1,2,3,4,5]"')
+will compute L(Sym^2 E,edge) for E=[1,2,3,4,5] to 16 digits of precision
+The result
+ 2n0: 8.370510845377639E-01
+ 2w0: 8.370510845377586E-01
+consists of two calculations using different parameters to test the
+functional equation (to see if these are sufficiently close).
+
+     sympow('-sp 3p12d2,4p8 -curve "[1,2,3,4,5]"')
+will compute the 0th-2nd derivatives of L(Sym^3 E,center) to 12 digits
+and L(Sym^4 E,edge) to 8 digits
+
+Special cases: When a curve has CM, Hecke power can be used instead
+
+     sympow('-sp 7p12d1 -hecke -curve "[0,0,1,-16758,835805]"')
+
+will compute the 0th-1st derivatives of L(Sym^7 psi,special) to 12 digits
+
+Bloch-Kato numbers can be obtained for powers not congruent to 0 mod 4:
+
+     sympow('-sp 2bp16 -curve "[1,2,3,4,5]"')
+
+should return
+ 2n0: 4.640000000000006E+02
+ 2w0: 4.639999999999976E+02
+which can be seen to be very close to the integer 464.
+
+Modular degrees can be computed with the -moddeg option.
+
+     sympow('-curve "[1,2,3,4,5]" -moddeg')
+
+should return
+Modular Degree is 464
+
+Analytic ranks can be computed with the -analrank option.
+
+     sympow('-curve "[1,2,3,4,5]" -analrank')
+
+should return
+Analytic Rank is 1 : L'-value 3.51873e+00
+
+and (if the mesh file for the fifth derivative is present)
+
+     sympow('-curve "[0,0,1,-79,342]" -analrank')
+
+should return
+Analytic Rank is 5 : leading L-term 3.02857e+01
+
+========================================================================
+
+Adding new symmetric powers:
+
+SYMPOW keeps data for symmetric powers in the directory datafiles
+If a pre-computed mesh of inverse Mellin transform values is not
+available for a given symmetric power, SYMPOW will fail. The command
+
+     sympow('-new_data 2')
+
+will add the data the 2nd symmetric power, while
+
+     sympow('-new_data 3d2')
+
+will add the data for the 2nd derivative and 3rd symmetric power,
+
+     sympow('-new_data 6d0h')
+
+will add the data for the 0th derivative of the 6th Hecke power, and
+
+     sympow('-new_data 4c')
+
+will add data for the 4th symmetric power for curves with CM
+(these need to be done separately for powers divisible by 4).
+
+The mesh files are stored in binary form, and thus endian-ness is a
+worry when moving from one platform to another.
+
+To enable modular degree computations, the 2nd symmetric power must
+be extant, and analytic rank requires derivatives of the 1st power.
+"""
+        pager(h)
+
+
+
+
+# An instance
+sympow = Sympow()
+
+

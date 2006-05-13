34e9b87271005ed5d3985bdfa6cdef934f10f7e5
William Stein <wstein@ucsd.edu>
2006-05-13 21:56:39 +0000

    [project @ berlekamp-massey; fix to work for integral domains; added examples.]

--- src/sage/matrix/berlekamp_massey.py
+++ src/sage/matrix/berlekamp_massey.py
@@ -1,5 +1,8 @@
 """
-Implementation of the Berlekamp-Massey Algorithm
+Minimal Polynomials of Linear Recurrence Sequences
+
+AUTHOR:
+    -- William Stein
 """
 
 
@@ -19,11 +22,12 @@ Implementation of the Berlekamp-Massey Algorithm
 #*****************************************************************************
 
 import sage.rings.polynomial_ring as polynomial_ring
+import sage.rings.rational_field
 
 def berlekamp_massey(a):
     """
     Use the Berlekamp-Massey algorithm to find the minimal polynomial
-    of a linearly generated sequence a.
+    of a linearly recurrence sequence a.
 
     The minimal polynomial of a linear recurrence $\{a_r\}$ is by
     definition the unique monic polynomial~$g$, such that if $\{a_r\}$
@@ -32,13 +36,21 @@ def berlekamp_massey(a):
     polynomial $x^j + \sum_{i=0}^{j-1} b_i x^i$.
 
     INPUT:
-        a -- a list of even length of elements of a single field
+        a -- a list of even length of elements of a field (or domain)
 
     OUTPUT:
-        Polynomial -- the minimal polynomial of the sequence
+        Polynomial -- the minimal polynomial of the sequence (as a
+           polynomial over the field in which the entries of a live)
 
     EXAMPLES:
-
+        sage: berlekamp_massey([1,2,1,2,1,2])
+        x^2 - 1
+        sage: berlekamp_massey([GF(7)(1),19,1,19])
+        x^2 + 6
+        sage: berlekamp_massey([2,2,1,2,1,191,393,132])
+        x^4 - 36727/11711*x^3 + 34213/5019*x^2 + 7024942/35133*x - 335813/1673
+        sage: berlekamp_massey(prime_range(2,38))
+        x^6 - 14/9*x^5 - 7/9*x^4 + 157/54*x^3 - 25/27*x^2 - 73/18*x + 37/9
     """
 
     if not isinstance(a, list):
@@ -48,8 +60,11 @@ def berlekamp_massey(a):
 
     M = len(a)//2
 
-    K = a[0].parent()
-    R = polynomial_ring.PolynomialRing(K)
+    try:
+        K = a[0].parent().fraction_field()
+    except AttributeError:
+        K = sage.rings.rational_field.RationalField()
+    R = polynomial_ring.PolynomialRing(K, name='x')
     x = R.gen()
 
     f = {}
@@ -70,5 +85,7 @@ def berlekamp_massey(a):
         s[j] = s[j-2] - q[j]*s[j-1]
         t[j] = t[j-2] - q[j]*t[j-1]
     t = s[j].reverse()
-    return ~(t[t.degree()]) * t  # make monic  (~ is inverse in python)
+    f = ~(t[t.degree()]) * t  # make monic  (~ is inverse in python)
+    return f
+    return polynomial_ring.PolynomialRing(a[0].parent(),name='x')(f)
 

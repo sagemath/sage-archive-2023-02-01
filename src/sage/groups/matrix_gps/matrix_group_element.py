4260f3ef7aa1806f85f59bb2e4fa708b1189d44c
wdjoyner <wdjoyner@gmail.com>
2006-05-15 18:42:13 +0000

    [project @ matrix group element additions]

--- src/sage/groups/matrix_gps/matrix_group_element.py
+++ src/sage/groups/matrix_gps/matrix_group_element.py
@@ -4,7 +4,8 @@ GL, SL, Sp.
 
 AUTHORS:
    David Joyner -- initial version
-
+   David Joyner -- (2006-05) various modifications to address William Stein's
+                   TODO's.
 
 TODO: I don't like this at all.
   The matrix group element class should derive from matrix, so that
@@ -27,6 +28,8 @@ from sage.interfaces.gap import gap
 from linear import LinearGroup_generic
 import sage.structure.element as element
 from general_linear import GeneralLinearGroup_generic
+from sage.matrix.matrix_space import MatrixSpace, is_MatrixSpace
+from sage.matrix.matrix import Matrix
 
 def is_MatrixGroupElement(x):
     return isinstance(x, MatrixGroupElement)
@@ -72,16 +75,139 @@ class MatrixGroupElement(element.Element_cmp_,
     def _gap_init_(self):
         return self.__mat._gap_init_()
 
-    def _repr_(self):
+    def _gap_latex_(self):
         """
-        Return string representation of this matrix
+        wraps GAP's LaTeX command (written by S. Kohl)
 
         EXAMPLES:
+            sage: F = GF(3); MS = MatrixSpace(F,2,2)
+            sage: gens = [MS([[1,0],[0,1]]),MS([[1,1],[0,1]])]
+            sage: G = MatrixGroup(gens)
+            sage: g = G([[1, 1], [0, 1]])
+            sage: g._gap_latex_()
+            '\\left(\\begin{array}{rr}%\nZ(3)^{0}&Z(3)^{0}\\\\%\n0*Z(3)&Z(3)^{0}\\\\%\n\\end{array}\\right)%\n'
+
+        Type view(g._latex_()) to see the object in an xdvi window (assuming you
+        have latex and xdvi installed).
+        """
+        s1 = self.__mat._gap_init_()
+        s2 = gap.eval("LaTeX("+s1+")")
+        return eval(s2)
+        #s2 = s2.replace("\\\\left(\\\\","\left(\ ")
+        #s3 = s2.replace("\\\\left(\\","\left( \n ")
+        #s4 = s3.replace("\\\\right)","\right) ")
+        #s5 = s4.replace("\\\\\\"," ")
+        #s6 = s5.replace("\\\\end","\end")
+
+    def __repr__(self):
+        """
+        Return string representation of this matrix.
+
+        EXAMPLES:
+            sage: F = GF(3); MS = MatrixSpace(F,2,2)
+            sage: gens = [MS([[1,0],[0,1]]),MS([[1,1],[0,1]])]
+            sage: G = MatrixGroup(gens)
+            sage: g = G([[1, 1], [0, 1]])
+            sage: g
+            [1 1]
+            [0 1]
+
         """
         return str(self.__mat)
 
+    def __str__(self):
+        """
+        Return string representation of this matrix (called by print).
+
+        EXAMPLES:
+            sage: F = GF(3); MS = MatrixSpace(F,2,2)
+            sage: gens = [MS([[1,0],[0,1]]),MS([[1,1],[0,1]])]
+            sage: G = MatrixGroup(gens)
+            sage: g = G([[1, 1], [0, 1]])
+            sage: print g
+            [[1, 1], [0, 1]]
+
+        """
+        rws = (self.__mat).rows()
+        lrws = [list(x) for x in rws]
+        return str(lrws)
+
     def _latex_(self):
+        """
+        EXAMPLES:
+            sage: F = GF(3); MS = MatrixSpace(F,2,2)
+            sage: gens = [MS([[1,0],[0,1]]),MS([[1,1],[0,1]])]
+            sage: G = MatrixGroup(gens)
+            sage: g = G([[1, 1], [0, 1]])
+            sage: g._latex_()
+            '\\left(\\begin{array}{rr}\n1&1\\\\\n0&1\n\\end{array}\\right)'
+
+        Type view(g._latex_()) to see the object in an xdvi window (assuming you
+        have latex and xdvi installed).
+        """
         return self.__mat._latex_()
+
+    def __mul__(self,other):
+        """
+
+        EXAMPLES:
+            sage: F = GF(3); MS = MatrixSpace(F,2,2)
+            sage: gens = [MS([[1,0],[0,1]]),MS([[1,1],[0,1]])]
+            sage: G = MatrixGroup(gens)
+            sage: g = G([[1,1],[0,1]])
+            sage: h = G([[1,1],[0,1]])
+            sage: g*h
+            [1 2]
+            [0 1]
+
+        """
+        m = self.parent().degree()
+        n = other.parent().degree()
+        if m != n:
+            raise TypeError, "Matrices must have compatible dimensions"
+        F = self.parent().base_ring()
+        M = MatrixSpace(F,n,n)
+        x = M(self.__mat)*M(other.__mat)
+        return MatrixGroupElement((self.parent()).matrix_space()(x), self.parent())
+
+    def __add__(self,other):
+        """
+        You can add and scalar multiply even though it is not a group
+        operation.
+
+        EXAMPLES:
+            sage: F = GF(3); MS = MatrixSpace(F,2,2)
+            sage: gens = [MS([[1,0],[0,1]]),MS([[1,1],[0,1]])]
+            sage: G = MatrixGroup(gens)
+            sage: g = G([[1,1],[0,1]])
+            sage: h = G([[1,2],[0,1]])
+            sage: g+h
+            [2 0]
+            [0 2]
+            sage: 2*g
+            [2 2]
+            [0 2]
+            sage: MS = MatrixSpace(QQ,2,2)
+            sage: gens = [MS([[1,0],[0,1]]),MS([[1,1],[0,1]])]
+            sage: G = MatrixGroup(gens)
+            sage: g = G([[1,1],[0,1]])
+            sage: h = G([[1,2],[0,1]])
+            sage: g+h
+            [2 3]
+            [0 2]
+            sage: 5/3*h
+            [ 5/3 10/3]
+            [   0  5/3]
+
+        """
+
+        m = self.parent().degree()
+        n = other.parent().degree()
+        if m != n:
+            raise TypeError, "Matrices must have compatible dimensions"
+        F = self.parent().base_ring()
+        M = MatrixSpace(F,n,n)
+        return M(self.__mat)+M(other.__mat)
 
     def order(self):
         """
@@ -109,18 +235,14 @@ class MatrixGroupElement(element.Element_cmp_,
         from sage.groups.perm_gps.permgroup import PermutationGroup
         from sage.interfaces.all import gap
         G = g.parent()
-        #print G
         gap.eval("l:=One(Rationals)")
         s1 = "gens := GeneratorsOfGroup(%s)"%G._gap_init_()
-        #print s1
         gap.eval(s1)
         s2 = "g0:=%s; gensH:=%s"%(gap(g),words)
         gap.eval(s2)
         s3 = 'G:=Group(gens); H:=Group(gensH)'
-        #print s3
         gap.eval(s3)
         phi = gap.eval("hom:=EpimorphismFromFreeGroup(G)")
-        #print phi
         l1 = gap.eval("ans:=PreImagesRepresentative(hom,g0)")
         l2 = l1
         l4 = []
@@ -140,3 +262,22 @@ class MatrixGroupElement(element.Element_cmp_,
             print "         ",l1
             print "         ",l5
         return l2
+
+    def list(self):
+        """
+        Return list representation of this matrix.
+
+        EXAMPLES:
+            sage: F = GF(3); MS = MatrixSpace(F,2,2)
+            sage: gens = [MS([[1,0],[0,1]]),MS([[1,1],[0,1]])]
+            sage: G = MatrixGroup(gens)
+            sage: g = G.random()
+            sage: g.list()
+            [[1, 1], [0, 1]]
+
+        """
+        rws = (self.__mat).rows()
+        lrws = [list(x) for x in rws]
+        return lrws
+
+

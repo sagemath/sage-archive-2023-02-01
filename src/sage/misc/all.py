3ac5a11171a4d6cb1066ec69105de1f7bde9940b
William Stein <wstein@ucsd.edu>
2006-06-15 19:34:33 +0000

    [project @ constants -- fixed imports]

--- src/sage/misc/all.py
+++ src/sage/misc/all.py
@@ -40,11 +40,6 @@ from latex import latex, view, lprint
 
 from mathml import mathml
 
-from constants import (pi, e, NaN, golden_ratio, log2, euler_gamma, catalan,
-                       khinchin, twinprime, merten, brun)
-
-from functions import FunctionRing, sin as sin0, cos as cos0, airy_ai, airy_bi
-
 from trace import *
 
 ##########################################################################

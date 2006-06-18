a22f5a82f01229c8fe20f010aa1ac25c2d8744f6
William Stein <wstein@ucsd.edu>
2006-06-18 06:04:20 +0000

    [project @ notebook -- a new file (like all.py but for the notebook)]

---
+++ src/sage/all_notebook.py
@@ -0,0 +1,7 @@
+from sage.misc.latex import Latex, lprint
+latex = Latex(density=125)
+latex_debug = Latex(debug=True, density=125)
+slide = Latex(slide=True, density=100)
+slide_debug = Latex(slide=True, debug=True, density=100)
+
+

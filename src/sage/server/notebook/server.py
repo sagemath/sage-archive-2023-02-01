434c2f819c41635dbc60f2cd855b9b47d36072d7
Justin C. Walker <justin@mac.com>
2006-08-09 08:19:04 +0000

    [project @ AddressInUse Patch 2] Patch the proper file this time.
     Avoid the "Address in use" error message from the notebook server
     by configuring the httpd server socket with SO_REUSEPORT.  This won't
     avoid the error message when multiple servers are running (the message
     is correct in this case).  It is useful in avoiding the message when the
     system is holding the port open, to adhere to TCP state-change requirements
     defined in order to avoid accepting stale packets (from an old connection)
     on a new connection.
    
     Also, need to "import socket" to get the constants needed for the patch.

--- src/sage/server/notebook/server.py
+++ src/sage/server/notebook/server.py
@@ -12,6 +12,7 @@ Web Server Component of SAGE Notebook
 
 # Python libraries
 import BaseHTTPServer
+import socket
 import cgi
 import os, sys
 import select
@@ -627,6 +628,7 @@ class NotebookServer:
     def __init__(self, notebook, port, address):
         self.__notebook = notebook
         self.__httpd = BaseHTTPServer.HTTPServer((address,int(port)), WebServer)
+        self.__httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         self.__address = address
         self.__port = port
 

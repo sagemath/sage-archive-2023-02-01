260e7f3f4b7a6ff2ad7b58960f49985528a36d13
William Stein <wstein@gmail.com>
2006-10-13 00:32:49 -0700

    Switching to slideshow mode maintains current focused cell.

--- src/sage/server/notebook/js.py
+++ src/sage/server/notebook/js.py
@@ -687,8 +687,8 @@ function cell_focus(id) {
     return true;
 }
 function cell_blur(id) {
-    if(!in_slide_mode)
-        current_cell = -1;
+   /* if(!in_slide_mode)
+        current_cell = -1; */
     e = get_cell(id);
     if(e == null) return;
     e.className="cell_input";

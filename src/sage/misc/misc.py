8d7cf998d7803c8b5b675d4fab61c075f7f33d6b
Martin Albrecht <malb@informatik.uni-bremen.de>
2006-10-20 10:34:47 +0200

    merged changes from upstream repository

--- src/sage/misc/misc.py
+++ src/sage/misc/misc.py
@@ -1124,6 +1124,8 @@ def branch_current_hg_notice(branch):
 
     NOTE: If the branch is main, then return an empty string.
     """
+    if branch[-1] == '/':
+        branch = branch[:-1]
     if branch == 'main':
         return ''
     notice = 'Loading SAGE library. Current Mercurial branch is: '

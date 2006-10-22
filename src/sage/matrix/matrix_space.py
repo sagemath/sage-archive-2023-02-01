d876de7071d4abcb61244d0ca759789d045061aa
William Stein <wstein@gmail.com>
2006-10-22 07:00:30 -0700

    Fix call method.

--- src/sage/matrix/matrix_space.py
+++ src/sage/matrix/matrix_space.py
@@ -188,8 +188,8 @@ class MatrixSpace_generic(gens.Generators):
             self.__ncols = ncols
         self.__matrix_class = self._get_matrix_class()
 
-    def __call__(self, entries=0, copy=True):
-        return self.matrix(entries, True, copy)
+    def __call__(self, entries=0, coerce=True, copy=True):
+        return self.matrix(entries, coerce=coerce, copy=copy)
 
     def _coerce_(self, x):
         """

83cc12e0ad40bf6ebbd6b3559b09a413c0927834
William Stein <wstein@gmail.com>
2006-11-22 12:28:35 -0800

    Added better enumeration of elements of G_H(N) list.

--- src/sage/modular/modsym/ghlist.py
+++ src/sage/modular/modsym/ghlist.py
@@ -21,16 +21,21 @@ List of coset represenatives for $\Gamma_H(N)$ in $\SL_2(\ZZ)$.
 
 
 import sage.rings.arith as arith
+import p1list
 
 class GHlist:
     def __init__(self, group):
         self.__group = group
         N = group.level()
-        v = [group._reduce_coset(u,v) for u in xrange(N) for v in xrange(N) \
-                if arith.GCD(arith.GCD(u,v),N) == 1]
-        v = list(set(v))
-        v.sort()
-        self.__list = v
+        #v = [group._reduce_coset(u,v) for u in xrange(N) for v in xrange(N) \
+        #        if arith.GCD(arith.GCD(u,v),N) == 1]
+        v = group._coset_reduction_data_first_coord()
+        N = group.level()
+        coset_reps = set([a for a, _, _ in v if arith.GCD(a,N) == 1])
+        w = [group._reduce_coset(x*u, x*v) for x in coset_reps for u,v in p1list.P1List(N).list()]
+        w = list(set(w))
+        w.sort()
+        self.__list = w
 
     def __getitem__(self, i):
         return self.__list[i]

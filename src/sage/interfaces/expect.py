cca90cc1d56eebc76973065536d382e013a2023a
William Stein <wstein@gmail.com>
2007-01-11 14:10:46 -0800

    Make sure . is not in user's path.

--- src/sage/interfaces/expect.py
+++ src/sage/interfaces/expect.py
@@ -67,6 +67,13 @@ tmp='%s/tmp'%SAGE_TMP_INTERFACE
 ##     return ' '.join([t] + c[1:])
 
 
+# . in user's path causes *HUGE* trouble, e.g., pexpect will try to
+# run a directory name!
+p = os.environ['PATH'].split(':')
+os.environ['PATH'] = ':'.join([v for v in p if v.strip() != '.'])
+
+
+
 class Expect(ParentWithBase):
     """
     Expect interface object.

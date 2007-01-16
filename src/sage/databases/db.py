c49f4b7aee38776e1b3aa8717713c831b3c67124
Nick Alexander <ncalexander@gmail.com>
2007-01-15 19:01:24 -0800

    Minor change: transition _uniq class to new Python object model.

--- src/sage/databases/db.py
+++ src/sage/databases/db.py
@@ -113,14 +113,15 @@ Storage = sage.databases.compressed_storage.CompressedStorage(FileStorage.FileSt
 
 DB_HOME = "%s/data/"%sage.misc.misc.SAGE_ROOT
 
-_db = {}
 class _uniq(object):
-    def __new__(cls, name="", read_only=True):
-        global _db
-        if _db.has_key(cls):
-            return _db[cls]
+    _db = {} # Class variable, no globals!
+
+    def __new__(cls, name="", read_only=True, unique_key=None):
+        key = (cls, unique_key)
+        if _uniq._db.has_key(key):
+            return _uniq._db[key]
         X = object.__new__(cls)
-        _db[cls] = X
+        _uniq._db[key] = X
         return X
 
 class Database(_uniq):
@@ -130,7 +131,6 @@ class Database(_uniq):
             self.name = name
             self._thresh = thresh
             self._load_()
-
 
     def _load_(self):
         name = self.name

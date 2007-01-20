31cdeed91cbb6037143cdff4bb22ec727069a8a6
Yi Qiang <yqiang@gmail.com>
2007-01-19 22:04:47 -0800

    Added sage.dsage as a module

--- src/setup.py
+++ src/setup.py
@@ -758,6 +758,7 @@ setup(name        = 'sage',
                      'sage.server.trac',
 
                      'sage.structure',
+                     'sage.dsage'
                      ],
 
       ext_modules = ext_modules,

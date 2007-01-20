2580b45b9a38f4b23d202759048cf0dd5c477fbe
Yi Qiang <yqiang@gmail.com>
2007-01-19 23:12:18 -0800

    Made calls nonblocking

--- src/sage/dsage/dsage.py
+++ src/sage/dsage/dsage.py
@@ -3,31 +3,35 @@ class DistributedSage(object):
     def __init__(self):
         pass
 
-    def server(self):
+    def server(self, blocking=False):
         cmd = 'dsage_server.py'
+        if not blocking:
+            cmd += '&'
         os.system(cmd)
 
-    def worker(self):
+    def worker(self, blocking=False):
         cmd = 'dsage_worker.py'
+        if not blocking:
+            cmd += '&'
         os.system(cmd)
 
     def console(self):
         cmd = 'dsage_console.py'
         os.system(cmd)
 
-    def setup():
+    def setup(self):
         cmd = 'dsage_setup.py'
         os.system(cmd)
 
-    def setup_server():
+    def setup_server(self):
         cmd = 'dsage_setup.py server'
         os.system(cmd)
 
-    def setup_worker():
+    def setup_worker(self):
         cmd = 'dsage_setup.py worker'
         os.system(cmd)
 
-    def setup_client():
+    def setup_client(self):
         cmd = 'dsage_setup.py client'
         os.system(cmd)
 

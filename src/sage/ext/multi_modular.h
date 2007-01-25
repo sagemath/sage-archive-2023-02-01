4b40e6e8fcbf75a352368d21d6142631c70535ae
Robert Bradshaw <robertwb@math.washington.edu>
2007-01-24 19:46:00 -0800

    multi modular basis infrastructure

---
+++ src/sage/ext/multi_modular.h
@@ -0,0 +1,13 @@
+#define mod_int size_t
+
+
+/* This is the largest value we can do arithmatic on without risking overflowing.
+ * It will be optimized to a constant if any optimization is turned on.
+ * The expression below assumes unsigned.
+ */
+#define MOD_INT_MAX ((1 << (sizeof(mod_int)*4)) - 1)
+
+/* Don't want to be up against the limit so
+ * we have room to gather.
+ */
+#define START_PRIME_MAX (MOD_INT_MAX/10)

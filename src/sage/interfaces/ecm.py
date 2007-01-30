046cb809fd4a3a81913fe194d0c218a2c276c6f7
William Stein <wstein@gmail.com>
2007-01-30 01:28:38 -0800

    more improvements of the same nature.

--- src/sage/interfaces/ecm.py
+++ src/sage/interfaces/ecm.py
@@ -166,6 +166,7 @@ class ECM:
         cleaner.cleaner(child.pid, self.__cmd)
         child.timeout = None
 	child.__del__ = nothing   # program around studid exception ignored error
+        child.expect('[ECM]')
         child.sendline(str(n))
         child.sendline("bad") # child.sendeof()
         while True:

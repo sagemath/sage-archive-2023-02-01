9db8c20653a6954a4e75c0d0a0171b92727fc219
William Stein <wstein@gmail.com>
2007-02-15 17:04:25 -0800

    link in gmp

--- src/setup.py
+++ src/setup.py
@@ -503,8 +503,8 @@ ext_modules = [ \
               ), \
 
     Extension('sage.graphs.graph_fast',
-              ['sage/graphs/graph_fast.pyx']
-              ), \
+              ['sage/graphs/graph_fast.pyx'],
+              libraries = ['gmp']), \
 
     ]
 

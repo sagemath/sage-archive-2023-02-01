95ed97626a4aff8ddae70b2d500ada9b7e7524e6
William Stein <wstein@gmail.com>
2007-03-13 09:56:57 -0700

    wrote first version of numerical_newforms

---
+++ src/sage/modular/modform/numerical.py
@@ -0,0 +1,237 @@
+"""
+Numerical computation of newforms
+"""
+
+#########################################################################
+#       Copyright (C) 2004--2006 William Stein <wstein@gmail.com>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#                  http://www.gnu.org/licenses/
+#########################################################################
+
+from sage.structure.sage_object import SageObject
+from sage.rings.complex_double  import CDF
+from sage.misc.misc             import verbose
+from sage.modular.modsym.all    import ModularSymbols
+from sage.modular.congroup      import is_CongruenceSubgroup, Gamma0
+from sage.rings.integer         import Integer
+from sage.modular.dims          import dimension_new_cusp_forms_group
+from sage.rings.arith           import next_prime
+from sage.modules.all           import vector
+from random                     import randint
+
+
+class NumericalNewforms(SageObject):
+    def __init__(self, group, weight=2, eps=1e-10):
+        if not is_CongruenceSubgroup(group):
+            group = Gamma0(Integer(group))
+        self._group  = group
+        self._weight = Integer(weight)
+        if self._weight < 2:
+            raise ValueError, "weight must be at least 2"
+        self._eps = eps
+
+    def level(self):
+        return self._group.level()
+
+    def weight(self):
+        return self._weight
+
+    def _repr_(self):
+        return "Numerical newforms for %s of weight %s"%(
+            self._group, self._weight)
+
+    def _modular_symbols(self):
+        try:
+            return self.__modular_symbols
+        except AttributeError:
+            M = ModularSymbols(self._group,
+                    self._weight, sign=1).cuspidal_submodule()
+            self.__modular_symbols = M
+            return M
+
+    def __len__(self):
+        try:
+            return self.__len
+        except AttributeError:
+            pass
+        n = dimension_new_cusp_forms_group(self._group, self._weight)
+        self.__len = n
+        return n
+
+    def _eigenvectors(self):
+        try:
+            return self.__eigenvectors
+        except AttributeError:
+            pass
+        verbose('Finding eigenvector basis')
+        M = self._modular_symbols()
+        M_amb = M.ambient_module()
+        N = self.level()
+        n = len(self)
+
+        def next_p(p):
+            p = next_prime(p)
+            while N%p == 0:
+                p = next_prime(p)
+            return p
+
+        p = next_p(1)
+
+        t = M.T(p).matrix()
+        t_amb = M_amb.T(p).matrix()
+        eps = self._eps
+
+        w = []
+        while len(w) < n:
+            tm = verbose('computing eigenvectors (found %s of %s)'%(len(w),n))
+            evals, B = t.change_ring(CDF).eigen()
+            verbose('done computing eigenvectors', t=tm)
+            # Find the eigenvalues that occur with multiplicity 1 up
+            # to the given eps.
+            v = list(evals)
+            v.sort()
+            w = []
+            i = 0
+            while i < len(v):
+                e = v[i]
+                if i + 1 == len(v) or abs(e-v[i+1]) >= eps:
+                    w.append((e,i))
+                i += 1
+            if len(w) < n:
+                p = next_p(p)
+                tt = M.T(p).matrix()
+                tt_amb = M_amb.T(p).matrix()
+                c = randint(1,5)
+                if c > 1:
+                    tt = c*tt
+                    tt_amb = c*tt_amb
+                t += tt
+                t_amb += tt_amb
+            #endif
+        #end while
+
+        evals, B = t_amb.change_ring(CDF).eigen()
+        z = []
+        # match up the eigenvalues
+        for e, _ in w:
+            j = -1
+            for i in range(len(evals)):
+                if abs(e - evals[i]) < eps:
+                    j = i
+                    break
+            if j == -1:
+                raise RuntimeError, "Precision error."
+            z.append(j)
+        self.__eigenvectors = B.matrix_from_columns(z)
+        return self.__eigenvectors
+
+    def _easy_vector(self):
+        """
+        Return a very sparse vector v such that v times the eigenvector matrix
+        has all entries nonzero.
+
+        ALGORITHM:
+           1. Choose row with the most nonzero entries.   (put 1 there)
+           2. Consider submatrix of columns corresponding
+              to zero entries in row chosen in 1.
+           3. Find row of submatrix with most nonzero entries,
+              and add appropriate multiple.  Repeat.
+
+        EXAMPLES:
+            sage: n = numerical_newforms(37)
+            sage: n._easy_vector()
+            (1.0, 1.0)
+            sage: n = numerical_newforms(43)
+            sage: n._easy_vector()
+            (0, 1.0, 0)
+            sage: n = numerical_newforms(125)
+            sage: n._easy_vector()
+            (1.0, 0, 0, 0, 0, 0, 0, 0)
+        """
+        try:
+            return self.__easy_vector
+        except AttributeError:
+            pass
+        E = self._eigenvectors()
+        eps = self._eps
+
+
+        def best_row(M):
+            R = M.rows()
+            v = [len(support(r, eps)) for r in R]
+            m = max(v)
+            i = v.index(m)
+            return i, R[i]
+
+        i, e = best_row(E)
+
+        x = (CDF**E.nrows()).zero_vector()
+        x[i] = 1
+
+        while True:
+            s = set(support(e, eps))
+            zp = [i for i in range(e.degree()) if not i in s]
+            if len(zp) == 0:
+                break
+            C = E.matrix_from_columns(zp)
+            # best row
+            i, f = best_row(C)
+            x[i] += 1   # simplistic
+            e = x*E
+
+        self.__easy_vector = x
+        return x
+
+    def eigenvalues(self, p):
+        p = Integer(p)
+        try:
+            return self.__eigenvalues[p]
+        except AttributeError:
+            self.__eigenvalues = {}
+        except KeyError:
+            pass
+
+    def _eigendata(self):
+        try:
+            return self.__eigendata
+        except AttributeError:
+            pass
+        x = self._easy_vector()
+
+        B = self._eigenvectors()
+        def phi(y):
+            return y.element() * B
+
+        phi_x = phi(x)
+        V = phi_x.parent()
+        phi_x_inv = V([a**(-1) for a in phi_x])
+        eps = self._eps
+        nzp = support(x, eps)
+        x_nzp = vector(CDF, x.list_from_positions(nzp))
+        self.__eigendata = (phi_x, phi_x_inv, nzp, x_nzp)
+        return self.__eigendata
+
+    def ap(self, p):
+        return self.eigenvalues([p])[0]
+
+    def eigenvalues(self, primes):
+        if isinstance(primes, (int, long, Integer)):
+            primes = [Integer(primes)]
+        phi_x, phi_x_inv, nzp, x_nzp = self._eigendata()
+        B = self._eigenvectors()
+        def phi(y):
+            return y.element() * B
+
+        ans = []
+        m = self._modular_symbols().ambient_module()
+        for p in primes:
+            t = m._compute_hecke_matrix_prime(p, nzp)
+            w = phi(x_nzp*t)
+            ans.append([w[i]*phi_x_inv[i] for i in range(w.degree())])
+        return ans
+
+
+def support(v, eps):
+    return [i for i in range(v.degree()) if abs(v[i]) > eps]

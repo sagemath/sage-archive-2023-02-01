64fe9b63e28e5f21ca44797393d9407775abc76c
Yi Qiang <yqiang@gmail.com>
2007-03-25 16:42:55 -0700

    Moved xml generation from monitordb to server.

--- src/sage/dsage/server/server.py
+++ src/sage/dsage/server/server.py
@@ -19,16 +19,17 @@
 import zlib
 import cPickle
 import datetime
+import xml
+import cStringIO
 
 from twisted.spread import pb
 from twisted.python import log
 
 from sage.dsage.misc.hostinfo import HostInfo
-import sage.dsage.server.worker_tracker as worker_tracker
 import sage.dsage.server.client_tracker as client_tracker
 from sage.dsage.server.hostinfo_tracker import hostinfo_list
 from sage.dsage.errors.exceptions import BadTypeError
-from sage.dsage.database.job import Job, expand_job
+from sage.dsage.database.job import expand_job
 
 pb.setUnjellyableForClass(HostInfo, HostInfo)
 
@@ -349,15 +350,193 @@ class DSageServer(pb.Root):
                 if h['uuid'] not in h.values():
                     hostinfo_list.append(h)
 
-    def write_stats_to_xml(self):
+    def generate_xml_stats(self):
         r"""
-        Writes the server statistics to a XML file to be consumed by other
-        programs
+        This method returns a an XML document to be consumed by the Dashboard
+        widget
 
         """
 
-        raise NotImplementedError
+        def create_gauge(doc):
+            gauge = doc.createElement('gauge')
+            doc.appendChild(gauge)
 
+            return doc, gauge
+
+        def add_totalAgentCount(doc, gauge):
+            totalAgentCount = doc.createElement('totalAgentCount')
+            gauge.appendChild(totalAgentCount)
+            working_workers = self.monitordb.get_worker_count(connected=True,
+                                                              busy=True)
+            free_workers = self.monitordb.get_worker_count(connected=True,
+                                                           busy=False)
+            disconnected_workers = self.monitordb.get_worker_count(
+                                   connected=False,
+                                   busy=False)
+            total_workers = (working_workers +
+                             free_workers +
+                             disconnected_workers)
+            count = doc.createTextNode(str(total_workers))
+            totalAgentCount.appendChild(count)
+
+            return doc, totalAgentCount
+
+        def add_onlineAgentCount(doc, gauge):
+            onlineAgentCount = doc.createElement('onlineAgentCount')
+            gauge.appendChild(onlineAgentCount)
+            free_workers = self.monitordb.get_worker_count(connected=True,
+                                                           busy=False)
+            busy_workers = self.monitordb.get_worker_count(connected=True,
+                                                           busy=True)
+            count = doc.createTextNode(str(free_workers + busy_workers))
+            onlineAgentCount.appendChild(count)
+
+            return doc, onlineAgentCount
+
+        def add_offlineAgentCount(doc, gauge):
+            offlineAgentCount = doc.createElement('offlineAgentCount')
+            gauge.appendChild(offlineAgentCount)
+            worker_count = self.monitordb.get_worker_count(connected=False,
+                                                           busy=False)
+            count = doc.createTextNode(str(worker_count))
+            offlineAgentCount.appendChild(count)
+
+            return doc, offlineAgentCount
+
+        def add_workingAgentCount(doc, gauge):
+            workingAgentCount = doc.createElement('workingAgentCount')
+            gauge.appendChild(workingAgentCount)
+            worker_count = self.monitordb.get_worker_count(connected=True,
+                                                           busy=True)
+            count = doc.createTextNode(str(worker_count))
+            workingAgentCount.appendChild(count)
+
+            return doc, workingAgentCount
+
+        def add_availableAgentCount(doc, gauge):
+            availableAgentCount = doc.createElement('availableAgentCount')
+            gauge.appendChild(availableAgentCount)
+            # worker_count = self.monitordb.get_worker_count(connected=True)
+            worker_count = self.monitordb.get_worker_count(connected=True,
+                                                           busy=False)
+            count = doc.createTextNode(str(worker_count))
+            availableAgentCount.appendChild(count)
+
+            return doc, availableAgentCount
+
+        def add_unavailableAgentCount(doc, gauge):
+            unavailableAgentCount = doc.createElement('unavailableAgentCount')
+            gauge.appendChild(unavailableAgentCount)
+            worker_count = self.monitordb.get_worker_count(connected=True,
+                                                           busy=True)
+            count = doc.createTextNode(str(worker_count))
+            unavailableAgentCount.appendChild(count)
+
+            return doc, unavailableAgentCount
+
+        def add_workingMegaHertz(doc, gauge):
+            workingMegaHertz = doc.createElement('workingMegaHertz')
+            gauge.appendChild(workingMegaHertz)
+            cpu_speed = self.monitordb.get_cpu_speed(connected=True)
+            mhz = doc.createTextNode(str(cpu_speed))
+            workingMegaHertz.appendChild(mhz)
+
+            return doc, workingMegaHertz
+
+        def add_availableProcessorCount(doc, gauge):
+            pass
+
+        def add_unavailableProcessorCount(doc, gauge):
+            pass
+
+        def add_onlineProcessorCount(doc, gauge):
+            onlineProcessorCount = doc.createElement('onlineProcessorCount')
+            gauge.appendChild(onlineProcessorCount)
+            cpu_count = self.monitordb.get_cpu_count(connected=True)
+            c = doc.createTextNode(str(cpu_count))
+            onlineProcessorCount.appendChild(c)
+
+            return doc, onlineProcessorCount
+
+        def add_offlineProcessorCount(doc, gauge):
+            offlineProcessorCount = doc.createElement('offlineProcessorCount')
+            gauge.appendChild(offlineProcessorCount)
+            cpu_count = self.monitordb.get_cpu_count(connected=False)
+            c = doc.createTextNode(str(cpu_count))
+            offlineProcessorCount.appendChild(c)
+
+            return doc, offlineProcessorCount
+
+        def add_workingProcessorCount(doc, gauge):
+            workingProcessorCount = doc.createElement('workingProcessorCount')
+            gauge.appendChild(workingProcessorCount)
+            worker_count = self.monitordb.get_cpu_count(connected=True)
+            pcount = doc.createTextNode(str(worker_count))
+            workingProcessorCount.appendChild(pcount)
+
+            return doc, workingProcessorCount
+
+        def add_workingAgentPercentage(doc, gauge):
+            workingAgentPercentage = doc.createElement(
+                                                    'workingAgentPercentage')
+            gauge.appendChild(workingAgentPercentage)
+            working_workers = self.monitordb.get_worker_count(connected=True,
+                                                              busy=True)
+            free_workers = self.monitordb.get_worker_count(connected=True,
+                                                           busy=False)
+            disconnected_workers = self.monitordb.get_worker_count(
+                                   connected=False,
+                                   busy=False)
+            total_workers = (working_workers +
+                             free_workers +
+                             disconnected_workers)
+
+            worker_percentage = float(working_workers / total_workers) * 100
+            percentage = doc.createTextNode(str(worker_percentage))
+            workingAgentPercentage.appendChild(percentage)
+
+            return doc, workingAgentPercentage
+
+        def add_date(doc, gauge):
+            date = datetime.datetime.now()
+
+            year = doc.createElement('Year')
+            gauge.appendChild(year)
+            year.appendChild(doc.createTextNode(str(date.year)))
+
+            seconds = doc.createElement('Seconds')
+            gauge.appendChild(seconds)
+            seconds.appendChild(doc.createTextNode(str(date.second)))
+
+            minutes = doc.createElement('Minutes')
+            gauge.appendChild(minutes)
+            minutes.appendChild(doc.createTextNode(str(date.minute)))
+
+            return doc, year, seconds, minutes
+
+        doc = xml.dom.minidom.Document()
+        doc, gauge = create_gauge(doc)
+
+        add_totalAgentCount(doc, gauge)
+        add_onlineAgentCount(doc, gauge)
+        add_offlineAgentCount(doc, gauge)
+        add_availableAgentCount(doc, gauge)
+        add_unavailableAgentCount(doc, gauge)
+        add_workingAgentCount(doc, gauge)
+        add_workingAgentPercentage(doc, gauge)
+
+        add_onlineProcessorCount(doc, gauge)
+        add_offlineAgentCount(doc, gauge)
+        add_availableProcessorCount(doc, gauge)
+        add_unavailableProcessorCount(doc, gauge)
+        add_workingProcessorCount(doc, gauge)
+        add_workingMegaHertz(doc, gauge)
+
+        add_date(doc, gauge)
+        s = cStringIO.StringIO()
+        doc.writexml(s, newl='\n')
+
+        return s.getvalue()
 
 class DSageWorkerServer(DSageServer):
     r"""

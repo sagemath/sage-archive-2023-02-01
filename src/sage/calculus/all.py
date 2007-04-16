d2257a4f2265bdc428b6cdbc29447a4acad84372
William Stein <wstein@gmail.com>
2007-04-16 07:18:02 -0700

    move predefined vars out of calculus.py

--- src/sage/calculus/all.py
+++ src/sage/calculus/all.py
@@ -7,9 +7,14 @@ from calculus import (SymbolicExpressionRing,
                       is_SymbolicExpression,
                       is_SymbolicExpressionRing)
 
+
+from functional import (diff, derivative,
+                        integrate, limit, lim,
+                        taylor, simplify)
+
 from var import var
 
-from calculus import (a,
+from predefined import (a,
                       b,
                       c,
                       d,
@@ -59,7 +64,3 @@ from calculus import (a,
                       Y,
                       Z)
 
-
-
-
-from functional import (diff, derivative, integrate, limit, lim, taylor, simplify)

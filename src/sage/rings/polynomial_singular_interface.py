de2056d7686ec2f21f5100f55e9d0cc4c189fd0b
William Stein <wstein@gmail.com>
2007-05-02 06:21:48 -0700

    Add an error message if one tries to create a singular ring of characteristic p with p composite.

--- src/sage/rings/polynomial_singular_interface.py
+++ src/sage/rings/polynomial_singular_interface.py
@@ -171,31 +171,31 @@ class PolynomialRing_singular_repr:
             #  size_t bits = 1 + (size_t) ((float)digits * 3.5);
             precision = self.base_ring().precision()
             digits = sage.rings.arith.integer_ceil((2*precision - 2)/7.0)
-            self.__singular = singular.ring("(real,%d,0)"%digits, _vars, order=order)
+            self.__singular = singular.ring("(real,%d,0)"%digits, _vars, order=order, check=False)
 
         elif is_ComplexField(self.base_ring()):
             # singular converts to bits from base_10 in mpr_complex.cc by:
             #  size_t bits = 1 + (size_t) ((float)digits * 3.5);
             precision = self.base_ring().precision()
             digits = sage.rings.arith.integer_ceil((2*precision - 2)/7.0)
-            self.__singular = singular.ring("(complex,%d,0,I)"%digits, _vars,  order=order)
+            self.__singular = singular.ring("(complex,%d,0,I)"%digits, _vars,  order=order, check=False)
 
         elif is_RealDoubleField(self.base_ring()):
             # singular converts to bits from base_10 in mpr_complex.cc by:
             #  size_t bits = 1 + (size_t) ((float)digits * 3.5);
-            self.__singular = singular.ring("(real,15,0)", _vars, order=order)
+            self.__singular = singular.ring("(real,15,0)", _vars, order=order, check=False)
 
         elif is_ComplexDoubleField(self.base_ring()):
             # singular converts to bits from base_10 in mpr_complex.cc by:
             #  size_t bits = 1 + (size_t) ((float)digits * 3.5);
-            self.__singular = singular.ring("(complex,15,0,I)", _vars,  order=order)
+            self.__singular = singular.ring("(complex,15,0,I)", _vars,  order=order, check=False)
 
         elif self.base_ring().is_prime_field() or (self.base_ring() is ZZ and force):
-            self.__singular = singular.ring(self.characteristic(), _vars, order=order)
+            self.__singular = singular.ring(self.characteristic(), _vars, order=order, check=False)
 
         elif self.base_ring().is_finite(): #must be extension field
             gen = str(self.base_ring().gen())
-            r = singular.ring( "(%s,%s)"%(self.characteristic(),gen), _vars, order=order)
+            r = singular.ring( "(%s,%s)"%(self.characteristic(),gen), _vars, order=order, check=False)
             self.__minpoly = "("+(str(self.base_ring().modulus()).replace("x",gen)).replace(" ","")+")"
             singular.eval("minpoly=%s"%(self.__minpoly) )
 

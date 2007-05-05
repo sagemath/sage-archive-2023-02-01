1534c28c92317656108fbef28557f8d98cb427b6
William Stein <wstein@gmail.com>
2007-05-05 16:44:20 -0700

    Some improvements and fixes for the new version of QSieve.

--- src/sage/misc/misc.py
+++ src/sage/misc/misc.py
@@ -1061,7 +1061,7 @@ set_trace = pdb.set_trace
 
 def tmp_dir(name):
     r"""
-    Create and return a temporary directory in \code{\$HOME/.sage/temp/pid/}
+    Create and return a temporary directory in \code{\$HOME/.sage/temp/hostname/pid/}
     """
     name = str(name)
     n = 0

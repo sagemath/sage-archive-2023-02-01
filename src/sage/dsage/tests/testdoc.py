7eb07c1b630472b337d5085e9e51bf90826cfa5c
Yi Qiang <yqiang@gmail.com>
2007-05-16 16:37:56 -0700

    Fixed doctests.

--- src/sage/dsage/tests/testdoc.py
+++ src/sage/dsage/tests/testdoc.py
@@ -1,12 +1,12 @@
-"""nodoctest
+"""
 These test that DSage is *really* working for normal users locally
 on their system:
 
 WARNING: Currently these non-blocking startups leave processes
 hanging around!
 
-   sage: dsage.server(blocking=False)
-   sage: dsage.worker(blocking=False)
+   sage: dsage.server(blocking=False, verbose=False)
+   sage: dsage.worker(blocking=False, verbose=False)
    sage: d = DSage()
    sage: sleep(0.5)
    sage: a = d('2 + 3')
@@ -14,7 +14,10 @@ hanging around!
    sage: a
    5
    sage: v = [d('%s^2'%i) for i in range(100,103)]
-   sage: _ = [x.wait() for x in v]
+
+Set timeout to 10 seconds it it will not hang the doctests indefinitely.
+
+   sage: _ = [x.wait(timeout=15) for x in v]
    sage: print v
    [10000, 10201, 10404]
 """

6be65569d78b5303630be3a50032c49ebe88dce0
William Stein <wstein@gmail.com>
2007-06-02 20:45:05 -0700

    doctest fixes

--- src/sage/databases/lincodes.py
+++ src/sage/databases/lincodes.py
@@ -1,4 +1,4 @@
-"""
+"""nodoctest
 Linear codes
 """
 
@@ -37,67 +37,67 @@ def parse_bound_html(text, n, k):
                 upper = int(m.group('val'))
     return lower, upper
 
-def linear_code_bound(q, n, k, verbose=False):
-    r"""
-    Find bounds on the minimum distance of linear codes over GF(q)
-    with length n and dimension k, courtesy of
-         \url{http://www.win.tue.nl/~aeb/voorlincod.html}.
-    If no bounds are in the database, returns lower and upper
-    bounds of -1.
+## def linear_code_bound(q, n, k, verbose=False):
+##     r"""
+##     Find bounds on the minimum distance of linear codes over GF(q)
+##     with length n and dimension k, courtesy of
+##          \url{http://www.win.tue.nl/~aeb/voorlincod.html}.
+##     If no bounds are in the database, returns lower and upper
+##     bounds of -1.
 
-    INPUT:
-        q -- integer, the field order, which must be in
-                      [2, 3, 4, 5, 7, 8, 9]
-        n -- integer, the length of the code
-        k -- integer, the dimension of the code
-        verbose -- bool (default=False), print verbose message
+##     INPUT:
+##         q -- integer, the field order, which must be in
+##                       [2, 3, 4, 5, 7, 8, 9]
+##         n -- integer, the length of the code
+##         k -- integer, the dimension of the code
+##         verbose -- bool (default=False), print verbose message
 
-    OUTPUT:
-        integer -- lower bound
-        integer -- upper bound
-        str -- text about why the bounds are as given
+##     OUTPUT:
+##         integer -- lower bound
+##         integer -- upper bound
+##         str -- text about why the bounds are as given
 
-    EXAMPLES:
-    To find lower and upper bounds for values q=7, n=32, k=8, type
-        sage: lower, upper, text = linear_code_bound(7, 32, 8)     # optional -- needs internet
-        sage: lower                   # optional
-        19
-        sage: upper                   # optional
-        21
-        sage: text                    # optional
-        'Lb(32,8) = 19 DG4\n\nUb(32,8) = 21 follows by a one-step Griesmer bound from:\nUb(10,7) = 3 is found by considering shortening to:\nUb(9,6) = 3 is found by construction B:\n[consider deleting the (at most) 6 coordinates of a word in the dual]'
+##     EXAMPLES:
+##     To find lower and upper bounds for values q=7, n=32, k=8, type
+##         sage: lower, upper, text = linear_code_bound(7, 32, 8)     # optional -- needs internet
+##         sage: lower                   # optional
+##         19
+##         sage: upper                   # optional
+##         21
+##         sage: text                    # optional
+##         'Lb(32,8) = 19 DG4\n\nUb(32,8) = 21 follows by a one-step Griesmer bound from:\nUb(10,7) = 3 is found by considering shortening to:\nUb(9,6) = 3 is found by construction B:\n[consider deleting the (at most) 6 coordinates of a word in the dual]'
 
-    When bounds are not known the upper and lower returned bounds are -1:
-        sage: linear_code_bound(9, 32, 200)          # optional -- needs internet
-        (-1, -1, '(error executing -why-)')
+##     When bounds are not known the upper and lower returned bounds are -1:
+##         sage: linear_code_bound(9, 32, 200)          # optional -- needs internet
+##         (-1, -1, '(error executing -why-)')
 
-    This function raises an IOError if an error occurs downloading
-    data or parsing it.  It raises a ValueError if the q input is
-    invalid.
+##     This function raises an IOError if an error occurs downloading
+##     data or parsing it.  It raises a ValueError if the q input is
+##     invalid.
 
-    AUTHOR: 2005-11-14: Steven Sivek
-    """
-    q = Integer(q)
-    if not q in [2, 3, 4, 5, 7, 8, 9]:
-        raise ValueError, "q (=%s) must be in [2,3,4,5,7,8,9]"%q
-    n = Integer(n)
-    k = Integer(k)
+##     AUTHOR: 2005-11-14: Steven Sivek
+##     """
+##     q = Integer(q)
+##     if not q in [2, 3, 4, 5, 7, 8, 9]:
+##         raise ValueError, "q (=%s) must be in [2,3,4,5,7,8,9]"%q
+##     n = Integer(n)
+##     k = Integer(k)
 
-    param = ("%s/%s/%s"%(q,n,k)).replace('L','')
+##     param = ("%s/%s/%s"%(q,n,k)).replace('L','')
 
-    url = "http://homepages.cwi.nl/htbin/aeb/lincodbd/"+param
-    if verbose:
-        print "Looking up the bounds at %s"%url
-    f = urllib.urlopen(url)
-    s = f.read()
-    f.close()
+##     url = "http://homepages.cwi.nl/htbin/aeb/lincodbd/"+param
+##     if verbose:
+##         print "Looking up the bounds at %s"%url
+##     f = urllib.urlopen(url)
+##     s = f.read()
+##     f.close()
 
-    i = s.find("<PRE>")
-    j = s.find("</PRE>")
-    if i == -1 or j == -1:
-        raise IOError, "Error parsing data (missing pre tags)."
-    text = s[i+5:j].strip()
-    lower, upper = parse_bound_html(text, n, k)
+##     i = s.find("<PRE>")
+##     j = s.find("</PRE>")
+##     if i == -1 or j == -1:
+##         raise IOError, "Error parsing data (missing pre tags)."
+##     text = s[i+5:j].strip()
+##     lower, upper = parse_bound_html(text, n, k)
 
-    return lower, upper, text
+##     return lower, upper, text
 

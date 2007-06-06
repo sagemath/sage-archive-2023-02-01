a23991e7bcb1638d9ccc704fb6fce0eeff65a304
Nick Alexander <ncalexander@gmail.com>
2007-06-05 23:15:56 -0700

    Updates to Polynomial_*_dense.resultant.
     * Removed Polynomial_rational_dense.resultant; the recently updated
       Polynomial.resultant PARI code handles this case.
    
     * Updated coercions in Polynomial_integer_dense.resultant and
       Polynomial_dense_mod_p.resultant.

--- src/sage/rings/polynomial/polynomial_element_generic.py
+++ src/sage/rings/polynomial/polynomial_element_generic.py
@@ -917,31 +917,6 @@ class Polynomial_rational_dense(Polynomial_generic_field):
             b *= a
         return self.parent()(c)
 
-    def resultant(self, other):
-        """
-        Returns the resultant of self and other, which must lie in the same
-        polynomial ring.
-
-        INPUT:
-            other -- a polynomial
-        OUTPUT:
-            an element of the base ring of the polynomial ring
-
-        NOTES:
-            Implemented using pari's polresultant function.
-
-        EXAMPLES:
-            sage: R.<x> = QQ[]
-            sage: f = x^3 + x + 1;  g = x^3 - x - 1
-            sage: f.resultant(g)
-            -8
-        """
-        if not isinstance(other, Polynomial):
-            other = self.polynomial(other)
-        if self.parent() != other.parent():
-            raise TypeError
-        return QQ(str(self.__poly.polresultant(other.__poly, 0)))
-
     def hensel_lift(self, p, e):
         """
         Assuming that self factors modulo $p$ into distinct factors,
@@ -1346,11 +1321,12 @@ class Polynomial_integer_dense(Polynomial_generic_domain,
         EXAMPLES:
             sage: x = PolynomialRing(ZZ,'x').0
             sage: f = x^3 + x + 1;  g = x^3 - x - 1
-            sage: f.resultant(g)
+            sage: r = f.resultant(g); r
             -8
+            sage: r.parent() is ZZ
+            True
         """
-        if not isinstance(other, Polynomial) or self.parent() != other.parent():
-            other = self.polynomial(other)
+        other = self.parent()._coerce_(other)
         return ZZ(str(self.__poly.resultant(other.__poly, 0)))
 
     def ntl_set_directly(self, v):
@@ -1361,7 +1337,7 @@ class Polynomial_integer_dense(Polynomial_generic_domain,
         class.  Use this function to set the value of this polynomial using
         the NTL constructor, which is potentially quicker.   The input v
         is either a vector of ints or a string of the form '[ n1 n2 n3 ... ]'
-        where the ni are integers and there are no commas between them.
+        where the \code{n_i} are integers and there are no commas between them.
         The optimal input format is the string format, since that's what NTL uses.
 
         EXAMPLES:
@@ -1848,14 +1824,15 @@ class Polynomial_dense_mod_p(Polynomial_dense_mod_n,
             an element of the base ring of the polynomial ring
 
         EXAMPLES:
-            sage: _.<x> = PolynomialRing(GF(19))
+            sage: R.<x> = GF(19)['x']
             sage: f = x^3 + x + 1;  g = x^3 - x - 1
-            sage: f.resultant(g)
+            sage: r = f.resultant(g); r
             11
+            sage: r.parent() is GF(19)
+            True
         """
-        if not isinstance(other, Polynomial) or self.parent() != other.parent():
-            other = self.polynomial(other)
         self.parent()._ntl_set_modulus()
+        other = self.parent()._coerce_(other)
         return self.base_ring()(str(self.ntl_ZZ_pX().resultant(other.ntl_ZZ_pX())))
 
     def discriminant(self):

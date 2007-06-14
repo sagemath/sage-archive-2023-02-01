496546a81e52c57627c0e027d4a633ce0c90750c
Robert L Miller <rlm@robertlmiller.com>
2007-06-14 15:51:17 -0700

    ...

--- src/sage/graphs/graph.py
+++ src/sage/graphs/graph.py
@@ -3378,7 +3378,7 @@ class Graph(GenericGraph):
             [ 0 -1  0 -1  0  0  2  0  0]
             [ 0  0 -1  0  0 -1  0  3 -1]
             [-1 -1  0 -1  0  0  0 -1  4]
-            sage: M.determinant()
+            sage: M.determinant() # long time
             0
 
         """

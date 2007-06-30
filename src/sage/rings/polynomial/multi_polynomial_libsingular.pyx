57662691637800aa7a1f098aab9d09411b9c79ea
William Stein <wstein@gmail.com>
2007-06-30 15:24:44 -0700

    ...

--- src/sage/rings/polynomial/multi_polynomial_libsingular.pyx
+++ src/sage/rings/polynomial/multi_polynomial_libsingular.pyx
@@ -108,7 +108,6 @@ cdef extern from "dlfcn.h":
 cdef extern from "string.h":
     char *strdup(char *s)
 
-
 cdef init_singular():
     """
     This initializes the Singular library. Right now, this is a hack.
@@ -119,7 +118,6 @@ cdef init_singular():
     context these symbols are not known globally. The work around so
     far is to load the library again and to specifiy RTLD_GLOBAL.
     """
-
     cdef void *handle
 
 
@@ -139,7 +137,7 @@ cdef init_singular():
     # Steal Memory Manager back or weird things may happen
     sage.rings.memory.pmem_malloc()
 
-# call it
+ # call it
 init_singular()
 
 order_dict = {"dp":ringorder_dp,
@@ -265,7 +263,9 @@ cdef class MPolynomialRing_libsingular(MPolynomialRing_generic):
             self._ring.algring = rCopy0(k._ring)
             rComplete(self._ring.algring,1)
             self._ring.P = self._ring.algring.N
-            self._ring.parameter = self._ring.algring.names
+            #self._ring.parameter = self._ring.algring.names
+            self._ring.parameter = <char**>omAlloc0(sizeof(char*)*2)
+            self._ring.parameter[0] = omStrDup(self._ring.algring.names[0])
 
             nmp = <lnumber*>omAlloc0Bin(rnumber_bin)
             nmp.z= <napoly*>p_Copy(minpoly._poly, self._ring.algring) # fragile?
@@ -302,14 +302,8 @@ cdef class MPolynomialRing_libsingular(MPolynomialRing_generic):
     def __dealloc__(self):
         """
         """
-        ##################################################
-        #
-        # TODO: This breaks like crazy on 32-bit linux.
-        #
-        # rDelete(self._ring)
-        #
-        ##################################################
-
+        rChangeCurrRing(self._ring)
+        rDelete(self._ring)
 
     cdef _coerce_c_impl(self, element):
         """

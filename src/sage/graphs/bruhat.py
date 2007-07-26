b0d1f61d847f31a09a7183f842d6fcd9967b552a
Emily Kirkman <eakirkman@gmail.com>
2007-07-26 01:55:52 -0700

    version one of bruhat classes

--- src/sage/graphs/bruhat.py
+++ src/sage/graphs/bruhat.py
@@ -1,6 +1,16 @@
+"""
+A module for dealing with Bruhat Intervals of the symmetric group.
+Contains classes for BruhatNode, BruhatSn and BruhatInterval.
 
+AUTHORS:
+    - Sean Howe, Robert Miller, Emily Kirkman (2007-07-24): initial version
+
+TODO:
+ - all sub intervals (maybe own class)
+"""
 #*****************************************************************************
-#           Copyright (C) 2007 Sean Howe and Emily A. Kirkman
+#           Copyright (C) 2007 Sean Howe, Emily A. Kirkman
+#                                   and Robert L. Miller
 #
 # Distributed  under  the  terms  of  the  GNU  General  Public  License (GPL)
 #                         http://www.gnu.org/licenses/
@@ -8,6 +18,10 @@
 
 class BruhatNode():
     """
+    A node in a BruhatInterval.  Contains instance field variables to
+    hold the permutation, degree and up and down neighbors of self.
+
+    (Helper Class for BruhatInterval -- shouldn't get called on its own).
     """
     def __init__(self):
         self.__permutation__ = None
@@ -16,19 +30,60 @@ class BruhatNode():
         self.__down_connected__ = [] # nodes that self covers
 
     def __get_copy__(self):
+        """
+        Returns a deep copy of self without neighbor information.
+        (For use in finding subintervals).
+        """
         copy=BruhatNode()
         copy.__permutation__=self.__permutation__
         return copy
 
 class BruhatInterval():
+    """
+    Class BruhatInterval.
 
-    def __init__(self,height):
+    A class for constructing bruhat intervals on symmetric groups.
+    Upon construction, a list structure is created to store the nodes
+    of the bruhat interval at each height, and a graph variable is
+    initialized to None, where the create_graph function will later
+    store the DiGraph representing the interval.
+
+    INPUT:
+        end_height -- the greatest height of the interval
+        start_height -- the least height of the interval (note that
+            the identity is height=0)
+
+    EXAMPLES:
+        sage: b = BruhatSn(4)
+        sage.: (b.get_graph()).show()
+        sage: b_sub = b.sub_interval_from_id((4,2,3,1),5)
+        sage.: (b_sub.get_graph()).show()
+    """
+    def __init__(self, end_height, start_height=None):
+        if start_height is None:
+            start_height = 0
         self.__graph__ = None
         self.__nodes__ = []
-        for i in range(0,height+1):
+        for i in range(0,end_height+1):
             self.__nodes__.append([])
 
-    def create_graph(self):
+    def get_graph(self):
+        """
+        Returns the DiGraph representing the Bruhat Interval.
+
+        EXAMPLES:
+            sage: b = BruhatSn(4)
+            sage.: (b.get_graph()).show()
+            sage: b_sub = b.sub_interval_from_id((4,2,3,1),5)
+            sage.: (b_sub.get_graph()).show()
+        """
+        return self.__graph__
+
+    def _create_graph(self):
+        """
+        Creates a DiGraph representing the Bruhat Interval and stores
+        it in self.__graph__
+        """
         from sage.graphs import graph
 
         self.__graph__ = graph.DiGraph()
@@ -40,6 +95,20 @@ class BruhatInterval():
                         self.__graph__.add_arc(node.__permutation__, node_above.__permutation__)
 
     def _sub_interval_add_below(self, sub_interval, to_add, height, added_by):
+        """
+        A recursive helper function for generating sub intervals.  (Should
+        not be called directly).
+
+        INPUT:
+            sub_interval -- a BruhatInterval to add nodes to (recursively)
+            to_add -- the BruhatNode to add next, which will be copied and
+                added to the current sub_interval, unless it is already in
+                the sub_interval; in which case a connection will be added
+                between the copy and added_by
+            height -- the current height (i.e.: the height of to_add)
+            added_by -- a BruhatNode in sub_interval that is immediately
+                above to_add
+        """
         alreadyAdded=False
         x=None
         for x in sub_interval.__nodes__[height]:
@@ -62,6 +131,24 @@ class BruhatInterval():
             self._sub_interval_add_below(sub_interval,child,height-1,adding)
 
     def sub_interval_from_id(self, end_perm, height):
+        """
+        Returns a BruhatInterval instance for the subinterval of self
+        that starts at the identity and ends at end_perm.
+
+        INPUT:
+            end_perm -- the permutation at maximal height in the sub interval
+            height -- the height of end_perm
+
+        NOTE:
+            If it is known that you will start from the identity, using this
+            method is much faster than the general sub_interval function.
+
+        EXAMPLES:
+            sage: b = BruhatSn(4)
+            sage.: (b.get_graph()).show()
+            sage: b_sub = b.sub_interval_from_id((4,2,3,1),5)
+            sage.: (b_sub.get_graph()).show()
+        """
         sub_interval=BruhatInterval(height)
 
         x=None
@@ -70,12 +157,64 @@ class BruhatInterval():
                 break
         self._sub_interval_add_below(sub_interval, x, height, None)
 
+        sub_interval._create_graph()
         return sub_interval
+
+    def is_isomorphic(self, other):
+        """
+        The comparison operator for two instances of BruhatInterval.
+        Returns True if the two intervals are isomorphic, and False
+        otherwise.
+
+        Relies on NICE algorithm isomorphism tester.
+
+        INPUT:
+            other -- an instance of BruhatInterval to test for
+                isomorphism with self
+
+        EXAMPLES:
+            sage: b = BruhatSn(4)
+            sage: b.__graph__.show()
+            sage: b_sub = b.sub_interval_from_id((4,2,3,1),5)
+            sage: b_sub.__graph__.show()
+            sage: b.is_isomorphic(b_sub)
+            False
+            sage: a1 = b.sub_interval_from_id((2,3,1,4),2)
+            sage: a2 = b.sub_interval_from_id((3,1,2,4),2)
+            sage: a1.is_isomorphic(a2)
+            True
+        """
+        g = self.__graph__
+        h = other.__graph__
+
+        # TODO : When NICE is updated, remove the following 4 lines:
+        if g.size() != h.size():
+            return False
+        if g.order() != h.order():
+            return False
+
+        return g.is_isomorphic(h)
 
 class BruhatSn(BruhatInterval):
     """
-    """
+    Class BruhatSn.
 
+    Constructs the Bruhat Interval (or a portion of it up to user-defined
+    height) from the symmetric group.
+
+    INPUT:
+        n -- As in S_n !
+        height -- the height to build the interval up to.  (If left as
+            default, then BruhatSn will construct the entire group).
+
+    EXAMPLES:
+        sage: glist = []
+        sage: for i in range(2,6):
+        ...       b = BruhatSn(i)
+        ...       glist.append(b.get_graph())
+        ...
+        sage.: graphs_list.show_graphs(glist, layout='spring')
+    """
     def __init__(self, n, height=None):
         if height is None:
             height = n*(n-1)/2
@@ -84,8 +223,12 @@ class BruhatSn(BruhatInterval):
 
         first_perm=tuple(range(1,n+1))
         self._add_above(first_perm,0,height,None)
+        self._create_graph()
 
     def _swapitch(self, i, j, k):
+        """
+        TODO : Robert - What does this do????
+        """
         if k == i:
             return j
         elif k == j:
@@ -94,6 +237,16 @@ class BruhatSn(BruhatInterval):
             return k
 
     def _add_above(self, permutation, height, max_height, added_by):
+        """
+        Recursive helper function for adding nodes above current place
+        in interval.
+
+        INPUT:
+            permutation -- the current permutation
+            height -- the current height
+            max_height -- the stopping height
+            added_by -- the node below
+        """
         permutation_length = len(permutation)
         already_added = False
 
@@ -128,6 +281,41 @@ class BruhatSn(BruhatInterval):
                         w = tuple([permutation[self._swapitch(i,j,k)] for k in range(0,permutation_length)])
                         self._add_above(w, height+1, max_height, new_node)
 
+    def show_graph(self, labels=False):
+        """
+        Shows the graph nicely (i.e.: with nodes of same height drawn at
+        same height).
 
+        INPUT:
+            labels -- whether or not to display node labels (permutations)
+        """
+        heights=height_unknown(self.__graph__)
+        self.__graph__.show(heights=heights, vertex_size=10, vertex_labels=labels)
 
+def height_unknown(G):
+    """
+    Helper function to determine height for plotting.
+    """
+    h = {}
+    for v in G:
+        if G.in_degree(v) == 0:
+            break
+    i = 0
+    h = recurse_unknown(G, i, v, h)
+    return h
+
+def recurse_unknown(G, i, v, h, seen=None):
+    """
+    Helper function to determine height for plotting.
+    """
+    if seen is None: seen = []
+    seen.append(v)
+    try:
+        h[i].append(v)
+    except:
+        h[i] = [v]
+    for a, b, _ in G.outgoing_arc_iterator(v):
+        if b not in seen:
+            h = recurse_unknown(G, i+1, b, h, seen)
+    return h
 

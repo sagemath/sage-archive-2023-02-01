fb8e69ef5695b248e3f4f6771e1c8d1f998eb6b5
William Stein <wstein@gmail.com>
2007-07-28 17:30:58 -0700

    Added copyright header'd version.

--- src/sage/combinat/partitions_c.cc
+++ src/sage/combinat/partitions_c.cc
@@ -1,3 +1,22 @@
+/*
+    Program to compute the number of partitions of n.
+
+    Author: Jonathan Bober
+    Version: .1 (7/28/2007)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 
 #include <cmath>

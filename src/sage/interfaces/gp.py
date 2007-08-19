5bdbe97cb347782dd55a357f16ddc098db1bd663
Mike Hansen <mhansen@gmail.com>
2007-08-19 13:05:54 -0400

    Fixed bug #456.

--- src/sage/interfaces/gp.py
+++ src/sage/interfaces/gp.py
@@ -91,6 +91,7 @@ from expect import Expect, ExpectElement, ExpectFunction, FunctionElement
 from sage.misc.misc import verbose
 from sage.libs.pari.all import pari
 import sage.rings.all
+import sage.rings.complex_number
 
 class Gp(Expect):
     """
@@ -330,7 +331,7 @@ class GpElement(ExpectElement):
         real = str(self.real()).replace(' E','e')
         imag = str(self.imag()).replace(' E','e')
         GP.set_real_precision(orig)
-        return sage.rings.all.ComplexNumber(real, imag )
+        return sage.rings.complex_number.ComplexNumber(CC, real, imag )
 
     def __len__(self):
         return int(self.length())

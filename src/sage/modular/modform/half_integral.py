f7f04fd98dab8b02c1825b340c8b4205f17f2314
William Stein <wstein@gmail.com>
2007-08-21 07:29:43 -0700

    Reorganize some code related to modular forms.

--- src/sage/modular/modform/half_integral.py
+++ src/sage/modular/modform/half_integral.py
@@ -7,93 +7,13 @@ AUTHORS:
     -- William Stein, 2007-08
 """
 
-import math
-
-from sage.rings.all  import Integer, ZZ
 from sage.matrix.all import MatrixSpace
 from sage.modular.dirichlet import DirichletGroup
 
-from sage.misc.all import verbose
-
-from sage.misc.all import cputime
-
 import constructor
 
+from theta import theta2_qexp, theta_qexp
 
-def theta2_qexp(prec=10, var='q', K=ZZ):
-    r"""
-    Return the $q$-expansion of the $\theta$ series $\theta_2$.
-
-    INPUT:
-        prec -- integer; the absolute precision of the output
-        var -- (default: 'q') variable name
-        K -- (default: ZZ) base ring of answer
-
-    OUTPUT:
-        a power series over K
-
-    EXAMPLES:
-        sage: theta2_qexp(18)
-        q + q^9 + O(q^18)
-        sage: theta2_qexp(49)
-        q + q^9 + q^25 + O(q^49)
-        sage: theta2_qexp(100, 'q', QQ)
-        q + q^9 + q^25 + q^49 + q^81 + O(q^100)
-        sage: f = theta2_qexp(100, 't', GF(3)); f
-        t + t^9 + t^25 + t^49 + t^81 + O(t^100)
-        sage: parent(f)
-        Power Series Ring in t over Finite Field of size 3
-        sage: theta2_qexp(200)
-        q + q^9 + q^25 + q^49 + q^81 + q^121 + q^169 + O(q^200)
-    """
-    prec = Integer(prec)
-    if prec <= 0:
-        raise ValueError, "prec must be positive"
-    v = [Integer(0)] * prec
-    one = Integer(1)
-    n = int(math.sqrt(prec))
-    if n*n < prec:
-        n += 1
-    for m in xrange(1, n, 2):
-        v[m*m] = one
-    return K[[var]](v, prec=prec)
-
-def theta3_qexp(prec=10, var='q', K=ZZ):
-    r"""
-    Return the $q$-expansion of the $\theta$ series $\theta_2$.
-
-    INPUT:
-        prec -- integer; the absolute precision of the output
-        var -- (default: 'q') variable name
-        K -- (default: ZZ) base ring of answer
-
-    OUTPUT:
-        a power series over K
-
-    EXAMPLES:
-        sage: theta3_qexp(25)
-        1 + 2*q + 2*q^4 + 2*q^9 + 2*q^16 + O(q^25)
-        sage: theta3_qexp(10)
-        1 + 2*q + 2*q^4 + 2*q^9 + O(q^10)
-        sage: theta3_qexp(100)
-        1 + 2*q + 2*q^4 + 2*q^9 + 2*q^16 + 2*q^25 + 2*q^36 + 2*q^49 + 2*q^64 + 2*q^81 + O(q^100)
-        sage: theta3_qexp(100, 't')
-        1 + 2*t + 2*t^4 + 2*t^9 + 2*t^16 + 2*t^25 + 2*t^36 + 2*t^49 + 2*t^64 + 2*t^81 + O(t^100)
-        sage: theta3_qexp(100, 't', GF(2))
-        1 + O(t^100)
-    """
-    prec = Integer(prec)
-    if prec <= 0:
-        raise ValueError, "prec must be positive"
-    v = [Integer(0)] * prec
-    v[0] = Integer(1)
-    two = Integer(2)
-    n = int(math.sqrt(prec))
-    if n*n != prec:
-        n += 1
-    for m in xrange(1, n):
-        v[m*m] = two
-    return K[[var]](v, prec=prec)
 
 def half_integral_weight_modform_basis(chi, k, prec):
     r"""
@@ -185,13 +105,14 @@ def half_integral_weight_modform_basis(chi, k, prec):
     B   = C.basis()
 
     # This computation of S below -- of course --dominates the whole function.
-    tm  = cputime()
+    #from sage.misc.all import cputime
+    #tm  = cputime()
     #print "Computing basis..."
     S   = [f.q_expansion(prec) for f in B]
     #print "Time to compute basis", cputime(tm)
 
     T2  = theta2_qexp(prec)
-    T3  = theta3_qexp(prec)
+    T3  = theta_qexp(prec)
     n   = len(S)
     MS  = MatrixSpace(M.base_ring(), 2*n, prec)
     A   = MS.zero_matrix()

097d003e08e7af9cdb811767b9ced6ef5a3166ff
William Stein <wstein@gmail.com>
2007-08-27 17:03:06 -0700

    Incorporated new version of desolvers into SAGE.

--- src/sage/calculus/desolvers.py
+++ src/sage/calculus/desolvers.py
@@ -1,6 +1,6 @@
 """
 This file contains functions useful for solving differential equations
-which occur commonly in a 1st semster differential equations course.
+which occur commonly in a 1st semester differential equations course.
 
 * desolve -- Computes the "general solution" to a 1st or 2nd order
              ODE via maxima.
@@ -42,35 +42,41 @@ maxima = Maxima()
 
 def desolve(de,vars):
     """
-    Solves a 1st or 2nd order ODE via maxima. Initials conditions
+    Solves a 1st or 2nd order linear ODE via maxima. Initials conditions
     are not given.
 
     INPUT:
-        de    -- a string representing the ODE
+        de    -- a lambda expression representing the ODE
                  (eg, de = "diff(f(x),x,2)=diff(f(x),x)+sin(x)")
         vars  -- a list of strings representing the variables
                  (eg, vars = ["x","y"], if x is the independent
                   variable and y is the dependent variable)
 
     EXAMPLES:
-        sage: desolvers.desolve("x**2*diff(y,x) + 3*y*x = sin(x)/x",["x","y"])
-        y=(%c-cos(x))/x^3
+        sage: from sage.calculus.desolvers import desolve
+        sage: t = var('t')
+        sage: x = function('x', t)
+        sage: de = lambda y: diff(y,t) + y - 1
+        sage: desolve(de(x(t)),[x,t])
+        '%e^-t*(%e^t+%c)'
 
     AUTHOR: David Joyner (1-2006)
     """
-    maxima("depends("+vars[1]+","+vars[0]+");")
-    maxima("de:"+de+";")
-    cmd = "ode2("+de+","+vars[1]+","+vars[0]+");"
-    print maxima.eval(cmd)
-    #Maxima.eval(cmd)
+    #maxima("depends("+vars[1]+","+vars[0]+");")
+    #maxima("de:"+de+";")
+    #cmd = "ode2("+de+","+vars[1]+","+vars[0]+");"
+    #return maxima.eval(cmd)
+    de0 = de._maxima_()
+    soln = de0.ode2(vars[0],vars[1])
+    return soln.rhs()._maxima_init_()
 
-def desolve_laplace(de,vars,ics=None):
+#def desolve_laplace2(de,vars,ics=None):
     """
-    Solves an ODE using laplace transforms via maxima. Initials conditions
+    Solves an ODE using laplace transforms via maxima. Initial conditions
     are optional.
 
     INPUT:
-        de    -- a string representing the ODE
+        de    -- a lambda expression representing the ODE
                  (eg, de = "diff(f(x),x,2)=diff(f(x),x)+sin(x)")
         vars  -- a list of strings representing the variables
                  (eg, vars = ["x","f"], if x is the independent
@@ -80,29 +86,72 @@ def desolve_laplace(de,vars,ics=None):
                  (eg, f(0)=1, f'(0)=2 is ics = [0,1,2])
 
     EXAMPLES:
-        sage: desolvers.desolve_laplace("diff(f(x),x,2)=2*diff(f(x),x)-f(x)",["x","f"])
-        f(x)=x*%e^x*(?%at('diff(f(x),x,1),x=0))-f(0)*x*%e^x+f(0)*%e^x
-        sage: desolvers.desolve_laplace("diff(f(x),x,2)=2*diff(f(x),x)-f(x)",["x","f"],[0,1,2])
-        f(x)=x*%e^x+%e^x
+        sage.: from sage.calculus.desolvers import desolve_laplace
+        sage.: x = var('x')
+        sage.: f = function('f', x)
+        sage.: de = lambda y: diff(y,x,x) - 2*diff(y,x) + y
+        sage.: desolve_laplace(de(f(x)),[f,x])
+         #x*%e^x*(?%at('diff('f(x),x,1),x=0))-'f(0)*x*%e^x+'f(0)*%e^x
+        sage.: desolve_laplace(de(f(x)),[f,x],[0,1,2])  ## IC option does not work
+         #x*%e^x*(?%at('diff('f(x),x,1),x=0))-'f(0)*x*%e^x+'f(0)*%e^x
+
+    AUTHOR: David Joyner (1st version 1-2006, 8-2007)
+    """
+#    ######## this method seems reasonable but doesnt work for some reason
+#    name0 = vars[0]._repr_()[0:(len(vars[0]._repr_())-2-len(str(vars[1])))]
+#    name1 = str(vars[1])
+#    #maxima("de:"+de+";")
+#    if ics!=None:
+#        ic0 = maxima("ic:"+str(vars[1])+"="+str(ics[0]))
+#        d = len(ics)
+#        for i in range(d-1):
+#            maxima(vars[0](vars[1])).diff(vars[1],i).atvalue(ic0,ics[i+1])
+#    de0 = de._maxima_()
+#    #cmd = "desolve("+de+","+vars[1]+"("+vars[0]+"));"
+#    #return maxima.eval(cmd)
+#    return de0.desolve(vars[0]).rhs()
+
+def desolve_laplace(de,vars,ics=None):
+    """
+    Solves an ODE using laplace transforms via maxima. Initials conditions
+    are optional.
+
+    INPUT:
+        de    -- a lambda expression representing the ODE
+                 (eg, de = "diff(f(x),x,2)=diff(f(x),x)+sin(x)")
+        vars  -- a list of strings representing the variables
+                 (eg, vars = ["x","f"], if x is the independent
+                  variable and f is the dependent variable)
+        ics   -- a list of numbers representing initial conditions,
+                 with symbols allowed which are represented by strings
+                 (eg, f(0)=1, f'(0)=2 is ics = [0,1,2])
+
+    EXAMPLES:
+        sage: from sage.calculus.desolvers import desolve_laplace
+        sage: x = var('x')
+        sage: f = function('f', x)
+        sage: de = lambda y: diff(y,x,x) - 2*diff(y,x) + y
+        sage: desolve_laplace(de(f(x)),["x","f"])
+        "x*%e^x*(?%at('diff(f(x),x,1),x=0))-f(0)*x*%e^x+f(0)*%e^x"
+        sage: desolve_laplace(de(f(x)),["x","f"],[0,1,2])
+         'x*%e^x+%e^x'
 
     WARNING:
-        The second SAGE command in the above example sets the values
-        of f(0) and f'(0) in maxima, so subsequent ODEs involving
-        these variables will have these initial conditions
+        The second SAGE command in the above example sets the values of f(0) and f'(0)
+        in Maxima, so subsequent ODEs involving these variables will have these initial conditions
         automatically imposed.
 
-    AUTHOR: David Joyner (1-2006)
+    AUTHOR: David Joyner (1-2006,8-2007)
     """
-    maxima("de:"+de+";")
+    maxima("de:"+de._repr_()+"=0;")
     if ics!=None:
         d = len(ics)
         for i in range(0,d-1):
             ic = "atvalue(diff("+vars[1]+"("+vars[0]+"),"+str(vars[0])+","+str(i)+"),"+str(vars[0])+"="+str(ics[0])+","+str(ics[1+i])+")"
             maxima(ic)
             #print i,ic
-    cmd = "desolve("+de+","+vars[1]+"("+vars[0]+"));"
-    print maxima.eval(cmd)
-    #maxima.eval(cmd)
+    cmd = "desolve("+de._repr_()+","+vars[1]+"("+vars[0]+"));"
+    return maxima(cmd).rhs()._maxima_init_()
 
 def desolve_system(des,vars,ics=None):
     """
@@ -114,7 +163,7 @@ def desolve_system(des,vars,ics=None):
         de    -- a list of strings representing the ODEs in maxima notation
                  (eg, de = "diff(f(x),x,2)=diff(f(x),x)+sin(x)")
         vars  -- a list of strings representing the variables
-                 (eg, vars = ["t","x","y"], where t is the indeendent variable
+                 (eg, vars = ["t","x","y"], where t is the independent variable
                   and x,y the dependent variables)
         ics   -- a list of numbers representing initial conditions
                  (eg, x(0)=1, y(0)=2 is ics = [0,1,2])
@@ -125,67 +174,94 @@ def desolve_system(des,vars,ics=None):
         automatically imposed.
 
     EXAMPLES:
-        sage: des=["'diff(x(t),t)=-4*y(t)","'diff(y(t),t)=-x(t)"]
+        sage: from sage.calculus.desolvers import desolve_system
+        sage: t = var('t')
+        sage: x = function('x', t)
+        sage: y = function('y', t)
+        sage: de1 = lambda z: diff(z[0],t) + z[1] - 1
+        sage: de2 = lambda z: diff(z[1],t) - z[0] + 1
+        sage: des = [de1([x(t),y(t)]),de2([x(t),y(t)])]
         sage: vars = ["t","x","y"]
-        sage: desolvers.desolve_system(des,vars)
-        [x(t)=(2*y(0)+x(0))*%e^-(2*t)/2-(2*y(0)-x(0))*%e^(2*t)/2,y(t)=(2*y(0)-x(0))*%e^(2*t)/4+(2*y(0)+x(0))*%e^-(2*t)/4]
-	sage: des=["'diff(x(t),t)=-4*y(t)","'diff(y(t),t)=-x(t)"]
-    	sage: vars = ["t","x","y"]
-    	sage: desolvers.desolve_system(des,vars,[0,270,90])
-        [x(t)=45*%e^(2*t)+225*%e^-(2*t),y(t)=225*%e^-(2*t)/2-45*%e^(2*t)/2]
-    	sage: des=["'diff(x(t),t)=-4*y(t)","'diff(y(t),t)=-z(t)","'diff(z(t),t)=-2*x(t)"]
-    	sage: vars = ["t","x","y","z"]
-    	sage: ics=[0,270,90,100]
-    	sage: desolvers.desolve_system(des,vars,ics)
-    	[x(t)=%e^t*(260*cos(sqrt(3)*t)/3-80*sin(sqrt(3)*t)/sqrt(3))+550*%e^-(2*t)/3,y(t)=%e^t*(85*sin(sqrt(3)*t)/sqrt(3)-5*cos(sqrt(3)*t)/3)+275*%e^-(2*t)/3,z(t)=%e^t*(-90*sin(sqrt(3)*t)/sqrt(3)-250*cos(sqrt(3)*t)/3)+550*%e^-(2*t)/3]
+        sage: desolve_system(des,vars)
+        ['(1-y(0))*sin(t)+(x(0)-1)*cos(t)+1', '(x(0)-1)*sin(t)+(y(0)-1)*cos(t)+1']
+        sage: ics = [0,1,-1]
+        sage: soln = desolve_system(des,vars,ics); soln
+        ['2*sin(t)+1', '1-2*cos(t)']
+        sage: solnx = lambda s: RR(eval(soln[0].replace("t","s")))
+        sage: solnx(3)
+        1.28224001611973
+        sage: solny = lambda s: RR(eval(soln[1].replace("t","s")))
+        sage: P1 = plot([solnx,solny],0,1)
+        sage: P2 = parametric_plot((solnx,solny),0,1)
 
-    AUTHOR: David Joyner (3-2006)
+        Now type show(P1), show(P2) to view these.
+
+
+    AUTHOR: David Joyner (3-2006, 8-2007)
     """
     d = len(des)
+    dess = [de._repr_() + "=0" for de in des]
     for i in range(d):
-        cmd="de:"+des[int(i)]+";"
+        cmd="de:" + dess[int(i)] + ";"
         maxima(cmd)
-    desstr = "["+",".join(des)+"]"
-    #print desstr
+    desstr = "[" + ",".join(dess) + "]"
     d = len(vars)
-    varss = list(vars[i]+"("+vars[0]+")" for i in range(1,d))
-    varstr = "["+",".join(varss)+"]"
-    #print varstr
+    varss = list(vars[i] + "(" + vars[0] + ")" for i in range(1,d))
+    varstr = "[" + ",".join(varss) + "]"
     if ics!=None:
-        d = len(ics)
+        #d = len(ics) ## must be same as len(des)
         for i in range(1,d):
-            ic = "atvalue("+vars[int(i)]+"("+vars[0]+"),"+str(vars[0])+"="+str(ics[0])+","+str(ics[int(i)])+")"
+            ic = "atvalue(" + vars[int(i)] + "("+vars[0] + ")," + str(vars[0]) + "=" + str(ics[0]) + "," + str(ics[int(i)]) + ")"
             maxima(ic)
-    cmd = "desolve("+desstr+","+varstr+");"
-    #print cmd
-    print maxima.eval(cmd)
-    #maxima.eval(cmd)
+    cmd = "desolve(" + desstr + "," + varstr + ");"
+    soln = maxima(cmd)
+    return [f.rhs()._maxima_init_() for f in soln]
 
 def eulers_method(f,x0,y0,h,x1,method="table"):
     """
-    This implements Euler's method for
-    finding numerically the solution of the 1st order
-    ODE y' = f(x,y), y(a)=c. The "x" column of the table
-    increments from x0 to x1 by h (so (x1-x0)/h must be
-    an integer). In the "y" column, the new y-value equals the
-    old y-value plus the corresponding entry in the
+    This implements Euler's method for finding numerically the solution of the 1st order
+    ODE y' = f(x,y), y(a)=c. The "x" column of the table increments from
+    x0 to x1 by h (so (x1-x0)/h must be an integer). In the "y" column,
+    the new y-value equals the old y-value plus the corresponding entry in the
     last column.
 
+    *For pedagogical purposes only.*
+
     EXAMPLES:
+        sage: from sage.calculus.desolvers import eulers_method
         sage: x,y = PolynomialRing(QQ,2,"xy").gens()
-        sage: desolvers.eulers_method(5*x+y-5,0,1,1/2,1,method="none")
+        sage.: eulers_method(5*x+y-5,0,1,1/2,1)
+         x                    y                  h*f(x,y)
+         0                    1                   -2
+       1/2                   -1                 -7/4
+         1                -11/4                -11/8
+        sage: x,y = PolynomialRing(QQ,2,"xy").gens()
+        sage: eulers_method(5*x+y-5,0,1,1/2,1,method="none")
         [[0, 1], [1/2, -1], [1, -11/4], [3/2, -33/8]]
         sage: RR = RealField(sci_not=0, prec=4, rnd='RNDU')
         sage: x,y = PolynomialRing(RR,2,"xy").gens()
-        sage: desolvers.eulers_method(5*x+y-5,0,1,1/2,1,method="None")
+        sage: eulers_method(5*x+y-5,0,1,1/2,1,method="None")
         [[0, 1], [1/2, -1.0], [1, -2.7], [3/2, -4.0]]
         sage: RR = RealField(sci_not=0, prec=4, rnd='RNDU')
         sage: x,y=PolynomialRing(RR,2,"xy").gens()
-        sage: desolvers.eulers_method(5*x+y-5,0,1,1/2,1)
+        sage.: eulers_method(5*x+y-5,0,1,1/2,1)
          x                    y                  h*f(x,y)
          0                    1                -2.00
        1/2                -1.00                -1.75
          1                -2.75                -1.37
+        sage: x,y=PolynomialRing(QQ,2,"xy").gens()
+        sage: eulers_method(5*x+y-5,1,1,1/3,2)
+                 x                    y                  h*f(x,y)
+                 1                    1                  1/3
+               4/3                  4/3                    1
+               5/3                  7/3                 17/9
+                 2                 38/9                83/27
+        sage: eulers_method(5*x+y-5,0,1,1/2,1,method="none")
+        [[0, 1], [1/2, -1], [1, -11/4], [3/2, -33/8]]
+        sage: pts = eulers_method(5*x+y-5,0,1,1/2,1,method="none")
+        sage: P1 = list_plot(pts)
+        sage: P2 = line(pts)
+        sage.: show(P1+P2)
 
     AUTHOR: David Joyner
     """
@@ -210,28 +286,29 @@ def eulers_method_2x2(f,g, t0, x0, y0, h, t1,method="table"):
         x' = f(t, x, y), x(t0)=x0.
         y' = g(t, x, y), y(t0)=y0.
     The "t" column of the table increments from t0 to t1 by
-    h (so (t1-t0)/h must be an integer).
-    In the "x" column, the new x-value equals the
-    old x-value plus the corresponding entry in the next
-    (third) column. In the "y" column, the new y-value equals the
-    old y-value plus the corresponding entry in the next
-    (last) column.
+    h (so (t1-t0)/h must be an integer). In the "x" column, the new x-value equals the
+    old x-value plus the corresponding entry in the next (third) column.
+    In the "y" column, the new y-value equals the old y-value plus the
+    corresponding entry in the next (last) column.
+
+    *For pedagogical purposes only.*
 
     EXAMPLES:
+        sage: from sage.calculus.desolvers import eulers_method_2x2
         sage: t, x, y = PolynomialRing(QQ,3,"txy").gens()
         sage: f = x+y+t; g = x-y
         sage: eulers_method_2x2(f,g, 0, 0, 0, 1/3, 1,method="none")
         [[0, 0, 0], [1/3, 0, 0], [2/3, 1/9, 0], [1, 10/27, 1/27], [4/3, 68/81, 4/27]]
-        sage: desolvers.eulers_method_2x2(f,g, 0, 0, 0, 1/3, 1)
+        sage.:. eulers_method_2x2(f,g, 0, 0, 0, 1/3, 1)
          t                    x                h*f(t,x,y)                    y           h*g(t,x,y)
          0                    0                         0                    0                    0
        1/3                    0                       1/9                    0                    0
        2/3                  1/9                      7/27                    0                 1/27
          1                10/27                     38/81                 1/27                  1/9
-        sage: RR = RealField(sci_not=0, prec=4, rnd='RNDU')
-        sage: t,x,y=PolynomialRing(RR,3,"txy").gens()
-        sage: f = x+y+t; g = x-y
-        sage: eulers_method_2x2(f,g, 0, 0, 0, 1/3, 1)
+        sage.: RR = RealField(sci_not=0, prec=4, rnd='RNDU')
+        sage.: t,x,y=PolynomialRing(RR,3,"txy").gens()
+        sage.: f = x+y+t; g = x-y
+        sage.:. eulers_method_2x2(f,g, 0, 0, 0, 1/3, 1)
          t                    x                h*f(t,x,y)                    y           h*g(t,x,y)
          0                    0                     0.000                    0                0.000
        1/3                0.000                     0.125                0.000                0.000
@@ -289,12 +366,16 @@ def eulers_method_2x2_plot(f,g, t0, x0, y0, h, t1):
     (xrange[0],xrange[1]) x (yrange[0],yrange[1])
     and plots using Euler's method the numerical solution of
     the 1st order ODEs x' = f(t,x,y), x(a)=x0, y' = g(t,x,y), y(a) = y0.
+
+    *For pedagogical purposes only.*
+
     ===
     The following example plots the solution to theta''+sin(theta)=0,
     theta(0)=3/4, theta'(0) = 0.  Type P[0].show() to plot the solution,
     (P[0]+P[1]).show() to plot (t,theta(t)) and (t,theta'(t)).
     ===
     EXAMPLES:
+        sage: from sage.calculus.desolvers import eulers_method_2x2_plot
         sage: f = lambda z : z[2]; g = lambda z : -sin(z[1])
         sage: P = eulers_method_2x2_plot(f,g, 0.0, 0.75, 0.0, 0.1, 1.0)
     """

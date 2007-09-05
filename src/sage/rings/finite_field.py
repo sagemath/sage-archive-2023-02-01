decd4a49c6584e49a3532b15a5f029e062373f81
William Stein <wstein@gmail.com>
2007-09-05 08:30:03 -0700

    Fix a bug in finite field caching that Martin's patch introduced.

--- src/sage/rings/finite_field.py
+++ src/sage/rings/finite_field.py
@@ -168,7 +168,7 @@ def FiniteField(order, name=None, modulus=None, names=None,
     if elem_cache is None:
         elem_cache = order < 500
 
-    key = (order, name, modulus)
+    key = (order, name, modulus, args, list(kwds))
     if cache.has_key(key):
         K = cache[key]()
         if not K is None:

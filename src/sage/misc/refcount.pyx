00ca90c2fec40a11d582b45a2c7f44c4cfcd21ac
Robert Bradshaw <robertwb@math.washington.edu>
2007-09-06 18:05:57 -0700

    Added refcount function.

---
+++ src/sage/misc/refcount.pyx
@@ -0,0 +1,18 @@
+cdef extern from *:
+    ctypedef struct PyObject:
+        Py_ssize_t ob_refcnt
+
+def ref_count(o):
+    """
+    Return the number of references to \code{o}.
+
+    EXAMPLES:
+      sage: from sage.misc.refcount import ref_count
+      sage: ref_count(33)
+      1
+      sage: L = [-7] * 5
+      sage: ref_count(L[0])
+      6
+    """
+    # Exclude the reference on this call stack...
+    return (<PyObject *>o).ob_refcnt - 1

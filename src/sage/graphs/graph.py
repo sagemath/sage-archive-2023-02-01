9d99b3067055468566b7d2a83ce567571d306be2
Jason Grout <jason-sage@creativetrax.com>
2007-09-22 23:39:10 -0500

    Added docs and doctests.  Fixed a bug by checking that boundary is a list before setting the boundary member.

--- src/sage/graphs/graph.py
+++ src/sage/graphs/graph.py
@@ -559,7 +559,7 @@ class GenericGraph(SageObject):
 
     def add_vertex(self, name=None):
         """
-        Creates an isolated vertex.
+        Creates an isolated vertex.  If the vertex already exists, then nothing is done.
 
         INPUT:
         n -- Name of the new vertex. If no name is specified, then the vertex
@@ -585,6 +585,7 @@ class GenericGraph(SageObject):
     def add_vertices(self, vertices):
         """
         Add vertices to the (di)graph from an iterable container of vertices.
+        Vertices that already exist in the graph will not be added again.
 
         EXAMPLES:
             sage: d = {0: [1,4,5], 1: [2,6], 2: [3,7], 3: [4,8], 4: [9], 5: [7,8], 6: [8,9], 7: [9]}
@@ -601,7 +602,8 @@ class GenericGraph(SageObject):
 
     def delete_vertex(self, vertex):
         """
-        Deletes vertex, removing all incident edges.
+        Deletes vertex, removing all incident edges.  Deleting a non-existant
+        vertex will raise an exception.
 
         EXAMPLES:
             sage: G = graphs.WheelGraph(9)
@@ -610,6 +612,12 @@ class GenericGraph(SageObject):
             sage: D = DiGraph({0:[1,2,3,4,5],1:[2],2:[3],3:[4],4:[5],5:[1]})
             sage: D.delete_vertex(0); D
             Digraph on 5 vertices
+            sage: D.vertices()
+            [1, 2, 3, 4, 5]
+            sage: D.delete_vertex(0)
+            Traceback (most recent call last):
+            ...
+            NetworkXError: node 0 not in graph
 
         """
         self._nxg.delete_node(vertex)
@@ -617,12 +625,18 @@ class GenericGraph(SageObject):
     def delete_vertices(self, vertices):
         """
         Remove vertices from the (di)graph taken from an iterable container of
-        vertices.
+        vertices.  Deleting a non-existant vertex will raise an exception.
 
         EXAMPLE:
             sage: D = DiGraph({0:[1,2,3,4,5],1:[2],2:[3],3:[4],4:[5],5:[1]})
             sage: D.delete_vertices([1,2,3,4,5]); D
             Digraph on 1 vertex
+            sage: D.vertices()
+            [0]
+            sage: D.delete_vertices([1])
+            Traceback (most recent call last):
+            ...
+            NetworkXError: node 1 not in graph
 
         """
         self._nxg.delete_nodes_from(vertices)
@@ -631,13 +645,18 @@ class GenericGraph(SageObject):
         return self._boundary
 
     def set_boundary(self, boundary):
-        self._boundary = boundary
+        if isinstance(boundary,list):
+            self._boundary = boundary
 
     def vertex_boundary(self, vertices1, vertices2=None):
         """
         Returns a list of all vertices in the external boundary of vertices1,
         intersected with vertices2. If vertices2 is None, then vertices2 is the
-        complement of vertices1.
+        complement of vertices1.  This is much faster if vertices1 is smaller than
+        vertices2.
+
+        The external boundary of a set of vertices is the union of the neighborhoods
+        of each vertex in the set.
 
         EXAMPLE:
             sage: G = graphs.CubeGraph(4)

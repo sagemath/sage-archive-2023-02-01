876cfeae12de9d2a4615f782d6cd3a6fe5a5edc3
David Roe <roed@math.harvard.edu>
2007-10-05 03:27:00 -0400

    Added elementvar and idealappr to pari/gen.pyx

--- src/sage/libs/pari/gen.pyx
+++ src/sage/libs/pari/gen.pyx
@@ -4210,6 +4210,11 @@ cdef class gen(sage.structure.element.RingElement):
         _sig_on
         return self.new_gen(idealadd(self.g, t0, t1))
 
+    def idealappr(self, x, long flag=0):
+        t0GEN(x)
+        _sig_on
+        return self.new_gen(idealappr(self.g, t0))
+
     def idealdiv(self, x, y, long flag=0):
         t0GEN(x); t1GEN(y)
         _sig_on
@@ -4255,6 +4260,11 @@ cdef class gen(sage.structure.element.RingElement):
         t0GEN(x); t1GEN(p)
         _sig_on
         return idealval(self.g, t0, t1)
+
+    def elementval(self, x, p):
+        t0GEN(x); t1GEN(p)
+        _sig_on
+        return element_val(self.g, t0, t1)
 
     def modreverse(self):
         """

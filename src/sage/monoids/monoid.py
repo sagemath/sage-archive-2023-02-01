43273c1725e19b8194dd554bb266959169afd9df
Carl Witty <cwitty@newtonlabs.com>
2007-10-07 09:09:54 -0700

    Moved import into function to avoid circular import chain.

--- src/sage/monoids/monoid.py
+++ src/sage/monoids/monoid.py
@@ -2,7 +2,6 @@ r"""
 Base class for all monoids
 """
 
-import sage.categories.all
 from sage.structure.parent_gens import ParentWithGens
 
 def is_Monoid(x):
@@ -10,4 +9,10 @@ def is_Monoid(x):
 
 class Monoid_class(ParentWithGens):
     def category(self):
+        # Import this locally, rather than at the top level, to avoid
+        # a circular import chain
+        # (category_types -> sage.algebras.all -> sage.algebras.free_algebra ->
+        # sage.monoids.free_monoid -> sage.monoids.monoid ->
+        # sage.categories.all -> category_types)
+        import sage.categories.all
         return sage.categories.all.Monoids()

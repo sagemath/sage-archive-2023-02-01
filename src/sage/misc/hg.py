04ad8a95874916bef5c38676d9c67130d17e72bb
William Stein <wstein@gmail.com>
2007-10-12 18:27:30 -0700

    unset and set library path before running ssh-keygen.

--- src/sage/misc/hg.py
+++ src/sage/misc/hg.py
@@ -415,7 +415,7 @@ class HG:
     mv = rename
 
     def log(self, branches=None, keyword=None, limit=None,
-                  rev=None, merges=False, only_merges=False,
+                  rev=None, merges=True, only_merges=False,
                   patch=None, template=False, include=None,
                   exclude=None, verbose=False):
         """
@@ -432,7 +432,7 @@ class HG:
             limit    -- (integer, default: None, or 20 in notebook mdoe)
                         limit number of changes displayed
             rev      -- (integer) show the specified revision
-            merges   -- (bool, default: False) whether or not to show merges
+            merges   -- (bool, default: True) whether or not to show merges
             only_merges -- (bool, default: False) if true, show only merges
             patch    -- (string, default: None) show given patch
             template -- (string, default: None) display with template

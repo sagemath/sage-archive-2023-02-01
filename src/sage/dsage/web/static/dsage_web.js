e2616effe0a1026e09ef5aab3819364bd5788b84
Yi Qiang <yqiang@gmail.com>
2007-10-14 19:45:48 -0700

    More ajax hackery.

--- src/sage/dsage/web/static/dsage_web.js
+++ src/sage/dsage/web/static/dsage_web.js
@@ -1,48 +1,32 @@
-//enter refresh time in "minutes:seconds" Minutes should range from 0 to inifinity. Seconds should range from 0 to 59
-var limit="0:10"
-
-if (document.images){
-    var parselimit=limit.split(":")
-    parselimit=parselimit[0]*60+parselimit[1]*1
-}
-function beginrefresh(){
-    if (!document.images)
-        return
-    if (parselimit==1)
-        window.location.reload()
-    else{
-        parselimit-=1
-        curmin=Math.floor(parselimit/60)
-        cursec=parselimit%60
-        if (curmin!=0)
-            curtime=curmin+" minutes and "+cursec+" seconds left until page refresh!"
-        else
-            curtime=cursec+" seconds left until page refresh!"
-        window.status=curtime
-        setTimeout("beginrefresh()",1000)
-    }
-}
-
 function add_row() {
     var jobs_table = document.getElementById("jobs_table");
     top_row = jobs_table.insertRow(-1);
-    top_row.className = 'tr0'
-    first_cell = top_row.insertCell(0)
-    first_cell.innerHTML= 'Woot'
+    top_row.className = 'tr0';
+    first_cell = top_row.insertCell(0);
+    first_cell.innerHTML= 'Woot';
 }
 
 function init() {
-    add_row()
-    beginrefresh()
+
 }
 
-new Ajax.Request("/get_details?job_id=fooo",
+function build_jobs_table(transport) {
+    var xmldoc = XML.newDocument();
+    xmldoc.load(transport.responseText);
+}
+
+var get_jobs = new Ajax.PeriodicalUpdater('jobs_table', '/get_jobs',
   {
-    method:'post',
-    onSuccess: function(transport){
-      var response = transport.responseText || "no response text";
-      alert("Success! \n\n" + response);
-    },
-    onFailure: function(){ alert('Something went wrong...') }
+    method: 'get',
+    insertion: Insertion.Bottom,
+    frequency: 1,
+    decay: 2
   });
-window.onload=init()
+
+// new Ajax.Request("/get_jobs",
+//   {
+//     method:'post',
+//     onSuccess: build_jobs_table(transport),
+//     onFailure: function(){ alert('Something went wrong...') }
+//   });
+// window.onload=init()
--- src/sage/dsage/web/web_server.py
+++ src/sage/dsage/web/web_server.py
@@ -30,69 +30,72 @@ JS_FILE = os.path.join(DSAGE_LOCAL,'web/static/dsage_web.js')
 PROTOTYPE = os.path.join(DSAGE_LOCAL,'web/static/prototype.js')
 INDEX = os.path.join(DSAGE_LOCAL,'web/static/index.html')
 
-def create_jobs_table(jdicts):
-    """
-    Returns an HTML table of jobs given a list of job dictionaries.
-
-    """
-
-    html = """
-    <table class='jobs_table' cellspacing='0' padding='5px' border='solid #000 1px'>
-    <tr class='thead'>
-        <td>Job ID</td>
-        <td>Status</td>
-        <td>Username</td>
-        <td>Creation Time</td>
-        <td>Last Update</td>
-        <td>Priority</td>
-    </tr>
-    """
-
-    for jdict in jdicts:
-        html+="""
-        <tr class='tr0'>
-            <td><a href='get_details?job_id=%s'>%s</a></td>
-            <td>%s</td>
-            <td>%s</td>
-            <td>%s</td>
-            <td>%s</td>
-            <td>%s</td>
-        </tr>
-        """ % (jdict['job_id'],jdict['job_id'], jdict['status'],
-               jdict['username'], jdict['creation_time'],
-               jdict['update_time'], jdict['priority'])
-
-    html += '</table>'
-
-    return html
+# def create_jobs_table(jdicts):
+#     """
+#     Returns an HTML table of jobs given a list of job dictionaries.
+#
+#     """
+#
+#     html = """
+#     <table class='jobs_table' cellspacing='0' padding='5px' border='solid #000 1px'>
+#     <tr class='thead'>
+#         <td>Job ID</td>
+#         <td>Status</td>
+#         <td>Username</td>
+#         <td>Creation Time</td>
+#         <td>Last Update</td>
+#         <td>Priority</td>
+#     </tr>
+#     """
+#
+#     for i, jdict in enumerate(jdicts):
+#         html+="""
+#         <tr class='tr%s'
+#         """ % (i % 2)
+#         html+="""
+#             <td><a href='get_details?job_id=%s'>%s</a></td>
+#             <td>%s</td>
+#             <td>%s</td>
+#             <td>%s</td>
+#             <td>%s</td>
+#             <td>%s</td>
+#         </tr>
+#         """ % (jdict['job_id'],jdict['job_id'], jdict['status'],
+#                jdict['username'], jdict['creation_time'],
+#                jdict['update_time'], jdict['priority'])
+#
+#     html += '</table>'
+#
+#     return html
 
 class Toplevel(resource.Resource):
     addSlash = True
-    header = """
-    <html>
-    <header>
-        <link rel="stylesheet" type="text/css" href="dsage_web.css" />
-        <scritp src='prototype.js' type='text/javascript'></script>
-        <script src='dsage_web.js' type='text/javascript'></script>
-    </header>
-    <body>
-        <div id='header'>
-            <div id='title'><h1>DSAGE Job Status</h1></div>
-        </div>
-    """
-
-    footer = """
-    </body>
-    </html>
-    """
+    # header = """
+    # <html>
+    # <header>
+    #     <link rel="stylesheet" type="text/css" href="dsage_web.css" />
+    #     <scritp src='prototype.js' type='text/javascript'></script>
+    #     <script src='dsage_web.js' type='text/javascript'></script>
+    # </header>
+    # <body>
+    #     <div id='header'>
+    #         <div id='title'><h1>DSAGE Job Status</h1></div>
+    #     </div>
+    # """
+    #
+    # footer = """
+    # </body>
+    # </html>
+    # """
 
     def __init__(self, dsage_server):
         self.dsage_server = dsage_server
         self.child_get_details = GetJobDetails(self.dsage_server)
+        self.child_get_jobs = GetJobs(self.dsage_server)
 
     def render(self, ctx):
         jobs = self.dsage_server.jobdb.get_all_jobs()
-        jobs_html = create_jobs_table(jobs)
+        # jobs_html = create_jobs_table(jobs)
         f = open(INDEX)
         return http.Response(stream=f.read())
         # return http.Response(stream=self.header + jobs_html + self.footer)
@@ -100,6 +103,73 @@ setattr(Toplevel, 'child_dsage_web.css', static.File(CSS_FILE))
 setattr(Toplevel, 'child_dsage_web.js', static.File(JS_FILE))
 setattr(Toplevel, 'child_prototype.js', static.File(PROTOTYPE))
 # setattr(Toplevel, 'child_index.html', static.File(INDEX))
+
+class GetJobs(resource.PostableResource):
+    """
+    This resource returns a list of jobs in XML format to the client.
+
+    """
+
+    def __init__(self, dsage_server):
+        self.dsage_server = dsage_server
+        self.jdicts = []
+
+    def render(self, request):
+        jdicts = self.dsage_server.jobdb.get_all_jobs()
+        html = ""
+        if len(jdicts) != len(self.jdicts):
+            self.jdicts = jdicts
+            for i, jdict in enumerate(jdicts):
+                html+="""
+                <tr class='tr%s'
+                """ % (i % 2)
+                html+="""
+                    <td><a href='get_details?job_id=%s'>%s</a></td>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>%s</td>
+                </tr>
+                """ % (jdict['job_id'],jdict['job_id'], jdict['status'],
+                       jdict['username'], jdict['creation_time'],
+                       jdict['update_time'], jdict['priority'])
+
+            html += '</table>'
+        else:
+            pass
+        # xml_stream = self.build_xml(jdicts)
+
+        return http.Response(stream=html)
+
+    def build_xml(self, jdicts):
+        """
+        Builds an XML structure from a list of jdicts.
+
+        """
+
+
+        from xml.etree.ElementTree import (ElementTree as ET,
+                                           Element,
+                                           SubElement,
+                                           dump,
+                                           XML)
+        from cStringIO import StringIO
+        root = Element('jobs')
+
+        for jdict in jdicts:
+            job = SubElement(root, 'job')
+            for k, v in jdict.iteritems():
+                if k not in ('job_id', 'status', 'username', 'creation_time',
+                             'last_update', 'priority'):
+                    continue
+                job.set(k, str(v))
+
+        xml_stream = StringIO()
+        tree = ET(root) # Wrap it into an ElementTree
+        tree.write(xml_stream)
+
+        return xml_stream
 
 class GetJobDetails(resource.PostableResource):
     """
@@ -151,10 +221,10 @@ class GetJobDetails(resource.PostableResource):
                 """ %(k, v)
             html += "</table>"
             html += """
-            <div id='codeh'>
+            <div id='title'>
                 Job Code
             </div>
-            <div id='code'>
+            <div id='job_code'>
             %s
             <div>
             """ % jdict['code']

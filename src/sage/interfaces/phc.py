a6107847bec7707616677dfe01e0d173c69da260
William Stein <wstein@gmail.com>
2007-10-18 18:37:37 -0700

    Apply trac #399 patch by Marshall Hampton

--- src/sage/interfaces/phc.py
+++ src/sage/interfaces/phc.py
@@ -1,8 +1,6 @@
 r"""nodoctest
 Interface to PHC.
 
-WARNING -- currently totally broken!! -- do not use.
-
 PHC computes numerical information about systems of polynomials over
 the complex numbers.
 
@@ -14,10 +12,19 @@ components.
 AUTHOR:
    -- PHC was written by J. Verschelde, R. Cools, and many others (?)
    -- William Stein and Kelly ?? -- first version of interface to PHC
+   -- Marshall Hampton -- second version of interface to PHC
+
+TODO:
+    This either needs to be able to handle arbitrary PHCpack command options, or have
+    enough special purpose functions added to get the full functionality.  One important
+    missing special case is having a root-counting function that could do mixed volumes and
+    structured Bezout counts.
+    Another nice feature to have would be a graphical output of the path-tracking, as has been done for Maple.
 """
 
 ########################################################################
 #       Copyright (C) 2006 William Stein <wstein@gmail.com>
+#       Copyright (C) 2007 Marshall Hampton <hamptonio@gmail.com>
 #       TODO -- add yourself as copyright holder.
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
@@ -30,16 +37,133 @@ AUTHOR:
 import copy, os
 
 import sage.misc.misc
+from sage.rings.complex_field import ComplexField
+from sage.rings.all import CC
+
+def get_solution_dicts(output_file_contents, input_ring, get_failures = True):
+    '''
+    Returns a list of dictionaries of variable:value (key:value) pairs.
+    Only used internally; see the solution_dict function in the PHC_Object class definition for details.
+    '''
+    output_list = output_file_contents.splitlines()
+    test = 'False'
+    solution_dicts = []
+    for solution_line in range(len(output_list)-1,-1,-1):
+        if output_list[solution_line].find('THE SOLUTIONS') == 0:
+            break
+    var_number = int(output_list[solution_line+2].split(' ')[1])
+    sol_number = int(output_list[solution_line+2].split(' ')[0])
+    for i in range(solution_line + 1,len(output_list)):
+        if output_list[i].count('the solution for t') == 1:
+            if output_list[i-3].count('success') > 0 or get_failures == True:
+                temp_dict = {}
+                for j in range(1,var_number+1):
+                    rawsplit = output_list[i+j].split(': ')[1].split(' ')
+                    for extras in range(rawsplit.count('')):
+                        rawsplit.remove('')
+                    temp_var = output_list[i+j].split(': ')[0].replace(' ','')
+                    temp_dict[input_ring(temp_var)] = CC(rawsplit[0],rawsplit[1])
+                solution_dicts.append(temp_dict)
+    return solution_dicts
+
+def get_variable_list(output_file_contents):
+    '''
+    Returns the variables, as strings, in the order in which PHCpack has processed them.
+    '''
+    output_list = output_file_contents.splitlines()
+    for solution_line in range(len(output_list)-1,-1,-1):
+        if output_list[solution_line].find('THE SOLUTIONS') == 0:
+            break
+    var_number = int(output_list[solution_line+2].split(' ')[1])
+    varlist = []
+    for var_ind in range(var_number):
+        var = output_list[solution_line + 8 + var_ind].split(' ')[1]
+        varlist.append(var)
+    return varlist
+
+class PHC_Object:
+    """
+    A container for data from the PHCpack program - lists of float solutions, etc.
+    """
+    def __init__(self, output_file_contents, input_ring):
+	'''
+	INPUT:
+	    output_file_contents: the string output of PHCpack
+	    input_ring: for coercion of the variables into the desired ring.
+	'''
+        self.output_file_contents = output_file_contents
+	self.input_ring = input_ring
+
+    def solution_dicts(self, get_failures = False):
+        """
+        Returns a list of solutions in dictionary form: variable:value.
+
+	INPUT:
+	    self: for access to self_out_file_contents, the string of raw PHCpack output.
+	    get_failures (optional): a boolean.  The default (False) is to not process failed homotopies.  These either lie on positive-dimensional components or at infinity.
+
+        OUTPUT:
+	    solution_dicts: a list of dictionaries.  Each dictionary element is of the form variable:value, where the variable is an element of the input_ring, and the value is in ComplexField.
+
+        """
+        try:
+            return self.__solution_dicts
+        except AttributeError:
+            pass
+        solution_dicts = get_solution_dicts(self.output_file_contents, self.input_ring, get_failures = get_failures)
+        self.__solution_dicts = solution_dicts
+        return solution_dicts
+
+    def solutions(self, get_failures = False):
+	"""
+	Returns a list of solutions in the ComplexField.  Use the variable_list function to get the order of variables used by PHCpack, which is usually different than the term order of the input_ring.
+
+	INPUT:
+	    self: for access to self_out_file_contents, the string of raw PHCpack output.
+	    get_failures (optional): a boolean.  The default (False) is to not process failed homotopies.  These either lie on positive-dimensional components or at infinity.
+
+        OUTPUT:
+	    solutions: a list of lists of ComplexField-valued solutions.
+	"""
+	try:
+	    return self.__solutions
+	except AttributeError:
+            pass
+        solution_dicts = get_solution_dicts(self.output_file_contents, self.input_ring, get_failures = get_failures)
+        self.__solution_dicts = solution_dicts
+	solutions = [sol_dict.values() for sol_dict in solution_dicts]
+	self.__solutions = solutions
+        return solutions
+
+    def variable_list(self):
+        """
+	Returns the variables, as strings, in the order in which PHCpack has processed them.
+        """
+        try:
+            return self.__var_list
+        except AttributeError:
+            pass
+        var_list = get_variable_list(self.output_file_contents)
+        self.__var_list = var_list
+        return var_list
 
 class PHC:
     """
-    ... say what it is...
+    A class to interface with PHCpack, for computing numerical homotopies and root counts.
 
     EXAMPLES:
         sage: from sage.interfaces.phc import phc
         sage: R.<x,y> = PolynomialRing(QQ,2)
-        sage: v = phc([x^2 + 1, x*y - 1])          # optional -- you must have phc install
-        sage.: print v
+	sage: testsys = [x^2 + 1, x*y - 1]
+        sage: v = phc.blackbox(testsys, R)     # optional -- you must have phc install
+	sage: v.solutions()
+	[[1.00000000000000*I, -1.00000000000000*I], [-1.00000000000000*I, 1.00000000000000*I]]
+	sage: v.solution_dicts()
+	[{y: 1.00000000000000*I, x: -1.00000000000000*I}, {y: -1.00000000000000*I, x: 1.00000000000000*I}]
+	sage: residuals = [[test_equation.subs(sol) for test_equation in testsys] for sol in v.solution_dicts()]
+	sage: residuals
+	[[0, 0], [0, 0]]
+        sage: print v
          2
         x^2+ 1;
         x*y-1;
@@ -80,23 +204,23 @@ class PHC:
         Well conditioned and distinct roots counted to the left.
         <BLANKLINE>
         TIMING INFORMATION for Solving the polynomial system
-        The elapsed time in seconds was                  0.016529000 =  0h 0m 0s 17ms
-        User time in seconds was                         0.014512000 =  0h 0m 0s 15ms
-        System CPU time in seconds was                   0.002017000 =  0h 0m 0s  2ms
+        The elapsed time in seconds was                  0.013681000 =  0h 0m 0s 14ms
+	User time in seconds was                         0.011680000 =  0h 0m 0s 12ms
+	System CPU time in seconds was                   0.002001000 =  0h 0m 0s  2ms
         Non-I/O page faults was                          0
         I/O page faults was                              0
         Signals delivered was                            0
         Swaps was                                        0
         Total context switches was                       0
         <BLANKLINE>
-          ---------------------------------------------------------------------
-          |                    TIMING INFORMATION SUMMARY                     |
-          ---------------------------------------------------------------------
-          |   root counts  |  start system  |  continuation  |   total time   |
-          ---------------------------------------------------------------------
-          |  0h 0m 0s  0ms |  0h 0m 0s  0ms |  0h 0m 0s  0ms |  0h 0m 0s 15ms |
-          ---------------------------------------------------------------------
-        PHC ran from 24 October 2006, 07:33:07 till 24 October 2006, 07:33:07.
+	  ---------------------------------------------------------------------
+ 	  |                    TIMING INFORMATION SUMMARY                     |
+	  ---------------------------------------------------------------------
+	  |   root counts  |  start system  |  continuation  |   total time   |
+	  ---------------------------------------------------------------------
+	  |  0h 0m 0s  0ms |  0h 0m 0s  0ms |  0h 0m 0s  0ms |  0h 0m 0s 12ms |
+	  ---------------------------------------------------------------------
+        PHC ran from 3 July 2007, 11:33:02 till 3 July 2007, 11:33:02.
         The total elapsed time is 0 seconds.
         <BLANKLINE>
     """
@@ -133,11 +257,11 @@ class PHC:
         if not isinstance(polys, (list, tuple)):
             raise TypeError, 'polys must be a list or tuple'
         s = '%s\n'%len(polys)
-        if len(polys) == 0:
-            ngens = 1
-        else:
-            ngens = polys[0].parent().ngens()
-        s += '%s\n'%ngens
+        #if len(polys) == 0:
+        #    ngens = 1
+        #else:
+        #    ngens = polys[0].parent().ngens()
+        #s += '%s\n'%ngens
 
         # TODO: actually implement the variable mapping (copy code from gfan?)
         # For now, we'll assume all variables satisfy the restrictive
@@ -148,23 +272,15 @@ class PHC:
         # Current just return "none" for the mapping.
         return s, None
 
-    def __call__(self, polys, mode='b',
-                 cmd_list=['y', 'input', 'output'], verbose=False):
+    def blackbox(self, polys, input_ring, verbose=False):
         """
-        Returns as a string the result of running PHC with the given polynomials,
-        mode, and command list.  The command list is a sequence of commands that
-        would get input interactively to PHC if you ran it from the command line.
+        Returns as a string the result of running PHC with the given polynomials
+        under blackbox mode (the '-b' option).
 
         INPUT:
             polys -- a list of multivariate polynomials (elements of a multivariate
                      polynomial ring).
-            mode -- (default: 'b' for batch mode) one of the the letters
-                    '0', 'a', b', 'c', 'd', 'e', 'f',
-                    'k', 'l', 'm', 'p', 'q', 'r', 's', 'v', 'w', 'z'
-            cmd_list -- a list of commands, which will get input to PHC.
-                    Use the special commands 'input' and 'output' to represent
-                    the tempory input filename and output filename, respectively.
-                    default: ['y', 'input', 'output']
+            input_ring: for coercion of the variables into the desired ring.
             verbose -- print lots of verbose information about what this function does.
 
         OUTPUT:
@@ -174,7 +290,6 @@ class PHC:
         # Get three temporary file names (these will be in SAGE_HOME/.sage/tmp/pid)
         input_filename = sage.misc.misc.tmp_filename()
         output_filename = sage.misc.misc.tmp_filename()
-        cmd_filename = sage.misc.misc.tmp_filename()
         log_filename = sage.misc.misc.tmp_filename()
 
         # Get the input polynomial text
@@ -187,32 +302,8 @@ class PHC:
             print "The following file will be the input polynomial file to phc."
             print input
 
-        # Replace 'input' and 'output' in the command list by the
-        # temporary file names.
-        # CRUCIAL: We *must* make a copy of cmd_list, otherwise we would
-        # be modifying it in place, which is very bad.
-        cmd_list = copy.copy(cmd_list)
-        try:
-            cmd_list[cmd_list.index('input')] =  input_filename
-        except ValueError:
-            pass
-        try:
-            cmd_list[cmd_list.index('output')] = output_filename
-        except ValueError:
-            pass
-
-        # Create a single carriage-return separated string from the command list
-        cmd_in = '\n'.join([str(x) for x in cmd_list])
-
-        if verbose:
-            print "The following command list will be fed to phc from stdin:"
-            print cmd_in
-
-        # Write the input command list to a temporary file.
-        open(cmd_filename,'w').write(cmd_in + '\n\n')
-
         # Create the phc command line>
-        cmd = 'phc -%s < %s > %s'%(mode, cmd_filename, log_filename)
+        cmd = 'phc -b %s %s'%(input_filename, output_filename)
 
         if verbose:
             print "The phc command line is:"
@@ -224,9 +315,8 @@ class PHC:
         # Was there an error?
         if e:
             if os.system('which phc'):
-                raise RuntimeError, "\n\n" + '-'*70 + '\n Install the *free* PHCpack somewhere on your computer\n' + \
-                      " so that the command 'phc' is in your PATH.\n" + \
-                      " Get it at http://www.cs.kuleuven.ac.be/~nines/research/phcpack/Download/index.html" + '\n' + '-'*70
+                print os.system('which phc') + '  PHC needs to be installed and in your path'
+                raise RuntimeError
             # todo -- why? etc.
             raise RuntimeError, open(log_filename).read() + "\nError running phc."
 
@@ -239,11 +329,9 @@ class PHC:
         # Delete the temporary files
         os.unlink(input_filename)
         os.unlink(output_filename)
-        os.unlink(cmd_filename)
-        os.unlink(log_filename)
 
         # All done
-        return out
+        return PHC_Object(out, input_ring)
 
 
 ################################

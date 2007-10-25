17f42824d8941b23825fea1a6d82278bd5ef6c74
Carl Witty <cwitty@newtonlabs.com>
2007-10-24 18:36:37 -0700

    Sort results of variety(), for better cross-architecture doctest compatibility. This compensates for the fact that the results of Singular's triangular decomposition algorithm triangLfak appear in a different order on different architectures.  (It would be better to fix this in triangular_decomposition(), but the obvious approach of sorting its result failed due to TRAC #991.)

--- src/sage/rings/polynomial/multi_polynomial_ideal.py
+++ src/sage/rings/polynomial/multi_polynomial_ideal.py
@@ -1076,7 +1076,7 @@ class MPolynomialIdeal_singular_repr:
             Multivariate Polynomial Ring in x, y over Finite Field in w of size 3^3
 
             sage: V = I.variety(); V
-            [{y: w^2 + 2, x: 2*w}, {y: w^2 + 2*w, x: 2*w + 2}, {y: w^2 + w, x: 2*w + 1}]
+            [{y: w^2 + 2, x: 2*w}, {y: w^2 + w, x: 2*w + 1}, {y: w^2 + 2*w, x: 2*w + 2}]
 
             sage: [f.subs(v) for f in I.gens() for v in V] # check that all polynomials vanish
             [0, 0, 0, 0, 0, 0]
@@ -1120,6 +1120,7 @@ class MPolynomialIdeal_singular_repr:
 
             for v in Vbar:
                 V.append(dict([(P(var),val) for var,val in v.iteritems()]))
+        V.sort()
         return Sequence(V)
 
 class MPolynomialIdeal_macaulay2_repr:

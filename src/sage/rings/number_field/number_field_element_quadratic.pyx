c27f191e541afdf07efcfb5c62ea679ff31f67db
mabshoff <mabshoff@sage.math.washington.edu>
2007-11-02 12:57:51 -0700

    Revert the work-around from #1044 due to Robert Bradshaw's fix.

--- src/sage/rings/number_field/number_field_element_quadratic.pyx
+++ src/sage/rings/number_field/number_field_element_quadratic.pyx
@@ -483,7 +483,7 @@ cdef class NumberFieldElement_quadratic(NumberFieldElement_absolute):
             sage: (1+a)*3
             3*a + 3
         """
-        cdef Rational c = _c
+        cdef Rational c = <Rational>_c
         cdef NumberFieldElement_quadratic res = <NumberFieldElement_quadratic>self._new()
         res.disc = self.disc
         mpz_mul(res.a, self.a, mpq_numref(c.value))
@@ -500,7 +500,7 @@ cdef class NumberFieldElement_quadratic(NumberFieldElement_absolute):
             sage: 5*(a-1/5)
             5*a - 1
         """
-        cdef Rational c = _c
+        cdef Rational c = <Rational>_c
         cdef NumberFieldElement_quadratic res = <NumberFieldElement_quadratic>self._new()
         res.disc = self.disc
         mpz_mul(res.a, self.a, mpq_numref(c.value))

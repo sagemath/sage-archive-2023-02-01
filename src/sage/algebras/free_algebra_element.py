49fb2310da03b8effb411485a0614c65163043f1
mabshoff <mabshoff@sage.math.washington.edu>
2007-11-01 17:54:01 -0700

    Backed out changeset 9a53ba484dcb

--- src/sage/algebras/free_algebra_element.py
+++ src/sage/algebras/free_algebra_element.py
@@ -85,9 +85,6 @@ class FreeAlgebraElement(AlgebraElement):
             sage: A.<x,y,z>=FreeAlgebra(ZZ,3)
             sage: latex(-x+3*y^20*z)
             \left(-1\right)x + 3y^{20}z
-            sage: alpha,beta,gamma=FreeAlgebra(ZZ,3,'alpha,beta,gamma').gens()
-            sage: latex(alpha-beta)
-            \alpha + \left(-1\right)\beta
         """
         v = self.__monomial_coefficients.items()
         v.sort()

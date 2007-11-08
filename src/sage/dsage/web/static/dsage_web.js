f58a9f17b8a62d258cc34ca70269b3134d1a2f71
Yi Qiang <yqiang@gmail.com>
2007-11-08 14:55:13 -0800

    Various web server fixes.

--- src/sage/dsage/web/static/dsage_web.js
+++ src/sage/dsage/web/static/dsage_web.js
@@ -1,44 +1,67 @@
 var count = 10; // Default count
 
 function getServerDetails () {
+    $('#server_details').load('get_server_details');
+    showServer()
+}
+
+function showServer () {
     $('#jobs_table').hide();
     $('#job_details').hide();
     $('#help').hide();
-
-    $('#server_details').load('get_server_details');
     $('#server_details').show();
+    $('#navigator').hide();
+}
+function getJobs (c) {
+    count = c // Set global variable count to the selected count.
+
+    // Load jobs table with the given number of jobs.
+    $('#jobs_table').load('get_jobs', {'count': c}, function() {
+            $("#jobs_table").tablesorter(); } );
+
+    // Change current selected count to red
+    $('#navigator > a').css('color', 'black')
+    $('#' + c).css('color', 'red')
 }
 
-function getJobs (c) {
-    count = c
+function showJobs () {
     $('#server_details').hide();
     $('#job_details').hide();
     $('#help').hide();
-    $('#jobs_table').load('get_jobs', {'count': count}, function() {
-            $("#jobs_table").tablesorter(); } );
     $('#jobs_table').show();
 }
 
 function getJobDetails (job_id) {
-    $('#jobs_table').hide();
-    $('#server_details').hide();
-    $('#help').hide();
     $('#job_details').load('get_details', {'job_id': job_id}, function() {
                                         $("#job_details").tablesorter(); } );
+    showJobDetails()
+}
+
+function showJobDetails () {
+    $('#jobs_table').hide();
+    $('#navigator').hide();
+    $('#server_details').hide();
+    $('#help').hide();
     $('#job_details').show();
 }
 
 function getHelp () {
+    $('#help').load('get_help');
+    showHelp()
+}
+
+function showHelp () {
     $('#jobs_table').hide();
+    $('#navigator').hide();
     $('#server_details').hide();
     $('#job_details').hide();
-    $('#help').load('get_help');
     $('#help').show();
 }
 
 $(document).ready(function()
     {
         getJobs(count) // Run it the first time
-        setInterval('getJobs(count)', 5000); // Update it every 5 seconds.
+        showJobs() // Show jobs
+        // setInterval('getJobs(count)', 5000); // Update it every 5 seconds.
     }
 );

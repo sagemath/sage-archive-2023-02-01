7a520a78d3beaf42b5199ca3c72318e830497d80
Yi Qiang <yqiang@gmail.com>
2007-11-08 18:49:11 -0800

    Added ajax history!

--- src/sage/dsage/web/static/dsage_web.js
+++ src/sage/dsage/web/static/dsage_web.js
@@ -9,8 +9,8 @@ function showServer () {
     $('#jobs_table').hide();
     $('#job_details').hide();
     $('#help').hide();
-    $('#server_details').show();
     $('#navigator').hide();
+    $('#server_details').show();
 }
 function getJobs (c) {
     count = c // Set global variable count to the selected count.
@@ -29,6 +29,7 @@ function showJobs () {
     $('#job_details').hide();
     $('#help').hide();
     $('#jobs_table').show();
+    $('#navigator').show();
 }
 
 function getJobDetails (job_id) {
@@ -58,10 +59,51 @@ function showHelp () {
     $('#help').show();
 }
 
+// PageLoad function
+// This function is called when:
+// 1. after calling $.historyInit();
+// 2. after calling $.historyLoad();
+// 3. after pushing "Go Back" button of a browser
+function pageload(hash) {
+	// hash doesn't contain the first # character.
+	if(hash) {
+		// restore ajax loaded state
+        if (hash=='jobs') {
+            showJobs()
+        }
+        else if (hash=='server') {
+            getServerDetails()
+        }
+        else if (hash=='help') {
+            getHelp()
+        }
+	}
+	else {
+		// start page
+		showJobs()
+	}
+}
+
+function gethash (that) {
+    var hash = that.href;
+	hash = hash.replace(/^.*#/, '');
+	// moves to a new page.
+	// pageload is called at once.
+    $.historyLoad(hash);
+	return false;
+}
+
 $(document).ready(function()
     {
+        // Initialize history plugin.
+		// The callback is called at once by present location.hash.
+		$.historyInit(pageload);
+		console.log('historyInit')
+		// set onlick event for buttons
+		$("a[@rel='history']").click(function() {gethash(this); });
+		console.log('setonclick')
         getJobs(count) // Run it the first time
-        showJobs() // Show jobs
+        // showJobs() // Show jobs
         // setInterval('getJobs(count)', 5000); // Update it every 5 seconds.
     }
 );

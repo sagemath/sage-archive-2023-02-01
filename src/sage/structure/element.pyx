5600b21c7ea1034125c3c8ad39a011fae0bbe1d5
Paul Zimmermann <zimmerma@loria.fr>
2007-11-15 21:15:04 +0100

    fixed typo

--- src/sage/structure/element.pyx
+++ src/sage/structure/element.pyx
@@ -10,8 +10,8 @@ AUTHORS:
    -- Gonzalo Tornaria (2007-06): recursive base extend for coercion -- lots of tests
 
 
-\subsection{The Abstract Element Class Heierarchy}
-This is the abstract class heierchary, i.e., these are all
+\subsection{The Abstract Element Class Hierarchy}
+This is the abstract class hierchary, i.e., these are all
 abstract base classes.
 \begin{verbatim}
 SageObject

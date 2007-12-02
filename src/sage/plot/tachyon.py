0b53329413cfdcde57873433d8cc190ccc7f390c
William Stein <wstein@gmail.com>
2007-12-01 22:03:00 -0800

    Fix trac #799 -- tachyon texfunc issue -- and add some doctests.

--- src/sage/plot/tachyon.py
+++ src/sage/plot/tachyon.py
@@ -293,6 +293,9 @@ class Tachyon(SageObject):
             sage: t.plane((0,0,0),(0,0,1),'black')
             sage: t.show()
         """
+        type = int(type)
+        if type < 0 or type > 9:
+            raise ValueError, "type must be an integer between 0 and 9"
         return Texfunc(type,center,rotate,scale).str()
 
     def texture(self, name, ambient=0.2, diffuse=0.8,

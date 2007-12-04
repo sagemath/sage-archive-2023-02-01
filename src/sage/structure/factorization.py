d4ff3d6b575082cde2b46db4dfcb0da26b3ae9dc
William Stein <wstein@gmail.com>
2007-12-03 20:52:20 -0800

    Fix mistake where factorizations are sorted without regard to actual factors (only the degree and exponent were taken into account).

--- src/sage/structure/factorization.py
+++ src/sage/structure/factorization.py
@@ -126,8 +126,7 @@ class Factorization(SageObject, list):
             a = self[0][0].degree()
             def _cmp(f,g):
                 try:
-                    #return cmp(f[0].degree()*f[1], g[0].degree()*g[1])
-                    return cmp((f[0].degree(),f[1]), (g[0].degree(),g[1]))
+                    return cmp((f[0].degree(),f[1],f[0]), (g[0].degree(),g[1],g[0]))
                 except (AttributeError, NotImplementedError):
                     return cmp(f[0], g[0])
         except (AttributeError, NotImplementedError):

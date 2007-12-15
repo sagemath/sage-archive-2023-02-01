c79bb13c18b5e36dfb45eb22b9107cb977045450
jkantor <jkantor@sage.math.washington.edu>
2007-12-15 01:33:58 -0800

    Make the arpack doctest more stable numerically

--- src/sage/numerical/test.py
+++ src/sage/numerical/test.py
@@ -12,17 +12,18 @@ sage: from scipy import sparse
 sage: import arpack
 
 #Test arpack
+#This matrix is the finite difference approximation to
+# the eigenvalue problem
+#d^2f/dx^2=\lambda f, on [0,\pi], which boundary values 0
+# The lowest eigenvalue calulated should be close to 1
 sage: import scipy
-sage: n=scipy.zeros((3,100))
+sage: n=scipy.zeros((3,500))
 sage: n[0,:]=-1
-sage: n[1,:]=1
-sage: n[2,:]=-2
-sage: A=sparse.spdiags(n,[-1,0,1],int(100),int(100))
-sage: e,v=arpack.eigen(A,3)
-sage: e
-array([ 3.8270...+0.j,  3.8229...+0.j,  ...+0.j,  0.
-+0.j])
-
-
+sage: n[1,:]=2
+sage: n[2,:]=-1
+sage: A=sparse.spdiags(n,[-1,0,1],int(500),int(500))
+sage: e,v=arpack.speigs.ARPACK_eigs(A.matvec,500,6,which='SM')
+sage: e[0]*float(501/pi)**2
+0.999............
 """
 

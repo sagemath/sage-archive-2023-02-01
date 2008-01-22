4a6dfe87f802ea17deae53b61c547be22f07df11
Robert L. Miller <rlm@rlmiller.org>
2008-01-21 17:19:15 -0800

    various cleanups to libecm interface

--- src/sage/interfaces/libecm.pyx
+++ src/sage/interfaces/libecm.pyx
@@ -12,23 +12,19 @@ AUTHOR:
 EXAMPLE:
 
     sage: import sage.interfaces.libecm
-    sage: from sage.interfaces.libecm import py_ecm_factor
-    sage: py_ecm_factor(999, 0.00)
+    sage: from sage.interfaces.libecm import ecmfactor
+    sage: ecmfactor(999, 0.00)
+    (True, 27)
+    sage: ecmfactor(999, 0.00, verbose=True)
     Performing one curve with B1=0
-    Found factor in step 1:
-    27
-
-
+    Found factor in step 1: 999
+    (True, 999)
 
 """
 
 include '../ext/cdefs.pxi'
 
 from sage.rings.integer cimport Integer
-import sys
-
-#cdef extern from "gmp.h":
-#    pass
 
 cdef extern from "ecm.h":
     ctypedef struct __ecm_param_struct:
@@ -37,38 +33,38 @@ cdef extern from "ecm.h":
     int ecm_factor (mpz_t, mpz_t, double, ecm_params)
     int ECM_NO_FACTOR_FOUND
 
-def py_ecm_factor(n, B1):
-    ecmfactor(Integer(n), B1)
-
-cdef void ecmfactor(Integer number, py_B1):
+def ecmfactor(number, py_B1, verbose=False):
     cdef mpz_t n, f
     cdef int res
     cdef double B1
-    cdef Integer sage_int_f
+    cdef Integer sage_int_f, sage_int_number
 
     sage_int_f = Integer(0)
-
-    mpz_init(n)
-
-    mpz_set(n, number.value)
-
+    sage_int_number = Integer(number)
     B1 = py_B1
 
+    mpz_init(n)
+    mpz_set(n, sage_int_number.value)
     mpz_init(f) # For potential factor
 
-    print "Performing one curve with B1=%1.0f"%B1
+    if verbose:
+        print "Performing one curve with B1=%1.0f"%B1
 
     res = ecm_factor(f, n, B1, NULL)
 
     if res > 0:
-        print "Found factor in step %d: "%res
         mpz_set(sage_int_f.value, f)
-        print sage_int_f
-        #mpz_out_str(sys.stdout, 10, f)
-        print "\n"
-    elif res == ECM_NO_FACTOR_FOUND:
-        print "Found no factor."
-    else:
-        print "ECM lib error"
+
     mpz_clear(f)
     mpz_clear(n)
+
+    if res > 0:
+        if verbose:
+            print "Found factor in step %d: %d"%(res,sage_int_f)
+        return (True, sage_int_f)
+    elif res == ECM_NO_FACTOR_FOUND:
+        if verbose:
+            print "Found no factor."
+        return (False, None)
+    else:
+        raise RuntimeError( "ECM lib error" )

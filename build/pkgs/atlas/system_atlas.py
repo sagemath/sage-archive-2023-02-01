5bd532ea3e5b3d89e34e9666e21d744eeb33fd15
mabshoff <mabshoff@sage.math.washington.edu>
2008-01-21 22:50:12 -0800

    atlas-3.8.p9: resolve #1721, #1787

--- build/pkgs/atlas/system_atlas.py
+++ build/pkgs/atlas/system_atlas.py
@@ -5,20 +5,22 @@ import sys
 
 fortran = os.popen2(os.environ['SAGE_LOCAL']+'/bin/'+'which_fortran')[1].read()
 SAGE_LOCAL_LIB = os.environ['SAGE_LOCAL']+'/lib'
+SAGE_LOCAL_INCLUDE = os.environ['SAGE_LOCAL']+'/include'
 
 if os.environ.has_key('SAGE_ATLAS_LIB'):
     ATLAS_LIB=os.environ['SAGE_ATLAS_LIB']
     if os.path.isdir(ATLAS_LIB):
 
-        has_atlas = os.path.exists(ATLAS_LIB+'/libatlas.so')
-        has_lapack = os.path.exists(ATLAS_LIB+'/liblapack.so')
-        has_cblas = os.path.exists(ATLAS_LIB+'/libcblas.so')
-        has_f77blas = os.path.exists(ATLAS_LIB+'/libf77blas.so')
+        has_atlas = os.path.exists(ATLAS_LIB+'/lib//libatlas.so')
+        has_lapack = os.path.exists(ATLAS_LIB+'/lib/liblapack.so')
+        has_cblas = os.path.exists(ATLAS_LIB+'/lib/libcblas.so')
+        has_f77blas = os.path.exists(ATLAS_LIB+'/lib/libf77blas.so')
+        has_atlas_headers = os.path.exists(ATLAS_LIB+'/include/atlas')
 
-        if has_atlas == True and has_lapack == True and has_cblas == True and has_f77blas == True:
+        if has_atlas == True and has_lapack == True and has_cblas == True and has_f77blas == True and has_atlas_headers == True:
 
-            s_gfortran = os.popen2('readelf -s ' +ATLAS_LIB+'/libf77blas.so | grep gfortran')[1].read()
-            s_g95 = os.popen2('readelf -s ' + ATLAS_LIB + '/libf77blas.so | grep g95')[1].read()
+            s_gfortran = os.popen2('readelf -s ' +ATLAS_LIB+'/lib/libf77blas.so | grep gfortran')[1].read()
+            s_g95 = os.popen2('readelf -s ' + ATLAS_LIB + '/lib/libf77blas.so | grep g95')[1].read()
 
             if s_gfortran !='' and not fortran.startswith('gfortran'):
                 print "Symbols in lib77blas indicate it was build with gfortran \n"
@@ -35,24 +37,27 @@ if os.environ.has_key('SAGE_ATLAS_LIB'):
                 sys.exit(2)
 
 
-            os.system(' ln -sf ' + ATLAS_LIB + '/libatlas.so '  + SAGE_LOCAL_LIB+'/libatlas.so')
-            os.system(' ln -sf ' + ATLAS_LIB + '/libcblas.so '  + SAGE_LOCAL_LIB+ '/libcblas.so')
-            os.system(' ln -sf ' + ATLAS_LIB + '/liblapack.so '  + SAGE_LOCAL_LIB+'/liblapack.so')
-            os.system(' ln -sf ' + ATLAS_LIB + '/libf77blas.so '  + SAGE_LOCAL_LIB+'/libf77blas.so')
+            os.system(' ln -sf ' + ATLAS_LIB + '/lib/libatlas.so '  + SAGE_LOCAL_LIB+'/libatlas.so')
+            os.system(' ln -sf ' + ATLAS_LIB + '/lib/libcblas.so '  + SAGE_LOCAL_LIB+ '/libcblas.so')
+            os.system(' ln -sf ' + ATLAS_LIB + '/lib/liblapack.so '  + SAGE_LOCAL_LIB+'/liblapack.so')
+            os.system(' ln -sf ' + ATLAS_LIB + '/lib/libf77blas.so '  + SAGE_LOCAL_LIB+'/libf77blas.so')
+            os.system(' ln -sf ' + ATLAS_LIB + '/include/atlas '  + SAGE_LOCAL_INCLUDE+'/')
+            os.system(' ln -sf ' + ATLAS_LIB + '/include/cblas.h '  + SAGE_LOCAL_INCLUDE+'/cblas.h')
+            os.system(' ln -sf ' + ATLAS_LIB + '/include/clapack.h '  + SAGE_LOCAL_INCLUDE+'/clapack.h')
             sys.exit(0)
 
         else:
 
             print("Unable to find one of liblapack.so, libcblas.so, libatlas.so, or libf77blas.so")
             print("in supplied directory.")
-            print("Set SAGE_ATLAS_LIB to a directory having liblapack.so,libcblas.so,libatlas.so,libf77blas.so,")
+            print("Set SAGE_ATLAS_LIB to the lib directory of liblapack.so,libcblas.so,libatlas.so,libf77blas.so,")
             print("to use existing atlas libraries, or unset SAGE_ATLAS_LIB to build one from source.")
             print("Then type make.")
             sys.exit(2)
     else:
             print("Unable to find one of liblapack.so, libcblas.so, libatlas.so, or libf77blas.so")
             print("in supplied directory.")
-            print("Set SAGE_ATLAS_LIB to a directory having liblapack.so,libcblas.so,libatlas.so,libf77blas.so,")
+            print("Set SAGE_ATLAS_LIB to the lib directory of liblapack.so,libcblas.so,libatlas.so,libf77blas.so,")
             print("to use existing atlas libraries, or unset SAGE_ATLAS_LIB to build one from source.")
             print("Then type make.")
             sys.exit(2)

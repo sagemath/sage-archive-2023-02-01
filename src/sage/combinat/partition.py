a13d35f22e16688417c8a1f61141545ae8c0ff49
Mike Hansen <mhansen@gmail.com>
2008-02-14 15:49:12 -0800

    #2164 part 2

--- src/sage/combinat/partition.py
+++ src/sage/combinat/partition.py
@@ -49,6 +49,7 @@ from sage.libs.all import pari
 import tableau
 import permutation
 import sf.sfa
+import copy
 
 def Partition(l=None, exp=None, core_and_quotient=None):
     """
@@ -1758,9 +1759,9 @@ class Partitions_constraints(CombinatorialClass):
             sage: [x for x in Partitions(4, min_part=2)]
             [[4], [2, 2]]
             sage: [x for x in Partitions(4, length=3, min_part=0)]
-            [[4, 0, 0], [3, 1, 0], [2, 2, 0], [2, 1, 1]]
+            [[2, 1, 1]]
             sage: [x for x in Partitions(4, min_length=3, min_part=0)]
-            [[4, 0, 0], [3, 1, 0], [2, 2, 0], [2, 1, 1], [1, 1, 1, 1]]
+            [[2, 1, 1], [1, 1, 1, 1]]
             sage: [x for x in Partitions(4, outer=[3,1,1])]
             [[3, 1], [2, 1, 1]]
             sage: [x for x in Partitions(4, outer=['inf', 1, 1])]
@@ -1776,101 +1777,10 @@ class Partitions_constraints(CombinatorialClass):
             sage: [x for x in Partitions(11, max_slope=-1, min_slope=-3, min_length=2, max_length=4, outer=[6,5,2])]
             [[6, 5], [6, 4, 1], [6, 3, 2], [5, 4, 2]]
         """
-        n = self.n
+        for p in Partitions_n(self.n)._fast_iterator():
+            if misc.check_integer_list_constraints([p], **self.constraints):
+                yield Partition_class(p)
 
-        #put the constraints in the namespace
-        length = self.constraints.get('length',None)
-        min_length = self.constraints.get('min_length',None)
-        max_length = self.constraints.get('max_length',None)
-        max_part = self.constraints.get('max_part',None)
-        min_part = self.constraints.get('min_part',None)
-        max_slope = self.constraints.get('max_slope',None)
-        min_slope = self.constraints.get('min_slope',None)
-        inner = self.constraints.get('inner',None)
-        outer = self.constraints.get('outer',None)
-
-        #Preproccess the constraints
-        if max_slope == "inf":
-            max_slope = n
-        if min_slope == "-inf":
-            min_slope = -n
-        if length is not None:
-            min_length = length
-            max_length = length
-        if outer is not None:
-            for i in range(len(outer)):
-                if outer[i] == "inf":
-                    outer[i] = n
-
-        l = []
-        old_p = Partition_class([self.n])
-
-        #Go through all of the partitions
-        while old_p != False:
-            meets_constraints = True
-
-            #Handle the case if the user specifies
-            #min_part = 0
-            if min_part == 0:
-                #if length is not None and len(p) < length:
-                #    p += [0]*(len(p)-length)
-                if min_length is not None and len(old_p) < min_length:
-                    p = old_p + [0]*(min_length-len(old_p))
-                else:
-                    p = old_p
-            else:
-                p = old_p
-
-            #if length is not None:
-            #    if len(p) != length:
-            #        meets_constraints = False
-
-            if min_length is not None:
-                if len(p) < min_length:
-                    meets_constraints = False
-
-            if max_length is not None:
-                if len(p) > max_length:
-                    meets_constraints = False
-
-            if max_part is not None:
-                if max(p) > max_part:
-                    meets_constraints = False
-
-            if min_part is not None:
-                if min(p) < min_part:
-                    meets_constraints = False
-
-            if outer is not None:
-                if len(p) > len(outer):
-                    meets_constraints = False
-                else:
-                    for i in range(len(p)):
-                        if p[i] > outer[i]:
-                            meets_constraints = False
-
-            if inner is not None:
-                if len(p) < len(inner):
-                    meets_constraints = False
-                else:
-                    for i in range(len(inner)):
-                        if p[i] < inner[i]:
-                            meets_constraints = False
-
-
-            if max_slope is not None:
-                if max([p[i+1]-p[i] for i in range(0, len(p)-1)]+[-n] ) > max_slope:
-                    meets_constraints = False
-
-            if min_slope is not None:
-                if min([p[i+1]-p[i] for i in range(0, len(p)-1)]+[n] ) < min_slope:
-                    meets_constraints = False
-
-
-            if meets_constraints:
-                yield p
-
-            old_p = old_p.next()
 
 class Partitions_n(CombinatorialClass):
     object_class = Partition_class

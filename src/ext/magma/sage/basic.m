b0f5b52a00990f0567a33b6e134deaadb2219d0e
William Stein <wstein@gmail.com>
2008-02-15 00:08:25 -0800

    followup to #2169 -- (magma/sage interface) some further optimizations and fixes

--- src/ext/magma/sage/basic.m
+++ src/ext/magma/sage/basic.m
@@ -1,42 +1,63 @@
+function PreparseElts(R)
+    if Type(R) eq RngInt then
+       return false;
+    end if;
+    return true;
+end function;
+
 intrinsic Sage(X::.) -> MonStgElt
 {}
-    return Sprintf("%o", X);
+    return Sprintf("%o", X), true;
 end intrinsic;
 
 intrinsic Sage(X::SetEnum) -> MonStgElt
 {}
     Y := [Sage(z) : z in X];
-    return Sprintf("Set(%o)", Y);
+    return Sprintf("Set(%o)", Y), true;
 end intrinsic;
 
 intrinsic Sage(X::SetIndx) -> MonStgElt
 {WARNING: Sage does not have an analogue of indexed sets.}
     Y := [z : z in X];
-    return Sprintf("%o", Y);
+    return Sprintf("%o", Y), true;
 end intrinsic;
 
-intrinsic Sage(X::SetMulti) -> MonStgElt
+intrinsic Sage(X::SetMulti) -> MonStgElt, BoolElt
 {WARNING: Sage does not have an analogue of multisets.}
     Y := [z : z in X];
-    return Sprintf("%o", Y);
+    return Sprintf("%o", Y), true;
 end intrinsic;
 
-intrinsic Sage(X::RngInt) -> MonStgElt
+intrinsic Sage(X::RngInt) -> MonStgElt, BoolElt
 {}
-    return "ZZ";
+    return "ZZ", false;
 end intrinsic;
 
-function convert_matrix(X)
-    return Sprintf("matrix(%o, %o, %o, %o)", Sage(BaseRing(X)),
+intrinsic Sage(X::RngIntElt) -> MonStgElt, BoolElt
+{}
+    return Sprintf("Integer('%h')", X), false;
+end intrinsic;
+
+/* Matrices */
+
+function convert_matrix(X, preparse_entries)
+    if preparse_entries then
+        return Sprintf("matrix(%o, %o, %o, %o)", Sage(BaseRing(X)),
                     Nrows(X), Ncols(X), [Sage(y) : y in Eltseq(X)]);
+    else
+        return Sprintf("matrix(%o, %o, %o, %o)", Sage(BaseRing(X)),
+                    Nrows(X), Ncols(X), Eltseq(X));
+    end if;
 end function;
 
-intrinsic Sage(X::AlgMatElt) -> MonStgElt
+intrinsic Sage(X::AlgMatElt) -> MonStgElt, BoolElt
 {}
-    return convert_matrix(X);
+    pp := PreparseElts(BaseRing(X));
+    return convert_matrix(X, pp), pp;
 end intrinsic;
 
-intrinsic Sage(X::ModMatRngElt) -> MonStgElt
+intrinsic Sage(X::ModMatRngElt) -> MonStgElt, BoolElt
 {}
-    return convert_matrix(X);
+    pp := PreparseElts(BaseRing(X));
+    return convert_matrix(X, pp), pp;
 end intrinsic;

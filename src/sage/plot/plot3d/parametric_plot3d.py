ea2aa1982f8a17b2543a738b4c40cbf3e0fd94db
David Joyner <wdjoyner@gmail.com>
2008-02-18 14:10:09 -0500

    Added examples in http://trac.sagemath.org/sage_trac/ticket/1857 - wdj

--- src/sage/plot/plot3d/parametric_plot3d.py
+++ src/sage/plot/plot3d/parametric_plot3d.py
@@ -286,6 +286,47 @@ def parametric_plot3d(f, urange, vrange=None, plot_points="automatic", **kwds):
         sage: p1 = parametric_plot3d([sin(u)*cos(u)*log(u^2)*v*(1-v)/2, ((u^6)^(1/20)*(cos(u)^2)^(1/4)-1/2)*v*(1-v), v^(0.5)], (u, 0.001, 1), (v, 0, 1), plot_points=[70,70], color='red')
         sage: p2 = parametric_plot3d([-sin(u)*cos(u)*log(u^2)*v*(1-v)/2, ((u^6)^(1/20)*(cos(u)^2)^(1/4)-1/2)*v*(1-v), v^(0.5)], (u, 0.001, 1), (v, 0, 1), plot_points=[70,70], color='red')
         sage: show(p1+p2, frame=False)
+
+    A Hyperhelicoidal:
+        sage: u = var("u")
+        sage: v = var("v")
+        sage: fx = (sinh(v)*cos(3*u))/(1+cosh(u)*cosh(v))
+        sage: fy = (sinh(v)*sin(3*u))/(1+cosh(u)*cosh(v))
+        sage: fz = (cosh(v)*sinh(u))/(1+cosh(u)*cosh(v))
+        sage: parametric_plot3d([fx, fy, fz], (u, -pi, pi), (v, -pi, pi), plot_points = [50,50], frame=False, color="red")
+
+    A Helicoid (lines through a helix, http://en.wikipedia.org/wiki/Helix):
+        sage: u, v = var('u,v')
+        sage: fx = sinh(v)*sin(u)
+        sage: fy = -sinh(v)*cos(u)
+        sage: fz = 3*u
+        sage: parametric_plot3d([fx, fy, fz], (u, -pi, pi), (v, -pi, pi), plot_points = [50,50], frame=False, color="red")
+
+    Kuen's surface (http://www.math.umd.edu/research/bianchi/Gifccsurfs/ccsurfs.html):
+        sage: fx = (2*(cos(u) + u*sin(u))*sin(v))/(1+ u^2*sin(v)^2)
+        sage: fy = (2*(sin(u) - u*cos(u))*sin(v))/(1+ u^2*sin(v)^2)
+        sage: fz = log(tan(1/2 *v)) + (2*cos(v))/(1+ u^2*sin(v)^2)
+        sage: parametric_plot3d([fx, fy, fz], (u, 0, 2*pi), (v, 0.01, pi-0.01), plot_points = [50,50], frame=False, color="green")
+
+
+    A 5-pointed star:
+        sage: fx = cos(u)*cos(v)*(abs(cos(1*u/4))^0.5 + abs(sin(1*u/4))^0.5)^(-1/0.3)*(abs(cos(5*v/4))^1.7 + abs(sin(5*v/4))^1.7)^(-1/0.1)
+        sage: fy = cos(u)*sin(v)*(abs(cos(1*u/4))^0.5 + abs(sin(1*u/4))^0.5)^(-1/0.3)*(abs(cos(5*v/4))^1.7 + abs(sin(5*v/4))^1.7)^(-1/0.1)
+        sage: fz = sin(u)*(abs(cos(1*u/4))^0.5 + abs(sin(1*u/4))^0.5)^(-1/0.3)
+        sage: parametric_plot3d([fx, fy, fz], (u, -pi/2, pi/2), (v, 0, 2*pi), plot_points = [50,50], frame=False, color="green")
+
+    A cool self-intersecting surface (Eppener surface?):
+        sage: fx = u - u^3/3 + u*v^2
+        sage: fy = v - v^3/3 + v*u^2
+        sage: fz = u^2 - v^2
+        sage: parametric_plot3d([fx, fy, fz], (u, -25, 25), (v, -25, 25), plot_points = [50,50], frame=False, color="green")
+
+    The breather surface (http://en.wikipedia.org/wiki/Breather_surface):
+        sage: fx = (2*sqrt(0.84)*cosh(0.4*u)*(-(sqrt(0.84)*cos(v)*cos(sqrt(0.84)*v)) - sin(v)*sin(sqrt(0.84)*v)))/(0.4*((sqrt(0.84)*cosh(0.4*u))^2 + (0.4*sin(sqrt(0.84)*v))^2))
+        sage: fy = (2*sqrt(0.84)*cosh(0.4*u)*(-(sqrt(0.84)*sin(v)*cos(sqrt(0.84)*v)) + cos(v)*sin(sqrt(0.84)*v)))/(0.4*((sqrt(0.84)*cosh(0.4*u))^2 + (0.4*sin(sqrt(0.84)*v))^2))
+        sage: fz = -u + (2*0.84*cosh(0.4*u)*sinh(0.4*u))/(0.4*((sqrt(0.84)*cosh(0.4*u))^2 + (0.4*sin(sqrt(0.84)*v))^2))
+        sage: parametric_plot3d([fx, fy, fz], (u, -13.2, 13.2), (v, -37.4, 37.4), plot_points = [90,90], frame=False, color="green")
+
     """
     # TODO:
     #   * Surface -- behavior of functions not defined everywhere -- see note above

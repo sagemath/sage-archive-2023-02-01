89677fb83bd2b3dace9a98c9191cad3c33a3ccef
bump <bump@match.stanford.edu>
2008-02-20 14:30:00 -0800

    moved SpinCrystal to new file spins.py and renamed it CrystalOfSpins

--- src/sage/combinat/crystals/all.py
+++ src/sage/combinat/crystals/all.py
@@ -1,5 +1,5 @@
 from crystals import Crystal
 from letters import CrystalOfLetters
-from letters import SpinCrystal
+from spins import CrystalOfSpins
 from tensor_product import TensorProductOfCrystals
 from tensor_product import CrystalOfTableaux

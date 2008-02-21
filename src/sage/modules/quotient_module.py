e5ea6a50818f6843845a1ab4c0460d850e5b5fc2
William Stein <wstein@gmail.com>
2008-02-21 10:30:21 -0800

    trac #2230 -- some hashing doctest fallout during alpha2 cycle.

--- src/sage/modules/quotient_module.py
+++ src/sage/modules/quotient_module.py
@@ -128,13 +128,13 @@ class FreeModule_ambient_field_quotient(FreeModule_ambient_field):
             sage: Q = V/W; Q.dimension()
             0
             sage: hash(Q)
-            954887582            # 32-bit
-            fixme    # 64-bit
+            954887582               # 32-bit
+            -5856620741060301410    # 64-bit
 
         The hash is just got by hashing both V and W.
             sage: hash((V, W))
-            954887582            # 32-bit
-            fixme    # 64-bit
+            954887582             # 32-bit
+            -5856620741060301410  # 64-bit
         """
         return self.__hash
 

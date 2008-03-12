a10a6d357e04cccc1d1b892acd800fdbed3c7090
bump <bump@match.stanford.edu>
2008-03-12 06:51:54 -0700

    Fast Crystal Advert

--- src/sage/combinat/crystals/crystals.py
+++ src/sage/combinat/crystals/crystals.py
@@ -45,6 +45,7 @@ Here are some more elaborate crystals (see their respective documentations):
     sage: Tens = TensorProductOfCrystals(C, C)
     sage: Spin = CrystalOfSpins(['B', 3])
     sage: Tab  = CrystalOfTableaux(['A', 3], shape = [2,1,1])
+    sage: Fast  = FastCrystal(['B', 2], 3/2, 1/2)
 
 One can get (currently) crude ploting via:
 

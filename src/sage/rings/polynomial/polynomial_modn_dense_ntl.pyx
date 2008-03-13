7606447e4a663fee050b64d5cdb188b7bf6abc15
Martin Albrecht <malb@informatik.uni-bremen.de>
2008-03-13 09:54:22 +0000

    small_roots should return elements of the base_ring() not integers

--- src/sage/rings/polynomial/polynomial_modn_dense_ntl.pyx
+++ src/sage/rings/polynomial/polynomial_modn_dense_ntl.pyx
@@ -544,7 +544,8 @@ cdef class Polynomial_dense_mod_n(Polynomial):
         f = sum([ZZ(B[0,i]//X**i)*x**i for i in range(B.ncols())])
         R = f.roots()
 
-        return [r for r,m in R if r<=X]
+        ZmodN = self.base_ring()
+        return list(set([ZmodN(r) for r,m in R if r<=X]))
 
 cdef class Polynomial_dense_modn_ntl_zz(Polynomial_dense_mod_n):
 

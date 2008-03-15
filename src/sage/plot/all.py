97c5e45e0d33497dfe6b6bd3bb41f2b979e1a1cc
Carl Witty <cwitty@newtonlabs.com>
2008-03-15 16:14:21 -0700

    Implicit plots (based on contour plots)

--- src/sage/plot/all.py
+++ src/sage/plot/all.py
@@ -1,7 +1,7 @@
 from plot import (Graphics, line, polygon, plot, point, points,
                   text, circle, disk, hue, graphics_array,
                   list_plot, networkx_plot, parametric_plot,
-                  polar_plot, contour_plot, arrow,
+                  polar_plot, contour_plot, implicit_plot, arrow,
                   plot_vector_field, matrix_plot, bar_chart,
                   is_Graphics, rainbow,
                   show_default)

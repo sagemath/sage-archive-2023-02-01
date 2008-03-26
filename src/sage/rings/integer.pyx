8d8e135b999960b4f12a0cec3c97bd55592d3bd4
Robert Bradshaw <robertwb@math.washington.edu>
2008-03-26 04:11:13 -0700

    exact gamma function for integers and half-integers

--- src/sage/rings/integer.pyx
+++ src/sage/rings/integer.pyx
@@ -16,6 +16,7 @@ AUTHORS:
     -- Robert Bradshaw (2007-04-12): is_perfect_power, Jacobi symbol (with Kronecker extension)
                                      Convert some methods to use GMP directly rather than pari, Integer() -> PY_NEW(Integer)
     -- David Roe (2007-03-21): sped up valuation and is_square, added val_unit, is_power, is_power_of and divide_knowing_divisible_by
+    -- Robert Bradshaw (2008-03-26): gamma function, multifactorials
 
 EXAMPLES:
    Add 2 integers:
@@ -113,6 +114,13 @@ cdef extern from "mpz_pylong.h":
 
 cdef extern from "convert.h":
     cdef void t_INT_to_ZZ( mpz_t value, long *g )
+
+cdef extern from *:
+    cdef void mpz_swap(mpz_t a, mpz_t b)
+
+cdef extern from "math.h":
+    cdef double log_c "log" (double)
+    cdef double ceil_c "ceil" (double)
 
 from sage.libs.pari.gen cimport gen as pari_gen, PariInstance
 
@@ -2100,25 +2108,132 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
         if mpz_sgn(self.value) < 0:
             raise ValueError, "factorial -- self = (%s) must be nonnegative"%self
 
-        if mpz_cmp_ui(self.value,4294967295) > 0:
+        if not mpz_fits_uint_p(self.value):
             raise ValueError, "factorial not implemented for n >= 2^32.\nThis is probably OK, since the answer would have billions of digits."
 
-        cdef unsigned int n
-        n = self
-
-        cdef mpz_t x
-        cdef Integer z
-
-        mpz_init(x)
+        cdef Integer z = PY_NEW(Integer)
 
         _sig_on
-        mpz_fac_ui(x, n)
+        mpz_fac_ui(z.value, mpz_get_ui(self.value))
         _sig_off
 
-        z = PY_NEW(Integer)
-        set_mpz(z, x)
-        mpz_clear(x)
         return z
+
+    def multifactorial(self, int k):
+        r"""
+        Computes the k-th factorial $n!^{(k)}$ of self. For k=1 this is the
+        standard factorial, and for k greater than one it is the product of
+        every k-th terms down from self to k. The recursive definition is used
+        to extend this function to the negative integers.
+
+        EXAMPLES:
+            sage: 5.multifactorial(1)
+            120
+            sage: 5.multifactorial(2)
+            15
+            sage: 23.multifactorial(2)
+            316234143225
+            sage: prod([1..23, step=2])
+            316234143225
+            sage: (-29).multifactorial(7)
+            1/2640
+        """
+        if k <= 0:
+            raise ValueError, "multifactorial only defined for positive values of k"
+
+        if not mpz_fits_sint_p(self.value):
+            raise ValueError, "multifactorial not implemented for n >= 2^32.\nThis is probably OK, since the answer would have billions of digits."
+
+        cdef int n = mpz_get_si(self.value)
+
+        # base case
+        if 0 < n < k:
+            return ONE
+
+        # easy to calculate
+        elif n % k == 0:
+            factorial = Integer(n/k).factorial()
+            if k == 2:
+                return factorial << (n/k)
+            else:
+                return factorial * Integer(k)**(n/k)
+
+        # negative base case
+        elif -k < n < 0:
+            return ONE / (self+k)
+
+        # reflection case
+        elif n < -k:
+            if (n/k) % 2:
+                sign = -ONE
+            else:
+                sign = ONE
+            return sign / Integer(-k-n).multifactorial(k)
+
+        # compute the actual product, optimizing the number of large multiplications
+        cdef int i,j
+
+        # we need (at most) log_2(#factors) concurrent sub-products
+        cdef int prod_count = <int>ceil_c(log_c(n/k+1)/log_c(2))
+        cdef mpz_t* sub_prods = <mpz_t*>malloc(prod_count * sizeof(mpz_t))
+        if sub_prods == NULL:
+            raise MemoryError
+        for i from 0 <= i < prod_count:
+            mpz_init(sub_prods[i])
+
+        _sig_on
+
+        cdef residue = n % k
+        cdef int tip = 0
+        for i from 1 <= i <= n//k:
+            mpz_set_ui(sub_prods[tip], k*i + residue)
+            # for the i-th terms we use the bits of i to calculate how many
+            # times we need to multiply "up" the stack of sub-products
+            for j from 0 <= j < 32:
+                if i & (1 << j):
+                    break
+                tip -= 1
+                mpz_mul(sub_prods[tip], sub_prods[tip], sub_prods[tip+1])
+            tip += 1
+        cdef int last = tip-1
+        for tip from last > tip >= 0:
+            mpz_mul(sub_prods[tip], sub_prods[tip], sub_prods[tip+1])
+
+        _sig_off
+
+        cdef Integer z = PY_NEW(Integer)
+        mpz_swap(z.value, sub_prods[0])
+
+        for i from 0 <= i < prod_count:
+            mpz_clear(sub_prods[i])
+        free(sub_prods)
+
+        return z
+
+
+    def gamma(self):
+        r"""
+        The gamma function on integers is the factorial function (shifted by
+        one) on positive integers, and $\pm \infty$ on non-positive integers.
+
+        EXAMPLES:
+            sage: gamma(5)
+            24
+            sage: gamma(0)
+            -Infinity
+            sage: gamma(-1)
+            +Infinity
+            sage: gamma(-2^150)
+            -Infinity
+        """
+        if mpz_sgn(self.value) > 0:
+            return (self-ONE).factorial()
+        else:
+            from sage.rings.infinity import infinity
+            if mpz_even_p(self.value):
+                return -infinity
+            else:
+                return infinity
 
     def floor(self):
         """
@@ -3731,7 +3846,7 @@ cdef PyObject* fast_tp_new(RichPyTypeObject *t, PyObject *a, PyObject *k):
         #
         # The clean version of the following line is:
         #
-        #  mpz_init(( <mpz_t>(<char *>new + mpz_t_offset) )
+        #  mpz_init( <mpz_t>(<char *>new + mpz_t_offset) )
         #
         # We save time both by avoiding an extra function call and
         # because the rest of the mpz struct was already initalized

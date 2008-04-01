f62deb23fd91ee2c518d4ee27e32b1bf0f8bb4a3
tabbott <tabbott@mit.edu>
2008-04-01 12:21:50 -0700

    Working debian/ directory for building SAGE.

--- src/ext/dist/debian/check-use-debian.pl
+++ src/ext/dist/debian/check-use-debian.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 # Script to answer queries of the form "is this available in Debian"
-@use_debian = ('bzip2', 'atlas', 'blas', 'clisp', 'cvxopt', 'cython', 'ecm', 'f2c', 'fortran', 'freetype', 'gap', 'gd', 'gdmodule', 'gmp', 'gnutls', 'gsl', 'ipython', 'lapack', 'libgcrypt', 'libgpg_error', 'libpng', 'matplotlib', 'maxima', 'mercurial', 'moin', 'mpfi', 'mpfr', 'networkx', 'numpy', 'opencdk', 'pari', 'pexpect', 'pycrypto', 'python', 'python_gnutls', 'quaddouble', 'r', 'readline','scipy','scons','sqlite','sympy','termcap','twisted','weave','zlib','zodb3');
-@our_debian = ('cddlib', 'eclib', 'flint', 'flintqs', 'libfplll', 'genus2reduction','gfan','givaro','iml','lcalc','libm4ri','linbox','ntl','palp','singular','symmetrica','sympow');
-@ignore = (@use_debian,@our_debian);
+@use_debian = ('bzip2', 'atlas', 'blas', 'clisp', 'cvxopt', 'cython', 'ecm', 'f2c', 'freetype', 'gap', 'gd', 'gdmodule', 'gmp', 'gnutls', 'gsl', 'ipython', 'lapack', 'libgcrypt', 'libgpg_error', 'libpng', 'matplotlib', 'maxima', 'mercurial', 'moin', 'mpfi', 'mpfr', 'networkx', 'numpy', 'opencdk', 'pari', 'pexpect', 'pycrypto', 'python', 'python_gnutls', 'quaddouble', 'r', 'readline','scipy','scons','sqlalchemy','sqlite','sympy','termcap','twisted','weave','zlib','zodb3');
+@our_debian = ('cddlib', 'eclib', 'flint', 'flintqs', 'libfplll','genus2reduction','gfan','givaro','iml','lcalc','libm4ri','linbox','ntl','palp','singular','symmetrica','sympow','tachyon','rubiks','scipy_sandbox');
+@ignore = (@use_debian,@our_debian,'ipython1','setuptools','jmol');
 
 while(<>) {
     $arg = $_;

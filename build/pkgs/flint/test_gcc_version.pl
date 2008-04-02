f2f6e4cde8386936aa4573903d81b0c01a167941
mabshoff <mabshoff@sage.math.washington.edu>
2008-04-01 17:15:51 -0700

    Check in test_gcc_version.pl

---
+++ build/pkgs/flint/test_gcc_version.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+#
+# This script tests if gcc is of version greater than
+# 3.4.0. If so it exits with status 0 and no output.
+# If not, it exits with status 1, and prints a warning
+$ver_string=`gcc -dumpversion`;
+$ver_string =~ m/(\d)\.(\d)\.(\d)/;
+
+
+
+if($1 > 3)
+   {
+       exit(0);
+
+   }
+
+if($1 == 3)
+{
+    if($2 > 3)
+    {
+	exit(0);
+    }
+
+}
+
+print "WARNING: gcc version less than 3.4.0 \n";
+exit(1);

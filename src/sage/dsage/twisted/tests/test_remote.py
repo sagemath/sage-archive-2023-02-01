09b6735bee41b9c5b68155e7bac515ef0c948575
Yi Qiang <yqiang@gmail.com>
2008-04-11 09:09:46 -0700

    [mq]: pb_unittest.patch

--- src/sage/dsage/twisted/tests/test_remote.py
+++ src/sage/dsage/twisted/tests/test_remote.py
@@ -22,7 +22,7 @@ from glob import glob
 
 from twisted.trial import unittest
 from twisted.spread import pb
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 from twisted.cred import portal, credentials
 from twisted.conch.ssh import keys
 from twisted.python import log
@@ -39,34 +39,33 @@ from sage.dsage.database.db_config import init_db_sa as init_db
 from sage.dsage.database.job import Job
 from sage.dsage.errors.exceptions import BadJobError
 from sage.dsage.misc.hostinfo import HostInfo
+from sage.dsage.misc.misc import gen_uuid
 from sage.dsage.twisted.tests.test_pubkeyauth import TEST_PUB_KEY
 from sage.dsage.twisted.tests.test_pubkeyauth import TEST_PRIV_KEY
 
 hf = HostInfo().host_info
-hf['uuid'] = ''
+hf['uuid'] = gen_uuid()
 hf['workers'] = 2
 
 RANDOM_DATA =  ''.join([chr(i) for i in [random.randint(65, 123) for n in
                        range(500)]])
 
-
-class ClientRemoteCallsTest(unittest.TestCase):
-    """
-    Tests of remote procedure calls go here.
-
-    """
-
+class RemoteTests(unittest.TestCase):
     test_db = tempfile.NamedTemporaryFile()
 
+    def _login(self, *args, **kwargs):
+        d = self.factory.login(self.creds, None)
+
     def setUp(self):
+        from sqlalchemy.orm import clear_mappers
+        clear_mappers()
+
         Session = init_db(self.test_db.name)
         self.jobdb = JobDatabase(Session)
         self.workerdb = WorkerDatabase(Session)
         self.clientdb = ClientDatabase(Session)
-        self.dsage_server = DSageServer(self.jobdb,
-                                        self.workerdb,
-                                        self.clientdb,
-                                        log_level=5)
+        self.dsage_server = DSageServer(self.jobdb,self.workerdb,
+                                        self.clientdb, log_level=5)
         self.realm = Realm(self.dsage_server)
         self.p = _SSHKeyPortalRoot(portal.Portal(self.realm))
         self.p.portal.registerChecker(
@@ -86,10 +85,8 @@ class ClientRemoteCallsTest(unittest.TestCase):
         self.algorithm = 'rsa'
         self.blob = self.public_key.blob()
         self.signature = self.private_key.sign(self.data)
-        self.creds = credentials.SSHPrivateKey(self.username,
-                                               self.algorithm,
-                                               self.blob,
-                                               self.data,
+        self.creds = credentials.SSHPrivateKey(self.username, self.algorithm,
+                                               self.blob, self.data,
                                                self.signature)
         pubkey_str = self.public_key.toString('openssh')
         try:
@@ -98,14 +95,24 @@ class ClientRemoteCallsTest(unittest.TestCase):
         except:
             self.clientdb.add_client(self.username, pubkey_str)
 
-    def tearDown(self):
-        from sqlalchemy.orm import clear_mappers
-        self.jobdb._shutdown()
-        clear_mappers()
-        self.connection.disconnect()
-        os.remove(self.test_db.name)
+        self.factory = ClientFactory(self._login, (), {})
+        self.factory.continueTrying = False
+        self.connection = reactor.connectTCP(self.hostname,
+                                             self.port,
+                                             self.factory)
 
-        return self.server.stopListening()
+    def tearDown(self):
+        self.jobdb._shutdown()
+        os.remove(self.test_db.name)
+        self.server.stopListening()
+        self.connection.disconnect()
+
+
+class ClientRemoteCallsTest(RemoteTests):
+    """
+    Tests of remote procedure calls go here.
+
+    """
 
     def _catch_failure(self, failure, *args):
         log.msg("Error: ", failure.getErrorMessage())
@@ -114,23 +121,17 @@ class ClientRemoteCallsTest(unittest.TestCase):
     def testremoteSubmitJob(self):
         """tests perspective_submit_job"""
         jobs = self.create_jobs(1)
+        d = self.factory.login(self.creds, None)
+        d.addCallback(self._LoginConnected2, jobs)
 
-        def _login(self, *args, **kwargs):
-            d = self.factory.login(self.creds, None)
-            d.addCallback(self._LoginConnected2, args[0])
-
-            return d
-        factory = ClientFactory(_login, (jobs), {})
-        factory.continueTrying = False
-        self.connection = reactor.connectTCP(self.hostname,
-                                             self.port,
-                                             factory)
+        return d
 
     def _LoginConnected2(self, remoteobj, jobs):
         job = jobs[0]
         job.code = "2+2"
         d = remoteobj.callRemote('submit_job', job._reduce())
         d.addCallback(self._got_job_id)
+
         return d
 
     def _got_job_id(self, job_id):
@@ -139,17 +140,10 @@ class ClientRemoteCallsTest(unittest.TestCase):
     def testremoteSubmitBadJob(self):
         """tests perspective_submit_job"""
 
-        def _login(self, *args, **kwargs):
-            d = self.factory.login(self.creds, None)
-            d.addCallback(self._LoginConnected3)
+        d = self.factory.login(self.creds, None)
+        d.addCallback(self._LoginConnected3)
 
-            return d
-
-        factory = ClientFactory(_login, (), {})
-        factory.continueTrying = False
-        self.connection = reactor.connectTCP(self.hostname,
-                                             self.port,
-                                             factory)
+        return d
 
     def _LoginConnected3(self, remoteobj):
         d = remoteobj.callRemote('submit_job', None)
@@ -168,79 +162,21 @@ class ClientRemoteCallsTest(unittest.TestCase):
 
         return jobs
 
-class WorkerRemoteCallsTest(unittest.TestCase):
+class WorkerRemoteCallsTest(RemoteTests):
     """
     Tests remote calls for monitors.
 
     """
 
-    test_db = tempfile.NamedTemporaryFile()
-
-    def setUp(self):
-        Session = init_db(self.test_db.name)
-        self.jobdb = JobDatabase(Session)
-        self.workerdb = WorkerDatabase(Session)
-        self.clientdb = ClientDatabase(Session)
-        self.dsage_server = DSageServer(self.jobdb,
-                                        self.workerdb,
-                                        self.clientdb,
-                                        log_level=5)
-        self.realm = Realm(self.dsage_server)
-        self.p = _SSHKeyPortalRoot(portal.Portal(self.realm))
-        self.p.portal.registerChecker(
-                            PublicKeyCredentialsCheckerDB(self.clientdb))
-        self.client_factory = pb.PBServerFactory(self.p)
-        self.hostname = 'localhost'
-        self.server = reactor.listenTCP(0, self.client_factory)
-        self.port = self.server.getHost().port
-
-        # public key authentication information
-        self.username = 'unit_test'
-        self.pubkey_file = TEST_PUB_KEY
-        self.privkey_file = TEST_PRIV_KEY
-        self.data = RANDOM_DATA
-        self.private_key = keys.Key.fromString(data=self.privkey_file)
-        self.public_key = keys.Key.fromString(data=self.pubkey_file)
-        self.algorithm = 'rsa'
-        self.blob = self.public_key.blob()
-        self.signature = self.private_key.sign(self.data)
-        self.creds = credentials.SSHPrivateKey(self.username,
-                                               self.algorithm,
-                                               self.blob,
-                                               self.data,
-                                               self.signature)
-        enc_pubkey = self.public_key.toString('openssh')
-        try:
-            self.clientdb.del_client(self.username)
-            self.clientdb.add_client(self.username, enc_pubkey)
-        except:
-            self.clientdb.add_client(self.username, enc_pubkey)
-
-    def tearDown(self):
-        from sqlalchemy.orm import clear_mappers
-        self.jobdb._shutdown()
-        clear_mappers()
-        self.connection.disconnect()
-        os.remove(self.test_db.name)
-
-        return self.server.stopListening()
-
     def testremote_get_job(self):
         job = Job()
         job.code = "2+2"
         self.dsage_server.submit_job(job._reduce())
+        d = self.factory.login(self.creds, (pb.Referenceable(), hf))
+        d.addCallback(self._logged_in)
+        d.addCallback(self._get_job)
 
-        def _login(self, *args, **kwargs):
-            d = self.factory.login(self.creds, (pb.Referenceable(), hf))
-            d.addCallback(self._logged_in)
-            d.addCallback(self._get_job)
-
-            return d
-        factory = ClientFactory(_login, (), {})
-        factory.continueTrying = False
-        self.connection = reactor.connectTCP(self.hostname,
-                                             self.port,
-                                             factory)
+        return d
 
     def _logged_in(self, remoteobj):
         self.assert_(remoteobj is not None)
@@ -257,28 +193,23 @@ class WorkerRemoteCallsTest(unittest.TestCase):
         self.assertEquals(type(jdict), dict)
 
     def testremote_job_done(self):
-        def _login(self, *args, **kwargs):
-            d = self.factory.login(args[0], (pb.Referenceable(), hf))
-            d.addCallback(self._logged_in)
-            d.addCallback(self._job_done, jdict)
-
-            return d
-        factory = ClientFactory(_login, (self.creds), {})
-        factory.continueTrying = False
-
-        self.connection = reactor.connectTCP(self.hostname, self.port,
-                                             factory)
         job = Job()
         job.code = "2+2"
         job_id = self.dsage_server.submit_job(job._reduce())
-        jdict = self.dsage_server.get_job_by_id(job_id)
+        jdict = self.dsage_server.get_job()
+
+        d = self.factory.login(self.creds, (pb.Referenceable(), hf))
+        d.addCallback(self._logged_in)
+        d.addCallback(self._job_done, jdict)
+
+        return d
 
     def _job_done(self, remoteobj, jdict):
         import time
         job_id = jdict['job_id']
         result = jdict['result']
         d = remoteobj.callRemote('job_done', job_id,
-                                 'Nothing.', result, False,
+                                 'Nothing.', result, True,
                                  time.time() - time.time())
         d.addCallback(self._done_job)
 
@@ -287,7 +218,7 @@ class WorkerRemoteCallsTest(unittest.TestCase):
     def _done_job(self, job_id):
         self.assertEquals(type(job_id), str)
         jdict = self.dsage_server.get_job_by_id(job_id)
-        self.assertEquals(jdict['status'], 'new')
+        self.assertEquals(jdict['status'], 'completed')
         self.assertEquals(jdict['output'], 'Nothing.')
 
     def testremote_job_failed(self):
@@ -296,17 +227,11 @@ class WorkerRemoteCallsTest(unittest.TestCase):
         job_id = self.dsage_server.submit_job(job._reduce())
         jdict = self.dsage_server.get_job_by_id(job_id)
 
-        def _login(self, *args, **kwargs):
-            d = self.factory.login(self.creds, (pb.Referenceable(), hf))
-            d.addCallback(self._logged_in)
-            d.addCallback(self._job_failed, args[0])
+        d = self.factory.login(self.creds, (pb.Referenceable(), hf))
+        d.addCallback(self._logged_in)
+        d.addCallback(self._job_failed, jdict)
 
-            return d
-        factory = ClientFactory(_login, (jdict), {})
-        factory.continueTrying = False
-        self.connection = reactor.connectTCP(self.hostname,
-                                             self.port,
-                                             factory)
+        return d
 
     def _job_failed(self, remoteobj, jdict):
         d = remoteobj.callRemote('job_failed', jdict['job_id'], 'Failure')
@@ -327,18 +252,12 @@ class WorkerRemoteCallsTest(unittest.TestCase):
         job_id = self.dsage_server.submit_job(job._reduce())
         jdict = self.dsage_server.get_job_by_id(job_id)
 
-        def _login(self, *args, **kwargs):
-            d = self.factory.login(self.creds, (pb.Referenceable(), hf))
-            d.addCallback(self._logged_in)
-            d.addCallback(self._get_killed_jobs_list)
-            d.addCallback(self._got_killed_jobs_list, args[0])
+        d = self.factory.login(self.creds, (pb.Referenceable(), hf))
+        d.addCallback(self._logged_in)
+        d.addCallback(self._get_killed_jobs_list)
+        d.addCallback(self._got_killed_jobs_list, jdict)
 
-            return d
-        factory = ClientFactory(_login, (jdict), {})
-        factory.continueTrying = False
-        self.connection = reactor.connectTCP(self.hostname,
-                                             self.port,
-                                             factory)
+        return d
 
     def _get_killed_jobs_list(self, remoteobj):
         d = remoteobj.callRemote('get_killed_jobs_list')
@@ -348,7 +267,6 @@ class WorkerRemoteCallsTest(unittest.TestCase):
     def _got_killed_jobs_list(self, killed_jobs_list, jdict):
         self.assertEquals(len(killed_jobs_list), 1)
         self.assertEquals(killed_jobs_list[0]['job_id'], jdict['job_id'])
-
 
 if __name__ == 'main':
     unittest.main()

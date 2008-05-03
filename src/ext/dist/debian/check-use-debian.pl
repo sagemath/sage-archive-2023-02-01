2624f448a242e8a583a3767d33cc58ab49c30dc1
tabbott <tabbott@mit.edu>
2008-05-03 04:20:41 -0400

    Various updates to SAGE's Debian packaging.

--- src/ext/dist/debian/check-use-debian.pl
+++ src/ext/dist/debian/check-use-debian.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # Script to answer queries of the form "is this available in Debian"
 @use_debian = ('bzip2', 'atlas', 'blas', 'clisp', 'cvxopt', 'cython', 'ecm', 'f2c', 'freetype', 'gap', 'gd', 'gdmodule', 'gmp', 'gnutls', 'gsl', 'ipython', 'lapack', 'libgcrypt', 'libgpg_error', 'libpng', 'matplotlib', 'maxima', 'mercurial', 'moin', 'mpfi', 'mpfr', 'networkx', 'numpy', 'opencdk', 'pari', 'pexpect', 'pycrypto', 'python', 'python_gnutls', 'quaddouble', 'r', 'readline','scipy','scons','sqlalchemy','sqlite','sympy','termcap','twisted','weave','zlib','zodb3');
-@our_debian = ('cddlib', 'eclib', 'flint', 'flintqs', 'libfplll','genus2reduction','gfan','givaro','iml','lcalc','libm4ri','linbox','ntl','palp','singular','symmetrica','sympow','tachyon','rubiks','scipy_sandbox');
+@our_debian = ('cddlib', 'eclib', 'flint', 'flintqs', 'libfplll','genus2reduction','gfan','givaro','iml','lcalc','libm4ri','linbox','ntl','palp','singular','symmetrica','sympow','tachyon','rubiks','scipy_sandbox','zn_poly','polybori');
 @ignore = (@use_debian,@our_debian,'ipython1','setuptools','jmol');
 
 while(<>) {

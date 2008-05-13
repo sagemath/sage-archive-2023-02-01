49a830b50c9094c9f994c4873e5b4e8f56e95b2a
John Cremona <john.cremona@gmail.com>
2008-05-13 09:12:34 +0100

    Added longer doctest for #3111

--- src/sage/schemes/elliptic_curves/ell_finite_field.py
+++ src/sage/schemes/elliptic_curves/ell_finite_field.py
@@ -1011,6 +1011,9 @@ class EllipticCurve_finite_field(EllipticCurve_field, HyperellipticCurve_finite_
             sage: E = EllipticCurve('389a')
             sage: for p in [5927, 2297, 1571, 1709, 3851, 127, 3253, 5783, 3499, 4817]:
             ...       G = E.change_ring(GF(p)).abelian_group()
+            sage: for p in prime_range(10000):           #long time (~20s)
+            ...       if p != 389:
+            ...           G=E.change_ring(GF(p)).abelian_group()
         """
         if not debug:
             # if we're in debug mode, always recalculate

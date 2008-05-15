272784d32b41978a02f82db99d370c1dae6042d0
Robert Bradshaw <robertwb@math.washington.edu>
2008-05-14 23:41:32 -0700

    plotting Zp

--- src/sage/rings/padics/padic_base_generic.py
+++ src/sage/rings/padics/padic_base_generic.py
@@ -51,3 +51,50 @@ class pAdicBaseGeneric(padic_generic.pAdicGeneric):
 
     def _uniformizer_print(self):
         return self.variable_name()
+
+    def plot(self, max_points=2500, **args):
+        """
+        Creates a visualization of this p-adic ring as a fractal similar as
+        a generalization of the the Sierpi\'nski triangle. The resulting image
+        attempts to capture the algebraic and topological characteristics of $\Z_p$.
+
+        INPUT:
+            point_count -- the maximum number or points to plot, which
+                           controls the depth of recursion (default 2500)
+            **args      -- color, size, etc. that are passed to the
+                           underlying point graphics objects
+
+        REFERENCES:
+            Cuoco, A. ``Visualizing the $p$-adic Integers'', The American
+                Mathematical Monthly, Vol. 98, No. 4 (Apr., 1991), pp. 355-364
+
+        EXAMPLES:
+            sage: Zp(3).plot()
+            sage: Zp(5).plot(max_points=625)
+            sage: Zp(23).plot(rgbcolor=(1,0,0))
+        """
+        if not args.has_key('pointsize'):
+            args['pointsize'] = 1
+        from sage.misc.mrange import cartesian_product_iterator
+        from sage.rings.real_double import RDF
+        from sage.plot.plot import points, circle, Graphics
+        p = self.prime()
+        phi = 2*RDF.pi()/p
+        V = RDF**2
+        vs = [V([(phi*t).sin(), (phi*t).cos()]) for t in range(p)]
+        all = []
+        depth = max(RDF(max_points).log(p).floor(), 1)
+        scale = min(RDF(1.5/p), 1/RDF(3))
+        pts = [vs]*depth
+        if depth == 1 and 23 < p < max_points:
+            extras = int(max_points/p)
+            if p/extras > 5:
+                pts = [vs]*depth + [vs[::extras]]
+        for digits in cartesian_product_iterator(pts):
+            p = sum([v * scale**n for n, v in enumerate(digits)])
+            all.append(tuple(p))
+        g = points(all, **args)
+        # Set default plotting options
+        g._Graphics__show_axes = False
+        g._Graphics__aspect_ratio = 1
+        return g

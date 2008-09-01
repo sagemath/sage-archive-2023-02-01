1236e6ae39331473203da0d936087f6294a0f955
Jason Merrill <jason.merrill@gmail.com>
2008-09-01 01:13:47 -0400

    Added a call function to FreeModuleElement_generic_dense, which gives the result of calling each of its components.  There's still a broken doctest.

--- src/sage/modules/free_module_element.pyx
+++ src/sage/modules/free_module_element.pyx
@@ -1194,7 +1194,7 @@ def make_FreeModuleElement_generic_dense_v1(parent, entries, degree, is_mutable)
 
 cdef class FreeModuleElement_generic_dense(FreeModuleElement):
     """
-    A generic dense element of a free module. a
+    A generic dense element of a free module.
     """
     ## these work fine on the command line but fail in doctests :-(
 ##         TESTS:
@@ -1431,6 +1431,28 @@ cdef class FreeModuleElement_generic_dense(FreeModuleElement):
         """
         return cmp(left._entries, (<FreeModuleElement_generic_dense>right)._entries)
 
+    def __call__(self, *args, **kwargs):
+        """
+        Calling a free module element returns the result of calling each component.
+
+        EXAMPLES:
+            sage: x, y = var('x,y')
+            sage: f = x^2 + y^2
+            sage: g = f.gradient()
+            sage: g
+            (2*x, 2*y)
+            sage: type(g)
+            <type 'sage.modules.free_module_element.FreeModuleElement_generic_dense'>
+            sage: g(y=2, x=3)
+            (6, 4)
+            sage: f(x,y) = x^2 + y^2
+            sage: g = f.gradient()
+            sage: g(3,2)
+            (6, 4)
+            sage: g(x=3, y=2)
+            (6, 4)
+        """
+        return vector([e(*args, **kwargs) for e in self])
 
     def n(self, *args, **kwargs):
         """

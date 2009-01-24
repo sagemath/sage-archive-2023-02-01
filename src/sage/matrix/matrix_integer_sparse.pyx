748a5f440836c1a364be4d0aca461ffef1e21eea
J. H. Palmieri <palmieri@math.washington.edu>
2009-01-24 08:25:34 -0800

    kernel method for sparse integer matrices (#5089)

--- src/sage/matrix/matrix_integer_sparse.pyx
+++ src/sage/matrix/matrix_integer_sparse.pyx
@@ -296,3 +296,30 @@ cdef class Matrix_integer_sparse(matrix_sparse.Matrix_sparse):
             v.append('%s %s %s'%(ij[0]+1,ij[1]+1,x))
         v.append('0 0 0\n')
         return '\n'.join(v)
+
+    def kernel(self, algorithm='padic', LLL=False, proof=None, echelonize=True):
+        r"""
+        Return the left kernel of this matrix, as a module over the
+        integers.  This is the saturated ZZ-module spanned by all the
+        row vectors v such that v*self = 0.
+
+        INPUT:
+            algorithm -- 'padic': a new p-adic based algorithm
+                         'pari': use PARI
+            LLL -- bool (default: False); if True the basis is an LLL
+                   reduced basis; otherwise, it is an echelon basis.
+            proof -- None (default: proof.linear_algebra()); if False,
+                   impacts how determinants are computed.
+
+        By convention if self has 0 rows, the kernel is of dimension
+        0, whereas the kernel is the whole domain if self has 0 columns.
+
+        EXAMPLES:
+            sage: M = MatrixSpace(ZZ,4,2,sparse=True)(range(8))
+            sage: M.kernel()
+            Free module of degree 4 and rank 2 over Integer Ring
+            Echelon basis matrix:
+            [ 1  0 -3  2]
+            [ 0  1 -2  1]
+        """
+        return self.dense_matrix().kernel(algorithm, LLL, proof, echelonize)

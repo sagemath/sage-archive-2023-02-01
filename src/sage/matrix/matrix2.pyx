99ed73b6056530699dc60cf5fb4c049ffab4e7b7
J. H. Palmieri <palmieri@math.washington.edu>
2009-01-30 16:41:04 -0800

    speed up right_nullity for matrices (#5144)

--- src/sage/matrix/matrix2.pyx
+++ src/sage/matrix/matrix2.pyx
@@ -1418,7 +1418,7 @@ cdef class Matrix(matrix1.Matrix):
             sage: A.right_nullity()
             1
         """
-        return self.transpose().nullity()
+        return self.ncols() - self.rank()
 
     kernel = left_kernel
 

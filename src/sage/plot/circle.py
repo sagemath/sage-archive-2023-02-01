1f0cfc65d3164b485a9152d6c9b36d5e225bac88
Karl-Dieter Crisman <kcrisman@gmail.com>
2009-05-12 12:08:29 -0400

    Brings plot/circle.py to 100% coverage and adds 3D plotting potential.

--- src/sage/plot/circle.py
+++ src/sage/plot/circle.py
@@ -20,9 +20,31 @@ from math import sin, cos, pi
 
 class Circle(GraphicPrimitive):
     """
-    Circle graphics primitive.
+    Primitive class for the Circle graphics type.  See circle? for information
+    about actually plotting circles.
+
+    TESTS:
+
+    We test creating a circle::
+
+        sage: C = circle((2,3), 5)
     """
     def __init__(self, x, y, r, options):
+        """
+        Initializes base class Circle.
+
+        EXAMPLES::
+
+            sage: C = circle((2,3), 5, edgecolor='red', alpha=.5, fill=True)
+            sage: C[0].x
+            2.0
+            sage: C[0].r
+            5.0
+            sage: C[0].options()['edgecolor']
+            'red'
+            sage: C[0].options()['alpha']
+            0.500000000000000
+        """
         self.x = float(x)
         self.y = float(y)
         self.r = float(r)
@@ -32,7 +54,8 @@ class Circle(GraphicPrimitive):
         """
         Returns a dictionary with the bounding box data.
 
-        EXAMPLES:
+        EXAMPLES::
+
             sage: p = circle((3, 3), 1)
             sage: d = p.get_minmax_data()
             sage: d['xmin']
@@ -46,20 +69,45 @@ class Circle(GraphicPrimitive):
                            dict=True)
 
     def _allowed_options(self):
-        return {'alpha':'How transparent the line is.',
-                'fill': 'Whether or not to fill the polygon.',
-                'thickness':'How thick the border of the polygon is.',
-                'edgecolor':'2d only: The color of the edge as an rgb tuple.',
-                'facecolor':'2d only: The color of the face as an rgb tuple.',
+        """
+        Return the allowed options for the Circle class.
+
+        EXAMPLES::
+
+            sage: p = circle((3, 3), 1)
+            sage: p[0]._allowed_options()['alpha']
+            'How transparent the figure is.'
+            sage: p[0]._allowed_options()['facecolor']
+            '2D only: The color of the face as an rgb tuple.'
+        """
+        return {'alpha':'How transparent the figure is.',
+                'fill': 'Whether or not to fill the circle.',
+                'thickness':'How thick the border of the circle is.',
+                'edgecolor':'2D only: The color of the edge as an rgb tuple.',
+                'facecolor':'2D only: The color of the face as an rgb tuple.',
                 'rgbcolor':'The color (edge and face) as an rgb tuple.',
                 'hue':'The color given as a hue.',
-                'zorder':'2d only: The layer level in which to draw',
-                'linestyle':"2d only: The style of the line, which is one of 'dashed', 'dotted', 'solid', 'dashdot'."}
+                'zorder':'2D only: The layer level in which to draw',
+                'linestyle':"2D only: The style of the line, which is one of 'dashed', 'dotted', 'solid', 'dashdot'."}
 
     def _repr_(self):
+        """
+        String representation of Circle primitive.
+
+        EXAMPLES::
+
+            sage: C = circle((2,3), 5)
+            sage: c = C[0]; c
+            Circle defined by (2.0,3.0) with r=5.0
+        """
         return "Circle defined by (%s,%s) with r=%s"%(self.x, self.y, self.r)
 
     def _render_on_subplot(self, subplot):
+        """
+        TESTS::
+
+            sage: C = circle((2,pi), 2, edgecolor='black', facecolor='green', fill=True)
+        """
         import matplotlib.patches as patches
         options = self.options()
         p = patches.Circle((float(self.x), float(self.y)), float(self.r))
@@ -76,11 +124,40 @@ class Circle(GraphicPrimitive):
         p.set_linestyle(options['linestyle'])
         subplot.add_patch(p)
 
-    def plot3d(self, **kwds):
+    def plot3d(self, z=0, **kwds):
         """
+        Plots a 2D circle (actually a 50-gon) in 3D,
+        with default height zero.
+
+        INPUT:
+
+
+        -  ``z`` - optional 3D height above `xy`-plane.
+
         EXAMPLES:
+
             sage: circle((0,0), 1).plot3d()
+
+        This example uses this method implicitly, but does not pass
+        the optional parameter z to this method::
+
             sage: sum([circle((random(),random()), random()).plot3d(z=random()) for _ in range(20)])
+
+        These examples are explicit, and pass z to this method::
+
+            sage: C = circle((2,pi), 2, hue=.8, alpha=.3, fill=True)
+            sage: c = C[0]
+            sage: d = c.plot3d(z=2)
+            sage: d.texture.opacity
+            0.300000000000000
+
+        ::
+
+            sage: C = circle((2,pi), 2, hue=.8, alpha=.3)
+            sage: c = C[0]
+            sage: d = c.plot3d(z=2)
+            sage: d.jmol_repr(d.testing_render_params())[0][-1]
+            'color $line_1 translucent 0.7 [204,0,255]'
         """
         options = dict(self.options())
         fill = options['fill']
@@ -96,41 +173,55 @@ class Circle(GraphicPrimitive):
         ydata = [y+r*sin(t*dt) for t in range(n+1)]
         if fill:
             from polygon import Polygon
-            return Polygon(xdata, ydata, options).plot3d()
+            return Polygon(xdata, ydata, options).plot3d(z)
         else:
             from line import Line
-            return Line(xdata, ydata, options).plot3d()
+            return Line(xdata, ydata, options).plot3d().translate((0,0,z))
 
 
 @options(alpha=1, fill=False, thickness=1, edgecolor='black', facecolor='red', linestyle='solid', zorder=5)
 def circle(center, radius, **options):
     """
-    Return a circle at a point = $(x,y)$ with radius = $r$.
-    Type \code{circle.options} to see all options.
+    Return a circle at a point center = `(x,y)` (or `(x,y,z)` and
+    parallel to the `xy`-plane) with radius = `r`.  Type
+    ``circle.options`` to see all options.
 
-    circle(center, radius, **kwds)
+    OPTIONS:
 
-    INPUT:
-        center -- a 2-tuple (x,y)
-        radius -- a positive number
-        alpha -- default: 1
-        fill -- default: False
-        thickness -- default: 1
-        rgbcolor -- default: (0,0,0)
-        linestyle -- default: 'solid' (2d plotting only)
-        edgecolor -- default: 'black' (2d plotting only)
-        facecolor -- default: 'red' (2d plotting only)
+    - ``alpha`` - default: 1
 
-    EXAMPLES:
+    - ``fill`` - default: False
+
+    - ``thickness`` - default: 1
+
+    - ``rgbcolor`` - default: (0,0,0)
+
+    - ``linestyle`` - default: 'solid' (2D plotting only)
+
+    - ``edgecolor`` - default: 'black' (2D plotting only)
+
+    - ``facecolor`` - default: 'red' (2D plotting only, useful only
+      if Fill=True)
+
+    EXAMPLES::
+
         sage: c = circle((1,1), 1, rgbcolor=(1,0,0))
         sage: c
 
-    To correct the apect ratio of certain graphics, it is necessary
-    to show with a `\code{figsize}' of square dimensions.
+    We can also use this command to plot three-dimensional circles parallel
+    to the `xy`-plane.
+
+        sage: c = circle((1,1,3), 1, rgbcolor=(1,0,0))
+        sage: c
+        sage: type(c)
+        <class 'sage.plot.plot3d.base.TransformGroup'>
+
+    To correct the aspect ratio of certain graphics, it is necessary
+    to show with a ``figsize`` of square dimensions::
 
         sage: c.show(figsize=[5,5],xmin=-1,xmax=3,ymin=-1,ymax=3)
 
-    Here we make an more complicated plot with many circles of different colors
+    Here we make a more complicated plot, with many circles of different colors::
 
         sage: g = Graphics()
         sage: step=6; ocur=1/5; paths=16;
@@ -143,8 +234,35 @@ def circle(center, radius, **options):
         ...
         sage: g.show(xmin=-(paths+1)^2, xmax=(paths+1)^2, ymin=-(paths+1)^2, ymax=(paths+1)^2, figsize=[6,6])
 
+    Note that the ``rgbcolor`` option overrides the other coloring options.
+    This produces red fill in a blue circle::
+
+        sage: C = circle((2,3), 1, fill=True, edgecolor='blue')
+
+    This produces an all-green filled circle::
+
+        sage: C = circle((2,3), 1, fill=True, edgecolor='blue', rgbcolor='green')
+
+    The option ``hue`` overrides *all* other options, so be careful with its use.
+    This produces a purplish filled circle::
+
+        sage: C = circle((2,3), 1, fill=True, edgecolor='blue', rgbcolor='green', hue=.8)
+
+    TESTS:
+
+    We cannot currently plot circles in more than three dimensions::
+
+        sage: c = circle((1,1,1,1), 1, rgbcolor=(1,0,0))
+        Traceback (most recent call last):
+        ...
+        ValueError: The center of a plotted circle should have two or three coordinates.
     """
     from sage.plot.plot import Graphics
     g = Graphics()
     g.add_primitive(Circle(center[0], center[1], radius, options))
-    return g
+    if len(center)==2:
+        return g
+    elif len(center)==3:
+        return g[0].plot3d(z=center[2])
+    else:
+        raise ValueError, 'The center of a plotted circle should have two or three coordinates.'

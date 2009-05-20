82d6cd897d10bc907354f4658296fba81024a7d0
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2009-05-20 10:41:34 +0200

    ext_rep module for reading combinatorial designs

--- src/sage/combinat/designs/all.py
+++ src/sage/combinat/designs/all.py
@@ -3,6 +3,8 @@ from block_design import (AffineGeometryDesign,
                           WittDesign, HadamardDesign,
                           BlockDesign_generic, BlockDesign)
 
+from ext_rep import (designs_from_XML, designs_from_XML_url)
+
 from incidence_structures import (IncidenceStructure,
                           IncidenceStructureFromMatrix)
 

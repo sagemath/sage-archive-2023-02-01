80ba31ebb88650a52f2ff1ee9925b49d3ae9b7cc
Jason Grout <jason-sage@creativetrax.com>
2009-05-27 00:36:26 -0500

    Updated to scipy 0.7.0

--- build/pkgs/scipy/copy_patches.pl
+++ build/pkgs/scipy/copy_patches.pl
@@ -5,7 +5,6 @@ $ver_string = `sage_fortran --version`;
 if ($ver_string =~ m/G95/)
 {
     print "Using g95";
-    copy("patches/setup.py", "src/src");
     copy("patches/setup.py.integrate", "src/scipy/integrate/setup.py");
     copy("patches/setup.py.optimize","src/scipy/optimize/setup.py");
     copy("patches/setup.py.special","src/scipy/special/setup.py");

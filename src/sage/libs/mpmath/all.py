65c20ef2a2257d8f7fdc4e9707f29fbc499eb23e
Fredrik Johansson <fredrik.johansson@gmail.com>
2009-06-03 22:27:31 +0200

    Use mpmath to compute numerical values of constants.
    Previously khinchin, mertens and twinprime were LimitedPrecisionConstant,
    but with this patch they can be computed to any precision.
    
    The patch also adds the Glaisher constant (which is available in mpmath)
    and corrects the name of the Mertens constant.

--- src/sage/libs/mpmath/all.py
+++ src/sage/libs/mpmath/all.py
@@ -19,3 +19,15 @@ from mpmath import *
 # Utilities
 from utils import call, mpmath_to_sage, sage_to_mpmath
 
+# Use mpmath internal functions for constants, to avoid unnecessary overhead
+_constants_funcs = {
+  'glaisher' : glaisher_fixed,
+  'khinchin' : khinchin_fixed,
+  'twinprime' : twinprime_fixed,
+  'mertens' : mertens_fixed
+}
+
+def eval_constant(name, ring):
+    prec = ring.precision() + 20
+    return ring(_constants_funcs[name](prec)) >> prec
+

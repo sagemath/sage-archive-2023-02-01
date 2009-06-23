c952a46927dc64ddb1b883e7bb55b11ca125007a
Martin Albrecht <malb@informatik.uni-bremen.de>
2009-06-23 11:07:54 +0100

    adding lead() to Polynomial wrapper

--- build/pkgs/polybori/patches/PyPolyBoRi.py
+++ build/pkgs/polybori/patches/PyPolyBoRi.py
@@ -2,17 +2,25 @@ from sage import all
 from sage.rings.polynomial.pbori import *
 
 class RingProxy:
-    def __init__(self, ring, order):
-        self.ring = ring
-        self.ordercode = order
+   def __init__(self, ring, order):
+       self.ring = ring
+       self.ordercode = order
 
-    def set(self):
-        self.ring._change_ordering(self.ordercode)
+   def set(self):
+       self.ring._change_ordering(self.ordercode)
 
 def global_ring():
     return RingProxy(get_cring(), get_order_code())
+    #return get_cring()
 
-Polynomial = lambda x = None: get_cring()._coerce_(x)
+class PolynomialFactory:
+    def lead(self, x):
+        return x.lead()
+    def __call__(self, x=None):
+        return get_cring()._coerce_(x)
+
+Polynomial = PolynomialFactory()
+
 Monomial = lambda x = None: get_cring()._monom_monoid(x)
 
 class OrderCode:
@@ -26,3 +34,4 @@ def Ring(n, order='lp'):
     return BooleanPolynomialRing(n, 'x', order=order)
 
 BoolePolynomialVector = BooleanPolynomialVector
+

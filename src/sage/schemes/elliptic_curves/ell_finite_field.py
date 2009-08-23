78f25f42ebf640ac6c900f7562192c3edadb8a0f
Minh Van Nguyen <nguyenminh2@gmail.com>
2009-08-22 19:04:23 -0700

    trac 5857: reviewer patch: trivial typo in ReST formatting

--- src/sage/schemes/elliptic_curves/ell_finite_field.py
+++ src/sage/schemes/elliptic_curves/ell_finite_field.py
@@ -1226,7 +1226,8 @@ class EllipticCurve_finite_field(EllipticCurve_field, HyperellipticCurve_finite_
         """
         Return an iterator through the points of this elliptic curve.
 
-        EXAMPLES:
+        EXAMPLES::
+
             sage: E = EllipticCurve(GF(11), [1,2])
             sage: E.list()
             [(0 : 1 : 0),
@@ -1241,8 +1242,6 @@ class EllipticCurve_finite_field(EllipticCurve_field, HyperellipticCurve_finite_
             (2 : 1 : 1) 8
             ...
             (10 : 0 : 1) 2
-
-
         """
         for P in self.points():
             yield P

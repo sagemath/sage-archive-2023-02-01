f0a89b127c9276d6dcdddb55e83e708e7f200261
Minh Van Nguyen <nguyenminh2@gmail.com>
2009-09-08 07:50:06 -0700

    Some general clean up

--- build/pkgs/glpk/setup.py
+++ build/pkgs/glpk/setup.py
@@ -4,9 +4,9 @@ from Cython.Distutils import build_ext
 
 setup(
    cmdclass = {'build_ext': build_ext},
-   ext_modules = [Extension("sage.numerical.mipGlpk",
-           ["patch/mipGlpk.pyx"],
-         include_dirs=["../../../local/include/","../../../devel/sage/c_lib/include/"],
-         language='c++',
-         libraries=["csage","stdc++","glpk"])]
+   ext_modules = [ Extension("sage.numerical.mipGlpk",
+       ["patch/mipGlpk.pyx"],
+       include_dirs = ["../../../local/include/", "../../../devel/sage/c_lib/include/"],
+       language = 'c++',
+       libraries=["csage", "stdc++", "glpk"]) ]
    )

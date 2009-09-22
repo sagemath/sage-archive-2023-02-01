6f17a714544f80d9fea7f89415f46ba6674911f7
Jason Grout <jason-sage@creativetrax.com>
2009-09-22 11:01:47 -0500

    fixed typo in documentation.

--- src/sage/graphs/cliquer.pyx
+++ src/sage/graphs/cliquer.pyx
@@ -1,7 +1,7 @@
 r"""
 Cliquer : routines for finding cliques in graphs
 
-This module defines functions based on Cliquer, an exact branch-and-bound algorithm developped by Patric R. J. Ostergard and written by Sampo Niskanen.
+This module defines functions based on Cliquer, an exact branch-and-bound algorithm developed by Patric R. J. Ostergard and written by Sampo Niskanen.
 
 AUTHORS:
 

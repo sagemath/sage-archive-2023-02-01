16112469b3d7d26046113718eab7d6962f473436
Mike Hansen <mhansen@gmail.com>
2009-09-25 11:07:22 +0700

    Trac #6919: basic arithmetic using FLINT is broken (very serious!)

--- src/sage/libs/flint/flint.pyx
+++ src/sage/libs/flint/flint.pyx
@@ -1,3 +1,15 @@
+"""
+TESTS:
+
+We verify that Trac #6919 is correctly fixed::
+
+    sage: R.<x> = PolynomialRing(ZZ)
+    sage: A = 2^(2^17+2^15)
+    sage: a = A * x^31
+    sage: b = (A * x) * x^30
+    sage: a == b
+    True
+"""
 include "../../ext/stdsage.pxi"
 include "../../ext/cdefs.pxi"
 

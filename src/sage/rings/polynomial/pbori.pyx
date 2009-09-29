673e45375afb6cdbac0d7684e2870ade3777dc9e
Minh Van Nguyen <nguyenminh2@gmail.com>
2009-09-29 02:05:11 -0700

    trac 7064: fix warnings when building reference manual in Sage 4.1.2.alpha4

--- src/sage/rings/polynomial/pbori.pyx
+++ src/sage/rings/polynomial/pbori.pyx
@@ -4011,7 +4011,7 @@ cdef class BooleanPolynomial(MPolynomial):
 
 cdef class PolynomialFactory:
     """
-    Implements PolyBoRi's ``Polynomial()'' constructor.
+    Implements PolyBoRi's ``Polynomial()`` constructor.
     """
     def lead(self, x):
         """
@@ -4047,7 +4047,7 @@ cdef class PolynomialFactory:
 
 cdef class MonomialFactory:
     """
-    Implements PolyBoRi's ``Monomial()'' constructor.
+    Implements PolyBoRi's ``Monomial()`` constructor.
     """
     def __call__(self, x=None):
         """
@@ -4072,7 +4072,7 @@ cdef class MonomialFactory:
 
 cdef class VariableFactory:
     """
-    Implements PolyBoRi's ``Variable()'' constructor.
+    Implements PolyBoRi's ``Variable()`` constructor.
     """
     def __call__(self, x=None):
         """

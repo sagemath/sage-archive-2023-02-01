1e0c26fc27df3530c8aa3943d7d5c536c91563da
Florent Hivert <Florent.Hivert@univ-rouen.fr>
2010-01-19 12:46:23 +0100

    #7976: Extends ClasscallMetaclass to control inheritance.        Fix exception over-catching in ClasscallMetaclass

--- src/sage/misc/classcall_metaclass.py
+++ src/sage/misc/classcall_metaclass.py
@@ -2,17 +2,10 @@ r"""
 ClasscallMetaclass
 """
 #*****************************************************************************
-#  Copyright (C) 2008 Nicolas M. Thiery <nthiery at users.sf.net>
+#  Copyright (C) 2008    Nicolas M. Thiery <nthiery at users.sf.net>
+#  Copyright (C) 2009    Florent Hivert <Florent.Hivert at univ-rouen.fr>
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
-#
-#    This code is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    General Public License for more details.
-#
-#  The full text of the GPL is available at:
-#
 #                  http://www.gnu.org/licenses/
 #******************************************************************************
 
@@ -22,9 +15,14 @@ class ClasscallMetaclass(NestedClassMetaclass):
     """
     A (trivial) metaclass for customizing class calls via a static method
 
-    For a class ``cls`` in this metaclass, ``cls(<some arguments>)``
-    triggers a call to ``cls.__classcall__(cls, <some arguments>)``.
-    If ``cls.__classcall__`` is not implemented, then
+    For a class ``cls`` in this metaclass, the call ``cls(<some arguments>)``
+    triggers in this priority order the following calls if the respective
+    methods exists:
+
+     - ``cls.__classcall_private__(cls, <some arguments>)``
+     - ``cls.__classcall__(cls, <some arguments>)``
+
+    If neither of these two methods are implemented, then
     ``type.__call__(cls, <some arguments>)`` is called as usual, which
     in turns uses :meth:`__new__` and :meth:`__init__` as usual (see
     Section "Basic Customization" in the Python Reference Manual).
@@ -56,11 +54,21 @@ class ClasscallMetaclass(NestedClassMetaclass):
         True
 
     Another difference is that :meth:`__classcall__` is inherited by
-    subclasses. Depending on the context this may, or may not, be
-    desirable.
+    subclasses. If this is not desirable, one should define the method
+    :meth:`__classcall_private__` which will not be called by
+    subclass. Specifically, If a class ``cls`` define both methods
+    ``__classcall__`` and ``__classcall_private__`` then, for any subclass
+    ``sub`` of ``cls``
 
-    Todo: any better name for ``__classcall__``?
+     - ``cls(<args>)`` will call ``cls.__classcall_private__(cls, <args>)``
+     - ``sub(<args>)`` will call ``cls.__classcall__(sub, <args>)``
+
+    AUTHORS:
+
+     - Nicolas M. Thiery (2009-04) first release
+     - Florent Hivert (2010-01) added __classcall_private__
     """
+
     def __call__(cls, *args, **options):
         """
         This method implements ``cls(<some arguments>)``.
@@ -69,10 +77,10 @@ class ClasscallMetaclass(NestedClassMetaclass):
 
             sage: from sage.misc.classcall_metaclass import ClasscallMetaclass
             sage: class Foo(object):
-            ...       __metaclass__ = sage.structure.unique_representation.ClasscallMetaclass
+            ...       __metaclass__ = ClasscallMetaclass
             ...       @staticmethod
             ...       def __classcall__(cls):
-            ...           print "calling call_cls"
+            ...           print "calling classcall"
             ...           return type.__call__(cls)
             ...       def __new__(cls):
             ...           print "calling new"
@@ -81,12 +89,62 @@ class ClasscallMetaclass(NestedClassMetaclass):
             ...           print "calling init"
             ...
             sage: Foo()
-            calling call_cls
+            calling classcall
             calling new
             calling init
             <__main__.Foo object at ...>
+
+       This behavior is inherited::
+
+            sage: class Bar(Foo): pass
+            sage: Bar()
+            calling classcall
+            calling new
+            calling init
+            <__main__.Bar object at ...>
+
+       We now show the usage of :meth:`__classcall_private__`::
+
+            sage: class FooNoInherits(object):
+            ...       __metaclass__ = ClasscallMetaclass
+            ...       @staticmethod
+            ...       def __classcall_private__(cls):
+            ...           print "calling private classcall"
+            ...           return type.__call__(cls)
+            ...
+            sage: FooNoInherits()
+            calling private classcall
+            <__main__.FooNoInherits object at ...>
+
+            sage: class BarNoInherits(FooNoInherits): pass
+            sage: BarNoInherits()
+            <__main__.BarNoInherits object at ...>
+
+       We now show the usage of both::
+
+            sage: class Foo2(object):
+            ...       __metaclass__ = ClasscallMetaclass
+            ...       @staticmethod
+            ...       def __classcall_private__(cls):
+            ...           print "calling private classcall"
+            ...           return type.__call__(cls)
+            ...       @staticmethod
+            ...       def __classcall__(cls):
+            ...           print "calling classcall with %s"%cls
+            ...           return type.__call__(cls)
+            ...
+            sage: Foo2()
+            calling private classcall
+            <__main__.Foo2 object at ...>
+
+            sage: class Bar2(Foo2): pass
+            sage: Bar2()
+            calling classcall with <class '__main__.Bar2'>
+            <__main__.Bar2 object at ...>
         """
-        try:
+        if '__classcall_private__' in cls.__dict__:
+            return cls.__classcall_private__(cls, *args, **options)
+        elif hasattr(cls, "__classcall__"):
             return cls.__classcall__(cls, *args, **options)
-        except AttributeError:
+        else:
             return type.__call__(cls, *args, **options)

60d40d025cc00cf31b4583a6143656868f141524
Robert L. Miller <rlm@rlmiller.org>
2010-03-15 16:42:13 -0700

    [mq]: trac_8539.patch

--- src/sage/matrix/matrix_integer_sparse.pyx
+++ src/sage/matrix/matrix_integer_sparse.pyx
@@ -318,6 +318,25 @@ cdef class Matrix_integer_sparse(matrix_sparse.Matrix_sparse):
         return nzc
 
     def _mod_int(self, modulus):
+        """
+        Helper function in reducing matrices mod n.
+
+        INPUT:
+
+        - `modulus` - a number
+
+        OUTPUT:
+
+        This matrix, over `ZZ/nZZ`.
+
+        TESTS::
+
+            sage: M = Matrix(ZZ, sparse=True)
+            sage: B = M._mod_int(7)
+            sage: B.parent()
+            Full MatrixSpace of 0 by 0 sparse matrices over Ring of integers modulo 7
+
+        """
         return self._mod_int_c(modulus)
 
     cdef _mod_int_c(self, mod_int p):
@@ -326,7 +345,7 @@ cdef class Matrix_integer_sparse(matrix_sparse.Matrix_sparse):
         cdef mpz_vector* self_row
         cdef c_vector_modint* res_row
         res = Matrix_modn_sparse.__new__(Matrix_modn_sparse, matrix_space.MatrixSpace(
-            IntegerModRing(p), self._nrows, self._ncols, sparse=False), None, None, None)
+            IntegerModRing(p), self._nrows, self._ncols, sparse=True), None, None, None)
         for i from 0 <= i < self._nrows:
             self_row = &(self._matrix[i])
             res_row = &(res.rows[i])

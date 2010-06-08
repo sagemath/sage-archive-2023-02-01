86a4f090a5715313bdf8577ee42df69d4e9bbf4b
Brant Jones <brant at math.ucdavis.edu>
2010-06-07 17:02:36 -0700

    #8984:  Implements the alcove path model of Lenart--Postnikov and implements associated_coroot() in the root_lattice_realization.

--- src/sage/combinat/crystals/all.py
+++ src/sage/combinat/crystals/all.py
@@ -10,3 +10,6 @@ from affine import AffineCrystalFromClassicalAndPromotion
 from kirillov_reshetikhin import KirillovReshetikhinCrystal
 from highest_weight_crystals import HighestWeightCrystal
 from direct_sum import DirectSumOfCrystals
+from alcove_path import ClassicalCrystalOfAlcovePaths
+from alcove_path import ClassicalCrystalOfAlcovePathsElement
+

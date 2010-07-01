33dd0ad6d7aedc2281638c9aad66f7e25a95f241
David Kirkby <david.kirkby@onetel.net>
2010-07-01 13:43:03 +0100

    #9399 Remove Sun-specific junk in rings/finite_rings/stdint.h

--- src/sage/rings/finite_rings/stdint.h
+++ src/sage/rings/finite_rings/stdint.h
@@ -1,10 +1,5 @@
 
-#if defined(__sun)
-typedef int int_fast32_t;
-typedef long long int_fast64_t;
-#else
 #include <stdint.h>
-#endif
 
 #define INTEGER_MOD_INT32_LIMIT 46341          //  = ceil(sqrt(2^31-1))
 #define INTEGER_MOD_INT64_LIMIT 2147483647     //  = 2^31-1 for now, should be 3037000500LL = ceil(sqrt(2^63-1))

41aead292d7b1485994f0372867dfc0b201bd84c
Leif Leonhardy <not.really@online.de>
2010-07-10 21:44:58 +0200

    Removed BOOST and M4RI source; minor fixes in spkg-install and patches/custom.py
    === polybori-0.6.4.p2 (Leif Leonhardy, July 10th, 2010) ===
     * Removed Michael Abshoff from maintainer list (see #7738)
     * Deleted Boost source tree again since it was split off into a separate
       spkg (see below, 0.5.rc.p7), modified spkg-install accordingly
     * Deleted M4RI source tree, because it is a standard Sage package
     * Little clean-up and minor fixes in patches/custom.py
       - Note that CFLAGS etc. are still *overwritten* rather than modified!
     * Updated "Dependencies" section above

--- build/pkgs/polybori/patches/custom.py
+++ build/pkgs/polybori/patches/custom.py
@@ -1,25 +1,34 @@
 import os
 import sys
 
+# FIXME: Do we really want to *overwrite* the flags (e.g. if set by the user)?
 CCFLAGS=["-O3 -Wno-long-long -Wreturn-type -g -fPIC"]
-CXXFLAGS=CCFLAGS+["-ftemplate-depth-100 -g -fPIC"]
+#CXXFLAGS=CCFLAGS+["-ftemplate-depth-100 -g -fPIC"]
+CXXFLAGS=CCFLAGS+["-ftemplate-depth-100"]
+
+# FIXME: Should we include LDFLAGS here? (see above)
+if not globals().has_key("LINKFLAGS"): LINKFLAGS=[] # s.t. we can *append* below
+
 
 if sys.platform=='darwin':
     FORCE_HASH_MAP=True
 
 
-if os.environ.has_key('SAGE_DEBUG'):
+if os.environ.get('SAGE_DEBUG', "no") == "yes":
     CPPDEFINES=[]
-    CCFLAGS=[" -pg"] + CCFLAGS
-    CXXFLAGS=[" -pg"] + CXXFLAGS
-    LINKFLAGS=[" -pg"]
+    CCFLAGS=["-pg"] + CCFLAGS
+    CXXFLAGS=["-pg"] + CXXFLAGS
+    # LINKFLAGS=["-pg"]
+    LINKFLAGS=["-pg"] + LINKFLAGS
 
-if os.environ['SAGE64'] == "yes":
-    print "64 bit OSX build"
-    CCFLAGS=[" -m64"] + CCFLAGS
-    CXXFLAGS=[" -m64"] + CXXFLAGS
-    LINKFLAGS=[" -m64"]
+if os.environ.get('SAGE64', "no") == "yes":
+    print "Building a 64-bit version of PolyBoRi"
+    CCFLAGS=["-m64"] + CCFLAGS
+    CXXFLAGS=["-m64"] + CXXFLAGS
+    # LINKFLAGS=["-m64"]
+    LINKFLAGS=["-m64"] + LINKFLAGS
 
+# (BOOSTINCDIR is set and now exported in spkg-install:)
 CPPPATH=[os.environ['BOOSTINCDIR']]+[os.environ['SAGE_LOCAL']+"/include"]
 PYPREFIX=os.environ['PYTHONHOME']
 PBP=os.environ['PYTHONHOME']+'/bin/python'
@@ -32,6 +41,7 @@ HAVE_TEX4HT=False
 HAVE_PYTHON_EXTENSION=False
 EXTERNAL_PYTHON_EXTENSION=True
 
+# (CC and CXX should have been set by sage-env, but never mind...:)
 try:
   CC = os.environ['CC']
 except:

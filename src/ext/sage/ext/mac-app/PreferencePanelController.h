089fc991015098e82baa8ec326b7dde777e43f31
Ivan Andrus <darthandrus@gmail.com>
2010-09-21 08:20:50 +0200

    Trac 9873: Completely new Sage.app

---
+++ src/ext/sage/ext/mac-app/PreferencePanelController.h
@@ -0,0 +1,27 @@
+//
+//  PreferencePanelController.h
+//
+//  Created by Ivan Andrus on 26/6/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferencePanelController : NSObject {
+    IBOutlet id appController;
+
+    IBOutlet id showInDock;
+    IBOutlet id TerminalApplescript;
+    IBOutlet id TerminalEmulator;
+    IBOutlet id SessionType;
+    IBOutlet id DefaultArgs;
+}
+
+- (IBAction)apply:(id)sender;
+- (IBAction)resetTerminalApplescript:(id)sender;
+- (void)windowDidBecomeKey:(NSNotification *)aNotification;
+- (void)comboBoxWillDismiss:(NSNotification *)notification;
+- (void)controlTextDidEndEditing:(NSNotification *)aNotification;
+- (void)updateForComboBoxChanges;
+
+@end

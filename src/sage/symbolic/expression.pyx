7c78ddd5ad7338a98b949cb7b694d2fe0ae9c159
Karl-Dieter Crisman <kcrisman@gmail.com>
2010-10-04 20:41:32 -0400

    Trac 9881 - reviewer patch clarifies minor point and moves doctests where they belong

--- src/sage/symbolic/expression.pyx
+++ src/sage/symbolic/expression.pyx
@@ -4713,7 +4713,8 @@ cdef class Expression(CommutativeRingElement):
         symbolic expression.
 
         If self is not real, return the complex half-plane (left or right)
-        in which the number lies.
+        in which the number lies.  If self is pure imaginary, return the sign
+        of the imaginary part of self.
 
         EXAMPLES::
 
@@ -4730,6 +4731,16 @@ cdef class Expression(CommutativeRingElement):
             1
             sage: SR(I).csgn()
             1
+            sage: SR(-I).csgn()
+            -1
+            sage: SR(1+I).csgn()
+            1
+            sage: SR(1-I).csgn()
+            1
+            sage: SR(-1+I).csgn()
+            -1
+            sage: SR(-1-I).csgn()
+            -1
 
         Using the ``hold`` parameter it is possible to prevent automatic
         evaluation::
@@ -5995,16 +6006,6 @@ cdef class Expression(CommutativeRingElement):
             1
             sage: SR(0).binomial(0)
             1
-            sage: SR(-I).csgn()
-            -1
-            sage: SR(1+I).csgn()
-            1
-            sage: SR(1-I).csgn()
-            1
-            sage: SR(-1+I).csgn()
-            -1
-            sage: SR(-1-I).csgn()
-            -1
         """
         cdef Expression nexp = self.coerce_in(k)
         _sig_on

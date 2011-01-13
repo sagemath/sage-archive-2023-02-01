63f4800188df401ca320dcf73c63889daab24b37
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2011-01-13 02:15:48 +0100

    #10614: Increase maximum startuptime

--- src/sage/tests/startup.py
+++ src/sage/tests/startup.py
@@ -4,7 +4,7 @@ Test that on sage.math (a fast computer), the Sage startup time (from
 a warm cache) has not got out of hand.::
 
     sage: if os.uname()[1] == 'sage.math.washington.edu':
-    ...     print float(os.popen("sage -startuptime>/dev/null; sage -startuptime|grep sage.all").readlines()[0].split()[1]) < 1.5
+    ...     print float(os.popen("sage -startuptime>/dev/null; sage -startuptime|grep sage.all").readlines()[0].split()[1]) < 2.0
     ... else: print True   # nothing when not on sage.math
     True
 

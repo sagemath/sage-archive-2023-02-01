79ee9e9192172d837df7482bff68d8840dce29b5
Nicolas M. Thiery <nthiery@users.sf.net>
2011-04-08 11:25:40 +0200

    Trac #11156: #11156: sage.misc.sage_unittest.InstanceTester should call its base's __init__

--- src/sage/misc/sage_unittest.py
+++ src/sage/misc/sage_unittest.py
@@ -375,6 +375,7 @@ class InstanceTester(unittest.TestCase):
             sage: QQ._tester()
             Testing utilities for Rational Field
         """
+        unittest.TestCase.__init__(self)
         self._instance = instance
         self._verbose = verbose
         self._elements = elements

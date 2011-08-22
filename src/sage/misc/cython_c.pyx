88e05f295f29476c11fc035f3e5b6b72783a8f7c
Martin Albrecht <martinralbrecht@googlemail.com>
2011-08-22 08:09:57 +0100

    Trac #11712: #11712: add documentation about pragmas to cython() function

--- src/sage/misc/cython_c.pyx
+++ src/sage/misc/cython_c.pyx
@@ -5,27 +5,54 @@ def cython(code,
           verbose=False, compile_message=False,
           make_c_file_nice=False, use_cache=False):
     """
-    Given a block of Cython (Sage's variant of Pyrex) code (as a text
-    string), this function compiles it using your C compiler, and
-    includes it into the global scope of the module that called this
-    function.
+    Given a block of Cython code (as a text string), this function
+    compiles it using a C compiler, and includes it into the global
+    scope of the module that called this function.
 
-    WARNING: Only use this from Python code, not from extension code,
-    since from extension code you would change the global scope (i.e.,
-    of the Sage interpreter). And it would be stupid, since you're
-    already writing Cython!
+    The following pragmas are available:
 
-    Also, never use this in the standard Sage library.  Any code that
-    uses this can only run on a system that has a C compiler
-    installed, and we want to avoid making that assumption for casual
-    Sage usage.  Also, any code that uses this in the library would
-    greatly slow down startup time, since currently there is no
-    caching.
+    - ``clang`` - may be either c or c++ indicating whether a C or C++
+      compiler should be used.
+
+    - ``clib`` - additional libraries to be linked in, the space
+      separated list is split and passed to distutils.
+
+    - ``cinclude`` - additional directories to search for header
+      files. The space separated list is split and passed to
+      distutils.
+
+    - ``cfile`` - additional C or C++ files to be compiled
+
+    - ``cargs`` - additional parameters passed to the compiler
+
+    For example::
+
+        #clang C++
+        #clib givaro
+        #cinclude /usr/local/include/
+        #cargs -ggdb
+        #cfile foo.c
 
     AUTHOR: William Stein, 2006-10-31
 
-    TODO: Need to create a clever caching system so code only gets
-    compiled once.
+    .. warn:
+
+        Only use this from Python code, not from extension code, since
+        from extension code you would change the global scope (i.e.,
+        of the Sage interpreter). And it would be stupid, since you're
+        already writing Cython!
+
+        Also, never use this in the standard Sage library.  Any code
+        that uses this can only run on a system that has a C compiler
+        installed, and we want to avoid making that assumption for
+        casual Sage usage.  Also, any code that uses this in the
+        library would greatly slow down startup time, since currently
+        there is no caching.
+
+    .. todo:
+
+        Need to create a clever caching system so code only gets
+        compiled once.
     """
     tmpfile = sage.misc.misc.tmp_filename() + ".spyx"
     open(tmpfile,'w').write(code)

cb679158b56d9b0c6009ec0b1f343e9ef3d4d2f5
William Stein <wstein@gmail.com>
2011-11-13 12:55:23 -0800

    Trac #12025: trac 12025: get doctest coverage of monoids/monoid.py to 100%

--- src/sage/monoids/monoid.py
+++ src/sage/monoids/monoid.py
@@ -6,25 +6,45 @@ from sage.structure.parent import Parent
 from sage.misc.cachefunc import cached_method
 
 def is_Monoid(x):
+    """
+    Return True if x is of type Monoid_class.
+
+    EXAMPLES::
+
+        sage: from sage.monoids.monoid import is_Monoid
+        sage: is_Monoid(0)
+        False
+        sage: is_Monoid(ZZ)   # not technical math meaning of monoid
+        False
+        sage: is_Monoid(sage.monoids.monoid.Monoid_class(('a','b')))
+        True
+        sage: F.<a,b,c,d,e> = FreeMonoid(5)
+        sage: is_Monoid(F)
+        True
+    """
     return isinstance(x, Monoid_class)
 
 class Monoid_class(Parent):
     def __init__(self,names):
+        """
+        EXAMPLES::
+
+            sage: from sage.monoids.monoid import Monoid_class
+            sage: Monoid_class(('a','b'))
+            <class 'sage.monoids.monoid.Monoid_class_with_category'>
+        """
         from sage.categories.monoids import Monoids
-        #self._assign_names(names)
         Parent.__init__(self, base=self,names=names,category=Monoids())
+
     @cached_method
     def gens(self):
+        """
+        Return generators for self.
+
+        EXAMPLES::
+
+            sage: F.<a,b,c,d,e> = FreeMonoid(5)
+            sage: F.gens()
+            (a, b, c, d, e)
+        """
         return tuple(self.gen(i) for i in range(self.ngens()))
-#    def category(self):
-#        # Defining a category method is deprecated for parents.
-#        # Instead, the category should be specified in the constructor.
-#        # See: http://sagetrac.org/sage_trac/wiki/CategoriesRoadMap
-#        if self._is_category_initialized():
-#            return Parent.category(self)
-#
-#        # Import this locally, rather than at the top level, to avoid
-#        # a circular import chain
-#        # (category_types -> sage.algebras.all -> sage.algebras.free_algebra ->
-#        # sage.monoids.free_monoid -> sage.monoids.monoid ->
-#        # sage.categories.all -> category_types)

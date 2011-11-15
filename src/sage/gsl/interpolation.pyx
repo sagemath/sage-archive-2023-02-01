d130ec8c44bf899de27c36b73a2ec6f052843cd5
William Stein <wstein@gmail.com>
2011-11-15 08:49:50 -0800

    Trac #12036: trac 12036: improve doctest coverage of gsl/interpolation.pyx from 0% to 100%

--- src/sage/gsl/interpolation.pyx
+++ src/sage/gsl/interpolation.pyx
@@ -17,12 +17,22 @@ cdef class Spline:
     spline is recomputed.
 
     EXAMPLES:
-    This is the example in the GSL documentation.
+
+    This example is in the GSL documentation::
+
         sage: v = [(i + sin(i)/2, i+cos(i^2)) for i in range(10)]
         sage: s = spline(v)
         sage: show(point(v) + plot(s,0,9, hue=.8))
     """
     def __init__(self, v=[]):
+        """
+        EXAMPLES::
+
+            sage: S = spline([(1,1), (2,3), (4,5)]); S
+            [(1, 1), (2, 3), (4, 5)]
+            sage: type(S)
+            <type 'sage.gsl.interpolation.Spline'>
+        """
         self.v = list(v)
         self.started = 0
 
@@ -30,6 +40,29 @@ cdef class Spline:
         self.stop_interp()
 
     def __setitem__(self, int i, xy):
+        """
+        EXAMPLES::
+
+            sage: S = spline([(1,1), (2,3), (4,5)]); S
+            [(1, 1), (2, 3), (4, 5)]
+            sage: S(1.5)
+            2.0625
+
+        Replace 0th point, which changes the spline::
+
+            sage: S[0]=(0,1); S
+            [(0, 1), (2, 3), (4, 5)]
+            sage: S(1.5)
+            2.5
+
+        If you set the n-th point and n > len(S), then (0,0) points
+        are inserted and the n-th entry is set (which may be a weird
+        thing to do, but that is what happens)::
+
+            sage: S[4] = (6,10)
+            sage: S
+            [(0, 1), (2, 3), (4, 5), (0, 0), (6, 10)]
+        """
         cdef int j
         if i < len(self.v):
             self.v[i] = xy
@@ -40,21 +73,69 @@ cdef class Spline:
         self.stop_interp()
 
     def __getitem__(self, int i):
+        """
+        EXAMPLES::
+
+            sage: S = spline([(1,1), (2,3), (4,5)]); S[0]
+            (1, 1)
+            sage: S[-1]
+            (4, 5)
+            sage: S[1]
+            (2, 3)
+        """
         return self.v[i]
 
     def __delitem__(self, int i):
+        """
+        EXAMPLES::
+
+            sage: S = spline([(1,1), (2,3), (4,5)]); S
+            [(1, 1), (2, 3), (4, 5)]
+            sage: del S[1]
+            sage: S
+            [(1, 1), (4, 5)]
+        """
         del self.v[i]
 
     def append(self, xy):
+        """
+        EXAMPLES::
+
+            sage: S = spline([(1,1), (2,3), (4,5)]); S.append((5,7)); S
+            [(1, 1), (2, 3), (4, 5), (5, 7)]
+        """
         self.v.append(xy)
 
     def list(self):
+        """
+        Underlying list of points that this spline goes through.  This
+        is a reference to the list, not a copy.
+
+        EXAMPLES::
+
+            sage: S = spline([(1,1), (2,3), (4,5)]); S.list()
+            [(1, 1), (2, 3), (4, 5)]
+        """
         return self.v
 
     def __len__(self):
+        """
+        Number of points that the spline goes through.
+
+        EXAMPLES::
+
+            sage: len(spline([(1,1), (2,3), (4,5)]))
+            3
+        """
         return len(self.v)
 
     def __repr__(self):
+        """
+        EXAMPLES::
+
+            sage: spline([(1,1), (2,3), (4,5)]).__repr__()
+            '[(1, 1), (2, 3), (4, 5)]'
+        """
         return str(self.v)
 
     cdef start_interp(self):
@@ -95,6 +176,21 @@ cdef class Spline:
         self.started = 0
 
     def __call__(self, double x):
+        """
+        Value of the spline function at x.
+
+        EXAMPLES::
+
+            sage: S = spline([(1,1), (2,3), (4,5)])
+            sage: S(1)
+            1.0
+            sage: S(2)
+            3.0
+            sage: S(4)
+            5.0
+            sage: S(3.5)
+            4.65625
+        """
         if not self.started:
             self.start_interp()
         sig_on()

274cdb94851d2ee5f705c323f85587e045fcb492
Mike Hansen <mhansen@gmail.com>
2012-05-28 00:43:31 -0700

    Trac #12862: #8470: The partition function under FreeBSD

--- src/sage/combinat/partitions_c.cc
+++ src/sage/combinat/partitions_c.cc
@@ -144,7 +144,7 @@ const unsigned int min_precision = DBL_MANT_DIG;                            // T
 const unsigned int double_precision = DBL_MANT_DIG;                         // The assumed precision of a double.
 
 
-#if defined(__sparc) || defined(__CYGWIN__)
+#if defined(__sparc) || defined(__CYGWIN__)  || defined(__FreeBSD__)
 // On sparc solaris long double is bad/broken/different, etc.  E.g.,
 // LDBL_MANT_DIG is 113 rather than 106, which causes all kinds of trouble.
 // So we only use double_precision.

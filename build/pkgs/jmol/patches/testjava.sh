214b52da6ac9273f9c016dbbbe8979cada89e916
Jonathan Gutow <gutow@uwosh.edu>
2012-07-12 09:27:22 -0500

    Add java version checking

--- build/pkgs/jmol/patches/testjava.sh
+++ build/pkgs/jmol/patches/testjava.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 type -atp java
-#OUT=$?
+OUT=$?
 #echo $OUT
-#if [ $OUT -eq 0 ]; then
-#  echo "0"
+if [ $OUT -eq 0 ]; then
+#found java now check 1.5<=version<=1.7
+#version >1.7 may be OK just not checked yet.
+  java -version 2>&1|grep version.*[1]\.[567]
 #else
 #  echo "1"
-#fi
+fi
+#OUT=$?
+#echo $OUT

eb27e38df3dfc10980656e3a9d4dca734dd2562a
Kevin Halasz <Khalasz@pugetsound.edu>
2012-07-13 15:21:40 -0700

    Trac #13251: Add General dihedral groups

--- src/sage/groups/perm_gps/all.py
+++ src/sage/groups/perm_gps/all.py
@@ -5,7 +5,7 @@ from permgroup_named import (SymmetricGroup, AlternatingGroup,
                        DihedralGroup, CyclicPermutationGroup,
                        DiCyclicGroup, TransitiveGroup, PGL, PSL, PSp,PSU,PGU,
                        MathieuGroup, KleinFourGroup, QuaternionGroup,
-                       SuzukiGroup, TransitiveGroups)
+                       SuzukiGroup, TransitiveGroups, GeneralDihedralGroup)
 
 from permgroup import  PermutationGroup, PermutationGroup_generic, PermutationGroup_subgroup, direct_product_permgroups
 

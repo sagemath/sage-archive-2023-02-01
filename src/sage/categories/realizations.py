300eb3474c1c17872d1d32975129f3d53b5c6253
Franco Saliola <saliola@gmail.com>
2012-07-15 10:47:29 -0400

    Trac #12959: Improvements to Sets.WithRealizations (doctest fix)

--- src/sage/categories/realizations.py
+++ src/sage/categories/realizations.py
@@ -76,9 +76,9 @@ def Realizations(self):
         Category of realizations of The subset algebra of {1, 2, 3} over Rational Field
 
         sage: C = GradedHopfAlgebrasWithBasis(QQ).Realizations(); C
-        Join of Category of graded hopf algebras with basis over Rational Field and Category of realizations of sets
+        Join of Category of graded hopf algebras with basis over Rational Field and Category of realizations of magmas
         sage: C.super_categories()
-        [Category of graded hopf algebras with basis over Rational Field, Category of realizations of sets]
+        [Category of graded hopf algebras with basis over Rational Field, Category of realizations of magmas]
 
         sage: TestSuite(C).run()
 

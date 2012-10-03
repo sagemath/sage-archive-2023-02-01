1f951baf1e33015dd9a55d452940b6b9290f6763
Robert Bradshaw <robertwb@math.washington.edu>
2012-10-02 23:13:45 -0700

    Trac #13562: Make a couple large graph imports lazy.

--- src/sage/graphs/all.py
+++ src/sage/graphs/all.py
@@ -1,5 +1,7 @@
-from graph_generators import graphs
-from digraph_generators import digraphs
+from sage.misc.lazy_import import lazy_import
+
+lazy_import("sage.graphs.graph_generators", "graphs")
+lazy_import("sage.graphs.digraph_generators", "digraphs")
 from graph_database import GraphDatabase, GenericGraphQuery, GraphQuery
 from graph import Graph
 from digraph import DiGraph
@@ -12,13 +14,13 @@ import sage.graphs.generic_graph
 import sage.graphs.graph_decompositions
 import sage.graphs.graph
 import sage.graphs.digraph
-import graph_coloring
+lazy_import("sage.graphs", "graph_coloring")
 import sage.graphs.graph_decompositions
 import sage.graphs.modular_decomposition.modular_decomposition
 import sage.graphs.comparability
 from sage.graphs.cliquer import *
 from graph_database import graph_db_info
-from graph_editor import graph_editor
+lazy_import("sage.graphs.graph_editor", "graph_editor")
 
 import sage.graphs.isgci
 from sage.graphs.isgci import graph_classes

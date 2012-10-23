a99a69627c1659ee0c8a9d25a4eda29e9641e158
Volker Braun <vbraun.name@gmail.com>
2012-10-23 22:40:46 +0100

    Trac #11763: fix the associahedron
    The Associahedron class derives from Polyhedron_QQ_ppl. It must
    be converted to the parent/element framework as well.

--- src/sage/combinat/root_system/associahedron.py
+++ src/sage/combinat/root_system/associahedron.py
@@ -20,16 +20,25 @@ AUTHORS:
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 from sage.geometry.polyhedron.backend_ppl import Polyhedron_QQ_ppl
+from sage.geometry.polyhedron.parent import Polyhedra_QQ_ppl
 from sage.combinat.root_system.cartan_type import CartanType
 from sage.modules.free_module_element import vector
+from sage.rings.all import QQ
 
-class Associahedron(Polyhedron_QQ_ppl):
+
+def Associahedron(cartan_type):
     r"""
-    The generalized associahedron is a polytopal complex with vertices in one-to-one correspondence
-    with clusters in the cluster complex, and with edges between two vertices if and only if the associated two
-    clusters intersect in codimension 1.
+    Construct an associahedron
 
-    The associahedron of type `A_n` is one way to realize the classical associahedron as defined in
+    An Associahedron
+
+    The generalized associahedron is a polytopal complex with vertices
+    in one-to-one correspondence with clusters in the cluster complex,
+    and with edges between two vertices if and only if the associated
+    two clusters intersect in codimension 1.
+
+    The associahedron of type `A_n` is one way to realize the
+    classical associahedron as defined in
 
     http://en.wikipedia.org/wiki/Associahedron.
 
@@ -41,9 +50,14 @@ class Associahedron(Polyhedron_QQ_ppl):
         sage: Asso = Associahedron(['A',2]); Asso
         Generalized associahedron of type ['A', 2] with 5 vertices
         sage: sorted(Asso.Hrepresentation(), key=repr)
-        [An inequality (-1, 0) x + 1 >= 0, An inequality (0, -1) x + 1 >= 0, An inequality (0, 1) x + 1 >= 0, An inequality (1, 0) x + 1 >= 0, An inequality (1, 1) x + 1 >= 0]
+        [An inequality (-1, 0) x + 1 >= 0,
+         An inequality (0, -1) x + 1 >= 0,
+         An inequality (0, 1) x + 1 >= 0,
+         An inequality (1, 0) x + 1 >= 0,
+         An inequality (1, 1) x + 1 >= 0]
         sage: Asso.Vrepresentation()
-        (A vertex at (1, -1), A vertex at (1, 1), A vertex at (-1, 1), A vertex at (-1, 0), A vertex at (0, -1))
+        (A vertex at (1, -1), A vertex at (1, 1), A vertex at (-1, 1),
+         A vertex at (-1, 0), A vertex at (0, -1))
 
         sage: Associahedron(['B',2])
         Generalized associahedron of type ['B', 2] with 6 vertices
@@ -61,9 +75,25 @@ class Associahedron(Polyhedron_QQ_ppl):
     TESTS::
 
         sage: sorted(Associahedron(['A',3]).vertices())
-        [[-3/2, 0, -1/2], [-3/2, 0, 3/2], [-3/2, 1, -3/2], [-3/2, 2, -3/2], [-3/2, 2, 3/2], [-1/2, -1, -1/2], [-1/2, 0, -3/2], [1/2, -2, 1/2], [1/2, -2, 3/2], [3/2, -2, 1/2], [3/2, -2, 3/2], [3/2, 0, -3/2], [3/2, 2, -3/2], [3/2, 2, 3/2]]
+        [A vertex at (-3/2, 0, -1/2), A vertex at (-3/2, 0, 3/2),
+         A vertex at (-3/2, 1, -3/2), A vertex at (-3/2, 2, -3/2),
+         A vertex at (-3/2, 2, 3/2), A vertex at (-1/2, -1, -1/2),
+         A vertex at (-1/2, 0, -3/2), A vertex at (1/2, -2, 1/2),
+         A vertex at (1/2, -2, 3/2), A vertex at (3/2, -2, 1/2),
+         A vertex at (3/2, -2, 3/2), A vertex at (3/2, 0, -3/2),
+         A vertex at (3/2, 2, -3/2), A vertex at (3/2, 2, 3/2)]
+
         sage: sorted(Associahedron(['B',3]).vertices())
-        [[-3, 0, 0], [-3, 0, 3], [-3, 2, -2], [-3, 4, -3], [-3, 5, -3], [-3, 5, 3], [-2, 1, -2], [-2, 3, -3], [-1, -2, 0], [-1, -1, -1], [1, -4, 1], [1, -3, 0], [2, -5, 2], [2, -5, 3], [3, -5, 2], [3, -5, 3], [3, -3, 0], [3, 3, -3], [3, 5, -3], [3, 5, 3]]
+        [A vertex at (-3, 0, 0), A vertex at (-3, 0, 3),
+         A vertex at (-3, 2, -2), A vertex at (-3, 4, -3),
+         A vertex at (-3, 5, -3), A vertex at (-3, 5, 3),
+         A vertex at (-2, 1, -2), A vertex at (-2, 3, -3),
+         A vertex at (-1, -2, 0), A vertex at (-1, -1, -1),
+         A vertex at (1, -4, 1), A vertex at (1, -3, 0),
+         A vertex at (2, -5, 2), A vertex at (2, -5, 3),
+         A vertex at (3, -5, 2), A vertex at (3, -5, 3),
+         A vertex at (3, -3, 0), A vertex at (3, 3, -3),
+         A vertex at (3, 5, -3), A vertex at (3, 5, 3)]
 
         sage: Associahedron(['A',4]).f_vector()
         (1, 42, 84, 56, 14, 1)
@@ -72,31 +102,30 @@ class Associahedron(Polyhedron_QQ_ppl):
 
     REFERENCES:
 
-    - [CFZ] Chapoton, Fomin, Zelevinsky - Polytopal realizations of generalized associahedra, arXiv:0202004.
+    - [CFZ] Chapoton, Fomin, Zelevinsky - Polytopal realizations of
+      generalized associahedra, arXiv:0202004.
     """
-    def __init__(self, cartan_type):
-        """
-        TESTS::
+    cartan_type = CartanType( cartan_type )
+    parent = Associahedra(QQ, cartan_type.rank())
+    return parent(cartan_type)
 
-            sage: Asso = Associahedron(['A',2]); Asso
-            Generalized associahedron of type ['A', 2] with 5 vertices
-            sage: TestSuite(Asso).run()
 
-        """
-        self._cartan_type = CartanType( cartan_type )
-        assert self._cartan_type.is_finite()
-        root_space = self._cartan_type.root_system().root_space()
-        # TODO: generalize this as a method of root lattice realization
-        rhocheck = sum( beta.associated_coroot() for beta in root_space.positive_roots() )/2
-        I = root_space.index_set()
-        inequalities = []
-        for orbit in root_space.almost_positive_roots_decomposition():
-            c = rhocheck.coefficient(orbit[0].leading_support())
-            for beta in orbit:
-                inequalities.append( [c] + [ beta.coefficient(i) for i in I ] )
-        Polyhedron_QQ_ppl.__init__(self, len(I), None, [inequalities,[]])
-        # check that there are non non trivial facets
-        assert self.n_facets() == len(inequalities)
+
+
+
+class Associahedron_class(Polyhedron_QQ_ppl):
+    r"""
+    The Python class of an associahedron
+
+    You should use the :func:`Associahedron` convenience function to
+    construct associahedra from the cartan type.
+
+    TESTS::
+
+        sage: Asso = Associahedron(['A',2]); Asso
+        Generalized associahedron of type ['A', 2] with 5 vertices
+        sage: TestSuite(Asso).run()
+    """
 
     def _repr_(self):
         r"""
@@ -107,11 +136,12 @@ class Associahedron(Polyhedron_QQ_ppl):
             sage: Associahedron(['A',3])._repr_()
             "Generalized associahedron of type ['A', 3] with 14 vertices"
         """
-        return 'Generalized associahedron of type %s with %s vertices'%(self._cartan_type,self.n_vertices())
+        return 'Generalized associahedron of type %s with %s vertices'\
+            %(self._cartan_type,self.n_vertices())
 
     def cartan_type(self):
         r"""
-        Returns the Cartan type of self.
+        Returns the Cartan type
 
         EXAMPLES::
 
@@ -128,10 +158,76 @@ class Associahedron(Polyhedron_QQ_ppl):
 
             sage: Asso = Associahedron(['A',2])
             sage: Asso.vertices()
-            [[1, -1], [1, 1], [-1, 1], [-1, 0], [0, -1]]
+            (A vertex at (1, -1), A vertex at (1, 1),
+             A vertex at (-1, 1), A vertex at (-1, 0),
+             A vertex at (0, -1))
 
             sage: Asso.vertices_in_root_space()
             (alpha[1] - alpha[2], alpha[1] + alpha[2], -alpha[1] + alpha[2], -alpha[1], -alpha[2])
         """
         root_space = self._cartan_type.root_system().root_space()
         return tuple( root_space.from_vector(vector(V)) for V in self.vertex_generator() )
+
+
+
+
+
+
+class Associahedra(Polyhedra_QQ_ppl):
+    """
+    Parent of Associahedra of specified dimension
+
+    EXAMPLES::
+
+        sage: from sage.combinat.root_system.associahedron import Associahedra
+        sage: parent = Associahedra(QQ,2);  parent
+        Polyhedra in QQ^2
+        sage: type(parent)
+        <class 'sage.combinat.root_system.associahedron.Associahedra_with_category'>
+        sage: parent(['A',2])
+        Generalized associahedron of type ['A', 2] with 5 vertices
+
+    Importantly, the parent knows the dimension of the ambient
+    space. If you try to construct an associahedron of a different
+    dimension, a ``ValueError`` is raised::
+
+        sage: parent(['A',3])
+        Traceback (most recent call last):
+        ...
+        ValueError: V-representation data requires a list of length ambient_dim
+    """
+    Element = Associahedron_class
+
+    def _element_constructor_(self, cartan_type, **kwds):
+        """
+        The element constructor.
+
+        This method is called internally when we try to convert
+        something into an element. In this case, the only thing that
+        can be converted into an associahedron is the Cartan type.
+
+        EXAMPLES::
+
+            sage: from sage.combinat.root_system.associahedron import Associahedra
+            sage: parent = Associahedra(QQ,2)
+            sage: parent(['A',2])
+            Generalized associahedron of type ['A', 2] with 5 vertices
+            sage: parent._element_constructor_(['A',2])
+            Generalized associahedron of type ['A', 2] with 5 vertices
+        """
+        cartan_type = CartanType( cartan_type )
+        assert cartan_type.is_finite()
+        root_space = cartan_type.root_system().root_space()
+        # TODO: generalize this as a method of root lattice realization
+        rhocheck = sum( beta.associated_coroot() for beta in root_space.positive_roots() )/2
+        I = root_space.index_set()
+        inequalities = []
+        for orbit in root_space.almost_positive_roots_decomposition():
+            c = rhocheck.coefficient(orbit[0].leading_support())
+            for beta in orbit:
+                inequalities.append( [c] + [ beta.coefficient(i) for i in I ] )
+        associahedron = super(Associahedra, self)._element_constructor_(None, [inequalities,[]])
+        associahedron._cartan_type = cartan_type
+        return associahedron
+
+

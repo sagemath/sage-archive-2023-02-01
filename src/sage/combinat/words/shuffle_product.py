8ec5028e5146ad09dae8ae8016a1e8129c3b6f42
Frederic Chapoton <chapoton at math.univ-lyon1.fr>
2013-03-03 12:18:35 +0100

    Trac #14121: add a test

--- src/sage/combinat/words/shuffle_product.py
+++ src/sage/combinat/words/shuffle_product.py
@@ -62,6 +62,13 @@ class ShuffleProduct_w1w2(CombinatorialClass):
             True
             sage: w in S
             False
+
+        We check that :trac:`14121` is solved::
+
+            sage: w = W('ab')
+            sage: x = W('ac')
+            sage: x*w in w.shuffle(x)
+            True
         """
         from sage.combinat.words.word import Word
         if not isinstance(x, Word_class):

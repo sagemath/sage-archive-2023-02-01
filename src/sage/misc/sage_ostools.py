b1806a8fce8467ea6a1a6a244ff542681ae64210
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2013-03-11 13:53:22 +0100

    Trac #14253: Use os.access() instead of "command" shell builtin

--- src/sage/misc/sage_ostools.py
+++ src/sage/misc/sage_ostools.py
@@ -4,23 +4,18 @@ Miscellaneous operating system functions
 
 def have_program(program, path=None):
     """
-    Return ``True`` if ``program`` is found in the path.
+    Return ``True`` if a ``program`` executable is found in the path
+    given by ``path``.
 
     INPUT:
 
     - ``program`` - a string, the name of the program to check.
-    - ``path`` - string or None.  If a nonempty string, use this as
-      the setting for the PATH environment variable.
+
+    - ``path`` - string or None. Paths to search for ``program``,
+      separated by ``os.pathsep``. If ``None``, use the :envvar:`PATH`
+      environment variable.
 
     OUTPUT: bool
-
-    This uses the shell command ``command -v``, which is Posix
-    standard and seems to be present and behave consistently on
-    various platforms (including Linux, BSD, Mac, Solaris, Cygwin).
-    This is in contrast to ``which``, which does not use seem to use
-    return codes consistently: if ``program`` does not exist, then on
-    some platforms, ``which program`` has a return code of 0, on some
-    a return code of 1.
 
     EXAMPLES::
 
@@ -29,19 +24,18 @@ def have_program(program, path=None):
         True
         sage: have_program('there_is_not_a_program_with_this_name')
         False
+        sage: have_program('sage', path=SAGE_ROOT)
+        True
+        sage: have_program('ls', path=SAGE_ROOT)
+        False
     """
-    from subprocess import call, PIPE
     import os
-    try:
-        if path:
-            # env is a copy of the current environment, so modifying
-            # it won't affect os.environ.
-            env = os.environ.copy()
-            env['PATH'] = path
-            return not call('command -v %s' % program, shell=True,
-                            stdout=PIPE, env=env)
-        else:
-            return not call('command -v %s' % program, shell=True,
-                            stdout=PIPE)
-    except OSError:
-        return False
+    if path is None:
+        path = os.environ.get('PATH', "")
+    for p in path.split(os.pathsep):
+        try:
+            if os.access(os.path.join(p, program), os.X_OK):
+                return True
+        except OSError:
+            pass
+    return False

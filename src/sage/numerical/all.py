e4aaa9ffffd7b58ee87ef78ebb51b20cc08b3d06
Andrey Novoseltsev <novoselt@gmail.com>
2013-03-17 05:05:58 +0000

    Bind interactive simplex method module into documentation and global namespace.

--- src/sage/numerical/all.py
+++ src/sage/numerical/all.py
@@ -9,3 +9,7 @@ from optimize import (find_fit,
                       minimize_constrained)
 from sage.numerical.mip import MixedIntegerLinearProgram
 from sage.numerical.backends.generic_backend import default_mip_solver
+
+from sage.misc.lazy_import import lazy_import
+lazy_import("sage.numerical.interactive_simplex_method",
+            ["LPProblem", "LPProblemStandardForm"])

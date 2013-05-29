4a7cea2fbd4085bd134f0563257879f7f4e3e7a7
Nathann Cohen <nathann.cohen@gmail.com>
2013-05-29 14:09:19 +0200

    Trac #14525: cliquer does not like the empty graph -- reviewer's patch

--- src/sage/graphs/cliquer.pyx
+++ src/sage/graphs/cliquer.pyx
@@ -111,7 +111,7 @@ def all_max_clique(graph):
 
     EXAMPLES::
 
-        sage: graphs.ChvatalGraph().cliques_maximum()
+        sage: graphs.ChvatalGraph().cliques_maximum() # indirect doctest
         [[0, 1], [0, 4], [0, 6], [0, 9], [1, 2], [1, 5], [1, 7], [2, 3],
          [2, 6], [2, 8], [3, 4], [3, 7], [3, 9], [4, 5], [4, 8], [5, 10],
          [5, 11], [6, 10], [6, 11], [7, 8], [7, 11], [8, 10], [9, 10], [9, 11]]

c45f42f4dbdcbec6b8a4295a8cab60149bd46c2a
Julian Rueth <julian.rueth@gmail.com>
2013-07-10 10:26:48 +0200

    fixed modulus and capped absolute elements

---
+++ src/sage/rings/padics/qadic_flint_CA.pyx
@@ -0,0 +1,5 @@
+include "sage/libs/linkages/padics/fmpz_poly_unram.pxi"
+include "CA_template.pxi"
+
+cdef class qAdicCappedAbsoluteElement(CAElement):
+    pass
---
+++ src/sage/rings/padics/qadic_flint_FM.pyx
@@ -0,0 +1,5 @@
+include "sage/libs/linkages/padics/fmpz_poly_unram.pxi"
+include "FM_template.pxi"
+
+cdef class qAdicFixedModulusElement(FMElement):
+    pass

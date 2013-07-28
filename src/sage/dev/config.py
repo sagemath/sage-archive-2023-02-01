c79f3c69fa8b744571b6468a715465d9655c95b0
Julian Rueth <julian.rueth@fsfe.org>
2013-07-28 23:28:57 +0200

    docstring cleanup

--- src/sage/dev/config.py
+++ src/sage/dev/config.py
@@ -1,3 +1,23 @@
+r"""
+Configuration wrapper
+
+A wrapper around the ``devrc`` file in ``DOT_SAGE`` directory which stores the
+configuration for :class:`SageDev`.
+
+AUTHORS:
+
+- TODO: add authors from github's history and trac's history
+
+"""
+#*****************************************************************************
+#       Copyright (C) 2013 TODO
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
 from sage.env import DOT_SAGE
 
 class Config(collections.MutableMapping):
@@ -7,16 +27,19 @@ class Config(collections.MutableMapping):
 
     INPUT:
 
-    - ``devrc`` -- a string (default: the absolute path of the ``devrc`` file in ``DOT_SAGE``)
+    - ``devrc`` -- a string (default: the absolute path of the ``devrc`` file
+    in ``DOT_SAGE``)
 
     EXAMPLES::
 
+        sage: from sage.dev.sagedev import Config
+        sage: Config() # random output, the output depends on the user's config
         sage: dev._config
         Config('''
         [trac]
         username = doctest
-        password_timeout = .5
         ''')
+
     """
     def __init__(self, devrc = os.path.join(DOT_SAGE, 'devrc')):
         r"""
@@ -26,6 +49,7 @@ class Config(collections.MutableMapping):
 
             sage: from sage.dev.sagedev import Config
             sage: type(Config())
+
         """
         self._config = configparser.ConfigParser()
         self._devrc = devrc
@@ -40,6 +64,7 @@ class Config(collections.MutableMapping):
 
             sage: repr(dev._config)
             "Config('''\n[trac]\nusername = doctest\npassword_timeout = .5\n''')"
+
         """
         return "Config('''\n"+"\n".join([ "[%s]\n"%s+"\n".join(["%s = %s"%(o,self[s][o]) for o in self[s] ]) for s in self ])+"\n''')"
 
@@ -49,17 +74,21 @@ class Config(collections.MutableMapping):
 
         EXAMPLES::
 
-            sage: from sage.dev.sagedev import Config, doctest_config
-            sage: c = doctest_config()
+            sage: from sage.dev.sagedev.test.config import Config
+            sage: c = Config()
+            sage: c["trac"]["username"] = "foo"
+
+            sage: from sage.dev.sagedev.config import Config
+            sage: c2 = Config(c._devrc)
+            sage: c2
             sage: c._write_config()
-            sage: c = Config(c._devrc)
-            sage: c._read_config()
-            sage: c
+            sage: c2._read_config()
+            sage: c2
             Config('''
             [trac]
-            username = doctest
-            password_timeout = .5
+            username = foo
             ''')
+
         """
         if os.path.exists(self._devrc):
             self._config.read(self._devrc)
@@ -70,14 +99,15 @@ class Config(collections.MutableMapping):
 
         EXAMPLES::
 
-            sage: from sage.dev.sagedev import doctest_config
-            sage: c = doctest_config()
+            sage: from sage.dev.sagedev.test.config import Config
+            sage: c = Config()
             sage: os.unlink(c._devrc)
             sage: os.path.exists(c._devrc)
             False
             sage: c._write_config()
             sage: os.path.exists(c._devrc)
             True
+
         """
         with open(self._devrc, 'w') as F:
             self._config.write(F)
@@ -91,56 +121,175 @@ class Config(collections.MutableMapping):
 
         EXAMPLES::
 
-            sage: dev._config['trac']
+            sage: from sage.dev.sagedev.test.config import Config
+            sage: c = Config()
+            sage: c['trac']
             IndexableForSection('''
             username = doctest
-            password_timeout = .5
             ''')
-            sage: dev._config['tig']
+            sage: c['tig']
             Traceback (most recent call last):
             ...
             KeyError: 'tig'
+
         """
         if not section in self:
             raise KeyError(section)
 
         class IndexableForSection(collections.MutableMapping):
+            r"""
+            A section of a :class:`Config`.
+
+            TESTS::
+
+                sage: from sage.dev.sagedev.test.config import Config
+                sage: c = Config()
+                sage: c["trac"]
+
+            """
             def __init__(this, section):
+                r"""
+                Initialization.
+
+                TESTS::
+
+                    sage: from sage.dev.sagedev.test.config import Config
+                    sage: c = Config()
+                    sage: type(c['trac'])
+
+                """
                 this._section = section
+
             def __repr__(this):
+                r"""
+                Return a printable representation of this section.
+
+                TESTS::
+
+                    sage: from sage.dev.sagedev.test.config import Config
+                    sage: c = Config()
+                    sage: c["trac"]
+
+                """
                 return "IndexableForSection('''\n"+"\n".join(["%s = %s"%(o,this[o]) for o in this])+"\n''')"
+
             def __getitem__(this, option):
+                r"""
+                Return the value for ``option`` or raise a ``KeyError`` if
+                this option is not set.
+
+                TESTS::
+
+                    sage: from sage.dev.sagedev.test.config import Config
+                    sage: c = Config()
+                    sage: c["trac"]["username"]
+                    doctest
+                    sage: c["trac"]["nousername"]
+                    Traceback (most recent call last):
+                    ...
+                    KeyError: nousername
+
+                """
                 try:
                     return self._config.get(this._section, option)
                 except configparser.NoOptionError:
                     raise KeyError(option)
+
             def __iter__(this):
+                r"""
+                Return an iterator over the options in this section.
+
+                TESTS::
+
+                    sage: from sage.dev.sagedev.test.config import Config
+                    sage: c = Config()
+                    sage: list(c["trac"])
+
+                """
                 return iter(self._config.options(this._section))
+
             def __setitem__(this, option, value):
+                r"""
+                Set ``option`` to ``value``.
+
+                TESTS::
+
+                    sage: from sage.dev.sagedev.test.config import Config
+                    sage: c = Config()
+                    sage: c["trac"]["username"] = "foo"
+                    sage: c["trac"]["username"]
+                    foo
+
+                """
                 self._config.set(this._section, option, value)
                 self._write_config()
+
             def getboolean(this, option):
+                r"""
+                TODO
+                """
                 return self._config.getboolean(this._section, option)
+
             def __delitem__(this, option):
+                r"""
+                Remove ``option`` from this section.
+
+                TESTS::
+
+                    sage: from sage.dev.sagedev.test.config import Config
+                    sage: c = Config()
+                    sage: del(c["trac"]["username"])
+                    sage: c["trac"]["username"]
+                    Traceback (most recent call last):
+                    ...
+                    KeyError: username
+
+                """
                 self._config.remove_option(this._section, option)
                 self._write_config()
+
             def __len__(this):
+                r"""
+                Return the number of options in this section.
+
+                TESTS::
+
+                    sage: from sage.dev.sagedev.test.config import Config
+                    sage: c = Config()
+                    sage: len(c["trac"])
+
+                """
                 return len(self._config.options(this._section))
+
             def __contains__(this, option):
+                r"""
+                Return whether this section contains ``options``.
+
+                TESTS::
+
+                    sage: from sage.dev.sagedev.test.config import Config
+                    sage: c = Config()
+                    sage: "username" in c["trac"]
+                    True
+
+                """
                 return option in self._config.options(this._section)
 
         return IndexableForSection(section)
 
     def __contains__(self, section):
         r"""
-        returns true if section is in the configuration
+        Returns ``True`` if ``section`` is in the configuration.
 
         EXAMPLES::
 
-            sage: 'trac' in dev._config
+            sage: from sage.dev.sagedev.test.config import Config
+            sage: c = Config()
+            sage: 'trac' in c
             True
-            sage: 'nottrac' in dev._config
+            sage: 'nottrac' in c
             False
+
         """
         return section in self._config.sections()
 
@@ -150,22 +299,26 @@ class Config(collections.MutableMapping):
 
         EXAMPLES::
 
-            sage: list(dev._config)
-            ['trac']
+            sage: from sage.dev.sagedev.test.config import Config
+            sage: c = Config()
+            sage: list(c)
+            ['git', 'trac']
+
         """
         return iter(self._config.sections())
 
     def __setitem__(self, section, dictionary):
         r"""
-        Set ``section`` to ``dictionary``.
+        Copy the options from ``dictionary`` to ``section``.
 
         EXAMPLES::
 
-            sage: from sage.dev.sagedev import doctest_config
-            sage: c = doctest_config()
-            sage: c['foo'] = {'foo':'foo'}
-            sage: c['foo']['foo']
+            sage: from sage.dev.sagedev.test.config import Config
+            sage: c = Config()
+            sage: c["foo"] = {"foo":"foo"}
+            sage: c["foo"]["foo"]
             'foo'
+
         """
         if self._config.has_section(section):
             self.remove_section(section)
@@ -176,26 +329,30 @@ class Config(collections.MutableMapping):
 
     def __len__(self):
         r"""
-        get the number of sections in the configuration
+        Get the number of sections in the configuration.
 
         EXAMPLES::
 
-            sage: len(dev._config)
-            1
+            sage: from sage.dev.sagedev.test.config import Config
+            sage: c = Config()
+            sage: len(c)
+            2
+
         """
         return len(self._config.sections())
 
     def __delitem__(self, section):
         r"""
-        remove ``section`` from the configuration
+        Remove ``section`` from the configuration
 
         EXAMPLES::
 
-            sage: from sage.dev.sagedev import doctest_config
-            sage: c = doctest_config()
+            sage: from sage.dev.sagedev.test.config import Config
+            sage: c = Config()
             sage: del c['git']
             sage: list(c)
             ['trac']
+
         """
         self._config.remove_section(section)
         self._write_config()
--- src/sage/dev/user_interface.py
+++ src/sage/dev/user_interface.py
@@ -1,108 +1,141 @@
-"""
+r"""
 User Interface
 
-Abstract base class that is used for displaying messages and prompting for user
-input.
+This file provides an abstract base class that is used for displaying messages
+and prompting for user input.
+
+.. SEEALSO::
+
+    :class:`CmdLineInterface` for an implementation of this class
+
+AUTHORS:
+
+- TODO: add authors from github's history and trac's history
+
 """
+#*****************************************************************************
+#       Copyright (C) 2013 TODO
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
 
 class UserInterface(object):
+    r"""
+    An abstract base class for displaying messages and prompting the user for
+    input.
+
+    TESTS::
+
+        sage: from sage.dev.user_interface import UserInterface
+        sage: UI = UserInterface()
+        sage: type(UI)
+
+    """
     def select(self, prompt, options, default=None):
-        """
-        should ask user to select from list of options and return
-        selected option
+        r"""
+        Ask the user to select from list of options and return selected option.
 
         INPUT:
 
-        - ``prompt`` -- prompt to display
+        - ``prompt`` -- a string, prompt to display
 
-        - ``options`` -- iterable that contains list of options
+        - ``options`` -- iterable of strings, the options
 
-        - ``default`` -- default option
+        - ``default`` -- a string or ``None`` (default: ``None``), the default
+          option
 
         TESTS::
 
-            sage: UI = sage.dev.user_interface.UserInterface()
-            sage: s = UI.select("Should I delete your home directory?",
-            ....:         ("yes","no","maybe"), default=1)
+            sage: from sage.dev.user_interface import UserInterface
+            sage: UserInterface().select("Should I delete your home directory?", ("yes","no","maybe"), default=1)
             Traceback (most recent call last):
             ...
             NotImplementedError
+
         """
         raise NotImplementedError
 
     def confirm(self, question, default_no=False):
-        """
-        should ask yes/no question from the developer and return the
-        boolean of the response
+        r"""
+        Ask a yes/no question and return the response as a boolean.
 
         INPUT:
 
         - ``question`` -- a string
 
-        - ``default_no`` -- boolean whether to default to no
+        - ``default_no`` -- boolean (default: ``False``), whether to default to
+          no
 
         TESTS::
 
-            sage: UI = sage.dev.user_interface.UserInterface()
-            sage: UI.confirm("Should I delete your home directory?")
+            sage: from sage.dev.user_interface import UserInterface
+            sage: UserInterface().confirm("Should I delete your home directory?")
             Traceback (most recent call last):
             ...
             NotImplementedError
+
         """
         return self.select(question, ("yes", "no"), int(default_no)) == "yes"
 
     def get_input(self, prompt):
-        """
-        gets input from developer after displaying prompt
+        r"""
+        Read input after displaying ``prompt``.
 
         TESTS::
 
-            sage: UI = sage.dev.user_interface.UserInterface()
-            sage: s = UI.get_input("What do you want for dinner?")
+            sage: from sage.dev.user_interface import UserInterface
+            sage: UserInterface().get_input("What do you want for dinner?")
             Traceback (most recent call last):
             ...
             NotImplementedError
+
         """
         raise NotImplementedError
 
     def get_password(self, prompt):
-        """
-        gets password from developer after displaying prompt
+        r"""
+        Ask for a password after displaying ``prompt``.
 
         TESTS::
 
-            sage: UI = sage.dev.user_interface.UserInterface()
-            sage: p = UI.get_password("What is the passphrase for your safe?")
+            sage: from sage.dev.user_interface import UserInterface
+            sage: UserInterface().get_password("What is the passphrase for your safe?")
             Traceback (most recent call last):
             ...
             NotImplementedError
+
         """
         raise NotImplementedError
 
     def show(self, message):
-        """
-        should display message to user
+        r"""
+        Display ``message``.
 
         TESTS::
 
-            sage: UI = sage.dev.user_interface.UserInterface()
-            sage: UI.show("I ate fillet mignon for dinner.")
+            sage: from sage.dev.user_interface import UserInterface
+            sage: UserInterface().show("I ate filet mignon for dinner.")
             Traceback (most recent call last):
             ...
             NotImplementedError
+
         """
         raise NotImplementedError
 
     def edit(self, filename):
-        """
-        should drop user into editor with filename open
+        r"""
+        Drop user into an editor with ``filename`` open.
 
         TESTS::
 
-            sage: UI = sage.dev.user_interface.UserInterface()
-            sage: UI.edit("filename")
+            sage: from sage.dev.user_interface import UserInterface
+            sage: UserInterface().edit("filename")
             Traceback (most recent call last):
             ...
             NotImplementedError
+
         """
         raise NotImplementedError

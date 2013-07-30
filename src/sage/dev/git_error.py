4853940209bc511ddf75506f4e5a7ce3c5d7c085
Julian Rueth <julian.rueth@fsfe.org>
2013-07-30 15:57:54 +0200

    fixed GitError

--- src/sage/dev/git_error.py
+++ src/sage/dev/git_error.py
@@ -1,4 +1,4 @@
 class GitError(RuntimeError):
     def __init__(self, exit_code):
-        RuntimeError("git returned with non-zero exit code (%s)"%exit_code)
+        RuntimeError.__init__(self, "git returned with non-zero exit code (%s)"%exit_code)
 

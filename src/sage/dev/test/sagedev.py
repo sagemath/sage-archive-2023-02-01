ee62afe0dc00329595e14650418b798485c0754e
Julian Rueth <julian.rueth@fsfe.org>
2013-08-01 17:00:41 +0200

    added DoctestSageDev

---
+++ src/sage/dev/test/sagedev.py
@@ -0,0 +1,32 @@
+import sage.dev.sagedev
+
+class DoctestSageDev(sage.dev.sagedev.SageDev):
+    def __init__(self, config, trac_server):
+        from user_interface import DoctestUserInterface
+        UI = DoctestUserInterface(config['UI'])
+        from trac_interface import DoctestTracInterface
+        trac = DoctestTracInterface(config['trac'], UI, trac_server)
+        from sage.dev.git_interface import GitInterface
+        config['git']['repository'] = trac_server.git._config['src']
+        git = GitInterface(config['git'], UI)
+
+        self._trac_server = trac_server
+
+        sage.dev.sagedev.SageDev.__init__(self, config, UI, trac, git)
+
+    def _pull_master_branch(self):
+        from sage.dev.git_interface import SUPER_SILENT
+        import os
+        old_cwd = os.getcwd()
+        self._chdir()
+        try:
+            from sage.dev.sagedev import MASTER_BRANCH
+            self.git.fetch(SUPER_SILENT, self._trac_server.git._config['src'], "{0}:{0}".format(MASTER_BRANCH))
+            self.git.checkout(SUPER_SILENT, MASTER_BRANCH)
+        finally:
+            os.chdir(old_cwd)
+
+    def _chdir(self):
+        import os
+        os.chdir(self.config['git']['src'])
+

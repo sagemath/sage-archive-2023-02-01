de4258be32d3f9f81d6c4be59cca189ad43cb815
Konstantin Ziegler <zieglerk@bit.uni-bonn.de>
2013-08-20 12:52:21 +0200

    Replace 'four' by 'five' in documentation to match number of list items

--- src/sage/rings/polynomial/polynomial_ring_constructor.py
+++ src/sage/rings/polynomial/polynomial_ring_constructor.py
@@ -65,7 +65,7 @@ def PolynomialRing(base_ring, arg1=None, arg2=None,
     Return the globally unique univariate or multivariate polynomial
     ring with given properties and variable name or names.
 
-    There are four ways to call the polynomial ring constructor:
+    There are five ways to call the polynomial ring constructor:
 
     1. ``PolynomialRing(base_ring, name,    sparse=False)``
     2. ``PolynomialRing(base_ring, names,   order='degrevlex')``
@@ -731,4 +731,3 @@ def BooleanPolynomialRing_constructor(n=None, names=None, order="lex"):
 #########################################################################################
 # END (Factory function for making polynomial rings)
 #########################################################################################
-

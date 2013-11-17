db8cd7e36829d173baad8d27276e091265e6d998
Peter Bruin <P.Bruin@warwick.ac.uk>
2013-11-17 13:23:33 +0000

    top-level changes related to PariInstance

--- src/sage/libs/pari/all.py
+++ src/sage/libs/pari/all.py
@@ -1,13 +1,3 @@
-import gen as _gen
-
-pari = _gen.pari
-
-pari_gen = _gen.gen
-
+from gen import gen as pari_gen, PariError
+from pari_instance import pari
 allocatemem = pari.allocatemem
-
-PariError = _gen.PariError
-
-from gen import (prec_dec_to_words, prec_dec_to_bits,
-                 prec_bits_to_words, prec_bits_to_dec,
-                 prec_words_to_bits, prec_words_to_dec)

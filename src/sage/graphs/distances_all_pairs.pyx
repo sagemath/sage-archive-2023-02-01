1d5d52a76338cc9a11f2b9cbd56d5f5cced7e7f1
Nathann Cohen <nathann.cohen@gmail.com>
2013-12-20 12:51:43 +0100

    trac #15553: Broken links in the doc of graph/ and numerical/

--- src/sage/graphs/distances_all_pairs.pyx
+++ src/sage/graphs/distances_all_pairs.pyx
@@ -483,8 +483,8 @@ def is_distance_regular(G, parameters = False):
 
     .. SEEALSO::
 
-        * :meth:`Graph.is_regular`
-        * :meth:`Graph.is_strongly_regular`
+        * :meth:`~sage.graphs.generic_graph.GenericGraph.is_regular`
+        * :meth:`~Graph.is_strongly_regular`
 
     EXAMPLES::
 

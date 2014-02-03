769e4533706b305b24ad23a52080e4599ed8b2e5
Rudi Pendavingh <rudi.pendavingh@gmail.com>
2014-02-03 18:44:05 +0100

    Fixed SetSystem._isomorphism(S,T) where S is an empty isopmorphism.

--- src/sage/matroids/set_system.pyx
+++ src/sage/matroids/set_system.pyx
@@ -445,7 +445,10 @@ cdef class SetSystem:
         if E is None:
             E = xrange(self._len)
         if P is None:
-            P = SetSystem(self._groundset, [self._groundset], capacity=self._groundset_size)
+            if len(self._groundset)>0:
+                P = SetSystem(self._groundset, [self._groundset], capacity=self._groundset_size)
+            else:
+                P = SetSystem([], [])
         cnt = self._incidence_count(E)
         self._groundset_partition(P, cnt)
         return P
@@ -604,6 +607,9 @@ cdef class SetSystem:
             ....:                                      ['a', 'c', 'd']])
             sage: S._isomorphism(T)
             {1: 'c', 2: 'd', 3: 'b', 4: 'a'}
+            sage: S = SetSystem([], [])
+            sage: S._isomorphism(S)
+            {}
         """
         cdef long l, p
         if SP is None or OP is None:

f3b93bb5cae14d11f5ffde00f8ca203bd3383c24
Clemens Heuberger <clemens.heuberger@aau.at>
2014-02-21 19:08:51 +0200

    Allow non-hashable output labels in prepone_output

--- src/sage/combinat/finite_state_machine.py
+++ src/sage/combinat/finite_state_machine.py
@@ -4001,25 +4001,30 @@ class FiniteStateMachine(SageObject):
             sage: C.transitions()
             [Transition from 0 to 1: 0|-]
 
-        Currently, output labels have to be hashable::
+        Output labels do not have to be hashable::
 
             sage: C = Transducer([(0, 1, 0, []),
             ....:                 (1, 0, 0, [vector([0,0]), 0]),
             ....:                 (1, 1, 1, [vector([0,0]), 1]),
             ....:                 (0, 0, 1, 0)],
-            ....:                 determine_alphabets=False)
+            ....:                 determine_alphabets=False,
+            ....:                 initial_states=[0])
             sage: C.prepone_output()
-            Traceback (most recent call last):
-            ...
-            TypeError: mutable vectors are unhashable
-
+            sage: sorted(C.transitions())
+            [Transition from 0 to 1: 0|(0, 0),
+             Transition from 0 to 0: 1|0,
+             Transition from 1 to 0: 0|0,
+             Transition from 1 to 1: 1|1,(0, 0)]
         """
         def find_common_output(state):
             if len(filter(lambda transition: len(transition.word_out) == 0, self.transitions(state))) > 0:
                 return ()
-            first_letters = set(map(lambda transition: transition.word_out[0], self.transitions(state)))
-            if len(first_letters) == 1:
-                return (first_letters.pop(),)
+            first_letters = map(lambda transition: transition.word_out[0], self.transitions(state))
+            if first_letters == []:
+                return ()
+            first_item = first_letters.pop()
+            if all( [ item == first_item for item in first_letters ]):
+                return (first_item,)
             return ()
 
         changed = 1

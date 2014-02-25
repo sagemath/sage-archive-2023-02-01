a7fbaca952aad9c17a5df07475a1d1121ab55848
Julian Rueth <julian.rueth@fsfe.org>
2014-02-25 01:08:17 +0100

    replaced all occurences of GPLv2 with GPLv2+

--- src/sage/combinat/words/word_infinite_datatypes.py
+++ src/sage/combinat/words/word_infinite_datatypes.py
@@ -5,11 +5,10 @@ Datatypes for words defined by iterators and callables
 #       Copyright (C) 2009 Franco Saliola <saliola@gmail.com>
 #                          Vincent Delecroix <20100.delecroix@gmail.com>
 #
-#  Distributed under the terms of the GNU General Public License version 2
-#  (GPLv2)
-#
-#  The full text of the GPLv2 is available at:
-#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 from sage.combinat.words.word_datatypes import WordDatatype

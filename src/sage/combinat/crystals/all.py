fbe3f6c7bdfd1e2fdb67db8cd35018a00d21db7e
Travis Scrimshaw <tscrim at ucdavis.edu>
2014-03-09 12:17:58 -0700

    Added all remaining deprecations.

--- src/sage/combinat/crystals/all.py
+++ src/sage/combinat/crystals/all.py
@@ -1,179 +1,64 @@
 import catalog as crystals
 
-def CrystalOfLetters(cartan_type, element_print_style=None, dual=None):
-    """
-    TESTS::
+from sage.misc.lazy_import import lazy_import
 
-        sage: C = CrystalOfLetters(['A',5])
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.Letters instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from letters import CrystalOfLetters
-    deprecation(15882,'this being removed from the global namespace. Use crystals.Letters instead')
-    return CrystalOfLetters(cartan_type, element_print_style, dual)
+lazy_import('sage.combinat.crystals.letters',
+            'CrystalOfLetters',
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.Letters instead"))
 
-def FastCrystal(cartan_type, shape, format="string"):
-    """
-    TESTS::
+lazy_import('sage.combinat.crystals.fast_crystals',
+            'FastCrystal',
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.FastRankTwo instead"))
 
-        sage: C = FastCrystal(['A',2],shape=[4,1])
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.FastRankTwo instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from fast_crystals import FastCrystal
-    deprecation(15882,'this being removed from the global namespace. Use crystals.FastRankTwo instead')
-    return FastCrystal(cartan_type, shape, format)
+lazy_import('sage.combinat.crystals.highest_weight_crystals',
+            'HighestWeightCrystal',
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.HighestWeight instead"))
 
-def CrystalOfTableaux(cartan_type, shapes=None, shape=None):
-    """
-    TESTS::
+lazy_import('sage.combinat.crystals.kyoto_path_model',
+            'KyotoPathModel',
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.KyotoPathModel instead"))
 
-        sage: C = CrystalOfTableaux(['A',2],shape=[4,1])
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.Tableaux instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from tensor_product import CrystalOfTableaux
-    deprecation(15882,'this being removed from the global namespace. Use crystals.Tableaux instead')
-    return CrystalOfTableaux(cartan_type, shapes, shape)
+lazy_import('sage.combinat.crystals.direct_sum',
+            'DirectSumOfCrystals',
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.DirectSum instead"))
 
-def HighestWeightCrystal(dominant_weight, model=None):
-    """
-    TESTS::
+lazy_import('sage.combinat.crystals.tensor_product',
+            ['CrystalOfTableaux', 'TensorProductOfCrystals'],
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.<tab> instead"))
 
-        sage: C = CartanType(['E',6])
-        sage: La = C.root_system().weight_lattice().fundamental_weights()
-        sage: T = HighestWeightCrystal(La[1])
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.HighestWeight instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from highest_weight_crystals import HighestWeightCrystal
-    deprecation(15882,'this being removed from the global namespace. Use crystals.HighestWeight instead')
-    return HighestWeightCrystal(dominant_weight, model)
+lazy_import('sage.combinat.crystals.spins',
+            ['CrystalOfSpins', 'CrystalOfSpinsPlus', 'CrystalOfSpinsMinus'],
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.<tab> instead"))
 
-def CrystalOfSpins(ct):
-    """
-    TESTS::
+lazy_import('sage.combinat.crystals.affine',
+            ['AffineCrystalFromClassical', 'AffineCrystalFromClassicalAndPromotion'],
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.<tab> instead"))
 
-        sage: C = CrystalOfSpins(['B',3])
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.Spins instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from spins import CrystalOfSpins
-    deprecation(15882,'this being removed from the global namespace. Use crystals.Spins instead')
-    return CrystalOfSpins(ct)
+lazy_import('sage.combinat.crystals.elementary_crystals',
+            ['TCrystal', 'RCrystal', 'ElementaryCrystal', 'ComponentCrystal'],
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.elementary.<tab> instead"))
 
-def CrystalOfSpinsPlus(ct):
-    """
-    TESTS::
+lazy_import('sage.combinat.crystals.kirillov_reshetikhin',
+            'KirillovReshetikhinCrystal',
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.kirillov_resetikhin.KashiwaraNakashimaTabelaux instead"))
 
-        sage: C = CrystalOfSpinsPlus(['D',4])
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.SpinsPlus instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from spins import CrystalOfSpinsPlus
-    deprecation(15882,'this being removed from the global namespace. Use crystals.SpinsPlus instead')
-    return CrystalOfSpinsPlus(ct)
+lazy_import('sage.combinat.crystals.alcove_path',
+            ['CrystalOfAlcovePaths', 'ClassicalCrystalOfAlcovePaths'],
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.AlcovePath instead"))
 
-def CrystalOfSpinsMinus(ct):
-    """
-    TESTS::
+lazy_import('sage.combinat.crystals.littelmann_path',
+            ['CrystalOfLSPaths', 'CrystalOfProjectedLevelZeroLSPaths'],
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.<tab> instead"))
 
-        sage: C = CrystalOfSpinsMinus(['D',4])
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.SpinsMinus instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from spins import CrystalOfSpinsMinus
-    deprecation(15882,'this being removed from the global namespace. Use crystals.SpinsMinus instead')
-    return CrystalOfSpinsMinus(ct)
+lazy_import('sage.combinat.crystals.generalized_young_walls',
+            ['InfinityCrystalOfGeneralizedYoungWalls', 'CrystalOfGeneralizedYoungWalls'],
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.<tab> instead"))
 
-def KyotoPathModel(crystals, weight):
-    """
-    TESTS::
+lazy_import('sage.combinat.crystalsmonomial_crystals',
+            ['InfinityCrystalOfNakajimaMonomials', 'CrystalOfNakajimaMonomials'],
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.<tab> instead"))
 
-        sage: B = crystals.kirillov_reshetikhin.KashiwaraNakashimaTableaux(['A',2,1], 1,1)
-        sage: L = RootSystem(['A',2,1]).weight_space()
-        sage: C = KyotoPathModel(B, L.fundamental_weight(0))
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.KyotoPathModel instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from kyoto_path_model import KyotoPathModel
-    deprecation(15882,'this being removed from the global namespace. Use crystals.KyotoPathModel instead')
-    return KyotoPathModel(crystals, weight)
-
-def AffineCrystalFromClassical(cartan_type, *args, **options):
-    """
-    TESTS::
-
-        sage: B = crystals.Tableaux(['A',2],shape=[1])
-        sage: C = AffineCrystalFromClassical(['A',2,1], B)
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.AffineFromClassical instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from affine import AffineCrystalFromClassical
-    deprecation(15882,'this being removed from the global namespace. Use crystals.AffineFromClassical instead')
-    return AffineCrystalFromClassical(cartan_type, *args, **options)
-
-def AffineCrystalFromClassicalAndPromotion(cartan_type, *args, **options):
-    """
-    TESTS::
-
-        sage: n = 2
-        sage: B = crystals.Tableaux(['A',n],shape=[1])
-        sage: pr = attrcall("promotion")
-        sage: pr_inverse = attrcall("promotion_inverse")
-        sage: C = AffineCrystalFromClassicalAndPromotion(['A',n,1],B,pr,pr_inverse,1)
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.AffineFromClassicalAndPromotion instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from affine import AffineCrystalFromClassicalAndPromotion
-    deprecation(15882,'this being removed from the global namespace. Use crystals.AffineFromClassicalAndPromotion instead')
-    return AffineCrystalFromClassicalAndPromotion(cartan_type, *args, **options)
-
-def DirectSumOfCrystals(crystals, **options):
-    """
-    TESTS::
-
-        sage: C = crystals.Letters(['A',2])
-        sage: C2 = DirectSumOfCrystals([C,C])
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.DirectSum instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from direct_sum import DirectSumOfCrystals
-    deprecation(15882,'this being removed from the global namespace. Use crystals.DirectSum instead')
-    return DirectSumOfCrystals(crystals, **options)
-
-def TensorProductOfCrystals(*crystals, **options):
-    """
-    TESTS::
-
-        sage: C = crystals.Letters(['A',2])
-        sage: C2 = TensorProductOfCrystals(C,C)
-        doctest:...: DeprecationWarning: this being removed from the global namespace. Use crystals.TensorProduct instead
-        See http://trac.sagemath.org/15882 for details.
-    """
-    from sage.misc.superseded import deprecation
-    from tensor_product import TensorProductOfCrystals
-    deprecation(15882,'this being removed from the global namespace. Use crystals.TensorProduct instead')
-    return TensorProductOfCrystals(*crystals, **options)
-
-
-from kirillov_reshetikhin import KirillovReshetikhinCrystal
-from alcove_path import CrystalOfAlcovePaths
-from alcove_path import ClassicalCrystalOfAlcovePaths
-from littelmann_path import CrystalOfLSPaths, CrystalOfProjectedLevelZeroLSPaths
-from generalized_young_walls import InfinityCrystalOfGeneralizedYoungWalls, CrystalOfGeneralizedYoungWalls
-from infinity_crystals import InfinityCrystalOfTableaux
-from elementary_crystals import TCrystal, RCrystal, ElementaryCrystal, ComponentCrystal
-from monomial_crystals import InfinityCrystalOfNakajimaMonomials, CrystalOfNakajimaMonomials
+lazy_import('sage.combinat.crystals.infinity_crystals',
+            'InfinityCrystalOfTableaux',
+            deprecation=(15882, "this is being removed from the global namespace. Use crystals.infinity.Tableaux instead"))
 

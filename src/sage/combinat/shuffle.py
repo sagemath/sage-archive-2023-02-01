66c0f3f2afe75a4867885c874c26f72220b4c63a
Jean-Baptiste Priez <jbp@kerios.fr>
2014-03-27 22:37:22 +0100

    trac 15595: Shuffle more efficient + some documentation + doctest

--- src/sage/combinat/shuffle.py
+++ src/sage/combinat/shuffle.py
@@ -17,6 +17,21 @@ then the product is equals to the other.
 
 The shuffle product has been introduced by S. Eilenberg & S. Mac Lane in 1953 _[EilLan53].
 
+EXAMPLE::
+
+    sage: from sage.combinat.shuffle import ShuffleProduct
+    sage: list(ShuffleProduct([1,2], ["a", "b", "c"]))
+    [[1, 2, 'a', 'b', 'c'],
+     ['a', 1, 2, 'b', 'c'],
+     [1, 'a', 2, 'b', 'c'],
+     ['a', 'b', 1, 2, 'c'],
+     ['a', 1, 'b', 2, 'c'],
+     [1, 'a', 'b', 2, 'c'],
+     ['a', 'b', 'c', 1, 2],
+     ['a', 'b', 1, 'c', 2],
+     ['a', 1, 'b', 'c', 2],
+     [1, 'a', 'b', 'c', 2]]
+
 References:
 -----------
 
@@ -31,7 +46,7 @@ Author:
  - Jean-Baptiste Priez
 """
 #*****************************************************************************
-#       Copyright (C) 2012 Jean-Baptiste Priez <jbp@kerios.fr>
+#       Copyright (C) 2014 Jean-Baptiste Priez <jbp@kerios.fr>
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
 #
@@ -39,43 +54,10 @@ Author:
 #
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
-from sage.graphs.digraph import DiGraph
-from sage.graphs.linearextensions import LinearExtensions
 from sage.misc.misc import forall
-from sage.misc.cachefunc import cached_function
 from sage.rings.arith import binomial
 from sage.structure.sage_object import SageObject
 import collections, itertools
-
-@cached_function
-def Sh(n, m):
-    """
-    TESTS::
-
-        sage: from sage.combinat.shuffle import Sh
-        sage: list(Sh(3,2))
-        [[1, 2, 3, 4, 5],
-         [1, 2, 4, 3, 5],
-         [1, 2, 4, 5, 3],
-         [1, 4, 2, 3, 5],
-         [1, 4, 2, 5, 3],
-         [1, 4, 5, 2, 3],
-         [4, 1, 2, 3, 5],
-         [4, 1, 2, 5, 3],
-         [4, 1, 5, 2, 3],
-         [4, 5, 1, 2, 3]]
-    """
-    # [1,...,n].shifted_shuffle([1,...,m])
-    # <=> [1,...,n,n+1,...,n+m].right_permutohedron_interval(
-    #     [n+1,...,n+m,1,...,n])
-    # <=>
-    d = DiGraph()
-    d.add_vertices(xrange(1, n + m + 1))
-    d.add_edges(itertools.chain(
-            itertools.combinations(xrange(1, n + 1), 2),
-            itertools.combinations(xrange(n + 1, n + m + 1), 2)
-    ))
-    return LinearExtensions(d)
 
 class SetShuffleProduct(SageObject):
     """
@@ -86,6 +68,25 @@ class SetShuffleProduct(SageObject):
         sage: from sage.combinat.shuffle import SetShuffleProduct
         sage: TestSuite(SetShuffleProduct).run()
 
+    EXAMPLE::
+
+        sage: from sage.combinat.shuffle import SetShuffleProduct
+        sage: list(SetShuffleProduct({(1,), (2,3)}, {(4,5), (6,)}))
+        [[2, 3, 4, 5],
+         [4, 2, 3, 5],
+         [2, 4, 3, 5],
+         [4, 5, 2, 3],
+         [4, 2, 5, 3],
+         [2, 4, 5, 3],
+         [2, 3, 6],
+         [6, 2, 3],
+         [2, 6, 3],
+         [1, 4, 5],
+         [4, 1, 5],
+         [4, 5, 1],
+         [1, 6],
+         [6, 1]]
+
     """
 
     def __init__(self, l1, l2, elem_constructor=None):
@@ -94,15 +95,10 @@ class SetShuffleProduct(SageObject):
 
             sage: from sage.combinat.shuffle import SetShuffleProduct
             sage: SetShuffleProduct({(1,2,3), (2,3,4)}, {(5,)})
+            Shuffle set product of : [(2, 3, 4), (1, 2, 3)] and [(5,)]
             sage: list(SetShuffleProduct({(1,2,3), (2,3,4)}, {(5,)}))
-            [[2, 3, 4, 5],
-             [2, 3, 5, 4],
-             [2, 5, 3, 4],
-             [5, 2, 3, 4],
-             [1, 2, 3, 5],
-             [1, 2, 5, 3],
-             [1, 5, 2, 3],
-             [5, 1, 2, 3]]
+            [[2, 3, 4, 5], [2, 5, 3, 4], [5, 2, 3, 4], [2, 3, 5, 4], [1, 2, 3, 5], [1, 5, 2, 3],
+             [5, 1, 2, 3], [1, 2, 5, 3]]
 
         """
         assert(isinstance(l1, collections.Iterable) and
@@ -119,9 +115,9 @@ class SetShuffleProduct(SageObject):
             try:
                 e = iter(l1).next()
                 if hasattr(e, "parent") and hasattr(e.parent(), "_element_constructor"):
-                    self._set_constructor = e.parent()._element_constructor_
+                    self._element_constructor = e.parent()._element_constructor_
                 else:
-                    self._set_constructor = list
+                    self._element_constructor = list
             except StopIteration:
                 self._element_constructor = list
 
@@ -129,6 +125,7 @@ class SetShuffleProduct(SageObject):
         """
         TESTS::
 
+            sage: from sage.combinat.shuffle import SetShuffleProduct
             sage: SetShuffleProduct([[1,2],[3,4]], [[1,4]])
             Shuffle set product of : [[1, 2], [3, 4]] and [[1, 4]]
 
@@ -140,9 +137,10 @@ class SetShuffleProduct(SageObject):
         """
         TESTS::
 
-            sage: SetShuffleProduct([[BinaryTree()], [BinaryTree([]), BinaryTree([[],[]])]],
-            ....: [[1,4]])
-            Shuffle set product of:
+            sage: from sage.combinat.shuffle import SetShuffleProduct
+            sage: ascii_art(SetShuffleProduct([[BinaryTree()], [BinaryTree([]), BinaryTree([[],[]])]],
+            ....: [[1,4]]))
+            Set shuffle product of:
             [       [          ] ]
             [       [ o,   o   ] ]
             [       [     / \  ] ]     [ [      ] ]
@@ -162,7 +160,7 @@ class SetShuffleProduct(SageObject):
             sage: list(SetShuffleProduct([[],[]], [[]]))
             [[], []]
             sage: list(SetShuffleProduct([[1,2],[3]], [[4]]))
-            [[1, 2, 4], [1, 4, 2], [4, 1, 2], [3, 4], [4, 3]]
+            [[1, 2, 4], [4, 1, 2], [1, 4, 2], [3, 4], [4, 3]]
             sage: list(SetShuffleProduct([[1,2],[3,4]], [[1,4]], elem_constructor=set))
             [set([1, 2, 4]),
              set([1, 2, 4]),
@@ -186,8 +184,12 @@ class SetShuffleProduct(SageObject):
 
     def cardinality(self):
         """
+        The cardinality is defined by the sum of the cardinality of all shuffles.
+        That means by a sum of binomials.
+
         TESTS::
 
+            sage: from sage.combinat.shuffle import SetShuffleProduct
             sage: SetShuffleProduct([[1,2],[3,4]], [[1,4]], elem_constructor=set).cardinality()
             12
         """
@@ -204,6 +206,21 @@ class ShuffleProduct(SageObject):
     """
     Shuffle product of two iterable.
 
+    EXAMPLES::
+
+        sage: from sage.combinat.shuffle import ShuffleProduct
+        sage: list(ShuffleProduct("abc", "de", elem_constructor="".join))
+        ['abcde',
+         'adbce',
+         'dabce',
+         'abdce',
+         'adebc',
+         'daebc',
+         'deabc',
+         'adbec',
+         'dabec',
+         'abdec']
+
     """
 
     def __init__(self, l1, l2, elem_constructor=None):
@@ -214,16 +231,8 @@ class ShuffleProduct(SageObject):
             sage: ShuffleProduct([1,2,3],[4,5])
             Shuffle product of : [1, 2, 3] and [4, 5]
             sage: list(ShuffleProduct(Word("aa"), Word("bbb"), Word))
-            [word: aabbb,
-             word: ababb,
-             word: abbab,
-             word: abbba,
-             word: baabb,
-             word: babab,
-             word: babba,
-             word: bbaab,
-             word: bbaba,
-             word: bbbaa]
+            [word: aabbb, word: baabb, word: ababb, word: bbaab, word: babab, word: abbab,
+             word: bbbaa, word: bbaba, word: babba, word: abbba]
 
         """
         assert(isinstance(l1, collections.Iterable) and
@@ -279,31 +288,27 @@ class ShuffleProduct(SageObject):
 
     def __iter__(self):
         """
+        Efficient iteration from a gray code on binary words in `B(n,k)`.
+
+        (with `B(n,k)` the number of binary words of sizez `n` with `k` one.
+
         TESTS::
 
             sage: from sage.combinat.shuffle import ShuffleProduct
             sage: list(ShuffleProduct([1,2,3],[4,5]))
-            [[1, 2, 3, 4, 5],
-             [1, 2, 4, 3, 5],
-             [1, 2, 4, 5, 3],
-             [1, 4, 2, 3, 5],
-             [1, 4, 2, 5, 3],
-             [1, 4, 5, 2, 3],
-             [4, 1, 2, 3, 5],
-             [4, 1, 2, 5, 3],
-             [4, 1, 5, 2, 3],
-             [4, 5, 1, 2, 3]]
+            [[1, 2, 3, 4, 5], [1, 4, 2, 3, 5], [4, 1, 2, 3, 5], [1, 2, 4, 3, 5], [1, 4, 5, 2, 3],
+             [4, 1, 5, 2, 3], [4, 5, 1, 2, 3], [1, 4, 2, 5, 3], [4, 1, 2, 5, 3], [1, 2, 4, 5, 3]]
             sage: B = BinaryTree
             sage: ascii_art(list(ShuffleProduct([B([]), B([[],[]])],
             ....:   [B([[[],[]],[[],None]])])))
-            [ [                       ]  [                       ]
-            [ [ o,   o        __o__   ]  [ o,     __o__      o   ]  [     __o__    o    o
-            [ [     / \      /     \  ]  [       /     \    / \  ]  [    /     \       / \
-            [ [    o   o,   o       o ]  [      o       o  o   o ]  [   o       o     o   o
-            [ [            / \     /  ]  [     / \     /         ]  [  / \     /
-            [ [           o   o   o   ], [    o   o   o  ,       ], [ o   o   o  ,  ,
+            [ [                       ]                             [
+            [ [ o,   o        __o__   ]  [     __o__    o    o   ]  [ o,     __o__      o
+            [ [     / \      /     \  ]  [    /     \       / \  ]  [       /     \    / \
+            [ [    o   o,   o       o ]  [   o       o     o   o ]  [      o       o  o   o
+            [ [            / \     /  ]  [  / \     /            ]  [     / \     /
+            [ [           o   o   o   ], [ o   o   o  ,  ,       ], [    o   o   o  ,
             <BLANKLINE>
-               ]
+             ] ]
              ] ]
              ] ]
              ] ]
@@ -311,11 +316,50 @@ class ShuffleProduct(SageObject):
              ] ]
 
         """
-        n1 = len(self._l1)
-        n2 = len(self._l2)
+
+        ############ Gray code #############
+        def gen(n, k):
+            if 0 < k and k < n:
+
+                for _ in gen(n-1, k): yield
+
+                if k == 1: swap(n, n-1)
+                else: swap(n, k-1)
+                yield
+
+                for _ in neg(n-1, k-1): yield
+
+        def neg(n, k):
+            if 0 < k and k < n:
+
+                for _ in gen(n-1, k-1): yield
+
+                if k == 1: swap(n, n-1)
+                else: swap(n, k-1)
+                yield
+
+                for _ in neg(n-1, k): yield
+
+        def swap(i,j):
+            l[i-1], l[j-1] = l[j-1], l[i-1]
+
+        ########################################
+
+
+        m = len(self._l1)
+        n = len(self._l2)
+        mn = m + n
+        l = [0] * m + [1] * n # [0, 0 ... m times, 1, 1, 1 ... n times]
+
         OC = self._element_constructor
-        for sig in Sh(n1, n2):
-            yield OC([self._l1[i-1] if i<= n1 else self._l2[i-n1-1] for i in sig])
+        yield OC(list(self._l1) + list(self._l2))
+
+        for _ in gen(mn, m):
+            l1 = iter(self._l1)
+            l2 = iter(self._l2)
+            d = {0: l1.next, 1: l2.next}
+            yield OC([d[l[k]]() for k in range(mn)])
+
 
 
     def __contains__(self, li):
@@ -335,6 +379,10 @@ class ShuffleProduct(SageObject):
             sage: [1,4,2,2,5,3] in sh
             False
         """
+        # FIXME: if the elem_constructor is a function??
+        ### for example: ShuffleProduct("a", "bc", elem_constructor="".join)
+        #if li.__class__ != self._element_constructor:
+        #    return False
         l1 = self._l1
         l2 = self._l2
         len_l1 = len(l1)
@@ -356,6 +404,9 @@ class ShuffleProduct(SageObject):
 
     def cardinality(self):
         """
+        The shuffle of `l_1` and `l_2` respectively of size `m` and `n`
+        is `\binom{m+n}{n}`.
+
         TESTS::
 
             sage: from sage.combinat.shuffle import ShuffleProduct
@@ -367,35 +418,3 @@ class ShuffleProduct(SageObject):
         ll1 = len(self._l1)
         ll2 = len(self._l2)
         return binomial(ll1 + ll2, ll1)
-
-
-#from sage.combinat.map_reduce import SearchForestMapReduce
-
-def parallel_shuffle(l1, l2):
-    """
-    FIXME:: use SearchForestParallelIterator !
-    """
-    def children(node):
-        (l1, l2, acc) = node
-        if len(l1) == 0:
-            if len(l2) == 0:
-                return
-            yield ([], [], acc + l2)
-        elif len(l2) == 0:
-            yield ([], [], acc + l1)
-        else:
-            yield (l1[1:], l2, acc + [l1[0]])
-            yield (l1, l2[1:], acc + [l2[0]])
-
-    def post_proc(node):
-        if len(node[0]) > 0 or len(node[1]) > 0:
-            return []
-        return [node[2]]
-
-    return None
-    """SearchForestMapReduce(
-            roots=[(l1, l2, [])],
-            children=children,
-            map_function=post_proc,
-            reduce_init=[]
-    ).run()"""

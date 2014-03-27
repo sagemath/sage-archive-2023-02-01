4a9b2e19851e68bae5106f1ae8e27876b107de69
Ralf Stephan <ralf@ark.in-berlin.de>
2014-03-27 18:27:02 +0100

    fix typo

--- src/sage/functions/exp_integral.py
+++ src/sage/functions/exp_integral.py
@@ -1339,7 +1339,7 @@ class Function_exp_integral(BuiltinFunction):
     input. Convert the input to a higher precision explicitly if a
     result with higher precision is desired::
         
-        sage: Ei(RealField(100)(1,1))
+        sage: Ei(RealField(300)(1.1))
         2.16737827956340282358378734233807621497112737591639704719499002090327541763352339357795426
         
     ALGORITHM: Uses mpmath.

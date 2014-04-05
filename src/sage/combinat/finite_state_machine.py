sagemath/sage-archive-2023-02-01

commit 2d43fdda50bd5bad3b079e7861fbb725471d1d04

FiniteStateMachine: add failing doctests: non-hashable colors
The following behaviour is undesired (docstrings says that
colors are tuples of the colors of the constituent states,
but this is not the case and leads to problems in
Automaton.determinisation):

    sage: A = Automaton([[0, 0, 0]], initial_states=[0])
    sage: B = A.product_FiniteStateMachine(A,
    ....:                                  lambda t1, t2: (0, None))
    sage: B.states()[0].color
    [None, None]
    sage: B.determinisation()
    Traceback (most recent call last):
    ...
    TypeError: unhashable type: 'list'

    sage: A = Automaton([[0, 0, 0]], initial_states=[0])
    sage: B = A.composition(A, algorithm='explorative')
    sage: B.states()[0].color
    [None, None]
    sage: B.determinisation()
    Traceback (most recent call last):
    ...
    TypeError: unhashable type: 'list'

Inserted doctests documenting this undesired behaviour.

src/sage/combinat/finite_state_machine.py
@@ -3625,6 +3625,21 @@ class FiniteStateMachine(SageObject):
             ....:                                  only_accessible_components=False)
             sage: H.states()
             [(0, 0), (1, 0), (0, 1), (1, 1)]
+
+        TESTS:
+
+        Check that colors are correctly dealt with. In particular, the
+        new colors have to be hashable such that
+        :meth:`Automaton.determinisation` does not fail::
+
+            sage: A = Automaton([[0, 0, 0]], initial_states=[0])
+            sage: B = A.product_FiniteStateMachine(A,
+            ....:                                  lambda t1, t2: (0, None))
+            sage: B.states()[0].color
+            (None, None)
+            sage: B.determinisation()
+            Automaton with 1 states
+
         """
         result = self.empty_copy()
         if new_input_alphabet is not None:
@@ -3901,6 +3916,17 @@ class FiniteStateMachine(SageObject):
              Transition from ('B', 2) to ('B', 1): 1|1,
              Transition from ('B', 1) to ('B', 1): 0|0,
              Transition from ('B', 1) to ('B', 2): 1|0]
+
+        Check that colors are correctly dealt with. In particular, the
+        new colors have to be hashable such that
+        :meth:`Automaton.determinisation` does not fail::
+
+            sage: A = Automaton([[0, 0, 0]], initial_states=[0])
+            sage: B = A.composition(A, algorithm='explorative')
+            sage: B.states()[0].color
+            (None, None)
+            sage: B.determinisation()
+            Automaton with 1 states
 
         TODO:
 

commit 547968aaa6eff0d3c96018bc72f080a188881134

FiniteStateMachine.product_FiniteStateMachine, composition: fix unhashable colors
This fixes the failing doctests introduced in commit
2d43fdda50bd5bad3b079e7861fbb725471d1d04.

src/sage/combinat/finite_state_machine.py
@@ -3663,7 +3663,7 @@ class FiniteStateMachine(SageObject):
                 state.is_initial = True
             if all(map(lambda s: s.is_final, state.label())):
                 state.is_final = True
-            state.color = map(lambda s: s.color, state.label())
+            state.color = tuple(map(lambda s: s.color, state.label()))
 
         if only_accessible_components:
             if new_input_alphabet is None:
@@ -3965,7 +3965,7 @@ class FiniteStateMachine(SageObject):
         for state in F.states():
             if all(map(lambda s: s.is_final, state.label())):
                 state.is_final = True
-            state.color = map(lambda s: s.color, state.label())
+            state.color = tuple(map(lambda s: s.color, state.label()))
 
         return F
 
c88c3d163e169d5fdd2ce6e200f8b402ad8eda3a
Nathann Cohen <nathann.cohen@gmail.com>
2014-04-22 23:56:02 +0200

    trac #2686: missing # long time

--- src/sage/graphs/generators/smallgraphs.py
+++ src/sage/graphs/generators/smallgraphs.py
@@ -466,12 +466,12 @@ def Cell120():
 
     EXAMPLES::
 
-        sage: g = graphs.Cell120()
-        sage: g.size()
+        sage: g = graphs.Cell120()      # long time
+        sage: g.size()                  # long time
         1200
-        sage: g.is_regular(4)
+        sage: g.is_regular(4)           # long time
         True
-        sage: g.is_vertex_transitive()
+        sage: g.is_vertex_transitive()  # long time
         True
     """
     from sage.rings.rational_field import QQ

bbd22f1422f7b643a55afabeb44cfd8204b7dd3b
Nicolas M. Thiéry <nthiery@users.sf.net>
2014-04-25 18:46:15 +0200

    - Use __getitem__ only on sequences and Parents not in EnumeratedSets - Use Python's collection to detect sequences and iterables - Catch less exceptions

--- src/sage/combinat/ranker.py
+++ src/sage/combinat/ranker.py
@@ -18,6 +18,7 @@ Rankers
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
+from collections import Iterable, Sequence
 from sage.misc.cachefunc import cached_function
 from sage.structure.parent import Parent
 from sage.categories.enumerated_sets import EnumeratedSets
@@ -138,7 +139,8 @@ def on_fly():
     return [rank, unrank]
 
 def unrank(L, i):
-    """
+    r"""
+
     Return the `i`-th element of `L`.
 
     INPUT:
@@ -148,15 +150,58 @@ def unrank(L, i):
 
     The purpose of this utility is to give a uniform idiom to recover
     the `i`-th element of an object ``L``, whether ``L`` is a list,
-    tuple, enumerated set, some old parent of Sage still implementing
-    unranking in the method ``__getitem__``, or iterable.
+    tuple (or more generally a :class:`collections.Sequence`), an
+    enumerated set, some old parent of Sage still implementing
+    unranking in the method ``__getitem__``, or an iterable (see
+    :class:`collections.Iterable`). See :trac:`15919`.
 
-    See :trac:`15919`.
+    EXAMPLES:
+
+    list, tuples, and other :class:`sequences <collections.Sequence>`::
+
+        sage: from sage.combinat.ranker import unrank
+        sage: unrank(['a','b','c'], 2)
+        'c'
+        sage: unrank(('a','b','c'), 1)
+        'b'
+        sage: unrank(xrange(3,13,2), 1)
+        5
+
+    Enumerated sets::
+
+        sage: unrank(GF(7), 2)
+        2
+        sage: unrank(IntegerModRing(29), 10)
+        10
+
+    An old parent with unranking implemented in __getitem__::
+
+        sage: M = MatrixSpace(GF(3), 2, 2)
+        sage: hasattr(M, "unrank")
+        False
+        sage: M[42]
+        [1 0]
+        [2 1]
+        sage: unrank(M, 42)
+        [1 0]
+        [2 1]
+
+    An iterable::
+
+        sage: unrank(NN,4)
+        4
+
+    An iterator::
+
+        sage: unrank(('a{}'.format(i) for i in range(20)), 0)
+        'a0'
+        sage: unrank(('a{}'.format(i) for i in range(20)), 2)
+        'a2'
 
     .. WARNING::
 
-        When unranking an iterator, it returns the``i``-th step beyond
-        where it is currently at::
+        When unranking an iterator, it returns the``i``-th element
+        beyond where it is currently at::
 
             sage: from sage.combinat.ranker import unrank
             sage: it = iter(range(20))
@@ -164,26 +209,6 @@ def unrank(L, i):
             2
             sage: unrank(it, 2)
             5
-
-    EXAMPLES::
-
-        sage: from sage.combinat.ranker import unrank
-        sage: unrank(['a','b','c'], 2)
-        'c'
-        sage: unrank(('a','b','c'), 1)
-        'b'
-        sage: unrank(GF(7), 2)
-        2
-        sage: unrank(IntegerModRing(29), 10)
-        10
-        sage: unrank(MatrixSpace(GF(3), 2, 2), 42)
-        [1 0]
-        [2 1]
-
-        sage: unrank(('a{}'.format(i) for i in range(20)), 0)
-        'a0'
-        sage: unrank(('a{}'.format(i) for i in range(20)), 2)
-        'a2'
 
     TESTS::
 
@@ -197,26 +222,29 @@ def unrank(L, i):
         Traceback (most recent call last):
         ...
         IndexError: index out of range
+
+        sage: M[100]
+        Traceback (most recent call last):
+        ...
+        IndexError: list index out of range
     """
     if L in EnumeratedSets:
         return L.unrank(i)
-    if isinstance(L, (list, tuple, xrange)):
+    if isinstance(L, Sequence):
         return L[i]
-
-    # Fallback using __getitem__()
-    try:
-        return L[i]
-    except Exception:
-        pass
-
-    # Last fallback seeing if an object is iterable
-    try:
-        it = iter(L)
-        for _ in range(i):
-            it.next()
-        return it.next()
-    except StopIteration as e:
-        raise IndexError("index out of range")
-    except Exception: # Make the exception uniform
-        raise ValueError("Don't know how to unrank on {}".format(L))
+    if isinstance(L, Parent):
+        # handle parents still implementing unranking in __getitem__
+        try:
+            return L[i]
+        except (AttributeError, TypeError, ValueError):
+            pass
+    if isinstance(L, Iterable):
+        try:
+            it = iter(L)
+            for _ in range(i):
+                it.next()
+            return it.next()
+        except StopIteration as e:
+            raise IndexError("index out of range")
+    raise ValueError("Don't know how to unrank on {}".format(L))
 

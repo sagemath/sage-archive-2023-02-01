0061d17ca5e04465c1189d62aa032f98a0c120cf
Charles Bouillaguet <charles.bouillaguet@lifl.fr>
2014-05-14 08:24:48 +0200

    too much code deduplication kills code semantics

--- src/sage/rings/polynomial/polynomial_element.pyx
+++ src/sage/rings/polynomial/polynomial_element.pyx
@@ -2022,7 +2022,19 @@ cdef class Polynomial(CommutativeAlgebraElement):
         _, R = self.quo_rem(other)
         return R
 
-    mod = __mod__
+    def mod(self, other):
+        """
+        Remainder of division of self by other.
+
+        EXAMPLES::
+
+            sage: R.<x> = ZZ[]
+            sage: x % (x+1)
+            -1
+            sage: (x^3 + x - 1) % (x^2 - 1)
+            2*x - 1
+        """
+        return self.__mod__(other)
 
     def _is_atomic(self):
         """

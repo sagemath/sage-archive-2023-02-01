1dc012fca30a9722df5d491a50218b8d030b07ff
Peter Bruin <P.Bruin@warwick.ac.uk>
2014-05-16 14:31:55 +0100

    Trac 13973: fix doctests to adjust for differently presented solutions

--- src/sage/symbolic/expression.pyx
+++ src/sage/symbolic/expression.pyx
@@ -9231,7 +9231,7 @@ cdef class Expression(CommutativeRingElement):
             sage: from sage.calculus.calculus import maxima
             sage: sol = maxima(cos(x)==0).to_poly_solve(x)
             sage: sol.sage()
-            [[x == -1/2*pi + 2*pi*z...], [x == 1/2*pi + 2*pi*z...]]
+            [[x == 1/2*pi + pi*z82]]
 
         If a returned unsolved expression has a denominator, but the
         original one did not, this may also be true::

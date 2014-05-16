6f13d31f76878318e3df70cde722874063b4dd28
Marc Mezzarobba <marc@mezzarobba.net>
2014-05-16 11:48:53 +0200

    rm global is_AmbientSpace, is_AlgebraicScheme

--- src/sage/schemes/generic/all.py
+++ src/sage/schemes/generic/all.py
@@ -1,7 +1,5 @@
 # code exports
 
 from spec             import Spec
-from algebraic_scheme import is_AlgebraicScheme
-from ambient_space    import is_AmbientSpace
 from scheme           import is_Scheme
 from hypersurface     import ProjectiveHypersurface, AffineHypersurface

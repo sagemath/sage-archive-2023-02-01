7db57742c744f66ae944ea22231a55c9ca498514
Daniel Krenn <devel@danielkrenn.at>
2014-05-21 15:53:27 +0200

    added spaces after comment-symbol in code

--- src/sage/combinat/finite_state_machine.py
+++ src/sage/combinat/finite_state_machine.py
@@ -6288,11 +6288,11 @@ class FiniteStateMachine(SageObject):
         cache = {}
 
         def find_final_word_out(state):
-            #The return value is the output which is produced when
-            #reading the given letters until a final state is reached.
-            #If no final state can be reached, then None is returned.
-            #For final states, the final word out is returned.
-            #For final states with empty final output, that is [].
+            # The return value is the output which is produced when
+            # reading the given letters until a final state is reached.
+            # If no final state can be reached, then None is returned.
+            # For final states, the final word out is returned.
+            # For final states with empty final output, that is [].
             position, letter = trailing_letters.next()
             if state.is_final:
                 return state.final_word_out

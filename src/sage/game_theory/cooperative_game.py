b2e35c0303ed2267b1086607d4163aec9a89cfdc
James Campbell <james.campbell@tanti.org.uk>
2014-06-03 16:32:31 +0100

    is_nullplayer passes all tests

--- src/sage/game_theory/cooperative_game.py
+++ src/sage/game_theory/cooperative_game.py
@@ -272,27 +272,42 @@ class CooperativeGame(SageObject):
         Returns True if the current payoff_vector possesses the null player property.
 
         EXAMPLES::
+        A payoff_vector that returns True. ::
 
-            sage: integer_function = {(): 0,
-            ....:                     (1,): 6,
-            ....:                     (2,): 12,
-            ....:                     (3,): 42,
-            ....:                     (1, 2,): 12,
-            ....:                     (1, 3,): 42,
-            ....:                     (2, 3,): 42,
-            ....:                     (1, 2, 3,): 42}
-            sage: integer_game = CooperativeGame(integer_function, [2, 5, 35])
-            sage: integer_game.is_symmetry()
+            sage: letter_function = {(): 0,
+            ....:                    ('A',): 6,
+            ....:                    ('B',): 12,
+            ....:                    ('C',): 42,
+            ....:                    ('A', 'B',): 12,
+            ....:                    ('A', 'C',): 42,
+            ....:                    ('B', 'C',): 42,
+            ....:                    ('A', 'B', 'C',): 42}
+            sage: letter_game = CooperativeGame(letter_function, [14, 14, 14])
+            sage: letter_game.is_nullplayer()
             True
+
+        A payoff_vector that returns False. ::
+
+            sage: A_function = {(): 0,
+            ....:               (1,): 0,
+            ....:               (2,): 12,
+            ....:               (3,): 42,
+            ....:               (1, 2,): 12,
+            ....:               (1, 3,): 42,
+            ....:               (2, 3,): 55,
+            ....:               (1, 2, 3,): 55}
+            sage: A_game = CooperativeGame(A_function, [10, 10, 25])
+            sage: A_game.is_nullplayer()
+            False
         """
         sets = list(self.char_fun.keys())
         element = [i for i in sets if len(i) == 1]
-        other = [i for i in sets if len(i) > 0]
+        other = [i for i in sets]
         status = True
         for j in element:
             for k in other:
                 union = tuple(sorted(set(j) | set(k)))
-                if self.char_fun[union] == self.char_fun[j]:
+                if self.char_fun[union] == self.char_fun[k]:
                     pass
                 else:
                     status = False
@@ -300,7 +315,7 @@ class CooperativeGame(SageObject):
 
             if status is False:
                 pass
-            elif self.char_fun[j] == 0:
+            elif self.payoff_vector[element.index(j)] == 0:
                 pass
             else:
                 return False

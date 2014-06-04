2b204ca4d9664ca42f0001cbf6d3fc08dd47aa6f
James Campbell <james.campbell@tanti.org.uk>
2014-06-04 15:06:03 +0100

    adds a longer function and game

--- src/sage/game_theory/cooperative_game.py
+++ src/sage/game_theory/cooperative_game.py
@@ -622,3 +622,25 @@ class CooperativeGame(SageObject):
             else:
                 pass
         return True
+
+r"""
+sage: long_function = {(): 0,
+....:                  (1,): 0,
+....:                  (2,): 0,
+....:                  (3,): 0,
+....:                  (4,): 0,
+....:                  (1, 2): 0,
+....:                  (1, 3): 0,
+....:                  (1, 4): 0,
+....:                  (2, 3): 0,
+....:                  (2, 4): 0,
+....:                  (3, 4): 0,
+....:                  (1, 2, 3): 0,
+....:                  (1, 2, 4): 45,
+....:                  (1, 3, 4): 40,
+....:                  (2, 3, 4): 0,
+....:                  (1, 2, 3, 4): 65}
+sage: long_game = CooperativeGame(long_function)
+sage: long_game.shapley_value()
+{1: 20, 2: 20, 3: 5, 4: 20}
+"""

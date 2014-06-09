fe3a8a37cc9dfcf966921749d32358e4db98b64d
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2014-06-09 10:02:39 +0200

    trac #15635 two errors corrected

--- src/sage/combinat/free_prelie_algebras.py
+++ src/sage/combinat/free_prelie_algebras.py
@@ -16,7 +16,8 @@ AUTHORS:
 #*****************************************************************************
 
 from sage.categories.all import GradedModulesWithBasis
-from sage.combinat.free_module import CombinatorialFreeModule
+from sage.combinat.free_module import (CombinatorialFreeModule,
+                                       CombinatorialFreeModuleElement)
 from sage.combinat.words.alphabet import Alphabet
 from sage.combinat.rooted_tree import RootedTrees, LabelledRootedTrees
 from sage.misc.lazy_attribute import lazy_attribute
@@ -239,7 +240,7 @@ class FreePreLieAlgebra(CombinatorialFreeModule):
             B[[[], [[]]]]
         """
         npb = self.nap_product_on_basis
-        return self._module_morphism(self._module_morphism(self.npb,
+        return self._module_morphism(self._module_morphism(npb,
                                                            position=0,
                                                            codomain=self),
                                      position=1)

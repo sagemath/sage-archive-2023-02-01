494fa09f4e0158ffcea941df9abc95fa1ddb8ac9
James Campbell <james.campbell@tanti.org.uk>
2014-06-12 15:24:22 +0100

    tidies obtain_Nash

--- src/sage/game_theory/normal_form_game.py
+++ src/sage/game_theory/normal_form_game.py
@@ -40,7 +40,7 @@ class NormalFormGame(Game):
                     self[k][0] = int(matrix1[k])
                     self[k][1] = int(matrix2[k])
 
-    def obtain_Nash(self, algorithm="gambit"):
+    def obtain_Nash(self, algorithm="LCP"):
         r"""
         A function to return the Nash equilibrium for a game.
         Optional arguments can be used to specify the algorithm used.
@@ -54,7 +54,7 @@ class NormalFormGame(Game):
 
           - lrsnash (``algorithm="lrsnash"``). This algorithm is only suited for 2 player games. See the [insert website here] web site.
 
-          - gambit (``algorithm="gambit"``). This algorithm is only suited for 2 player games. See the [insert website here] web site. NOTE THAT WE NEED TO GET THE ACTUAL NAME OF THE GAMBIT ALGORITHM
+          - LCP (``algorithm="LCP"``). This algorithm is only suited for 2 player games. See the [insert website here] web site. NOTE THAT WE NEED TO GET THE ACTUAL NAME OF THE GAMBIT ALGORITHM
 
           - support enumeration (``algorithm="supportenum"``). This is a very inefficient algorithm (in essence a brute force approach).
 
@@ -66,20 +66,21 @@ class NormalFormGame(Game):
            - When set to ``False`` (default) it is assumed that players aim to minimise their utility.
         """
 
-        if algorithm == "gambit":
+        if algorithm == "LCP":
+            if len(self.players) > 2:
+                raise NotImplementedError("Nash equilibrium for games with "
+                      "more than 2 players have not been implemented yet."
+                      "Please see the gambit website [LINK] that has a variety"
+                      " of available algorithms.")
             solver = ExternalLCPSolver()
             return solver.solve(self)
 
-        if not algorithm:
-            if len(game.players) > 2:
-                raise NotImplementedError("Nash equilibrium for games with more than 2 players have not been implemented yet. Please see the gambit website [LINK] that has a variety of available algorithms.")
-            algorithm = "lrsnash"  # This will do for now: when we have a variety of algorithms we will test the best and set the best one.
-        if algorithm == "lrsnash":
-            if not is_package_installed('lrs'):  # This is different to how you've used above, this is better unless there are Sage common practices that point to your way.
-                raise NotImplementedError("lrs is not installed")
-            # Run lrs (Have something simple that takes opposite of matrices as input to lrs)
-            return vector(1/3,1/3,1/3),vector(1/2,1/2)
-        raise NotImplementedError("%s is not yet implemented" % algorithm)
+        # if algorithm == "lrsnash":
+        #     if not is_package_installed('lrs'):  # This is different to how you've used above, this is better unless there are Sage common practices that point to your way.
+        #         raise NotImplementedError("lrs is not installed")
+        #     # Run lrs (Have something simple that takes opposite of matrices as input to lrs)
+        #     return vector(1/3,1/3,1/3),vector(1/2,1/2)
+        # raise NotImplementedError("%s is not yet implemented" % algorithm)
 
 
     def game_matrix(game):

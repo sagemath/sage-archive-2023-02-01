a15518ffd355647240ef717e1befdef8ff7da861
James Campbell <james.campbell@tanti.org.uk>
2014-06-12 10:55:11 +0100

    creates simple class

--- src/sage/game_theory/normal_form_game.py
+++ src/sage/game_theory/normal_form_game.py
@@ -9,29 +9,40 @@ This module implements 2 by 2 normal form (bi-matrix) games. A variety of operat
 """
 from itertools import product
 from sage.misc.package import is_package_installed
+from gambit import Game
 
 
-def normal_form_game(matrix1, matrix2):
-    """
-    Creates an instance of the Gambit Game class from
-    2 matrices.
-    """
-    if is_package_installed('gambit') is not True:
-        raise NotImplementedError("Optional package Gambit is not installed")
-    else:
-        from gambit.lib.libgambit import new_table
+class NormalFormGame(Game):
+    def __new__(NormalFormGame):
+        g = Game.new_table()
+        g.__class__ = NormalFormGame
+        return g
 
-    #create a test for all matrices being the same shape.
-    p1_strats = range(len(matrix1.rows()))
-    p2_strats = range(len(matrix1.columns()))
+    def __init__(self, matrix1=False, matrix2=False, game=False ):
 
-    game = new_table([len(p1_strats), len(p2_strats)])
 
-    for k in product(p1_strats, p2_strats):
-            game[k][0] = int(matrix1[k])
-            game[k][1] = int(matrix2[k])
 
-    return game
+    def _from_matrix_to_game(matrix1, matrix2):
+        """
+        Creates an instance of the Gambit Game class from
+        2 matrices.
+        """
+        if is_package_installed('gambit') is not True:
+            raise NotImplementedError("Optional package Gambit is not installed")
+        else:
+            from gambit.lib.libgambit import new_table
+
+        #create a test for all matrices being the same shape.
+        p1_strats = range(len(matrix1.rows()))
+        p2_strats = range(len(matrix1.columns()))
+
+        game = new_table([len(p1_strats), len(p2_strats)])
+
+        for k in product(p1_strats, p2_strats):
+                game[k][0] = int(matrix1[k])
+                game[k][1] = int(matrix2[k])
+
+        return game
 
 
 #def game_matrix(game):

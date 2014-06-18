9cd6c3a251acc7fcff4256e5d2f9c19149c42126
James Campbell <james.campbell@tanti.org.uk>
2014-06-18 08:54:02 +0100

    changes name of file

--- src/sage/game_theory/parser.py
+++ src/sage/game_theory/parser.py
@@ -1,14 +1,18 @@
-class Formatter():
+class Parser():
+    r"""
+    A class for parsing the outputs of different algorithms so that they can
+    be used be used computationaly
+    """
 
-    def __init__(self, messy_crap, game=None):
-        self.messy_crap = messy_crap
+    def __init__(self, mess, game=None):
+        self.mess = mess
         self.game = game
 
     def format_lrs(self):
         from sage.misc.sage_eval import sage_eval
         p2_strategies = []
         p1_strategies = []
-        for i in self.messy_crap:
+        for i in self.mess:
             if i.startswith('2'):
                 nums = [sage_eval(k) for k in i.split()]
                 p2_strategies.append(nums[1:-1])
@@ -20,7 +24,7 @@ class Formatter():
 
     def format_gambit(self):
         nice_stuff = []
-        for gambitstrategy in self.messy_crap:
+        for gambitstrategy in self.mess:
             gambitstrategy = eval(str(gambitstrategy)[str(gambitstrategy).index("["): str(gambitstrategy).index("]") + 1])
             profile = [gambitstrategy[:len(self.game.players[int(0)].strategies)]]
             for player in list(self.game.players)[1:]:

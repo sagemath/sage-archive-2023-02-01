52d119150c0ebff7ba7d8baddb5f3c6a57f34efc
James Campbell <james.campbell@tanti.org.uk>
2014-06-19 10:18:00 +0100

    adding players now works

--- src/sage/game_theory/normal_form_game.py
+++ src/sage/game_theory/normal_form_game.py
@@ -7,15 +7,15 @@ class NormalFormGame(SageObject):
 
     def __init__(self):
         self.players = []
-        self.strategy_profiles = {}
 
     def add_player(self, num_strategies):
         self.players.append(_Player(num_strategies))
         self.generate_strategy_profiles()
 
     def generate_strategy_profiles(self):
+        self.strategy_profiles = {}
         strategy_sizes = [range(p.num_strategies) for p in self.players]
-        for profile in product(strategy_sizes):
+        for profile in product(*strategy_sizes):
             self.strategy_profiles[profile] = False
 
     def add_strategy(self, player):
@@ -26,8 +26,6 @@ class NormalFormGame(SageObject):
         self.strategy_profiles[strategy_profile] = utility_vector
 
     def _is_complete(self):
-        # will check that all strategy profiles have a legitimate payoff_vector
-        # solve_nash etc can't be called until that's true.
         return all(self.strategy_profiles.values())
 
     def obtain_Nash(self, algorithm="LCP", maximization=True):

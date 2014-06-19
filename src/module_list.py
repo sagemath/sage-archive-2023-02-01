e3d56a83a925024799db11824c9af5da6cdabff4
Ingolfur Edvardsson <ingolfured@gmail.com>
2014-06-19 14:00:34 +0100

    added cvxopt to the module_list

--- src/module_list.py
+++ src/module_list.py
@@ -1406,6 +1406,11 @@ ext_modules = [
               include_dirs = [SAGE_INC, "sage/c_lib/include/"],
               libraries=["stdc++"]),
 
+    Extension("sage.numerical.backends.cvxopt_backend",
+              ["sage/numerical/backends/cvxopt_backend.pyx"],
+              include_dirs = [SAGE_INC, "sage/c_lib/include/"],
+              libraries=["stdc++"]),
+
     Extension("sage.numerical.backends.glpk_graph_backend",
               ["sage/numerical/backends/glpk_graph_backend.pyx"],
               include_dirs = [SAGE_INC, "sage/c_lib/include/"],

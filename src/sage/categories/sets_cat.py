06db0c52e21fdd0df24c9297e48c5e8fce061a3a
Nicolas M. Thiéry <nthiery@users.sf.net>
2014-06-22 12:52:59 -0700

    10668: Trivial comment addition in Sets.ElementMethods

--- src/sage/categories/sets_cat.py
+++ src/sage/categories/sets_cat.py
@@ -1475,6 +1475,8 @@ Please use, e.g., S.algebra(QQ, category = Semigroups())""".format(self))
 
 
     class ElementMethods:
+        ## Should eventually contain the basic operations which are no math
+        ## latex, hash, ...
         ##def equal(x,y):
         ##def =(x,y):
 

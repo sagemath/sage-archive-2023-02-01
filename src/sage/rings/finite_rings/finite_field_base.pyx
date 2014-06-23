a081f2f5827bcb378b3fa0895d074a25a74454e8
Julian Rueth <julian.rueth@fsfe.org>
2014-06-23 22:20:29 +0200

    Added copyright header to finite_field_base.pyx
    The contents of that header were created automatically from the contents of git
    log (every contributor with >10 lines of contribution).

--- src/sage/rings/finite_rings/finite_field_base.pyx
+++ src/sage/rings/finite_rings/finite_field_base.pyx
@@ -7,7 +7,28 @@ TESTS::
     sage: F = K.factor(3)[0][0].residue_field()
     sage: loads(dumps(F)) == F
     True
+
+AUTHORS:
+
+- Adrien Brochard, David Roe, Jeroen Demeyer, Julian Rueth, Niles Johnson,
+  Peter Bruin, Travis Scrimshaw, Xavier Caruso: initial version
+
 """
+#*****************************************************************************
+#       Copyright (C) 2009 David Roe <roed@math.harvard.edu>
+#       Copyright (C) 2010 Niles Johnson <nilesj@gmail.com>
+#       Copyright (C) 2011 Jeroen Demeyer <jdemeyer@cage.ugent.be>
+#       Copyright (C) 2012 Adrien Brochard <aaa.brochard@gmail.com>
+#       Copyright (C) 2012 Travis Scrimshaw <tscrim@ucdavis.edu>
+#       Copyright (C) 2012 Xavier Caruso <xavier.caruso@normalesup.org>
+#       Copyright (C) 2013 Peter Bruin <P.Bruin@warwick.ac.uk>
+#       Copyright (C) 2014 Julian Rueth <julian.rueth@fsfe.org>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
 include "sage/ext/stdsage.pxi"
 
 from sage.categories.finite_fields import FiniteFields

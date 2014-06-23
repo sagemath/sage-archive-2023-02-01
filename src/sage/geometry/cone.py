d437be4a258a648e86f8cc1845ade75467609601
Nathann Cohen <nathann.cohen@gmail.com>
2014-06-23 16:28:26 +0200

    trac #16504: Broken doctest

--- src/sage/geometry/cone.py
+++ src/sage/geometry/cone.py
@@ -3853,7 +3853,7 @@ class ConvexRationalPolyhedralCone(IntegralRayCollection,
         from sage.numerical.mip import MixedIntegerLinearProgram
         p = MixedIntegerLinearProgram(maximization=False)
         p.set_objective(None)
-        x = p.new_variable(integer=True)
+        x = p.new_variable(integer=True, nonnegative=True)
         x = [ x[i] for i in range(0,len(basis)) ]
         for i in range(0,self.lattice_dim()):
             p.add_constraint(sum(b[i]*x[j] for j,b in enumerate(basis)) == point[i])

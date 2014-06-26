1776763d3c6fb4e9b4a8a7a9139dd3fb7d136bbe
James Campbell <james.campbell@tanti.org.uk>
2014-06-26 17:19:50 +0100

    fixes a weird dash

--- src/sage/game_theory/normal_form_game.py
+++ src/sage/game_theory/normal_form_game.py
@@ -439,7 +439,7 @@ class NormalFormGame(SageObject, MutableMapping):
 
     .. [A2000] Avis, David.
        *A revised implementation of the reverse search vertex enumeration algorithm.*
-       Polytopes—combinatorics and computation
+       Polytopes-combinatorics and computation
        Birkhäuser Basel, 2000.
 
     """

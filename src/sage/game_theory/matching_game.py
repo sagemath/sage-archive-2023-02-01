0a569e54b5ed0689f0ed81caff03aafe6be257c7
James Campbell <james.campbell@tanti.org.uk>
2014-06-27 16:18:31 +0100

    starts to populate class

--- src/sage/game_theory/matching_game.py
+++ src/sage/game_theory/matching_game.py
@@ -4,10 +4,14 @@ from sage.structure.sage_object import SageObject
 class Matching_Game(SageObject):
     r"""
     """
-    def __init__(self):
+    def __init__(self, num_pairs=0):
         r"""
         """
-        pass
+        self.husbands = []
+        self.reviewers = []
+        for i in range(num_pairs):
+            self.add_player('suitor')
+            self.add_player('reviewer')
 
     def _repr_(self):
         r"""
@@ -18,3 +22,27 @@ class Matching_Game(SageObject):
         r"""
         """
         pass
+
+    def _is_complete(self):
+        if len(self.suitors) != len(self.reviewers):
+            raise ValueError("must have the same number of suitors as reviewers.")
+
+    def add_player(self, type):
+        if type == 'suitor':
+            self.suitors.append(_Player('suitor'))
+            for reviewer in self.reviewers:
+                reviewer.preference = [-1 for i in range(len(self.suitors))]
+        elif type == 'reviewer':
+            self.reviewers.append(_Player('reviewer'))
+            for suitor in self.suitors:
+                suitor.preference = [-1 for i in range(len(self.reviewers))]
+        else:
+            raise ValueError("type must be suitor or wife")
+
+
+
+
+class _Player():
+    def __init__(self, type):
+        self.type = type
+        self.preference = []

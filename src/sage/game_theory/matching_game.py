74c461d14cd87c9a555e605160d4a064f8fb6d48
James Campbell <james.campbell@tanti.org.uk>
2014-06-27 15:51:35 +0100

    adds matching game.py

---
+++ src/sage/game_theory/matching_game.py
@@ -0,0 +1,20 @@
+from sage.structure.sage_object import SageObject
+
+
+class Matching_Game(SageObject):
+    r"""
+    """
+    def __init__(self):
+        r"""
+        """
+        pass
+
+    def _repr_(self):
+        r"""
+        """
+        pass
+
+    def _latex(self):
+        r"""
+        """
+        pass

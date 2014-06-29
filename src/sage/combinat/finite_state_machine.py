81d8722eb2579cbcb09ab0ef28436988801950fa
Clemens Heuberger <clemens.heuberger@aau.at>
2014-06-29 20:05:03 +0200

    FSMState.__copy__: copy final_word_out and color
    In contrast to FSMState.__deepcopy__, FSMState.__copy__ omitted copying
    the attributes final_word_out and color. This is now fixed.

--- src/sage/combinat/finite_state_machine.py
+++ src/sage/combinat/finite_state_machine.py
@@ -1161,7 +1161,9 @@ class FSMState(SageObject):
             'A'
         """
         new = FSMState(self.label(), self.word_out,
-                       self.is_initial, self.is_final)
+                       self.is_initial, self.is_final,
+                       color=self.color,
+                       final_word_out=self.final_word_out)
         if hasattr(self, 'hook'):
             new.hook = self.hook
         return new

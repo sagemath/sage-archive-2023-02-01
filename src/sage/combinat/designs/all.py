fb636aa9c5019845211ddc90e1166f23a53c684f
Nathann Cohen <nathann.cohen@gmail.com>
2014-07-08 15:29:39 +0200

    trac #16622: Hypergraph is now an incidence structure

--- src/sage/combinat/designs/all.py
+++ src/sage/combinat/designs/all.py
@@ -5,6 +5,8 @@ from ext_rep import (designs_from_XML, designs_from_XML_url)
 from incidence_structures import (IncidenceStructure,
                           IncidenceStructureFromMatrix)
 
+from incidence_structures import IncidenceStructure as Hypergraph
+
 from covering_design import (CoveringDesign,
                              schonheim,
                              trivial_covering_design)

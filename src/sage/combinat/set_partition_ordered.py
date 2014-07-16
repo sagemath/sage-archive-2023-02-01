d352932a118318242660700a153d4aeb81540747
elixyre <jbp@kerios.fr>
2014-07-15 23:23:54 -0400

    Ticket 16646: test added

--- src/sage/combinat/set_partition_ordered.py
+++ src/sage/combinat/set_partition_ordered.py
@@ -561,6 +561,9 @@ class OrderedSetPartitions_scomp(OrderedSetPartitions):
 
             sage: [ p for p in OrderedSetPartitions([1], [1]) ]
             [[{1}]]
+
+            sage: OrderedSetPartitions(42).first()
+            [{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, {25}, {26}, {27}, {28}, {29}, {30}, {31}, {32}, {33}, {34}, {35}, {36}, {37}, {38}, {39}, {40}, {41}, {42}]
         """
         comp = self.c
         lset = [x for x in self._set]

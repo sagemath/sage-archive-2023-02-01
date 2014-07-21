7c2ee9f0605d81bf7ca86be040e4552a62cf9260
Daniel Krenn <devel@danielkrenn.at>
2014-07-21 11:15:29 +0200

    partial revert: GPL2
    This is a partial revert of
      de00128 - copyright updated (years, GPL3, authors)
    since Sage is GPL2 (comment on trac #16677)

--- src/sage/combinat/finite_state_machine.py
+++ src/sage/combinat/finite_state_machine.py
@@ -769,11 +769,10 @@ Methods
 #               2012--2014 Daniel Krenn <dev@danielkrenn.at>
 #               2012--2014 Sara Kropf <sara.kropf@aau.at>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#                  http://www.gnu.org/licenses/
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                http://www.gnu.org/licenses/
 #*****************************************************************************
 
 from sage.structure.sage_object import SageObject
--- src/sage/combinat/finite_state_machine_generators.py
+++ src/sage/combinat/finite_state_machine_generators.py
@@ -56,11 +56,10 @@ Functions and methods
 #                     2014 Daniel Krenn <dev@danielkrenn.at>
 #                     2014 Sara Kropf <sara.kropf@aau.at>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#                  http://www.gnu.org/licenses/
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                http://www.gnu.org/licenses/
 #*****************************************************************************
 
 from sage.combinat.finite_state_machine import Transducer

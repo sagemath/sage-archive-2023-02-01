a94e77e87c4acd07252f7dfdf31095e55cc61309
Volker Braun <vbraun.name@gmail.com>
2014-08-11 22:40:58 +0100

    Symbolic (in)equality involving linear tensors

--- src/module_list.py
+++ src/module_list.py
@@ -1394,6 +1394,11 @@ ext_modules = [
               include_dirs=[SAGE_INC],
               libraries=["stdc++"]),
 
+    Extension("sage.numerical.linear_tensor_element",
+              ["sage/numerical/linear_tensor_element.pyx"],
+              include_dirs=[SAGE_INC],
+              libraries=["stdc++"]),
+
     Extension("sage.numerical.backends.generic_backend",
               ["sage/numerical/backends/generic_backend.pyx"],
               include_dirs = [SAGE_INC, "sage/c_lib/include/"],
--- src/sage/numerical/linear_tensor.py
+++ src/sage/numerical/linear_tensor.py
@@ -14,8 +14,7 @@ matrix MIP linear expressions/constraints. The intented way to
 construct them is implinicly by acting with vectors or matrices on
 linear functions. For example::
 
-    sage: p = MixedIntegerLinearProgram('ppl')   # base ring is QQ
-    sage: x = p.new_variable(nonnegative=False)
+    sage: mip.<x> = MixedIntegerLinearProgram('ppl')   # base ring is QQ
     sage: 3 + x[0] + 2*x[1]            # a linear function
     3 + x_0 + 2*x_1
     sage: x[0] * vector([3,4]) + 1     # vector linear function
@@ -24,19 +23,24 @@ linear functions. For example::
     [1 + 3*x_0 x_0]
     [4*x_0     1  ]
 
+Just like :mod:`sage.numerical.linear_functions`, (in)equalities
+become symbolic inequalities. See
+:mod:`~sage.numerical.linear_tensor_constraints` for detais.
+
 .. NOTE::
 
     For breverity, we just use ``LinearTensor`` in class names. It is
     understood that this refers to the above tensor product
     construction.
 """
+from copy import copy
 
 from sage.structure.parent import Parent
-from sage.structure.element import ModuleElement, Element
 from sage.misc.cachefunc import cached_function
 from sage.matrix.matrix_space import is_MatrixSpace
 from sage.modules.free_module import is_FreeModule
 from sage.numerical.linear_functions import is_LinearFunction, LinearFunctionsParent_class
+from sage.numerical.linear_tensor_element import LinearTensor
 
 #*****************************************************************************
 #
@@ -116,267 +120,6 @@ def LinearTensorParent(free_module_parent, linear_functions_parent):
     return LinearTensorParent_class(free_module_parent, linear_functions_parent)
 
 
-#*****************************************************************************
-#
-# Elements of linear functions tensored with a free module
-#
-#*****************************************************************************
-
-class LinearTensor(ModuleElement):
-    r"""
-    A linear function tensored with a free module
-
-    .. warning::
-
-        You should never instantiate :class:`LinearTensor`
-        manually. Use the element constructor in the parent
-        instead.
-
-    EXAMPLES::
-
-        sage: parent = MixedIntegerLinearProgram().linear_functions_parent().tensor(RDF^2)
-        sage: parent({0: [1,2], 3: [-7,-8]})
-        (1.0, 2.0)*x_0 + (-7.0, -8.0)*x_3
-    """
-
-    def __init__(self, parent, f):
-        r"""
-        Constructor taking a dictionary as its argument.
-
-        INPUT:
-        
-        - ``parent`` -- the parent :class:`LinearTensorParent_class`.
-        
-        - ``f`` -- A linear function tensored by a free module is
-          represented as a dictionary. The values are the coefficient
-          (free module elements) of the variable represented by the
-          keys. The key ``-1`` corresponds to the constant term.
-
-        EXAMPLES:
-
-        With a dictionary::
-
-            sage: LT = MixedIntegerLinearProgram().linear_functions_parent().tensor(RDF^2)
-            sage: LT({0: [1,2], 3: [-7,-8]})
-            (1.0, 2.0)*x_0 + (-7.0, -8.0)*x_3
-        
-            sage: TestSuite(LT).run(skip=['_test_additive_associativity', '_test_elements', '_test_pickling', '_test_zero'])
-        """
-        ModuleElement.__init__(self, parent)
-        assert isinstance(f, dict)
-        self._f = f
-
-    def __getitem__(self, *indices):
-        """
-        Return the linear function component with given tensor indices.
-
-        INPUT:
-
-        - ``*indices`` -- one or more integers. The basis indices of
-          the free module. E.g. a single integer for vectors, two for
-          matrices.
-
-        EXAMPLES::
-
-            sage: p = MixedIntegerLinearProgram().linear_functions_parent().tensor(RDF^2)
-            sage: lt = p({0:[1,2], 3:[4,5]});  lt
-            (1.0, 2.0)*x_0 + (4.0, 5.0)*x_3
-            sage: lt[0]
-            x_0 + 4*x_3
-            sage: lt[1]
-            2*x_0 + 5*x_3
-        """
-        f = dict([key, value.__getitem__(*indices)] for key, value in self._f.items())
-        LF = self.parent().linear_functions()
-        return LF(f)
-
-    def dict(self):
-        r"""
-        Return the dictionary corresponding to the tensor product.
-
-        OUTPUT:
-
-        The linear function tensor product is represented as a
-        dictionary. The value are the coefficient (free module
-        elements) of the variable represented by the keys (which are
-        integers). The key ``-1`` corresponds to the constant term.
-
-        EXAMPLE::
-
-            sage: p = MixedIntegerLinearProgram().linear_functions_parent().tensor(RDF^2)
-            sage: lt = p({0:[1,2], 3:[4,5]})
-            sage: lt.dict()
-            {0: (1.0, 2.0), 3: (4.0, 5.0)}
-        """
-        return dict(self._f)
-
-    def _repr_(self):
-        """
-        Return a string representation.
-
-        OUTPUT:
-
-        String.
-        
-        EXAMPLES::
-
-            sage: from sage.numerical.linear_functions import LinearFunctionsParent
-            sage: R.<s,t> = RDF[]
-            sage: LT = LinearFunctionsParent(RDF).tensor(R)
-            sage: LT.an_element()  # indirect doctest
-            (s) + (5.0*s)*x_2 + (7.0*s)*x_5
-
-            sage: LT = LinearFunctionsParent(RDF).tensor(RDF^2)
-            sage: LT.an_element()  # indirect doctest
-            (1.0, 0.0) + (5.0, 0.0)*x_2 + (7.0, 0.0)*x_5
-        """
-        if is_MatrixSpace(self.parent().free_module()):
-            return self._repr_matrix()
-        terms = []
-        for key in sorted(self._f.keys()):
-            coeff = self._f[key]
-            if coeff._is_atomic():
-                if key == -1:
-                    term = '({1})'.format(key, coeff)
-                else:
-                    term = '({1})*x_{0}'.format(key, coeff)
-            else:
-                if key == -1:
-                    term = '{1}'.format(key, coeff)
-                else:
-                    term = '{1}*x_{0}'.format(key, coeff)
-            terms.append(term)
-        return ' + '.join(terms)
-            
-    def _repr_matrix(self):
-        """
-        Return a matrix-like string representation.
-
-        OUTPUT:
-
-        String.
-
-        EXAMPLES::
-
-            sage: from sage.numerical.linear_functions import LinearFunctionsParent
-            sage: LT = LinearFunctionsParent(RDF).tensor(RDF^(2,2))
-            sage: LT.an_element()  # indirect doctest
-            [1 + 5*x_2 + 7*x_5 0]
-            [0                 0]
-        """
-        MS = self.parent().free_module()
-        assert is_MatrixSpace(MS)
-        col_lengths = []
-        columns = []
-        for c in range(MS.ncols()):
-            column = []
-            for r in range(MS.nrows()):
-                cell = repr(self[r, c])
-                column.append(cell)
-            columns.append(column)
-            col_lengths.append(max(map(len, column)))
-        s = ''
-        for r in range(MS.nrows()):
-            if r > 0:
-                s += '\n'
-            s += '['
-            for c in range(MS.ncols()):
-                if c > 0:
-                    s += ' '
-                s += columns[c][r].ljust(col_lengths[c])
-            s += ']'
-        return s
-
-    def _add_(self, b):
-        r"""
-        Return sum.
-
-        INPUT:
-        
-        - ``b`` -- a :class:`LinearTensor`.
-
-        OUTPUT:
-
-        A :class:`LinearTensor`.
-
-        EXAMPLE::
-
-            sage: from sage.numerical.linear_functions import LinearFunctionsParent
-            sage: LT = LinearFunctionsParent(RDF).tensor(RDF^2)
-            sage: LT({0: [1,2], 3: [-7,-8]}) + LT({2: [5,6], 3: [2,-2]}) + 16
-            (16.0, 16.0) + (1.0, 2.0)*x_0 + (5.0, 6.0)*x_2 + (-5.0, -10.0)*x_3
-        """
-        result = dict(self._f)
-        for key, coeff in b.dict().iteritems():
-            result[key] = self._f.get(key, 0) + coeff
-        return self.parent()(result)
-
-    def _neg_(self):
-        r"""
-        Return the negative.
-
-        OUTPUT:
-
-        A :class:`LinearTensor`.
-
-        EXAMPLE::
-
-            sage: from sage.numerical.linear_functions import LinearFunctionsParent
-            sage: LT = LinearFunctionsParent(RDF).tensor(RDF^2)
-            sage: -LT({0: [1,2], 3: [-7,-8]})
-            (-1.0, -2.0)*x_0 + (7.0, 8.0)*x_3
-        """
-        result = dict([key, -coeff] for key, coeff in self._f.items())
-        return self.parent()(result)
-
-    def _sub_(self, b):
-        r"""
-        Return difference.
-
-        INPUT:
-        
-        - ``b`` -- a :class:`LinearTensor`.
-
-        OUTPUT:
-
-        A :class:`LinearTensor`.
-
-        EXAMPLE::
-
-            sage: from sage.numerical.linear_functions import LinearFunctionsParent
-            sage: LT = LinearFunctionsParent(RDF).tensor(RDF^2)
-            sage: LT({0: [1,2], 3: [-7,-8]}) - LT({1: [1,2]})
-            (1.0, 2.0)*x_0 + (-1.0, -2.0)*x_1 + (-7.0, -8.0)*x_3
-            sage: LT({0: [1,2], 3: [-7,-8]}) - 16
-            (-16.0, -16.0) + (1.0, 2.0)*x_0 + (-7.0, -8.0)*x_3
-        """
-        result = dict(self._f)
-        for key, coeff in b.dict().iteritems():
-            result[key] = self._f.get(key, 0) - coeff
-        return self.parent()(result)
-
-    def _rmul_(self, b):
-        r"""
-        Return right multiplication by scalar.
-
-        INPUT:
-
-        - ``b`` -- base ring element. The scalar to multiply by.
-
-        OUTPUT:
-
-        A :class:`LinearTensor`.
-
-        EXAMPLE::
-
-            sage: from sage.numerical.linear_functions import LinearFunctionsParent
-            sage: LT = LinearFunctionsParent(RDF).tensor(RDF^2)
-            sage: 10 * LT({0: [1,2], 3: [-7,-8]})
-            (10.0, 20.0)*x_0 + (-70.0, -80.0)*x_3
-        """
-        result = dict([key, b*coeff] for key, coeff in self._f.items())
-        return self.parent()(result)
-
 
 #*****************************************************************************
 #
@@ -403,7 +146,6 @@ class LinearTensorParent_class(Parent):
         sage: LinearTensorParent_class
         <class 'sage.numerical.linear_tensor.LinearTensorParent_class'>
     """
-
     Element = LinearTensor
 
     def __init__(self, free_module, linear_functions):
@@ -447,6 +189,40 @@ class LinearTensorParent_class(Parent):
             Linear functions over Real Double Field
         """
         return 'Tensor product of {0} and {1}'.format(self.free_module(), self.linear_functions())
+
+    def _convert_constant(self, m):
+        """
+        Convert ``m`` to a constant free module element.
+
+        OUTPUT:
+
+        A :meth:`free_module`` element.
+
+        EXAMPLES::
+
+            sage: mip = MixedIntegerLinearProgram()
+            sage: LF = mip.linear_functions_parent()
+            sage: LF.tensor(RDF^2)._convert_constant(42)
+            (42.0, 42.0)
+            sage: LF.tensor(RDF^(2,2))._convert_constant(42)
+            [42.0  0.0]
+            [ 0.0 42.0]
+        """
+        M = self.free_module()
+        m = M.base_ring()(m)
+        if is_MatrixSpace(M):
+            # Turn constants into diagonal matrices
+            m_matrix = copy(M.zero_matrix())
+            for i in range(min(M.ncols(), M.nrows())):
+                m_matrix[i, i] = m
+            m_matrix.set_immutable()
+            return m_matrix
+        elif is_FreeModule(M):
+            # Turn constants into vectors with all entries equal
+            m_vector = M([m] * M.degree())
+            return m_vector
+        else:
+            return M(m)
         
     def _element_constructor_(self, x):
         """
@@ -454,7 +230,8 @@ class LinearTensorParent_class(Parent):
 
         INPUT:
 
-        - ``x`` -- anything that defines a :class:`LinearTensor`. See
+        - ``x`` -- anything that defines a
+          :class:`~sage.numerical.linear_tensor_element.LinearTensor`. See
           examples.
 
         EXAMPLES::
@@ -469,7 +246,7 @@ class LinearTensorParent_class(Parent):
             sage: LT({1:[1, 2]})    # indirect doctest
             (1.0, 2.0)*x_1
             sage: type(_)
-            <class 'sage.numerical.linear_tensor.LinearTensorParent_class_with_category.element_class'>
+            <type 'sage.numerical.linear_tensor_element.LinearTensor'>
 
         Construct from scalar:
 
@@ -486,31 +263,31 @@ class LinearTensorParent_class(Parent):
             sage: LT_QQ(42.1)
             [421/10 0     ]
             [0      421/10]
+
+        Construct from a linear function::
+
+            sage: from sage.numerical.linear_functions import LinearFunctionsParent
+            sage: LF_ZZ = LinearFunctionsParent(ZZ)
+            sage: lf = LF_ZZ({-1:3, 1:2, 3:1})
+            sage: LT(lf)
+            (3.0, 3.0) + (2.0, 2.0)*x_1 + (1.0, 1.0)*x_3
         """
+        M = self.free_module()
         if is_LinearTensor(x):
             if x.parent() is self:
                 return x
             else:
                 x = x.dict()
-        M = self.free_module()
-        if isinstance(x, dict):
-            x = dict( (int(key), M(value)) for key, value in x.items() )
+        elif is_LinearFunction(x):
+            x = dict([key, self._convert_constant(value)] for key, value in x.dict().items())
+        elif isinstance(x, dict):
+            x = dict([int(key), M(value)] for key, value in x.items())
         else:
             try:
                 x = {-1: M(x)}
             except (TypeError, ValueError):
                 x_R = M.base_ring()(x)
-                if is_MatrixSpace(M):
-                    # Turn constants into diagonal matrices
-                    x_matrix = M.zero_matrix()
-                    for i in range(min(M.ncols(), M.nrows())):
-                        x_matrix[i, i] = x_R
-                    x = {-1: x_matrix}
-                elif is_FreeModule(M):
-                    # Turn constants into vectors with all entries equal
-                    x = {-1: M([x_R] * M.degree())}
-                else:
-                    raise
+                x = {-1: self._convert_constant(x_R)}
         return self.element_class(self, x)
 
     def _coerce_map_from_(self, R):
@@ -533,12 +310,16 @@ class LinearTensorParent_class(Parent):
             2
             sage: parent._coerce_map_from_(int)
             True
+
+            sage: mip.<x> = MixedIntegerLinearProgram()
+            sage: LF = mip.linear_functions_parent()
+            sage: LT = LF.tensor(RDF^3)
+            sage: LT.has_coerce_map_from(LF)
+            True
         """
-        if R in [int, float, long, complex]:
+        if self.free_module().has_coerce_map_from(R):
             return True
-        from sage.rings.real_mpfr import mpfr_prec_min
-        from sage.rings.all import RealField
-        if RealField(mpfr_prec_min()).has_coerce_map_from(R):
+        if self.linear_functions().has_coerce_map_from(R):
             return True
         return False
 
@@ -560,4 +341,6 @@ class LinearTensorParent_class(Parent):
         """
         m = self.free_module().an_element()
         return self._element_constructor_({-1:m, 2:5*m, 5:7*m})
+
+
 

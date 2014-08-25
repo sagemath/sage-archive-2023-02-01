15b1d2836160cdf7344960dabea88a434edc5f10
David Mödinger <david.moedinger@uni-ulm.de>
2014-08-25 15:41:12 +0200

    Added a few examples

--- src/sage/matrix/weak_popov.pyx
+++ src/sage/matrix/weak_popov.pyx
@@ -81,7 +81,7 @@ cpdef mulders_storjohann(M,transposition=False):
 
     INPUT:
 
-     - `M` - Matrix.
+     - `M` - Matrix over a polynomialring.
      
      - `transposition` - Boolean (default: False) indicating if a Matrix
      U should be computed so that U*M = M.weak_popov_form()
@@ -106,14 +106,15 @@ cpdef mulders_storjohann(M,transposition=False):
 
     EXAMPLES:
     
-    The value transposition can be used to check unimodular equivalence. ::
+    The value transposition can be used to get a second matrix to check 
+    unimodular equivalence. ::
     
         sage: F.<a> = GF(2^4,'a')
         sage: PF.<x> = F[]
         sage: A = matrix(PF,[[1,a*x^17+1],[0,a*x^11+a^2*x^7+1]])
         sage: Ac = copy(A)
         sage: au = A.weak_popov_form(implementation="cython",transposition=True)
-        sage: au[1]*A == au[0]
+        sage: au[1]*Ac == au[0]
         True
         sage: au[1].is_invertible()
         True
@@ -121,14 +122,62 @@ cpdef mulders_storjohann(M,transposition=False):
     The cython implementation can be used to speed up the computation of
     a weak popov form. ::
     
-        sage: B = 
-        matrix(PF,[[x^2+a,x^2+a,x^2+a],[x^3+a*x+1,x+a^2,x^5+a*x^4+a^2*x^3]])
+        sage: B = matrix(PF,[[x^2+a,x^2+a,x^2+a], [x^3+a*x+1,x+a^2,x^5+a*x^4+a^2*x^3]])
         sage: B.weak_popov_form(implementation="cython")
         [                    x^2 + a                     x^2 + a                 
-    x^2 + a]
+              x^2 + a]
         [x^5 + (a + 1)*x^3 + a*x + 1       x^5 + a*x^3 + x + a^2       a*x^4 + 
-(a^2 + a)*x^3]
+        (a^2 + a)*x^3]
     
+    Matrices containing only zeros will return the way they are. ::
+    
+        sage: Z = matrix(PF,5,3)
+        sage: Z.weak_popov_form(implementation="cython")
+        [0 0 0]
+        [0 0 0]
+        [0 0 0]
+        [0 0 0]
+        [0 0 0]
+    
+    Generally matrices in weak popov form will just be returned. ::
+    
+        sage: F.<a> = GF(17,'a')
+        sage: PF.<x> = F[]
+        sage: C = matrix(PF,[[1,7,x],[x^2,x,4],[2,x,11]])
+        sage: C.weak_popov_form(implementation="cython")
+        [  1   7   x]
+        [x^2   x   4]
+        [  2   x  11]
+    
+    And the transposition will be the identity matrix. ::
+    
+        sage: C.weak_popov_form(implementation="cython",transposition=True)
+        (
+        [  1   7   x]  [1 0 0]
+        [x^2   x   4]  [0 1 0]
+        [  2   x  11], [0 0 1]
+        )
+
+
+    It is an error to call this function with a matrix not over a polynomial
+    ring. ::
+
+        sage: M = matrix([[1,0],[1,1]])
+        sage: M.weak_popov_form(implementation="cython")
+        Traceback (most recent call last):
+        ...
+        TypeError: the entries of M must lie in a univariate polynomial ring
+
+    It is also an error to call this function using a matrix containing
+    elements of the fraction field.
+
+        sage: R.<t> = QQ['t']
+        sage: M = matrix([[1/t,1/(t^2),t],[0,0,t]])
+        sage: M.weak_popov_form()
+        Traceback (most recent call last):
+        ...
+        TypeError: the entries of M must lie in a univariate polynomial ring
+            
     .. SEEALSO::
 
         :meth:`is_weak_popov <sage.matrix.matrix0.is_weak_popov>`
@@ -138,6 +187,10 @@ cpdef mulders_storjohann(M,transposition=False):
     .. [MS] T. Mulders, A. Storjohann, "On lattice reduction for polynomial
           matrices," J. Symbolic Comput. 35 (2003), no. 4, 377--401
     """
+    from sage.rings.polynomial.polynomial_ring import is_PolynomialRing
+    if not is_PolynomialRing(M.base_ring()):
+        raise TypeError("the entries of M must lie in a univariate polynomial ring")
+
     if transposition==True:
         from sage.matrix.constructor import identity_matrix
         U = identity_matrix(M.base_ring(),M.nrows())
@@ -146,12 +199,14 @@ cpdef mulders_storjohann(M,transposition=False):
     lps = defaultdict(list)
     for c in range(M.nrows()):
         lp = leading_position(M[c])
-        if not M[c,lp]==-1:
+        if M[c, lp] == 0:
+            lps[-c-1].append(c)    # This enables easy success check while ignoring a zero row
+        else:
             lps[lp].append(c)
-    
-    while len(lps)<M.nrows():
+
+    while len(lps) < M.nrows():
         for pos in lps:
-            if len(lps[pos])>1:
+            if len(lps[pos]) > 1:
                 if (M[lps[pos][0]][pos].degree() >= M[lps[pos][1]][pos].degree()):
                     rowtochange = lps[pos][0]
                     basisrow = lps[pos][1]
@@ -160,7 +215,11 @@ cpdef mulders_storjohann(M,transposition=False):
                     basisrow = lps[pos][0]
                 simple_transformation(M,rowtochange,basisrow,pos,U)
                 lps[pos].remove(rowtochange)
-                lps[leading_position(M[rowtochange])].append(rowtochange)
+                
+                if M[rowtochange, leading_position(M[rowtochange])] == 0:
+                    lps[-rowtochange-1].append(rowtochange)    # ignore a line of pure zeros
+                else:
+                    lps[leading_position(M[rowtochange])].append(rowtochange)
                 break
     if U is not None:
         return (M,U)

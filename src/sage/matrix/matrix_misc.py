4106d05f9df82f5292e9aacb0aa4d0f3487e54f9
David Mödinger <david.moedinger@uni-ulm.de>
2014-08-28 15:57:37 +0200

    Deprecation include for keyword.

--- src/sage/matrix/matrix_misc.py
+++ src/sage/matrix/matrix_misc.py
@@ -26,6 +26,11 @@ def row_iterator(A):
         yield A.row(i)
 
 def weak_popov_form(M,ascend=True):
+    from sage.misc.superseded import deprecation
+    deprecation(16888, 'You can just call row_reduced_form() instead')
+    return row_reduced_form(M,ascend)
+
+def row_reduced_form(M,ascend=None):
     """
     This function computes a weak Popov form of a matrix over a rational
     function field `k(x)`, for `k` a field.
@@ -74,6 +79,9 @@ def weak_popov_form(M,ascend=True):
     See docstring for weak_popov_form method of matrices for
     more information.
     """
+    if ascend is not None:
+        from sage.misc.superseded import deprecation
+        deprecation(16888, 'You can just call row_reduced_form() instead')
 
     # determine whether M has polynomial or rational function coefficients
     R0 = M.base_ring()
@@ -180,22 +188,24 @@ def weak_popov_form(M,ascend=True):
                     # so continue onto next step of algorithm
                     break
 
-    # sort the rows in order of degree
-    d = []
-    from sage.rings.all import infinity
-    for i in range(len(r)):
-        d.append(max([e.degree() for e in r[i]]))
-        if d[i] < 0:
-            d[i] = -infinity
-        else:
-            d[i] -= den.degree()
+    if ascend is not None:
+        # sort the rows in order of degree
+        d = []
+        from sage.rings.all import infinity
+        for i in range(len(r)):
+            d.append(max([e.degree() for e in r[i]]))
+            if d[i] < 0:
+                d[i] = -infinity
+            else:
+                d[i] -= den.degree()
 
-    for i in range(len(r)):
-        for j in range(i+1,len(r)):
-            if (ascend and d[i] > d[j]) or (not ascend and d[i] < d[j]):
-                (r[i], r[j]) = (r[j], r[i])
-                (d[i], d[j]) = (d[j], d[i])
-                (N[i], N[j]) = (N[j], N[i])
+        for i in range(len(r)):
+            for j in range(i+1,len(r)):
+                if (ascend and d[i] > d[j]) or (not ascend and d[i] < d[j]):
+                    (r[i], r[j]) = (r[j], r[i])
+                    (d[i], d[j]) = (d[j], d[i])
+                    (N[i], N[j]) = (N[j], N[i])
 
-    # return reduced matrix and operations matrix
-    return (matrix(r)/den, matrix(N), d)
+        # return reduced matrix and operations matrix
+        return (matrix(r)/den, matrix(N), d)
+    return matrix(r)/den

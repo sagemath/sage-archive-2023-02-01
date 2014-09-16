58ff710db65e25485fc57c707040edaea3092154
Volker Braun <vbraun.name@gmail.com>
2014-09-16 14:42:17 +0100

    Add a sage -ipynb option

---
+++ src/sage/repl/ipython_notebook.py
@@ -0,0 +1,12 @@
+"""
+Sage Extensions for the IPython Notebook
+"""
+
+from IPython.html.notebookapp import NotebookApp
+
+from sage.repl.interpreter import SageCrashHandler
+
+
+class SageNotebookApp(NotebookApp):
+
+    pass

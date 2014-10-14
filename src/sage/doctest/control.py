3ccc895dc14ea8c702d25b4f98c4740c6f1139b7
John H. Palmieri <palmieri@math.washington.edu>
2014-10-14 14:23:00 -0700

    Print the git branch when doctesting

--- src/sage/doctest/control.py
+++ src/sage/doctest/control.py
@@ -964,6 +964,16 @@ class DocTestController(SageObject):
         else:
             self.test_safe_directory()
             self.create_run_id()
+            from sage.env import SAGE_ROOT
+            DOT_GIT= os.path.join(SAGE_ROOT, '.git')
+            if os.path.exists(DOT_GIT):
+                import subprocess
+                branch = subprocess.check_output(["git",
+                                                  "--git-dir=" + DOT_GIT,
+                                                  "rev-parse",
+                                                  "--abbrev-ref",
+                                                  "HEAD"])
+                self.log("Git branch: " + branch)
             self.add_files()
             self.expand_files_into_sources()
             self.filter_sources()

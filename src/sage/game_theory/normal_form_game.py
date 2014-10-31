4003bde6e6500ccc5e85e0ceb6792e3f658f550e
vince <vincent.knight@gmail.com>
2014-10-31 07:15:43 +0000

    Fixed brackets

--- src/sage/game_theory/normal_form_game.py
+++ src/sage/game_theory/normal_form_game.py
@@ -195,8 +195,8 @@ class NormalFormGame(SageObject, MutableMapping):
 
         sage: y = var('y')
         sage: A = matrix([[1, -1], [-1, 1]])
-        sage: p = plot((A * vector([y, 1 - y]))[0], y, 0, 1, color='blue', legend_label='$u_1(r_1, (y, 1-y)$'), axes_labels=['$y$', ''])
-        sage: p += plot((A * vector([y, 1 - y]))[1], y, 0, 1, color='red', legend_label='$u_1(r_2, (y, 1-y)$'))
+        sage: p = plot((A * vector([y, 1 - y]))[0], y, 0, 1, color='blue', legend_label='$u_1(r_1, (y, 1-y))$', axes_labels=['$y$', ''])
+        sage: p += plot((A * vector([y, 1 - y]))[1], y, 0, 1, color='red', legend_label='$u_1(r_2, (y, 1-y))$')
 
     We see that the only point at which player 1 is indifferent amongst
     available strategies is when `y=1/2`.

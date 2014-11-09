b87a7366fe35b3164a60f0f788ffc96d32071377
Travis Scrimshaw <tscrim at ucdavis.edu>
2014-11-09 12:28:16 -0800

    Changed some things around to make it faster and marked tests as long.

--- src/sage/combinat/k_tableau.py
+++ src/sage/combinat/k_tableau.py
@@ -7,7 +7,7 @@ whereas standard strong `k`-tableaux correspond to saturated chains in the stron
 For semistandard tableaux, the notion of weak and strong horizontal strip is necessary.
 More information can be found in [LLMS2006]_ .
 
-    .. SEEALSO:: :meth:`sage.combinat.k_tableau.StrongTableau`, :meth:`sage.combinat.k_tableau.WeakTableau`
+.. SEEALSO:: :meth:`sage.combinat.k_tableau.StrongTableau`, :meth:`sage.combinat.k_tableau.WeakTableau`
 
 REFERENCES:
 
@@ -2143,7 +2143,7 @@ class WeakTableaux_factorized_permutation(WeakTableaux_abstract):
             sage: T = WeakTableaux_factorized_permutation(3, [2,1], [1,1,1])
             sage: TestSuite(T).run()
             sage: T = WeakTableaux_factorized_permutation(4, [[6,2,1], [2]], [2,1,1,1])
-            sage: TestSuite(T).run()
+            sage: TestSuite(T).run() # long time
         """
         if shape == [] or shape[0] in ZZ:
             shape = (Core(shape, k+1), Core([],k+1))
@@ -2169,7 +2169,7 @@ class WeakTableaux_factorized_permutation(WeakTableaux_abstract):
             sage: T = WeakTableaux_factorized_permutation(3, [4,1], [2,2])
             sage: TestSuite(T).run()
             sage: T = WeakTableaux_factorized_permutation(4, [[6,2,1], [2]], [2,1,1,1])
-            sage: TestSuite(T).run()
+            sage: TestSuite(T).run() # long time
         """
         self.k = k
         self._skew = shape[1]!=[]
@@ -4510,30 +4510,43 @@ class StrongTableaux(UniqueRepresentation, Parent):
             []
         """
         LL = list(T)
-        if LL==[] or all(v is None for v in sum(LL,[])):
+        if not LL or all(v is None for v in sum(LL,[])):
             return []
-        marks = [v for row in T for v in row if v is not None and v<0]+[0]
+        marks = [v for row in T for v in row if v is not None and v<0] + [0]
         m = -min(marks) # the largest marked cell
         transeq = [] # start with the empty list and append on the right
         sh = Core(map(len,T), k+1)
-        j = max([ c-r for r in range(len(LL)) for c in range(len(LL[r])) if LL[r][c]==-m ])
+        j = max([ c-r for r,row in enumerate(LL) for c,val in enumerate(row)
+                  if val == -m ])
+        P = sh.to_partition()
         for l in range(k):
-            msh = sh.affine_symmetric_group_action([j-l,j+1],transposition=True)
+            msh = sh.affine_symmetric_group_action([j-l,j+1], transposition=True)
+            mP = msh.to_partition()
             # my worry here is that the affine symmetric group action might apply an invalid
             # transposition but get something of the right length anyway.  How do I test if it is applying
             # a valid or invalid transposition?
-            if msh.length()==sh.length()-1:
+            if msh.length() == sh.length() - 1:
                 # if applying t_{j-l,j+1} reduces the size of the shape by 1
-                skewcells = SkewPartition([sh.to_partition(),msh.to_partition()]).cells()
-                valcells = [LL[c[0]][c[1]] for c in skewcells if c[1]-c[0]!=j] # values in all the cells except content j
-                regcells = [LL[c[0]][c[1]] for c in skewcells if c[1]-c[0]==j] # values in the cells with content j
-                if all(x==m for x in valcells) and regcells==[-m]:
+                valcells = [] # values in all the cells except content j
+                regcells = [] # values in the cells with content j
+                valid = True
+                for (x,y) in SkewPartition([P, mP]).cells():
+                    if y-x != j:
+                        if LL[x][y] != m:
+                            valid = False
+                            break
+                        valcells.append(LL[x][y])
+                    else:
+                        regcells.append(LL[x][y])
+                if valid and regcells == [-m]:
                     # if all labels that are not content j are v and the label
                     # with content j = -m
-                    MM = [[LL[a][b] for b in range(len(LL[a])) if (a,b) in msh.to_partition().cells()] for a in range(len(msh.to_partition()))]
-                    transeq = self.marked_CST_to_transposition_sequence(MM,k)
+                    mcells = mP.cells()
+                    MM = [[LL[a][b] for b in range(len(LL[a])) if (a,b) in mcells]
+                          for a in range(len(mP))]
+                    transeq = self.marked_CST_to_transposition_sequence(MM, k)
                     if not transeq is None:
-                        return [[j-l, j+1]]+transeq
+                        return [[j-l, j+1]] + transeq
 
     @classmethod
     def transpositions_to_standard_strong( self, transeq, k, emptyTableau=[] ):

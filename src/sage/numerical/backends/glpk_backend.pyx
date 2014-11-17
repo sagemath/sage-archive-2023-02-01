9ecce275cf6f9455e2b732c84518fe293aa2bdea
Jean-Pierre Flori <jean-pierre.flori@ssi.gouv.fr>
2014-11-17 04:36:42 -0800

    Modify GLPK test involving numerical noise.

--- src/sage/numerical/backends/glpk_backend.pyx
+++ src/sage/numerical/backends/glpk_backend.pyx
@@ -886,8 +886,8 @@ cdef class GLPKBackend(GenericBackend):
             0
             sage: p.get_objective_value()
             7.5
-            sage: p.get_variable_value(0)
-            -0.0
+            sage: p.get_variable_value(0) # abs tol 1e-15
+            0.0
             sage: p.get_variable_value(1)
             1.5
         """
@@ -916,8 +916,8 @@ cdef class GLPKBackend(GenericBackend):
             0
             sage: p.get_objective_value()
             7.5
-            sage: p.get_variable_value(0)
-            -0.0
+            sage: p.get_variable_value(0) # abs tol 1e-15
+            0.0
             sage: p.get_variable_value(1)
             1.5
         """

e1683f05e63a9e3f02f5976dcf6e7197ed79b4f6
Clemens Heuberger <clemens.heuberger@aau.at>
2014-11-24 15:59:11 +0100

    Trac #17194: Changes required after merge of #17267

--- src/sage/rings/real_arb.pxd
+++ src/sage/rings/real_arb.pxd
@@ -1,9 +1,8 @@
 from sage.libs.arb.arb cimport arb_t
+from sage.libs.mpfi cimport mpfi_t
 from sage.rings.real_mpfi cimport RealIntervalFieldElement
 from sage.structure.parent cimport Parent
 from sage.structure.element cimport Element
-
-include 'mpfi.pxi'
 
 cdef void mpfi_to_arb(arb_t target, const mpfi_t source, const unsigned long precision)
 cdef void arb_to_mpfi(mpfi_t target, arb_t source, const unsigned long precision)
--- src/sage/rings/real_arb.pyx
+++ src/sage/rings/real_arb.pyx
@@ -26,7 +26,8 @@ from sage.libs.arb.arb cimport *
 from sage.libs.arb.arf cimport arf_t, arf_get_mpfr
 from sage.libs.arb.fmpr cimport fmpr_t, fmpr_init, fmpr_clear, fmpr_get_mpfr
 from sage.libs.arb.mag cimport mag_t, mag_get_fmpr
-from sage.libs.mpfr cimport mpfr_inits2, mpfr_clears, mpfr_t, mpfr_rnd_t
+from sage.libs.mpfi cimport mpfi_get_left, mpfi_get_right, mpfi_interv_fr
+from sage.libs.mpfr cimport mpfr_t, mpfr_init2, mpfr_clear, GMP_RNDN
 from sage.misc.cachefunc import cached_function
 from sage.rings.real_mpfi import RealIntervalField, RealIntervalField_class
 from sage.rings.real_mpfr cimport RealField_class, RealField, RealNumber

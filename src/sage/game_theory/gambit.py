de5e744f8679da196164847b32c8167e8bdec9ff
vince <vincent.knight@gmail.com>
2014-11-24 16:42:45 +0000

    Minor typos etc

--- src/sage/game_theory/gambit.py
+++ src/sage/game_theory/gambit.py
@@ -10,10 +10,11 @@ To install gambit as an optional package run (from root of sage)::
 
 The `python API documentation for gambit
 <http://www.gambit-project.org/gambit14/pyapi.html>_` shows various examples
-however due to the preparser in Sage to use these integers must first be
+however due to the preparser in Sage: integers must first be
 converted to Python integers.
 
-Here is an example showing a construction of the Prisoners Dilemma::
+Here is an example showing a construction and solution of the Prisoners
+Dilemma::
 
     sage: import gambit
     sage: g = gambit.Game.new_table([2,2])
@@ -49,7 +50,7 @@ Here is a list of various other solvers that can be used:
 
 Note that the above example used an algorithm that enumerates all pure
 strategies. This will fail to find all Nash equilibria in certain games.
-For example here is an implementation of matching pennies::
+For example here is an implementation of Matching Pennies::
 
 
     sage: g = gambit.Game.new_table([2,2])
@@ -65,14 +66,14 @@ For example here is an implementation of matching pennies::
     sage: solver.solve(g)
     []
 
-If we solver this with the `LCP` solver we get the required Nash equilibrium::
+If we solve this with the `LCP` solver we get the expected Nash equilibrium::
 
     sage: solver = gambit.nash.ExternalLCPSolver()
     sage: solver.solve(g)
     [<NashProfile for '': [0.5, 0.5, 0.5, 0.5]>]
 
 
-Here is another example showing the battle of the sexes game::
+Here is another example showing the Battle of the Sexes game::
 
     sage: g = gambit.Game.new_table([2,2])
     sage: g[int(0), int(0)][int(0)] = int(2)
@@ -88,6 +89,9 @@ Here is another example showing the battle of the sexes game::
     [<NashProfile for '': [1.0, 0.0, 1.0, 0.0]>,
      <NashProfile for '': [0.6666666667, 0.3333333333, 0.3333333333, 0.6666666667]>,
      <NashProfile for '': [0.0, 1.0, 0.0, 1.0]>]
+
+Note that all of the above examples can be used in Ipython without the need to
+convert integers to instances of `int`.
 
 AUTHOR:
 

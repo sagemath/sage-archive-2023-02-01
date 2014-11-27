c39b6807ccd7662739eb02e9df5ced511c41ff39
Emmanuel Charpentier <emm.charpentier@free.fr>
2014-11-27 21:43:35 +0100

    Trac #17410: unambiguously doctest longest_path()

--- src/sage/graphs/generic_graph.py
+++ src/sage/graphs/generic_graph.py
@@ -6007,10 +6007,10 @@ class GenericGraph(GenericGraph_pyx):
 
         :trac:`14412`::
 
-            sage: l = [(0, 1), (0, 3), (2, 0)]
+            sage: l = [(0, 1), (0, 3), (2, 0), (3, 4)]
             sage: G = DiGraph(l)
             sage: G.longest_path().edges()
-            [(0, 3, None), (2, 0, None)]
+            [(0, 3, None), (2, 0, None), (3, 4, None)]
         """
         self._scream_if_not_simple()
 

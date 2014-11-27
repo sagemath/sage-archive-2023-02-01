fcc15b40f515eef263f8afdc6c4d848fa28fead0
Emmanuel Charpentier <emm.charpentier@free.fr>
2014-11-27 22:49:33 +0100

    Trac #17410: unambiguously doctest longest_path()
    This version tests the equality of sets of edges.

--- src/sage/graphs/generic_graph.py
+++ src/sage/graphs/generic_graph.py
@@ -6009,8 +6009,9 @@ class GenericGraph(GenericGraph_pyx):
 
             sage: l = [(0, 1), (0, 3), (2, 0), (3, 4)]
             sage: G = DiGraph(l)
-            sage: G.longest_path().edges()
-            [(0, 3, None), (2, 0, None), (3, 4, None)]
+            sage: H = {(0, 3, None), (2, 0, None), (3, 4, None)}
+            sage: bool(H=={x for x in G.longest_path().edges()})
+            True
         """
         self._scream_if_not_simple()
 

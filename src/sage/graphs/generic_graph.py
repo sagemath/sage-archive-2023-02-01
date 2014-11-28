52c5c08ea0e189c25b2513f80a0355ecb421c713
Emmanuel Charpentier <emm.charpentier@free.fr>
2014-11-28 09:44:29 +0100

    Trac #17410: unambiguously doctest longest_path()
    Cosmetics up to reviewer's suggestions

--- src/sage/graphs/generic_graph.py
+++ src/sage/graphs/generic_graph.py
@@ -6009,8 +6009,8 @@ class GenericGraph(GenericGraph_pyx):
 
             sage: l = [(0, 1), (0, 3), (2, 0), (3, 4)]
             sage: G = DiGraph(l)
-            sage: H = {(0, 3, None), (2, 0, None), (3, 4, None)}
-            sage: bool(H=={x for x in G.longest_path().edges()})
+            sage: H = {(0, 3), (2, 0), (3, 4)}
+            sage: H=={x for x in G.longest_path().edges(labels=False)}
             True
         """
         self._scream_if_not_simple()

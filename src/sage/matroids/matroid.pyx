ff68516e2ce63ac8a8d6de57ec9490e9b05c9ad3
Travis Scrimshaw <tscrim at ucdavis.edu>
2014-12-12 18:23:09 -0800

    Speedup k-closure by using python sets.

--- src/sage/matroids/matroid.pyx
+++ src/sage/matroids/matroid.pyx
@@ -1938,7 +1938,7 @@ cdef class Matroid(SageObject):
             raise ValueError("input X is not a subset of the groundset.")
         return self._is_closed(frozenset(X))
 
-    cpdef is_subset_k_closed(self, X, k):
+    cpdef is_subset_k_closed(self, X, int k):
         r"""
         Test if ``X`` is a ``k``-closed set of the matroid.
 
@@ -1990,7 +1990,7 @@ cdef class Matroid(SageObject):
             return self.is_closed(X)
 
         cdef frozenset cl
-        for T in Subsets(X, k):
+        for T in combinations(X, k):
             cl = self.closure(T)
             if not cl.issubset(T):
                 return False
@@ -4333,7 +4333,7 @@ cdef class Matroid(SageObject):
 
     # matroid k-closed
 
-    cpdef is_k_closed(self, k):
+    cpdef is_k_closed(self, int k):
         r"""
         Return if ``self`` is a ``k``-closed matroid.
 
@@ -4358,9 +4358,13 @@ cdef class Matroid(SageObject):
             sage: M.is_k_closed(4)
             True
         """
-        for S in subsets(self.groundset()):
-            if self.is_subset_k_closed(S, k) and not self.is_closed(S):
-                return False
+        import itertools
+        G = self.groundset()
+        cdef int m
+        for m in range(len(G)+1):
+            for S in combinations(G, m):
+                if self.is_subset_k_closed(S, k) and not self.is_closed(S):
+                    return False
         return True
 
     # optimization

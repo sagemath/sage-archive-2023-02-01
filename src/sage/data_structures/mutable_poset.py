6d27fbc78b76c41254d3f87da3cac0c1b1529b14
Daniel Krenn <devel@danielkrenn.at>
2015-01-21 19:19:29 +0100

    create data_structures/mutual_poset.py

---
+++ src/sage/data_structures/mutable_poset.py
@@ -0,0 +1,17 @@
+r"""
+Asymptotic Ring
+"""
+#*****************************************************************************
+# Copyright (C) 2015 Daniel Krenn <dev@danielkrenn.at>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                http://www.gnu.org/licenses/
+#*****************************************************************************
+
+import sage
+
+
+# *****************************************************************************
+

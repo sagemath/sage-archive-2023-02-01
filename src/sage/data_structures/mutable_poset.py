dcdc59a075651b589df89dca2d683786c3ff043b
Daniel Krenn <devel@danielkrenn.at>
2015-01-21 19:22:16 +0100

    create datastructure MutablePoset

--- src/sage/data_structures/mutable_poset.py
+++ src/sage/data_structures/mutable_poset.py
@@ -15,3 +15,243 @@ import sage
 
 # *****************************************************************************
 
+
+class MutablePosetElement(sage.structure.sage_object.SageObject):
+    r"""
+    An element of a mutable poset.
+    """
+    def __init__(self, poset, value):
+        r"""
+        See :class:`MutablePosetElement` for details.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: P = MP()
+            sage: from sage.data_structures.mutable_poset import MutablePosetElement
+            sage: MutablePosetElement(P, (1, 2))
+            (1, 2)
+        """
+        self._poset_ = poset
+        self._value_ = value
+        self.predecessors = set()
+        self.successors = set()
+
+
+    @property
+    def poset(self):
+        r"""
+        The poset to which the element belongs.
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        A poset.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: P = MP()
+            sage: from sage.data_structures.mutable_poset import MutablePosetElement
+            sage: e = MutablePosetElement(P, (1, 2))
+            sage: e.poset is P
+            True
+        """
+        return self._poset_
+
+
+    @property
+    def value(self):
+        r"""
+        The value of the element.
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        An object.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: P = MP()
+            sage: from sage.data_structures.mutable_poset import MutablePosetElement
+            sage: e = MutablePosetElement(P, (1, 2))
+            sage: e.value
+            (1, 2)
+        """
+        return self._value_
+
+
+    def __repr__(self):
+        r"""
+        Return the representation of the element.
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        A string.
+
+        This methods usually returns the representation string of its
+        :meth:`value`. The only exception is if this value is
+        ``None``. In this case either ``'zero'`` or ``'oo'`` is
+        returned depending in the nonexistence of predecessors and
+        sucessors respectively.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: P = MP()
+            sage: from sage.data_structures.mutable_poset import MutablePosetElement
+            sage: repr(MutablePosetElement(P, (1, 2)))  # indirect doctest
+            '(1, 2)'
+            sage: repr(P._zero_)  # indirect doctest
+            'zero'
+            sage: repr(P._oo_)  # indirect doctest
+            'oo'
+        """
+        if self.value is None:
+            if not self.predecessors:
+                return 'zero'
+            if not self.successors:
+                return 'oo'
+        return repr(self.value)
+
+
+    def __hash__(self):
+        r"""
+        Return the hash of the element.
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        A hash value.
+
+        This returns the hash value of the element.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: P = MP()
+            sage: from sage.data_structures.mutable_poset import MutablePosetElement
+            sage: hash(MutablePosetElement(P, (1, 2))) == hash((1, 2))
+            True
+        """
+        return hash(self.value)
+
+
+# *****************************************************************************
+
+
+class MutablePoset(sage.structure.sage_object.SageObject):
+    r"""
+    A mutable poset.
+    """
+    def __init__(self, data=None):
+        r"""
+        See :class:`MutablePoset` for details.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: MP()
+            poset()
+        """
+
+        if data is not None:
+            raise NotImplementedError
+
+        self._zero_ = MutablePosetElement(self, None)
+        self._oo_ = MutablePosetElement(self, None)
+        self._zero_.successors.add(self._oo_)
+        self._oo_.predecessors.add(self._zero_)
+        self._elements_ = {}
+
+
+    def iter_elements(self):
+        r"""
+        Return an iterator over all elements.
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        An iterator.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: P = MP()
+            sage: tuple(P.iter_elements())
+            ()
+        """
+        return self._elements_.itervalues()
+
+
+    def iter_all(self):
+        r"""
+        Return an iterator over all elements including a smallest
+        element (`0`) and a largest element (`\infty`).
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        An iterator.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: P = MP()
+            sage: tuple(P.iter_all())
+            (oo, zero)
+        """
+        yield self._oo_
+        for e in self._elements_.itervalues():
+            yield e
+        yield self._zero_
+
+
+    def repr(self):
+        r"""
+        Return a representation of the poset.
+
+        INPUT:
+
+        Nothing.
+
+        OUTPUT:
+
+        A string.
+
+        TESTS::
+
+            sage: from sage.data_structures.mutable_poset import MutablePoset as MP
+            sage: print MP().repr()
+            poset()
+        """
+        s = 'poset('
+        s += ', '.join(repr(element) for element in self.iter_elements())
+        s += ')'
+        return s
+
+
+    __repr__ = repr
+
+
+# *****************************************************************************

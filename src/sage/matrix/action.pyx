437c4e8e481084a2b6414ce2878e2ae12cb98160
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2015-03-02 14:34:14 +0100

    Use coercion instead of pushout for matrix actions

--- src/sage/matrix/action.pyx
+++ src/sage/matrix/action.pyx
@@ -5,7 +5,7 @@ multiplications.
 .. WARNING::
 
     The class :class:`MatrixMulAction` and its descendants extends the class
-    :class:`Action`. As a cosnequence objects from these classes only keep weak
+    :class:`Action`. As a consequence, objects from these classes only keep weak
     references to the underlying sets which are acted upon. This decision was
     made in :trac:`715` in order to allow garbage collection within the coercion
     framework, where actions are mainly used, and avoid memory leaks.
@@ -34,6 +34,16 @@ multiplications.
     guarantee that the set that is acted upon will always be cached in such a
     way, so that following the above example is good practice.
 
+EXAMPLES:
+
+An action requires a common parent for the base rings, so the following
+doesn't work (see :trac:`17859`)::
+
+    sage: vector(QQ, [1]) * matrix(Zmod(2), [[1]])
+    Traceback (most recent call last):
+    ...
+    TypeError: unsupported operand parent(s) for '*': 'Vector space of dimension 1 over Rational Field' and 'Full MatrixSpace of 1 by 1 dense matrices over Ring of integers modulo 2'
+
 AUTHOR:
 
 - Robert Bradshaw (2007-09): Initial version.
@@ -59,8 +69,8 @@ cdef class MatrixMulAction(Action):
         if not is_MatrixSpace(G):
             raise TypeError, "Not a matrix space: %s" % G
         if G.base_ring() is not S.base_ring():
-            from sage.categories.pushout import pushout
-            base = pushout(G.base_ring(), S.base_ring())
+            from sage.structure.element import get_coercion_model
+            base = get_coercion_model().common_parent(G.base_ring(), S.base_ring())
         else:
             base = G.base_ring()
         Action.__init__(self, G, S, is_left, operator.mul)
@@ -169,7 +179,8 @@ cdef class MatrixMatrixAction(MatrixMulAction):
 
         """
         if self.G.ncols() != self.underlying_set().nrows():
-            raise TypeError, "incompatible dimensions %s, %s" % (self.G.ncols(),  self.underlying_set().nrows())
+            raise TypeError("incompatible dimensions %s, %s" %
+                    (self.G.ncols(),  self.underlying_set().nrows()))
         return MatrixSpace(base, self.G.nrows(), self.underlying_set().ncols(),
                            sparse = self.G.is_sparse() and self.underlying_set().is_sparse())
 

3445593fe0907c66e830afd9defdf8b3e5e7dcce
Daniel Bump <bump@match.stanford.edu>
2015-03-19 09:56:17 -0700

    docstring revised

--- src/sage/combinat/root_system/root_lattice_realizations.py
+++ src/sage/combinat/root_system/root_lattice_realizations.py
@@ -3418,12 +3418,11 @@ class RootLatticeRealizations(Category_over_base_ring):
 
             .. warning::
 
-                The current implementation tests that the scalar products
-                with the coroots are all non negative integers, which is not
-                sufficient. For example, if `x` is the sum of a dominant
-                element of the weight lattice plus some other element
-                orthogonal to all coroots, then the current implementation
-                erroneously reports `x` to be a dominant weight::
+                Tests that the scalar products with the coroots are all non
+                negative integers, which is not sufficient. For example, if
+                `x` is the sum of a dominant element of the weight lattice
+                plus some other element orthogonal to all coroots, then the
+                implementation correctly reports `x` to be a dominant weight::
 
                     sage: x = Lambda[1] + L([-1,-1,-1])
                     sage: x.is_dominant_weight()

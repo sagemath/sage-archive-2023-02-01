70e4d3af7d4f26e4cf7fca155ccfe12193e6dae4
Volker Braun <vbraun.name@gmail.com>
2015-03-21 20:59:06 +0100

    fix doctest in symbolic expression show()

--- src/sage/symbolic/expression.pyx
+++ src/sage/symbolic/expression.pyx
@@ -10305,15 +10305,22 @@ cdef class Expression(CommutativeRingElement):
 
     def show(self):
         """
-        Show this symbolic expression, i.e., typeset it nicely.
+        Pretty-Print this symbolic expression
+
+        This typeset it nicely and prints it immediately.
+
+        OUTPUT:
+
+        This method does not return anything. Like ``print``, output
+        is sent directly to the screen.
 
         EXAMPLES::
 
             sage: (x^2 + 1).show()
-            x^{2}  + 1
+            <html><script type="math/tex">\newcommand{\Bold}[1]{\mathbf{#1}}x^{2} + 1</script></html>
         """
-        from sage.misc.functional import _do_show
-        return _do_show(self)
+        from sage.repl.rich_output.pretty_print import pretty_print
+        pretty_print(self)
 
     def plot(self, *args, **kwds):
         """

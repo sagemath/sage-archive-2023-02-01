fefc6c2a8e466e5afaa608be6f3ffe540b21e26f
Martin Rubey <martin.rubey@tuwien.ac.at>
2015-03-23 12:51:24 +0100

    improving documentation:  make doc passes

--- src/sage/databases/findstat.py
+++ src/sage/databases/findstat.py
@@ -1,16 +1,20 @@
 r"""
-This is the Sage interface for the FindStat project at www.findstat.org.
+FindStat - the Combinatorial Statistic Finder.
 
-TODO:
+You can use the sage interface to FindStat to:
 
-- in FindStat, it would be nice if the post keys would coincide with
-  the json keys.  Below is a long list of constants, a good scheme
-  should be worked out.  Possibly it is best to use the database
-  field names themselves everywhere - also in the json templates.
+    - identify a combinatorial statistic from the values on a few small objects.
+    - obtain more terms, formulae, references, etc. for a given statistic.
+    - edit statistics and submit new statistics 
 
-To run all doctests, run
+AUTHORS:
 
-sage -t --optional=sage,internet path/to/interface.sage
+- Martin Rubey (2015): initial version.
+
+EXAMPLES::
+
+
+
 """
 from sage.misc.cachefunc import cached_method
 from sage.misc.misc import verbose
@@ -167,10 +171,12 @@ class FindStat():
 
     def __call__(self, query, collection=None, depth=2, max_values=FINDSTAT_MAX_VALUES):
         r"""
-        Returns an instance of a ``FindStatStatistic``.  This should
-        be the only way to access ``FindStatStatistic``.  We do the
-        preprocessing of the data here, and call the appropriate
-        method of ``FindStatStatistic`` to launch the query.
+        Return an instance of a ``FindStatStatistic``.
+
+        This should be the only way to access ``FindStatStatistic``.
+        We do the preprocessing of the data here, and call the
+        appropriate method of ``FindStatStatistic`` to launch the
+        query.
 
         INPUT:
 
@@ -210,7 +216,7 @@ class FindStat():
             the given order to the object and then computing the
             statistic on the result.
 
-        EXAMPLES::
+        EXAMPLES:
 
         A particular statistic can be called by its St-number or number::
 
@@ -247,7 +253,7 @@ class FindStat():
             0: (St000018: The [[/Permutations/Inversions|number of inversions]] of a permutation., [], 200)
             ...
 
-        To search for a distribution, send a list of lists, or a single pair:
+        To search for a distribution, send a list of lists, or a single pair::
 
             sage: findstat((Permutations(4), [pi.length() for pi in Permutations(4)])) # optional -- internet
             0: (St000004: The [[/Permutations/Descents-Major|major index]] of a permutation., [], 24)
@@ -361,7 +367,7 @@ class FindStat():
 
     def set_user(self, name=None, email=None):
         r"""
-        Sets the user for this session.
+        Set the user for this session.
         """
         if not isinstance(name, str):
             raise ValueError("The given name is not a string")
@@ -372,7 +378,7 @@ class FindStat():
 
     def login(self):
         r"""
-        Open the login page in a browser.
+        Open the FindStat login page in a browser.
         """
         webbrowser.open(FINDSTAT_URL_LOGIN)
 
@@ -382,15 +388,19 @@ class FindStat():
         r"""
 
         INPUT:
+
         - id, an integer designating the FindStat id of a statistic
 
-        OUTUT:
+        OUTPUT:
+
         - a FindStatStatistic instance
 
-        TODO:
-        - this method caches the statistics.  It may make sense to
-        provide a method that clears the cache, or reloads a single
-        statistic
+        TODO::
+
+            this method caches the statistics.  It may make sense to
+            provide a method that clears the cache, or reloads a
+            single statistic.
+
         """
         if id > 0:
             if id not in self._statistic_cache.keys():
@@ -438,7 +448,9 @@ class FindStatStatistic(SageObject):
 
     def __eq__(self, other):
         """
-        TODO: this is *very* rudimentary
+        TODO:: 
+
+            this is *very* rudimentary
         """
         if self._query == "ID" and other._query == "ID":
             if self._modified or other._modified:
@@ -459,9 +471,12 @@ class FindStatStatistic(SageObject):
 
     def _find_by_id(self):
         r"""
+
         expects that _id is a valid identifier
 
-        OUTPUT: self
+        OUTPUT: 
+
+        - self
 
         TESTS::
 
@@ -508,7 +523,9 @@ class FindStatStatistic(SageObject):
         objects, list of values), each containing as many values as
         objects, and that collection is appropriately set
 
-        OUTPUT: self
+        OUTPUT: 
+
+        - self
 
         TESTS::
 
@@ -518,15 +535,24 @@ class FindStatStatistic(SageObject):
             ValueError: The depth must be a non-negative integer less than or equal to 5.
         """
         self._query = "data"
-        # FindStat allows to search for at most FINDSTAT_MAX_VALUES values.
-        data = self._data[:min(max_values, FINDSTAT_MAX_VALUES)]
 
-        # for distribution searches, we cannot simply restrict to the
-        # first values, so we raise an error
+        # FindStat allows to search for at most FINDSTAT_MAX_VALUES
+        # values.  For the user's convenience, from data, we take the
+        # first min(max_values, FINDSTAT_MAX_VALUES) such that all
+        # elements are in the precomputed range
+        data = []
+        total = min(max_values, FINDSTAT_MAX_VALUES)
+        for (elements, values) in self._data:
+            if total >= len(elements):
+                if all(self._collection.in_range(e) for e in elements):
+                    data += [(elements, values)]
+                    total -= len(elements)
+
+        # this might go wrong:
         try:
-            assert sum(len(objects) for objects, values in data) <= FINDSTAT_MAX_VALUES
+            assert data != []
         except:
-            raise NotImplementedError("It is unclear how to restrict distribution searches to at most %s values" %FINDSTAT_MAX_VALUES)
+            raise ValueError("after discarding elements not in the range, and keeping less than %s values, nothing remained to send to FindStat." %FINDSTAT_MAX_VALUES)
 
         url = FINDSTAT_URL_RESULT + self._collection.url_name() + "/"
 
@@ -672,12 +698,12 @@ class FindStatStatistic(SageObject):
 
     def submit(self, max_values=FINDSTAT_MAX_SUBMISSION_VALUES):
         r"""
-        Open the FindStat web page for edits in a browser.
+        Open the FindStat web page for editing the statistic ``self`` in a browser.
 
-        TODO:
+        TODO::
 
-        - decide whether we want to somehow take into account when
-          there is a statistic that matches the data with depth 0
+            decide whether we want to somehow take into account when
+            there is a statistic that matches the data with depth 0
 
         """
         # if the statistic is given as a function, and we have a new
@@ -774,11 +800,11 @@ class FindStatCollection(SageObject):
              lambda x: BinaryTree(str(x))],
         13: [None, None, None, Core,                  lambda x: Cores(x[1], x[0]),
              None,
-             lambda x: x.k(),
+             lambda x: (x.length(), x.k()),
              lambda X: "( "+X._repr_()+", "+str(X.k())+" )",
              lambda x: (lambda pi, k: Core(pi, k))(*literal_eval(x))],
         5:  [None, None, None, DyckWord,              DyckWords,               None,
-             lambda x: x.length(),
+             lambda x: x.length()/2,
              lambda x: str(list(DyckWord(x))),
              lambda x: DyckWord(literal_eval(x))],
         22: [None, None, None, CartanType_abstract,   _finite_irreducible_cartan_types_by_rank,
@@ -788,7 +814,7 @@ class FindStatCollection(SageObject):
              lambda x: CartanType(*literal_eval(str(x)))],
         18: [None, None, None, GelfandTsetlinPattern, lambda x: GelfandTsetlinPatterns(*x),
              None,
-             len,
+             lambda x: (len(x), max(max(row) for row in x)),
              str,
              lambda x: GelfandTsetlinPattern(literal_eval(x))],
         20: [None, None, None, Graph,                 graphs,
@@ -878,18 +904,31 @@ class FindStatCollection(SageObject):
 
     def __init__(self, entry):
         r"""
-        Initializes a FindStat collection from
+        Initialize a FindStat collection.
 
-        * a string, eg. 'Dyck paths' or "DyckPaths"
-        * a sage object
+        INPUT:
 
-        TODO:
+        - a string, eg. 'Dyck paths' or "DyckPaths", case-insensitve
+        - an integer designating the FindStat id of the collection
+        - a sage object belonging to a collection
+        - an iterable producing a sage object belonging to a collection
 
-        * a FindStat collection should also work.
+        TODO::
 
-        TESTS::
+            a FindStat collection should also work.
+
+        EXAMPLES::
 
             sage: FindStatCollection('Dyck paths')                 # optional -- internet
+            Cc0005: Dyck paths
+
+            sage: FindStatCollection(5)                            # optional -- internet
+            Cc0005: Dyck paths
+
+            sage: FindStatCollection(DyckWord([1,0,1,0]))          # optional -- internet
+            Cc0005: Dyck paths
+
+            sage: FindStatCollection(DyckWords(2))                 # optional -- internet
             Cc0005: Dyck paths
 
         """
@@ -975,10 +1014,16 @@ class FindStatCollection(SageObject):
         """
         return self.id() == other.id()
 
+    def in_range(self, element):
+        r"""
+        Check whether an element of the collection is in FindStat's precomputed range.
+        """
+        n = self.to_size()(element)
+        return element in self._sageconstructor(n)
+
     def first_terms(self, statistic, max_values=FINDSTAT_MAX_SUBMISSION_VALUES):
         r"""
-
-        given a statistic, compute the first few terms and return them as a list of pairs
+        Compute the first few terms of the given statistic.
 
         """
         if self._sageconstructor_overridden is None:
@@ -999,38 +1044,69 @@ class FindStatCollection(SageObject):
         return self._url_name
 
     def to_size(self):
-        r"""
+        r""" 
+        Return a function that returns the FindStat size of an object.
 
-        returns a function that returns the FindStat size of a sage object
+        OUTPUT:
+        
+        The function that produces the size as needed by the
+        constructor of the collection.
+
+        TESTS::
+
+            sage: c = FindStatCollection("GelfandTsetlinPatterns")                 # optional -- internet
+            sage: c.to_size()(GelfandTsetlinPattern([[4, 1], [1]]))                # optional -- internet
+            (2, 4)
         """
         return self._to_size
 
 
     def to_string(self):
-        r"""
+        r""" 
+        Return a function that returns the FindStat normal
+        representation given an object.
 
-        returns a function that converts a sage object into a FindStat string
+        OUTPUT:
+        
+        The function that produces the string representation as
+        needed by the FindStat search webpage.
+
+        TESTS::
+
+            sage: p = Poset((range(3), [[0, 1], [1, 2]]))
+            sage: c = FindStatCollection("Posets")                                 # optional -- internet
+            sage: c.to_string()(p)                                                 # optional -- internet
+            '([(0, 2), (2, 1)], 3)'
         """
         return self._to_str
 
     def from_string(self):
         r"""
-        Returns the sage constructor corresponding to the combinatorial collection, or `None`.
+        Return a function that returns the object given the FindStat
+        normal representation.
 
         OUTPUT:
 
-        - string or `None`.
+        The function that produces the sage object given its FindStat
+        normal representation as a string.
+        
+        TESTS::
+
+            sage: c = FindStatCollection("Posets")                                 # optional -- internet
+            sage: p = c.from_string()('([(0, 2), (2, 1)], 3)')                     # optional -- internet
+            sage: p.cover_relations()
+            [[0, 2], [2, 1]]
 
         """
         return self._from_str
 
     def __repr__(self):
         r"""
-        Prints the name of the collection
+        Return the representation of the FindStat collection.
 
         OUTPUT:
 
-        - string.
+        The representation, including the identifier and the name.
 
         EXAMPLES::
 
@@ -1041,29 +1117,29 @@ class FindStatCollection(SageObject):
 
     def name(self):
         r"""
-        returns the name of the collection
+        Return the name of the FindStat collection.
 
         OUTPUT:
 
-        - string.
+        The name of the FindStat collection, in singular.
 
         EXAMPLES::
 
-            sage: FindStatCollection("Binary trees")                      # optional -- internet
-            Cc0010: Binary trees
+            sage: FindStatCollection("Binary trees").name()               # optional -- internet
+            u'Binary tree'
         """
         return self._name
 
 class FindStatMap(SageObject):
     r"""
-    The class of FindStat maps.
+    The class of FindStat maps
 
     """
     _findstat_maps = []
 
     def __init__(self, entry):
         r"""
-        Initializes a FindStat map
+        Initialize a FindStat map.
         """
         if not self._findstat_maps:
             self._findstat_maps += json.load(urlopen(FINDSTAT_URL_DOWNLOADS + "/maps.json"))
@@ -1089,28 +1165,72 @@ class FindStatMap(SageObject):
             raise ValueError, "Could not find FindStat map for " + str(entry)
 
     def id(self):
+        r"""
+        Return the FindStat identifier of the map.
+
+        OUTPUT:
+        
+        The identifier as an integer.
+        """
         return self._map[FINDSTAT_MAP_IDENTIFIER]
 
     def id_str(self):
+        r"""
+        Return the FindStat identifier of the map.
+
+        OUTPUT:
+        
+        The identifier as a string.
+        """
+
         id = str(self.id())
         return 'Mp00000'[:-len(id)] + id
 
     def __repr__(self):
         r"""
-        Prints the name of the map
+        Return the representation of the FindStat map.
         """
         return "%s: %s" %(self.id_str(), self._map[FINDSTAT_MAP_NAME])
 
     def name(self):
+        r"""
+        Return the FindStat name of the map.
+
+        OUTPUT:
+        
+        The name as a string.
+        """
+
         return self._map[FINDSTAT_MAP_NAME]
 
     def description(self):
+        r"""
+        Return the FindStat description of the map.
+
+        OUTPUT:
+        
+        The description as a string.
+        """
         return self._map[FINDSTAT_MAP_DESCRIPTION]
 
     def domain(self):
+        r"""
+        Return the FindStat collection which is the domain of the map.
+
+        OUTPUT:
+        
+        The domain of the map as a ``FindStatCollection``.
+        """
         return FindStatCollection(self._map[FINDSTAT_MAP_DOMAIN])
 
     def codomain(self):
+        r"""
+        Return the FindStat collection which is the codomain of the map.
+
+        OUTPUT:
+        
+        The codomain of the map as a ``FindStatCollection``.
+        """
         return FindStatCollection(self._map[FINDSTAT_MAP_CODOMAIN])
 
 

ff9e4a2dbde2912cee7c11cfced2b677d5eae032
Bryan Gillespie <bgillespie@berkeley.edu>
2015-03-23 20:05:49 -0700

    Updated copyright

--- src/sage/combinat/integer_list.py
+++ src/sage/combinat/integer_list.py
@@ -2,16 +2,9 @@ r"""
 Tools for generating lists of integers in lexicographic order
 """
 #*****************************************************************************
+#       Copyright (C) 2015 Bryan Gillespie <Brg008@gmail.com>,
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
-#
-#    This code is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    General Public License for more details.
-#
-#  The full text of the GPL is available at:
-#
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 

f910ad888bb07e504d95859327ee25a6cd33085c
Marc Mezzarobba <marc@mezzarobba.net>
2015-04-22 14:43:47 +0200

    #17932 misc/flatten.py: fix indentation

--- src/sage/misc/flatten.py
+++ src/sage/misc/flatten.py
@@ -2,20 +2,20 @@
 
 import sys
 def flatten(in_list, ltypes=(list, tuple), max_level=sys.maxsize):
-   """
-   Flattens a nested list.
+    """
+    Flattens a nested list.
 
-   INPUT:
+    INPUT:
 
-   - in_list -- a list or tuple
-   - ltypes -- optional list of particular types to flatten
-   - max_level -- the maximum level to flatten
+    - in_list -- a list or tuple
+    - ltypes -- optional list of particular types to flatten
+    - max_level -- the maximum level to flatten
 
-   OUTPUT:
+    OUTPUT:
 
     a flat list of the entries of in_list
 
-   EXAMPLES::
+    EXAMPLES::
 
        sage: flatten([[1,1],[1],2])
        [1, 1, 1, 2]
@@ -31,18 +31,18 @@ def flatten(in_list, ltypes=(list, tuple), max_level=sys.maxsize):
        [3]
 
 
-   In the following example, the vector isn't flattened because
-   it is not given in the ltypes input. ::
+    In the following example, the vector isn't flattened because
+    it is not given in the ltypes input. ::
 
        sage: flatten((['Hi',2,vector(QQ,[1,2,3])],(4,5,6)))
        ['Hi', 2, (1, 2, 3), 4, 5, 6]
 
-   We give the vector type and then even the vector gets flattened::
+    We give the vector type and then even the vector gets flattened::
 
        sage: flatten((['Hi',2,vector(QQ,[1,2,3])], (4,5,6)), ltypes=(list, tuple,sage.modules.vector_rational_dense.Vector_rational_dense))
        ['Hi', 2, 1, 2, 3, 4, 5, 6]
 
-   We flatten a finite field. ::
+    We flatten a finite field. ::
 
        sage: flatten(GF(5))
        [0, 1, 2, 3, 4]
@@ -51,37 +51,37 @@ def flatten(in_list, ltypes=(list, tuple), max_level=sys.maxsize):
        sage: flatten([GF(5)], ltypes = (list, tuple, sage.rings.finite_rings.finite_field_prime_modn.FiniteField_prime_modn))
        [0, 1, 2, 3, 4]
 
-   Degenerate cases::
+    Degenerate cases::
 
-      sage: flatten([[],[]])
-      []
-      sage: flatten([[[]]])
-      []
-   """
-   index = 0
-   current_level = 0
-   new_list = [x for x in in_list]
-   level_list = [0]*len(in_list)
+        sage: flatten([[],[]])
+        []
+        sage: flatten([[[]]])
+        []
+    """
+    index = 0
+    current_level = 0
+    new_list = [x for x in in_list]
+    level_list = [0]*len(in_list)
 
-   while index < len(new_list):
-      len_v=None
-      while isinstance(new_list[index], ltypes) and current_level<max_level:
-         v = list(new_list[index])
-         len_v = len(v)
-         new_list[index : index + 1] = v
-         old_level = level_list[index]
-         level_list[index : index + 1] = [0]*len_v
-         if len_v != 0:
-            current_level += 1
-            level_list[index + len_v - 1] = old_level + 1
-         else:
-            current_level -= old_level
-            index -=1
-            break
+    while index < len(new_list):
+        len_v=None
+        while isinstance(new_list[index], ltypes) and current_level<max_level:
+            v = list(new_list[index])
+            len_v = len(v)
+            new_list[index : index + 1] = v
+            old_level = level_list[index]
+            level_list[index : index + 1] = [0]*len_v
+            if len_v != 0:
+                current_level += 1
+                level_list[index + len_v - 1] = old_level + 1
+            else:
+                current_level -= old_level
+                index -=1
+                break
 
-      # If len_v==0, then index points to a previous element, so we
-      # don't need to do anything.
-      if len_v!=0:
-         current_level -= level_list[index]
-      index += 1
-   return new_list
+        # If len_v==0, then index points to a previous element, so we
+        # don't need to do anything.
+        if len_v!=0:
+            current_level -= level_list[index]
+        index += 1
+    return new_list

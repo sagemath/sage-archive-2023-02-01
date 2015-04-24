6e81e4b500f7698446fd19de792744a33667eafa
Nicolas M. Thiéry <nthiery@users.sf.net>
2015-04-24 16:01:59 +0200

    16659: proofreading and little additions to the doc; small refactoring of the code

--- src/sage/categories/semisimple_algebras.py
+++ src/sage/categories/semisimple_algebras.py
@@ -116,39 +116,37 @@ class SemisimpleAlgebras(Category_over_base_ring):
                 @cached_method
                 def central_orthogonal_idempotents(self):
                     r"""
-                    Return a maximal list of central orthogonal idempotents of
-                    ``self``.
+                    Return a maximal list of central orthogonal
+                    idempotents of ``self``.
 
-                    Central orthogonal idempotents of an algebra `A` are
-                    idempotents `(e_1, \dots, e_n)` such that `e_i e_j = 0` if
-                    `i \neq j`. Moreover, all `e_i` commute with all elements
-                    of `A` (central).
+                    *Central orthogonal idempotents* of an algebra `A`
+                    are idempotents `(e_1, \dots, e_n)` in the center
+                    of `A` such that `e_i e_j = 0` whenever `i \neq j`.
+
+                    With the maximality condition, they sum up to `1`
+                    and are uniquely determined (up to order).
 
                     INPUT:
 
                     - ``self`` -- a semisimple algebra.
 
-                    OUTPUT:
+                    EXAMPLES:
 
-                    - a complete list of central orthogonal idempotents.
+                    For the algebra of the symmetric group `S_3`, we
+                    recover the sum and alternating sum of all
+                    permutations, together with a third idempotent::
 
-                    EXAMPLES::
-
-                        sage: import itertools
                         sage: A3 = SymmetricGroup(3).algebra(QQ)
-                        sage: orths = A3.central_orthogonal_idempotents(); orths
-                        [2/3*() - 1/3*(1,2,3) - 1/3*(1,3,2), 1/6*() + 1/6*(2,3)
-                        + 1/6*(1,2) + 1/6*(1,2,3) + 1/6*(1,3,2) + 1/6*(1,3),
-                        1/6*() - 1/6*(2,3) - 1/6*(1,2) + 1/6*(1,2,3) +
-                        1/6*(1,3,2) - 1/6*(1,3)]
-                        sage: all(e*e == e for e in orths)
-                        True
-                        sage: all(e*f == f*e and e*f == 0 for e,f in itertools.product(orths, orths) if e != f)
-                        True
-                        sage: all(e*a == a*e for e in orths for a in A3.basis())
+                        sage: idempotents = A3.central_orthogonal_idempotents()
+                        sage: idempotents
+                        [2/3*() - 1/3*(1,2,3) - 1/3*(1,3,2),
+                         1/6*() + 1/6*(2,3) + 1/6*(1,2) + 1/6*(1,2,3) + 1/6*(1,3,2) + 1/6*(1,3),
+                         1/6*() - 1/6*(2,3) - 1/6*(1,2) + 1/6*(1,2,3) + 1/6*(1,3,2) - 1/6*(1,3)]
+                        sage: A3.is_identity_decomposition_into_orthogonal_idempotents(idempotents)
                         True
 
-                    ::
+                    For the semisimple quotient of a quiver algebra,
+                    we recover the vertices of the quiver::
 
                         sage: A = FiniteDimensionalAlgebrasWithBasis(QQ).example(); A
                         An example of a finite dimensional algebra with basis:
@@ -169,16 +167,13 @@ class SemisimpleAlgebras(Category_over_base_ring):
                     @cached_method
                     def _orthogonal_decomposition(self, generators=None):
                         r"""
-                        Return a list of orthogonal quasi-idempotents of a
-                        semisimple commutative finite dimensional algebra
-                        ``self``.
+                        Return a list of orthogonal quasi-idempotents of this
+                        semisimple commutative finite dimensional algebra.
 
                         INPUT:
 
-                        - ``self`` a finite dimensional semisimple commutative
-                          algebra.
-                        - ``generators`` a list of generators of ``self``. By
-                          default it will be the basis of ``self``.
+                        - ``generators`` -- a list of generators of
+                          ``self`` (default: the basis of ``self``)
 
                         OUTPUT:
 
@@ -190,16 +185,18 @@ class SemisimpleAlgebras(Category_over_base_ring):
 
                         ALGORITHM:
 
-                        A commutative semisimple algebra `A` is a direct
-                        sum of dimension 1 sub-algebras thanks to Schur
-                        Lemma. The algorithm is recursive a proceed in two
-                        steps:
+                        Thanks to Schur's Lemma, a commutative
+                        semisimple algebra `A` is a direct sum of
+                        dimension 1 subalgebras. The algorithm is
+                        recursive and proceeds as follows:
 
                         0. If `A` is of dimension 1, return a non zero
                            element.
-                        1. Find a generator `a \in A` such that the
-                           morphism `x \mapsto ax` has at least two (right)
-                           eigenspaces.
+
+                        1. Otherwise: find one of the generators such
+                           that the morphism `x \mapsto ax` has at
+                           least two (right) eigenspaces.
+
                         2. Decompose both eigenspaces recursively.
 
                         EXAMPLES::
@@ -218,21 +215,25 @@ class SemisimpleAlgebras(Category_over_base_ring):
 
                         .. TODO::
 
-                            Improve speed by only using matrix operations, if
-                            possible.
+                            Improve speed by using matrix operations
+                            only, if possible.
                         """
-                        # Main program
+                        category = Algebras(self.base_ring()).Semisimple().WithBasis().FiniteDimensional().Commutative().Subobjects()
                         if generators is None:
                             generators = self.basis().list()
                         if self.dimension() == 1:
                             return self.basis().list()
 
-                        def rec(space, generators):
+                        def rec(space, generators=None):
+                            if generators is None:
+                                generators = space.basis().list()
+                            generators = list(generators)
+
                             if space.dimension() == 1:
                                 return space.basis().list()
                             eigenspaces = []
 
-                            # Searching a good generator...
+                            # Searching for a good generator ...
                             while len(eigenspaces) < 2:
                                 if generators == []:
                                     raise Exception("Unable to fully decompose...")
@@ -241,60 +242,71 @@ class SemisimpleAlgebras(Category_over_base_ring):
                                         gen*space.basis()[i],
                                         codomain=space,
                                         triangular='lower')
-                                eigenspaces = phi.matrix(space.base_ring()).eigenspaces_right()
+                                eigenspaces = phi.matrix().eigenspaces_right()
 
-                            # Gotcha! Let's settle the algebra...
-                            eigenspaces = [vector_space for _, vector_space in eigenspaces]
-                            eigenspaces = [[space.from_vector(vector)
-                                for vector in eigenspace.basis()]
-                                for eigenspace in eigenspaces]
-                            decomp = [space.submodule(v,
-                                category=SemisimpleAlgebras(space.base_ring()).WithBasis().FiniteDimensional().Commutative().Subobjects())
-                                for v in eigenspaces]
+                            # Gotcha! Let's split the algebra ...
+                            subalgebras = \
+                            [space.submodule(map(space.from_vector, eigenspace.basis()),
+                                             category=category)
+                             for eigenvalue, eigenspace in eigenspaces]
 
                             # Decompose recursively each eigenspace
-                            return map(lambda x: x.lift(), [x for eigenspace in
-                                decomp for x in rec(eigenspace, eigenspace.basis().list())])
+                            return [idempotent.lift()
+                                    for subalgebra in subalgebras
+                                    for idempotent in rec(subalgebra)]
 
                         return rec(self, generators)
 
                     @cached_method
                     def central_orthogonal_idempotents(self):
                         r"""
-                        Return the set of central orthogonal idempotents of ``self``.
+                        Return the central orthogonal idempotents of
+                        this semisimple commutative algebra.
 
-                        INPUT:
-
-                        - ``self`` a commutative semisimple algebra.
+                        Those idempotents form a maximal decomposition
+                        of the identity into primitive orthogonal
+                        idempotents.
 
                         OUTPUT:
 
-                        - list of central orthogonal idempotents of ``self``.
-
-                        .. NOTE::
-
-                            All idempotents returned are primitive.
+                        - a list of orthogonal idempotents of ``self``.
 
                         EXAMPLES::
 
-                            sage: import itertools
-                            sage: A5 = SymmetricGroup(5).algebra(QQ)
-                            sage: Z5 = A5.center()
-                            sage: orths = Z5.central_orthogonal_idempotents(); orths
-                            [3/10*B[0] - 1/10*B[2] + 1/20*B[6], 1/120*B[0] +
-                            1/120*B[1] + 1/120*B[2] + 1/120*B[3] + 1/120*B[4] +
-                            1/120*B[5] + 1/120*B[6], 1/120*B[0] - 1/120*B[1] +
-                            1/120*B[2] + 1/120*B[3] - 1/120*B[4] - 1/120*B[5] +
-                            1/120*B[6], 5/24*B[0] + 1/24*B[1] + 1/24*B[2] -
-                            1/24*B[3] + 1/24*B[4] - 1/24*B[5], 5/24*B[0] -
-                            1/24*B[1] + 1/24*B[2] - 1/24*B[3] - 1/24*B[4] +
-                            1/24*B[5], 2/15*B[0] + 1/15*B[1] + 1/30*B[3] -
-                            1/30*B[4] - 1/30*B[6], 2/15*B[0] - 1/15*B[1] +
-                            1/30*B[3] + 1/30*B[4] - 1/30*B[6]]
-                            sage: all(e*e == e for e in orths)
-                            True
-                            sage: all(e*f == f*e and e*f == 0 for e,f in itertools.product(orths, orths) if e != f)
+                            sage: A4 = SymmetricGroup(4).algebra(QQ)
+                            sage: Z4 = A4.center()
+                            sage: idempotents = Z4.central_orthogonal_idempotents()
+                            sage: idempotents
+                            [1/24*B[0] + 1/24*B[1] + 1/24*B[2] + 1/24*B[3] + 1/24*B[4],
+                             3/8*B[0] - 1/8*B[1] - 1/8*B[2] + 1/8*B[4],
+                             1/6*B[0] + 1/6*B[2] - 1/12*B[3],
+                             3/8*B[0] + 1/8*B[1] - 1/8*B[2] - 1/8*B[4],
+                             1/24*B[0] - 1/24*B[1] + 1/24*B[2] + 1/24*B[3] - 1/24*B[4]]
+
+                        Lifting those idempotents from the center, we
+                        recognize among them the sum and alternating
+                        sum of all permutations::
+
+                            sage: [e.lift() for e in idempotents]
+                            [1/24*() + 1/24*(3,4) + 1/24*(2,3) + 1/24*(2,3,4) + 1/24*(2,4,3)
+                             + 1/24*(2,4) + 1/24*(1,2) + 1/24*(1,2)(3,4) + 1/24*(1,2,3)
+                             + 1/24*(1,2,3,4) + 1/24*(1,2,4,3) + 1/24*(1,2,4) + 1/24*(1,3,2)
+                             + 1/24*(1,3,4,2) + 1/24*(1,3) + 1/24*(1,3,4) + 1/24*(1,3)(2,4)
+                             + 1/24*(1,3,2,4) + 1/24*(1,4,3,2) + 1/24*(1,4,2) + 1/24*(1,4,3)
+                             + 1/24*(1,4) + 1/24*(1,4,2,3) + 1/24*(1,4)(2,3),
+                             ...,
+                             1/24*() - 1/24*(3,4) - 1/24*(2,3) + 1/24*(2,3,4) + 1/24*(2,4,3)
+                             - 1/24*(2,4) - 1/24*(1,2) + 1/24*(1,2)(3,4) + 1/24*(1,2,3)
+                             - 1/24*(1,2,3,4) - 1/24*(1,2,4,3) + 1/24*(1,2,4) + 1/24*(1,3,2)
+                             - 1/24*(1,3,4,2) - 1/24*(1,3) + 1/24*(1,3,4) + 1/24*(1,3)(2,4)
+                             - 1/24*(1,3,2,4) - 1/24*(1,4,3,2) + 1/24*(1,4,2) + 1/24*(1,4,3)
+                             - 1/24*(1,4) - 1/24*(1,4,2,3) + 1/24*(1,4)(2,3)]
+
+                        We check that they indeed form a decomposition
+                        of the identity of `Z_4` into orthogonal idempotents::
+
+                            sage: Z4.is_identity_decomposition_into_orthogonal_idempotents(idempotents)
                             True
                         """
-                        return [(e.leading_coefficient()/(e*e).leading_coefficient())*e for
-                            e in self._orthogonal_decomposition()]
+                        return [(e.leading_coefficient()/(e*e).leading_coefficient())*e
+                                for e in self._orthogonal_decomposition()]

1f75112c7b835b8c7cdfdde992d8b86e4e7ab73e
Volker Braun <vbraun.name@gmail.com>
2015-04-24 22:32:13 -0400

    also support arguments that are not literal

--- src/sage/interacts/decorator.py
+++ src/sage/interacts/decorator.py
@@ -14,6 +14,7 @@ The Interact Decorator
 
 
 import inspect
+import textwrap
 from sage.misc.html import html
 
 
@@ -53,8 +54,9 @@ def _sagenb_interact(func, **kwds):
     EXAMPLES::
 
         sage: from sage.interacts.decorator import _sagenb_interact
+        sage: g = Graphics()
         sage: @_sagenb_interact
-        ....: def f(x=[1,2,3]):
+        ....: def f(x=[1,2,3], graph=g):
         ....:     html(x)
         <html>...</html>
         sage: f(2)
@@ -62,8 +64,9 @@ def _sagenb_interact(func, **kwds):
     """
     # Evil hack: can only generate function with given argspec via exec
     argspec = inspect.getargspec(func)
-    from textwrap import dedent
-    func_src = dedent("""
+    defaults = ['_argspec_defaults[{0}]'.format(i) for i in range(len(argspec.defaults))]
+    signature = inspect.formatargspec(argspec[0], argspec[1], argspec[2], defaults)
+    func_src = textwrap.dedent("""
     @sagenb_interact
     def sagenb_func{signature}:
         import sage.misc.html
@@ -73,10 +76,14 @@ def _sagenb_interact(func, **kwds):
         finally:
             sage.misc.html._old_and_deprecated_behavior = False
     """).format(
-        signature=inspect.formatargspec(*argspec),
+        signature=signature,
         args=inspect.formatargspec(argspec[0]),
+        _argspec_defaults=argspec.defaults,
     )
     from sagenb.notebook.interact import interact as sagenb_interact
     globals = dict(func=func, sagenb_interact=sagenb_interact)
     exec func_src in globals
     return globals['sagenb_func']
+
+
+

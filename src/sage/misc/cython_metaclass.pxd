bc4d9573462bba9cf9e37992fc519443ca3fa9fd
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2015-05-06 22:53:15 +0200

    Remove TypeInitMetaclass, add some documentation

--- src/sage/misc/cython_metaclass.pxd
+++ src/sage/misc/cython_metaclass.pxd
@@ -1,5 +1,2 @@
 cdef extern from "sage/misc/cython_metaclass.h":
     PyMethodDescr_CallSelf(desc, self)
-
-cdef class TypeInitMetaclass(type):
-    pass
--- src/sage/misc/cython_metaclass.pyx
+++ src/sage/misc/cython_metaclass.pyx
@@ -49,11 +49,18 @@ In Python, this would be ``meta.__init__(cls, name, bases, dict)``.
     created during the import of the module. Therefore,
     ``__getmetaclass__`` should not refer to any global objects,
     including the type being created or other types defined or imported
-    in the module. Note that ``cdef`` functions are not Python objects,
-    so those are safe to call.
+    in the module (unless you are very careful). Note that non-imported
+    ``cdef`` functions are not Python objects, so those are safe to call.
 
     The same warning applies to the ``__init__`` method of the
     metaclass.
+
+.. WARNING::
+
+    The ``__new__`` method of the metaclass (including the ``__cinit__``
+    method for Cython extension types) is never called if you're using
+    this from Cython. In particular, the metaclass cannot have any
+    attributes or virtual methods.
 
 EXAMPLES::
 
@@ -76,44 +83,6 @@ EXAMPLES::
     sage: class MyPythonType(MyDerivedType):
     ....:     pass
     Calling MyMetaclass.__init__(<class '...MyPythonType'>, 'MyPythonType', (<type '...MyDerivedType'>,), {'__module__': '__main__'})
-
-Application: patching extension types
-=====================================
-
-Sometimes it is needed to "patch" an extension type to implement
-something using the Python C API which cannot be implemented in plain
-Cython. For example, Cython does not allow defining a custom ``tp_new``
-function for a type. Therefore, if we do want a custom ``tp_new``, we
-need to manually change it in the ``PyTypeObject`` structure.
-
-For this, we provide :class:`TypeInitMetaclass`, which is a metaclass
-which calls ``cls.__typeinit__(cls)`` when ``cls`` is being created.
-
-.. code-block:: cython
-
-    cimport sage.misc.cython_metaclass
-    from cpython.object cimport PyTypeObject
-
-    cdef class Foo(object):
-        def __getmetaclass__(_):
-            from sage.misc.cython_metaclass import TypeInitMetaclass
-            return TypeInitMetaclass
-
-        def __typeinit__(cls):
-            if not isinstance(cls, type):
-                raise TypeError
-            (<PyTypeObject*>cls).tp_new = ...
-
-Note that ``__typeinit__`` is inherited as usual, so the class can also
-be a subclass.
-
-.. WARNING::
-
-    ``__typeinit__`` must be defined as ordinary method taking a single
-    argument, but it is called as if it was a class method (this is
-    to work around the way how Cython initializes class methods).
-    To be safe, check ``isinstance(cls, type)`` on the ``cls`` argument
-    before casting it to ``PyTypeObject*``.
 
 Implementation
 ==============
@@ -141,9 +110,6 @@ returns a non-type::
     Traceback (most recent call last):
     ...
     TypeError: __getmetaclass__ did not return a type
-
-Reference
-=========
 """
 
 #*****************************************************************************
@@ -155,58 +121,3 @@ Reference
 # (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
-
-cdef class TypeInitMetaclass(type):
-    """
-    Metaclass which calls ``cls.__typeinit__(cls)`` when ``cls``
-    is initialized.
-
-    For this to work from Cython, you need to add
-    ``cimport sage.misc.cython_metaclass`` in the Cython module.
-
-    EXAMPLES::
-
-        sage: cython('''
-        ....: cimport sage.misc.cython_metaclass
-        ....: from cpython.object cimport PyTypeObject
-        ....:
-        ....: cdef class Foo(object):
-        ....:     def __getmetaclass__(_):
-        ....:         from sage.misc.cython_metaclass import TypeInitMetaclass
-        ....:         return TypeInitMetaclass
-        ....:
-        ....:     def __typeinit__(cls):
-        ....:         if not isinstance(cls, type):
-        ....:             raise TypeError
-        ....:         print "Calling __typeinit__ of", (<PyTypeObject*>cls).tp_name
-        ....:
-        ....: cdef class Bar(Foo):
-        ....:     pass
-        ....: ''')
-        Calling __typeinit__ of ...Foo
-        Calling __typeinit__ of ...Bar
-        sage: class PythonFoo(Foo):
-        ....:     pass
-        Calling __typeinit__ of PythonFoo
-    """
-    def __init__(self, *args):
-        """
-        TESTS:
-
-        We define a ``__typeinit__`` method with an extra argument and
-        check that we get a proper exception::
-
-            sage: cython('''
-            ....: cimport sage.misc.cython_metaclass
-            ....: cdef class BadTypeInit(object):
-            ....:     def __getmetaclass__(_):
-            ....:         from sage.misc.cython_metaclass import TypeInitMetaclass
-            ....:         return TypeInitMetaclass
-            ....:     def __typeinit__(cls, arg):
-            ....:         pass
-            ....: ''')
-            Traceback (most recent call last):
-            ...
-            TypeError: PyMethodDescr_CallSelf requires a method without arguments
-        """
-        PyMethodDescr_CallSelf(self.__typeinit__, self)

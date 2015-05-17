8b2de9197d3785030858defdd7374b65995ad10d
zabrocki <zabrocki@mathstat.yorku.ca>
2015-05-17 07:34:31 -0400

    make the headers nice in the NCSF/QSym section of reference manual

--- src/sage/combinat/ncsf_qsym/__init__.py
+++ src/sage/combinat/ncsf_qsym/__init__.py
@@ -4,7 +4,7 @@ Non-Commutative Symmetric Functions and Quasi-Symmetric Functions
 
 - :ref:`sage.combinat.ncsf_qsym.tutorial`
 
-- :class:`sage.combinat.ncsf_qsym.ncsf.NonCommutativeSymmetricFunctions`
-- :class:`sage.combinat.ncsf_qsym.qsym.QuasiSymmetricFunctions`
+- :ref:`Non-Commutative Symmetric Functions (NCSF) <sage.combinat.ncsf_qsym.ncsf>`
+- :ref:`Quasi-Symmetric Functions (QSym) <sage.combinat.ncsf_qsym.qsym>`
 
 """

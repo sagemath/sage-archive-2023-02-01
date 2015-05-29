3715880126fce2176ca4f12ab700d3e1cf5ea804
David Lucas <david.lucas@inria.fr>
2015-05-29 11:25:24 +0200

    Fix related to encoders_catalog file

---
+++ src/sage/coding/encoders_catalog.py
@@ -0,0 +1,16 @@
+r"""
+Index of encoders
+
+The ``codes.encoders`` object may be used to access the codes that Sage can build.
+
+- :func:`linear_code.LinearCodeGeneratorMatrixEncoder <sage.coding.linear_code.LinearCodeGeneratorMatrixEncoder>
+
+
+.. NOTE::
+
+    To import these names into the global namespace, use:
+
+        sage: from sage.coding.encoders_catalog import *
+"""
+
+from linear_code import (LinearCodeGeneratorMatrixEncoder)

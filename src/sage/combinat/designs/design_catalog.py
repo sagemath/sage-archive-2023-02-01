10436405b88a88dd147ca5283a61c31f0c475aee
Quentin Honoré <quentinhonore3@gmail.com>
2015-06-04 09:02:43 +0200

    Add HughesPlane to the catalog

--- src/sage/combinat/designs/design_catalog.py
+++ src/sage/combinat/designs/design_catalog.py
@@ -39,6 +39,7 @@ This module gathers the following designs :
 
     :meth:`~sage.combinat.designs.block_design.ProjectiveGeometryDesign`
     :meth:`~sage.combinat.designs.block_design.DesarguesianProjectivePlaneDesign`
+    :meth:`~sage.combinat.designs.block_design.HughesPlane`
     :meth:`~sage.combinat.designs.bibd.balanced_incomplete_block_design`
     :meth:`~sage.combinat.designs.resolvable_bibd.resolvable_balanced_incomplete_block_design`
     :meth:`~sage.combinat.designs.resolvable_bibd.kirkman_triple_system`

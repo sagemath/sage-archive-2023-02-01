c30cf70daf27791feb128534cb1fad4d5b0dd652
R. Andrew Ohana <andrew.ohana@gmail.com>
2015-06-07 20:08:32 -0700

    Revert "move polybori python interface into sage"
    This reverts commit a05ce2d0ca39f83c5579823746fceef042c659a1.

--- src/sage/sat/converters/__init__.py
+++ src/sage/sat/converters/__init__.py
@@ -1,4 +1,2 @@
-from __future__ import absolute_import
-
-from .anf2cnf import ANF2CNFConverter
-from .polybori import CNFEncoder as PolyBoRiCNFEncoder
+from anf2cnf import ANF2CNFConverter
+from polybori import CNFEncoder as PolyBoRiCNFEncoder

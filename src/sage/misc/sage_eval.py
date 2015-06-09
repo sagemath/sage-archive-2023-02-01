7e60f88fba8805110e89fa2f7093d61a2d78542d
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2015-06-10 00:21:44 +0200

    prevent relative import of six

--- src/sage/misc/sage_eval.py
+++ src/sage/misc/sage_eval.py
@@ -10,6 +10,7 @@ Evaluating a String in Sage
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
+from __future__ import absolute_import
 import six
 from copy import copy
 import sage.repl.preparse as preparser

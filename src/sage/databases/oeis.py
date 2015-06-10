8c683186cad731a09200d84fbf44ab3c4e658d61
Thierry Monteil <sage@lma.metelu.net>
2015-06-10 19:41:03 +0200

    #16252 no space between doctest options.

--- src/sage/databases/oeis.py
+++ src/sage/databases/oeis.py
@@ -1640,10 +1640,10 @@ class OEISSequence(SageObject):
 
         EXAMPLES::
 
-            sage: f = oeis(45) ; f                      # optional -- internet, webbrowser
+            sage: f = oeis(45) ; f                      # optional -- internet webbrowser
             A000045: Fibonacci numbers: F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
 
-            sage: f.browse()                            # optional -- internet, webbrowser
+            sage: f.browse()                            # optional -- internet webbrowser
 
         TESTS::
 

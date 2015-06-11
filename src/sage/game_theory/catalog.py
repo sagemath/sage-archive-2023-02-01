8b109ba4b4f12471fc277e6d09086b071b7292af
Vince Knight <vincent.knight@gmail.com>
2015-06-11 10:33:02 +0100

    Have catalog working as wanted
    - Tests fail
    - Some objects appear in tab completion that I don't think we want...
      Not sure about this.

---
+++ src/sage/game_theory/catalog.py
@@ -0,0 +1,15 @@
+r"""
+Catalog Of Games
+
+Catalog
+-------
+
+This is a catalog of games that are currently implemented in Sage:
+
+Subcatalogs:
+
+* :ref:`sage.game_theory.catalog_normal_form_games`
+
+"""
+
+import catalog_normal_form_games as normal_form_games

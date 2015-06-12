f4955c5f50d4f34510176acff8956f49d3b8702a
Travis Scrimshaw <tscrim at ucdavis.edu>
2015-06-12 10:01:34 -0700

    Fixing RC crystals for extended and non-extended weight input.

--- src/sage/combinat/rigged_configurations/rc_crystal.py
+++ src/sage/combinat/rigged_configurations/rc_crystal.py
@@ -91,7 +91,7 @@ class CrystalOfRiggedConfigurations(Parent, UniqueRepresentation):
        *A rigged configuration model for* `B(\infty)`. :arxiv:`1404.6539`.
     """
     @staticmethod
-    def __classcall_private__(cls, cartan_type, wt=None):
+    def __classcall_private__(cls, cartan_type, wt=None, WLR=None):
         r"""
         Normalize the input arguments to ensure unique representation.
 
@@ -100,8 +100,16 @@ class CrystalOfRiggedConfigurations(Parent, UniqueRepresentation):
             sage: La = RootSystem(['A', 2]).weight_lattice().fundamental_weights()
             sage: RC = crystals.RiggedConfigurations(La[1])
             sage: RC2 = crystals.RiggedConfigurations(['A', 2], La[1])
-            sage: RC is RC2
+            sage: RC3 = crystals.RiggedConfigurations(['A', 2], La[1], La[1].parent())
+            sage: RC is RC2 and RC2 is RC3
             True
+
+            sage: La = RootSystem(['A',2,1]).weight_lattice().fundamental_weights()
+            sage: LaE = RootSystem(['A',2,1]).weight_lattice(extended=True).fundamental_weights()
+            sage: RC = crystals.RiggedConfigurations(La[1])
+            sage: RCE = crystals.RiggedConfigurations(LaE[1])
+            sage: RC is RCE
+            False
         """
         if wt is None:
             wt = cartan_type
@@ -109,13 +117,18 @@ class CrystalOfRiggedConfigurations(Parent, UniqueRepresentation):
         else:
             cartan_type = CartanType(cartan_type)
 
+        if WLR is None:
+            WLR = wt.parent()
+        else:
+            wt = WLR(wt)
+
         if not cartan_type.is_simply_laced():
             vct = cartan_type.as_folding()
-            return CrystalOfNonSimplyLacedRC(vct, wt)
+            return CrystalOfNonSimplyLacedRC(vct, wt, WLR)
 
-        return super(CrystalOfRiggedConfigurations, cls).__classcall__(cls, wt)
+        return super(CrystalOfRiggedConfigurations, cls).__classcall__(cls, wt, WLR=WLR)
 
-    def __init__(self, wt):
+    def __init__(self, wt, WLR):
         r"""
         Initialize ``self``.
 
@@ -129,7 +142,7 @@ class CrystalOfRiggedConfigurations(Parent, UniqueRepresentation):
             sage: RC = crystals.RiggedConfigurations(La[0])
             sage: TestSuite(RC).run() # long time
         """
-        self._cartan_type = wt.parent().cartan_type()
+        self._cartan_type = WLR.cartan_type()
         self._wt = wt
         self._rc_index = self._cartan_type.index_set()
         # We store the cartan matrix for the vacancy number calculations for speed
@@ -240,7 +253,7 @@ class CrystalOfNonSimplyLacedRC(CrystalOfRiggedConfigurations):
     """
     Highest weight crystal of rigged configurations in non-simply-laced type.
     """
-    def __init__(self, vct, wt):
+    def __init__(self, vct, wt, WLR):
         """
         Initialize ``self``.
 
@@ -251,7 +264,7 @@ class CrystalOfNonSimplyLacedRC(CrystalOfRiggedConfigurations):
             sage: TestSuite(RC).run()
          """
         self._folded_ct = vct
-        CrystalOfRiggedConfigurations.__init__(self, wt)
+        CrystalOfRiggedConfigurations.__init__(self, wt, WLR)
 
     @lazy_attribute
     def virtual(self):

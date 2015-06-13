a0124fb056aa8880f1b85af7ff095fa74fabb936
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2015-06-13 14:53:50 +0200

    trac #18442 first review commit

--- src/sage/geometry/polyhedron/base.py
+++ src/sage/geometry/polyhedron/base.py
@@ -2643,7 +2643,7 @@ class Polyhedron_base(Element):
 
     __and__ = intersection
 
-    def edge_truncation(self, cut_frac = Integer(1)/3):
+    def edge_truncation(self, cut_frac=None):
         r"""
         Return a new polyhedron formed from two points on each edge
         between two vertices.
@@ -2666,6 +2666,9 @@ class Polyhedron_base(Element):
             sage: trunc_cube.n_inequalities()
             14
         """
+        if cut_frac is None:
+            cut_frac = ZZ.one() / 3
+
         new_vertices = []
         for e in self.bounded_edges():
             new_vertices.append((1-cut_frac)*e[0]() + cut_frac *e[1]())
@@ -2679,15 +2682,17 @@ class Polyhedron_base(Element):
                           lines=new_lines,
                           base_ring=self.parent()._coerce_base_ring(cut_frac))
 
-    def barycentric_subdivision(self, subdivision_frac=Integer(1)/3):
+    def barycentric_subdivision(self, subdivision_frac=None):
         r"""
-        Return the barycentric subdivision of a compact polyhedron
+        Return the barycentric subdivision of a compact polyhedron.
+
+        DEFINITION ? REFERENCE ?
 
         INPUT:
 
         - ``subdivision_frac`` -- number. Gives the proportion how far the new
           vertices are pulled out of the polytope. Default is `\frac{1}{3}` and
-          the value should be smaller than `\frac{1}{2}`. The subdivision is 
+          the value should be smaller than `\frac{1}{2}`. The subdivision is
           computed on the polar polyhedron.
 
         OUTPUT:
@@ -2696,18 +2701,22 @@ class Polyhedron_base(Element):
 
         EXAMPLES::
 
-            sage: P=polytopes.hypercube(3)
+            sage: P = polytopes.hypercube(3)
             sage: P.barycentric_subdivision()
-            A 3-dimensional polyhedron in QQ^3 defined as the convex hull of 26 vertices
-            sage: P=Polyhedron(vertices=[[0,0,0],[0,1,0],[1,0,0],[0,0,1]])
+            A 3-dimensional polyhedron in QQ^3 defined as the convex hull
+            of 26 vertices
+            sage: P = Polyhedron(vertices=[[0,0,0],[0,1,0],[1,0,0],[0,0,1]])
             sage: P.barycentric_subdivision()
-            A 3-dimensional polyhedron in QQ^3 defined as the convex hull of 14 vertices
-            sage: P=Polyhedron(vertices=[[0,1,0],[0,0,1],[1,0,0]])
+            A 3-dimensional polyhedron in QQ^3 defined as the convex hull
+            of 14 vertices
+            sage: P = Polyhedron(vertices=[[0,1,0],[0,0,1],[1,0,0]])
             sage: P.barycentric_subdivision()
-            A 2-dimensional polyhedron in QQ^3 defined as the convex hull of 6 vertices
-            sage: P=polytopes.regular_polygon(5)
+            A 2-dimensional polyhedron in QQ^3 defined as the convex hull
+            of 6 vertices
+            sage: P = polytopes.regular_polygon(5, base_ring=RDF)
             sage: P.barycentric_subdivision()
-            A 2-dimensional polyhedron in AA^2 defined as the convex hull of 10 vertices
+            A 2-dimensional polyhedron in RDF^2 defined as the convex hull
+            of 10 vertices
 
         TESTS::
 
@@ -2715,50 +2724,55 @@ class Polyhedron_base(Element):
             Traceback (most recent call last):
             ...
             ValueError: The subdivision fraction should be between 0 and 1/2.
-            sage: P=Polyhedron(ieqs=[[1,0,1],[0,1,0],[1,0,0],[0,0,1]])
+            sage: P = Polyhedron(ieqs=[[1,0,1],[0,1,0],[1,0,0],[0,0,1]])
             sage: P.barycentric_subdivision()
             Traceback (most recent call last):
             ...
             ValueError: The polytope has to be compact.
         """
+        if subdivision_frac is None:
+            subdivision_frac = ZZ.one() / 3
 
         if not self.is_compact():
             raise ValueError("The polytope has to be compact.")
-        if not (subdivision_frac < Integer(1)/2 and subdivision_frac > 0):
-            raise ValueError("The subdivision fraction should be between 0 and 1/2.")
+        if not (0 < subdivision_frac < ZZ.one() / 2):
+            raise ValueError("The subdivision fraction should be "
+                             "between 0 and 1/2.")
 
+        b_ring = self.parent()._coerce_base_ring(subdivision_frac)
         barycenter = self.center()
 
         ambient_dim = self.ambient_dim()
         polytope_dim = self.dimension()
 
         if ambient_dim != polytope_dim:
-            start_polar = Polyhedron((self-barycenter).polar().vertices())
-            polar = Polyhedron((self-barycenter).polar().vertices())
+            start_polar = Polyhedron((self - barycenter).polar().vertices())
+            polar = Polyhedron((self - barycenter).polar().vertices())
         else:
-            start_polar = (self-barycenter).polar()
-            polar = (self-barycenter).polar()
+            start_polar = (self - barycenter).polar()
+            polar = (self - barycenter).polar()
 
         for i in range(self.dimension() - 1):
 
             new_ineq = []
             subdivided_faces = list(start_polar.faces(i))
             Hrep = polar.Hrepresentation()
-            
+
             for face in subdivided_faces:
-            
+
                 face_vertices = face.vertices()
                 normal_vectors = []
-                
+
                 for facet in Hrep:
-                    if False not in map(lambda x: facet.contains(x) and not facet.interior_contains(x), face_vertices):
+                    if all(facet.contains(v) and not facet.interior_contains(v)
+                           for v in face_vertices):
                         # The facet contains the face
                         normal_vectors.append(facet.A())
 
                 normal_vector = sum(normal_vectors)
                 B = - normal_vector * (face_vertices[0].vector())
-                linear_evaluation = set([-normal_vector * (v.vector()) for v in
-                    polar.vertices()])
+                linear_evaluation = set([-normal_vector * (v.vector())
+                                         for v in polar.vertices()])
 
                 if B == max(linear_evaluation):
                     C = max(linear_evaluation.difference(set([B])))
@@ -2771,15 +2785,13 @@ class Polyhedron_base(Element):
             new_ieqs = polar.inequalities_list() + new_ineq
             new_eqns = polar.equations_list()
 
-            polar = Polyhedron(ieqs=new_ieqs, eqns=new_eqns, base_ring=
-                self.parent()._coerce_base_ring(subdivision_frac))
+            polar = Polyhedron(ieqs=new_ieqs, eqns=new_eqns,
+                               base_ring=b_ring)
 
         if ambient_dim != polytope_dim:
-            barycentric_sub = (Polyhedron(polar.polar().vertices()))+barycenter
+            return (Polyhedron(polar.polar().vertices())) + barycenter
         else:
-            barycentric_sub = (polar.polar())+barycenter
-
-        return barycentric_sub
+            return (polar.polar()) + barycenter
 
     def _make_polyhedron_face(self, Vindices, Hindices):
         """

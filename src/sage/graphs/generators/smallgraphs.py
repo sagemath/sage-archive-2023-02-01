ce460ff8d0ef5d2c68859870a524ed09f9b93c26
David Coudert <david.coudert@inria.fr>
2015-06-14 19:32:41 +0200

    trac #18702: Thomsen Graph

--- src/sage/graphs/generators/smallgraphs.py
+++ src/sage/graphs/generators/smallgraphs.py
@@ -4096,10 +4096,9 @@ def ThomsenGraph():
         'EFz_'
         sage: (graphs.ThomsenGraph()).show() # long time
     """
+    edges = {0:[3, 4, 5], 1:[3, 4, 5], 2:[3, 4, 5]}
     pos_dict = {0:(-1,1),1:(0,1),2:(1,1),3:(-1,0),4:(0,0),5:(1,0)}
-    import networkx
-    G = networkx.complete_bipartite_graph(3,3)
-    return Graph(G, pos=pos_dict, name="Thomsen graph")
+    return Graph(edges, pos=pos_dict, name="Thomsen graph")
 
 def TietzeGraph():
     r"""

2682fdaed250a214bd584e6f963d8cccc8c81fc3
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2015-06-17 18:21:30 +0200

    Format exceptions raised by setup.py like errors in sage-spkg

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,39 @@ from __future__ import print_function
 import os, sys, time, errno, platform, subprocess, glob
 from distutils.core import setup
 
+
+def excepthook(*exc):
+    """
+    When an error occurs, display an error message similar to the error
+    messages from ``sage-spkg``.
+
+    In particular, ``build/install`` will recognize "sage" as a failed
+    package, see :trac:`16774`.
+    """
+    stars = '*' * 72
+
+    print(stars, file=sys.stderr)
+    import traceback
+    traceback.print_exception(*exc, file=sys.stderr)
+    print(stars, file=sys.stderr)
+    print("Error building the Sage library", file=sys.stderr)
+    print(stars, file=sys.stderr)
+
+    try:
+        logfile = os.path.join(os.environ['SAGE_LOGS'],
+                "sage-%s.log" % os.environ['SAGE_VERSION'])
+    except:
+        pass
+    else:
+        print("Please email sage-devel (http://groups.google.com/group/sage-devel)", file=sys.stderr)
+        print("explaining the problem and including the relevant part of the log file", file=sys.stderr)
+        print("  " + logfile, file=sys.stderr)
+        print("Describe your computer, operating system, etc.", file=sys.stderr)
+        print(stars, file=sys.stderr)
+
+sys.excepthook = excepthook
+
+
 #########################################################
 ### List of Extensions
 ###

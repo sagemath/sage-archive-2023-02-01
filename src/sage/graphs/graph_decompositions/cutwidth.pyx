cbad95aafc4f99deca7a9081eaf1a6590651dbcd
David Coudert <david.coudert@inria.fr>
2015-06-20 11:21:59 +0200

    trac #18746: Dynamic programming algorithm

--- src/sage/graphs/graph_decompositions/cutwidth.pyx
+++ src/sage/graphs/graph_decompositions/cutwidth.pyx
@@ -69,7 +69,11 @@ Methods
 #                        http://www.gnu.org/licenses/
 #*****************************************************************************
 
-
+include 'sage/ext/stdsage.pxi'
+include 'sage/ext/interrupt.pxi'
+include 'fast_digraph.pyx'
+from libc.stdint cimport uint8_t
+from sage.ext.memory cimport check_allocarray
 
 
 ################################################################################
@@ -152,3 +156,192 @@ def width_of_cut_decomposition(G, L):
     return max(cpt)
 
 
+################################################################################
+# Dynamic Programming algorithm for cutwidth
+################################################################################
+
+def cutwidth_dyn(G, lower_bound=0, verbose=False):
+    """
+    Dynamic programming algorithm for the cutwidth of a Graph.
+
+    This function uses dynamic programming algorithm for determining an optimal
+    layout for the cutwidth of `G`. See the :mod:`module's documentation
+    <sage.graphs.graph_decompositions.cutwidth>` for more details on this
+    method.
+
+    INPUT:
+
+    - ``G`` -- a Graph
+
+    - ``lower_bound`` -- (default: 0) the algorithm returns a solution with cost
+      larger or equal to that bound.
+
+    - ``verbose`` (boolean) -- whether to display information on the
+      computations.
+
+    OUTPUT:
+
+    A pair ``(cost, ordering)`` representing the optimal ordering of the
+    vertices and its cost.
+    
+    .. NOTE::
+
+        Because of its current implementation, this algorithm only works on
+        graphs on strictly less than 32 vertices. This can be changed to 63 if
+        necessary, but 32 vertices already require 4GB of memory.
+
+    TESTS:
+
+    Giving anything else than a Graph::
+
+        sage: from sage.graphs.graph_decompositions import cutwidth
+        sage: cutwidth.cutwidth_dyn([])
+        Traceback (most recent call last):
+        ...
+        ValueError: The parameter must be a Graph.
+
+    Giving a too large Graph::
+
+        sage: from sage.graphs.graph_decompositions import cutwidth
+        sage: cutwidth.cutwidth_dyn(graphs.PathGraph(40))
+        Traceback (most recent call last):
+        ...
+        ValueError: The graph should have at most 31 vertices !
+
+    Giving a wrong type lower bound::
+
+        sage: from sage.graphs.graph_decompositions import cutwidth
+        sage: cutwidth.cutwidth_dyn(Graph(), lower_bound='toto')
+        Traceback (most recent call last):
+        ...
+        ValueError: The specified lower bound must be an integer.
+    """
+    from sage.graphs.graph import Graph
+    from sage.rings.integer_ring import ZZ
+    if not isinstance(G, Graph):
+        raise ValueError("The parameter must be a Graph.")
+
+    if G.order() >= 32:
+        raise ValueError("The graph should have at most 31 vertices !")
+
+    if not lower_bound in ZZ:
+        raise ValueError("The specified lower bound must be an integer.")
+
+    cdef FastDigraph g = FastDigraph(G)
+
+    cdef unsigned int mem = 1 << g.n
+    cdef uint8_t * neighborhoods = <uint8_t *> check_allocarray(mem, sizeof(uint8_t))
+    
+    memset(neighborhoods, <uint8_t> -1, mem)
+
+    cdef int i,j , k
+
+    sig_on()
+    for k in range(lower_bound, G.size()+1):
+        if exists(g, neighborhoods, 0, k) <= k:
+            break
+    sig_off()
+
+    cdef list order = find_order(g, neighborhoods, k)
+
+    sage_free(neighborhoods)
+
+    return k, list( g.int_to_vertices[i] for i in order )
+
+
+cdef inline int compute_edge_cut(FastDigraph g, int S):
+    r"""
+    Returns the number of edges from `S` to `V\S`.
+
+    INPUT:
+
+    - ``g`` -- a FastDigraph
+    
+    - ``S`` -- (integer) an integer describing a set of vertices
+    """
+    cdef int Sbar = ~S
+    cdef int i
+    cdef int cpt = 0
+    for i in range(g.n):
+        cpt += popcount32( (Sbar&g.graph[i]) * ((S>>i)&1) )
+
+    return cpt
+
+
+cdef inline int exists(FastDigraph g, uint8_t * neighborhoods, int current, int cost):
+    """
+    Check whether an ordering with the given cost exists, and updates data in
+    the neighborhoods array at the same time. See the module's documentation.
+
+    This method differs from method
+    `sage.graphs.graph_decompositions.vertex_separation.exists` by a single
+    line. We can certainly combine codes.
+    """
+    # If this is true, it means the set has not been evaluated yet
+    if neighborhoods[current] == <uint8_t>-1:
+        neighborhoods[current] = compute_edge_cut(g, current)
+
+    # If the cost of this set is too high, there is no point in going further.
+    # Same thing if the current set is the whole vertex set.
+    if neighborhoods[current] > cost or (current == (1<<g.n)-1):
+        return neighborhoods[current]
+
+    # Minimum of the costs of the outneighbors
+    cdef int mini = (1<<g.n)-1
+
+    cdef int i
+    cdef int next_set
+
+    for i in range(g.n):
+        if (current >> i)&1:
+            continue
+
+        # For each of the out-neighbors next_set of current
+        next_set = current | 1<<i
+
+        # Check whether there exists a cheap path toward {1..n}, and updated the
+        # cost.
+        mini = min(mini, exists(g, neighborhoods, next_set, cost))
+
+        # We have found a path !
+        if mini <= cost:
+            return mini
+
+    # Updating the cost of the current set with the minimum of the cost of its
+    # outneighbors.
+    neighborhoods[current] = mini
+
+    return neighborhoods[current]
+
+
+cdef list find_order(FastDigraph g, uint8_t * neighborhoods, int cost):
+    """
+    Returns the ordering once we are sure it exists
+
+    This is a copy/paste of method
+    `sage.graphs.graph_decompositions.vertex_separation.find_order` and so we
+    can certainly directly import it.
+    """
+    cdef list ordering = []
+    cdef int current = 0
+    cdef int n = g.n
+    cdef int i
+
+    while n:
+        # We look for n vertices
+
+        for i in range(g.n):
+            if (current >> i)&1:
+                continue
+
+            # Find the next set with small cost (we know it exists)
+            next_set = current | 1<<i
+            if neighborhoods[next_set] <= cost:
+                ordering.append(i)
+                current = next_set
+                break
+
+        # One less to find
+        n -= 1
+
+    return ordering

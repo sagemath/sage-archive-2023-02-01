582d1dc602aedbd81900fc7ec860a40cd8f8ff8f
Vince Knight <vincent.knight@gmail.com>
2015-06-20 19:26:32 +0100

    Revert "Revert "Changing an import to a lazy import.""
    This reverts commit 215dac5c369238eb276a89a82eca75111669f461.

--- src/sage/game_theory/all.py
+++ src/sage/game_theory/all.py
@@ -1,7 +1,6 @@
 from sage.misc.lazy_import import lazy_import
 
-import catalog as game_theory
-
+lazy_import('sage.game_theory', 'catalog', 'game_theory')
 lazy_import('sage.game_theory.cooperative_game', 'CooperativeGame')
-lazy_import('sage.game_theory.normal_form_game', 'NormalFormGame')
 lazy_import('sage.game_theory.matching_game', 'MatchingGame')
+lazy_import('sage.game_theory.normal_form_game', 'NormalFormGame')

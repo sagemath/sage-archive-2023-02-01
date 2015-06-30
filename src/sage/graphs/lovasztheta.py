83676aa53b5c605338fca458fb5aadfdbf212c74
Dima Pasechnik <dimpase@gmail.com>
2015-06-30 16:02:34 +0100

    implementation using csdp's dedicated theta solver

---
+++ src/sage/graphs/lovasztheta.py
@@ -0,0 +1,59 @@
+r"""
+Computing Lovazs theta-function of graphs
+
+AUTHORS:
+
+- Dima Pasechnik (2015-06-30): Initial version
+
+REFERENCE:
+
+.. [Lovasz1979] Laszlo Lovasz,
+  "On the Shannon capacity of a graph",
+  IEEE Trans. Inf. Th. 25(1979), 1-7.
+
+Methods
+-------
+"""
+
+def lovasz_theta(graph):
+    """
+    Returns value of Lovasz theta-function of graph 
+
+    Implemented for undirected graphs only. Use
+    to_undirected to convert a digraph to an undirected graph.
+    
+    Currently, the only solver to compute this value is the
+    one from the optional/experimental package csdp, 
+    which you might need to install with 'sage -i csdp'
+
+    EXAMPLES::
+
+          sage: C=graphs.PetersenGraph()
+          sage: lovasz_theta(C)                             # optional csdp
+          4.0
+          sage: lovasz_theta(graphs.CycleGraph(5))          # optional csdp
+          2.236068
+
+    TEST::
+
+        sage: g = Graph()
+        sage: lovasz_theta(g)
+        0
+    """
+    n = graph.order()
+    if n == 0:
+        return 0
+    g = graph.relabel(inplace=False, perm=range(1,n+1)).networkx_graph()
+
+    from networkx import write_edgelist
+    from sage.misc.temporary_file import tmp_filename
+    import os, subprocess
+    from sage.env import SAGE_LOCAL
+    
+    tf_name = tmp_filename()
+    tf = open(tf_name, 'wb')
+    tf.write(str(n)+'\n'+str(g.number_of_edges())+'\n')
+    write_edgelist(g, tf, data=False)
+    tf.close()
+    lines = subprocess.check_output([os.path.join(SAGE_LOCAL, 'bin', 'theta'), tf_name])
+    return float(lines.split()[-1])

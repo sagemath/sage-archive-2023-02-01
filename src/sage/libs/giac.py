fab096e4658fb1300ec56943f9601b7cf9c915ed
Frederic HAN <frederic.han@imj-prg.fr>
2015-06-30 19:49:35 +0200

    test import, gens, rings. change conversion to sage

--- src/sage/libs/giac.py
+++ src/sage/libs/giac.py
@@ -3,8 +3,38 @@ from sage.misc.sage_eval import sage_eval
 from sage.rings.polynomial.multi_polynomial_sequence import PolynomialSequence
 
 def groebner_basis_libgiac(gens, epsilon=None, prot=False, *args, **kwds):
-    from giacpy import libgiac, giacsettings
-    F = libgiac(gens)
+
+    if not (isinstance(gens,list)):
+        raise ValueError("Fisrt argument must be a list")
+    try:
+       from giacpy import libgiac,giacsettings
+
+    except ImportError:
+            raise ImportError("""One of the optional packages giac or giacpy is missing""")
+
+    proba_epsilon_orig=giacsettings.proba_epsilon  # save the original giac config
+
+    # get the ring from gens
+    P = iter(gens).next().parent()
+    K = P.base_ring()
+    p = K.characteristic()
+
+    if (  K.is_prime_field()  and  p == 0 ):
+
+          F = libgiac(gens)
+
+    elif ( K.is_prime_field()  and ( p < 2**31 ) ):
+
+          F = (libgiac(gens) % p)
+
+    else:
+        raise NotImplementedError("only prime fields of cardinal < 2^31 are implemented in giac for Groebner Basis")
+
+
+    if P.term_order() != "degrevlex":
+        raise NotImplementedError("Only degrevlex term orderings are supported by Giac's Groebner basis engine.")
+
+    # proof or probabilistic reconstruction
     if epsilon is None:
         if proof_polynomial():
             giacsettings.proba_epsilon = 0
@@ -13,16 +43,15 @@ def groebner_basis_libgiac(gens, epsilon=None, prot=False, *args, **kwds):
     else:
         giacsettings.proba_epsilon = epsilon
 
-    P = iter(gens).next().parent()
-    # TODO: check permitted base rings
-    if P.term_order() != "degrevlex":
-        raise NotImplementedError("Only degrevlex term orderings are supported by Giac's Gröbner basis engine.")
-
+    # compute de groebner basis with giac
     gb_giac = F.gbasis([P.gens()], "revlex")
-    # TODO: we shouldn't use string parsing here
-    gens_dict = P.gens_dict()
-    gb = []
-    for f in gb_giac:
-        gb.append(sage_eval(str(f), gens_dict))
 
-    return PolynomialSequence(P, gb)
+    # TODO: we shouldn't use string parsing here
+    #gens_dict = P.gens_dict()
+    #gb = []
+    #for f in gb_giac:
+    #    gb.append(sage_eval(str(f), gens_dict))
+    #
+    #return PolynomialSequence( P, gb)
+    # ?why not (it seems faster was your method better for ram?):
+    return PolynomialSequence( gb_giac, P ) # should it be , immutable=True ?

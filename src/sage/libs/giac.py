b8725bcb234f3731f30b5f2cc8c1828246714e5e
Martin R. Albrecht <martinralbrecht@googlemail.com>
2015-07-01 09:46:56 +0100

    obey PEP8 style conventions

--- src/sage/libs/giac.py
+++ src/sage/libs/giac.py
@@ -6,29 +6,23 @@ def groebner_basis_libgiac(gens, epsilon=None, prot=False, *args, **kwds):
     """
     """
     try:
-       from giacpy import libgiac,giacsettings
-
+        from giacpy import libgiac, giacsettings
     except ImportError:
-            raise ImportError("""One of the optional packages giac or giacpy is missing""")
+        raise ImportError("""One of the optional packages giac or giacpy is missing""")
 
-    proba_epsilon_orig=giacsettings.proba_epsilon  # save the original giac config
+    proba_epsilon_orig = giacsettings.proba_epsilon  # save the original giac config
 
     # get the ring from gens
     P = iter(gens).next().parent()
     K = P.base_ring()
     p = K.characteristic()
 
-    if (  K.is_prime_field()  and  p == 0 ):
-
-          F = libgiac(gens)
-
-    elif ( K.is_prime_field()  and ( p < 2**31 ) ):
-
-          F = (libgiac(gens) % p)
-
+    if K.is_prime_field() and p == 0:
+        F = libgiac(gens)
+    elif K.is_prime_field() and p < 2**31:
+        F = (libgiac(gens) % p)
     else:
         raise NotImplementedError("only prime fields of cardinal < 2^31 are implemented in giac for Groebner Basis")
-
 
     if P.term_order() != "degrevlex":
         raise NotImplementedError("Only degrevlex term orderings are supported by Giac's Groebner basis engine.")
@@ -45,4 +39,4 @@ def groebner_basis_libgiac(gens, epsilon=None, prot=False, *args, **kwds):
     # compute de groebner basis with giac
     gb_giac = F.gbasis([P.gens()], "revlex")
 
-    return PolynomialSequence( gb_giac, P ) # should it be , immutable=True ?
+    return PolynomialSequence(gb_giac, P, immutable=True)

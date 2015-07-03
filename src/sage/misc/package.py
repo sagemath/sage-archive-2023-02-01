a9a45bf4314373cda000c34cf8296b457acb5fde
Nathann Cohen <nathann.cohen@gmail.com>
2015-07-03 12:23:38 +0100

    trac #18408: Updated lists

--- src/sage/misc/package.py
+++ src/sage/misc/package.py
@@ -105,35 +105,50 @@ def _list_to_table(list_of_packages):
 
     return s
 
-_STANDARD_PACKAGES = ['atlas', 'backports_ssl_match_hostname', 'boehm_gc', 'boost_cropped', 'bzip2', 'cddlib', 'cephes',
-                      'certifi', 'cliquer', 'combinatorial_designs', 'conway_polynomials', 'cvxopt', 'cython', 'dateutil',
-                      'docutils', 'ecl', 'eclib', 'ecm', 'elliptic_curves', 'extcode', 'fflas_ffpack', 'flint', 'flintqs',
-                      'freetype', 'gap', 'gcc', 'gd', 'gdmodule', 'genus2reduction', 'gf2x', 'gfan', 'git', 'givaro', 'glpk',
-                      'graphs', 'gsl', 'iconv', 'iml', 'ipython', 'jinja2', 'jmol', 'jsonschema', 'lcalc', 'libfplll', 'libgap',
-                      'libgd', 'libm4ri', 'libm4rie', 'libpng', 'linbox', 'lrcalc', 'm4ri', 'm4rie', 'markupsafe', 'mathjax',
-                      'matplotlib', 'maxima', 'mercurial', 'mistune', 'mpc', 'mpfi', 'mpfr', 'mpir', 'mpmath', 'ncurses',
-                      'networkx', 'ntl', 'numpy', 'palp', 'pari', 'pari_galdata', 'pari_seadata_small', 'patch', 'pexpect', 'pil',
-                      'pillow', 'pip', 'pkgconf', 'pkgconfig', 'polybori', 'polytopes_db', 'ppl', 'pycrypto', 'pygments', 'pynac',
-                      'pyparsing', 'python', 'pyzmq', 'r', 'ratpoints', 'readline', 'rpy2', 'rubiks', 'sage', 'sage_root',
-                      'sage_scripts', 'sagenb', 'sagetex', 'scipy', 'scons', 'setuptools', 'singular', 'six', 'sphinx',
-                      'sqlalchemy', 'sqlite', 'symmetrica', 'sympow', 'sympy', 'tachyon', 'tornado', 'zeromq', 'zlib', 'zn_poly']
+_STANDARD_PACKAGES = ['atlas', 'backports_ssl_match_hostname', 'boehm_gc',
+                      'boost_cropped', 'bzip2', 'cddlib', 'cephes', 'certifi', 'cliquer',
+                      'combinatorial_designs', 'conway_polynomials', 'cvxopt', 'cython', 'dateutil',
+                      'docutils', 'ecl', 'eclib', 'ecm', 'elliptic_curves', 'fflas_ffpack', 'flint',
+                      'flintqs', 'freetype', 'gap', 'gd', 'gdmodule', 'genus2reduction', 'gf2x',
+                      'gfan', 'git', 'givaro', 'glpk', 'graphs', 'gsl', 'iconv', 'iml', 'ipython',
+                      'jinja2', 'jmol', 'jsonschema', 'lcalc', 'libfplll', 'libgap', 'libgd',
+                      'libpng', 'linbox', 'lrcalc', 'm4ri', 'm4rie', 'markupsafe', 'mathjax',
+                      'matplotlib', 'maxima', 'mercurial', 'mistune', 'mpc', 'mpfi', 'mpfr', 'mpmath',
+                      'ncurses', 'networkx', 'ntl', 'numpy', 'palp', 'pari', 'pari_galdata',
+                      'pari_seadata_small', 'patch', 'pexpect', 'pil', 'pillow', 'pip', 'pkgconf',
+                      'pkgconfig', 'planarity', 'polybori', 'polytopes_db', 'ppl', 'pycrypto',
+                      'pygments', 'pynac', 'pyparsing', 'python', 'pyzmq', 'r', 'ratpoints',
+                      'readline', 'rpy2', 'rubiks', 'rw', 'sage', 'sage_root', 'sage_scripts',
+                      'sagenb', 'sagetex', 'scipy', 'setuptools', 'singular', 'six', 'sphinx',
+                      'sqlalchemy', 'sqlite', 'symmetrica', 'sympow', 'sympy', 'tachyon', 'tornado',
+                      'zeromq', 'zlib', 'zn_poly']
 
-_OPTIONAL_PACKAGES = ['4ti2', 'PyQt_x11', 'TOPCOM', 'arb', 'autotools', 'beautifulsoup', 'benzene', 'biopython', 'bliss', 'brian',
-                      'buckygen', 'cbc', 'ccache', 'chomp', 'cluster_seed', 'compilerwrapper', 'coxeter3', 'cryptominisat', 'cunningham_tables', 'd3js',
-                      'database_cremona_ellcurve', 'database_gap', 'database_jones_numfield-v4', 'database_kohel', 'database_odlyzko_zeta',
-                      'database_pari', 'database_stein_watkins', 'database_stein_watkins_mini', 'database_symbolic_data', 'dot2tex', 'extra_docs',
-                      'fricas', 'gambit', 'gap_packages', 'gcc', 'gdb', 'gdbm', 'ginv', 'git', 'git_trac', 'gmp', 'gnuplotpy', 'guppy', 'java3d',
-                      'kash3', 'knoboo', 'latte_int', 'libogg', 'libtheora', 'lidia', 'lie', 'lrs', 'mcqd', 'modular_decomposition', 'mpi4py', 'mpir',
-                      'nauty', 'normaliz', 'nose', 'nzmath', 'openmpi', 'openssl', 'ore_algebra', 'p_group_cohomology', 'patchbot', 'phc', 'plantri',
-                      'pybtex', 'pycryptoplus', 'python2', 'python3', 'pyx', 'pyzmq', 'qhull', 'sage_mode', 'sip', 'termcap', 'threejs', 'tides',
-                      'topcom', 'trac', 'valgrind', 'zeromq']
+_OPTIONAL_PACKAGES = ['PyQt_x11', 'TOPCOM', 'arb', 'beautifulsoup', 'benzene',
+                      'biopython', 'bliss', 'brian', 'buckygen', 'cbc', 'ccache', 'chomp',
+                      'cluster_seed', 'coxeter3', 'cryptominisat', 'cunningham_tables', 'd3js',
+                      'database_cremona_ellcurve', 'database_gap', 'database_jones_numfield',
+                      'database_kohel', 'database_odlyzko_zeta', 'database_pari',
+                      'database_stein_watkins', 'database_stein_watkins_mini',
+                      'database_symbolic_data', 'dot2tex', 'extra_docs', 'gambit', 'gap_packages',
+                      'gcc', 'gdb', 'giac', 'giacpy', 'ginv', 'git_trac', 'gmp', 'gnuplotpy', 'guppy',
+                      'java3d', 'kash3', 'knoboo', 'libogg', 'libtheora', 'lidia', 'lie', 'lrslib',
+                      'mcqd', 'modular_decomposition', 'mpi4py', 'mpir', 'nauty', 'normaliz', 'nose',
+                      'nzmath', 'openmpi', 'openssl', 'ore_algebra', 'p_group_cohomology', 'phc',
+                      'plantri', 'pybtex', 'python2', 'python3', 'pyx', 'qhull', 'sage_mode', 'scons',
+                      'sip', 'termcap', 'threejs', 'tides', 'topcom', 'trac']
 
-_EXPERIMENTAL_PACKAGES = ['PyQt4', 'PyVTK', 'QScintilla2', 'asymptote', 'bison', 'boost_1_34_1', 'cadabra', 'clapack', 'clisp',
-                          'cmake', 'csdp', 'dvipng', 'ets', 'fes', 'flex', 'gcc', 'gdb', 'gnofract4d', 'gnuplot', 'graphviz', 'latte_int', 'libcprops',
-                          'libjpeg', 'libsigsegv', 'macaulay2', 'mayavi_2', 'meataxe', 'modglue', 'mpich2', 'numarray', 'numeric', 'openopt', 'pcre',
-                          'pexpect', 'phcpack', 'polymake', 'processing', 'pygame', 'pygsl', 'pygtk', 'pynifti', 'pyqt', 'pyrexembed', 'qasm', 'qepcad',
-                          'quantlib', 'quantlib_swig', 'reallib3-linux', 'sandpile', 'scitools++', 'semigroupe', 'simpqs', 'sip', 'soya', 'soya_cvs',
-                          'superlu', 'surf', 'vtk_meta', 'wxPython', 'yafray', 'yassl']
+
+_EXPERIMENTAL_PACKAGES = ['4ti2', 'PyQt4', 'PyVTK', 'QScintilla2', 'asymptote',
+                          'autotools', 'bison', 'cadabra', 'clapack', 'clisp', 'cmake', 'compilerwrapper',
+                          'csdp', 'dvipng', 'ets', 'fes', 'flex', 'fricas', 'gnofract4d', 'gnuplot',
+                          'graphviz', 'latte_int', 'libcprops', 'libjpeg', 'libsigsegv', 'macaulay2',
+                          'mayavi', 'meataxe', 'modglue', 'mpich2', 'numarray', 'numeric', 'openopt',
+                          'pcre', 'phcpack', 'polymake', 'processing', 'pygame', 'pygsl', 'pygtk',
+                          'pynifti', 'pyqt', 'pyrexembed', 'qasm', 'qepcad', 'quantlib', 'quantlib_swig',
+                          'reallib3_linux', 'sandpile', 'scitools++', 'semigroupe', 'simpqs', 'sip',
+                          'soya', 'soya_cvs', 'superlu', 'surf', 'valgrind', 'vtk_meta', 'wxPython',
+                          'yafray', 'yassl']
+
 
 __doc__ = __doc__.format(STANDARD_PACKAGES     =_list_to_table(_STANDARD_PACKAGES),
                          OPTIONAL_PACKAGES     =_list_to_table(_OPTIONAL_PACKAGES),
@@ -457,9 +472,9 @@ def optional_packages():
         sage: from sage.misc.package import optional_packages
         sage: installed, not_installed = optional_packages() # optional internet
         sage: min(installed+not_installed)                   # optional internet
-        '4ti2'
+        'PyQt_x11'
         sage: max(installed+not_installed)                   # optional internet
-        'zeromq'
+        'trac'
     """
     return _package_lists_from_sage_output('optional')
 
@@ -486,7 +501,7 @@ def experimental_packages():
         sage: from sage.misc.package import experimental_packages
         sage: installed, not_installed = experimental_packages() # optional internet
         sage: min(installed+not_installed)                   # optional internet
-        'PyQt4'
+        '4ti2'
         sage: max(installed+not_installed)                   # optional internet
         'yassl'
     """

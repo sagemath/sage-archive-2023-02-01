4aab0caaa22c7c06d125719451be93d458e85dfb
George H. Seelinger <ghseeli@gmail.com>
2015-07-05 19:47:48 -0500

    changed major typo in AbstractDiagrams initializer

--- src/sage/combinat/diagram_algebras.py
+++ src/sage/combinat/diagram_algebras.py
@@ -575,7 +575,7 @@ class AbstractPartitionDiagrams(Parent, UniqueRepresentation):
             category = FiniteEnumeratedSets()
         Parent.__init__(self, category=category)
         self.diagram_func = diagram_func
-        self.order = orderda.partition_diagrams, 
+        self.order = order, 
 
     def __iter__(self):
         r"""

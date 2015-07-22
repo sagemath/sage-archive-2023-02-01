9c7020440bef3f8302e608c0532ff680c9dd2e35
Dima Pasechnik <dimpase@gmail.com>
2015-07-22 18:14:15 +0100

    ported maxima patches; tarball needs bootstrapping; trivial doctests fix
    to be continued

--- src/sage/interfaces/maxima_abstract.py
+++ src/sage/interfaces/maxima_abstract.py
@@ -426,7 +426,7 @@ class MaximaAbstract(ExtraTabCompletion, Interface):
         EXAMPLES::
 
             sage: maxima.version()
-            '5.35.1'
+            '5.36.0.1'
         """
         return maxima_version()
 
@@ -2213,7 +2213,7 @@ def maxima_version():
 
         sage: from sage.interfaces.maxima_abstract import maxima_version
         sage: maxima_version()
-        '5.35.1'
+        '5.36.0.1'
     """
     return os.popen('maxima --version').read().split()[-1]
 

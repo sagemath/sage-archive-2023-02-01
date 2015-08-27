497e25091830667b6a40b98a304a0b5d2c500aeb
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2015-08-27 14:02:52 +0200

    Fixed heading

--- src/sage/quadratic_forms/ternary.pyx
+++ src/sage/quadratic_forms/ternary.pyx
@@ -1,18 +1,16 @@
+"""
+Helper code for ternary quadratic forms
+"""
+
 #*****************************************************************************
 #       Copyright (C) 2012 Gustavo Rama
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
-#
-#    This code is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    General Public License for more details.
-#
-#  The full text of the GPL is available at:
-#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
-
 
 
 from sage.rings.integer_ring import ZZ

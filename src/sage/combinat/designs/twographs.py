4fd56a1f10901663ea001460dd2dfe0da666f4c8
Dima Pasechnik <dimpase@gmail.com>
2015-09-06 10:44:35 -0700

    use TaylorTwographSRG in taylor_twograph

--- src/sage/combinat/designs/twographs.py
+++ src/sage/combinat/designs/twographs.py
@@ -187,38 +187,15 @@ def taylor_twograph(q):
     EXAMPLES::
 
         sage: from sage.combinat.designs.twographs import taylor_twograph
-        sage: T=taylor_twograph(3); T                      # long time
+        sage: T=taylor_twograph(3); T
         Incidence structure with 28 points and 1260 blocks
-        sage: t=graphs.TaylorTwographSRG(3).twograph(); t  # faster to build
-        Incidence structure with 28 points and 1260 blocks
-        sage: t.is_isomorphic(T)                           # long time
-        True
-
     """
+    from sage.graphs.generators.families import TaylorTwographSRG
     from sage.rings.arith import is_prime_power
     p, k = is_prime_power(q,get_data=True)
     if k==0 or p==2:
        raise ValueError('q must be a an odd prime power')
-    from sage.schemes.projective.projective_space import ProjectiveSpace
-    from sage.rings.finite_rings.constructor import FiniteField
-    from sage.modules.free_module_element import free_module_element as vector
-    from sage.rings.finite_rings.integer_mod import mod
-    from __builtin__ import sum
-    Fq = FiniteField(q**2, 'a')
-    PG = ProjectiveSpace(2, Fq)
-    def S(xx,yy):
-        x = vector(xx)
-        y = vector(yy)
-        return sum(map(lambda j: x[j]*y[2-j]**q, xrange(3)))
-
-    V = filter(lambda x: S(x,x)==0, PG)
-    def make_tester():
-        if mod(q,4)==1:
-            return lambda (x,y,z): not (S(x,y)*S(y,z)*S(z,x)).is_square()
-        else:
-            return lambda (x,y,z): (S(x,y)*S(y,z)*S(z,x)).is_square()
-    f = make_tester()
-    return TwoGraph(filter(f, combinations(V,3)))
+    return TaylorTwographSRG(q).twograph()
 
 def is_twograph(T):
     r"""

641a24ca2c062e473d4b0279357b966c2cfe6436
Daniel Krenn <git@danielkrenn.at>
2015-09-17 21:09:18 +0200

    module header

--- src/sage/symbolic/subring.py
+++ src/sage/symbolic/subring.py
@@ -1,3 +1,24 @@
+r"""
+Subrings of the Symbolic Ring
+
+AUTHORS:
+
+- Daniel Krenn (2015)
+
+
+Classes and Methods
+===================
+"""
+
+#*****************************************************************************
+# Copyright (C) 2015 Daniel Krenn <dev@danielkrenn.at>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
 
 import sage
 from ring import SymbolicRing, SR

cb4633782fc4cc57f02b5212ea3e8973cbeeb289
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2015-09-19 20:53:49 +0200

    Add declarations for gmp/mpir C++ classes

--- src/sage/libs/lcalc/lcalc_Lfunction.pyx
+++ src/sage/libs/lcalc/lcalc_Lfunction.pyx
@@ -10,25 +10,21 @@ AUTHORS:
 - Yann Laigle-Chapuy (2009): refactored
 - Rishikesh (2009): initial version
 """
+
 #*****************************************************************************
 #       Copyright (C) 2009 William Stein <wstein@gmail.com>
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
-#
-#    This code is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    General Public License for more details.
-#
-#  The full text of the GPL is available at:
-#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
 include "sage/ext/interrupt.pxi"
 include "sage/ext/cdefs.pxi"
-include "sage/libs/mpfr.pxd"
 
+from sage.libs.mpfr cimport *
 from sage.rings.integer cimport Integer
 
 from sage.rings.complex_number cimport ComplexNumber

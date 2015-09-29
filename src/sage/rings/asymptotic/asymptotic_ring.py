dc00f9545788d7ccfd36b5c0df915dcd9e27360b
Benjamin Hackl <benjamin.hackl@aau.at>
2015-09-29 15:47:41 +0200

    improve documentation of _element_constructor_

--- src/sage/rings/asymptotic/asymptotic_ring.py
+++ src/sage/rings/asymptotic/asymptotic_ring.py
@@ -993,6 +993,12 @@ class AsymptoticRing(Ring, UniqueRepresentation):
 
         An element of this asymptotic ring.
 
+        .. NOTE::
+
+            Either ``data`` or ``summands`` has to be given. If
+            ``summands`` is specified, then no positional argument
+            may be passed (except for `int(0)`).
+
         TESTS::
 
             sage: AR = AsymptoticRing(growth_group='x^ZZ', coefficient_ring=ZZ)

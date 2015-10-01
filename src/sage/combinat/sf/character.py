612248ab39ee8d3d67e10a833e6c3059482daf4c
zabrocki <zabrocki@mathstat.yorku.ca>
2015-10-01 12:45:18 -0400

    changes to powersum.py, sf/__init__.py and module_list.rst

--- src/sage/combinat/sf/character.py
+++ src/sage/combinat/sf/character.py
@@ -19,8 +19,6 @@ from sage.combinat.sf.sfa import SymmetricFunctionAlgebra_generic as SFA_generic
 from sage.misc.cachefunc import cached_method
 from sage.categories.homset import Hom
 from sage.combinat.partition import Partition
-from sage.rings.arith import divisors, moebius
-from sage.functions.other import binomial
 
 class generic_character(SFA_generic):
     def _my_key(self, la):

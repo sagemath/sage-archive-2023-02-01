0b0ccdeff8b4a5de9df5142c7ca9c1c480275cfd
Travis Scrimshaw <tscrimsh at umn.edu>
2015-10-08 23:48:08 -0500

    Added reverse bijection for B^{1,1} of E_6^{(1)}.

--- src/sage/combinat/rigged_configurations/bij_type_E67.py
+++ src/sage/combinat/rigged_configurations/bij_type_E67.py
@@ -57,131 +57,40 @@ class KRTToRCBijectionTypeE67(KRTToRCBijectionAbstract):
             sage: bijection.cur_path[0].insert(0, [3])
             sage: bijection.next_state(3)
         """
-        # Note that type D_n only contains the (absolute) values between 1 and n
-        #   (unlike type A_n which is 1 to n+1). Thus we only need to subtract 1
-        #   to match the indices.
-        # Also note that we must subtract 1 from n to match the indices as well.
-        n = self.n
-        tableau_height = len(self.cur_path[0]) - 1
 
-        # If it is a regular value, we follow the A_n rules
-        if val > 0:
-            KRTToRCBijectionTypeA.next_state(self, val)
+        def find_singular_string(p, max_width):
+            max_pos = -1
+            if max_width > 0:
+                for i, vac_num in enumerate(p.vacancy_numbers):
+                    if p[i] <= max_width and vac_num == p.rigging[i]:
+                        max_pos = i
+                        break
+            if max_pos == -1:
+                return 0
+            return p[max_pos]
 
-            # If we are inserting n-1 (which will only update nu[a] for a <=
-            #   n-1), we need to update the vacancy number of nu[n] as well
-            if val == n - 1:
-                self._update_vacancy_nums(val)
-            if tableau_height >= n - 2:
-                self._correct_vacancy_nums()
-            return
+        b = self.tp_krt.parent().letters(val)
+        max_width = float("inf")
+        found = True
+        while found:
+            found = False
+            data = [(-a, find_singular_string(self.ret_rig_con[-a-1], max_width))
+                    for a in b.value if a < 0]
+            if not data:
+                break
 
-        pos_val = -val
+            max_val = max(l for a,l in data)
+            for a,l in data:
+                if l == max_val:
+                    found = True
+                    self.ret_rig_con[a-1].insert_cell(max_width)
+                    max_width = l
+                    b = b.e(a)
+                    break
 
-        if pos_val == n:
-            # Special case for `\overline{n}` and adding to make height `n`
-            #    where we only update the vacancy numbers
-            # This only occurs with `r = n - 1`
-            if self.cur_dims[0][0] == n - 1 and tableau_height == n - 1:
-                self._update_vacancy_nums(n-2)
-                self._update_vacancy_nums(n-1)
-                self._correct_vacancy_nums()
-                return
-
-            if len(self.ret_rig_con[n - 1]) > 0:
-                max_width = self.ret_rig_con[n - 1][0] + 1
-            else:
-                max_width = 1
-
-            # Update the last one and skip a rigged partition
-            max_width = self.ret_rig_con[n - 1].insert_cell(max_width)
-
-            for a in reversed(range(tableau_height, n - 2)):
-                max_width = self.ret_rig_con[a].insert_cell(max_width)
-                self._update_vacancy_nums(a + 1)
-                self._update_partition_values(a + 1)
-
-            # Update all other effected remaining values
-
-            self._update_vacancy_nums(n - 1)
-            if tableau_height >= n - 2:
-                self._correct_vacancy_nums()
-            self._update_partition_values(n - 1)
-
-            self._update_vacancy_nums(tableau_height)
-            self._update_partition_values(tableau_height)
-
-            if tableau_height > 0:
-                self._update_vacancy_nums(tableau_height - 1)
-                self._update_partition_values(tableau_height - 1)
-
-            self._update_vacancy_nums(n - 2)
-            return
-
-        # Always add a cell to the first singular value in the first
-        #   tableau we are updating.
-        if len(self.ret_rig_con[pos_val - 1]) > 0:
-            max_width = self.ret_rig_con[pos_val - 1][0] + 1
-        else:
-            max_width = 1
-        # Special case for `\overline{n-1}` to take the larger of the last two
-        if pos_val == n - 1 and len(self.ret_rig_con[n - 1]) > 0 and \
-          self.ret_rig_con[n - 1][0] + 1 > max_width:
-            max_width = self.ret_rig_con[n - 1][0] + 1
-
-        # Add cells similar to type A_n but we move to the right until we reach
-        #   the value of n-2
-        for a in range(pos_val - 1, n - 2):
-            max_width = self.ret_rig_con[a].insert_cell(max_width)
-
-        # Handle the special behavior near values of n
-        if tableau_height <= n - 2:
-            max_width2 = self.ret_rig_con[n - 2].insert_cell(max_width)
-            max_width = self.ret_rig_con[n - 1].insert_cell(max_width)
-            if max_width2 < max_width:
-                max_width = max_width2
-        elif pos_val <= self.cur_dims[0][0]:
-            # Special case when the height will become n
-            max_width = self.ret_rig_con[self.cur_dims[0][0] - 1].insert_cell(max_width)
-
-        # Go back following the regular A_n rules
-        if tableau_height <= n - 3:
-            max_width = self.ret_rig_con[n - 3].insert_cell(max_width)
-
-        self._update_vacancy_nums(n - 2)
-        self._update_vacancy_nums(n - 1)
-        if tableau_height >= n - 2:
-            self._correct_vacancy_nums()
-        self._update_partition_values(n - 2)
-        self._update_partition_values(n - 1)
-
-        for a in reversed(range(tableau_height, n - 3)):
-            max_width = self.ret_rig_con[a].insert_cell(max_width)
-            self._update_vacancy_nums(a + 1)
-            self._update_partition_values(a + 1)
-
-        # Update the final rigged partitions
-        if tableau_height < n - 2:
-            self._update_vacancy_nums(tableau_height)
-            self._update_partition_values(tableau_height)
-
-            if pos_val <= tableau_height:
-                for a in range(pos_val - 1, tableau_height):
-                    self._update_vacancy_nums(a)
-                    self._update_partition_values(a)
-                if pos_val > 1:
-                    self._update_vacancy_nums(pos_val-2)
-                    self._update_partition_values(pos_val-2)
-            elif 0 < tableau_height:
-                self._update_vacancy_nums(tableau_height - 1)
-                self._update_partition_values(tableau_height - 1)
-        elif pos_val <= n - 1:
-            for a in range(pos_val - 1, n - 2):
-                self._update_vacancy_nums(a)
-                self._update_partition_values(a)
-            if pos_val > 1:
-                self._update_vacancy_nums(pos_val-2)
-                self._update_partition_values(pos_val-2)
+        for a in range(6):
+            self._update_vacancy_nums(a)
+            self._update_partition_values(a)
 
 class RCToKRTBijectionTypeE67(RCToKRTBijectionAbstract):
     r"""
@@ -216,15 +125,13 @@ class RCToKRTBijectionTypeE67(RCToKRTBijectionAbstract):
             b = C((-1,6))
 
         last_size = 0
-        pos = [x for x in b.value if x > 0]
         found = True
         while found:
             found = False
-            assert len(pos) <= 2
-            data = [self._find_singular_string(self.cur_partitions[a-1], last_size)
-                    for a in pos]
-            data = [(val, pos[i], self.cur_partitions[pos[i]-1][val])
-                    for i,val in enumerate(data) if val is not None]
+            data = [(a, self._find_singular_string(self.cur_partitions[a-1], last_size))
+                    for a in b.value if a > 0]
+            data = [(val, a, self.cur_partitions[a-1][val])
+                    for a,val in data if val is not None]
             if not data:
                 break
 
@@ -235,7 +142,6 @@ class RCToKRTBijectionTypeE67(RCToKRTBijectionAbstract):
                     last_size = l
                     self.cur_partitions[a-1].remove_cell(i)
                     b = b.f(a)
-                    pos = [x for x in b.value if x > 0]
                     break
 
         for a in range(6):

0cded598ebe90e7aaa73d595069c9d7b8f0f1770
Nathann Cohen <nathann.cohen@gmail.com>
2015-10-09 09:33:29 +0200

    trac #19381: A new module

---
+++ src/sage/graphs/graph_input.py
@@ -0,0 +1,17 @@
+r"""
+Functions for reading/building graphs/digraphs.
+
+This module gathers functions needed to build a graph from any other data.
+
+ Note that because they are called by the constructors of :class:`Graph` and
+:class:`DiGraph`, most of these functions modify a graph inplace.
+
+Functions
+---------
+
+"""
+
+
+from sage.misc.rest_index_of_methods import gen_rest_table_index
+import sys
+__doc__ = __doc__.format(INDEX_OF_FUNCTIONS=gen_rest_table_index(sys.modules[__name__]))

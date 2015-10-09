a531c7071db02d97eba4dd9af10f355040c28e6f
Christian Stump <christian.stump@gmail.com>
2015-10-09 21:49:08 +0200

    added back the changes, based on #19296

--- src/sage/databases/findstat.py
+++ src/sage/databases/findstat.py
@@ -242,6 +242,7 @@ FINDSTAT_STATISTIC_IDENTIFIER                   = 'StatisticIdentifier'
 FINDSTAT_STATISTIC_COLLECTION                   = 'StatisticCollection'
 FINDSTAT_STATISTIC_DATA                         = 'StatisticData'
 FINDSTAT_STATISTIC_GENERATING_FUNCTION          = 'StatisticGeneratingFunction'
++FINDSTAT_STATISTIC_NAME                        = 'StatisticTitle'
 FINDSTAT_STATISTIC_DESCRIPTION                  = 'StatisticDescription'
 FINDSTAT_STATISTIC_REFERENCES                   = 'StatisticReferences'
 FINDSTAT_STATISTIC_CODE                         = 'StatisticCode'
@@ -912,6 +913,7 @@ class FindStatStatistic(SageObject):
                 raise
 
         self._description           = self._raw[FINDSTAT_STATISTIC_DESCRIPTION].encode("utf-8")
+        self._name                  = self._raw[FINDSTAT_STATISTIC_NAME].encode("utf-8")
         self._references            = self._raw[FINDSTAT_STATISTIC_REFERENCES].encode("utf-8")
         self._collection            = FindStatCollection(self._raw[FINDSTAT_STATISTIC_COLLECTION])
         self._code                  = self._raw[FINDSTAT_STATISTIC_CODE]
@@ -1497,7 +1499,7 @@ class FindStatStatistic(SageObject):
             sage: findstat(1).name()                                            # optional -- internet,random
             u'The number of ways to write a permutation as a minimal length product of simple transpositions.'
         """
-        return self._description.partition(FINDSTAT_SEPARATOR_NAME)[0]
+        return self._name
 
     def references(self):
         r"""
@@ -2099,6 +2101,22 @@ class FindStatCollection(Element):
             u'Binary tree'
         """
         return self._name
+
+    def name_plural(self):
+        r"""
+        Return the plural name of the FindStat collection.
+
+        OUTPUT:
+
+        The name of the FindStat collection, in plural.
+
+        EXAMPLES::
+
+            sage: from sage.databases.findstat import FindStatCollection
+            sage: FindStatCollection("Binary trees").name_plural()              # optional -- internet
+            u'Binary trees'
+        """
+        return self._name_plural
 
 class FindStatCollections(Parent, UniqueRepresentation):
     r"""

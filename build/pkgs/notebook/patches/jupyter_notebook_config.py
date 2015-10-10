4fcdb6ee743d6d73f90017bf919b87a7095e8fab
Volker Braun <vbraun.name@gmail.com>
2015-10-10 12:59:52 +0200

    Use jupyter_notebook_config.py to setup mathjax url

---
+++ build/pkgs/notebook/patches/jupyter_notebook_config.py
@@ -0,0 +1,10 @@
+# Configuration file for Sage's builtin Jupyter notebook server
+
+# Note for distributors: Sage uses mathjax, so the notebook server
+# needs to have the mathjax_url set to wherever your distribution
+# installs mathjax.
+
+import os
+c = get_config()
+
+c.NotebookApp.mathjax_url = '/nbextensions/mathjax/MathJax.js'

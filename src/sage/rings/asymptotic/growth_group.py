997380baa67cc3d7fda5e9791c29d58f3ff15515
Clemens Heuberger <clemens.heuberger@aau.at>
2015-10-20 12:59:40 +0200

    Trac #19399: Insert ReSt links

--- src/sage/rings/asymptotic/growth_group.py
+++ src/sage/rings/asymptotic/growth_group.py
@@ -1400,7 +1400,7 @@ class GenericGrowthGroup(
             sage: L1 is L2
             True
 
-        Test determining of the category (GenericGrowthGroup)::
+        Test determining of the category (:class:`GenericGrowthGroup`)::
 
             sage: from sage.rings.asymptotic.growth_group import GenericGrowthGroup
             sage: GenericGrowthGroup(ZZ, 'x').category()  # indirect doctest
@@ -1408,7 +1408,7 @@ class GenericGrowthGroup(
             sage: GenericGrowthGroup(ZZ, 'x', category=Groups()).category()  # indirect doctest
             Category of groups
 
-        Test determining of the category (MonomialGrowthGroup)::
+        Test determining of the category (:class:`MonomialGrowthGroup`)::
 
             sage: from sage.rings.asymptotic.growth_group import MonomialGrowthGroup
             sage: MonomialGrowthGroup(ZZ, 'x').category()  # indirect doctest
@@ -1421,7 +1421,7 @@ class GenericGrowthGroup(
             sage: MonomialGrowthGroup(W, 'x').category()  # indirect doctest
             Category of sets
 
-        Test determining of the category (ExponentialGrowthGroup)::
+        Test determining of the category (:class:`ExponentialGrowthGroup`)::
 
             sage: from sage.rings.asymptotic.growth_group import ExponentialGrowthGroup
             sage: ExponentialGrowthGroup(ZZ, 'x').category()  # indirect doctest

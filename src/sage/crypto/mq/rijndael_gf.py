f32b0434f2d8c064dc2d6792fa80666c8b3282b2
Thomas Gagne <thomasgagne100@gmail.com>
2015-11-08 20:56:23 -0800

    Updated the copyright template for RijndaelGF.

--- src/sage/crypto/mq/rijndael_gf.py
+++ src/sage/crypto/mq/rijndael_gf.py
@@ -418,21 +418,15 @@ REFERENCES:
 
 """
 
-###########################################################################
-# Copyright (c) 2015 Thomas Gagne <thomasgagne100@gmail.com>
+#*****************************************************************************
+#       Copyright (C) 2015 Thomas Gagne <thomasgagne100@gmail.com>
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# http://www.gnu.org/licenses/
-###########################################################################
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
 
 from sage.matrix.constructor import matrix
 from sage.matrix.constructor import column_matrix

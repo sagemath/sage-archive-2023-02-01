5240f6bb131eabed2e8b39ab00327cbe52e28212
Thomas Gagne <thomasgagne100@gmail.com>
2015-11-16 16:53:59 -0800

    Made the RijndaelGF module use lazy_import so it's not loaded on startup.

--- src/sage/crypto/mq/__init__.py
+++ src/sage/crypto/mq/__init__.py
@@ -1,3 +1,5 @@
 from sr import SR
 from sbox import SBox
-from rijndael_gf import RijndaelGF
+
+from sage.misc.lazy_import import lazy_import
+lazy_import('sage.crypto.mq.rijndael_gf', 'RijndaelGF')

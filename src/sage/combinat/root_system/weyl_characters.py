4a950ac85c2e083d93b2f5037b2c8ed329c25bea
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2015-11-29 12:47:12 +0100

    Make __pow__ more efficient

--- src/sage/combinat/root_system/weyl_characters.py
+++ src/sage/combinat/root_system/weyl_characters.py
@@ -1116,13 +1116,26 @@ class WeylCharacterRing(CombinatorialFreeModule):
                 sage: spin = B4(0,0,0,1)
                 sage: [spin^k for k in [0,1,3]]
                 [B4(0,0,0,0), B4(0,0,0,1), 5*B4(0,0,0,1) + 4*B4(1,0,0,1) + 3*B4(0,1,0,1) + 2*B4(0,0,1,1) + B4(0,0,0,3)]
+                sage: spin^-1
+                Traceback (most recent call last):
+                ...
+                ValueError: cannot invert self (= B4(0,0,0,1))
+                sage: x = 2 * B4.one(); x
+                2*B4(0,0,0,0)
+                sage: x^-3
+                1/8*B4(0,0,0,0)
             """
-            if n == 0:
+            n = ZZ(n)
+            if not n:
                 return self.parent().one()
-            elif n == 1:
-                return self
-            else:
-                return self * self ** (n-1)
+            if n < 0:
+                self = ~self
+                n = -n
+
+            res = self
+            for i in range(n-1):
+                res = self * res
+            return res
 
         def is_irreducible(self):
             """

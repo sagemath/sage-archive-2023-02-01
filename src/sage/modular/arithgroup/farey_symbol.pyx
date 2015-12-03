56c00c2792306e27cb1d07a6e9f409defccc309b
Marc Masdeu <marc.masdeu@gmail.com>
2015-12-03 10:36:23 +0000

    Added Trac number to test.

--- src/sage/modular/arithgroup/farey_symbol.pyx
+++ src/sage/modular/arithgroup/farey_symbol.pyx
@@ -366,7 +366,7 @@ cdef class Farey:
             sage: g == g1 or g * G([-1,0,0,-1]) == g1
             True
 
-        Check that it works for GammaH as well::
+        Check that it works for GammaH as well (Trac #19660)::
 
             sage: G = GammaH(147, [8])
             sage: G.farey_symbol().word_problem(G([1,1,0,1]))

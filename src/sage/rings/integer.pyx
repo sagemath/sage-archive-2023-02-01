1d70630b6f878752fa342150d1e4c2e6c4777c4a
Prateek Sharma <prateeksharma.iitp@gmail.com>
2015-12-05 15:16:40 +0530

    Redefining Multifactorial
    I increased the sub_products by 1 and set i in k*i+residue from 0.

--- src/sage/rings/integer.pyx
+++ src/sage/rings/integer.pyx
@@ -3919,6 +3919,8 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
             120
             sage: 5.multifactorial(2)
             15
+            sage: 5.multifactorial(3)
+            10
             sage: 23.multifactorial(2)
             316234143225
             sage: prod([1..23, step=2])
@@ -3957,29 +3959,27 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
             else:
                 sign = one
             return sign / Integer(-k-n).multifactorial(k)
-        elif n > k:
-            return n * Integer(n-k).multifactorial(k)
-            
+        
         # compute the actual product, optimizing the number of large
         # multiplications
         cdef int i,j
 
         # we need (at most) log_2(#factors) concurrent sub-products
-        cdef int prod_count = <int>ceil_c(log_c(n/k+1)/log_c(2))
+        cdef int prod_count = <int>ceil_c(log_c(n/k+1)/log_c(2))+1
         cdef mpz_t* sub_prods = <mpz_t*>check_allocarray(prod_count, sizeof(mpz_t))
         for i from 0 <= i < prod_count:
             mpz_init(sub_prods[i])
 
         sig_on()
-
+ 
         cdef residue = n % k
         cdef int tip = 0
-        for i from 1 <= i <= n//k:
+        for i from 0 <= i <= (n//k):
             mpz_set_ui(sub_prods[tip], k*i + residue)
             # for the i-th terms we use the bits of i to calculate how many
             # times we need to multiply "up" the stack of sub-products
             for j from 0 <= j < 32:
-                if i & (1 << j):
+                if i+1 & (1 << j):
                     break
                 tip -= 1
                 mpz_mul(sub_prods[tip], sub_prods[tip], sub_prods[tip+1])
@@ -3998,6 +3998,7 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
         sage_free(sub_prods)
 
         return z
+  
 
     def gamma(self):
         r"""

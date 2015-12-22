b522bebdacd61865dec0824c74c934485a3ea2e0
Julian Rüth <julian.rueth@fsfe.org>
2015-12-21 23:33:18 -0600

    fixed incorrect doctests related to p-adic hashing

--- src/sage/rings/padics/padic_ZZ_pX_CR_element.pyx
+++ src/sage/rings/padics/padic_ZZ_pX_CR_element.pyx
@@ -513,7 +513,7 @@ cdef class pAdicZZpXCRElement(pAdicZZpXElement):
             sage: hash(a)
             Traceback (most recent call last):
             ...
-            TypeError: unhashable type: 'sage.rings.padics.padic_ZZ_pX_CR_element.pAdicZZpXCRElement'
+            TypeError: unhashable type: 'sage.rings.padics.qadic_flint_CR.qAdicCappedRelativeElement'
 
         However, we want to cache computations which depend on them. Therefore
         they define a ``_cache_key`` which is hashable and uniquely identifies
@@ -531,7 +531,7 @@ cdef class pAdicZZpXCRElement(pAdicZZpXElement):
             sage: K.zero()._cache_key()
             (..., 0)
             sage: K(0,1)._cache_key()
-            (..., 0, 1)
+            (..., 1, 0)
 
         """
         if self._is_exact_zero():

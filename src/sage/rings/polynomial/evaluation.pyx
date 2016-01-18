1be1b6efd30ddc9426f7fcd9632aa5b773102291
Vincent Delecroix <20100.delecroix@gmail.com>
2016-01-18 14:51:00 -0300

    Trac 19822: use the standard copyright template

--- src/sage/rings/polynomial/evaluation.pyx
+++ src/sage/rings/polynomial/evaluation.pyx
@@ -17,14 +17,15 @@ For appropriate testing see
     Integrate these functions into
     :mod:`~sage.rings.polynomial.polynomial_compiled`
 """
-################################################################################
-#       Copyright (C) 2015 Vincent Delecroix <20100.delecroix@gmail.com>
+#*****************************************************************************
+#       Copyright (C) 2016 Vincent Delecroix <20100.delecroix@gmail.com>
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
-#  as published by the Free Software Foundation; either version 2 of
-#  the License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
-################################################################################
+#*****************************************************************************
 
 from sage.libs.mpfr cimport *
 from sage.libs.mpfi cimport *

e4da4c198ebdbdf44650e91342cbac64e44ba454
Volker Braun <vbraun.name@gmail.com>
2016-01-19 01:17:31 +0000

    Doctest fixes for changed subgroup generator choices

--- src/sage/groups/perm_gps/permgroup.py
+++ src/sage/groups/perm_gps/permgroup.py
@@ -1548,18 +1548,23 @@ class PermutationGroup_generic(group.FiniteGroup):
 
         EXAMPLES::
 
-            sage: SymmetricGroup(10).stabilizer(4)._order()
+            sage: G = SymmetricGroup(10).subgroup([(i, 10) for i in range(1, 10) if i != 4])
+            sage: G._order()
             362880
-            sage: SymmetricGroup(10).stabilizer(4).stabilizer(5)._order()
-            40320
-            sage: SymmetricGroup(200).stabilizer(100)._order() == factorial(199) # this should be very fast
-            True
 
         TESTS::
 
             sage: [SymmetricGroup(n).stabilizer(1)._gap_().Size() for n in [4..10]]
             [6, 24, 120, 720, 5040, 40320, 362880]
-            sage: [SymmetricGroup(n).stabilizer(1)._order() for n in [4..10]]
+            sage: special_gens = [
+            ....:     [(3,4), (2,4)],
+            ....:     [(4,5), (3,5), (2,5)],
+            ....:     [(5,6), (4,6), (3,6), (2,6)],
+            ....:     [(6,7), (5,7), (4,7), (3,7), (2,7)],
+            ....:     [(7,8), (6,8), (5,8), (4,8), (3,8), (2,8)],
+            ....:     [(8,9), (7,9), (6,9), (5,9), (4,9), (3,9), (2,9)],
+            ....:     [(9,10), (8,10), (7,10), (6,10), (5,10), (4,10), (3,10), (2,10)]]
+            sage: [SymmetricGroup(n).subgroup(gen)._order() for gen in special_gens]
             [6, 24, 120, 720, 5040, 40320, 362880]
         """
         gens = self.gens()

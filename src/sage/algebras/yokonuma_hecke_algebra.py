d10fc96a146dd9cb46c81e0e925d85a41d887517
Travis Scrimshaw <tscrimsh at umn.edu>
2016-01-21 15:44:23 -0600

    Changed to the standard copyright template.

--- src/sage/algebras/yokonuma_hecke_algebra.py
+++ src/sage/algebras/yokonuma_hecke_algebra.py
@@ -9,7 +9,10 @@ AUTHORS:
 #*****************************************************************************
 #  Copyright (C) 2015 Travis Scrimshaw <tscrimsh at umn.edu>
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 

42718e8d42381ba299eae472417402719577795e
Nathann Cohen <nathann.cohen@gmail.com>
2016-01-22 14:34:38 +0200

    trac #19941: new 'constructor.py' file

---
+++ src/sage/rings/finite_rings/constructor.py
@@ -0,0 +1,4 @@
+from sage.misc.superseded import deprecation
+deprecation(19941,"This module has been renamed to sage.rings.finite_rings.finite_field_constructor")
+
+from finite_field_constructor import *

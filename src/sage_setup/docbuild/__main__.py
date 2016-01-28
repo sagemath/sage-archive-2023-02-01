cb2b63f7e13f010b529411c9645a9b6ef337f713
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2016-01-28 20:09:37 +0100

    Remove old src/doc/ouput and .pyc files

--- src/sage_setup/docbuild/__main__.py
+++ src/sage_setup/docbuild/__main__.py
@@ -1,2 +1,10 @@
 from . import main
 main()
+
+# Remove old documentation output
+import os
+from sage.env import SAGE_DOC
+old_doc = os.path.join(SAGE_DOC, "output")
+if os.path.exists(old_doc):
+    from shutil import rmtree
+    rmtree(old_doc)

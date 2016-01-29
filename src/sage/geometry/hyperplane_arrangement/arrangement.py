dda0ab3d40972798cf18394d91eabf56418f36e5
Travis Scrimshaw <tscrimsh at umn.edu>
2016-01-29 13:52:44 -0600

    Use the standard copyright template.

--- src/sage/geometry/hyperplane_arrangement/arrangement.py
+++ src/sage/geometry/hyperplane_arrangement/arrangement.py
@@ -323,9 +323,10 @@ REFERENCES:
 #       Copyright (C) 2013 David Perkinson <davidp@reed.edu>
 #                          Volker Braun <vbraun.name@gmail.com>
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
-#  as published by the Free Software Foundation; either version 2 of
-#  the License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
--- src/sage/geometry/hyperplane_arrangement/check_freeness.py
+++ src/sage/geometry/hyperplane_arrangement/check_freeness.py
@@ -11,10 +11,14 @@ for details.
     This could be extended to a freeness check for more general modules
     over a polynomial ring.
 """
+
 #*****************************************************************************
 #       Copyright (C) 2015 Travis Scrimshaw <tscrimsh at umn.edu>
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 

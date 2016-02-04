005ced0b48cf670718148b4a8e948aabf06a8163
Travis Scrimshaw <tscrimsh at umn.edu>
2016-02-04 13:41:23 -0600

    Finishing implementation of Hochschild complexes.

--- src/sage/homology/hochschild_complex.py
+++ src/sage/homology/hochschild_complex.py
@@ -12,81 +12,191 @@ Hochschild Complexes
 #                  http://www.gnu.org/licenses/
 ########################################################################
 
+from sage.misc.cachefunc import cached_method
 from sage.structure.unique_representation import UniqueRepresentation
-from sage.structure.parent import Parent
-from sage.structure.element import Element
+from sage.structure.category_object import CategoryObject
 from sage.categories.category_types import ChainComplexes
 from sage.categories.tensor import tensor
 from sage.combinat.free_module import CombinatorialFreeModule
-#from sage.rings.all import ZZ
-#from sage.matrix.constructor import matrix
-#from sage.homology.chain_complex import ChainComplex_class
+from sage.homology.chain_complex import ChainComplex
 
-import itertools
-
-class HochschildComplex(UniqueRepresentation, Parent):
+class HochschildComplex(UniqueRepresentation, CategoryObject):
     r"""
     The Hochschild complex.
 
-    Let `A` be an algebra over a commutative ring `R` which is a
-    projective `R`-module, and `M` an `A`-bimodule. The *Hochschild complex*
-    is the chain complex given by
+    Let `A` be an algebra over a commutative ring `R` such
+    that `A` a projective `R`-module, and `M` an `A`-bimodule.
+    The *Hochschild complex* is the chain complex given by
 
     .. MATH::
 
         C_n(A, M) := M \otimes A^{\otimes n}
 
-    with the boundary operator
+    with the boundary operators given as follows. For fix `n`, define
+    the face maps
 
     .. MATH::
 
-        d_i(m \otimes a_1 \otimes \cdots \otimes a_n) = \begin{cases}
+        f_{n,i}(m \otimes a_1 \otimes \cdots \otimes a_n) = \begin{cases}
         m a_1 \otimes \cdots \otimes a_n & \text{if } i = 0, \\
-        (-1)^n a_n m \otimes a_1 \otimes \cdots \otimes a_{n-1}
+        a_n m \otimes a_1 \otimes \cdots \otimes a_{n-1}
         & \text{if } i = n, \\
-        (-1)^i m \otimes a_1 \otimes \cdots \otimes a_i a_{i+1} \otimes
+        m \otimes a_1 \otimes \cdots \otimes a_i a_{i+1} \otimes
         \cdots \otimes a_n & \text{otherwise.}
         \end{cases}
 
-    *Hochschild homology* is the homology of this complex, and
-    alternatively, the Hochschild homology can be described by
-    `HH_n(A, M) = \operatorname{Tor}_n^A(A, M)`.
+    We define the boundary operators as
+
+    .. MATH::
+
+        d_n = \sum_{i=0}^n (-1)^i f_{n,i}.
+
+    The *Hochschild homology* of `A` is the homology of this complex.
+    Alternatively, the Hochschild homology can be described by
+    `HH_n(A, M) = \operatorname{Tor}_n^{A^e}(A, M)`, where
+    `A^e = A \otimes A^o` (`A^o` is the opposite algebra of `A`)
+    is the enveloping algebra of `A`.
 
     *Hochschild cohomology* is the homology of the dual complex and
-    can be described by `HH^n(A, M) = \operatorname{Ext}^n_A(A, M)`.
+    can be described by `HH^n(A, M) = \operatorname{Ext}^n_{A^e}(A, M)`.
+
+    Another perspective on Hochschild homology is that `f_{n,i}`
+    make the family `C_n(A, M)` a simplicial object in the
+    category of `R`-modules, and the degeneracy maps are
+
+    .. MATH::
+
+        s_i(a_0 \otimes \cdots \otimes a_i \otimes 1 \otimes a_{i+1}
+        \otimes \cdots \otimes a_n).
+
+    The Hochschild homology can also be constructed as the homology
+    of this simplicial module.
 
     REFERENCES:
 
     - :wikipedia:`Hochschild_homology`
+    - https://ncatlab.org/nlab/show/Hochschild+cohomology
+
+    .. [Redondo] Maria Julia Redondo.
+       *Hochschild cohomology: some methods for computations*.
+       http://inmabb.criba.edu.ar/gente/mredondo/crasp.pdf
     """
     def __init__(self, A, M):
         """
         Initialize ``self``.
 
         EXAMPLES::
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: T = SGA.trivial_representation()
+            sage: H = SGA.hochschild_complex(T)
+
+        We skip the category test because the category containment
+        tests assumes ``H`` is an instance of :class:`Parent`::
+
+            sage: TestSuite(H).run(skip="_test_category")
+            sage: H.category() == ChainComplexes(QQ)
+            True
+            sage: H in ChainComplexes(QQ) # known bug
+            True
         """
         self._A = A
         self._M = M
-        Parent.__init__(self, category=ChainComplexes(A.base_ring()))
+        CategoryObject.__init__(self, category=ChainComplexes(A.base_ring()))
 
     def _repr_(self):
         """
         Return a string representation of ``self``.
 
         EXAMPLES::
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: T = SGA.trivial_representation()
+            sage: T.rename("Trivial representation of SGA")
+            sage: SGA.hochschild_complex(T)
+            Hochschild complex of Symmetric group algebra of order 3 over Rational Field
+             with coefficients in Trivial representation of SGA
+            sage: T.rename()  # reset the name
         """
         return "Hochschild complex of {} with coefficients in {}".format(self._A, self._M)
 
+    def _latex_(self):
+        r"""
+        Return a latex representation of ``self``.
+
+        EXAMPLES::
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: T = SGA.trivial_representation()
+            sage: H = SGA.hochschild_complex(T)
+            sage: latex(H)
+            C_{\bullet}\left(..., ...\right)
+        """
+        from sage.misc.latex import latex
+        return "C_{{\\bullet}}\\left({}, {}\\right)".format(latex(self._A), latex(self._M))
+
+    def algebra(self):
+        """
+        Return the defining algebra of ``self``.
+
+        EXAMPLES::
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: T = SGA.trivial_representation()
+            sage: H = SGA.hochschild_complex(T)
+            sage: H.algebra()
+            Symmetric group algebra of order 3 over Rational Field
+        """
+        return self._A
+
+    def coefficients(self):
+        """
+        Return the coefficients of ``self``.
+
+        EXAMPLES::
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: T = SGA.trivial_representation()
+            sage: H = SGA.hochschild_complex(T)
+            sage: H.coefficients()
+            Trivial representation of Standard permutations of 3 over Rational Field
+        """
+        return self._M
+
     def free_module(self, d):
         """
-        Return the free module in degree ``d``
+        Return the free module in degree ``d``.
+
+        EXAMPLES::
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: T = SGA.trivial_representation()
+            sage: H = SGA.hochschild_complex(T)
+            sage: H.free_module(0)
+            Trivial representation of Standard permutations of 3 over Rational Field
+            sage: H.free_module(1)
+            Trivial representation of Standard permutations of 3 over Rational Field
+             # Symmetric group algebra of order 3 over Rational Field
+            sage: H.free_module(2)
+            Trivial representation of Standard permutations of 3 over Rational Field
+             # Symmetric group algebra of order 3 over Rational Field
+             # Symmetric group algebra of order 3 over Rational Field
         """
+        if d < 0:
+            raise ValueError("only defined for non-negative degree")
         return tensor([self._M] + [self._A]*d)
 
     @cached_method
     def trivial_module(self):
         """
         Return the trivial module of ``self``.
+
+        EXAMPLES::
+
+            sage: E.<x,y> = ExteriorAlgebra(QQ)
+            sage: H = E.hochschild_complex(E)
+            sage: H.trivial_module()
+            Free module generated by {} over Rational Field
         """
         return CombinatorialFreeModule(self._A.base_ring(), [])
 
@@ -96,13 +206,59 @@ class HochschildComplex(UniqueRepresentation, Parent):
 
         EXAMPLES::
 
+            sage: E.<x,y> = ExteriorAlgebra(QQ)
+            sage: H = E.hochschild_complex(E)
+            sage: d1 = H.boundary(1)
+            sage: x = d1.domain().an_element(); x
+            2*1 # 1 + 2*1 # x + 3*1 # y
+            sage: d1(x)
+            0
+            sage: d1.matrix()
+            [ 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0]
+            [ 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0]
+            [ 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0]
+            [ 0  0  0  0  0  0  2  0  0 -2  0  0  0  0  0  0]
+
+            sage: s = SymmetricFunctions(QQ).s()
+            sage: H = s.hochschild_complex(s)
+            sage: d1 = H.boundary(1)
+            sage: x = d1.domain().an_element(); x
+            2*s[] # s[] + 2*s[] # s[1] + 3*s[] # s[2]
+            sage: d1(x)
+            0
+            sage: y = tensor([s.an_element(), s.an_element()])
+            sage: d1(y)
+            0
+            sage: z = tensor([s[2,1] + s[3], s.an_element()])
+            sage: d1(z)
+            0
+
+        TESTS::
+
+            sage: def test_complex(H, n):
+            ....:     phi = H.boundary(n)
+            ....:     psi = H.boundary(n+1)
+            ....:     comp = phi * psi
+            ....:     zero = H.free_module(n-1).zero()
+            ....:     return all(comp(b) == zero for b in H.free_module(n+1).basis())
+
             sage: SGA = SymmetricGroupAlgebra(QQ, 3)
-            sage: H = HochschildComplex(SGA, SGA)
-            sage: phi = H.boundary(2)
-            sage: psi = H.boundary(3)
-            sage: comp = phi * psi
-            sage: for b in H.free_module(3).basis():
-            ....:     assert comp(b) == 0
+            sage: H = SGA.hochschild_complex(SGA)
+            sage: test_complex(H, 1)
+            True
+            sage: test_complex(H, 2)
+            True
+            sage: test_complex(H, 3) # long time
+            True
+
+            sage: E.<x,y> = ExteriorAlgebra(QQ)
+            sage: H = E.hochschild_complex(E)
+            sage: test_complex(H, 1)
+            True
+            sage: test_complex(H, 2)
+            True
+            sage: test_complex(H, 3)
+            True
         """
         R = self._A.base_ring()
         one = R.one()
@@ -129,6 +285,58 @@ class HochschildComplex(UniqueRepresentation, Parent):
     def coboundary(self, d):
         """
         Return the coboundary morphism of degree ``d``.
+
+        EXAMPLES::
+
+            sage: E.<x,y> = ExteriorAlgebra(QQ)
+            sage: H = E.hochschild_complex(E)
+            sage: del1 = H.coboundary(1)
+            sage: x = del1.domain().an_element(); x
+            2 + 2*x + 3*y
+            sage: del1(x)
+            0
+            sage: del1.matrix()
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  2]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0 -2]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+            [ 0  0  0  0]
+
+        TESTS::
+
+            sage: def test_complex(H, n):
+            ....:     phi = H.coboundary(n)
+            ....:     psi = H.coboundary(n+1)
+            ....:     comp = psi * phi
+            ....:     zero = H.free_module(n+1).zero()
+            ....:     return all(comp(b) == zero for b in H.free_module(n-1).basis())
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: H = SGA.hochschild_complex(SGA)
+            sage: test_complex(H, 1)
+            True
+            sage: test_complex(H, 2)
+            True
+
+            sage: E.<x,y> = ExteriorAlgebra(QQ)
+            sage: H = E.hochschild_complex(E)
+            sage: test_complex(H, 1)
+            True
+            sage: test_complex(H, 2)
+            True
+            sage: test_complex(H, 3)
+            True
         """
         if self._A.category() is not self._A.category().FiniteDimensional():
             raise NotImplementedError("the algebra must be finite dimensional")
@@ -140,9 +348,38 @@ class HochschildComplex(UniqueRepresentation, Parent):
     def homology(self, d):
         """
         Return the ``d``-th homology group.
+
+        EXAMPLES::
+
+            sage: E.<x,y> = ExteriorAlgebra(QQ)
+            sage: H = E.hochschild_complex(E)
+            sage: H.homology(0)
+            Vector space of dimension 3 over Rational Field
+            sage: H.homology(1)
+            Vector space of dimension 4 over Rational Field
+            sage: H.homology(2)
+            Vector space of dimension 6 over Rational Field
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: T = SGA.trivial_representation()
+            sage: H = SGA.hochschild_complex(T)
+            sage: H.homology(0)
+            Vector space of dimension 1 over Rational Field
+            sage: H.homology(1)
+            Vector space of dimension 0 over Rational Field
+            sage: H.homology(2)
+            Vector space of dimension 0 over Rational Field
         """
         if self._A.category() is not self._A.category().FiniteDimensional():
             raise NotImplementedError("the algebra must be finite dimensional")
+
+        maps = {d: self.boundary(d).matrix(), d+1: self.boundary(d+1).matrix()}
+        C = ChainComplex(maps, degree_of_differential=-1)
+        try:
+            return C.homology(d)
+        except NotImplementedError:
+            pass
+        # Fallback if we are not working over a field or \ZZ
         bdry = self.boundary(d)
         bdry1 = self.boundary(d+1)
         ker = bdry.kernel()
@@ -152,43 +389,41 @@ class HochschildComplex(UniqueRepresentation, Parent):
     def cohomology(self, d):
         """
         Return the ``d``-th cohomology group.
+
+        EXAMPLES::
+
+            sage: E.<x,y> = ExteriorAlgebra(QQ)
+            sage: H = E.hochschild_complex(E)
+            sage: H.cohomology(0)
+            Vector space of dimension 3 over Rational Field
+            sage: H.cohomology(1)
+            Vector space of dimension 4 over Rational Field
+            sage: H.cohomology(2)
+            Vector space of dimension 6 over Rational Field
+
+            sage: SGA = SymmetricGroupAlgebra(QQ, 3)
+            sage: T = SGA.trivial_representation()
+            sage: H = SGA.hochschild_complex(T)
+            sage: H.cohomology(0)
+            Vector space of dimension 1 over Rational Field
+            sage: H.cohomology(1)
+            Vector space of dimension 0 over Rational Field
+            sage: H.cohomology(2)
+            Vector space of dimension 0 over Rational Field
         """
+        if self._A.category() is not self._A.category().FiniteDimensional():
+            raise NotImplementedError("the algebra must be finite dimensional")
+
+        maps = {d+1: self.coboundary(d+1).matrix(), d: self.coboundary(d).matrix()}
+        C = ChainComplex(maps, degree_of_differential=1)
+        try:
+            return C.homology(d+1)
+        except NotImplementedError:
+            pass
+        # Fallback if we are not working over a field or \ZZ
         cb = self.coboundary(d)
         cb1 = self.coboundary(d+1)
         ker = cb1.kernel()
         im_retract = ker.submodule([ker.retract(b) for b in cb.image_basis()])
         return ker.quotient_module(im_retract)
-
-# This is superceded by #19613
-class TrivialRepresentation(CombinatorialFreeModule):
-    def __init__(self, GA):
-        self._group_algebra = GA
-        CombinatorialFreeModule.__init__(self, GA.base_ring(), ['v'])
-
-    def _repr_(self):
-        return "Trivial representation of {}".format(self._group_algebra)
-
-    class Element(CombinatorialFreeModule.Element):
-        def _acted_upon_(self, scalar, self_on_left=False):
-            """
-            Return the action of ``scalar`` on ``self``.
-
-            EXAMPLES::
-
-                sage: SGA = SymmetricGroupAlgebra(QQ, 3)
-                sage: V = TrivialRepresentation(SGA)
-                sage: x = V.an_element()
-                sage: all(x * b == x for b in SGA.basis())
-                True
-                sage: all(b * x == x for b in SGA.basis())
-                True
-            """
-            if (isinstance(scalar, Element)
-                    and scalar.parent() is self.parent()._group_algebra):
-                d = self.monomial_coefficients(copy=True)
-                d['v'] *= sum(scalar.coefficients())
-                return self.parent()._from_dict(d)
-            return CombinatorialFreeModule.Element._acted_upon_(scalar, self_on_left)
-
-        _rmul_ = _lmul_ = _acted_upon_
 

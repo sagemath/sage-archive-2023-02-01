47bc138c13530e553a22642be9de6efb8fdcd80c
Volker Braun <vbraun.name@gmail.com>
2016-02-14 00:34:37 +0100

    Add explanation to the operations.py module docstring

--- src/sage/libs/gap/operations.py
+++ src/sage/libs/gap/operations.py
@@ -1,5 +1,12 @@
 """
 Operations for LibGAP Elements
+
+GAP functions for which several methods can be available are called
+operations, so GAP ``Size`` is an example of an operation. This module
+is for inspecting GAP operations from Python. In particular, it can
+list the operations that take a particular LibGAP element as first
+argument. This is used in tab completion, where Python ``x.[TAB]``
+lists all GAP operations for which ``Operation(x, ...)`` is defined.
 """
 
 import re

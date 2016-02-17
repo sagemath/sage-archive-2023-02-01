93c3159fb17ff129fc3121c3f3f1672f45406439
Volker Braun <vbraun.name@gmail.com>
2016-02-17 22:32:26 +0100

    Add explanation to ExtraTabCompletion

--- src/sage/interfaces/tab_completion.py
+++ src/sage/interfaces/tab_completion.py
@@ -1,10 +1,20 @@
 """
 Mixin For Extra Tab Completions
 
+The :class:`ExtraTabCompletion` class helps you to extend the tab
+completions for objects. This is used in interfaces with third-party
+interpreters where we want, for example, ``gap.[TAB]`` to also list
+GAP commands and not only the Python methods of the gap interface. To
+use it, just inherit (usually by multiple inheritance) from
+:class:`ExtraTabCompletion` and implement a ``_tab_completion`` method
+that returns a list of strings. These strings are then included in the
+tab completions on instances. It is up to you to make these "fake"
+method names work, usually by implementing ``__getattr__``.
+
 EXAMPLES::
 
     sage: from sage.interfaces.tab_completion import ExtraTabCompletion
-    sage: class Foo(ExtraTabCompletion):
+    sage: class Foo(ExtraTabCompletion, object):
     ....:      def a(self):
     ....:          return 1
     ....:      def _tab_completion(self):

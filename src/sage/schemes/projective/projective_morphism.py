7fa8ed43ebd95ad9a4cb7ebe85f17d9d5ceb6c27
Vincent Delecroix <20100.delecroix@gmail.com>
2016-02-17 09:15:31 -0300

    Trac 20074: fix doctests

--- src/sage/schemes/projective/projective_morphism.py
+++ src/sage/schemes/projective/projective_morphism.py
@@ -3107,10 +3107,12 @@ class SchemeMorphism_polynomial_projective_space(SchemeMorphism_polynomial):
             sage: K.<w> = NumberField(z^4 - 4*z^2 + 1,'z')
             sage: P.<x,y> = ProjectiveSpace(K,1)
             sage: H = End(P)
-            sage: f = H([x^2 - w/4*y^2, y^2])
-            sage: f.multiplier_spectra(2, False)
-            [0, 5.931851652578137? + 0.?e-17*I, 0.0681483474218635? - 1.930649271699173?*I,
-            0.0681483474218635? + 1.930649271699173?*I]
+            sage: f = H([x^2 - w/4*y^2,y^2])
+            sage: f.multiplier_spectra(2,False)
+            [0,
+             5.931851652578137? + 0.?e-49*I,
+             0.0681483474218635? - 1.930649271699173?*I,
+             0.0681483474218635? + 1.930649271699173?*I]
 
         ::
 

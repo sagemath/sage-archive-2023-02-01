7d4e719430c405b70af4931469672b77771fed7d
Benjamin Hackl <benjamin.hackl@aau.at>
2016-02-22 12:02:10 +0200

    improve cos: reduce by 2*k*pi first

--- src/sage/symbolic/ginac/inifcns_trig.cpp
+++ src/sage/symbolic/ginac/inifcns_trig.cpp
@@ -239,8 +239,20 @@ static ex cos_eval(const ex & x)
 		throw (std::runtime_error("cos_eval(): cos(infinity) encountered"));
 	}
 
+	ex coef_pi = x.coeff(Pi).expand();
+	ex rem = _ex0;
+	if (is_exactly_a<add>(coef_pi)){
+		for (size_t i=0; i < coef_pi.nops(); i++){
+			if ((coef_pi.op(i) / _ex2).info(info_flags::integer))
+				rem += Pi * coef_pi.op(i);
+		}
+	}
+	else if ((coef_pi / _ex2).info(info_flags::integer))
+		rem = Pi * coef_pi;
+	ex const x_red = x - rem;
+
 	// cos(n/d*Pi) -> { all known radicals with nesting depth 2 }
-	const ex SixtyExOverPi = _ex60*x/Pi;
+	const ex SixtyExOverPi = _ex60*x_red/Pi;
 	ex sign = _ex1;
 	if (is_exactly_a<numeric>(SixtyExOverPi)
                 and SixtyExOverPi.info(info_flags::integer)) {
@@ -286,7 +298,7 @@ static ex cos_eval(const ex & x)
 			return _ex0;
 	}
 
-	const ex TwentyforExOverPi = _ex24*x/Pi;
+	const ex TwentyforExOverPi = _ex24*x_red/Pi;
         sign = _ex1;
 	if (is_exactly_a<numeric>(TwentyforExOverPi)
                         and TwentyforExOverPi.info(info_flags::integer)) {
@@ -314,36 +326,36 @@ static ex cos_eval(const ex & x)
 			return sign*(_ex1_4*sqrt(_ex8 - _ex2*sqrt(_ex6) - _ex2*sqrt(_ex2)));
 	}
 
-	if (is_exactly_a<function>(x)) {
-		const ex &t = x.op(0);
+	if (is_exactly_a<function>(x_red)) {
+		const ex &t = x_red.op(0);
 
 		// cos(acos(x)) -> x
-		if (is_ex_the_function(x, acos))
+		if (is_ex_the_function(x_red, acos))
 			return t;
 
 		// cos(asin(x)) -> sqrt(1-x^2)
-		if (is_ex_the_function(x, asin))
+		if (is_ex_the_function(x_red, asin))
 			return sqrt(_ex1-power(t,_ex2));
 
 		// cos(atan(x)) -> 1/sqrt(1+x^2)
-		if (is_ex_the_function(x, atan))
+		if (is_ex_the_function(x_red, atan))
 			return power(_ex1+power(t,_ex2),_ex_1_2);
 	}
 
 	// cos(integer*pi) --> (-1)^integer
-	const ex ExOverPi = x/Pi;
+	const ex ExOverPi = x_red/Pi;
 	if (ExOverPi.info(info_flags::integer))
 		return pow(_ex_1, ExOverPi);
 	
 	// cos(float) -> float
-	if (x.info(info_flags::numeric) && !x.info(info_flags::crational))
-		return cos(ex_to<numeric>(x));
+	if (x_red.info(info_flags::numeric) && !x_red.info(info_flags::crational))
+		return cos(ex_to<numeric>(x_red));
 	
 	// cos() is even
-	if (x.info(info_flags::negative))
-		return cos(-x);
+	if (x_red.info(info_flags::negative))
+		return cos(-x_red);
 	
-	return cos(x).hold();
+	return cos(x_red).hold();
 }
 
 static ex cos_deriv(const ex & x, unsigned deriv_param)

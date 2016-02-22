8cf82427364f6b8e9a9a741dceea5469ea3f9b41
Benjamin Hackl <benjamin.hackl@aau.at>
2016-02-22 12:06:54 +0200

    simlify tangent too (remove integer*pi before evaluation)

--- src/sage/symbolic/ginac/inifcns_trig.cpp
+++ src/sage/symbolic/ginac/inifcns_trig.cpp
@@ -411,8 +411,21 @@ static ex tan_eval(const ex & x)
 		throw (std::runtime_error("tan_eval(): tan(infinity) encountered"));
 	}
 
+	ex coef_pi = x.coeff(Pi).expand();
+	ex rem = _ex0;
+	if (is_exactly_a<add>(coef_pi)){
+		for (size_t i=0; i < coef_pi.nops(); i++){
+			if (coef_pi.op(i).info(info_flags::integer))
+				rem += Pi * coef_pi.op(i);
+		}
+	}
+	else if (coef_pi.info(info_flags::integer))
+		rem = Pi * coef_pi;
+	ex const x_red = x - rem;
+
+
 	// tan(n/d*Pi) -> { all known non-nested radicals }
-	const ex SixtyExOverPi = _ex60*x/Pi;
+	const ex SixtyExOverPi = _ex60*x_red/Pi;
 	ex sign = _ex1;
 	if (is_exactly_a<numeric>(SixtyExOverPi)
                 and SixtyExOverPi.info(info_flags::integer)) {
@@ -459,7 +472,7 @@ static ex tan_eval(const ex & x)
 			return UnsignedInfinity;
 	}
 
-	const ex FortyeightExOverPi = _ex48*x/Pi;
+	const ex FortyeightExOverPi = _ex48*x_red/Pi;
         sign = _ex1;
 	if (is_exactly_a<numeric>(FortyeightExOverPi)
                         and FortyeightExOverPi.info(info_flags::integer)) {
@@ -495,37 +508,32 @@ static ex tan_eval(const ex & x)
 			return sign*(_ex2+sqrt(_ex6)+sqrt(_ex2)+sqrt(_ex3));
 	}
 
-	if (is_exactly_a<function>(x)) {
-		const ex &t = x.op(0);
+	if (is_exactly_a<function>(x_red)) {
+		const ex &t = x_red.op(0);
 
 		// tan(atan(x)) -> x
-		if (is_ex_the_function(x, atan))
+		if (is_ex_the_function(x_red, atan))
 			return t;
 
 		// tan(asin(x)) -> x/sqrt(1+x^2)
-		if (is_ex_the_function(x, asin))
+		if (is_ex_the_function(x_red, asin))
 			return t*power(_ex1-power(t,_ex2),_ex_1_2);
 
 		// tan(acos(x)) -> sqrt(1-x^2)/x
-		if (is_ex_the_function(x, acos))
+		if (is_ex_the_function(x_red, acos))
 			return power(t,_ex_1)*sqrt(_ex1-power(t,_ex2));
 	}
-	
-	// tan(integer*pi) -> 0
-	const ex ExOverPi = x/Pi;
-	if (ExOverPi.info(info_flags::integer))
-		return _ex0;
 
 	// tan(float) -> float
-	if (x.info(info_flags::numeric) && !x.info(info_flags::crational)) {
-		return tan(ex_to<numeric>(x));
+	if (x_red.info(info_flags::numeric) && !x_red.info(info_flags::crational)) {
+		return tan(ex_to<numeric>(x_red));
 	}
 	
 	// tan() is odd
-	if (x.info(info_flags::negative))
-		return -tan(-x);
+	if (x_red.info(info_flags::negative))
+		return -tan(-x_red);
 	
-	return tan(x).hold();
+	return tan(x_red).hold();
 }
 
 static ex tan_deriv(const ex & x, unsigned deriv_param)

1381a89d5e960afb76815f85e771f71f99a3d1c1
Ralf Stephan <ralf@ark.in-berlin.de>
2016-03-10 15:05:10 +0100

    16397: add tests

--- src/sage/symbolic/expression.pyx
+++ src/sage/symbolic/expression.pyx
@@ -3459,6 +3459,23 @@ cdef class Expression(CommutativeRingElement):
             I*x - 1/2
             sage: t.subs(x=I*x).subs(x=0).is_positive()
             False
+
+        Check if :trac:`16397` is fixed:
+
+            sage: cmp(1, sqrt(2))
+            -1
+            sage: cmp(SR(1), sqrt(2))
+            -1
+            sage: cmp(log(8), 3*log(2))
+            0
+            sage: RLF(1) < RLF(sqrt(2))
+            True
+            sage: RealSet((0, pi),[pi, pi],(pi,4))
+            (0, 4)
+            sage: RealSet((0, pi),[0, pi],(pi,4))
+            [0, 4)
+            sage: RealSet((0, pi),[0, 3.5],(pi,4))
+            [0, 4)
         """
         return mixed_order(left, right)
 

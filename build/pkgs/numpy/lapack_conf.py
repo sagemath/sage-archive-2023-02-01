7808e833c2a270292a031c456eed7b518aa311dc
François Bissey <francois.bissey@canterbury.ac.nz>
2016-03-11 18:46:03 +1300

    Small improvement and cleaning in lapack_conf.py

--- build/pkgs/numpy/lapack_conf.py
+++ build/pkgs/numpy/lapack_conf.py
@@ -4,17 +4,22 @@ import pkgconfig, os
 
 conf_file=open('site.cfg', 'w')
 
-conf_file.write('[DEFAULT]\n')
+conf_file.write('[ALL]\n')
 conf_file.write('library_dirs = '+ os.environ['SAGE_LOCAL']+ '/lib\n')
 conf_file.write('include_dirs = '+ os.environ['SAGE_LOCAL']+ '/include\n')
 
+pc_blas   = pkgconfig.parse('cblas blas')
+pc_lapack = pkgconfig.parse('lapack')
+
 if not (os.environ['UNAME'] == 'Darwin'):
     conf_file.write('[blas]\n')
-    conf_file.write('include_dirs = '+ ':'.join(list(pkgconfig.parse('cblas')['include_dirs']))+'\n')
-    conf_file.write('library_dirs = '+ ':'.join(list(pkgconfig.parse('blas cblas')['library_dirs']))+'\n')
-    conf_file.write('blas_libs    = '+', '.join(list(pkgconfig.parse('blas cblas')['libraries']))+'\n')
+    inc_dir = pc_blas['include_dirs']
+    if len(inc_dir) > 0 :
+        conf_file.write('include_dirs = '+ ':'.join(list(inc_dir))+'\n')
+    conf_file.write('library_dirs = '+ ':'.join(list(pc_blas['library_dirs']))+'\n')
+    conf_file.write('blas_libs    = '+', '.join(list(pc_blas['libraries']))+'\n')
     conf_file.write('[lapack]\n')
-    conf_file.write('library_dirs = '+ ':'.join(list(pkgconfig.parse('lapack')['library_dirs']))+'\n')
-    conf_file.write('lapack_libs  = '+', '.join(list(pkgconfig.parse('lapack')['libraries']))+'\n')
+    conf_file.write('library_dirs = '+ ':'.join(list(pc_lapack['library_dirs']))+'\n')
+    conf_file.write('lapack_libs  = '+', '.join(list(pc_lapack['libraries']))+'\n')
 
 conf_file.close()

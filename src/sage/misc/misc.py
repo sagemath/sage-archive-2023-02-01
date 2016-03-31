5283cdb8e6d1c6f345d10da2043e0ccc7f02c3ff
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2016-03-31 13:21:24 +0200

    Deprecate sage.misc.all.srange

--- src/sage/misc/misc.py
+++ src/sage/misc/misc.py
@@ -34,6 +34,11 @@ Test deprecation::
     Importing srange from here is deprecated. If you need to use it, please import it directly from sage.arith.srange
     See http://trac.sagemath.org/20094 for details.
     [0, 1, 2, 3, 4]
+    sage: sage.misc.all.srange(5)
+    doctest:...: DeprecationWarning:
+    Importing srange from here is deprecated. If you need to use it, please import it directly from sage.arith.srange
+    See http://trac.sagemath.org/20334 for details.
+    [0, 1, 2, 3, 4]
     sage: sage.misc.misc.sxrange(5)
     doctest:...: DeprecationWarning:
     Importing sxrange from here is deprecated. If you need to use it, please import it directly from sage.arith.srange

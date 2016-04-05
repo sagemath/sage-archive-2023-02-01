b7f1ed8c7e64dad808bf92edaec7d1cb100812b5
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-04-05 09:29:50 -0700

    get_solver: Add another doctest

--- src/sage/numerical/backends/generic_backend.pyx
+++ src/sage/numerical/backends/generic_backend.pyx
@@ -1308,6 +1308,10 @@ cpdef GenericBackend get_solver(constraint_generation = False, solver = None, ba
         <sage.numerical.backends.interactivelp_backend.InteractiveLPBackend object at ...>
         sage: p.base_ring()
         Number Field in sqrt5 with defining polynomial x^2 - 5
+        sage: p = get_solver('InteractiveLP', base_ring=QQ)
+        <sage.numerical.backends.interactivelp_backend.InteractiveLPBackend object at ...>
+        sage: p.base_ring()
+        Rational Field
     """
     if solver is None:
 

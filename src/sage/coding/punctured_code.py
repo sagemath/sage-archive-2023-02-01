21b357081ae6373a4c66b481afa0229aef05f12a
David Lucas <david.lucas@inria.fr>
2016-04-16 16:59:01 +0200

    Added generic decoders as available decoders for punctured codes

--- src/sage/coding/punctured_code.py
+++ src/sage/coding/punctured_code.py
@@ -16,7 +16,9 @@ on the `i`-th position.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
-from linear_code import AbstractLinearCode
+from linear_code import (AbstractLinearCode,
+                         LinearCodeSyndromeDecoder,
+                         LinearCodeNearestNeighborDecoder)
 from encoder import Encoder
 from decoder import Decoder, DecodingError
 from sage.misc.cachefunc import cached_method
@@ -660,3 +662,5 @@ class PuncturedCodeOriginalCodeDecoder(Decoder):
 PuncturedCode._registered_encoders["PuncturedMatrix"] = PuncturedCodePuncturedMatrixEncoder
 PuncturedCode._registered_decoders["OriginalCode"] = PuncturedCodeOriginalCodeDecoder
 PuncturedCodeOriginalCodeDecoder._decoder_type = {"dynamic"}
+PuncturedCode._registered_decoders["Syndrome"] = LinearCodeSyndromeDecoder
+PuncturedCode._registered_decoders["NearestNeighbor"] = LinearCodeNearestNeighborDecoder

a2e7ea3f0debda695c0c13bb8d4577300142d275
Jori Mäntysalo <jori.mantysalo@uta.fi>
2016-04-25 10:10:42 +0300

    Some fixes.

--- src/sage/combinat/posets/poset_examples.py
+++ src/sage/combinat/posets/poset_examples.py
@@ -960,36 +960,37 @@ class Posets(object):
     @staticmethod
     def UpDownPoset(n, m=1):
         r"""
-        Return the up-down poset on ``n`` elements where every ``(m+1)st`` 
+        Return the up-down poset on `n` elements where every `(m+1)`
         step is down and the rest are up.
-    
-        The case where ``m=1`` is sometimes referred to as the zig-zag poset
+
+        The case where `m=1` is sometimes referred to as the zig-zag poset
         or the fence.
-    
+
         INPUT:
     
         - ``n`` - nonnegative integer, number of elements in the poset
         - ``m`` - nonnegative integer (default 1), how frequently down steps occur
-    
+
         OUTPUT:
-    
-        The partially ordered set on \{ 0, 1, ... , ``n-1`` \}
-        where ``i>i+1`` if ``i+1`` is 0 ``mod m``, and ``i<i+1`` otherwise.
-        
+
+        The partially ordered set on `\{ 0, 1, \ldots, n-1 \}`
+        where `i` covers `i+1` if `m` divides `i+1`, and `i+1` covers `i`
+        otherwise.
+
         EXAMPLES::
-    
-            sage: P = UpDownPoset(2, 7); P
+
+            sage: P = Posets.UpDownPoset(7, 2); P
             Finite poset containing 7 elements
             sage: sorted(P.cover_relations())
             [[0, 1], [1, 2], [3, 2], [3, 4], [4, 5], [6, 5]]
-    
+
         Fibonacci numbers as the number of antichains of a poset::
-    
+
             sage: [len(Posets.UpDownPoset(n).antichains().list()) for n in range(0, 6)]
             [1, 2, 3, 5, 8, 13]
-    
-        TESTS:
-    
+
+        TESTS::
+
             sage: P = Posets.UpDownPoset(0); P
             Finite poset containing 0 elements
         """
@@ -1005,7 +1006,7 @@ class Posets(object):
             raise TypeError("parameter m must be an integer, not {0}".format(m))
         if m < 1:
             raise ValueError("parameter m must be positive, not {0}".format(m))
-    
+
         return Poset( (range(0,n), [[i,i+1] for i in range(n-1) if (i+1) % (m+1) != 0]+[[i+1,i] for i in range(n-1) if (i+1) % (m+1) == 0]) )
 
     @staticmethod

01a9efe93e35fb4e49e0215317bef2edafa53682
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-04-29 00:52:22 +0200

    18265: fixed the copyright headers of the new files

--- src/sage/categories/j_trivial_semigroups.py
+++ src/sage/categories/j_trivial_semigroups.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
 r"""
 J-Trivial Semigroups
 """
 #*****************************************************************************
-#  Copyright (C) 2009-2010 Florent Hivert <florent.hivert at univ-rouen.fr>
-#                2009-2015 Nicolas M. Thiery <nthiery at users.sf.net>
+#  Copyright (C) 2016 Nicolas M. Thiéry <nthiery at users.sf.net>
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #                  http://www.gnu.org/licenses/
-#******************************************************************************
+#*****************************************************************************
 
 from sage.categories.category_with_axiom import CategoryWithAxiom
 from sage.categories.semigroups import Semigroups

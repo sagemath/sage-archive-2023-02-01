6158f0ebc7c6de20451abf0a7f69985dbac4ea6a
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-05-06 23:20:17 +0200

    18265: added the (dummy) implication H-trivial -> J-trivial for groups

--- src/sage/categories/h_trivial_semigroups.py
+++ src/sage/categories/h_trivial_semigroups.py
@@ -28,3 +28,36 @@ class HTrivialSemigroups(CategoryWithAxiom):
             True
         """
         return [Semigroups().Aperiodic()]
+
+    def Inverse_extra_super_categories(self):
+        r"""
+        Implement the fact that an `H`-trivial inverse semigroup is `J`-trivial.
+
+        .. TODO::
+
+            Generalization for inverse semigroups.
+
+            Recall that there are two invertibility axioms for a semigroup `S`:
+
+            - One stating the existence, for all `x`, of a local inverse
+              `y` satisfying `x=xyx` and `y=yxy`;
+            - One stating the existence, for all `x`, of a global
+              inverse `y` satisfying `xy=yx=1`, where `1` is the unit
+              of `S` (which must of course exist).
+
+            It is sufficient to have local inverses for `H`-triviality
+            to imply `J`-triviality. However, at this stage, only the
+            second axiom is implemented in Sage (see
+            :meth:`Magmas.Unital.SubcategoryMethods.Inverse`). Therefore
+            this fact is only implemented for semigroups with global
+            inverses, that is groups. However the trivial group is the
+            unique `H`-trivial group, so this is rather boring.
+
+        EXAMPLES::
+
+            sage: Semigroups().HTrivial().Inverse_extra_super_categories()
+            [Category of j trivial semigroups]
+            sage: Monoids().HTrivial().Inverse()
+            Category of h trivial groups
+        """
+        return [self.JTrivial()]

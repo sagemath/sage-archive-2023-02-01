9659311ae23f9ce450c2f8a66cc5546c88f0fc4e
Ralf Stephan <ralf@ark.in-berlin.de>
2016-05-09 08:27:18 +0200

    revert commit 7803ea7b4ccba27b1afabd731354bc71c2a1169b; revert regression

--- src/sage/symbolic/ginac/exprseq.cpp
+++ src/sage/symbolic/ginac/exprseq.cpp
@@ -24,6 +24,12 @@
 
 namespace GiNaC {
 
+// Removing this will error Sage with undefined reference to
+// GiNaC::container<std::vector>::reg_info
+template <> GINAC_IMPLEMENT_REGISTERED_CLASS_OPT_T(exprseq, basic,
+  print_func<print_context>(&exprseq::do_print).
+  print_func<print_tree>(&exprseq::do_print_tree))
+
 /** Specialization of container::info() for exprseq. */
 template <> bool exprseq::info(unsigned inf) const
 {
--- src/sage/symbolic/ginac/exprseq.h
+++ src/sage/symbolic/ginac/exprseq.h
@@ -31,6 +31,9 @@ namespace GiNaC {
 
 typedef container<std::vector> exprseq;
 
+/** Specialization of container::get_tinfo() for exprseq. */
+template<> inline tinfo_t exprseq::get_tinfo() { return &exprseq::tinfo_static; }
+
 // defined in exprseq.cpp
 template<> bool exprseq::info(unsigned inf) const;
 

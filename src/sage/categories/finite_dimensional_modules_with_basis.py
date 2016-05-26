2f83f598d7d08ee2002b749e6ba2f81d48983a25
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-05-26 09:38:45 +0200

    #20681: revert accidental addition of blank lines

--- src/sage/categories/finite_dimensional_modules_with_basis.py
+++ src/sage/categories/finite_dimensional_modules_with_basis.py
@@ -271,8 +271,6 @@ class FiniteDimensionalModulesWithBasis(CategoryWithAxiom_over_base_ring):
                                            already_echelonized=already_echelonized)
             return QuotientModuleWithBasis(submodule, category=category)
 
-
-
     class ElementMethods:
         def dense_coefficient_list(self, order=None):
             """

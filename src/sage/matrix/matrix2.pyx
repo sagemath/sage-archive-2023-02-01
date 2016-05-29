10286f8ce024418658c96634087ad35d44129844
Jori Mäntysalo <jori.mantysalo@uta.fi>
2016-05-29 08:15:03 +0300

    Eigendecomposition is only defined for diagonalizable matrices.

--- src/sage/matrix/matrix2.pyx
+++ src/sage/matrix/matrix2.pyx
@@ -9788,8 +9788,9 @@ explicitly setting the argument to `True` or `False` will avoid this message."""
         This computation is performed in a naive way using the ranks of powers
         of `A-xI`, where `x` is an eigenvalue of the matrix `A`.  If desired,
         a transformation matrix `P` can be returned, which is such that the
-        Jordan canonical form is given by `P^{-1} A P`; this is called
-        *eigendecomposition* or *spectral decomposition*.
+        Jordan canonical form is given by `P^{-1} A P`; if the matrix is
+	diagonalizable, this equals to *eigendecomposition* or *spectral
+	decomposition*.
 
         INPUT:
 

32c9e6426cc1dd492590f89d94b2329d7c78b507
Florent Cayré <florent.cayre@logilab.fr>
2016-05-31 21:57:47 +0200

    20690: Only show the activate button when there are cells to be activated

--- src/doc/common/themes/sage/static/thebe-sage.js
+++ src/doc/common/themes/sage/static/thebe-sage.js
@@ -1,16 +1,19 @@
 $(function() {
     if (window.location.protocol.startsWith('http')) {
-        $('<button id="thebe-activate">Activate</button>')
-            .css({position: 'absolute', right: 0})
-            .click(function() {
-                new Thebe({
-                     tmpnb_mode: false,
-                     load_css: false,
-                     url: window.location.origin,
-                     kernel_name: "sagemath",
-                     selector: "pre:contains('sage: ')"
-                });
-            })
-            .prependTo('div.body');
+        var selector = "pre:contains('sage: ')";
+        if ($(selector).length > 0) {
+            $('<button id="thebe-activate">Activate</button>')
+                .css({position: 'absolute', right: 0})
+                .click(function() {
+                    new Thebe({
+                         tmpnb_mode: false,
+                         load_css: false,
+                         url: window.location.origin,
+                         kernel_name: "sagemath",
+                         selector: selector
+                    });
+                })
+                .prependTo('div.body');
+        }
     }
 });

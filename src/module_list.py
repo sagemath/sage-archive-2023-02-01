447ddb3e1b917c936a61109bae73e0993b1c87b9
Arpit Merchant <arpitdm@gmail.com>
2016-06-13 17:21:55 +0530

    Extensions for skew_polynomial_element and skew_polynomial_finite field added.

--- src/module_list.py
+++ src/module_list.py
@@ -1774,12 +1774,12 @@ ext_modules = [
     Extension('*', ['sage/symbolic/*.pyx']),
 
     Extension('sage.rings.polynomial.skew_polynomial_element',
-              sources = ['sage/rings/polynomial/skew_polynomial_element.pyx'],
-              depends = numpy_depends),
+              sources = ['sage/rings/polynomial/skew_polynomial_element.pyx']),
+#              depends = numpy_depends),
 
     Extension('sage.rings.polynomial.skew_polynomial_finite_field',
-              sources = ['sage/rings/polynomial/skew_polynomial_finite_field.pyx'],
-              depends = numpy_depends),
+              sources = ['sage/rings/polynomial/skew_polynomial_finite_field.pyx']),
+#              depends = numpy_depends),
 
     ################################
     ##

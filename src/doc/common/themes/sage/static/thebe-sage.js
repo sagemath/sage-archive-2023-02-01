b5e95e47d85a743c30b498f13823080bbf532ad4
Florent Cayré <florent.cayre@logilab.fr>
2016-06-14 05:02:56 +0200

    20690: Factor out a thebe-sage setup function before complexifying it for upcoming functionalities

--- src/doc/common/themes/sage/static/thebe-sage.js
+++ src/doc/common/themes/sage/static/thebe-sage.js
@@ -1,16 +1,21 @@
 $(function() {
+
+    function setupThebeSage(thebeOptions) {
+        return new Thebe(thebeOptions);
+    }
+
     if (window.location.protocol.startsWith('http')) {
         var selector = "pre:contains('sage: ')";
         if ($(selector).length > 0) {
             $('<button id="thebe-activate">Activate</button>')
                 .css({position: 'absolute', right: 0})
                 .click(function() {
-                    new Thebe({
-                         tmpnb_mode: false,
-                         load_css: false,
-                         url: window.location.origin,
-                         kernel_name: "sagemath",
-                         selector: selector
+                    setupThebeSage({
+                        tmpnb_mode: false,
+                        load_css: false,
+                        url: window.location.origin,
+                        kernel_name: "sagemath",
+                        selector: selector
                     });
                     $(this).attr('disabled', 'disabled');
                 })

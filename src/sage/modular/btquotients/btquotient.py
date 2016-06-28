d94b19ec7f8dd6ef619bb2a47c5233a9718aa6cf
Marc Masdeu <marc.masdeu@gmail.com>
2016-06-28 14:49:21 +0100

    Changed doctest to allow 32/64 bit output.

--- src/sage/modular/btquotients/btquotient.py
+++ src/sage/modular/btquotients/btquotient.py
@@ -1499,8 +1499,10 @@ class BruhatTitsQuotient(SageObject, UniqueRepresentation):
         EXAMPLES::
 
             sage: X = BruhatTitsQuotient(5,13)
-            sage: X._cache_key() # random
-            1375458358400022881
+            sage: X._cache_key()
+            -406423199 # 32-bit
+            1375458358400022881 # 64-bit
+
             sage: Y = BruhatTitsQuotient(5,13,use_magma = True) # optional - magma
             sage: Y._cache_key() == X._cache_key() # optional - magma
             False

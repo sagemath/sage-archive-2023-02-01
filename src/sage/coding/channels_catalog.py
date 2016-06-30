66a538101a1f2239348914bf0a07a7942a4563ea
David Lucas <david.lucas@inria.fr>
2016-06-30 12:59:09 +0200

    Reworked channels_catalog.py

--- src/sage/coding/channels_catalog.py
+++ src/sage/coding/channels_catalog.py
@@ -3,10 +3,9 @@ Index of channels
 
 The ``channels`` object may be used to access the codes that Sage can build.
 
-- :func:`channel_constructions.ErrorErasureChannel <sage.coding.channel_constructions.ErrorErasureChannel>`
-- :func:`channel_constructions.StaticErrorRateChannel <sage.coding.channel_constructions.StaticErrorRateChannel>`
-
-- :func:`channel_constructions.QarySymmetricChannel <sage.coding.channel_constructions.QarySymmetricChannel>`
+- :class:`channel_constructions.ErrorErasureChannel <sage.coding.channel_constructions.ErrorErasureChannel>`
+- :class:`channel_constructions.QarySymmetricChannel <sage.coding.channel_constructions.QarySymmetricChannel>`
+- :class:`channel_constructions.StaticErrorRateChannel <sage.coding.channel_constructions.StaticErrorRateChannel>`
 
 .. NOTE::
 
@@ -15,5 +14,16 @@ The ``channels`` object may be used to access the codes that Sage can build.
         sage: from sage.coding.channels_catalog import *
 
 """
+#*****************************************************************************
+#       Copyright (C) 2015 David Lucas <david.lucas@inria.fr>
+#
+#  Distributed under the terms of the GNU General Public License (GPL),
+#  version 2 or later (at your preference).
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
 
-from channel_constructions import (ErrorErasureChannel, StaticErrorRateChannel, QarySymmetricChannel)
+from sage.misc.lazy_import import lazy_import as _lazy_import
+_lazy_import('sage.coding.channel_constructions', ['ErrorErasureChannel',
+                                                   'QarySymmetricChannel',
+                                                   'StaticErrorRateChannel'])

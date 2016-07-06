38866d432454be77f8705936c1f6930fd39047b5
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2016-07-06 15:12:05 +0200

    trac 20958 fixing one detail

--- src/sage/libs/all.py
+++ src/sage/libs/all.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
-import sage.libs.ntl.all  as ntl
 
-from sage.libs.pari.all   import pari, pari_gen, PariError
+import sage.libs.ntl.all as ntl
 
-from . import symmetrica.all as symmetrica
+from sage.libs.pari.all import pari, pari_gen, PariError
+
+import sage.libs.symmetrica.all as symmetrica
 
 from sage.misc.lazy_import import lazy_import
 lazy_import('sage.libs.gap.libgap', 'libgap')

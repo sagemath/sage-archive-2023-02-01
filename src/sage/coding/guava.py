0b6ff31166fd88eba63ca59ab4aab0c610aeed37
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2016-07-06 13:22:15 +0200

    trac 20952 removing guava Binary Reed-Muller Code

--- src/sage/coding/guava.py
+++ src/sage/coding/guava.py
@@ -36,58 +36,7 @@ from sage.misc.randstate import current_randstate
 from sage.matrix.matrix_space import MatrixSpace
 from sage.rings.finite_rings.finite_field_constructor import FiniteField as GF
 from sage.interfaces.gap import gfq_gap_to_sage
-from sage.groups.perm_gps.permgroup import *
-from linear_code import *
-
-
-def BinaryReedMullerCode(r, k):
-    r"""
-    The binary 'Reed-Muller code' with dimension k and
-    order r is a code with length `2^k` and minimum distance `2^k-r`
-    (see for example, section 1.10 in [HP]_). By definition, the
-    `r^{th}` order binary Reed-Muller code of length `n=2^m`, for
-    `0 \leq r \leq m`, is the set of all vectors `(f(p)\ |\ p \in GF(2)^m)`,
-    where `f` is a multivariate polynomial of degree at most `r`
-    in `m` variables.
-
-    INPUT:
-
-    - ``r, k`` -- positive integers with `2^k>r`.
-
-    OUTPUT:
-
-    Returns the binary 'Reed-Muller code' with dimension `k` and order `r`.
-
-    EXAMPLE::
-
-        sage: C = codes.BinaryReedMullerCode(2,4); C  # optional - gap_packages (Guava package)
-        Binary Reed-Muller Code of order 2 and number of variables 4
-        sage: C.minimum_distance()              # optional - gap_packages (Guava package)
-        4
-        sage: C.generator_matrix()                       # optional - gap_packages (Guava package)
-        [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
-        [0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1]
-        [0 0 1 1 0 0 1 1 0 0 1 1 0 0 1 1]
-        [0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1]
-        [0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1]
-        [0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1]
-        [0 0 0 0 0 1 0 1 0 0 0 0 0 1 0 1]
-        [0 0 0 0 0 0 0 0 0 1 0 1 0 1 0 1]
-        [0 0 0 0 0 0 1 1 0 0 0 0 0 0 1 1]
-        [0 0 0 0 0 0 0 0 0 0 1 1 0 0 1 1]
-        [0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1]
-
-    AUTHOR: David Joyner (11-2005)
-    """
-    F = GF(2)
-    gap.load_package("guava")
-    gap.eval("C:=ReedMullerCode("+str(r)+", "+str(k)+")")
-    gap.eval("G:=GeneratorMat(C)")
-    k = int(gap.eval("Length(G)"))
-    n = int(gap.eval("Length(G[1])"))
-    G = [[gfq_gap_to_sage(gap.eval("G["+str(i)+"]["+str(j)+"]"),F) for j in range(1,n+1)] for i in range(1,k+1)]
-    MS = MatrixSpace(F,k,n)
-    return LinearCode(MS(G))
+from linear_code import LinearCode
 
 
 def QuasiQuadraticResidueCode(p):
@@ -126,12 +75,12 @@ def QuasiQuadraticResidueCode(p):
     """
     F = GF(2)
     gap.load_package("guava")
-    gap.eval("C:=QQRCode("+str(p)+")")
+    gap.eval("C:=QQRCode(" + str(p) + ")")
     gap.eval("G:=GeneratorMat(C)")
     k = int(gap.eval("Length(G)"))
     n = int(gap.eval("Length(G[1])"))
-    G = [[gfq_gap_to_sage(gap.eval("G[%s][%s]" % (i,j)),F)
-          for j in range(1,n+1)] for i in range(1,k+1)]
+    G = [[gfq_gap_to_sage(gap.eval("G[%s][%s]" % (i, j)), F)
+          for j in range(1, n + 1)] for i in range(1, k + 1)]
     MS = MatrixSpace(F, k, n)
     return LinearCode(MS(G))
 
@@ -169,6 +118,7 @@ def RandomLinearCodeGuava(n, k, F):
     gap.eval("G:=GeneratorMat(C)")
     k = int(gap.eval("Length(G)"))
     n = int(gap.eval("Length(G[1])"))
-    G = [[gfq_gap_to_sage(gap.eval("G[%s][%s]" % (i,j)),F) for j in range(1,n+1)] for i in range(1,k+1)]
+    G = [[gfq_gap_to_sage(gap.eval("G[%s][%s]" % (i, j)), F)
+          for j in range(1, n + 1)] for i in range(1, k + 1)]
     MS = MatrixSpace(F, k, n)
     return LinearCode(MS(G))

d2531cff9ef52d2534b9f7a867b47cd3f3cf0dd6
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2016-08-02 14:22:41 +0200

    trac #21145 fixing one failing doctest

--- src/sage/modular/hecke/module.py
+++ src/sage/modular/hecke/module.py
@@ -1122,8 +1122,9 @@ class HeckeModule_free_module(HeckeModule_generic):
 
         EXAMPLE::
 
-            sage: ModularSymbols(14).cuspidal_subspace().simple_factors()[1].dual_eigenvector()
-            (0, 1, 0, 0, 0)
+            sage: SF = ModularSymbols(14).cuspidal_subspace().simple_factors()
+            sage: sorted([u.dual_eigenvector() for u in SF])
+            [(0, 1, 0, 0, 0), (1, 0, -3, 2, -1)]
         """
         # TODO -- optimize by computing the answer for i not None in terms
         # of the answer for a given i if known !!

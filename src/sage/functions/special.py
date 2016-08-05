a080e1e8824bf56a760d54cbd7e6c9b79c351cef
Travis Scrimshaw <tscrimsh at umn.edu>
2016-08-05 09:25:03 -0500

    Removing uncessary ... in doctest.

--- src/sage/functions/special.py
+++ src/sage/functions/special.py
@@ -233,7 +233,7 @@ class SphericalHarmonic(BuiltinFunction):
 
             sage: spherical_harmonic(3,2,1,2*pi/3)
             1/240*sqrt(30)*(-15*I*sqrt(7)*sqrt(3)
-            ...- 15*sqrt(7))*cos(1)*sin(1)^2/sqrt(pi)
+             - 15*sqrt(7))*cos(1)*sin(1)^2/sqrt(pi)
         """
         if n in ZZ and m in ZZ and n > -1:
             if abs(m) > n:

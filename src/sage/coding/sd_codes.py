7f3536a87975acb8aca109e13696176ae6c2730b
Johan S. R. Nielsen <jsrn@jsrn.dk>
2016-08-18 11:06:19 +0200

    Title for deprecated sd_codes

--- src/sage/coding/sd_codes.py
+++ src/sage/coding/sd_codes.py
@@ -1,3 +1,7 @@
+r"""
+Deprecated name for sage.coding.self_dual_codes
+"""
+
 from sage.misc.superseded import deprecation
 deprecation(21165, "sage.coding.sd_codes is now called sage.coding.self_dual_codes")
 

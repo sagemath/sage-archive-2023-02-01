d57884c4796f72934817150eea6627e5a21c7010
Clement Pernet <clement.pernet@imag.fr>
2016-08-25 00:37:55 +0200

    update standard package list

--- src/sage/misc/package.py
+++ src/sage/misc/package.py
@@ -179,7 +179,7 @@ def list_packages(*pkg_types, **opts):
         sage: sorted(L.keys())
         ['alabaster',
          'arb',
-         'atlas',
+         'babel',
          ...
          'zn_poly',
          'zope_interface']

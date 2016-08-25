a4ccbec7d2d16f43342bb44a7fc756bf7d448081
Daniel Krenn <git@danielkrenn.at>
2016-08-25 17:49:17 +0200

    TestSuite

--- src/sage/combinat/recognizable_series.py
+++ src/sage/combinat/recognizable_series.py
@@ -1465,6 +1465,35 @@ class RecognizableSeriesSpace(UniqueRepresentation, Parent):
 
             sage: RecognizableSeriesSpace(ZZ, [0, 1])
             Space of recognizable series on {0, 1} with coefficients in Integer Ring
+
+        ::
+
+            sage: from itertools import islice
+            sage: Rec = RecognizableSeriesSpace(ZZ, [0, 1])
+            sage: TestSuite(Rec).run(  # long time
+            ....:    verbose=True,
+            ....:    elements=tuple(islice(Rec.some_elements(), 4)))
+            running ._test_additive_associativity() . . . pass
+            running ._test_an_element() . . . pass
+            running ._test_cardinality() . . . pass
+            running ._test_category() . . . pass
+            running ._test_elements() . . .
+              Running the test suite of self.an_element()
+              running ._test_category() . . . pass
+              running ._test_eq() . . . pass
+              running ._test_nonzero_equal() . . . pass
+              running ._test_not_implemented_methods() . . . pass
+              running ._test_pickling() . . . pass
+              pass
+            running ._test_elements_eq_reflexive() . . . pass
+            running ._test_elements_eq_symmetric() . . . pass
+            running ._test_elements_eq_transitive() . . . pass
+            running ._test_elements_neq() . . . pass
+            running ._test_eq() . . . pass
+            running ._test_not_implemented_methods() . . . pass
+            running ._test_pickling() . . . pass
+            running ._test_some_elements() . . . pass
+            running ._test_zero() . . . pass
         """
         self._indices_ = indices
         super(RecognizableSeriesSpace, self).__init__(

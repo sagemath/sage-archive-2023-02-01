019204823624ca730576d6144336337e45e26de6
Jean-Pierre Flori <jean-pierre.flori@ssi.gouv.fr>
2016-08-25 12:22:23 +0000

    Update "arity code" constants for Singular 4.

--- src/sage/libs/singular/function.pyx
+++ src/sage/libs/singular/function.pyx
@@ -1252,7 +1252,7 @@ cdef class SingularFunction(SageObject):
             Traceback (most recent call last):
             ...
             RuntimeError: error in Singular function call 'size':
-            Wrong number of arguments (got 2 arguments, arity code is 347)
+            Wrong number of arguments (got 2 arguments, arity code is 300)
             sage: size('foobar', ring=P)
             6
 
@@ -1655,17 +1655,17 @@ def singular_function(name):
         Traceback (most recent call last):
         ...
         RuntimeError: error in Singular function call 'factorize':
-        Wrong number of arguments (got 0 arguments, arity code is 350)
+        Wrong number of arguments (got 0 arguments, arity code is 303)
         sage: factorize(f, 1, 2)
         Traceback (most recent call last):
         ...
         RuntimeError: error in Singular function call 'factorize':
-        Wrong number of arguments (got 3 arguments, arity code is 350)
+        Wrong number of arguments (got 3 arguments, arity code is 303)
         sage: factorize(f, 1, 2, 3)
         Traceback (most recent call last):
         ...
         RuntimeError: error in Singular function call 'factorize':
-        Wrong number of arguments (got 4 arguments, arity code is 350)
+        Wrong number of arguments (got 4 arguments, arity code is 303)
 
     The Singular function ``list`` can be called with any number of
     arguments::

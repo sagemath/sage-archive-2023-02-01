c62e29530213087592fecee2c82b7b17a1615a08
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2016-08-26 19:06:09 +0200

    trac 21345 and better yet

--- src/sage/graphs/generic_graph.py
+++ src/sage/graphs/generic_graph.py
@@ -10822,7 +10822,7 @@ class GenericGraph(GenericGraph_pyx):
             sage: G.edge_labels()
             ['x', 'z', 'a', 'out']
         """
-        return [l for _, _, l in self.edges()]
+        return [l for _, _, l in self.edge_iterator()]
 
     def remove_multiple_edges(self):
         """

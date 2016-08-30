343131a9951dc842c462809b9366b13416382dfc
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2016-08-30 12:31:07 +0200

    Fix doctest for Trac #20767

--- src/sage/rings/asymptotic/growth_group.py
+++ src/sage/rings/asymptotic/growth_group.py
@@ -633,10 +633,8 @@ class Variable(sage.structure.unique_representation.CachedRepresentation,
             sage: Variable('1/x')._substitute_({'x': 'z'})
             Traceback (most recent call last):
             ...
-            TypeError: Cannot substitute in 1/x in
-            <class 'sage.rings.asymptotic.growth_group.Variable'>.
-            > *previous* TypeError: unsupported operand parent(s) for '/':
-            'Integer Ring' and '<type 'str'>'
+            TypeError: Cannot substitute in 1/x in <class 'sage.rings.asymptotic.growth_group.Variable'>.
+            > *previous* TypeError: unsupported operand type(s) for /: 'sage.rings.integer.Integer' and 'str'
             sage: Variable('1/x')._substitute_({'x': 0})
             Traceback (most recent call last):
             ...

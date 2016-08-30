f6859c31c0322c22d9ad2b6ab21b8e8c8948e16d
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2016-08-30 11:02:45 +0200

    Mark package tests as random

--- src/sage/misc/package.py
+++ src/sage/misc/package.py
@@ -21,7 +21,7 @@ command inside Sage::
 
     sage: from sage.misc.package import list_packages
     sage: pkgs = list_packages(local=True)
-    sage: sorted(pkgs.keys())
+    sage: sorted(pkgs.keys())  # random
     ['4ti2',
      'alabaster',
      'arb',
@@ -176,7 +176,7 @@ def list_packages(*pkg_types, **opts):
 
         sage: from sage.misc.package import list_packages
         sage: L = list_packages('standard')
-        sage: sorted(L.keys())
+        sage: sorted(L.keys())  # random
         ['alabaster',
          'arb',
          'atlas',

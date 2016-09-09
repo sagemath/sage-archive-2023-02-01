2809840d085219903103521bb9a79c74994b2005
Travis Scrimshaw <tscrimsh at umn.edu>
2016-09-09 08:58:17 -0500

    Some small reviewer changes.

--- src/sage/coding/linear_code.py
+++ src/sage/coding/linear_code.py
@@ -2353,14 +2353,17 @@ class AbstractLinearCode(Module):
         Raises a ``ValueError`` in case there is no non-zero vector in this
         linear code.
 
-        Raises a ``NotImplementedError`` if the base field of the code has size 
-        greater than 256.
-
         The minimum distance of the code is stored once it has been
         computed or provided during the initialization of :class:`LinearCode`.
         If ``algorithm`` is ``None`` and the stored value of minimum
         distance is found, then the stored value will be returned without
         recomputing the minimum distance again.
+
+        .. NOTE::
+
+            When using GAP, this raises a ``NotImplementedError`` if
+            the base field of the code has size greater than 256 due
+            to limitations in GAP.
 
         INPUT:
 
@@ -2370,10 +2373,6 @@ class AbstractLinearCode(Module):
         OUTPUT:
 
         - Integer, minimum distance of this code
-
-        .. WARNING::
-
-            Minimum distance calculation only works for codes over fields of size at most 256.
 
         EXAMPLES::
 
@@ -2413,13 +2412,14 @@ class AbstractLinearCode(Module):
             ...
             ValueError: The algorithm argument must be one of None, 'gap' or 'guava'; got 'something'
 
-            The field must be size at most 256::
-            sage: C = codes.RandomLinearCode(5, 2, GF(257,"a"))
+        The field must be size at most 256::
+
+            sage: C = codes.random_linear_code(GF(257,"a"), 5, 2)
             sage: C.minimum_distance()
             Traceback (most recent call last):
             ...
-            NotImplementedError: The GAP algorithms that sage is using
-             are limited to computing with fields of size at most 256.
+            NotImplementedError: the GAP algorithm that Sage is using
+             is limited to computing with fields of size at most 256
         """
         # If the minimum distance has already been computed or provided by
         # the user then simply return the stored value.
@@ -2431,8 +2431,9 @@ class AbstractLinearCode(Module):
         F = self.base_ring()
         q = F.order()
         if q > 256:
-            raise NotImplementedError("The GAP algorithms that sage is using "
-                                      "are limited to computing with fields of size at most 256.")
+            raise NotImplementedError("the GAP algorithm that Sage is using "
+                                      "is limited to computing with fields "
+                                      "of size at most 256")
 
         G = self.generator_matrix()
         n = self.length()

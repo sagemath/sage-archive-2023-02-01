751bd0fbde10aa234867122308c7bb76673cbaba
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-09-15 12:05:53 -0700

    Reword TODO item

--- src/generate_py_source.mk
+++ src/generate_py_source.mk
@@ -4,7 +4,7 @@
 all: sage/libs/pari/auto_gen.pxi sage/ext/interpreters/__init__.py
 
 # Auto-generated files
-# TODO: These belong in SAGE_BUILD_DIR as well.
+# TODO: Adjustments for VPATH builds will be necessary.
 sage/libs/pari/auto_gen.pxi: $(SAGE_LOCAL)/share/pari/pari.desc \
         sage/libs/pari/decl.pxi sage_setup/autogen/pari/*.py
 	python -c "from sage_setup.autogen.pari import rebuild; rebuild()"

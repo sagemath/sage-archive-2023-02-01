21d46ab3f71017d5e982ee6c844b44c6ca2f2528
Erik M. Bray <erik.bray@lri.fr>
2016-09-26 14:51:07 +0200

    On Windows, retry the rename operation a few times, since it can sometimes fail non-deterministically.

--- build/sage_bootstrap/util.py
+++ build/sage_bootstrap/util.py
@@ -1,4 +1,4 @@
-
+import time
 
 
 def is_url(url):
@@ -16,3 +16,24 @@ def is_url(url):
         url.startswith('ftp://')
     )
 
+
+def retry(func, exc=Exception, retries=2, delay=1):
+    """
+    Call ``func()``, and if an exception is raised retry it up to ``retries``
+    times, with a ``delay`` in seconds between each retry.
+
+    To be clear, the function is called up to ``retries + 1`` times.
+
+    If given, ``exc`` can be either an `Exception` or a tuple of `Exception`s
+    in which only those exceptions result in a retry, and all other exceptions
+    are raised.
+    """
+
+    while retries >= 0:
+        try:
+            func()
+        except exc:
+            retries -= 1
+            time.sleep(delay)
+        else:
+            break

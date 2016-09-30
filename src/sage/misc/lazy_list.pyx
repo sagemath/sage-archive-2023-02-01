34ed271a1edd912cbd3f06bc7e2d54e1aa8ed5c3
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2016-09-30 15:47:54 +0200

    trac 21583 one detail

--- src/sage/misc/lazy_list.pyx
+++ src/sage/misc/lazy_list.pyx
@@ -821,7 +821,7 @@ cdef class lazy_list_generic(object):
 
         We check commutation::
 
-            sage: l = lazy_list(iter(range(10000)))
+            sage: l = lazy_list(iter(xrange(10000)))
             sage: l1 = l[::2][:3001]
             sage: l2 = l[:6002][::2]
             sage: l1._info()

5b1b51b8d817f4d47578578bff77e075368f8ef5
Carlos R. Mafra <crmafra@gmail.com>
2016-10-05 19:06:19 +0100

    Fix derivative of gen_laguerre wrt non-implemented variables
    The problem was that the return value was in the 'else'
    branch of diff_param, so anything different than diff_param == 0
    would return the derivative wrt x.
    
    The proper way to do this is to actually check for diff_param == 2
    to return the derivative wrt x. When diff_param == 0 or 1 we
    print a "non-implemented" error message.
    
    Signed-off-by: Carlos R. Mafra <crmafra@gmail.com>

--- src/sage/functions/orthogonal_polys.py
+++ src/sage/functions/orthogonal_polys.py
@@ -1660,8 +1660,10 @@ class Func_laguerre(OrthogonalFunction):
         diff_param = kwds['diff_param']
         if diff_param == 0:
             raise NotImplementedError("Derivative w.r.t. to the index is not supported.")
-        else:
+        if diff_param == 1:
             return -gen_laguerre(n-1,1,x)
+        else:
+            raise ValueError("illegal differentiation parameter {}".format(diff_param))
 
 laguerre = Func_laguerre()
 
@@ -1781,7 +1783,7 @@ class Func_gen_laguerre(OrthogonalFunction):
         from sage.libs.mpmath.all import call as mpcall
         return mpcall(mpmath.laguerre, n, a, x, parent=the_parent)
 
-    def _derivative_(self, n, a, x, *args,**kwds):
+    def _derivative_(self, n, a, x, diff_param):
         """
         Return the derivative of `gen_laguerre(n,a,x)`.
 
@@ -1798,10 +1800,14 @@ class Func_gen_laguerre(OrthogonalFunction):
             ...
             NotImplementedError: Derivative w.r.t. to the index is not supported.
         """
-        diff_param = kwds['diff_param']
         if diff_param == 0:
             raise NotImplementedError("Derivative w.r.t. to the index is not supported.")
-        else:
+        elif diff_param == 1:
+            raise NotImplementedError("Derivative w.r.t. to the second index is not supported.")
+        elif diff_param == 2:
             return -gen_laguerre(n - 1, a + 1, x)
+        else:
+            raise ValueError("illegal differentiation parameter {}".format(diff_param))
+
 
 gen_laguerre = Func_gen_laguerre()

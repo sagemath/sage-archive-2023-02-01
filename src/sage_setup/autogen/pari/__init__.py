a83e25136481efb99d9cb8c00dde3065c0d04894
Erik M. Bray <erik.bray@lri.fr>
2016-10-07 12:04:28 +0000

    Do effectively exactly what the makefile for the autogen sources was doing, but purely in Python, removing the use of make.
    Autogen sources are generated now during the build_ext command just before running Cython (since
    the autogen sources are themselves in Cython mostly).

--- src/sage_setup/autogen/pari/__init__.py
+++ src/sage_setup/autogen/pari/__init__.py
@@ -1,5 +1,21 @@
-from sage_setup.autogen.pari.generator import PariFunctionGenerator
+import glob
+from os.path import join, getmtime, exists
 
-def rebuild():
+from sage_setup.autogen.pari.generator import PariFunctionGenerator
+from sage_setup.autogen.pari.parser import pari_share
+
+def rebuild(force=False):
+    src_files = [join(pari_share(), 'pari.desc'),
+                 join('sage', 'libs', 'pari', 'decl.pxi')] + \
+                glob.glob(join('sage_setup', 'autogen', 'pari', '*.py'))
+    gen_files = [join('sage', 'libs', 'pari', 'auto_gen.pxi')]
+
+    if all(exists(f) for f in gen_files):
+        src_mtime = max(getmtime(f) for f in src_files)
+        gen_mtime = min(getmtime(f) for f in gen_files)
+
+        if gen_mtime > src_mtime and not force:
+            return
+
     G = PariFunctionGenerator()
     G()

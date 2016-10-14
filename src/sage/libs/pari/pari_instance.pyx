eaf5abf1607df49d8247a29f1ab799b5bf3cdde9
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2016-10-14 17:00:20 +0200

    Fix some compiler warnings in PARI interface

--- src/sage/libs/pari/pari_instance.pyx
+++ src/sage/libs/pari/pari_instance.pyx
@@ -393,7 +393,7 @@ cdef void sage_putchar(char c):
     # so it doesn't print one when an error occurs.
     pari_set_last_newline(1)
 
-cdef void sage_puts(char* s):
+cdef void sage_puts(const char* s):
     sys.stdout.write(s)
     pari_set_last_newline(1)
 
@@ -549,7 +549,11 @@ cdef class PariInstance(PariInstance_auto):
         # We deliberately use low-level functions to minimize the
         # chances that something goes wrong here (for example, if we
         # are out of memory).
-        printf("top =  %p\navma = %p\nbot =  %p\nsize = %lu\n", pari_mainstack.top, avma, pari_mainstack.bot, <unsigned long>pari_mainstack.rsize)
+        printf("top =  %p\navma = %p\nbot =  %p\nsize = %lu\n",
+            <void*>pari_mainstack.top,
+            <void*>avma,
+            <void*>pari_mainstack.bot,
+            <unsigned long>pari_mainstack.rsize)
         fflush(stdout)
 
     def __dealloc__(self):

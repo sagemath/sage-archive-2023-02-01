a5666ba570942ef80a16eb5b33047636d95a3166
Anne Schilling <anne@math.ucdavis.edu>
2016-10-26 04:46:25 -0700

    new iterator along Jake's ideas

--- src/sage/combinat/lr_tableau.py
+++ src/sage/combinat/lr_tableau.py
@@ -30,7 +30,7 @@ from sage.categories.finite_enumerated_sets import FiniteEnumeratedSets
 from sage.structure.parent import Parent
 from sage.structure.list_clone import ClonableList
 from sage.combinat.tableau import SemistandardTableau, SemistandardTableaux
-from sage.combinat.partition import Partition
+from sage.combinat.partition import Partition, Partitions
 from sage.libs.symmetrica.all import kostka_tab
 
 class LittlewoodRichardsonTableau(SemistandardTableau):
@@ -210,10 +210,19 @@ class LittlewoodRichardsonTableaux(SemistandardTableaux):
             sage: LR.list()
             [[[1, 1, 3], [2, 3], [4]], [[1, 1, 3], [2, 4], [3]]]
         """
-        exp = [i for a in self._weight for i in a]
-        for t in kostka_tab(self._shape, exp):
-            if is_littlewood_richardson(t, self._heights):
-                yield self.element_class(self, t)
+        from sage.libs.lrcalc.lrcalc import lrskew
+        if len(self._weight) == 0:
+            yield self.element_class(self, [])
+        else:
+            for nu in Partitions(self._shape.size() - self._weight[-1].size(), outer=self._shape):
+                for s in lrskew(self._shape,nu,weight=self._weight[-1]):
+                    for t in LittlewoodRichardsonTableaux(nu,self._weight[:-1]):
+                        shift = sum(a.length() for a in self._weight[:-1])
+                        yield self.element_class(self,_tableau_join(t,s,shift=shift))
+#        exp = [i for a in self._weight for i in a]
+#        for t in kostka_tab(self._shape, exp):
+#            if is_littlewood_richardson(t, self._heights):
+#                yield self.element_class(self, t)
 
     def __contains__(self, t):
         """
@@ -273,117 +282,21 @@ def is_littlewood_richardson(t, heights):
             return False
     return True
 
-# jake's additions here
-
-from sage.libs.lrcalc.lrcalc import lrskew
-import itertools
-from sage.combinat.skew_tableau import SkewTableau
-
-def LRtabs_single(outer, weight, containing_inner=None, shift=0):
+def _tableau_join(t1, t2, shift=0):
     """
-    An iterator for the Yamanouchi skew tableaux of specified
-    outer shape ``outer`` and weight, but with arbitrary inner shape.
+    Join semistandard tableau ``t1`` with semistandard tableau ``t2`` shifted by ``shift``.
 
-    The optional argument ``containing_inner`` specifies a minimal inner
-    shape, i.e. the output tableaux must fit inside the skew shape
-    ``outer``/``containing_inner``, (though they do not need to fill
-    it up).
+    Concatenate the rows of ``t1`` and ``t2``, dropping any None's from ``t2``.
+    This method is intended for the case when the outer shape of ``t1`` is equal to
+    the inner shape of ``t2``.
 
-    The optional argument ``shift``, a positive integer, increases
-    all the entries of the tableau by that amount.
+    EXAMPLES::
+
+        sage: from sage.combinat.lr_tableau import _tableau_join
+        sage: _tableau_join([[1,2]],[[None,None,2],[3]],shift=5)
+        [[1, 2, 7], [8]]
     """
+    import itertools
+    return [[e1 for e1 in row1]+[e2+shift for e2 in row2 if e2 is not None]
+            for (row1, row2) in itertools.izip_longest(t1, t2, fillvalue=[])]
 
-
-    # TODO: fix lrcalc to check this condition!
-    # (this appears to be a bug in lrcalc)
-    if not Partition(outer).contains(weight):
-        return
-
-    # The highest-weight standard tableau of shape w (for use below)
-    w = SkewTableau([[i+1]*val for i, val in enumerate(weight)])
-    w = w.standardization()
-
-    # Step 1. From lrcalc, get an iterator for the LR tableaux of
-    # skew shape = outer/weight, and arbitrary content
-    related_tabs = lrskew(outer, weight)
-
-    # Step 2. Convert them to the desired tableaux using JDT
-    for t in related_tabs:
-        # Shuffle t with w (returns (rect(t),std))
-        # Note: std now has shape outer/(something) and content = weight
-        std = w.shuffle(t.standardization())[1] # shuffle returns (rect(t),std)
-
-        # Step 3. Check the inner shape (if necessary)
-        if (containing_inner is not None) and not(std.inner_shape().contains(containing_inner)):
-            continue
-
-        # Step 4. Destandardize (shifting if necessary)
-        yield _destandardize(std, weight, shift)
-
-def LRtabs_multi(outer, weights, inner=[]):
-    """
-    An iterator for the LR tableaux of skew shape outer/inner,
-    which are unions of Yamanouchi sub-tableaux of the specified
-    weights.
-
-    Example usage:
-        sage: LRtabs_multi([7, 4, 2], [[2, 1], [3, 2], [2]], [2, 1])
-        ...
-        Returns: the 25 tableaux of shape [7, 4, 2] / [2, 1], such that
-            the subtableau of {1, 2}'s is LR of content [2, 1]
-            the subtableau of {3, 4}'s is LR of content [3, 2]
-            the subtableau of    {5}'s is LR of content [2]
-
-    This should be equivalent to calling
-        ``LittlewoodRichardsonTableax(outer, [inner]+weights)``
-    except that the inner shape is left blank.
-
-    The algorithm proceeds by starting with an empty tableau of
-    shape = outer, then successively attaching, along the inner
-    edge, an LR tableau of the next required weight (working backwards).
-    """
-    shift = sum([len(w) for w in weights])
-    start = SkewTableau([[None]*l for l in outer])
-    return _LRtabs_multi_recursive(start, list(reversed(weights)), inner, shift)
-
-# This function builds up the tableaux by starting at the outer edge,
-# then successively attaching, along the inner edge, an LR tableau 
-# of the next required weight
-def _LRtabs_multi_recursive(partial_tab, weights, inner, shift):
-    if weights == []:
-        if partial_tab.inner_shape() == inner:
-            yield partial_tab
-        return
-
-    w = weights[0]
-    for t in LRtabs_single(partial_tab.inner_shape(), w, inner, shift-len(w)):
-        for res in _LRtabs_multi_recursive(_tableauJoin(t, partial_tab),
-                                           weights[1:],
-                                           inner,
-                                           shift-len(w)):
-            yield res
-
-def _tableauJoin(t1, t2):
-    """
-    A helper function:
-    Concatenate the rows of t1 and t2, dropping any None's from t2.
-    (Intended for the case where t1.outer_shape() == t2.inner_shape().)
-    """
-    return SkewTableau([[e1 for e1 in row1]+[e2 for e2 in row2 if e2 is not None] for (row1, row2) in itertools.izip_longest(t1, t2, fillvalue=[])])
-
-def _destandardize(T, beta, shift=0):
-    """
-    A helper function:
-    Convert a standard tableau back to a semistandard one
-    of specified content ``beta`` and entries in the alphabet
-    {``shift``+1, ..., ``shift``+``len(beta)``}.
-
-    NOTE:
-    There is no error-checking! It just changes the entries
-    1, ..., beta[0] to 1's, then beta[0]+1, ..., beta[1] to 2's, and so on.
-    """
-    S = [[e for e in row] for row in T]
-    conversion = [i+1 for i, val in enumerate(beta) for _ in range(val)]
-    for (r, c) in T.cells():
-        S[r][c] = conversion[T[r][c]-1]+shift
-    return SkewTableau(S)

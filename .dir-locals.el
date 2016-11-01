3443b243a7f49258bbd04cadc60c00ded76b99b4
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-11-01 11:24:03 -0700

    Add Emacs configuration - no tabs for indentation

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  ;; Use space instead of tabs for indentation
+  (indent-tabs-mode . nil)))

1a2b5247a648c7542510b647d7e602dd43d44520
Daniel Krenn <git@danielkrenn.at>
2016-11-07 20:02:52 +0100

    copyleft

--- omega.py
+++ omega.py
@@ -1,4 +1,13 @@
 
+# *****************************************************************************
+# Copyright (C) 2016 Daniel Krenn <dev@danielkrenn.at>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# http://www.gnu.org/licenses/
+# *****************************************************************************
 from sage.groups.indexed_free_group import IndexedFreeAbelianGroup
 from six import iteritems
 

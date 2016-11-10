f49fd9565500a97f318f2e1165f46137a567e221
Julian Rüth <julian.rueth@fsfe.org>
2016-11-09 23:51:32 -0500

    Fix printing of limit valuations
    by considering the chain of the initial approximant not the current one

--- limit_valuation.py
+++ limit_valuation.py
@@ -915,12 +915,13 @@ class FiniteExtensionFromLimitValuation(FiniteExtensionFromInfiniteValuation):
             # print the minimal information that singles out this valuation from all approximants
             assert(self._base_valuation._initial_approximation in self._approximants)
             approximants = [v.augmentation_chain()[::-1] for v in self._approximants]
-            augmentations = self._base_valuation._approximation.augmentation_chain()[::-1]
-            unique_approximant = self._base_valuation._initial_approximation
+            augmentations = self._base_valuation._initial_approximation.augmentation_chain()[::-1]
+            unique_approximant = None
             for l in range(len(augmentations)):
                 if len([a for a in approximants if a[:l+1] == augmentations[:l+1]]) == 1:
                     unique_approximant = augmentations[:l+1]
                     break
+            assert(unique_approximant is not None)
             if unique_approximant[0].is_gauss_valuation():
                 unique_approximant[0] = unique_approximant[0].restriction(unique_approximant[0].domain().base_ring())
             if len(unique_approximant) == 1:

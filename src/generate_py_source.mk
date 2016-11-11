27812d05c14501b7bdb501d271f4625704a028b7
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2016-11-11 16:42:45 +0100

    Remove old auto-generated files

--- src/generate_py_source.mk
+++ src/generate_py_source.mk
@@ -7,6 +7,7 @@ all: sage/libs/cypari2/auto_gen.pxi sage/ext/interpreters/__init__.py
 # TODO: Adjustments for VPATH builds will be necessary.
 sage/libs/cypari2/auto_gen.pxi: $(SAGE_LOCAL)/share/pari/pari.desc \
         sage_setup/autogen/pari/*.py
+	rm -f sage/libs/pari/auto_*.pxi
 	python -c "from sage_setup.autogen.pari import rebuild; rebuild()"
 
 sage/ext/interpreters/__init__.py: sage_setup/autogen/interpreters.py

32992baa7affa5c84f58adf181fd7952d12a8cab
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2016-11-13 10:26:40 +0100

    trac 21867 do not use six.moves.range at all in pyx file

--- src/sage/libs/pari/gen.pyx
+++ src/sage/libs/pari/gen.pyx
@@ -65,7 +65,6 @@ Now it takes much less than a second::
 #*****************************************************************************
 
 from __future__ import absolute_import, division, print_function
-from six.moves import range
 
 import types
 cimport cython
@@ -4764,7 +4763,7 @@ cpdef gen objtogen(s):
     if isinstance(s, complex):
         return new_t_COMPLEX_from_double(PyComplex_RealAsDouble(s), PyComplex_ImagAsDouble(s))
 
-    if isinstance(s, (list, range, tuple, types.GeneratorType)):
+    if isinstance(s, (list, xrange, tuple, types.GeneratorType)):
         length = len(s)
         v = pari_instance._empty_vector(length)
         for i from 0 <= i < length:

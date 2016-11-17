c4c1350b797e5797b3279948a5b8acb4f0c0e25d
Daniel Krenn <git@danielkrenn.at>
2016-11-17 18:56:07 +0100

    rewrite Omega_higher

--- omega.py
+++ omega.py
@@ -147,6 +147,89 @@ def Omega_factors_denominator(n, m):
                  for gx in x for gy in y)
 
 
+def Omega_higher(a, z):
+    r"""
+    EXAMPLES::
+
+        sage: Omega_higher(0, [1, -2])
+        (1, (-z0 + 1, -z0^2*z1 + 1))
+        sage: Omega_higher(0, [1, -3])
+        (1, (-z0 + 1, -z0^3*z1 + 1))
+        sage: Omega_higher(0, [1, -4])
+        (1, (-z0 + 1, -z0^4*z1 + 1))
+
+        sage: Omega_higher(0, [2, -1])
+        (z0*z1 + 1, (-z0 + 1, -z0*z1^2 + 1))
+        sage: Omega_higher(0, [3, -1])
+        (z0*z1^2 + z0*z1 + 1, (-z0 + 1, -z0*z1^3 + 1))
+        sage: Omega_higher(0, [4, -1])
+        (z0*z1^3 + z0*z1^2 + z0*z1 + 1, (-z0 + 1, -z0*z1^4 + 1))
+
+        sage: Omega_higher(0, [1, 1, -2])
+        (-z0^2*z1*z2 - z0*z1^2*z2 + z0*z1*z2 + 1,
+         (-z0 + 1, -z1 + 1, -z0^2*z2 + 1, -z1^2*z2 + 1))
+        sage: Omega_higher(0, [2, -1, -1])
+        (z0*z1*z2 + z0*z1 + z0*z2 + 1, (-z0 + 1, -z0*z1^2 + 1, -z0*z2^2 + 1))
+        sage: Omega_higher(0, [2, 1, -1])
+        (-z0*z1*z2^2 - z0*z1*z2 + z0*z2 + 1, (-z0 + 1, -z1 + 1, -z0*z2^2 + 1, -z1*z2 + 1))
+    """
+    if not z or any(zz == 0 for zz in z):
+        raise NotImplementedError
+
+    x = tuple(zz for zz in z if zz > 0)
+    y = tuple(-zz for zz in z if zz < 0)
+    xy = x + y
+    n = sum(x)
+    m = sum(y)
+
+    exponents = sorted(set(zz for zz in xy) - set([1]))
+    B = QQ.extension(
+        list(cyclotomic_polynomial(r) for r in exponents),
+        tuple('rho{}'.format(i) for i in range(len(exponents))))
+    L = LaurentPolynomialRing(B, ', '.join('z{}'.format(nn)
+                                           for nn in range(len(z))))
+    powers = dict(zip(exponents, iter(L(g) for g in B.gens())))
+    powers[2] = L(-1)
+    powers[1] = L(1)
+
+    def subs_power(expression, var, exponent, value=None):
+        r"""
+        Substitute ``var^exponent`` by ``value`` in ``expression``.
+        """
+        p = tuple(var.dict().popitem()[0]).index(1)
+        def subs_e(e):
+            e = list(e)
+            assert e[p] % exponent == 0
+            e[p] = e[p] // exponent
+            return tuple(e)
+        parent = expression.parent()
+        result = parent({subs_e(e): c for e, c in iteritems(expression.dict())})
+        if value is None:
+            return result
+        return result.subs({var: value})
+
+    Z = L.change_ring(QQ)
+
+    def de_power(expression):
+        for zz, var in zip(z, L.gens()):
+            if abs(zz) == 1:
+                continue
+            expression = subs_power(expression, var, abs(zz))
+        return Z(expression)
+
+    xy_var = _laurent_polynomial_ring_(n, m).gens()
+    x_var = iter(xy_var[:n])
+    y_var = iter(xy_var[n:])
+    rules = {next(x_var) if zz > 0 else next(y_var):
+             powers[abs(zz)]**j * var
+             for zz, var in zip(z, L.gens()) for j in range(abs(zz))}
+    factors_denominator = tuple(de_power(prod(f.subs(rules) for f in factors))
+                                for factors in Omega_factors_denominator(x, y))
+
+    numerator = de_power(Omega_numerator(a, n, m).subs(rules))
+
+    return numerator, factors_denominator
+
     Return `\Omega_{\ge}` of the expression specified by the input.
 
     .. MATH::
@@ -167,59 +250,11 @@ def Omega_factors_denominator(n, m):
     numerator as a laurent polynomial, its second component a factorization
     of the denominator as a list of laurent polynomials.
 
-    EXAMPLES::
-
-        sage: L.<x, y, z, w> = LaurentPolynomialRing(QQ)
-        sage: Omega_fundamental(0, [x], [y])
-        (1, (-x + 1, -x*y + 1))
-        sage: Omega_fundamental(0, [x], [y, z])
-        (1, (-x + 1, -x*y + 1, -x*z + 1))
-        sage: Omega_fundamental(0, [x, y], [z])
-        (-x*y*z + 1, (-x + 1, -y + 1, -x*z + 1, -y*z + 1))
-        sage: Omega_fundamental(0, [x, y, z], [w])
-        (x*y*z*w^2 + x*y*z*w - x*y*w - x*z*w - y*z*w + 1,
-         (-x + 1, -y + 1, -z + 1, -x*w + 1, -y*w + 1, -z*w + 1))
-        sage: Omega_fundamental(0, [x, y], [z, w])
-        (x^2*y*z*w + x*y^2*z*w - x*y*z*w - x*y*z - x*y*w + 1,
-         (-x + 1, -y + 1, -x*z + 1, -x*w + 1, -y*z + 1, -y*w + 1))
-
-        sage: Omega_fundamental(-2, [x], [y])
-        (x^2, (-x + 1, -x*y + 1))
-        sage: Omega_fundamental(-1, [x], [y])
-        (x, (-x + 1, -x*y + 1))
-        sage: Omega_fundamental(1, [x], [y])
-        (-x*y + y + 1, (-x + 1, -x*y + 1))
-        sage: Omega_fundamental(2, [x], [y])
-        (-x*y^2 - x*y + y^2 + y + 1, (-x + 1, -x*y + 1))
-    """
-    def flatten(z):
-        return sum((tuple(zz) for zz in z), tuple())
-
-    if group_factors:
-        flat_x = flatten(x)
-        flat_y = flatten(y)
-    else:
-        flat_x = x
-        flat_y = y
-        x = tuple((xx,) for xx in x)
-        y = tuple((yy,) for yy in y)
-
-    numerator = Omega_numerator(a, len(flat_x), len(flat_y))
-    numerator = numerator.subs(
-        {xi: xj for xi, xj in zip(numerator.parent().gens(), flat_x+flat_y)})
-
-    Factors_denominator = \
-        tuple(tuple(1 - xx for xx in gx) for gx in x) + \
-        tuple(tuple(1 - xx*yy for xx in gx for yy in gy)
-              for gx in x for gy in y)
-    if not group_factors:
-        factors_denominator = flatten(factors_denominator)
-
-    return numerator, factors_denominator
 
 
-def Omega_higher(a, z):
-    r"""
+
+
+
     Return `\Omega_{\ge}` of the expression specified by the input.
 
     .. MATH::
@@ -266,105 +301,6 @@ def Omega_higher(a, z):
         (x*y*z + x*y + x*z + 1, (-x + 1, -x*y^2 + 1, -x*z^2 + 1))
         sage: Omega_higher(0, [(x, 2), (y, 1), (z, -1)])
         (-x*y*z^2 - x*y*z + x*z + 1, (-x + 1, -y + 1, -x*z^2 + 1, -y*z + 1))
-    """
-    class Factor(object):
-        def __init__(self, zz):
-            if isinstance(zz, (tuple, list)):
-                self.value, self.exponent = zz
-            else:
-                self.value = zz
-                self.exponent = 1
-        def is_higher(self):
-            return abs(self.exponent) > 1
-        def z(self, positive=True):
-            if (self.exponent > 0) != positive:
-                return tuple()
-            elif self.is_higher():
-                rho = powers[abs(self.exponent)]
-                w = self.var
-                return tuple(L_high(rho**j * w)
-                             for j in srange(abs(self.exponent)))
-            else:
-                return (L_high(self.value),)
-        def x(self):
-            return self.z(positive=True)
-        def y(self):
-            return self.z(positive=False)
-
-    z = list(Factor(zz) for zz in z)
-
-    # -2. create new (larger) laurent polynomial ring
-    L_orig = z[0].value.parent()
-    B_orig = L_orig.base_ring()
-    exponents_pre = sorted(set(abs(factor.exponent)
-                               for factor in z if factor.is_higher()))
-    B_high = B_orig.extension(
-        list(cyclotomic_polynomial(r) for r in exponents_pre),
-        tuple('rho{}'.format(i) for i in srange(len(exponents_pre))))
-    powers = dict(zip(exponents_pre, B_high.gens()))
-
-    nv = len(tuple(None for factor in z if factor.is_higher()))
-    variable_names_high = \
-        tuple('Omega{}'.format(i) for i in srange(nv)) + \
-        L_orig.variable_names()
-    L_high = LaurentPolynomialRing(B_high, variable_names_high)
-
-    # -1. rewrite factors with higher powers
-    v = iter(L_high.gens())
-    Omega_map = dict()
-    for factor in z:
-        if factor.is_higher():
-            factor.var = next(v)
-            Omega_map[factor.var] = factor
-
-    # 0. apply Omega
-    def nonempty(T):
-        return tuple(t for t in T if t)
-    numerator, factors_denominator = Omega_fundamental(
-        a,
-        nonempty(factor.x() for factor in z),
-        nonempty(factor.y() for factor in z),
-        group_factors=True)
-
-    # 1. multiply grouped factors of denominator
-    factors_denominator = tuple(prod(factor for factor in factors)
-                                for factors in factors_denominator)
-
-    # 2. substitute helper variable with actual value
-    def subs_power(expression, var, exponent, value):
-        r"""
-        Substitute ``var^exponent`` by ``value`` in ``expression``.
-        """
-        p = tuple(var.dict().popitem()[0]).index(1)
-        def subs_e(e):
-            e = list(e)
-            assert e[p] % exponent == 0
-            e[p] = e[p] // exponent
-            return tuple(e)
-        parent = expression.parent()
-        result = parent({subs_e(e): c for e, c in iteritems(expression.dict())})
-        return result.subs({var: value})
-
-    def subs_Omega(expression):
-        r"""
-        Substitute all helper variables by their actual values.
-        """
-        for var, factor in iteritems(Omega_map):
-            expression = subs_power(expression, var,
-                                    abs(factor.exponent), factor.value)
-        return expression
-
-    factors_denominator = tuple(subs_Omega(factor)
-                                for factor in factors_denominator)
-
-    from sage.rings.fraction_field import FractionField_generic
-    if isinstance(numerator.parent(), FractionField_generic):
-        numerator = subs_Omega(L_high(numerator.numerator())) / \
-                    subs_Omega(L_high(numerator.denominator()))
-    else:
-        numerator = subs_Omega(numerator)
-
-    return numerator, factors_denominator
 
 
 def Omega(var, numerator, factors_denominator, operator=operator.ge):

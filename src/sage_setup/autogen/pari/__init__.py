91fb12ed8d96bede142c7ee4b62540e4f99c827c
Erik M. Bray <erik.bray@lri.fr>
2016-11-21 10:52:46 +0000

    Use sage_src_pari to get the output path for generated files.
    The files that should be cleaned up are any autogenerated files at the old
    location, if they exist.

--- src/sage_setup/autogen/pari/__init__.py
+++ src/sage_setup/autogen/pari/__init__.py
@@ -1,14 +1,17 @@
 import glob
+import os
 from os.path import join, getmtime, exists
 
 from sage_setup.autogen.pari.generator import PariFunctionGenerator
-from sage_setup.autogen.pari.parser import pari_share
+from sage_setup.autogen.pari.parser import pari_share, sage_src_pari
+
 
 def rebuild(force=False):
+    pari_module_path = sage_src_pari()
     src_files = [join(pari_share(), 'pari.desc'),
-                 join('sage', 'libs', 'pari', 'decl.pxi')] + \
+                 join(pari_module_path, 'decl.pxi')] + \
                 glob.glob(join('sage_setup', 'autogen', 'pari', '*.py'))
-    gen_files = [join('sage', 'libs', 'pari', 'auto_gen.pxi')]
+    gen_files = [join(pari_module_path, 'auto_gen.pxi')]
 
     if all(exists(f) for f in gen_files):
         src_mtime = max(getmtime(f) for f in src_files)
@@ -16,6 +19,11 @@ def rebuild(force=False):
 
         if gen_mtime > src_mtime and not force:
             return
+
+    # Clean up old auto-generated files
+    old_pari_module_path = join('sage', 'libs', 'pari')
+    for filename in glob.glob(join(old_pari_module_path, 'auto_*.pxi')):
+        os.remove(filename)
 
     G = PariFunctionGenerator()
     G()

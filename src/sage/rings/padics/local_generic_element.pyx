65ad98029b9cb57aa693f2e6dff7ed721ba5dc7b
Julian Rüth <julian.rueth@fsfe.org>
2016-12-20 17:09:40 -0500

    Remove duplicate header
    that apparently came in with an imported patch for #12555

--- src/sage/rings/padics/local_generic_element.pyx
+++ src/sage/rings/padics/local_generic_element.pyx
@@ -20,17 +20,6 @@ AUTHORS:
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
-#*****************************************************************************
-#       Copyright (C) 2007-2013 David Roe <roed.math@gmail.com>
-#                               William Stein <wstein@gmail.com>
-#
-#  Distributed under the terms of the GNU General Public License (GPL)
-#  as published by the Free Software Foundation; either version 2 of
-#  the License, or (at your option) any later version.
-#
-#                  http://www.gnu.org/licenses/
-#*****************************************************************************
-
 from sage.rings.infinity import infinity
 from sage.structure.element cimport ModuleElement, RingElement, CommutativeRingElement
 from sage.structure.element import coerce_binop

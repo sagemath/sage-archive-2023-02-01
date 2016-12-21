e409e54609b0d06340d4ad9df156252c0c96ef9c
Kwankyu Lee <Kwankyu@apollo.local>
2016-12-21 03:49:22 +0100

    Improve doctest coverage

--- src/sage/libs/gsl/array.pyx
+++ src/sage/libs/gsl/array.pyx
@@ -5,7 +5,7 @@ GSL arrays
 include "cysignals/memory.pxi"
 
 cdef class GSLDoubleArray:
-    r"""
+    """
     EXAMPLES::
 
         sage: a = WaveletTransform(128,'daubechies',4)
@@ -15,6 +15,12 @@ cdef class GSLDoubleArray:
         [0.0, 1.0, 1.0]
     """
     def __init__(self, size_t n, size_t stride = 1, data = None):
+        """
+        EXAMPLES::
+
+            sage: from sage.libs.gsl.array import GSLDoubleArray
+            sage: a = GSLDoubleArray(10)
+        """
         cdef int i
 
         self.n = n
@@ -27,22 +33,64 @@ cdef class GSLDoubleArray:
             for i from 0 <= i < n:
                 self.data[i] = 0
 
-    def  __dealloc__(self):
+    def __dealloc__(self):
+        """
+        EXAMPLES::
+
+            sage: from sage.libs.gsl.array import GSLDoubleArray
+            sage: a = GSLDoubleArray(10)
+            sage: del a
+        """
         sig_free(self.data)
 
     def __len__(self):
+        """
+        EXAMPLES::
+
+            sage: from sage.libs.gsl.array import GSLDoubleArray
+            sage: a = GSLDoubleArray(10)
+            sage: len(a)
+            10
+        """
         return self.n
 
     def __repr__(self):
+        """
+        EXAMPLES::
+
+            sage: from sage.libs.gsl.array import GSLDoubleArray
+            sage: a = GSLDoubleArray(10)
+            sage: a
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        """
         return str(list(self))
 
     def __setitem__(self, size_t i, x):
+        """
+        EXAMPLES::
+
+            sage: from sage.libs.gsl.array import GSLDoubleArray
+            sage: a = GSLDoubleArray(10)
+            sage: a[5] = 3
+            sage: a
+            [0.0, 0.0, 0.0, 0.0, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0]
+        """
         # just set real for now
         if i < 0 or i >= self.n:
             raise IndexError
         self.data[i] = x
 
     def __getitem__(self, i):
+        """
+        EXAMPLES::
+
+            sage: from sage.libs.gsl.array import GSLDoubleArray
+            sage: a = GSLDoubleArray(10)
+            sage: for i in range(10):
+            ....:     a[i] = i
+            sage: a[3:7]
+            [3.0, 4.0, 5.0, 6.0]
+        """
         if isinstance(i, slice):
             start, stop, step = i.indices(len(self))
             # TODO -- make this actually fast.

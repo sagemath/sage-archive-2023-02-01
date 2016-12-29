3bb5e73032c4da6f6d9851ee908efbc2eb239c2b
Erik M. Bray <erik.bray@lri.fr>
2016-12-29 12:19:07 +0000

    Generate auto-generated sources as the earliest step in the 'build' command.
    Make sure that entire sub-packages that are auto-generated (namely, sage.ext.interpreters) is added to the distribution's
    list of packages.

--- src/sage_setup/autogen/__init__.py
+++ src/sage_setup/autogen/__init__.py
@@ -10,3 +10,10 @@ def autogen_all():
 
     from sage_setup.autogen import interpreters
     interpreters.rebuild(os.path.join("sage", "ext", "interpreters"))
+
+    # Return list of sub-packages that should be appended to the list of
+    # packages built/installed by setup.py
+    #
+    # In the case of Pari it just adds new files to an existing package (rather
+    # than autogenerating the entire sub-package) so it's omitted here.
+    return ['sage.ext.interpreters']

1eba0e200088b0422579caa740119125012282e6
Daniel Krenn <git@danielkrenn.at>
2017-01-02 18:14:29 +0100

    Trac #22120: additional doctest for asymptotic rings

--- src/sage/rings/asymptotic/asymptotic_ring.py
+++ src/sage/rings/asymptotic/asymptotic_ring.py
@@ -1582,6 +1582,11 @@ class AsymptoticExpansion(CommutativeAlgebraElement):
             2^n
             sage: e.parent()
             Asymptotic Ring <QQ^n * n^QQ> over Symbolic Ring
+
+        :trac:`22120`::
+
+            sage: A.<w> = AsymptoticRing('w^QQbar', QQ)
+            sage: w^QQbar(sqrt(2))
         """
         if not self.summands:
             if exponent == 0:

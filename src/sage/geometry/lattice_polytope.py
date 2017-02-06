5281cf4faf91d67d95e29230d3f8120008598307
Andrey Novoseltsev <novoselt@gmail.com>
2017-02-05 19:17:27 -0700

    Use PPL for facet normals of full-dimensional polytopes

--- src/sage/geometry/lattice_polytope.py
+++ src/sage/geometry/lattice_polytope.py
@@ -729,7 +729,37 @@ class LatticePolytopeClass(SageObject, collections.Hashable):
         """
         assert not hasattr(self, "_facet_normals")
         if self.dim() == self.lattice_dim():
-            self._read_equations(self.poly_x("e"))
+            N = self.dual_lattice()
+            normals = []
+            constants = []
+            for c in self._PPL().minimized_constraints():
+                assert c.is_inequality()
+                normals.append(N(c.coefficients()))
+                normals[-1].set_immutable()
+                constants.append(c.inhomogeneous_term())
+            # Sort normals if facets are vertices
+            if (self.dim() == 1
+                and normals[0] * self.vertex(0) + constants[0] != 0):
+                normals = (normals[1], normals[0])
+                constants = (constants[1], constants[0])
+            self.is_reflexive.set_cache(all(c == 1 for c in constants))
+            if self.is_reflexive():
+                polar = LatticePolytope(
+                    normals, compute_vertices=False, lattice=N)
+                polar._dim = self._dim
+                polar.is_reflexive.set_cache(True)
+                polar._polar = self
+                self._polar = polar
+                self._facet_normals = polar._vertices
+                polar._facet_normals = self._vertices
+                self._facet_constants = vector(ZZ, [1] * polar.nvertices())
+                self._facet_constants.set_immutable()
+                polar._facet_constants = vector(ZZ, [1] * self.nvertices())
+                polar._facet_constants.set_immutable()
+            else:
+                self._facet_normals = PointCollection(normals, N)
+                self._facet_constants = vector(ZZ, constants)
+                self._facet_constants.set_immutable()
         else:
             sp = self._sublattice_polytope
             N = self.dual_lattice()

32f36caa57573abeb53c9d9c042846d7f56c1cf7
Javier Honrubia González <jhonrubia6@alumno.uned.es>
2017-02-16 20:16:08 +0100

    Added the disc model to hyperbolic_arc and polygons

--- src/sage/plot/hyperbolic_arc.py
+++ src/sage/plot/hyperbolic_arc.py
@@ -24,9 +24,15 @@ AUTHORS:
 from __future__ import print_function
 
 from sage.plot.bezier_path import BezierPath
+from sage.plot.graphics import Graphics
+from sage.plot.circle import circle
+from sage.plot.arc import arc
 from sage.plot.colors import to_mpl_color
 from sage.plot.misc import options, rename_keyword
 from sage.rings.all import CC
+from sage.symbolic.constants import pi
+from sage.functions.trig import cos, sin, tan
+from copy import deepcopy
 
 class HyperbolicArc(BezierPath):
     """
@@ -43,22 +49,29 @@ class HyperbolicArc(BezierPath):
     Note that constructions should use ``hyperbolic_arc``::
 
          sage: from sage.plot.hyperbolic_arc import HyperbolicArc
-
-         sage: print(HyperbolicArc(0, 1/2+I*sqrt(3)/2, {}))
+         sage: print(HyperbolicArc(0, 1/2+I*sqrt(3)/2, "UHP", {}))
          Hyperbolic arc (0.000000000000000, 0.500000000000000 + 0.866025403784439*I)
     """
 
-    def __init__(self, A, B, options):
+    def __init__(self, A, B, model, options):
         A, B = (CC(A), CC(B))
-        if A.imag()<0:
-            raise ValueError("%s is not a valid point in the UHP model"%(A))
-        if B.imag()<0:
-            raise ValueError("%s is not a valid point in the UHP model"%(B))
         self.path = []
-        self._hyperbolic_arc(A, B, True);
+        if model == "UHP":
+            if A.imag()<0:
+                raise ValueError("%s is not a valid point in the UHP model"%(A))
+            if B.imag()<0:
+                raise ValueError("%s is not a valid point in the UHP model"%(B))
+            self._UHP_hyperbolic_arc(A, B, True);
+        else:
+            if model == "PD":
+                if A.abs()>1:
+                    raise ValueError("%s is not a valid point in the PD model"%(A))
+                if B.abs()>1:
+                    raise ValueError("%s is not a valid point in the PD model"%(B))
+                self._PD_hyperbolic_arc(A, B)
+
         BezierPath.__init__(self, self.path, options)
         self.A, self.B = (A, B)
-
     def _repr_(self):
         """
         String representation of HyperbolicArc.
@@ -66,13 +79,12 @@ class HyperbolicArc(BezierPath):
         TESTS::
 
             sage: from sage.plot.hyperbolic_arc import HyperbolicArc
-            sage: HyperbolicArc(0, 1/2+I*sqrt(3)/2, {})._repr_()
+            sage: HyperbolicArc(0, 1/2+I*sqrt(3)/2, "UHP", {})._repr_()
             'Hyperbolic arc (0.000000000000000, 0.500000000000000 + 0.866025403784439*I)'
         """
 
         return "Hyperbolic arc (%s, %s)" % (self.A, self.B)
-
-    def _hyperbolic_arc(self, z0, z3, first=False):
+    def _UHP_hyperbolic_arc(self, z0, z3, first=False):
         """
         Function to construct Bezier path as an approximation to
         the hyperbolic arc between the complex numbers z0 and z3 in the
@@ -89,8 +101,8 @@ class HyperbolicArc(BezierPath):
         if z0.imag() == 0 and z3.imag() == 0:
             p = (z0.real()+z3.real())/2
             zm = CC(p, r)
-            self._hyperbolic_arc(z0, zm, first)
-            self._hyperbolic_arc(zm, z3)
+            self._UHP_hyperbolic_arc(z0, zm, first)
+            self._UHP_hyperbolic_arc(zm, z3)
             return
         else:
             zm = ((z0+z3)/2-p)/abs((z0+z3)/2-p)*r+p
@@ -107,18 +119,98 @@ class HyperbolicArc(BezierPath):
             self.path.append([(z1.real(), z1.imag()),
                               (z2.real(), z2.imag()),
                               (z3.real(), z3.imag())]);
+    def _bezier_path(self, arc0, z0, z3):
+        """
+        Returns the corresponding bezier path
+        """
+        ma = arc0._matplotlib_arc()
+        transform = ma.get_transform().get_matrix()
+        cA, cC, cE = transform[0]
+        cB, cD, cF = transform[1]
+        points = []
+        for u in ma._path.vertices:
+            x, y = list(u)
+            points += [(cA * x + cC * y + cE, cB * x + cD * y + cF)]
+
+        if abs(CC(points[0]) - z0)>0.00001:
+            points.reverse()
+
+        self.path.append(points[0: 4])
+        N = 4
+        while N < len(points):
+            self.path.append(points[N: N + 3])
+            N += 3
+        return
+    def _CenterAndRadiusOrthogonalcirclegiven2points(self,z1,z2):
+        """
+        Calculate center and radius of an orthogonal circle to the
+        unit disc through z1, z2 if they are ideal points or
+        through z1, z2 and their inverses.
+        """
+        z1,z2 = (CC(z1),CC(z2))
+        if abs(z1.abs()-1)<0.000001 and abs(z2.abs()-1)<0.000001:
+            #both z1,z2 are ideal points
+            phi1 = z1.arg()
+            phi2 = z2.arg()
+            deltaphi = 0.5 * abs(phi1-phi2)
+            phi = 0.5 * (phi1+phi2)
+            extR = sin(deltaphi) * tan(deltaphi)
+            R = cos(deltaphi) + extR
+            #center is at R*exp(i*(min(phi1,phi2)+deltaphi)
+            argcenter = min(phi1,phi2)+deltaphi
+            c = CC(R*cos(argcenter),R*sin(argcenter))
+            r = abs(tan(deltaphi))
+        else: 
+            if z1.abs()<1:
+                z3=(1/z1).conjugate()
+            else:
+                if z2.abs()<1:
+                    z3=(1/z2).conjugate()
+            k1=z1-z3
+            k2=z3-z2
+            s=(k1*k2.conjugate()).real()/(k1*k2.conjugate()).imag()
+            c=(z1+z2)/2+CC(0,1)*s*(z1-z2)/2
+            r=(z1-c).abs()
+        return c,r
+    def _PD_hyperbolic_arc(self, z0, z3):
+        """
+        Function to construct an hyperbolic arc between the complez numbers z0
+        and z3 in the Poincare Disc model
+        """
+        z0, z3 = (CC(z0), CC(z3))
+        phi0 = z0.arg()
+        phi3 = z3.arg()
+        if abs(phi0 - phi3) == 0 or abs(phi0 - phi3) == pi:
+            #The points lie in a geodesic of the first kind
+            self.path.append([(z0.real(),z0.imag()), (z3.real(),z3.imag())])
+            return
+        else:
+            #The points lie in a geodesic of the second kind
+            T=self._CenterAndRadiusOrthogonalcirclegiven2points(z0,z3)
+            a1=(z0-T[0]).arg()
+            a2=(z3-T[0]).arg()
+            if (T[0]).real()>0:
+                if a1<0:
+                    a1=a1+2*pi
+                if a2<0:
+                    a2=a2+2*pi
+            pic = arc((T[0].real(),T[0].imag()),T[1],sector=(a1,a2))[0]
+
+            #Transform arc into a bezier path
+            self._bezier_path(pic,z0,z3)
+            return
 
 @rename_keyword(color='rgbcolor')
 @options(alpha=1, fill=False, thickness=1, rgbcolor="blue", zorder=2, linestyle='solid')
-def hyperbolic_arc(a, b, **options):
+def hyperbolic_arc(a, b, model="UHP", **options):
     """
-    Plot an arc from a to b in hyperbolic geometry in the complex upper
-    half plane.
+    Plot an arc from a to b in hyperbolic plane.
 
     INPUT:
 
-    - ``a, b`` - complex numbers in the upper half complex plane
-      connected bye the arc
+    - ``a, b`` - complex numbers connected by a hyperbolic arc
+
+    - ``model``- default: ``'UHP'`` hyperbolic model used, which is one of ``'UHP'`` (Upper half plane), or ``'PD'`` (Poincare disc)
 
     OPTIONS:
 
@@ -136,24 +228,61 @@ def hyperbolic_arc(a, b, **options):
 
     Show a hyperbolic arc from 0 to 1::
 
-         sage: hyperbolic_arc(0, 1)
-         Graphics object consisting of 1 graphics primitive
+        sage: hyperbolic_arc(0, 1)
+        Graphics object consisting of 1 graphics primitive
+
+    .. PLOT::
+
+        sphinx_plot(hyperbolic_arc(0,1))
 
     Show a hyperbolic arc from 1/2 to `i` with a red thick line::
 
-         sage: hyperbolic_arc(1/2, I, color='red', thickness=2)
+        sage: hyperbolic_arc(1/2, I, color='red', thickness=2)
+        Graphics object consisting of 1 graphics primitive
+
+    .. PLOT::
+
+        sphinx_plot(hyperbolic_arc(0.5, I, color='red', thickness=2))
+
+    Show a hyperbolic arc from `1+i` to `1+2i` with dashed line::
+
+        sage: hyperbolic_arc(1+I, 1+2*I, linestyle='dashed')
+        Graphics object consisting of 1 graphics primitive
+
+    .. PLOT::
+
+        sphinx_plot(hyperbolic_arc(1+I, 1+2*I, linestyle='dashed'))
+
+    ::
+
+         sage: hyperbolic_arc(1+I, 1+ 2*I, linestyle='--')
          Graphics object consisting of 1 graphics primitive
 
-    Show a hyperbolic arc form `i` to `2 i` with dashed line::
+    .. PLOT::
 
-         sage: hyperbolic_arc(I, 2*I, linestyle='dashed')
-         Graphics object consisting of 1 graphics primitive
-         sage: hyperbolic_arc(I, 2*I, linestyle='--')
-         Graphics object consisting of 1 graphics primitive
+        sphinx_plot(hyperbolic_arc(1+I, 1+2*I, linestyle='dashed'))
+
+    Show a hyperbolic arc from `i` to `-1` in red, another hyperbolic arc
+    from $e^{i\pi/3}$ to $0.6*e^{i 3\pi/4}$ together with the disc frontier::
+
+        sage: z1 = CC((cos(pi/3),sin(pi/3)))
+        sage: z2 = CC((0.6*cos(3*pi/4),0.6*sin(3*pi/4)))
+        sage: hyperbolic_arc(z1,z2,model="PD",color="red")
+        Graphics object consisting of 2 graphics primitives
+
+    .. PLOT::
+
+        z1 = CC((cos(pi/3),sin(pi/3)))
+        z2 = CC((0.6*cos(3*pi/4),0.6*sin(3*pi/4)))
+        P = hyperbolic_arc(z1, z2, model="PD", color="red")
+        sphinx_plot(P)
+
     """
     from sage.plot.all import Graphics
     g = Graphics()
     g._set_extra_kwds(g._extract_kwds_for_show(options))
-    g.add_primitive(HyperbolicArc(a, b, options))
+    g.add_primitive(HyperbolicArc(a, b, model, options))
+    if model=="PD":
+        g  = g + circle((0,0),1, rgbcolor='black')
     g.set_aspect_ratio(1)
     return g
--- src/sage/plot/hyperbolic_polygon.py
+++ src/sage/plot/hyperbolic_polygon.py
@@ -25,9 +25,13 @@ AUTHORS:
 from __future__ import print_function
 
 from sage.plot.bezier_path import BezierPath
+from sage.plot.graphics import Graphics
+from sage.plot.circle import circle
+from sage.plot.arc import arc
 from sage.plot.misc import options, rename_keyword
 from sage.rings.all import CC
-
+from sage.symbolic.constants import pi
+from sage.functions.trig import cos, sin, tan
 
 class HyperbolicPolygon(BezierPath):
     """
@@ -48,28 +52,34 @@ class HyperbolicPolygon(BezierPath):
     :func:`hyperbolic_triangle`::
 
          sage: from sage.plot.hyperbolic_polygon import HyperbolicPolygon
-         sage: print(HyperbolicPolygon([0, 1/2, I], {}))
+         sage: print(HyperbolicPolygon([0, 1/2, I], "UHP", {}))
          Hyperbolic polygon (0.000000000000000, 0.500000000000000, 1.00000000000000*I)
     """
-    def __init__(self, pts, options):
+    def __init__(self, pts, model, options):
         """
         Initialize HyperbolicPolygon.
 
         EXAMPLES::
 
             sage: from sage.plot.hyperbolic_polygon import HyperbolicPolygon
-            sage: print(HyperbolicPolygon([0, 1/2, I], {}))
+            sage: print(HyperbolicPolygon([0, 1/2, I], "UHP", {}))
             Hyperbolic polygon (0.000000000000000, 0.500000000000000, 1.00000000000000*I)
         """
         pts = [CC(_) for _ in pts]
         self.path = []
-        self._hyperbolic_arc(pts[0], pts[1], True)
-        for i in range(1, len(pts) - 1):
-            self._hyperbolic_arc(pts[i], pts[i + 1])
-        self._hyperbolic_arc(pts[-1], pts[0])
-        BezierPath.__init__(self, self.path, options)
+        if model == "UHP":
+            self._UHP_hyperbolic_arc(pts[0], pts[1], True)
+            for i in range(1, len(pts) - 1):
+                self._UHP_hyperbolic_arc(pts[i], pts[i + 1])
+            self._UHP_hyperbolic_arc(pts[-1], pts[0])
+        else:
+            if model == "PD":
+                self._PD_hyperbolic_arc(pts[0], pts[1],True)
+                for i in range(1, len(pts) - 1):
+                    self._PD_hyperbolic_arc(pts[i], pts[i + 1])
+                self._PD_hyperbolic_arc(pts[-1], pts[0])
         self._pts = pts
-
+        BezierPath.__init__(self, self.path, options)
     def _repr_(self):
         """
         String representation of HyperbolicPolygon.
@@ -77,12 +87,11 @@ class HyperbolicPolygon(BezierPath):
         TESTS::
 
             sage: from sage.plot.hyperbolic_polygon import HyperbolicPolygon
-            sage: HyperbolicPolygon([0, 1/2, I], {})._repr_()
+            sage: HyperbolicPolygon([0, 1/2, I], "UHP", {})._repr_()
             'Hyperbolic polygon (0.000000000000000, 0.500000000000000, 1.00000000000000*I)'
         """
         return "Hyperbolic polygon ({})".format(", ".join(map(str, self._pts)))
-
-    def _hyperbolic_arc(self, z0, z3, first=False):
+    def _UHP_hyperbolic_arc(self, z0, z3, first=False):
         """
         Function to construct Bezier path as an approximation to
         the hyperbolic arc between the complex numbers z0 and z3 in the
@@ -90,20 +99,20 @@ class HyperbolicPolygon(BezierPath):
         """
         z0, z3 = (CC(z0), CC(z3))
         p = (abs(z0)*abs(z0)-abs(z3)*abs(z3))/(z0-z3).real()/2
-        r = abs(z0-p)
+        r = abs(z0 - p)
 
-        if abs(z3-z0)/r < 0.1:
-            self.path.append([(z0.real(), z0.imag()), (z3.real(), z3.imag())])
+        if abs(z3 - z0)/r < 0.1:
+            self.path.append([(z0.real(),z0.imag()), (z3.real(),z3.imag())])
             return
 
         if z0.imag() == 0 and z3.imag() == 0:
-            p = (z0.real()+z3.real())/2
+            p = (z0.real()+z3.real()) / 2
             zm = CC(p, r)
-            self._hyperbolic_arc(z0, zm, first)
-            self._hyperbolic_arc(zm, z3)
+            self._UHP_hyperbolic_arc(z0, zm, first)
+            self._UHP_hyperbolic_arc(zm, z3)
             return
         else:
-            zm = ((z0+z3)/2-p)/abs((z0+z3)/2-p)*r+p
+            zm = ((z0+z3)/2-p)/abs((z0+z3)/2-p)*r + p
             t = (8*zm-4*(z0+z3)).imag()/3/(z3-z0).real()
             z1 = z0 + t*CC(z0.imag(), (p-z0.real()))
             z2 = z3 - t*CC(z3.imag(), (p-z3.real()))
@@ -111,18 +120,108 @@ class HyperbolicPolygon(BezierPath):
             self.path.append([(z0.real(), z0.imag()),
                               (z1.real(), z1.imag()),
                               (z2.real(), z2.imag()),
-                              (z3.real(), z3.imag())])
+                              (z3.real(), z3.imag())]);
             first = False
         else:
             self.path.append([(z1.real(), z1.imag()),
                               (z2.real(), z2.imag()),
-                              (z3.real(), z3.imag())])
+                              (z3.real(), z3.imag())]);
+    def _bezier_path(self, arc0, z0, first):
+        """
+        Returns the corresponding bezier path
+        """
+        ma = arc0._matplotlib_arc()
+        transform = ma.get_transform().get_matrix()
+        cA, cC, cE = transform[0]
+        cB, cD, cF = transform[1]
+        points = []
+        for u in ma._path.vertices:
+            x, y = list(u)
+            points += [(cA * x + cC * y + cE, cB * x + cD * y + cF)]
+        if abs(CC(points[0]) - z0)>0.00001:
+            points.reverse() #order is important
+        if first:
+            self.path.append(points[0: 4])
+            N = 4
+        else:
+            N = 0
+            #the first point is equal to the last of the previous arc
+            del points[0]
+        #Complete the last tuple of control points
+        tail = self.path[-1]
+        ltail = len(tail)
+        while ltail < 3:
+            self.path[-1].append(points[N])
+            ltail +=1
+            N +=1
+        #Add new triplets
+        while N < len(points):
+            self.path.append(points[N: N + 3])
+            N += 3
+        return
+    def _CenterAndRadiusOrthogonalcirclegiven2points(self,z1,z2):
+        """
+        Calculate center and radius of an orthogonal circle to the
+        unit disc through z1, z2 if they are ideal points or
+        through z1, z2 and their inverses.
+        """
+        z1,z2 = (CC(z1),CC(z2))
+        if abs(z1.abs()-1)<0.000001 and abs(z2.abs()-1)<0.000001:
+            #both z1,z2 are ideal points
+            phi1 = z1.arg()
+            phi2 = z2.arg()
+            deltaphi = 0.5 * abs(phi1-phi2)
+            phi = 0.5 * (phi1+phi2)
+            extR = sin(deltaphi) * tan(deltaphi)
+            R = cos(deltaphi) + extR
+            #center is at R*exp(i*(min(phi1,phi2)+deltaphi)
+            argcenter = min(phi1,phi2)+deltaphi
+            c = CC(R*cos(argcenter),R*sin(argcenter))
+            r = abs(tan(deltaphi))
+        else: 
+            if z1.abs()<1:
+                z3=(1/z1).conjugate()
+            else:
+                if z2.abs()<1:
+                    z3=(1/z2).conjugate()
+            k1=z1-z3
+            k2=z3-z2
+            s=(k1*k2.conjugate()).real()/(k1*k2.conjugate()).imag()
+            c=(z1+z2)/2+CC(0,1)*s*(z1-z2)/2
+            r=(z1-c).abs()
+        return c,r
+    def _PD_hyperbolic_arc(self, z0, z3, first=False):
+        """
+        Function to construct an hyperbolic arc between the complez numbers z0
+        and z3 in the Poincare Disc model
+        """
+        z0, z3 = (CC(z0), CC(z3))
+        phi0 = z0.arg()
+        phi3 = z3.arg()
+        if abs(phi0 - phi3) == 0 or abs(phi0 - phi3) == pi:
+            #The points lie in a geodesic of the first kind
+            self.path.append([(z0.real(),z0.imag()), (z3.real(),z3.imag())])
+            return
+        else:
+            #The points lie in a geodesic of the second kind
+            T=self._CenterAndRadiusOrthogonalcirclegiven2points(z0,z3)
+            a1=(z0-T[0]).arg()
+            a2=(z3-T[0]).arg()
+            if (T[0]).real()>0:
+                if a1<0:
+                    a1=a1+2*pi
+                if a2<0:
+                    a2=a2+2*pi
+            pic = arc((T[0].real(),T[0].imag()),T[1],sector=(a1,a2))[0]
+            #Transform arc into a bezier path
+            self._bezier_path(pic,z0,first)
+            return
 
 
 @rename_keyword(color='rgbcolor')
 @options(alpha=1, fill=False, thickness=1, rgbcolor="blue", zorder=2,
          linestyle='solid')
-def hyperbolic_polygon(pts, **options):
+def hyperbolic_polygon(pts, model="UHP", **options):
     r"""
     Return a hyperbolic polygon in the hyperbolic plane with vertices ``pts``.
 
@@ -134,6 +233,9 @@ def hyperbolic_polygon(pts, **options):
 
     OPTIONS:
 
+    
+    - ``model`` -- default: ``UHP`` Model used for hyperbolic plane
+    
     - ``alpha`` -- default: 1
 
     - ``fill`` -- default: ``False``
@@ -165,19 +267,40 @@ def hyperbolic_polygon(pts, **options):
 
     .. PLOT::
 
-        P = hyperbolic_polygon([-1,3*I,2+2*I,1+I], fill=True, color='red')
-        sphinx_plot(P)
+        sphinx_plot(hyperbolic_polygon([-1,3*I,2+2*I,1+I], fill=True, color='red'))
 
+    Poincare disc model is supported via the parameter ``model``.
+    Show a hyperbolic polygon in the Poincare disc model with coordinates
+    `1`, `i`, `-1`, `-i`::
+
+        sage: hyperbolic_polygon([1,I,-1,-I], model="PD", color='green')
+        Graphics object consisting of 2 graphics primitives
+
+    .. PLOT::
+
+        sphinx_plot(hyperbolic_polygon([1,I,-1,-I], model="PD", color='green'))
+
+    With more options::
+
+        sage: hyperbolic_polygon([1,I,-1,-I], model="PD", color='green', fill=True, linestyle="-")
+        Graphics object consisting of 2 graphics primitives
+
+    .. PLOT::
+
+        P = hyperbolic_polygon([1,I,-1,-I], model="PD", color='green', fill=True, linestyle="-")
+        sphinx_plot(P)
     """
     from sage.plot.all import Graphics
     g = Graphics()
     g._set_extra_kwds(g._extract_kwds_for_show(options))
-    g.add_primitive(HyperbolicPolygon(pts, options))
+    g.add_primitive(HyperbolicPolygon(pts, model, options))
+    if model=="PD":
+        g  = g + circle((0,0),1, rgbcolor='black')
     g.set_aspect_ratio(1)
     return g
 
 
-def hyperbolic_triangle(a, b, c, **options):
+def hyperbolic_triangle(a, b, c, model="UHP", **options):
     """
     Return a hyperbolic triangle in the hyperbolic plane with vertices ``(a,b,c)``.
 
@@ -224,5 +347,31 @@ def hyperbolic_triangle(a, b, c, **options):
         P = hyperbolic_triangle(0, 1, 2+i, fill=true, rgbcolor='red', linestyle='--')
         sphinx_plot(P)
 
+    It can also plot a hyperbolic triangle in the Poincare Disc model::
+
+        sage: z1 = CC((cos(pi/3),sin(pi/3)))
+        sage: z2 = CC((0.6*cos(3*pi/4),0.6*sin(3*pi/4)))
+        sage: z3 = 1
+        sage: hyperbolic_triangle(z1, z2, z3, model="PD", color="red")
+        Graphics object consisting of 2 graphics primitives
+
+    .. PLOT::
+ 
+        z1 = CC((cos(pi/3),sin(pi/3)))
+        z2 = CC((0.6*cos(3*pi/4),0.6*sin(3*pi/4)))
+        z3 = 1
+        P = hyperbolic_triangle(z1, z2, z3, model="PD", color="red")
+        sphinx_plot(P)
+
+    ::
+
+        sage: hyperbolic_triangle(0.3+0.3*I, 0.8*I, -0.5-0.5*I, model="PD", color='magenta')
+        Graphics object consisting of 2 graphics primitives
+
+    .. PLOT::
+
+        P = hyperbolic_triangle(0.3+0.3*I, 0.8*I, -0.5-0.5*I, model="PD", color='magenta')
+        sphinx_plot(P)
+
     """
-    return hyperbolic_polygon((a, b, c), **options)
+    return hyperbolic_polygon((a, b, c), model, **options)

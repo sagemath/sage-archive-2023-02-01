77a8187f9f0aae97b17b315b81305f969cc703a7
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2017-02-28 11:28:33 +0100

    trac 22446 adding some doc to pari_group

--- src/sage/groups/pari_group.py
+++ src/sage/groups/pari_group.py
@@ -50,19 +50,71 @@ class PariGroup(Group):
         return "PARI group %s of degree %s" % (self.__x, self.__degree)
 
     def __eq__(self, other):
+        """
+        Test equality.
+
+        EXAMPLES::
+
+            sage: R.<x> = PolynomialRing(QQ)
+            sage: f1 = x^4 - 17*x^3 - 2*x + 1
+            sage: f2 = x^3 - x - 1
+            sage: G1 = f1.galois_group(pari_group=True)
+            sage: G2 = f2.galois_group(pari_group=True)
+            sage: G1 == G1
+            True
+            sage: G1 == G2
+            False
+        """
         return (isinstance(other, PariGroup) and
             (self.__x, self.__degree) == (other.__x, other.__degree))
 
     def __ne__(self, other):
+        """
+        Test inequality.
+
+        EXAMPLES::
+
+            sage: R.<x> = PolynomialRing(QQ)
+            sage: f1 = x^4 - 17*x^3 - 2*x + 1
+            sage: f2 = x^3 - x - 1
+            sage: G1 = f1.galois_group(pari_group=True)
+            sage: G2 = f2.galois_group(pari_group=True)
+            sage: G1 != G1
+            False
+            sage: G1 != G2
+            True
+        """
         return not self.__eq__(other)
 
     def _pari_(self):
         return self.__x
 
     def degree(self):
+        """
+        Return the degree of ``self``.
+
+        EXAMPLES::
+
+            sage: R.<x> = PolynomialRing(QQ)
+            sage: f1 = x^4 - 17*x^3 - 2*x + 1
+            sage: G1 = f1.galois_group(pari_group=True)
+            sage: G1.degree()
+            4
+        """
         return self.__degree
 
     def order(self):
+        """
+        Return the order of ``self``.
+
+        EXAMPLES::
+
+            sage: R.<x> = PolynomialRing(QQ)
+            sage: f1 = x^4 - 17*x^3 - 2*x + 1
+            sage: G1 = f1.galois_group(pari_group=True)
+            sage: G1.order()
+            24
+        """
         return Integer(self.__x[0])
 
     def permutation_group(self):

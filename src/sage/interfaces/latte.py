96e40991054c847b0d1fb97623aa36a61854411a
Marcelo Forets <mforets@gmail.com>
2017-03-06 16:38:54 +0100

    Integral of a polynomial over a polytope.

--- src/sage/interfaces/latte.py
+++ src/sage/interfaces/latte.py
@@ -178,3 +178,127 @@ def count(arg, ehrhart_polynomial=False, multivariate_generating_function=False,
         else:
             from sage.rings.integer import Integer
             return Integer(ans)
+
+def integrate(arg, polynomial, raw_output=False, verbose=False, **kwds):
+    r"""
+    Call to the function integrate from LattE integrale, to compute the integral of a polynomial over a polytope.
+
+    INPUT:
+
+    - ``arg`` -- a cdd or LattE description string
+
+    - ``polynomial`` -- the polynomial to be integrated, as
+
+    - ``raw_output`` -- if ``True`` then return directly the output string from LattE
+
+    - ``verbose`` -- if ``True`` then return directly verbose output from LattE
+
+    - For all other options of the count program, consult the LattE manual
+
+    OUTPUT:
+
+    Either a string (if ``raw_output`` if set to ``True``) or a rational.
+
+    EXAMPLES::
+
+        sage: from sage.interfaces.latte import integrate
+        sage: P = 2 * polytopes.cube()
+        sage: x, y, z = polygen(QQ, 'x, y, z')
+
+    Integrating over a polynomial over a polytope in either the H or V representation::
+
+        sage: integrate(P.cdd_Hrepresentation(), x^2*y^2*z^2, cdd=True)   # optional - latte_int
+        4096/27
+        sage: integrate(P.cdd_Vrepresentation(), x^2*y^2*z^2, cdd=True)   # optional - latte_int
+        4096/27
+
+    TESTS:
+
+    Testing raw output::
+
+        sage: from sage.interfaces.latte import integrate
+        sage: P = polytopes.cuboctahedron()
+        sage: cddin = P.cdd_Vrepresentation()
+        sage: x, y, z = polygen(QQ, 'x, y, z')
+        sage: f = 3*x^2*y^4*z^6 + 7*y^3*z^5
+        sage: integrate(cddin, f, cdd=True, raw_output=True)  # optional - latte_int
+        '629/47775'
+
+    Testing the ``verbose`` option::
+
+        sage: ans = integrate(cddin, f, cdd=True, verbose=True, raw_output=True)  # optional - latte_int
+        This is LattE integrale ...
+        ...
+        Invocation: integrate --valuation=integrate --triangulate --redundancy-check=none --cdd --monomials=... /dev/stdin
+        ...
+    """
+    from subprocess import Popen, PIPE
+    from sage.misc.misc import SAGE_TMP
+    from sage.rings.integer import Integer
+
+    args = ['integrate']
+    args.append('--valuation=integrate')
+    args.append('--triangulate')
+
+    if 'redundancy_check' not in kwds:
+        args.append('--redundancy-check=none')
+
+    for key,value in kwds.items():
+        if value is None or value is False:
+            continue
+
+        key = key.replace('_','-')
+        if value is True:
+            args.append('--{}'.format(key))
+        else:
+            args.append('--{}={}'.format(key, value))
+
+    # transform polynomial to LattE description
+    coefficients_list = polynomial.coefficients()
+    exponents_list = [list(exponent_vector_i) for exponent_vector_i in polynomial.exponents()]
+
+    # assuming that the order in coefficients() and exponents() methods match
+    monomials_list = zip(coefficients_list, exponents_list)
+    monomials_list = [list(monomial_i) for monomial_i in monomials_list]
+    monomials_list = str(monomials_list)
+
+    # write the polynomial description to a temp file
+    from sage.misc.temporary_file import tmp_filename
+    filename_polynomial = tmp_filename()
+
+    with open(filename_polynomial, 'w') as f:
+        f.write(monomials_list)
+    args += ['--monomials=' + filename_polynomial]
+
+    args += ['/dev/stdin']
+
+    try:
+        # The cwd argument is needed because latte
+        # always produces diagnostic output files.
+        latte_proc = Popen(args,
+                           stdin=PIPE, stdout=PIPE,
+                           stderr=(None if verbose else PIPE),
+                           cwd=str(SAGE_TMP))
+    except OSError:
+        from sage.misc.package import PackageNotFoundError
+        raise PackageNotFoundError('latte_int')
+
+    ans, err = latte_proc.communicate(arg)
+    ret_code = latte_proc.poll()
+    if ret_code:
+        if err is None:
+            err = ", see error message above"
+        else:
+            err = ":\n" + err
+        raise RuntimeError("LattE integrale program failed (exit code {})".format(ret_code) + err.strip())
+
+    ans = ans.splitlines()
+    ans = ans[-5].split()
+    assert(ans[0]=='Answer:')
+    ans = ans[1]
+
+    if raw_output:
+        return ans
+    else:
+        from sage.rings.rational import Rational
+        return Rational(ans)

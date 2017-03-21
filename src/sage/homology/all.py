98358f719ef9d712b2515f81389548a669242dd6
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2017-03-21 17:02:43 +0100

    Move unpickle override to all.py

--- src/sage/homology/all.py
+++ src/sage/homology/all.py
@@ -16,3 +16,8 @@ from sage.misc.lazy_import import lazy_import
 lazy_import('sage.homology.koszul_complex', 'KoszulComplex')
 lazy_import('sage.homology', 'simplicial_complexes_catalog', 'simplicial_complexes')
 lazy_import('sage.homology', 'simplicial_set_catalog', 'simplicial_sets')
+
+
+# For taking care of old pickles
+from sage.structure.sage_object import register_unpickle_override
+register_unpickle_override('sage.homology.examples', 'SimplicialSurface', SimplicialComplex)

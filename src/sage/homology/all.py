769d635266ab2cd061ce877c58cbc7ea6465ce48
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2017-03-21 10:05:46 +0100

    Lazy import catalogs in homology

--- src/sage/homology/all.py
+++ src/sage/homology/all.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+
 from .chain_complex import ChainComplex
 
 from .chain_complex_morphism import ChainComplexMorphism
@@ -7,13 +8,11 @@ from .simplicial_complex import SimplicialComplex, Simplex
 
 from .simplicial_complex_morphism import SimplicialComplexMorphism
 
-from . import simplicial_complexes_catalog as simplicial_complexes
-
 from .delta_complex import DeltaComplex, delta_complexes
 
 from .cubical_complex import CubicalComplex, cubical_complexes
 
 from sage.misc.lazy_import import lazy_import
 lazy_import('sage.homology.koszul_complex', 'KoszulComplex')
-
-from . import simplicial_set_catalog as simplicial_sets
+lazy_import('sage.homology', 'simplicial_complexes_catalog', 'simplicial_complexes')
+lazy_import('sage.homology', 'simplicial_set_catalog', 'simplicial_sets')

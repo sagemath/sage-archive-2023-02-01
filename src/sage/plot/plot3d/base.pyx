f5144256fd873eb051f174e4b86daf7abef1d4a5
Andrey Novoseltsev <novoselt@gmail.com>
2017-04-02 15:40:29 -0600

    Do not embedd all Sage options into a threejs scene

--- src/sage/plot/plot3d/base.pyx
+++ src/sage/plot/plot3d/base.pyx
@@ -420,6 +420,8 @@ cdef class Graphics3d(SageObject):
             html = f.read()
 
         html = html.replace('SAGE_SCRIPTS', scripts)
+        options = dict((key, options[key]) for key in
+            ['aspect_ratio', 'axes', 'axes_labels', 'decimals', 'frame'])
         html = html.replace('SAGE_OPTIONS', json.dumps(options))
         html = html.replace('SAGE_BOUNDS', bounds)
         html = html.replace('SAGE_LIGHTS', lights)

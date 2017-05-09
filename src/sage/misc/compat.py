efd8f13e8bc2a98d4d675dc4d567da6891165739
Erik M. Bray <erik.bray@lri.fr>
2017-05-09 15:18:36 +0200

    Don't use sage_wraps here as there's little benefit.  In particular the original function from ctypes does not have a docstring at all, so we were masking it.
    I think 8a09f157eea4ec50ff45d8d7ce2165c6cac21f4f also broke this on
    platforms other than Cygwin or OSX since it did not define an
    implementation to use for _find_library.

--- src/sage/misc/compat.py
+++ src/sage/misc/compat.py
@@ -15,14 +15,12 @@ import subprocess
 import sys
 
 from sage.env import SAGE_LOCAL
-from sage.misc.decorators import sage_wraps
 
 
 #################################################################
 # Replacements (as needed) for Python stdlib functions to provide
 # better platform compatibility
 #################################################################
-from ctypes.util import find_library as _orig_find_library
 if sys.platform == 'cygwin':
     # find_library that works in cygwin adapted from
     # http://cygwin-ports.svn.sourceforge.net/viewvc/cygwin-ports/ports/trunk/lang/python/2.5.2-ctypes-util-find_library.patch?revision=8245&view=markup
@@ -49,6 +47,8 @@ elif sys.platform == 'darwin':
     # On OSX non-standard library paths are not automatically found by the
     # find_library implementation without setting DYLD_LIBRARY_PATH; see
     # https://trac.sagemath.org/ticket/21399#comment:25
+    from ctypes.util import find_library as _orig_find_library
+
     def _find_library(name):
         sage_local_lib = os.path.join(SAGE_LOCAL, 'lib')
         orig_dyld_library_path = os.environ.get('DYLD_LIBRARY_PATH')
@@ -63,9 +63,13 @@ elif sys.platform == 'darwin':
         finally:
             if orig_dyld_library_path is not None:
                 os.environ['DYLD_LIBRARY_PATH'] = orig_dyld_library_path
+else:
+    # On other Unix-like platforms, at least where gcc is available,
+    # ctypes.util.find_library works, because it takes into account where gcc
+    # finds the library as if were being linked with
+    from ctypes.util import find_library as _find_library
 
 
-@sage_wraps(_orig_find_library)
 def find_library(name):
     """
     Returns the shared library filename for a given library.

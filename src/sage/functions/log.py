894c5f288f95ac22b7d468687c470ce046dbbc23
Ralf Stephan <ralf@ark.in-berlin.de>
2017-05-10 09:02:51 +0200

    22969: doctest fixes

--- src/sage/functions/log.py
+++ src/sage/functions/log.py
@@ -432,9 +432,9 @@ class Function_polylog(GinacFunction):
             sage: polylog(2.0, 1)
             1.64493406684823
             sage: polylog(2, 1.0)
-            NaN + NaN*I
+            NaN
             sage: polylog(2.0, 1.0)
-            NaN + NaN*I
+            NaN
         """
         GinacFunction.__init__(self, "polylog", nargs=2)
 

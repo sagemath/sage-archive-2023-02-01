3d7fd03b11b8ba38ed1348e673f44d38a8df7099
Kwankyu Lee <ekwankyu@gmail.com>
2017-06-06 10:15:37 +0200

    Fix EXAMPLE to EXAMPLES

--- src/sage/matrix/matrix2.pyx
+++ src/sage/matrix/matrix2.pyx
@@ -13274,7 +13274,7 @@ cdef class Matrix(matrix1.Matrix):
         - ``transformation`` -- boolean (default: ``False``): if ``True``,
           return the transformation matrix
 
-        EXAMPLE::
+        EXAMPLES::
 
             sage: B = matrix(ZZ, 3, [-1,-2,-3,4,5,6,7,8,9]); B
             [-1 -2 -3]

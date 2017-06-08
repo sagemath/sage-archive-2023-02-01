87ea9ff1253bac8c7bd11e7a7c6f33e141bf7882
Friedrich Wiemer <friedrich.wiemer@rub.de>
2017-06-08 11:13:30 +0200

    changed absolute imports to lazy imports

--- src/sage/crypto/all.py
+++ src/sage/crypto/all.py
@@ -1,16 +1,18 @@
-from __future__ import absolute_import
 from sage.misc.lazy_import import lazy_import
 
-lazy_import('sage.crypto.classical', 'AffineCryptosystem')
-lazy_import('sage.crypto.classical', 'HillCryptosystem')
-lazy_import('sage.crypto.classical', 'SubstitutionCryptosystem')
-lazy_import('sage.crypto.classical', 'ShiftCryptosystem')
-lazy_import('sage.crypto.classical', 'TranspositionCryptosystem')
-lazy_import('sage.crypto.classical', 'VigenereCryptosystem')
+lazy_import('sage.crypto.classical', ['AffineCryptosystem',
+                                      'HillCryptosystem',
+                                      'SubstitutionCryptosystem',
+                                      'ShiftCryptosystem',
+                                      'TranspositionCryptosystem',
+                                      'VigenereCryptosystem',
+                                     ])
 
-lazy_import('sage.crypto.stream', 'LFSRCryptosystem')
-lazy_import('sage.crypto.stream', 'ShrinkingGeneratorCryptosystem')
+lazy_import('sage.crypto.stream', ['LFSRCryptosystem',
+                                   'ShrinkingGeneratorCryptosystem',
+                                  ])
 
-lazy_import('sage.crypto.lfsr', 'lfsr_sequence')
-lazy_import('sage.crypto.lfsr', 'lfsr_autocorrelation')
-lazy_import('sage.crypto.lfsr', 'lfsr_connection_polynomial')
+lazy_import('sage.crypto.lfsr', ['lfsr_sequence',
+                                 'lfsr_autocorrelation',
+                                 'lfsr_connection_polynomial',
+                                ])

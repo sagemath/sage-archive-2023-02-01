baee716de596637d075ae814ef45167b38ea95dd
David Roe <roed.math@gmail.com>
2017-06-09 23:27:00 +0000

    Add inverse_of_unit to list of methods in polynomial quotient ring

--- src/sage/rings/polynomial/polynomial_quotient_ring.py
+++ src/sage/rings/polynomial/polynomial_quotient_ring.py
@@ -267,7 +267,7 @@ class PolynomialQuotientRing_generic(CommutativeRing):
         sage: isinstance(Q.an_element(),Q.element_class)
         True
         sage: [s for s in dir(Q.category().element_class) if not s.startswith('_')]
-        ['cartesian_product', 'is_idempotent', 'is_one', 'is_unit', 'lift', 'powers']
+        ['cartesian_product', 'inverse_of_unit', 'is_idempotent', 'is_one', 'is_unit', 'lift', 'powers']
         sage: first_class = Q.__class__
 
     We try to find out whether `Q` is a field. Indeed it is, and thus its category,
@@ -285,6 +285,7 @@ class PolynomialQuotientRing_generic(CommutativeRing):
          'euclidean_degree',
          'factor',
          'gcd',
+         'inverse_of_unit',
          'is_idempotent',
          'is_one',
          'is_unit',

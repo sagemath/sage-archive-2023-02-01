324f79c2c9c4484be5b843117aa02d0a75b19512
Friedrich Wiemer <friedrich.wiemer@rub.de>
2017-06-14 14:48:06 +0200

    deprecate sage.crypto.mq.sbox

---
+++ src/sage/crypto/mq/sbox.py
@@ -0,0 +1,7 @@
+from sage.misc.superseded import deprecated_function_alias
+
+import sage.crypto.sbox
+
+SBox = deprecated_function_alias(22986, sage.crypto.sbox.SBox)
+feistel_construction = deprecated_function_alias(22986, sage.crypto.sbox.feistel_construction)
+misty_construction = deprecated_function_alias(22986, sage.crypto.sbox.misty_construction)

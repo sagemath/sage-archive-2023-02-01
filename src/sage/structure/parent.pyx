5d873942ab2538b8b9bb2e27a9125f67a2e87d60
Travis Scrimshaw <tscrimsh at umn.edu>
2017-06-18 09:41:56 -0500

    Trivial doctest fixes.

--- src/sage/structure/parent.pyx
+++ src/sage/structure/parent.pyx
@@ -965,8 +965,8 @@ cdef class Parent(category_object.CategoryObject):
         it is a ring, from the point of view of categories::
 
             sage: MS.category()
-            Category of infinite algebras with basis over
-             (quotient fields and metric spaces)
+            Category of infinite finite dimensional algebras with basis
+             over (quotient fields and metric spaces)
             sage: MS in Rings()
             True
 

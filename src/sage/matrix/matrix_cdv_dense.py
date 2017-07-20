1f731ee1a5dfa9af675a7a510a74fb7bb99d24fa
Xavier Caruso <xavier.caruso@univ-rennes1.fr>
2017-07-20 01:26:06 -0400

    Check correctly (hopefully) that precision on input is enough

--- src/sage/matrix/matrix_cdv_dense.py
+++ src/sage/matrix/matrix_cdv_dense.py
@@ -17,12 +17,16 @@ def smith_normal_form(M, transformation):
     S = M.parent()(M.list())
     smith = M.parent()(0)
     R = M.base_ring()
+    if R.tracks_precision():
+        precM = min([ x.precision_absolute() for x in M.list() ])
+
     if transformation:
         from sage.matrix.special import identity_matrix
         left = identity_matrix(R,n)
         right = identity_matrix(R,m)
     else:
         left = right = None
+
     val = -Infinity
     for piv in range(min(n,m)):
         curval = Infinity
@@ -39,11 +43,8 @@ def smith_normal_form(M, transformation):
             break
         val = curval
 
-        if S[pivi,pivj] == 0:
-            if curval is Infinity:
-                break
-            else:
-                raise PrecisionError("Not enough precision to compute Smith normal form")
+        if R.tracks_precision() and precM is not Infinity and val <= precM:
+            raise PrecisionError("Not enough precision to compute Smith normal form")
 
         S.swap_rows(pivi,piv)
         S.swap_columns(pivj,piv)
@@ -69,11 +70,8 @@ def smith_normal_form(M, transformation):
                 right.add_multiple_of_column(j,piv,scalar)
 
     if transformation:
-        if R.tracks_precision():
-            prec = min([ x.precision_absolute() for x in M.list() ])
-            if prec is not Infinity:
-                prec -= curval
-            left = left.apply_map(lambda x: x.add_bigoh(prec))
+        if R.tracks_precision() and precM is not Infinity:
+            left = left.apply_map(lambda x: x.add_bigoh(precM-val))
         return smith, left, right
     else:
         return smith

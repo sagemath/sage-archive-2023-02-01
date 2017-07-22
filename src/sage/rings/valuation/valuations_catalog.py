37d718725faa28404b3db194cc81b9eede19c01e
Julian Rüth <julian.rueth@fsfe.org>
2017-07-22 01:25:06 -0500

    Remove absolute_import
    otherwise it shows up if you type valuations.<TAB>

--- src/sage/rings/valuation/valuations_catalog.py
+++ src/sage/rings/valuation/valuations_catalog.py
@@ -1,6 +1,5 @@
-from __future__ import absolute_import
 from sage.rings.padics.padic_valuation import pAdicValuation
 from sage.rings.function_field.function_field_valuation import FunctionFieldValuation
-from .gauss_valuation import GaussValuation
-from .trivial_valuation import TrivialDiscretePseudoValuation, TrivialPseudoValuation, TrivialValuation
-from .limit_valuation import LimitValuation
+from sage.rings.valuation.gauss_valuation import GaussValuation
+from sage.rings.valuation.trivial_valuation import TrivialDiscretePseudoValuation, TrivialPseudoValuation, TrivialValuation
+from sage.rings.valuation.limit_valuation import LimitValuation

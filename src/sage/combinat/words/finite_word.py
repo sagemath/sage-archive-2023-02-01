182d7d30a5f6ab340209e5f26c9dc61f4b43c6bd
Emile Nadeau <nadeau.emile@gmail.com>
2017-07-31 22:08:10 -0400

    Trac #23131: Add methods to compute longest common extension in words.

--- src/sage/combinat/words/finite_word.py
+++ src/sage/combinat/words/finite_word.py
@@ -2205,6 +2205,65 @@ class FiniteWord_class(Word_class):
             return False
         return True
 
+    def longest_forward_extension(self,x,y):
+        r"""
+        Compute the length of le longest factor of self that starts at x and that
+        matches a factor that starts at y. Returns 0 if x or y are not valid
+        position in self.
+
+        INPUTS:
+
+            x,y - positions in self
+
+        EXAMPLES:
+
+            sage:w=Word('0011001')
+            sage:w.longest_forward_extension(0,5)
+            3
+            sage:w.longest_forward_extension(0,2)
+            0
+            sage:w.longest_forward_extension(-3,2)
+            0
+        """
+        length=self.length()
+        if not (0<=x and 0<=y):
+            return 0
+        l=0
+        while x<length and y<length and self[x]==self[y]:
+            l+=1
+            x+=1
+            y+=1
+        return l
+
+    def longest_backward_extension(self,x,y):
+        r"""
+        Compute the length of le longest factor of self that ends at x and that
+        matches a factor that ends at y. Return 0 if x or y are not valid position
+        in self.
+
+        INPUTS:
+
+            x,y - positions in self
+
+        EXAMPLES:
+            
+            sage:w=Word('0011001')
+            sage:w.longest_backward_extension(7,2)
+            3
+            sage:w.longest_backward_extension(1,5)
+            1
+            sage:w.longest_forward_extension(4,23)
+        """
+        length=self.length()
+        if not (x<length and y<length):
+            return 0
+        l=0
+        while x>=0 and y>=0 and self[x]==self[y]:
+            l+=1
+            x-=1
+            y-=1
+        return l
+
     def longest_common_suffix(self, other):
         r"""
         Return the longest common suffix of ``self`` and ``other``.

62fe4ece1ac4af6a011e6be9b257c8a23aa99696
bbarros <bbarros@slu.edu>
2017-08-01 17:29:41 -0500

    8423: Added plotting Julia sets

--- src/sage/dynamics/complex_dynamics/all.py
+++ src/sage/dynamics/complex_dynamics/all.py
@@ -1,2 +1,3 @@
 from sage.misc.lazy_import import lazy_import
 lazy_import("sage.dynamics.complex_dynamics.mandel_julia", "mandelbrot_plot")
+lazy_import("sage.dynamics.complex_dynamics.mandel_julia", "julia_plot")
--- src/sage/dynamics/complex_dynamics/mandel_julia.py
+++ src/sage/dynamics/complex_dynamics/mandel_julia.py
@@ -1,12 +1,16 @@
 r"""
 Mandelbrot and Julia sets
 
-Plots the Mandelbrot set for the map `Q_c(z)=z^2+c` in the complex plane.
+Plots the Mandelbrot and Julia sets for the map `Q_c(z)=z^2+c` in the complex
+plane.
 
 The Mandelbrot set is the set of complex numbers `c` for which the function
 `Q_c(z)=z^2+c` does not diverge when iterated from `z = 0`. This set of complex
 numbers can be visualized by plotting each value for `c` in the complex plane.
 The Mandelbrot set is an example of a fractal when plotted in the complex plane.
+
+The Julia set for a given `c` is the set of complex number for which the
+function `Q_c(z)=z^2+c` does not diverge when iterated from `z = c`.
 
 AUTHORS:
 
@@ -25,12 +29,23 @@ AUTHORS:
 #*****************************************************************************
 
 from __future__ import absolute_import, division
-from sage.dynamics.complex_dynamics.mandel_julia_helper import fast_mandelbrot_plot
+from sage.dynamics.complex_dynamics.mandel_julia_helper import (fast_mandelbrot_plot,
+                                                                fast_julia_plot,
+                                                                julia_helper)
 from sagenb.notebook.interact import interact
 from sagenb.notebook.interact import slider
 from sagenb.notebook.interact import input_box
 from sagenb.notebook.interact import color_selector
+from sagenb.notebook.interact import checkbox
 from sage.plot.colors import Color
+from sage.repl.image import Image
+from sage.functions.log import function_log as log
+from sage.rings.rational_field import QQ
+from sage.rings.all import CC
+from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.schemes.projective.projective_space import ProjectiveSpace
+from sage.categories.homset import End
+from sage.misc.prandom import randint
 
 def mandelbrot_plot(**kwds):
     r"""
@@ -126,9 +141,169 @@ def mandelbrot_plot(**kwds):
             color_num=input_box(number_of_colors, 'Number of Colors'),
             image_color=color_selector(default=Color([j/255 for j in base_color]),
              label="Image Color", hide_box=True)):
-            return fast_mandelbrot_plot(real_center, im_center, width, iterations,
-             pixel_count, level_sep, color_num, image_color).show()
+            return fast_mandelbrot_plot(real_center, im_center, width,
+             iterations, pixel_count, level_sep, color_num, image_color).show()
 
     else:
         return fast_mandelbrot_plot(x_center, y_center, image_width, max_iteration,
          pixel_count, iteration_level, number_of_colors, base_color)
+
+def julia_plot(c=-1, **kwds):
+    r"""
+    Plots the Julia set of a given complex `c` value. Users can specify whether
+    they would like to diplay the Mandelbrot side by side with the Julia set.
+
+    The Julia set of a given `c` value is the set of complex numbers for which
+    the function `Q_c(z)=z^2+c` does not diverge when iterated from `z = c`.
+    This set of complex numbers can be visualized by plotting each value for
+    `c` in the complex plane. Julia sets are examples of fractals when plotted
+    in the complex plane.
+
+    ALGORITHM:
+
+    Let each pixel in the image be a point `c \in \mathbb{C}` and define the
+    map `Q_c(z) = z^2 + c`. If `|Q_{c}^{k}(c)| > 2` for some `k \geq 0`, it
+    follows that `Q_{c}^{n}(c) \to \infty`. Let `N` be the maximum number of
+    iterations. Compute the first `N` points on the orbit of `c` under `Q_c`.
+    If for any `k < N`, `|Q_{c}^{k}(c)| > 2`, we stop the iteration and assign
+    a color to the point `c` based on how quickly `c` escaped to infinity under
+    iteration of `Q_c`. If `|Q_{c}^{i}(c)| \leq 2` for all `i \leq N`, we assume
+    `c` is in the Julia set and assign the point `c` the color black.
+
+    INPUT:
+
+    - ``c`` -- complex (optional - default: ``-1``), complex point `c` that determines the Julia set.
+
+    kwds:
+
+    - ``period`` -- list (optional - default: ``None``), returns the Julia set for a `c` value with the given cycle structure.
+
+    - ``return_points`` -- boolean (optional - default: ``False``), when ``period`` and ``return_points`` are ``True``, all `c` values with the given cycle structure are returned in a list.
+
+    - ``mandelbrot`` -- boolean (optional - default: ``True``), when set to ``True``, an image of the Mandelbrot set is appended to the right of the Julia set.
+
+    - ``point_color`` -- RGB color (optional - default: ``[255, 0, 0]``), color of the point `c` in the Mandelbrot set.
+
+    - ``x_center`` -- double (optional - default: ``-1.0``), Real part of center point.
+
+    - ``y_center`` -- double (optional - default: ``0.0``), Imaginary part of center point.
+
+    - ``image_width`` -- double (optional - default: ``4.0``), width of image in the complex plane.
+
+    - ``max_iteration`` -- long (optional - default: ``500``), maximum number of iterations the map ``Q_c(z)``.
+
+    - ``pixel_count`` -- long (optional - default: ``500``), side length of image in number of pixels.
+
+    - ``base_color`` -- RGB color (optional - default: ``[40, 40, 40]``), color used to determine the coloring of set.
+
+    - ``iteration_level`` -- long (optional - default: 1), number of iterations between each color level.
+
+    - ``number_of_colors`` -- long (optional - default: 30), number of colors used to plot image.
+
+    - ``interact`` -- boolean (optional - default: ``False``), controls whether plot will have interactive functionality.
+
+    OUTPUT:
+
+    24-bit RGB image of the Julia set in the complex plane.
+
+    EXAMPLES:
+
+    ::
+
+        sage: julia_plot()
+        1001x500px 24-bit RGB image
+
+    To display only the Julia set, set ``mandelbrot`` to ``False``::
+
+        sage: julia_plot(mandelbrot=False)
+        500x500px 24-bit RGB image
+
+    To return the Julia set of a `c` value with cycle structure `(2,3)`, set ``period = [2,3]``::
+
+        sage: julia_plot(period=[2,3])
+        1001x500px 24-bit RGB image
+
+    To return all values for `c` that have cycle structure `(2,1)`, set ``period = [2,1]`` and ``return_points`` to ``True``::
+
+        sage: julia_plot(period=[2,1], return_points=True)
+        [-2.00000000000000, 0.000000000000000]
+
+    To display an interactive plot of the Julia set in the Notebook, set ``interact`` to ``True``::
+
+        sage: julia_plot(interact=True)
+        <html>...</html>
+    """
+
+    x_center = kwds.pop("x_center", 0.0)
+    y_center = kwds.pop("y_center", 0.0)
+    image_width = kwds.pop("image_width", 4.0)
+    max_iteration = kwds.pop("max_iteration", 500)
+    pixel_count = kwds.pop("pixel_count", 500)
+    base_color = kwds.pop("base_color", [50, 50, 50])
+    iteration_level = kwds.pop("iteration_level", 1)
+    number_of_colors = kwds.pop("number_of_colors", 50)
+    point_color = kwds.pop("point_color", [255, 0, 0])
+    interacts = kwds.pop("interact", False)
+    mandelbrot = kwds.pop("mandelbrot", True)
+    period = kwds.pop("period", None)
+    return_points = kwds.pop("return_points", False)
+
+    if period is not None:
+        R = PolynomialRing(QQ, 'c')
+        c = R.gen()
+        P = ProjectiveSpace(R, 1, 'x,y')
+        x,y = P.gens()
+        H = End(P)
+        f = H([x**2+c*y**2, y**2])
+        L = f.dynatomic_polynomial(period).subs({x:0,y:1}).roots(ring=CC)
+
+        if return_points:
+            c_list = []
+            for k in L:
+                c_list.append(k[0])
+            return c_list
+
+        else:
+            c = L[randint(0,len(L)-1)][0]
+
+    c_real = CC(c).real()
+    c_imag = CC(c).imag()
+
+    if interacts:
+        @interact(layout={'bottom':[['real_center'], ['im_center'], ['width']],
+         'top':[['iterations'], ['level_sep'], ['color_num'], ['mandel'],
+         ['cx'], ['cy']], 'right':[['image_color'], ['pt_color']]})
+        def _(cx = input_box(c_real, '$Re(c)$'),
+            cy = input_box(c_imag, '$Im(c)$'),
+            real_center=input_box(x_center, 'Real Center'),
+            im_center=input_box(y_center, 'Imaginary Center'),
+            width=input_box(image_width, 'Width of Image'),
+            iterations=input_box(max_iteration, 'Max Number of Iterations'),
+            level_sep=input_box(iteration_level, 'Iterations between Colors'),
+            color_num=input_box(number_of_colors, 'Number of Colors'),
+            image_color=color_selector(default=Color([j/255 for j in base_color]),
+             label="Image Color", hide_box=True),
+            pt_color=color_selector(default=Color([j/255 for j in point_color]),
+             label="Point Color", hide_box=True),
+            mandel=checkbox(mandelbrot, label='Mandelbrot set')):
+
+            if mandel:
+                return julia_helper(cx, cy, real_center, im_center,
+                 width, iterations, pixel_count, level_sep, color_num,
+                 image_color, pt_color).show()
+
+            else:
+                return fast_julia_plot(cx, cy, real_center, im_center,
+                 width, iterations, pixel_count, level_sep, color_num,
+                 image_color).show()
+
+    else:
+        if mandelbrot:
+            return julia_helper(c_real, c_imag, x_center, y_center,
+             image_width, max_iteration, pixel_count, iteration_level,
+             number_of_colors, base_color, point_color)
+
+        else:
+            return fast_julia_plot(c_real, c_imag, x_center, y_center,
+             image_width, max_iteration, pixel_count, iteration_level,
+             number_of_colors, base_color)

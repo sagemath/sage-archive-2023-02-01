4bf27e784f8095b268aa0313334423ac3b14da0b
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2017-08-07 21:11:31 +0200

    trac 23589 one detail

--- src/sage/misc/sageinspect.py
+++ src/sage/misc/sageinspect.py
@@ -1975,7 +1975,7 @@ def sage_getsourcelines(obj):
 
         sage: from sage.misc.sageinspect import sage_getsourcelines
         sage: sage_getsourcelines(matrix)[1]
-        26
+        28
         sage: sage_getsourcelines(matrix)[0][0][6:]
         'MatrixFactory(object):\n'
 

3fcd5813d68f8050992ed5bd36dac3bd89edd526
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2017-08-22 09:15:14 +0200

    implementation of Gauss sums for general finite fields

--- src/sage/rings/padics/misc.py
+++ src/sage/rings/padics/misc.py
@@ -100,6 +100,10 @@ def gauss_sum(a, p, f, prec=20):
         6*pi^2 + 7*pi^14 + 11*pi^26 + 3*pi^62 + 6*pi^74 + 3*pi^86 + 5*pi^98 +
         pi^110 + 7*pi^134 + 9*pi^146 + 4*pi^158 + 6*pi^170 + 4*pi^194 +
         pi^206 + 6*pi^218 + 9*pi^230 + O(pi^242)
+
+    .. SEEALSO::
+
+        :func:`sage.arith.misc.gauss_sum` for another version
     """
     from sage.rings.padics.factory import Zp
     from sage.rings.all import PolynomialRing

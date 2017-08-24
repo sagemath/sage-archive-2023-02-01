3f635679eed723dbb69a47168f5eb6fcc0163c32
Travis Scrimshaw <tscrimsh at umn.edu>
2017-08-24 11:29:33 -0500

    Removing unneeded imports of symmetrica and use getattr instead of eval.

--- src/sage/combinat/lr_tableau.py
+++ src/sage/combinat/lr_tableau.py
@@ -32,7 +32,6 @@ from sage.structure.parent import Parent
 from sage.structure.list_clone import ClonableList
 from sage.combinat.tableau import SemistandardTableau, SemistandardTableaux
 from sage.combinat.partition import Partition, Partitions
-from sage.libs.symmetrica.all import kostka_tab
 
 class LittlewoodRichardsonTableau(SemistandardTableau):
     r"""

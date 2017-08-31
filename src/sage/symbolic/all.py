18b6c7ad1b34bca2d52834f19ce6863c477ee690
Ralf Stephan <ralf@ark.in-berlin.de>
2017-08-31 09:53:20 +0200

    10035: hold context

--- src/sage/symbolic/all.py
+++ src/sage/symbolic/all.py
@@ -6,7 +6,7 @@ i = I
 from .ring import SR
 from .constants import (pi, e, NaN, golden_ratio, log2, euler_gamma, catalan,
                        khinchin, twinprime, mertens, glaisher)
-from .expression import Expression, solve_diophantine
+from .expression import Expression, solve_diophantine, hold
 from .callable import CallableSymbolicExpressionRing
 
 from sage.symbolic.relation import solve, solve_mod, solve_ineq

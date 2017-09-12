c4a44f633790effdff1d4123e8a7a7c3155e9c9e
Martin Rubey <martin.rubey@tuwien.ac.at>
2017-09-12 13:46:31 +0200

    undo edit of test output - this is fixed by #22525 anyway

--- src/sage/symbolic/integration/integral.py
+++ src/sage/symbolic/integration/integral.py
@@ -539,7 +539,7 @@ def integrate(expression, v=None, a=None, b=None, algorithm=None, hold=False):
 
         sage: f(x) = sqrt(x+sqrt(1+x^2))/x
         sage: integrate(f(x), x, algorithm="fricas")      # optional - fricas
-        2*sqrt(x + sqrt(x^2 + 1)) - 2*atan(sqrt(x + sqrt(x^2 + 1))) - log(sqrt(x + sqrt(x^2 + 1)) + 1) + log(sqrt(x + sqrt(x^2 + 1)) - 1)
+        2*sqrt(x + sqrt(x^2 + 1)) - 2*arctan(sqrt(x + sqrt(x^2 + 1))) - log(sqrt(x + sqrt(x^2 + 1)) + 1) + log(sqrt(x + sqrt(x^2 + 1)) - 1)
 
     The following definite integral is not found with the
     default integrator::
@@ -551,7 +551,7 @@ def integrate(expression, v=None, a=None, b=None, algorithm=None, hold=False):
     Both fricas and sympy give the correct result::
 
         sage: integrate(f(x), x, 1, 2, algorithm="fricas")  # optional - fricas
-        -1/2*pi + atan(8) + atan(5) + atan(2) + atan(1/2)
+        -1/2*pi + arctan(8) + arctan(5) + arctan(2) + arctan(1/2)
         sage: integrate(f(x), x, 1, 2, algorithm="sympy")
         -1/2*pi + arctan(8) + arctan(5) + arctan(2) + arctan(1/2)
 

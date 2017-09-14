c859cf2e15dd2c3a1d2840462f352b11e1c3dbea
Martin Rubey <martin.rubey@tuwien.ac.at>
2017-09-14 23:32:15 +0200

    speedup left_key_tableau, right_key_tableau and is_key_tableau

--- src/sage/combinat/tableau.py
+++ src/sage/combinat/tableau.py
@@ -1084,18 +1084,18 @@ class Tableau(ClonableList):
         r"""
         Return the sign matrix of ``self``.
 
-        A sign matrix is an `m \times n` matrix of 0's, 1's and -1's such that the 
-        partial sums of each column is either 0 or 1 and the partial sums of 
+        A sign matrix is an `m \times n` matrix of 0's, 1's and -1's such that the
+        partial sums of each column is either 0 or 1 and the partial sums of
         each row is non-negative. [Aval2008]_
-        
-        INPUT: 
 
-        - ``max_entry`` -- A non-negative integer, the  maximum allowable number in 
+        INPUT:
+
+        - ``max_entry`` -- A non-negative integer, the  maximum allowable number in
           the tableau. Defaults to the largest entry in the tableau if not specified.
 
 
-        EXAMPLES:: 
-       
+        EXAMPLES::
+
             sage: t = SemistandardTableau([[1,1,1,2,4],[3,3,4],[4,5],[6,6]])
             sage: t.to_sign_matrix(6)
             [ 0  0  0  1  0  0]
@@ -1125,7 +1125,7 @@ class Tableau(ClonableList):
 
         .. [Aval2008] Jean-Christope Aval.
            *Keys and Alternating Sign Matrices*,
-           Seminaire Lotharingien de Combinatoire 59 (2008) B59f 
+           Seminaire Lotharingien de Combinatoire 59 (2008) B59f
            :arxiv:`0711.2150`
         """
         from sage.rings.all import ZZ
@@ -1133,7 +1133,7 @@ class Tableau(ClonableList):
         PI = PositiveIntegers()
         for row in self:
             if any(c not in PI for c in row):
-                raise ValueError("the entries must be non-negative integers")        
+                raise ValueError("the entries must be non-negative integers")
         from sage.matrix.matrix_space import MatrixSpace
         if max_entry is None:
             max_entry=max([max(c) for c in self])
@@ -3508,8 +3508,12 @@ class Tableau(ClonableList):
             sage: t.is_key_tableau()
             False
         """
-        itr = enumerate(self.conjugate()[1:],1)
-        return all(x in self.conjugate()[i-1] for i, col in itr for x in col)
+        T_conj = self.conjugate()
+        for i in range(1, len(T_conj)):
+            if not all(x in T_conj[i-1] for x in T_conj[i]):
+                return False
+        return True
+
 
     def right_key_tableau(self):
         """
@@ -3555,9 +3559,6 @@ class Tableau(ClonableList):
             sage: t.right_key_tableau() == t
             True
         """
-        if self.is_key_tableau():
-            return self
-
         cols_list = self.conjugate()
         key = [[] for row in cols_list]
 
@@ -3619,9 +3620,6 @@ class Tableau(ClonableList):
             sage: t.left_key_tableau() == t
             True
         """
-        if self.is_key_tableau():
-            return self
-
         cols_list = self.conjugate()
         key = [[] for row in cols_list]
         key[0] = list(cols_list[0])
@@ -3903,7 +3901,7 @@ class Tableau(ClonableList):
         deg = self.shape()._initial_degree(e,multicharge)
         res = self.shape().initial_tableau().residue_sequence(e, multicharge)
         for r in self.reduced_row_word():
-            if res[r] == res[r+1]: 
+            if res[r] == res[r+1]:
                 deg -= 2
             elif res[r] == res[r+1] + 1 or res[r] == res[r+1] - 1:
                 deg += (e == 2 and 2 or 1)
@@ -3993,7 +3991,7 @@ class Tableau(ClonableList):
 
     def first_column_descent(self):
         r"""
-        Return the first cell where ``self`` is not column standard. 
+        Return the first cell where ``self`` is not column standard.
 
         Cells are ordered left to right along the rows and then top to bottom.
         That is, the cell `(r, c)` with `r` and `c` minimal such that

378d28611c10280b3caaf66ee293e925e328d70a
Vincent Delecroix <20100.delecroix@gmail.com>
2017-09-15 00:05:56 +0200

    23864: fix hash of multivariate laurent polys

--- src/sage/rings/polynomial/laurent_polynomial.pyx
+++ src/sage/rings/polynomial/laurent_polynomial.pyx
@@ -1708,12 +1708,11 @@ cdef class LaurentPolynomial_mpair(LaurentPolynomial):
         r"""
         TESTS:
 
-        Test that the hash is non-constant (the hash does not need to be
-        deterministic so we leave some slack for collisions)::
+        Test that the hash is non-constant::
 
             sage: L.<w,z> = LaurentPolynomialRing(QQ)
-            sage: len({hash(w^i*z^j) for i in [-2..2] for j in [-2..2]}) > 20
-            True
+            sage: len({hash(w^i*z^j) for i in [-2..2] for j in [-2..2]})
+            25
 
         Check that :trac:`20490` is fixed::
 
@@ -1723,11 +1722,49 @@ cdef class LaurentPolynomial_mpair(LaurentPolynomial):
             (a, a)
             sage: p == R.one()
             True
-            sage: hash(p) == hash(R.one())
+            sage: hash(p)
+            1
+
+        Check that :trac:`23864` is fixed (compatibility with integers, rationals
+        and polynomial rings)::
+
+            sage: L = LaurentPolynomialRing(QQ, 'x0,x1,x2')
+            sage: hash(L.zero())
+            0
+            sage: hash(L.one())
+            1
+            sage: hash(-L.one())
+            -2
+            sage: hash(L(1/2)) == hash(1/2)
+            True
+
+            sage: R = PolynomialRing(QQ, 'x0,x1,x2')
+            sage: x0,x1,x2 = R.gens()
+            sage: hash(x0) == hash(L(x0))
+            True
+            sage: hash(1 - 7* x0 + x1*x2) == hash(L(1 - 7 *x0 + x1*x2))
             True
         """
-        self._normalize()
-        return hash(self._poly) ^ hash(self._mon)
+        # we reimplement the hash from multipolynomial to handle negative exponents
+        # (see multi_polynomial.pyx)
+        cdef long result = 0
+        cdef long exponent
+        cdef list var_name_hash = [hash(v) for v in self._parent.variable_names()]
+        cdef int p
+        cdef int n = len(var_name_hash)
+        cdef long c_hash
+        for m,c in self._poly.dict().iteritems():
+            c_hash = hash(c)
+            if c_hash != 0:
+                for p in range(n):
+                    exponent = m[p] + self._mon[p]
+                    if not exponent:
+                        continue
+                    c_hash = (1000003 * c_hash) ^ var_name_hash[p]
+                    c_hash = (1000003 * c_hash) ^ exponent
+                result += c_hash
+
+        return result
 
     cdef _new_c(self):
         """
@@ -1758,7 +1795,15 @@ cdef class LaurentPolynomial_mpair(LaurentPolynomial):
             sage: f = x*y + 2*y*x^2 + y # indirect doctest
             sage: f.factor() # Notice the y has been factored out.
             (y) * (2*x^2 + x + 1)
+
+        Check that :trac:`23864` has been fixed::
+
+            sage: L.zero()._normalize()
         """
+        if not self._poly:
+            self._mon = ETuple({}, int(self.parent().ngens()))
+            return
+
         D = self._poly._mpoly_dict_recursive(self.parent().variable_names(), self.parent().base_ring())
         if i is None:
             e = None

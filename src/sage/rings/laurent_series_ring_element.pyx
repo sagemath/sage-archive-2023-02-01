a0d1e19b22522c31a49463717506511247281567
Koen van Woerden <koenvanwoerden@gmail.com>
2017-09-28 12:23:32 +0200

    Expand docstring.
    Expand docstring and explain what is meant by adding O(q^prec).

--- src/sage/rings/laurent_series_ring_element.pyx
+++ src/sage/rings/laurent_series_ring_element.pyx
@@ -694,8 +694,13 @@ cdef class LaurentSeries(AlgebraElement):
     def O(self, prec):
         r"""
         Return the Laurent series of precision at most ``prec`` got by adding
-        `O(q^\text{prec})` to `f`, where `q` is the variable.
-
+        `O(q^\text{prec})` to `f`, where `q` is the variable. 
+        
+        The precision of `f` and the integer ``prec`` can be arbitrary. The
+        resulting Laurent series will have precision equal to the minimum of
+        the precision of `f` and ``prec``. The term `O(q^\text{prec})` is the
+        zero series with precision ``prec``.
+       
         EXAMPLES::
 
             sage: R.<t> = LaurentSeriesRing(QQ)

6955600fc2baff0cf8a0fd60977cdda3ebca3ef9
Anthony Varilly-Alvarado <av15@rice.edu>
2017-10-01 17:30:10 +0000

    Documented qmodnz.py and qmodnz_element.py

--- src/sage/groups/abelian_gps/qmodnz.py
+++ src/sage/groups/abelian_gps/qmodnz.py
@@ -5,14 +5,73 @@ from sage.categories.groups import Groups
 from sage.arith import srange
 
 class QmodnZ(AbelianGroup):
+    r"""
+    The ``QmodnZ`` class represents the abelian group Q/nZ.
+
+    INPUT:
+
+    The constructor may be called in any of the following ways.
+
+    #. ``QmodnZ(n)``, where
+
+        - `n` -- a rational number (including 0 or negative rational numbers).
+
+    #. ``QQ/(n*ZZ)``, where
+
+        - `n` -- an integer (including 0 or negative integers).
+
+
+    OUTPUT:
+
+    The abelian group Q/nZ.
+
+    EXAMPLES::
+
+        sage: from sage.groups.abelian_gps.qmodnz import QmodnZ
+        sage: QQ/(19*ZZ)
+        Q/19Z
+
+        sage: QmodnZ(19)
+        Q/19Z
+
+        sage: QmodnZ(2/3)
+        Q/(2/3)Z
+    """
+
     Element = QmodnZ_Element
     def __init__(self, n=1):
+        r"""
+        EXAMPLES::
+
+            sage: G = QmodnZ(2)
+            sage: G
+            Q/2Z
+
+        TESTS::
+
+            sage: G = QQ/(19*ZZ)
+            sage: TestSuite(G).run()
+        """
         self.n = QQ(n).abs()
         category = Groups().Commutative().Topological().Infinite()
         AbelianGroup.__init__(self, base=ZZ, category=category)
         self._populate_coercion_lists_(coerce_list=[QQ])
 
     def _repr_(self):
+        r"""
+        Display the group.
+
+        EXAMPLES::
+
+            sage: G = QmodnZ(1); G
+            Q/Z
+
+            sage: G = QQ/(3*ZZ); G
+            Q/3Z
+
+            sage: G = QmodnZ(1/5); G
+            Q/(1/5)Z
+        """
         if self.n == 1:
             return "Q/Z"
         elif self.n in ZZ:
@@ -21,22 +80,116 @@ class QmodnZ(AbelianGroup):
             return "Q/(%s)Z"%(self.n)
 
     def __eq__(self, other):
+        r"""
+        Return ``True`` if ``self`` and ``other`` are identical.
+
+        This means they are the same abelian group.
+
+        EXAMPLES::
+
+            sage: G = QQ/(5*ZZ)
+            sage: H = QQ/(5*ZZ)
+            sage: K = QQ/(3*ZZ)
+            sage: L = QmodnZ(25/5)
+            sage: G == H #indirect doctest
+            True
+            sage: G == K #indirect doctest
+            False
+
+        TESTS::
+
+            sage: G == G
+            True
+            sage: H == G
+            True
+            sage: G == L
+            True
+            sage: K == 7
+            False
+        """
         if type(self) == type(other):
             return self.n == other.n
         return NotImplemented
 
     def __ne__(self, other):
+        r"""
+        Return ``True`` if ``self`` and ``other`` are not identical.
+
+        This means they are different abelian groups.
+
+        EXAMPLES::
+
+            sage: G = QQ/(5*ZZ)
+            sage: H = QQ/(5*ZZ)
+            sage: K = QQ/(3*ZZ)
+            sage: L = QmodnZ(25/5)
+            sage: G != H #indirect doctest
+            False
+            sage: G != K #indirect doctest
+            True
+        """
+
         if type(self) == type(other):
             return self.n != other.n
         return NotImplemented
 
+    #TODO: Disallow order comparisons between different Q/nZ's
+    # e.g., sage: QmodnZ(10/3) > QmodnZ(5/3)
+    # returns False.
+
     def _element_constructor_(self, x):
+        r"""
+        Construct an element in Q/nZ.
+
+        EXAMPLES::
+
+            sage: G = QmodnZ(2/3)
+            sage: G(5/6)
+            1/6
+        """
         return self.element_class(self, QQ(x))
 
     def random_element(self, *args, **kwds):
+        r"""
+        Return a random element of Q/nZ.
+
+        EXAMPLES::
+
+            sage: G = QQ/(6*ZZ)
+            sage: G.random_element()
+            1
+            sage: G.random_element()
+            35/6
+            sage: G.random_element()
+            1/4
+
+        Extra positional or keyword arguments are passed through::
+
+            sage: G = QmodnZ(4/5)
+            sage: G.random_element(distribution='1/n')
+            1/2
+            sage: G.random_element(distribution='1/n')
+            3/5
+            sage: G.random_element(distribution='1/n')
+            11/20
+        """
         return self(QQ.random_element(*args, **kwds))
 
     def __iter__(self):
+        r"""
+        Creates an iterator that generates the elements of Q/nZ without
+        repetition, organized by increasing denominator; for a fixed denominator
+        elements are listed by increasing numerator.
+
+        EXAMPLES:
+
+            The first 19 elements of Q/5Z::
+
+            sage: import itertools
+            sage: lst = [a for a in itertools.islice(QQ/(5*ZZ),19)]; lst
+            [0, 1, 2, 3, 4, 1/2, 3/2, 5/2, 7/2, 9/2, 1/3, 2/3, 4/3, 5/3, 7/3, 8/3, 10/3, 11/3, 13/3]
+        """
+
         if self.n == 0:
             for x in QQ:
                 yield self(x)
--- src/sage/groups/abelian_gps/qmodnz_element.py
+++ src/sage/groups/abelian_gps/qmodnz_element.py
@@ -5,7 +5,41 @@ from sage.rings.infinity import infinity
 from sage.structure.richcmp import richcmp, op_EQ, op_NE
 
 class QmodnZ_Element(AdditiveGroupElement):
+    r"""
+    The ``QmodnZ_Element`` class represents an element of the abelian group Q/nZ.
+
+    INPUT:
+
+    - ``q`` -- a rational number.
+
+    - ``parent`` -- the parent abelian group Q/nZ
+
+    OUTPUT:
+
+    The element `q` of abelian group Q/nZ, in standard form.
+
+    EXAMPLES::
+
+        sage: from sage.groups.abelian_gps.qmodnz import QmodnZ
+        sage: G = QQ/(19*ZZ)
+        sage: q = G(400/19)
+        sage: q
+        39/19
+
+    """
+
     def __init__(self, parent, x, construct=False):
+        r"""
+        Create an element of Q/nZ.
+
+        EXAMPLES::
+
+            sage: G = QQ/(3*ZZ)
+            sage: g = G.random_element()
+            sage: g
+            3/8
+        """
+
         AdditiveGroupElement.__init__(self, parent)
         # x = (a/b) = q(n/m) + r/mb
         # am = q(nb) + r
@@ -24,9 +58,42 @@ class QmodnZ_Element(AdditiveGroupElement):
             self._x = r/(m*b)
 
     def lift(self):
+        r"""
+        Returns the rational number representation of ``self``
+
+        EXAMPLES::
+            sage: G = QQ/(5*ZZ)
+            sage: g = G(2/4); g
+            1/2
+            sage: q = lift(g); q
+            1/2
+
+        TESTS::
+            sage: q.parent() == QQ
+            True
+        """
+
         return self._x
 
     def __neg__(self):
+        r"""
+        Returns the additive inverse of ``self`` in Q/nZ.
+
+        EXAMPLES::
+            sage: G = QmodnZ(5/7)
+            sage: g = G(13/21)
+            sage: -g
+            2/21
+
+        TESTS::
+            sage: G = QmodnZ(19/23)
+            sage: g = G(15/23)
+            sage: -g
+            4/23
+            sage: g + -g == G(0)
+            True
+        """
+
         if self._x == 0:
             return self
         else:
@@ -34,13 +101,47 @@ class QmodnZ_Element(AdditiveGroupElement):
             return QZ.element_class(QZ, QZ.n - self._x, True)
 
     def _add_(self, other):
+        r"""
+        Returns the sum of ``self`` and ``other`` in Q/nZ
+
+        EXAMPLES::
+            sage: G = QmodnZ(9/10)
+            sage: g = G(5)
+            sage: h = G(1/2)
+            sage: g + h
+            1/10
+            sage: g + h == G(1/10) #indirect doctest
+            True
+
+        TESTS::
+            sage: h + g == G(1/10)
+            True
+        """
+
         QZ = self.parent()
         ans = self._x + other._x
-        if ans > QZ.n:
+        if ans >= QZ.n:
             ans -= QZ.n
         return QZ.element_class(QZ, ans, True)
 
     def _sub_(self, other):
+        r"""
+        Returns the difference of ``self`` and ``other`` in Q/nZ
+
+        EXAMPLES::
+            sage: G = QmodnZ(9/10)
+            sage: g = G(4)
+            sage: h = G(1/2)
+            sage: g - h
+            4/5
+            sage: h - g
+            1/10
+            sage: g - h == G(4/5) #indirect doctest
+            True
+            sage: h - g == G(1/10) #indirect doctest
+            True
+        """
+
         QZ = self.parent()
         ans = self._x - other._x
         if ans < 0:
@@ -48,30 +149,108 @@ class QmodnZ_Element(AdditiveGroupElement):
         return QZ.element_class(QZ, ans, True)
 
     def _rmul_(self, c):
+        r"""
+        Returns the (right) scalar product of ``self`` by ``c`` in Q/nZ.
+
+        EXAMPLES::
+            sage: G = QmodnZ(5/7)
+            sage: g = G(13/21)
+            sage: g*6
+            1/7
+        """
         QZ = self.parent()
         return QZ.element_class(QZ, self._x * c)
 
     def _lmul_(self, c):
+        r"""
+        Returns the (left) scalar product of ``self`` by ``c`` in Q/nZ.
+
+        EXAMPLES::
+            sage: G = QmodnZ(5/7)
+            sage: g = G(13/21)
+            sage: 6*g
+            1/7
+
+        TESTS::
+            sage: 6*g == g*6
+            True
+            sage: 6*g == 5*g
+            False
+        """
         return self._rmul_(c)
 
-    def __truediv__(self, other):
+    def __div__(self, other):
+        #TODO: This needs to be implemented.
         QZ = self.parent()
-        other = ZZ(Other)
+        other = ZZ(other)
         return QZ.element_class(QZ, self._x / other, True)
 
     def _repr_(self):
+        r"""
+        Display the element
+
+        EXAMPLES::
+
+            sage: G = QmodnZ(8)
+            sage: g = G(25/7); g;
+            25/7
+
+        """
+
         return repr(self._x)
 
     def __hash__(self):
+        r"""
+        TESTS::
+
+            sage: G = QmodnZ(4)
+            sage: g = G(4/5)
+            sage: hash(g)
+            -7046029254386353128
+            sage: hash(G(3/4))
+            3938850096065010962
+            sage: hash(G(1))
+            1
+        """
+
         return hash(self._x)
 
     def _richcmp_(self, right, op):
+        r"""
+        Compare ``self`` with ``right``.
+
+        OUTPUT:
+
+        boolean
+
+        EXAMPLES::
+
+            sage: G = QQ/(4*ZZ)
+            sage: g = G(4/5)
+            sage: h = G(6/7)
+            sage: g == h
+            False
+            sage: g == g
+            True
+        """
         if op == op_EQ or op == op_NE:
             return richcmp(self._x, right._x, op)
         else:
             return NotImplemented
 
     def additive_order(self):
+        r"""
+        Returns the order of ``self`` in the abelian group Q/nZ.
+
+        EXAMPLES::
+            sage: G = QmodnZ(12)
+            sage: g = G(5/3)
+            sage: g.additive_order()
+            36
+            sage: (-g).additive_order() # indirect doctest
+            36
+        """
+
         # a/b * k = n/m * r
         QZ = self.parent()
         if QZ.n == 0:

ac55d864d5ec5dd8121a89b6771f7abbb5e46397
Martin Rubey <martin.rubey@tuwien.ac.at>
2017-10-01 16:32:17 +0200

    fix mistakes in definition of GrowthDiagram.rotate and GrowthDiagram.conjugate

--- src/sage/combinat/growth.py
+++ src/sage/combinat/growth.py
@@ -705,9 +705,24 @@ class GrowthDiagram(SageObject):
             sage: Gc = G.conjugate()
             sage: (Gc.P_symbol(), Gc.Q_symbol()) == (G.Q_symbol(), G.P_symbol())
             True
+
+        TESTS:
+
+        Check that labels and shape are handled correctly::
+
+            sage: o = [[2,1],[2,2],[3,2],[4,2],[4,1],[4,1,1],[3,1,1],[3,1],[3,2],[3,1],[2,1]]
+            sage: l = [o[i//2] if is_even(i) else min(o[(i-1)//2],o[(i+1)//2])
+            ....:      for i in range(2*len(o)-1)]
+            sage: la = list(range(len(o)-2, 0, -1))
+            sage: G = RuleRSK(labels=l[1:-1], shape=la)
+            sage: G.out_labels() == G.conjugate().out_labels()[::-1]
+            True
         """
         F = {(j,i): v for (i,j),v in self._filling.items()}
-        return GrowthDiagram(self.rule, filling=F)
+        return GrowthDiagram(self.rule,
+                             filling=F,
+                             shape=self.shape().conjugate(),
+                             labels=self.in_labels()[::-1])
 
     def rotate(self):
         r"""
@@ -729,11 +744,35 @@ class GrowthDiagram(SageObject):
             ....:            for t in [G.P_symbol(), G.Q_symbol()]])
             [   1  1  1    1  1  2 ]
             [   2      ,   3       ]
+
+        TESTS:
+
+        Check that shape is handled correctly::
+
+            sage: RuleRSK = GrowthDiagram.rules.RSK()
+            sage: G = GrowthDiagram(RuleRSK,
+            ....:                   filling={(0,2):1, (3,1):2, (2,1):3},
+            ....:                   shape=SkewPartition([[5,5,5,3],[3,1]]))
+            sage: G
+            .  .  .  0  0
+            .  0  3  2  0
+            1  0  0  0  0
+            0  0  0
+            sage: G.rotate()
+            .  .  0  0  0
+            0  0  0  0  1
+            0  2  3  0
+            0  0
         """
-        max_row = max(i for i, _ in self._filling)
-        max_col = max(j for _, j in self._filling)
-        F = {(max_row-i,max_col-j): v for (i,j),v in self._filling.items()}
-        return GrowthDiagram(self.rule, filling=F)
+        l = self._lambda[0]
+        h = len(self._lambda)
+        shape_lambda = [l-p for p in self._mu] + [l]*(h-len(self._mu))
+        shape_mu     = [l-p for p in self._lambda]
+        shape = SkewPartition([shape_lambda[::-1], shape_mu[::-1]])
+        F = {(l-i-1, h-j-1): v for (i,j),v in self._filling.items()}
+        return GrowthDiagram(self.rule,
+                             filling=F,
+                             shape=shape)
 
     def half_perimeter(self):
         r"""

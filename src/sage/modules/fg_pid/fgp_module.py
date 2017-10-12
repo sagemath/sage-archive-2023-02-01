68a45417c632bdce1e277e8a8d91de4a4c49e72a
Travis Scrimshaw <tscrimsh at umn.edu>
2017-10-11 20:18:11 -0500

    Make the functor behave like QuotientFunctor.

--- src/sage/modules/fg_pid/fgp_module.py
+++ src/sage/modules/fg_pid/fgp_module.py
@@ -220,6 +220,8 @@ from sage.rings.all import Integer, ZZ
 from sage.arith.all import lcm
 from sage.misc.cachefunc import cached_method
 from sage.misc.superseded import deprecation
+from sage.categories.modules import Modules
+from sage.categories.pushout import ConstructionFunctor
 
 import sage.misc.weak_dict
 from functools import reduce
@@ -1555,34 +1557,36 @@ class FGP_Module_class(Module):
 
     def construction(self):
         """
-        The construction functor and ambient module for self.
-        
+        The construction functor and ambient module for ``self``.
+
+        EXAMPLES::
+
+            sage: W = ZZ^2
+            sage: A1 = W.submodule([[1,0]])
+            sage: B1 = W.submodule([[2,0]])
+            sage: T1 = A1 / B1
+            sage: T1.construction()
+            (QuotientModuleFunctor,
+              Free module of degree 2 and rank 1 over Integer Ring
+              Echelon basis matrix:
+              [1 0])
+
         TESTS::
-        
-            sage: W=ZZ^2
+
+            sage: W = ZZ^2
             sage: A1 = W.submodule([[1,0]])
             sage: A2 = W.submodule([[0,1]])
             sage: B1 = W.submodule([[2,0]])
             sage: B2 = W.submodule([[0,2]])
-            sage: T1 = A1/B1
-            sage: T2 = A2/B2
-            sage: t1=T1.an_element()
-            sage: t2=T2.an_element()
-            sage: t1+t2
+            sage: T1 = A1 / B1
+            sage: T2 = A2 / B2
+            sage: t1 = T1.an_element()
+            sage: t2 = T2.an_element()
+            sage: t1 + t2
             (1, 1)
         """
-        from sage.categories.pushout import QuotientModuleFunctor
-        #We do not want Vector functors rather subspace functors.
-        #if self.cover().is_ambient():
-        #    cover = self.cover().span(self.cover().gens())
-        #if self.relations().is_ambient():
-        #    rels = self.relations().span(self.relations().gens())
-        Fcover = self.cover().construction()[0]
-        Frels = self.relations().construction()[0]
-        return QuotientModuleFunctor(Fcover,Frels),self.cover().ambient_module()
-        
-            
-            
+        return (QuotientModuleFunctor(self._W), self._V)
+
     def is_finite(self):
         """
         Return True if self is finite and False otherwise.
@@ -1662,6 +1666,164 @@ class FGP_Module_class(Module):
             -7071641102956720018 # 64-bit
         """
         return hash( (self.V(), self.W()) )
+
+##############################################################
+# Construction functor
+##############################################################
+
+class QuotientModuleFunctor(ConstructionFunctor):
+    r"""
+    Construct the quotient of two submodule functors.
+
+    INPUT:
+
+    - ``relations`` -- a module
+
+    .. NOTE::
+
+        This construction functor keeps track of the basis of defining
+        ``relations``. It can only be applied to free modules into which
+        this basis coerces.
+
+    EXAMPLES::
+
+        sage: A = ZZ^2
+        sage: B = 2*ZZ^2
+        sage: Q = A / B
+        sage: F = Q.construction()[0]
+        sage: F
+        QuotientModuleFunctor
+        sage: F(A) == Q
+        True
+        sage: F(B.ambient_module()) == Q
+        True
+
+    We can construct quotients from different ambient spaces::
+
+        sage: F(ZZ^2)
+        Finitely generated module V/W over Integer Ring with invariants (2, 2)
+        sage: F(2*ZZ^2)
+        Finitely generated module V/W over Integer Ring with invariants ()
+
+    This functor is used for constructing pushouts::
+
+        sage: A = ZZ^3
+        sage: x,y,z = A.basis()
+        sage: A1 = A.submodule([x])
+        sage: A2 = A.submodule([y, 2*x])
+        sage: B1 = A.submodule([])
+        sage: B2 = A.submodule([2*x])
+        sage: Q1 = A1 / B1
+        sage: Q2 = A2 / B2
+        sage: q3 = Q1.an_element() + Q2.an_element()
+    """
+    rank = 5 # ranking of functor, not rank of module
+
+    def __init__(self, relations):
+        """
+        Initialization of ``self``.
+
+        TESTS::
+
+            sage: from sage.modules.fg_pid.fgp_module import QuotientModuleFunctor
+            sage: B = 2*ZZ^2
+            sage: F = QuotientModuleFunctor(B)
+            sage: TestSuite(F).run()
+        """
+        R = relations.category().base_ring()
+        ConstructionFunctor.__init__(self, Modules(R), Modules(R))
+        self._relations = relations
+
+    def relations(self):
+        """
+        Return the defining relations of ``self``.
+
+        EXAMPLES::
+
+            sage: A = (ZZ**2) / span([[4,0],[0,3]], ZZ)
+            sage: A.construction()[0].relations()
+            Free module of degree 2 and rank 2 over Integer Ring
+            Echelon basis matrix:
+            [4 0]
+            [0 3]
+        """
+        return self._relations
+
+    def _apply_functor(self, ambient):
+        """
+        Apply the functor to an object of ``self``'s domain.
+
+        TESTS::
+
+            sage: A = ZZ^3
+            sage: B = 2 * A
+            sage: C = 4 * A
+            sage: D = B / C
+            sage: F = D.construction()[0]
+            sage: D == F(D.construction()[1])
+            True
+        """
+        return ambient.quotient(self._relations)
+
+    def __eq__(self, other):
+        """
+        The quotient functor ``self`` is equal to ``other`` if
+        it is a :class:`QuotientModuleFunctor` and the relations
+        subspace are equal.
+
+        EXAMPLES::
+
+            sage: F1 = ((ZZ^3) / (4*ZZ^3)).construction()[0]
+            sage: F2 = ((2*ZZ^3) / (4*ZZ^3)).construction()[0]
+            sage: F1 == F2
+            True
+            sage: F3 = ((ZZ^3) / (8*ZZ^3)).construction()[0]
+            sage: F1 == F3
+            False
+        """
+        if not isinstance(other, QuotientModuleFunctor):
+            return False
+        return self._relations == other._relations
+
+    def __ne__(self, other):
+        r"""
+        Check whether ``self`` is not equal to ``other``.
+
+        EXAMPLES::
+
+            sage: F1 = ((ZZ^3) / (4*ZZ^3)).construction()[0]
+            sage: F2 = ((2*ZZ^3) / (4*ZZ^3)).construction()[0]
+            sage: F1 != F2
+            False
+            sage: F3 = ((ZZ^3) / (8*ZZ^3)).construction()[0]
+            sage: F1 != F3
+            True
+        """
+        return not (self == other)
+
+    def merge(self, other):
+        r"""
+        Merge the construction functors ``self`` and ``other``.
+
+        EXAMPLES::
+
+            sage: A = ZZ^3
+            sage: x,y,z = A.basis()
+            sage: A1 = A.submodule([x])
+            sage: A2 = A.submodule([y, 2*x])
+            sage: B1 = A.submodule([])
+            sage: B2 = A.submodule([2*x])
+            sage: Q1 = A1 / B1
+            sage: Q2 = A2 / B2
+            sage: F1 = Q1.construction()[0]
+            sage: F2 = Q2.construction()[0]
+            sage: F3 = F1.merge(F2)
+            sage: q3 = Q1.an_element() + Q2.an_element()
+            sage: q3.parent() == F3(A1 + A2)
+            True
+        """
+        if isinstance(other, QuotientModuleFunctor):
+            return QuotientModuleFunctor(self._relations + other._relations)
 
 ##############################################################
 # Useful for testing

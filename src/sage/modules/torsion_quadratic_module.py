5785ec7196cbe96e51823535d8eb88e6ba86ad94
Simon Brandhorst <sbrandhorst@web.de>
2017-10-17 10:29:36 +0200

    diagonalization for padic matrices

--- src/sage/modules/torsion_quadratic_module.py
+++ src/sage/modules/torsion_quadratic_module.py
@@ -476,3 +476,295 @@ class TorsionQuadraticModule(FGP_Module_class):
             Q/2Z
         """
         return QmodnZ(self._modulus_qf)
+    
+    def gens_orthogonalize(self):
+        """
+        Return a free quadratic module such that its generators are mutually orthogonal for `p != 2` or have block size at most `2`.
+        """
+        gens = []
+        for p in self.annihilator().support():
+            Qp = self.primary_part(p)
+            gens_p = _diagonalize(Qp)
+            gens += gens_p
+        return gens
+    
+
+def diagonalize_p_adic(G,prime,precision=None):
+    """
+    Diagonalize a symmetric matrix over a p-adic field 
+    
+    Input:
+    
+        -- ``G`` - symmetric n x n matrix in `\QQ`
+        -- ``p`` - prime number
+        -- ``precision`` - defining precision if not set the minimal possible is taken.
+    Output:
+    
+        - ``D`` the diagonalized matrix
+        - ``U`` transformation matrix, i.e, D = U * G * U^T
+    
+    EXAMPLES::
+    
+        sage: 
+    """
+    from sage.rings.all import Zp
+    from sage.matrix.constructor import Matrix
+    #this calculation is local. Thus we can do it over the p-adics
+    p = prime
+    denom = G.denominator()
+    G = G*denom
+    if precision == None:
+        precision = G.det().valuation(p) + 1
+    R = Zp(p, prec = precision, type = 'fixed-mod', print_mode = 'digits')
+    G = G.change_ring(R)
+    D = deepcopy(G)
+    n = G.ncols()
+    
+    #indices of the diagonal entrys which are already used
+    cnt = 0
+    
+    #transformation matrix
+    U = Matrix.identity(R,n)
+    
+    if(p == 2):
+        D, U = _diagonalize_2_adic(G,D)
+    else:
+        while cnt < n:
+            pivot = _find_min_p(D,cnt)
+            piv1 = pivot[1]
+            piv2 = pivot[2]
+            minval = pivot[0]
+            assert D==D.T
+            #the smallest valuation is on the diagonal
+            if piv1 == piv2:
+                #move pivot to position [cnt,cnt]
+                if piv1 != cnt:
+                    U.swap_rows(cnt,piv1)
+                    D.swap_rows(cnt,piv1)
+                    D.swap_columns(cnt,piv1)
+                #we are already orthogonal to the part with i < cnt
+                #now make the rest orthogonal too
+                for i in range(cnt+1,n):
+                    if D[i,cnt]!= 0:
+                        c = D[i,cnt]//D[cnt,cnt]
+                        U[i,:] += -c * U[cnt,:]
+                        D[i,:] += -c * D[cnt,:]
+                        D[:,i] += -c * D[:,cnt]
+                cnt = cnt + 1
+            #the smallest valuation is off the diagonal
+            else:
+                #after this the diagonal entry will have smallest valuation.
+                row = pivot[1]
+                col = pivot[2]
+                U[row,:] += U[col,:]
+                D[row,:] += D[col,:]
+                D[:,row] += D[:,col]
+        assert D == Matrix.diagonal(D.diagonal())
+    assert U.determinant().valuation()==0
+    return D, U
+
+def _diagonalize_2_adic(G,D):
+    """
+    Diagonalize a symmetric matrix over the 2-adics
+    
+    (This method is called implicitly in diagonalize_p_adic(G,prime) )
+    
+    Input:
+
+        - ``G`` symmetric nxn matrix in `\Qp`
+    
+    Output:
+
+        - ``D`` the diagonalized matrix
+        - ``U`` transformation matrix i.e D = U * G * U^T
+    
+    Example:
+
+        sage: G = matrix(QQ,3,3,[1,0,1,0,2,1,1,1,3])
+        sage: G
+
+        [1 0 1]
+        [0 2 1]
+        [1 1 3]
+        sage: diagonalize_p_adic(G,2)
+
+        (
+        [1 0 0]  [ 1  0  0]
+        [0 2 0]  [ 0  1  0]
+        [0 0 6], [-2 -1  2]
+        )
+        
+        
+    Algorithm:
+
+        Based on the Algorithm in 'Sphere packings,lattices and groups'
+        by J.H Conway & N.J.A Sloane
+    
+    """
+    n = D.ncols()
+    R = D.base_ring()
+    #lists indices of the diagonal entrys which are already used
+    cnt = 0
+    
+    #transformation matrix
+    U = Matrix.identity(R,n)
+    
+    while cnt < n:
+            pivot = _find_min_p(D,cnt)
+            piv1 = pivot[1]
+            piv2 = pivot[2]
+            minval = pivot[0]
+            assert D==D.T
+            #the smallest valuation is on the diagonal
+            if piv1 == piv2:
+                #move pivot to position [cnt,cnt]
+                if piv1 != cnt:
+                    U.swap_rows(cnt,piv1)
+                    D.swap_rows(cnt,piv1)
+                    D.swap_columns(cnt,piv1)
+                #we are already orthogonal to the part with i < cnt
+                #now make the rest orthogonal too
+                for i in range(cnt+1,n):
+                    if D[i,cnt]!= 0:
+                        c = D[i,cnt]//D[cnt,cnt]
+                        U[i,:] += -c * U[cnt,:]
+                        D[i,:] += -c * D[cnt,:]
+                        D[:,i] += -c * D[:,cnt]
+                cnt = cnt + 1
+            #the smallest valuation is off the diagonal
+            else:
+                #move this 2 x 2 block to the top left (starting from cnt)
+                U.swap_rows(cnt,piv1)
+                D.swap_rows(cnt,piv1)
+                D.swap_columns(cnt,piv1)
+            
+                U.swap_rows(cnt+1,piv2)
+                D.swap_rows(cnt+1,piv2)
+                D.swap_columns(cnt+1,piv2)
+                #we split off a 2 x 2 block
+                if cnt != n-2: 
+                #else this is the last 2 x 2 block so there is nothing to do.
+                    content = R(2**minval)
+                    eqn_mat = D[cnt:cnt+2,cnt:cnt+2].list()
+                    eqn_mat = Matrix(R,2,2,[e//content for e in eqn_mat]) #do you know better syntax here?
+                    #calculate the inverse without using division
+                    inv = eqn_mat.adjoint() * eqn_mat.det().inverse_of_unit()
+                    B = U[cnt:cnt+2,:]
+                    C = D[cnt+2:,cnt:cnt+2]*inv
+                    U[cnt+2:,:] -= C*B
+                    D[cnt:,cnt:] = U[cnt:,:] * G * U[cnt:,:].transpose()
+                    assert D==U*G*U.T
+                #if the previous diagonal entry has the same valuation
+                #as the pivot we can split off a 1 x 1 matrix
+                if cnt > 0 and D[cnt-1,cnt-1].valuation() == minval:
+                    #a 3 x 3 block
+                    # a  0  0
+                    # 0 2b  c
+                    # 0  c 2d
+                    a = D[cnt-1,cnt-1]
+                    c = D[cnt,cnt+1]
+                    B = Matrix(R,2,3,[
+                                    1,1, 0,
+                                    c,0,-a
+                                    ])
+                    U[cnt-1:cnt+1,:] = B * U[cnt-1:cnt+2,:]
+                    D = U * G * U.transpose()
+                    #at this stage cnt-1 and cnt are orthogonal and both have minimal valuation
+                    cnt -= 3
+                cnt += 2
+    return D, U
+
+def _normalize_blocks(G):
+    R = G.base_ring()
+    D = copy(G)
+    p = R.prime()
+    n = G.ncols()
+    U = copy(G.parent().identity_matrix())
+    if p != 2:
+        #squareclasses 1,v
+        v = _min_nonsquare(p)
+        v = R(v)
+        for i in range(n):
+            d = D[i,i].unit_part()
+            if d.is_square():
+                D[i,i] = 1
+                U[i,:] *= d.inverse_of_unit().sqrt()
+            else:
+                D[i,i] = v
+                U[i,:] *= (v*d.inverse_of_unit()).sqrt()
+    else:
+        #squareclasses 1,3,5,7 modulo 8
+        for i in range(n):
+            d = D[i,i].unit_part()
+            if d != 0:
+                v = R(d.mod(8))
+                D[i,i] = v
+                U[i,:] *= (v*d.inverse_of_unit()).sqrt()
+        for i in range(n-1):
+            b = D[i,i+1]
+            if b !=0: #there is a 2 x 2 block here
+                
+                
+    return D, U
+
+
+def _find_min_p(G,cnt):
+    """
+    Find the smallest valuation and prefer diagonal entries.
+    
+    Input:
+
+        -- ``G`` - symmetric n x n matrix in `\Qp`
+        -- ``cnt`` - start search from this index
+    
+    Output:
+
+        - ``min`` minimal valuation
+        - ``min_i`` row of the minimal valuation
+        - ``min_j`` column of the minimal valuation
+    
+    Example:
+
+        sage: G = matrix(Qp(3),3,3,[4,0,1,0,4,2,1,2,1])
+        sage: G
+        [4 0 1]
+        [0 4 2]
+        [1 2 5]
+        sage: find_min_p(G,2,0)
+        (0,3,3)
+    
+    """
+    n = G.ncols()
+    min = G[cnt,cnt].valuation()
+    min_i = cnt
+    min_j = cnt
+    for i in range(cnt,n):
+        for j in range(i,n):
+            if (G[i,j]!=0) and (G[i,j].valuation() < min):
+                min_i = i
+                min_j = j
+                min = G[i,j].valuation()
+        #diagonal has precedence
+        if (G[i,i]!=0) and (G[i,i].valuation() <= min):
+            min_i = i
+            min_j = i
+            min = G[i,i].valuation()
+    return min, min_i, min_j
+
+def _min_nonsquare(prime):
+    """
+    Calculates minimal nonsquare in `\FF_p`
+    
+    Input:
+
+        - ``p`` prime number for `\Qp`
+    
+    Output:
+    
+        - ``a`` minimal nonsquare
+
+    """
+    R = sage.rings.all.GF(prime)
+    for i in range(2,prime):
+        if R(i).is_square() == False:
+            return i

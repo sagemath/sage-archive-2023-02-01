ac925246354752b6f27e6643efd2a7c6fccd0cd5
Jori Mäntysalo <jori.mantysalo@uta.fi>
2017-10-31 10:37:30 +0200

    Docstring formatting.

--- src/sage/graphs/bliss.pyx
+++ src/sage/graphs/bliss.pyx
@@ -224,7 +224,7 @@ cdef void empty_hook(void *user_param , unsigned int n, const unsigned int *aut)
 
 def canonical_form(G, partition=None, return_graph=False, certificate=False):
     """
-    Return a canonical label of ``G``
+    Return the canonical label of ``G``.
 
     A canonical label ``canonical_form(G)`` of ``G`` is a (di)graph defined on
     `\{0,...,n-1\}` such that ``G`` is isomorphic to ``H`` if and only if
@@ -235,10 +235,10 @@ def canonical_form(G, partition=None, return_graph=False, certificate=False):
     - ``G`` -- A graph or digraph.
 
     - ``partition`` -- A partition of the vertices of ``G`` into color classes.
-        Defaults to ``None``.
+      Defaults to ``None``.
 
     - ``return_graph`` -- If set to ``True``, ``canonical_form`` returns the
-        canonical graph of G. Otherwise, it returns its set of edges.
+      canonical graph of G. Otherwise, it returns its set of edges.
 
     - ``certificate`` -- If set to ``True`` returns the labeling of G into a
       canonical graph.

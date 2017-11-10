sagemath/sagetrac-mirror

Deprecate sage.matrix.matrix

src/sage/matrix/matrix.pyx
@@ -13,24 +13,17 @@ For design documentation see matrix/docs.py.
 #                  http://www.gnu.org/licenses/
 ################################################################################
 
-from sage.ext.stdsage cimport PY_SET_TP_NEW
+from sage.misc.superseded import deprecation
+deprecation(24096, "the module sage.modules.module_element is deprecated, import from sage.structure.element instead")
 
 def is_Matrix(x):
     """
     EXAMPLES::
 
-        sage: from sage.matrix.matrix import is_Matrix
+        sage: from sage.structure.element import is_Matrix
         sage: is_Matrix(0)
         False
         sage: is_Matrix(matrix([[1,2],[3,4]]))
         True
     """
     return isinstance(x, Matrix)
-
-cdef class Matrix(Matrix2):
-    pass
-
-# This is pretty nasty low level stuff. The idea is to speed up construction
-# of EuclideanDomainElements (in particular Integers) by skipping some tp_new
-# calls up the inheritance tree.
-PY_SET_TP_NEW(Matrix, Matrix2)

sagemath/sage-archive-2023-02-01

src/sage/matrix/matrix.pyx
@@ -13,24 +13,19 @@ For design documentation see matrix/docs.py.
 #                  http://www.gnu.org/licenses/
 ################################################################################
 
-from sage.ext.stdsage cimport PY_SET_TP_NEW
+from sage.misc.superseded import deprecation
+deprecation(24096, "the module sage.matrix.matrix is deprecated, import from sage.structure.element instead")
 
 def is_Matrix(x):
     """
     EXAMPLES::
 
         sage: from sage.matrix.matrix import is_Matrix
+        doctest:...: DeprecationWarning: the module sage.matrix.matrix is deprecated, import from sage.structure.element instead
+        See http://trac.sagemath.org/24096 for details.
         sage: is_Matrix(0)
         False
         sage: is_Matrix(matrix([[1,2],[3,4]]))
         True
     """
     return isinstance(x, Matrix)
-
-cdef class Matrix(Matrix2):
-    pass
-
-# This is pretty nasty low level stuff. The idea is to speed up construction
-# of EuclideanDomainElements (in particular Integers) by skipping some tp_new
-# calls up the inheritance tree.
-PY_SET_TP_NEW(Matrix, Matrix2)
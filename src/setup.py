7eb64232c374aafa2edd33d28af757a9079164d0
Erik M. Bray <erik.bray@lri.fr>
2017-11-10 18:53:00 +0100

    Fixes https://trac.sagemath.org/ticket/24192
    These settings should work for now on Cygwin.  I will work later on bringing it up to
    standard c99 compatibility bit by bit (this requires some upstream fixes to newlib too,
    ideally.

--- src/setup.py
+++ src/setup.py
@@ -432,10 +432,16 @@ class sage_build_cython(Command):
                 have_std_flag = True
             cflags.append(flag)
         if not have_std_flag:  # See Trac #23919
-            if cplusplus:
-                cflags.append("-std=c++11")
+            if sys.platform == 'cygwin':
+                if cplusplus:
+                    cflags.append("-std=gnu++11")
+                else:
+                    cflags.append("-std=gnu99")
             else:
-                cflags.append("-std=c99")
+                if cplusplus:
+                    cflags.append("-std=c++11")
+                else:
+                    cflags.append("-std=c99")
         cflags = extra_compile_args + cflags
         kwds['extra_compile_args'] = stable_uniq(cflags)
 

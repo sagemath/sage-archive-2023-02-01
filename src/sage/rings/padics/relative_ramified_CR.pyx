635021af851aac33b40cba44301e5339337023cd
David Roe <roed.math@gmail.com>
2017-11-15 23:23:49 +0000

    Change _poly_rep to always return the polynomial representing the element, not the unit

--- src/sage/rings/padics/relative_ramified_CR.pyx
+++ src/sage/rings/padics/relative_ramified_CR.pyx
@@ -3,4 +3,4 @@ include "CR_template.pxi"
 
 cdef class RelativeRamifiedCappedRelativeElement(CRElement):
     def _poly_rep(self):
-        return self.unit
+        return self.unit.parent()(ccoefficients(self.unit, self.ordp, self.relprec, self.prime_pow))
--- src/sage/rings/padics/relative_ramified_FP.pyx
+++ src/sage/rings/padics/relative_ramified_FP.pyx
@@ -3,4 +3,4 @@ include "FP_template.pxi"
 
 cdef class RelativeRamifiedFloatingPointElement(FPElement):
     def _poly_rep(self):
-        return self.unit
+        return self.unit.parent()(ccoefficients(self.unit, self.ordp, self.prime_pow.ram_prec_cap, self.prime_pow))

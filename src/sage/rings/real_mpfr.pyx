ec67c243b6947ef72514352d582eee0533859333
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2018-03-08 20:44:46 +0100

    get rid of __cmp__ in real_mpfr.pyx

--- src/sage/rings/real_mpfr.pyx
+++ src/sage/rings/real_mpfr.pyx
@@ -135,7 +135,7 @@ from sage.cpython.string cimport char_to_str
 from sage.misc.superseded import deprecation
 
 from sage.structure.element cimport RingElement, Element, ModuleElement
-from sage.structure.richcmp cimport rich_to_bool_sgn
+from sage.structure.richcmp cimport rich_to_bool_sgn, rich_to_bool
 cdef bin_op
 from sage.structure.element import bin_op
 
@@ -737,7 +737,7 @@ cdef class RealField_class(sage.rings.ring.Field):
             return self.convert_method_map(S, "_mpfr_")
         return self._coerce_map_via([RLF], S)
 
-    def __cmp__(self, other):
+    def __richcmp__(self, other, int op):
         """
         Compare two real fields, returning ``True`` if they are equivalent
         and ``False`` if they are not.
@@ -768,14 +768,15 @@ cdef class RealField_class(sage.rings.ring.Field):
             sage: RR == RS
             True
         """
-        if not isinstance(other, RealField_class):
-            return -1
+        cdef RealField_class _self = self
         cdef RealField_class _other
+        if not isinstance(other, RealField_class):
+            return rich_to_bool(op, -1)
+
         _other = other  # to access C structure
-        if self.__prec == _other.__prec and self.rnd == _other.rnd: \
-               #and self.sci_not == _other.sci_not:
-            return 0
-        return 1
+        if _self.__prec == _other.__prec and _self.rnd == _other.rnd:
+            return rich_to_bool(op, 0)
+        return rich_to_bool(op, 1)
 
     def __reduce__(self):
         """

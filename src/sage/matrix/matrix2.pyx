37b60090df7fa5a2d47c8bea0c2dcc4346cfa0da
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2018-03-13 21:13:04 +0100

    remove unused cmp_pivots from matrix2.pyx

--- src/sage/matrix/matrix2.pyx
+++ src/sage/matrix/matrix2.pyx
@@ -14974,30 +14974,6 @@ def decomp_seq(v):
     list.sort(v, key=lambda x: x[0].dimension())
     return Sequence(v, universe=tuple, check=False, cr=True)
 
-def cmp_pivots(x,y):
-    """
-    Compare two sequences of pivot columns.
-
-    - If x is shorter than y, return -1, i.e., x < y, "not as good".
-
-    - If x is longer than y, x > y, "better".
-
-    - If the length is the same then x is better, i.e., x > y if the
-      entries of x are correspondingly >= those of y with one being
-      greater.
-    """
-    x = tuple(x)
-    y = tuple(y)
-    if len(x) < len(y):
-        return -1
-    if len(x) > len(y):
-        return 1
-    if x < y:
-        return 1
-    elif x == y:
-        return 0
-    else:
-        return -1
 
 def _choose(Py_ssize_t n, Py_ssize_t t):
     """

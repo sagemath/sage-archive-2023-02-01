ffe58a9894001159136e891bde96b7ea36e467fb
SaiHarsh <saiharsh.t13@iiits.in>
2018-03-19 20:54:59 +0530

    Removed ear_decomposition.py Added ear_decomposition method to graph.py

--- src/sage/graphs/graph.py
+++ src/sage/graphs/graph.py
@@ -6732,6 +6732,170 @@ class Graph(GenericGraph):
             cover_g.sort()
             return cover_g
 
+    def ear_decomposition(self):
+
+      r"""
+      This module implements the function for computing the Ear Decomposition
+      of undirected connected graphs.
+
+      Input:     
+
+      - ``graph`` -- The undirected connected graph for which ear decomposition needs to be computed
+
+      Output:
+
+      - A nested list representing the cycle and chains of input graph G.
+
+      Example:
+        
+        sage: g = Graph([[0, 1],[0, 7],[0, 3],[0, 11],[2, 3],[1, 2],[1, 12],[2, 12],[3, 4],[3, 6],[4, 5],[4, 7],[4, 6],[5, 6],[7, 10],[7, 8],[7, 11],[8, 9],[8, 10],[8, 11],[9, 11]])
+        sage: g.ear_decomposition()
+        [[0, 11, 7, 4, 3, 2, 1, 0],
+         [0, 3],
+         [0, 7],
+         [1, 12, 2],
+         [3, 6, 4],
+         [4, 5, 6],
+         [7, 10, 8, 11],
+         [7, 8],
+         [11, 9, 8]]
+
+        
+        If input is one edge
+        sage: g = Graph([[1,2]])
+        sage: g.ear_decomposition()
+        ValueError: Please input a undirected connected graph with number of vertices > 2
+
+        
+
+        If input graph is not connected
+        sage: g = Graph([[1,2], [3,4]])
+        sage: g.ear_decomposition()
+        ValueError: Graph must be undirected connected
+
+        
+
+        sage: G.allow_loops(True)
+        sage: G.allow_multiple_edges(True)
+        sage: for i in range(20):
+        ....: u = randint(1, 7)
+        ....: v = randint(1, 7)
+        ....: G.add_edge(u, v)
+        sage: G
+        Looped multi-graph on 7 vertices
+        sage: H = copy(G)
+        sage: H
+        Looped multi-graph on 7 vertices
+        sage: H = copy(G)
+        sage: H.allow_loops(False)
+        sage: H.allow_multiple_edges(False)
+        sage: H
+        Graph on 7 vertices
+        sage: H.ear_decomposition() == G.ear_decomposition()
+        [[1, 3, 2, 1], [1, 6, 5, 4, 3], [1, 7, 6], [2, 5], [2, 7], [3, 7], [4, 7], [5, 7]]
+        [[1, 3, 2, 1], [1, 6, 5, 4, 3], [1, 7, 6], [2, 5], [2, 7], [3, 7], [4, 7], [5, 7]]
+        True
+        
+        
+        sage: g = Graph([['0', '1'],['0', '7'],['0', '3'],['0', '11'],['2', '3'],['1', '2'],['1', '12'],['2', '12'],['3', '4'],['3', '6'],['4', '5'],['
+        ....: 4', '7'],['4', '6'],['5', '6'],['7', '10'],['7', '8'],['7', '11'],['8', '9'],['8', '10'],['8', '11'],['9', '11']])
+        sage: g.ear_decomposition()
+        [['11', '7', '4', '3', '2', '1', '0', '11'], ['11', '8', '10', '7'], ['11', '9', '8'], ['0', '7'], ['0', '3'], ['1', '12', '2'], ['3', '6', '4'], ['4', '5', '6'], ['7', '8']]
+
+
+      """
+
+      """
+      graph                   : Input graph
+      time_of_visit           : List to store the order in which dfs visits vertices.
+      visited_vertice         : Boolean dict to mark vertices as visited or unvisited during Dfs traversal in graph.
+      traverse_visited_vertice: Boolean dict to mark vertices as visited or unvisited in Dfs tree traversal.
+      parent                  : Dict to store parent vertex of all the visited vertices.
+      value                   : List to store visit_time of vertices in Dfs traversal.
+      chains                  : List to store all the chains and cycles of the input graph G.
+      DFS()                   : Function that performs depth first search on input graph G.
+      """
+      graph = self
+      time_of_visit = []
+      visited_vertice = {}
+      traverse_visited_vertice = {}
+      parent = {}
+      value = {}
+      count = [0]
+      chains = []
+
+      n = graph.num_verts()
+        
+      if graph.is_directed():
+        raise ValueError("Graph must be undirected")
+
+      if not graph.is_connected():
+        raise ValueError("Graph must be undirected connected")
+      
+      if(n<3):
+        raise ValueError("Please input a undirected connected graph with number of vertices > 2")
+
+
+      vertices = graph.get_vertices().keys()
+      for i in vertices:
+        visited_vertice[i] = False
+        traverse_visited_vertice[i] = False
+      
+
+      parent[vertices[0]] = -1
+
+      def DFS(v):
+        """
+          v = the current vertex need to expand.
+        """
+        #make v are visited, update it's time of visited and value
+        visited_vertice[v] = True
+        time_of_visit.append(v)
+        value[v] = count[0]
+        count[0] += 1
+        
+        #Traverse though all the neighbor vertices of v
+        for neighbor in graph.neighbors(v):
+          #if any neighbor is not visited, enter
+          if(not visited_vertice[neighbor]):
+            #Upated neighbor's parent as v and expand neighbor
+            parent[neighbor] = v
+            DFS(neighbor)
+
+      def traverse(start, end):
+        #Make the firt end of non-tree edge visited
+        traverse_visited_vertice[start] = True
+        pointer = end
+        chain = []
+        chain.append(start)
+
+        #Traverse DFS Tree of G and print all the not visited vertices.
+        #Appending all the vertices in chain
+        while True:
+          if(traverse_visited_vertice[pointer]):
+            chain.append(pointer)
+            break
+          if(pointer==start):
+            chain.append(pointer)
+            break
+          chain.append(pointer)
+          traverse_visited_vertice[pointer] = True
+          pointer = parent[pointer]
+        chains.append(chain)
+
+
+      
+      #start the depth first search from first vertex
+      DFS(vertices[0])
+
+      #Traverse all the non Tree edges, according to depth first traversal
+      for i in range(n):
+        for neighbor in graph.neighbors(time_of_visit[i]):
+          if(value[time_of_visit[i]] < value[neighbor] and time_of_visit[i] != parent[neighbor]):
+            traverse(time_of_visit[i],neighbor)
+
+      return chains
+
     @doc_index("Clique-related methods")
     def cliques_vertex_clique_number(self, algorithm="cliquer", vertices=None,
                                      cliques=None):

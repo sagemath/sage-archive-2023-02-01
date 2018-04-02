183ddd40b2ebd9f53d242a112325bda119b8298c
Julian Rüth <julian.rueth@fsfe.org>
2018-04-03 01:42:11 +0300

    Make jupyter invocation easier to remember

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-exec sage -sh -c "$*"
+if [ x"$1" = jupyter ]; then
+    # If "jupyter" is given as a first argument, we start a jupyter notebook
+    # with reasonable default parameters for running it inside a container.
+    shift
+    exec sage -n jupyter --no-browser --ip='*' --port=8888 "$@"
+else
+    exec sage -sh -c "$*"
+fi

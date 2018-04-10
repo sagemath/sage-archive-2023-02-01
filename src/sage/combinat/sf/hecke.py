e6109d7310be7c034f32abc78230a662c73735a0
zabrocki <zabrocki@mathstat.yorku.ca>
2018-04-10 13:53:30 -0500

    delete Schur <-> qbar conversion and use only the power sum

--- src/sage/combinat/sf/hecke.py
+++ src/sage/combinat/sf/hecke.py
@@ -114,22 +114,37 @@ class HeckeCharacter(SymmetricFunctionAlgebra_multiplicative):
             sage: Sym = SymmetricFunctions(QQ)
             sage: qbar = Sym.qbar(q=2)
             sage: TestSuite(qbar).run()
+
+            sage: qbar = Sym.qbar(q=2)
+            sage: s = Sym.schur()
+            sage: qbar(s[2,1])
+            2/7*qbar[1, 1, 1] + 1/7*qbar[2, 1] - 1/7*qbar[3]
+            sage: s(qbar[2,1])
+            -s[1, 1, 1] + s[2, 1] + 2*s[3]
+
+            sage: qbar = SymmetricFunctions(QQ['q'].fraction_field()).qbar()
+            sage: s = qbar.symmetric_function_ring().s()
+            sage: s(qbar([2]))
+            -s[1, 1] + q*s[2]
+            sage: s(qbar([4]))
+            -s[1, 1, 1, 1] + q*s[2, 1, 1] - q^2*s[3, 1] + q^3*s[4]
+            sage: s(qbar[2,1])
+            -s[1, 1, 1] + (q-1)*s[2, 1] + q*s[3]
+            sage: qbar(s[1,1])
+            (q/(q+1))*qbar[1, 1] - (1/(q+1))*qbar[2]
+            sage: qbar(s[2])
+            (1/(q+1))*qbar[1, 1] + (1/(q+1))*qbar[2]
         """
         self.q = sym.base_ring()(q)
         SymmetricFunctionAlgebra_multiplicative.__init__(self, sym,
             basis_name="Hecke character with q={}".format(self.q),
             prefix="qbar")
-        self._s = sym.schur()
         self._p = sym.power()
-        self._self_to_s_cache = {}
-        self._s_to_self_cache = {}
 
         # temporary until Hom(GradedHopfAlgebrasWithBasis work better)
         category = ModulesWithBasis(self._sym.base_ring())
         self   .register_coercion(self._p._module_morphism(self._p_to_self_on_basis, codomain = self))
         self._p.register_coercion(self._module_morphism(self._self_to_p_on_basis, codomain = self._p))
-        self   .register_coercion(SetMorphism(Hom(self._s, self, category), self._s_to_self))
-        self._s.register_coercion(SetMorphism(Hom(self, self._s, category), self._self_to_s))
 
     def _p_to_self_on_generator(self, n):
         r"""
@@ -143,16 +158,16 @@ class HeckeCharacter(SymmetricFunctionAlgebra_multiplicative):
 
             sage: qbar = SymmetricFunctions(QQ['q'].fraction_field()).qbar('q')
             sage: qbar._p_to_self_on_generator(3)
-            ((q^2-2*q+1)/(q^2+q+1))*qbar[1, 1, 1] + ((-q-1)/(q^2+q+1))*qbar[2, 1] + (3/(q^3-1))*qbar[3]
+            ((q^2-2*q+1)/(q^2+q+1))*qbar[1, 1, 1] + ((-3*q+3)/(q^2+q+1))*qbar[2, 1] + (3/(q^2+q+1))*qbar[3]
             sage: qbar = SymmetricFunctions(QQ).qbar(-1)
             sage: qbar._p_to_self_on_generator(3)
-            2*qbar[2, 1] - 3/2*qbar[3]
+            2*qbar[2, 1] + 3*qbar[3]
         """
         if n==1:
             return self([1])
         q=self.q
         if q**n==self.base_ring().one():
-            raise ValueError("The parameter q=%s must not be a root of unity"%(q))
+            raise ValueError("The parameter q=%s must not be a %s root of unity"%(q,n))
         out=n*self([n]) - sum((q**i-1)*self._p_to_self_on_generator(i)*\
             self([n-i]) for i in range(1,n) if q**i!=1)
         return out*(q-1)/(q**n-1)
@@ -160,145 +175,66 @@ class HeckeCharacter(SymmetricFunctionAlgebra_multiplicative):
     def _p_to_self_on_basis(self, mu):
         r"""
         Convert `p_\mu` to ``self``
+
         INPUT:
+
         - ``mu`` -- a partition or a list of non-negative integers
+
         EXAMPLES::
+
             sage: qbar = SymmetricFunctions(QQ['q'].fraction_field()).qbar('q')
             sage: qbar._p_to_self_on_basis([3,1])
+            ((q^2-2*q+1)/(q^2+q+1))*qbar[1, 1, 1, 1] + ((-3*q+3)/(q^2+q+1))*qbar[2, 1, 1] + (3/(q^2+q+1))*qbar[3, 1]
+            sage: qbar = SymmetricFunctions(QQ).qbar(2)
+            sage: qbar._p_to_self_on_basis([3,1])
+            1/7*qbar[1, 1, 1, 1] - 3/7*qbar[2, 1, 1] + 3/7*qbar[3, 1]
         """
         return self.prod(self._p_to_self_on_generator(p) for p in mu)
 
     def _self_to_p_on_generator(self, n):
         r"""
+        Convert a generator of the basis to the power sum basis
+
+        INPUT:
+
+        - ``n`` -- a non-negative integer
+
+        EXAMPLES::
+
+            sage: qbar = SymmetricFunctions(QQ['q'].fraction_field()).qbar('q')
+            sage: qbar._self_to_p_on_generator(3)
+            (1/6*q^2-1/3*q+1/6)*p[1, 1, 1] + (1/2*q^2-1/2)*p[2, 1] + (1/3*q^2+1/3*q+1/3)*p[3]
+            sage: qbar = SymmetricFunctions(QQ).qbar(-1)
+            sage: qbar._self_to_p_on_generator(3)
+            2/3*p[1, 1, 1] + 1/3*p[3]
         """
         if n==1:
             return self._p([1])
         q=self.q
         BR = self.base_ring()
-        return q**(n-1)*self._p.sum(sum(q**(-i) for i in range(mu[0]))*BR.prod((1-q**(-p))\
-            for p in mu[1:])*self._p(mu)/mu.centralizer_size() for mu in
+        return q**(n-1)*self._p.sum(sum(q**(-i) for i in range(mu[0]))\
+            *BR.prod((1-q**(-p))for p in mu[1:])\
+            *self._p(mu)/mu.centralizer_size() for mu in
             Partitions(n) if not any(q**p==1 for p in mu[1:]))
 
     def _self_to_p_on_basis(self, mu):
         r"""
+        Convert a basis element indexed by the partition `mu` to the power basis
+
+        INPUT:
+
+        - ``mu`` -- a partition or a list of non-negative integers
+
+        EXAMPLES::
+
+            sage: qbar = SymmetricFunctions(QQ['q'].fraction_field()).qbar('q')
+            sage: qbar._self_to_p_on_basis([3,1])
+            (1/6*q^2-1/3*q+1/6)*p[1, 1, 1, 1] + (1/2*q^2-1/2)*p[2, 1, 1] + (1/3*q^2+1/3*q+1/3)*p[3, 1]
+            sage: qbar = SymmetricFunctions(QQ).qbar(-1)
+            sage: qbar._self_to_p_on_basis([3,1])
+            2/3*p[1, 1, 1, 1] + 1/3*p[3, 1]
         """
         return self._p.prod(self._self_to_p_on_generator(p) for p in mu)
-
-    def _s_to_self(self, x):
-        r"""
-        Isomorphism from the Schur basis into ``self``
-
-        INPUT:
-
-        - ``self`` -- a Hall-Littlewood symmetric function basis
-        - ``x`` -- an element of the Schur basis
-
-        OUTPUT:
-
-        - an element of ``self`` equivalent to ``x``
-
-        EXAMPLES::
-
-            sage: Sym = SymmetricFunctions(QQ)
-            sage: qbar = Sym.qbar(q=2)
-            sage: s = Sym.schur()
-            sage: qbar._s_to_self(s[2,1])
-            2/7*qbar[1, 1, 1] + 1/7*qbar[2, 1] - 1/7*qbar[3]
-
-        This is for internal use only. Instead use::
-
-            sage: qbar(s[2,1])
-            2/7*qbar[1, 1, 1] + 1/7*qbar[2, 1] - 1/7*qbar[3]
-        """
-        return self._from_cache(x, self._s_cache, self._s_to_self_cache, q=self.q)
-
-    def _self_to_s(self, x):
-        r"""
-        Isomorphism from ``self`` to the Schur basis
-
-        INPUT:
-
-        - ``self`` -- a Hall-Littlewood symmetric function basis
-        - ``x`` -- an element of the basis ``self``
-
-        OUTPUT:
-
-        - an element of the Schur basis equivalent to ``x``
-
-        EXAMPLES::
-
-            sage: Sym = SymmetricFunctions(QQ)
-            sage: qbar = Sym.qbar(q=2)
-            sage: s = Sym.schur()
-            sage: qbar._self_to_s(qbar[2,1])
-            -s[1, 1, 1] + s[2, 1] + 2*s[3]
-
-        This is for internal use only. Instead use::
-
-            sage: s(qbar[2,1])
-            -s[1, 1, 1] + s[2, 1] + 2*s[3]
-        """
-        return self._s._from_cache(x, self._s_cache, self._self_to_s_cache, q=self.q)
-
-    def _qbar_to_s(self, mu):
-        r"""
-        Return `\bar{q}_{\mu}` in the Schur basis.
-
-        EXAMPLES::
-
-            sage: q = ZZ['q'].fraction_field().gen()
-            sage: Sym = SymmetricFunctions(q.parent())
-            sage: qbar = Sym.hecke_character()
-            sage: qbar._qbar_to_s(Partition([2]))
-            -s[1, 1] + q*s[2]
-            sage: qbar._qbar_to_s(Partition([4]))
-            -s[1, 1, 1, 1] + q*s[2, 1, 1] - q^2*s[3, 1] + q^3*s[4]
-            sage: qbar._qbar_to_s(Partition([2,1]))
-            -s[1, 1, 1] + (q-1)*s[2, 1] + q*s[3]
-        """
-        if not mu:
-            return self._s.one()
-        ret = self._s.one()
-        q = self.q
-        mone = -self.base_ring().one()
-        return self._s.prod(sum(mone**(r-m) * q**(m-1)
-                                * self._s[_Partitions([m] + [1]*(r-m))]
-                                for m in range(1, r+1))
-                            for r in mu)
-
-    def _s_cache(self, n):
-        r"""
-        Compute the change of basis between the `\bar{q}` polynomials
-        and the Schur functions for partitions of size ``n``.
-
-        INPUT:
-
-        - ``self`` -- an instance of the Hall-Littlewood `P` basis
-        - ``n`` -- positive integer
-
-        EXAMPLES::
-
-            sage: Sym = SymmetricFunctions(FractionField(ZZ['q']))
-            sage: qbar = Sym.qbar()
-            sage: qbar._s_cache(2)
-            sage: l = lambda c: [ (i[0],[j for j in sorted(i[1].items())]) for i in sorted(c.items())]
-            sage: l(qbar._s_to_self_cache[2])
-            [([1, 1], [([1, 1], q/(q + 1)), ([2], -1/(q + 1))]),
-             ([2], [([1, 1], 1/(q + 1)), ([2], 1/(q + 1))])]
-            sage: l(qbar._self_to_s_cache[2])
-            [([1, 1], [([1, 1], 1), ([2], 1)]), ([2], [([1, 1], -1), ([2], q)])]
-            sage: qbar = Sym.qbar()
-            sage: qbar._s_cache(3)
-            sage: l(qbar._s_to_self_cache[2])
-            [([1, 1], [([1, 1], q/(q + 1)), ([2], -1/(q + 1))]),
-             ([2], [([1, 1], 1/(q + 1)), ([2], 1/(q + 1))])]
-        """
-        def coeff(part):
-            return lambda mu: self._qbar_to_s(part)[mu]
-        self._s._invert_morphism(n, self.base_ring(),
-                                 self._s_to_self_cache,
-                                 self._self_to_s_cache,
-                                 to_self_function=coeff)
 
     def coproduct_on_generators(self, r):
         r"""
@@ -311,10 +247,6 @@ class HeckeCharacter(SymmetricFunctionAlgebra_multiplicative):
             \Delta(\bar{q}_r) = \bar{q}_0 \otimes \bar{q}_r
             + (q - 1) \sum_{j=1}^{r-1} \bar{q}_j \otimes \bar{q}_{r-j}
             + \bar{q}_r \otimes \bar{q}_0
-
-        .. WARNING::
-
-            The formula given here is not been formally proven.
 
         EXAMPLES::
 
@@ -331,4 +263,3 @@ class HeckeCharacter(SymmetricFunctionAlgebra_multiplicative):
         q = self.q
         return T.sum_of_terms(((P(j), P(r-j)), one if j in [0,r] else q-one)
                               for j in range(r+1))
-

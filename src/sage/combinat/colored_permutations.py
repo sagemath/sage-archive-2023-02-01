55d7f221cbd3e9435ada34ab3eaf42deda0ca804
Travis Scrimshaw <tscrimsh@umn.edu>
2018-05-03 05:33:01 -0500

    Adding missing doctest.

--- src/sage/combinat/colored_permutations.py
+++ src/sage/combinat/colored_permutations.py
@@ -578,6 +578,14 @@ class ColoredPermutations(Parent, UniqueRepresentation):
 
             This returns a ``dict`` that should not be mutated since
             the result is cached.
+
+        EXAMPLES::
+
+            sage: C = ColoredPermutations(4, 3)
+            sage: C._inverse_simple_reflections()
+            {1: [[0, 0, 0], [2, 1, 3]],
+             2: [[0, 0, 0], [1, 3, 2]],
+             3: [[0, 0, 3], [1, 2, 3]]}
         """
         s = self.simple_reflections()
         return {i: ~s[i] for i in self.index_set()}

395e2548a86168afa96f20d9cc23dabb86717d7b
raghukul01 <raghukul.raman01@gmail.com>
2018-05-28 23:33:32 +0530

    Updated comments

--- src/sage/rings/number_field/bdd_height.py
+++ src/sage/rings/number_field/bdd_height.py
@@ -345,13 +345,13 @@ def integer_points_in_polytope(matrix, interval_radius):
 
 def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
     r""" 
-    Computes all elements in the number field `K` which have relative
-    multiplicative height at most ``height_bound``.
+    Computes all elements in the number field `K` which have height at most
+    ``height_bound``.
 
-    The function will only be called for number fields `K` with positive unit
+    The function can only be called for number fields `K` with positive unit
     rank. An error will occur if `K` is `QQ` or an imaginary quadratic field.
 
-    This algorithm computes 2 list: L containing elements x in `K` such that
+    This algorithm computes 2 lists: L containing elements x in `K` such that
     H_k(x) <= B, and a list L' containing elements x in `K` such that
     abs(H_k(x) - B) < tolerance.
 
@@ -362,7 +362,7 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
     ALGORITHM:
 
     This is an implementation of the revised algorithm (Algorithm 4) in
-    [Doyle-Krumm].
+    [Doyle-Krumm]_ .
 
     INPUT:
 
@@ -401,13 +401,6 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
         sage: K.<g> = QuadraticField(36865)
         sage: len(list(bdd_height(K,101))) # long time (4 s)
         131
-
-    ::
-
-        sage: from sage.rings.number_field.bdd_height import bdd_height
-        sage: K.<g> = NumberField(x^3 - 197*x + 39)
-        sage: len(list(bdd_height(K, 200))) # long time (5 s)
-        451
 
     ::
 
@@ -486,10 +479,10 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
         x_logs = []
         for i in range(r1):
             sigma = embeddings[i] # real embeddings
-            x_logs.append(abs(sigma(x)).log())
+            x_logs.append(sigma(x).abs().log())
         for i in range(r1, r + 1):
             tau = embeddings[i] # Complex embeddings
-            x_logs.append(2*abs(tau(x)).log())
+            x_logs.append(2*tau(x).abs().log())
         return vector(x_logs)
 
     def log_height_for_generators_approx(alpha,beta,Lambda):
@@ -514,8 +507,9 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
         Log_u_gi = vector(Log_gi) + unit_log_dict[u]
         arch_sum = sum([max(Log_u_gi[k], Log_gj[k]) for k in range(r + 1)])
         return (arch_sum - class_group_rep_norm_log_approx[n])
-    
 
+
+    # Step 1
     # Computes ideal class representative and their rational approx norm
     t = theta / (3*B)
     delta_1 = t / (6*r+12)
@@ -534,6 +528,7 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
     class_number = len(class_group_reps)
 
 
+    # Step 2
     # Find generators for principal ideals of bounded norm
     possible_norm_set = set([])
     for n in range(class_number):
@@ -549,6 +544,7 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
             lambda_gens_approx[g] = lambda_g_approx
 
 
+    # Step 3
     # Find a list of all generators corresponding to each ideal a_l
     generator_lists = []
     for l in range(class_number):
@@ -562,7 +558,8 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
         generator_lists.append(gens)
 
 
-    # Finds all relevent pair and thier height
+    # Step 4
+    # Finds all relevant pair and their height
     gen_height_approx_dictionary = dict()
     relevant_pair_lists = []
 
@@ -577,6 +574,8 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
                     gen_height_approx_dictionary[(n,i,j)] = log_height_for_generators_approx(gens[i],gens[j],t/6)
         relevant_pair_lists.append(relevant_pairs)
 
+
+    # Step 5
     b = rational_in(t/12 + RR(B).log(), t/4 + RR(B).log())
     maximum = 0
     for n in range(class_number):
@@ -584,6 +583,8 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
             maximum = max(maximum,gen_height_approx_dictionary[(n,p[0],p[1])])
     d_tilde = b + t/6 + maximum
 
+
+    # Step 6
     # computes fundamental units and their value under log map
     fund_units = UnitGroup(K).fundamental_units()
     fund_unit_logs = [log_map(fund_units[i]) for i in range(r)]
@@ -595,6 +596,8 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
     upper_bound = (r**2) * max(S_norm,S_inverse_norm)
     m = RR(upper_bound).ceil() + 1
 
+
+    # Step 7
     # Variables needed for rational approximation
     lambda_tilde = (t/12) / (d_tilde*r*(1+m))
     delta_tilde = min(lambda_tilde/((r**2)*((m**2)+m*lambda_tilde)), 1/(r**2))
@@ -602,12 +605,16 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
     M = RR(M).ceil()
     delta_2 = min(delta_tilde,(t/6)/(r*(r+1)*M))
 
+
+    # Step 8,9
     # Computes relevant points in polytope
     fund_unit_log_approx = [vector_delta_approximation(fund_unit_logs[i],delta_2) for i in range(r)]
     S_tilde = column_matrix(fund_unit_log_approx).delete_rows([r])
     S_tilde_inverse = S_tilde.inverse()
     U = integer_points_in_polytope(S_tilde_inverse, d_tilde)
 
+
+    # Step 10
     # tilde suffixed list are used for computing second list (L_primed)
     LL = [K(0)]
     U0 = []
@@ -615,6 +622,8 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
     L0 = []
     L0_tilde = []
 
+
+    # Step 11
     # Computes unit height
     unit_height_dict = dict()
     U_copy = copy(U)
@@ -635,6 +644,8 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
 
     relevant_tuples = set(U0+U0_tilde)
 
+
+    # Step 12
     # check for relevant packets
     for n in range(class_number):
         for pair in relevant_pair_lists[n]:
@@ -650,7 +661,8 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
                         L0_tilde.append([n,pair,u])
                         relevant_tuples.add(u);
 
-        
+
+    # Step 13
     # forms a dictionary of all_unit_tuples and thier value
     tuple_to_unit_dict = dict()
     for u in relevant_tuples:
@@ -659,6 +671,8 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
             unit *= (fund_units[k]**u[k])
         tuple_to_unit_dict[u] = unit
 
+
+    # Step 14
     # Build all output numbers
     L_primed = [] # This list contain points such that abs(H_k(u)-B) < tolerance
     roots_of_unity = K.roots_of_unity()
@@ -668,6 +682,9 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
     for u in U0_tilde:
         for zeta in roots_of_unity:
             L_primed.append(zeta*tuple_to_unit_dict[u])
+
+
+    # Step 15
     for p in L0:
         gens = generator_lists[p[0]]
         i = p[1][0]; j = p[1][1]
@@ -685,4 +702,5 @@ def bdd_height(K, height_bound, tolerance=1e-2, precision=53):
             L_primed.append(zeta*c_p)
             L_primed.append(zeta/c_p)
 
+    # Step 16
     return iter(LL + L_primed)

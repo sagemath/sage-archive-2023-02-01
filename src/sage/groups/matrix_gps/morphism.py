7b538fe4f62b988c6fdd7f478dd8f4687b6ae221
Travis Scrimshaw <tcscrims at gmail.com>
2018-05-30 17:11:39 +1000

    Deprecating the matrix_gps/morphism.py file.

--- src/sage/groups/matrix_gps/morphism.py
+++ src/sage/groups/matrix_gps/morphism.py
@@ -1,17 +1,7 @@
 """
 Homomorphisms Between Matrix Groups
 
-AUTHORS:
-
-- David Joyner and William Stein (2006-03): initial version
-
-- David Joyner (2006-05): examples
-
-- Simon King (2011-01): cleaning and improving code
-
-- Volker Braun (2013-1): port to new Parent, libGAP.
-
-- Simon Brandhorst (2018-5): deprecation
+Deprecated May, 2018.
 """
 
 #*****************************************************************************
@@ -22,17 +12,15 @@ AUTHORS:
 #  the License, or (at your option) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
-from __future__ import print_function
 
-from sage.categories.morphism import Morphism
-from sage.misc.latex import latex
-from sage.groups.libgap_morphism import GroupMorphism_libgap
-from sage.misc.superseded import deprecation
+from sage.misc.lazy_import import lazy_import
 
 def to_libgap(x):
     """
     Helper to convert ``x`` to a LibGAP matrix or matrix group
     element.
+
+    Deprecated; use the ``x.gap()`` method or ``libgap(x)`` instead.
 
     EXAMPLES::
 
@@ -46,53 +34,21 @@ def to_libgap(x):
         sage: to_libgap(matrix(QQ, [[1,2],[3,4]]))
         [ [ 1, 2 ], [ 3, 4 ] ]
     """
+    from sage.misc.superseded import deprecation
     deprecation(25444, "this function is deprecated."
-                "Try x.gap() or libgap(x) instead")
+                "Use x.gap() or libgap(x) instead")
     try:
         return x.gap()
     except AttributeError:
         from sage.libs.gap.libgap import libgap
         return libgap(x)
 
+lazy_import('sage.groups.libgap_morphism', 'GroupMorphism_libgap',
+            'MatrixGroupMorphism_im_gens', deprecation=25444)
 
-class MatrixGroupMap(Morphism):
-    r"""
-    This class is deprecated; use :class:`sage.groups.libgap_morphism.GroupMorphism_libgap` instead.
-    """
-    def _repr_type(self):
-        """
-        Part of the implementation of :meth:`_repr_`
+lazy_import('sage.categories.morphism', 'Morphism',
+            'MatrixGroupMap', deprecation=25444)
 
-        EXAMPLES::
-
-            sage: from sage.groups.matrix_gps.morphism import MatrixGroupMap
-            sage: MatrixGroupMap(ZZ.Hom(ZZ))._repr_type()
-            doctest:...: DeprecationWarning: MatrixGroupMap is deprecated.Use GroupMorphism_libgap instead.
-            See https://trac.sagemath.org/25444 for details.
-            'MatrixGroup'
-        """
-        deprecation(25444, "MatrixGroupMap is deprecated."
-                "Use GroupMorphism_libgap instead.")
-        return "MatrixGroup"
-
-class MatrixGroupMorphism(MatrixGroupMap):
-    r"""
-    This class is deprecated; use :class:`sage.groups.libgap_morphism.GroupMorphism_libgap` instead.
-    """
-    deprecation(25444, "MatrixGroupMorphism is deprecated."
-                "Use GroupMorphism_libgap instead.")
-
-
-class MatrixGroupMorphism_im_gens(GroupMorphism_libgap):
-    """
-    This class is deprecated; use :class:`sage.groups.libgap_morphism.GroupMorphism_libgap` instead.
-
-    Group morphism specified by images of generators.
-
-    EXAMPLES::
-
-        sage: from sage.groups.matrix_gps.morphism import MatrixGroupMorphism_im_gens
-    """
-    deprecation(25444, "MatrixGroupMorphism_im_gens is deprecated. "
-                "Use GroupMorphism_libgap instead.")
+lazy_import('sage.categories.morphism', 'Morphism',
+            'MatrixGroupMorphism', deprecation=25444)
 

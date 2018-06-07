2817d9081f84f8ff403e0c6e1a1ab04cdf24f966
Friedrich Wiemer <friedrich.wiemer@rub.de>
2018-06-07 16:42:02 +0200

    Fix delay introduced in the computation of walsh hadamard transform
    this patch reverts the computation of the single walsh hadamard
    transforms for each component function to the previous implementation,
    which does not use the component method of the sbox, as this greatly
    increased the runtime.

--- src/sage/crypto/sbox.py
+++ src/sage/crypto/sbox.py
@@ -582,7 +582,12 @@ class SBox(SageObject):
         else:
             raise ValueError("no such scaling for the LAM: %s" % scale)
 
-        L = [self.component_function(i).walsh_hadamard_transform() for i in range(ncols)]
+        B = BooleanFunction(self.m)
+        L = []
+        for j in range(ncols):
+            for i in range(nrows):
+                B[i] = ZZ(self(i)&j).popcount()
+            L.append(B.walsh_hadamard_transform())
 
         A = Matrix(ZZ, ncols, nrows, L)
         A = A.transpose()/scale_factor

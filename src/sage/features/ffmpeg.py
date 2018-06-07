ba9cf98f587a0304642946797b995f6790835aad
Sébastien Labbé <slabqc@gmail.com>
2018-06-07 15:35:02 +0200

    25305: added licence to newly created files

--- src/sage/features/ffmpeg.py
+++ src/sage/features/ffmpeg.py
@@ -2,6 +2,15 @@
 r"""
 Check for FFmpeg
 """
+#*****************************************************************************
+#       Copyright (C) 2018 Sebastien Labbe <slabqc@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
 
 from . import Executable
 

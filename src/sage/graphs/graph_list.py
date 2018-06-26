3d528aeb02e2d7cea993aa5c86b71c3f3883b9e0
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2018-06-26 09:08:28 +0200

    small cleanup of graph_list, removing use of isinstance(.., file) there

--- src/sage/graphs/graph_list.py
+++ src/sage/graphs/graph_list.py
@@ -9,13 +9,15 @@ AUTHORS:
   (to_graphics_array and show_graphs)
 """
 
-#*****************************************************************************
+# ****************************************************************************
 #           Copyright (C) 2007 Robert L. Miller <rlmillster@gmail.com>
 #                              and Emily A. Kirkman
 #
 # Distributed  under  the  terms  of  the  GNU  General  Public  License (GPL)
 #                         http://www.gnu.org/licenses/
-#*****************************************************************************
+# ****************************************************************************
+from io import IOBase
+
 
 def from_whatever(data):
     """
@@ -36,12 +38,12 @@ def from_whatever(data):
         [Graph on 15 vertices, Looped multi-graph on 17 vertices]
     """
     from sage.graphs.graph import Graph
-    if isinstance(data, file):
+    if isinstance(data, IOBase):
         if data.name[data.name.rindex('.'):] == '.g6':
             return from_graph6(data)
         elif data.name[data.name.rindex('.'):] == '.s6':
             return from_sparse6(data)
-        else: # convert to list of lines, do each separately
+        else:  # convert to list of lines, do each separately
             L = data.readlines()
             return from_whatever(L)
     if isinstance(data, list):
@@ -69,6 +71,7 @@ def from_whatever(data):
                 l.append(Graph(d, sparse=sparse))
         return l
 
+
 def from_graph6(data):
     """
     Returns a list of Sage Graphs, given a list of graph6 data.
@@ -87,20 +90,20 @@ def from_graph6(data):
         [Graph on 15 vertices, Graph on 25 vertices]
     """
     from sage.graphs.graph import Graph
-    if isinstance(data,str):
+    if isinstance(data, str):
         data = data.split('\n')
         l = []
         for d in data:
             if not d == '':
-                l.append(Graph(d, format = 'graph6'))
+                l.append(Graph(d, format='graph6'))
         return l
-    elif isinstance(data,list):
+    elif isinstance(data, list):
         l = []
         for d in data:
             if isinstance(d, str):
                 nn = d.rfind('\n')
                 if nn == -1:
-                    l.append(Graph(d,format='graph6'))
+                    l.append(Graph(d, format='graph6'))
                 elif len(d) == nn + 1:
                     l.append(Graph(d[:nn], format='graph6'))
                 else:
@@ -108,12 +111,13 @@ def from_graph6(data):
             else:
                 l.append(from_graph6(d))
         return l
-    elif isinstance(data,file):
+    elif isinstance(data, IOBase):
         strlist = data.readlines()
         l = []
         for s in strlist:
             l.append(Graph(s[:s.rfind('\n')], format='graph6'))
         return l
+
 
 def from_sparse6(data):
     """
@@ -133,14 +137,14 @@ def from_sparse6(data):
         [Looped multi-graph on 17 vertices, Looped multi-graph on 39 vertices]
     """
     from sage.graphs.graph import Graph
-    if isinstance(data,str):
+    if isinstance(data, str):
         data = data.split('\n')
         l = []
         for d in data:
             if not d == '':
-                l.append(Graph(d, format = 'sparse6', sparse=True))
+                l.append(Graph(d, format='sparse6', sparse=True))
         return l
-    elif isinstance(data,list):
+    elif isinstance(data, list):
         l = []
         for d in data:
             if isinstance(d, str):
@@ -154,14 +158,15 @@ def from_sparse6(data):
             else:
                 l.append(from_sparse6(d))
         return l
-    elif isinstance(data,file):
+    elif isinstance(data, IOBase):
         strlist = data.readlines()
         l = []
         for s in strlist:
             l.append(Graph(s[:s.rfind('\n')], format='sparse6', sparse=True))
         return l
 
-def to_graph6(list, file = None, output_list=False):
+
+def to_graph6(list, file=None, output_list=False):
     r"""
     Converts a list of Sage graphs to a single string of graph6 graphs.
     If file is specified, then the string will be written quietly to
@@ -192,7 +197,7 @@ def to_graph6(list, file = None, output_list=False):
     if file is None:
         if output_list:
             a = l.split('\n')
-            a = a[:len(a)-1]
+            a = a[:len(a) - 1]
             return a
         else:
             return l
@@ -200,7 +205,8 @@ def to_graph6(list, file = None, output_list=False):
         file.write(l)
         file.flush()
 
-def to_sparse6(list, file = None, output_list=False):
+
+def to_sparse6(list, file=None, output_list=False):
     r"""
     Converts a list of Sage graphs to a single string of sparse6
     graphs. If file is specified, then the string will be written
@@ -231,7 +237,7 @@ def to_sparse6(list, file = None, output_list=False):
     if file is None:
         if output_list:
             a = l.split('\n')
-            a = a[:len(a)-1]
+            a = a[:len(a) - 1]
             return a
         else:
             return l
@@ -239,7 +245,7 @@ def to_sparse6(list, file = None, output_list=False):
         file.write(l)
         file.flush()
 
-        
+
 def to_graphics_array(graph_list, **kwds):
     """
     Draw all graphs in a graphics array
@@ -248,7 +254,7 @@ def to_graphics_array(graph_list, **kwds):
 
     -  ``graph_list`` - a list of Sage graphs
 
-    GRAPH PLOTTING: 
+    GRAPH PLOTTING:
 
     Defaults to circular layout for graphs. This allows
     for a nicer display in a small area and takes much less time to
@@ -275,9 +281,9 @@ def to_graphics_array(graph_list, **kwds):
     """
     from sage.graphs import graph
     plist = []
-    for i in range(len(graph_list)):
-        if isinstance(graph_list[i], graph.GenericGraph):
-            pos = graph_list[i].get_pos()
+    for graph_i in graph_list:
+        if isinstance(graph_i, graph.GenericGraph):
+            pos = graph_i.get_pos()
             if pos is None:
                 if 'layout' not in kwds:
                     kwds['layout'] = 'circular'
@@ -286,9 +292,11 @@ def to_graphics_array(graph_list, **kwds):
                 if 'vertex_labels' not in kwds:
                     kwds['vertex_labels'] = False
                 kwds['graph_border'] = True
-                plist.append(graph_list[i].plot(**kwds))
+                plist.append(graph_i.plot(**kwds))
             else:
-                plist.append(graph_list[i].plot(pos=pos, vertex_size=50, vertex_labels=False, graph_border=True))
+                plist.append(graph_i.plot(pos=pos, vertex_size=50,
+                                          vertex_labels=False,
+                                          graph_border=True))
         else:
             raise TypeError('param list must be a list of Sage (di)graphs.')
     from sage.plot.plot import graphics_array
@@ -308,13 +316,11 @@ def show_graphs(graph_list, **kwds):
 
     INPUT:
 
-
-    -  ``list`` - a list of Sage graphs
-
+    -  ``list`` -- a list of Sage graphs
 
     GRAPH PLOTTING: Defaults to circular layout for graphs. This allows
     for a nicer display in a small area and takes much less time to
-    compute than the spring- layout algorithm for many graphs.
+    compute than the spring-layout algorithm for many graphs.
 
     EXAMPLES: Create a list of graphs::
 
@@ -362,8 +368,5 @@ def show_graphs(graph_list, **kwds):
     """
     graph_list = list(graph_list)
     for i in range(len(graph_list) // 20 + 1):
-        graph_slice =graph_list[20*i:20*(i+1)]
+        graph_slice = graph_list[20 * i: 20 * (i + 1)]
         to_graphics_array(graph_slice, **kwds).show()
-
-
-

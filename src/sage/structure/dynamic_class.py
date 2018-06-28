43747a18a24e009a223e97843e6e0f32ec6e7e11
Erik M. Bray <erik.bray@lri.fr>
2018-06-28 16:27:28 +0000

    ensure that each base is a new-style class before trying to get its MRO
    this check is technically only needed on Python 2

--- src/sage/structure/dynamic_class.py
+++ src/sage/structure/dynamic_class.py
@@ -407,7 +407,8 @@ def dynamic_class_internal(name, bases, cls=None, reduction=None, doccls=None, p
             cls_bases = cls.__bases__
             all_bases = set()
             for base in bases:
-                all_bases.update(base.mro())
+                if isinstance(base, type):
+                    all_bases.update(base.mro())
             cls_bases = tuple(b for b in cls_bases if b not in all_bases)
             bases = cls_bases + bases
     else:

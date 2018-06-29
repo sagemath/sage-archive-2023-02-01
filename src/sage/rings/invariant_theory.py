0423f81ec602b14d9ab95ee0a72e000a2224f64d
Jesper Noordsij <jesper.noordsij@gmail.com>
2018-06-29 09:07:19 +0200

    Removed canonical form to fix doctest.

--- src/sage/rings/invariant_theory.py
+++ src/sage/rings/invariant_theory.py
@@ -110,7 +110,6 @@ AUTHORS:
 #*****************************************************************************
 
 
-from sage.rings.all import QQ
 from sage.misc.functional import is_odd
 from sage.matrix.constructor import matrix
 from sage.structure.sage_object import SageObject
@@ -2115,39 +2114,6 @@ class BinaryQuintic(AlgebraicForm):
         invariants['I12'] = 5**10 * (R(2)**-1*3*clebsch['A']**3-2**5*R(3)**-1*clebsch['C'])
         invariants['I18'] = 2**8*R(3)**-1*5**15 * clebsch['R']
         return invariants
-
-    def canonical_form(self):
-        r"""
-        Returnsa canonical representative of the `GL(2,K)`-orbit of
-        the quintic.
-
-        OUTPUT:
-
-        A canonical `GL(2,K)`-equivalent binary quintic.
-
-        EXAMPLES::
-
-            sage: R.<x0, x1> = QQ[]
-            sage: p = 2*x1^5 + 4*x1^4*x0 + 5*x1^3*x0^2 + 7*x1^2*x0^3 - 11*x1*x0^4 + x0^5
-            sage: f = invariant_theory.binary_quintic(p, x0, x1)
-            sage: g = matrix(QQ, [[11,5],[7,2]])
-            sage: gf = f.transformed(g)
-            sage: f.canonical_form() == gf.canonical_form()
-            True
-        """
-        from sage.rings.invariants.reconstruction import binary_quintic_from_invariants
-        clebsch = self.clebsch_invariants(as_tuple=True)
-        K = self._ring.base_ring()
-        if K == QQ:
-            # Note this method works for any number field with class number 1, but
-            # the scaled method is a lot faster as computing the class number
-            # takes a lot of time.
-            coeffs = binary_quintic_from_invariants(clebsch, reduced=True)
-        else:
-            coeffs = binary_quintic_from_invariants(clebsch, scaled=True)
-        x, y = self._variables
-        form = sum([coeffs[i]*x**i*y**(5-i) for i in range(6)])
-        return invariant_theory.binary_quintic(form, x, y)
 
 ######################################################################
 def _covariant_conic(A_scaled_coeffs, B_scaled_coeffs, monomials):

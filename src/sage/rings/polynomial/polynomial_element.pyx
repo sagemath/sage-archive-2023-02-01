f70f05337b987bf385c844f9f0209a122664abbe
Travis Scrimshaw <tcscrims at gmail.com>
2018-07-03 01:18:46 +1000

    Improve the speed of _mpoly_dict_recursive.

--- src/sage/rings/polynomial/polynomial_element.pyx
+++ src/sage/rings/polynomial/polynomial_element.pyx
@@ -166,7 +166,7 @@ cpdef is_Polynomial(f):
 
 from .polynomial_compiled cimport CompiledPolynomialFunction
 
-from .polydict import ETuple
+from sage.rings.polynomial.polydict cimport ETuple
 
 cdef object is_AlgebraicRealField
 cdef object is_AlgebraicField
@@ -3200,7 +3200,7 @@ cdef class Polynomial(CommutativeAlgebraElement):
             -2/7*theta^3 + 2/3*theta - 19/993
         """
         R = self._parent.base_ring()[var]
-        return R(self.list())
+        return R(self.list(copy=False))
 
     def change_ring(self, R):
         """
@@ -3242,9 +3242,9 @@ cdef class Polynomial(CommutativeAlgebraElement):
                 R = self._parent.hom(R, self._parent.change_ring(R.codomain()))
             return R(self)
         else:
-            return self._parent.change_ring(R)(list(self))
+            return self._parent.change_ring(R)(self.list(copy=False))
 
-    def _mpoly_dict_recursive(self, variables=None, base_ring=None):
+    cpdef dict _mpoly_dict_recursive(self, tuple variables=None, base_ring=None):
         """
         Return a dict of coefficient entries suitable for construction of a
         MPolynomial_polydict with the given variables.
@@ -3261,32 +3261,37 @@ cdef class Polynomial(CommutativeAlgebraElement):
         if not self:
             return {}
 
+        cdef ETuple const_ix
+        cdef list mpolys
+        cdef Py_ssize_t k
+
         var = self._parent.variable_name()
         if variables is None:
             variables = self._parent.variable_names_recursive()
-        if not var in variables:
+        if var not in variables:
             x = base_ring(self) if base_ring else self
             const_ix = ETuple((0,)*len(variables))
             return { const_ix: x }
 
-        prev_variables = variables[:list(variables).index(var)]
+        cdef tuple prev_variables = variables[:variables.index(var)]
         const_ix = ETuple((0,)*len(prev_variables))
         mpolys = None
 
-        if len(prev_variables) > 0:
+        if prev_variables:
             try:
-                mpolys = [a._mpoly_dict_recursive(prev_variables, base_ring) for a in self]
-            except AttributeError as msg:
+                mpolys = [a._mpoly_dict_recursive(prev_variables, base_ring)
+                          for a in self]
+            except AttributeError:
                 pass
 
         if mpolys is None:
             if base_ring is not None and base_ring is not self.base_ring():
-                mpolys = [{const_ix:base_ring(a)} if a else {} for a in self]
+                mpolys = [{const_ix: base_ring(a)} if a else {} for a in self]
             else:
-                mpolys = [{const_ix:a} if a else {} for a in self]
+                mpolys = [{const_ix: a} if a else {} for a in self]
 
-        D = {}
-        leftovers = (0,) * (len(variables) - len(prev_variables) - 1)
+        cdef dict D = {}
+        cdef tuple leftovers = (0,) * (len(variables) - len(prev_variables) - 1)
         for k in range(len(mpolys)):
             for i,a in mpolys[k].iteritems():
                 j = ETuple((k,) + leftovers)
@@ -3815,8 +3820,9 @@ cdef class Polynomial(CommutativeAlgebraElement):
             sage: f.dict()
             {0: 13, 1: -1/7, 3: 1}
         """
-        X = {}
-        Y = self.list(copy=False)
+        cdef dict X = {}
+        cdef list Y = self.list(copy=False)
+        cdef Py_ssize_t i
         for i in xrange(len(Y)):
             c = Y[i]
             if c:

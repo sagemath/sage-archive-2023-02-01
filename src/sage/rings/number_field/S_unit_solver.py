8472bc4c1373401c8105e628e7ce62c3c057b987
Mckenzie West <mckenzierwest@gmail.com>
2018-07-02 15:01:40 +0200

    Adjusting documentation and code to fit with Python conventions

--- src/sage/rings/number_field/S_unit_solver.py
+++ src/sage/rings/number_field/S_unit_solver.py
@@ -66,9 +66,10 @@ def column_Log(SUK, iota, U, prec=106):
 
         sage: from sage.rings.number_field.S_unit_solver import column_Log
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: S = tuple(K.primes_above(3))
+        sage: SUK = UnitGroup(K, S=S)
         sage: phi_complex = K.places()[1]
-        sage: v_fin = tuple(K.primes_above(3))[0]
+        sage: v_fin = S[0]
         sage: U = [phi_complex, v_fin]
         sage: column_Log(SUK, xi^2, U) # abs tol 1e-29
         [1.464816384890812968648768625966, -2.197224577336219382790490473845]
@@ -79,7 +80,7 @@ def column_Log(SUK, iota, U, prec=106):
     """
     R = RealField(prec)
 
-    return [ R(SUK.number_field().abs_val(v,iota,prec)).log() for v in U]
+    return [ R(SUK.number_field().abs_val(v, iota, prec)).log() for v in U]
 
 def c3_func(SUK, prec=106):
     r"""
@@ -98,7 +99,7 @@ def c3_func(SUK, prec=106):
 
         sage: from sage.rings.number_field.S_unit_solver import c3_func
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
 
         sage: c3_func(SUK) # abs tol 1e-29
         0.4257859134798034746197327286726
@@ -117,20 +118,20 @@ def c3_func(SUK, prec=106):
 
     all_places = list(SUK.primes()) + SUK.number_field().places(prec)
     Possible_U = Combinations(all_places, SUK.rank())
-    c1 = 0
+    c1 = R(0)
     for U in Possible_U:
         # first, build the matrix C_{i,U}
         columns_of_C = []
         for unit in SUK.fundamental_units():
-            columns_of_C.append( column_Log(SUK, unit, U, prec) )
+            columns_of_C.append(column_Log(SUK, unit, U, prec))
         C = Matrix(SUK.rank(), SUK.rank(), columns_of_C)
         # Is it invertible?
         if abs(C.determinant()) > 10**(-10):
             poss_c1 = C.inverse().apply_map(abs).norm(Infinity)
-            c1 = max(poss_c1,c1)
-    return R(R(0.9999999)/(R(c1)*SUK.rank()))
+            c1 = R(max(poss_c1, c1))
+    return R(0.9999999) / (c1*SUK.rank())
 
-def c4_func(SUK,v, A, prec=106):
+def c4_func(SUK, v, A, prec=106):
     r"""
     Return the constant `c_4` from Smart's TCDF paper, [Sma1995]_
 
@@ -149,7 +150,7 @@ def c4_func(SUK,v, A, prec=106):
 
         sage: from sage.rings.number_field.S_unit_solver import c4_func
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
         sage: phi_real = K.places()[0]
         sage: phi_complex = K.places()[1]
         sage: v_fin = tuple(K.primes_above(3))[0]
@@ -168,9 +169,7 @@ def c4_func(SUK,v, A, prec=106):
 
     - [Sma1995]_ p. 824
     """
-    R = RealField(prec)
-
-    return R(max(SUK.number_field().abs_val(v, alpha, prec) for alpha in A))
+    return max(SUK.number_field().abs_val(v, alpha, prec) for alpha in A)
 
 def beta_k(betas_and_ns):
     r"""
@@ -188,10 +187,10 @@ def beta_k(betas_and_ns):
 
         sage: from sage.rings.number_field.S_unit_solver import beta_k
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
         sage: v_fin = tuple(K.primes_above(3))[0]
 
-        sage: betas = [[beta,beta.valuation(v_fin)] for beta in SUK.fundamental_units()]
+        sage: betas = [ [beta, beta.valuation(v_fin)] for beta in SUK.fundamental_units() ]
         sage: beta_k(betas)
         [xi, 1]
 
@@ -204,11 +203,11 @@ def beta_k(betas_and_ns):
             good_pair = pair
             break
     for pair in betas_and_ns:
-        if (pair[1].abs() != 0 and pair[1].abs() < good_pair[1].abs()):
+        if (pair[1].abs()!=0 and pair[1].abs()<good_pair[1].abs()):
             good_pair = pair
     return good_pair
 
-def mus(SUK,v):
+def mus(SUK, v):
     r"""
     Return a list `[\mu]`, for `\mu` defined on pp. 824-825 of TCDF, [Sma1995]_
 
@@ -225,10 +224,10 @@ def mus(SUK,v):
 
         sage: from sage.rings.number_field.S_unit_solver import mus
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
         sage: v_fin = tuple(K.primes_above(3))[0]
 
-        sage: mus(SUK,v_fin)
+        sage: mus(SUK, v_fin)
         [xi^2 - 2]
 
     REFERENCES:
@@ -263,8 +262,9 @@ def possible_mu0s(SUK, v):
 
         sage: from sage.rings.number_field.S_unit_solver import possible_mu0s
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
-        sage: v_fin = tuple(K.primes_above(3))[0]
+        sage: S = tuple(K.primes_above(3))
+        sage: SUK = UnitGroup(K, S=S)
+        sage: v_fin = S[0]
 
         sage: possible_mu0s(SUK,v_fin)
         [-1, 1]
@@ -285,13 +285,13 @@ def possible_mu0s(SUK, v):
     ns = [beta[1] for beta in beta_and_ns if beta[0] != betak]
     betas = [beta[0] for beta in beta_and_ns if beta[0] != betak]
     mu0s = []
-    for rs in combinations_with_replacement(range(nk.abs()),len(betas)):
+    for rs in combinations_with_replacement(range(nk.abs()), len(betas)):
         # n_0 = valuation_v of one of the coefficients of the equation = 0 for x + y = 1 p. 824
-        n_rs = zip(ns,rs)
+        n_rs = zip(ns, rs)
         sigma_tilde = -(sum([n_r[0]*n_r[1] for n_r in n_rs]))
         if sigma_tilde % nk == 0:
-            beta_rs = zip(betas,rs)
-            temp_prod = prod([beta_r[0]**beta_r[1] for beta_r in beta_rs])*betak**(sigma_tilde/nk)
+            beta_rs = zip(betas, rs)
+            temp_prod = prod([beta_r[0]**beta_r[1] for beta_r in beta_rs]) * betak**(sigma_tilde/nk)
             for alpha0 in SUK.roots_of_unity():
                 if alpha0*temp_prod not in mu0s:
                     mu0s.append(alpha0*temp_prod)
@@ -316,11 +316,11 @@ def c8_c9_func(SUK, v, A, prec=106):
 
         sage: from sage.rings.number_field.S_unit_solver import c8_c9_func
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
         sage: v_fin = K.primes_above(3)[0]
         sage: A = K.roots_of_unity()
 
-        sage: c8_c9_func(SUK,v_fin,A) # abs tol 1e-29
+        sage: c8_c9_func(SUK, v_fin,A) # abs tol 1e-29
         (4.524941291354698258804956696127e15, 1.621521281297160786545580368612e16)
 
     REFERENCES:
@@ -330,38 +330,38 @@ def c8_c9_func(SUK, v, A, prec=106):
 
     """
     R = RealField(prec)
-    num_mus = len(mus(SUK,v))+1
+    num_mus = len(mus(SUK, v))+1
     p = v.smallest_integer()
     f_p = v.residue_class_degree()
     d = SUK.number_field().degree()
     if p == 2:
-        local_c2 = Integer(197142)*Integer(36)**num_mus
+        local_c2 = Integer(197142) * Integer(36)**num_mus
     elif p%4 == 1:
-        local_c2 = Integer(35009)*(Integer(45)/Integer(2))**num_mus
+        local_c2 = Integer(35009) * (Integer(45)/Integer(2))**num_mus
     else:
-        local_c2 = Integer(30760)*Integer(25)**num_mus
+        local_c2 = Integer(30760) * Integer(25)**num_mus
     x = polygen(SUK.number_field())
-    if ( p > 2 and not ((x**2+1).is_irreducible()) ) or ( p==2 and not ((x**2+3).is_irreducible()) ):
+    if (p > 2 and not ((x**2+1).is_irreducible())) or (p==2 and not ((x**2+3).is_irreducible())):
         D = d
     else:
         D = 2*d
-    l_c3 = (num_mus+1)**(2*num_mus+4)*p**(D * f_p/d)*(f_p*R(p).log())**(-num_mus-1)*D**(num_mus+2)
+    l_c3 = (num_mus+1)**(2*num_mus+4) * p**(D * f_p/d) * (f_p*R(p).log())**(-num_mus-1) * D**(num_mus+2)
 
-    def modified_height(SUK,v,D,b):
+    def modified_height(SUK, v, D, b):
         #[Sma1998]_ p. 226
         max_log_b = max([phi(b).log().abs() for phi in SUK.number_field().places(prec)])
-        return R(max([b.global_height(),max_log_b/(2*R.pi()*D),f_p*R(p).log()/d]))
+        return R(max([b.global_height(), max_log_b/(2*R.pi()*D), f_p*R(p).log()/d]))
 
     mus_prod = prod([modified_height(SUK,v,D,b) for b in mus(SUK,v)])
-    local_c3 = R(max([mus_prod*modified_height(SUK,v,D,mu0) for mu0 in possible_mu0s(SUK,v)]))
+    local_c3 = R(max([mus_prod*modified_height(SUK, v, D, mu0) for mu0 in possible_mu0s(SUK, v)]))
 
     l_c3 *= local_c3
-    H = max([modified_height(SUK,v,D,alpha) for alpha in mus(SUK,v)+possible_mu0s(SUK,v)])
+    H = max([modified_height(SUK, v, D, alpha) for alpha in mus(SUK, v)+possible_mu0s(SUK, v)])
     if p == 2:
-        local_c4 = R(3*2**10*(num_mus+1)**2*D**2*H).log()
+        local_c4 = R(3 * 2**10 * (num_mus+1)**2 * D**2 * H).log()
     else:
-        local_c4 = R(2**11*(num_mus+1)**2*D**2*H).log()
-    local_c5 = 2*R(D).log()
+        local_c4 = R(2**11 * (num_mus+1)**2 * D**2 * H).log()
+    local_c5 = 2 * R(D).log()
     return R(local_c2*l_c3*local_c4), R(local_c2*l_c3*local_c4*local_c5)
 
 def c11_func(SUK, v, A, prec=106):
@@ -383,15 +383,15 @@ def c11_func(SUK, v, A, prec=106):
 
         sage: from sage.rings.number_field.S_unit_solver import c11_func
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
         sage: phi_real = K.places()[0]
         sage: phi_complex = K.places()[1]
         sage: A = K.roots_of_unity()
 
-        sage: c11_func(SUK,phi_real,A) # abs tol 1e-29
+        sage: c11_func(SUK, phi_real, A) # abs tol 1e-29
         3.255848343572896153455615423662
 
-        sage: c11_func(SUK,phi_complex,A) # abs tol 1e-29
+        sage: c11_func(SUK, phi_complex, A) # abs tol 1e-29
         6.511696687145792306911230847323
 
     REFERENCES:
@@ -400,9 +400,9 @@ def c11_func(SUK, v, A, prec=106):
     """
     R = RealField(prec)
     if is_real_place(v):
-        return R(R(4*c4_func(SUK, v, A, prec)).log()/(c3_func(SUK, prec)))
+        return R(4*c4_func(SUK, v, A, prec)).log() / c3_func(SUK, prec)
     else:
-        return R(2*(R(4*(c4_func(SUK,v, A, prec)).sqrt()).log())/(c3_func(SUK, prec)))
+        return 2*R(4*(c4_func(SUK, v, A, prec)).sqrt()).log() / c3_func(SUK, prec)
 
 def c13_func(SUK, v, prec=106):
     r"""
@@ -422,22 +422,20 @@ def c13_func(SUK, v, prec=106):
 
         sage: from sage.rings.number_field.S_unit_solver import c13_func
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
         sage: phi_real = K.places()[0]
         sage: phi_complex = K.places()[1]
 
-        sage: c13_func(SUK,phi_real) # abs tol 1e-29
+        sage: c13_func(SUK, phi_real) # abs tol 1e-29
         0.4257859134798034746197327286726
 
-        sage: c13_func(SUK,phi_complex) # abs tol 1e-29
+        sage: c13_func(SUK, phi_complex) # abs tol 1e-29
         0.2128929567399017373098663643363
 
-    It is an error to input a finite place
-
-    ::
+    It is an error to input a finite place. ::
 
         sage: phi_finite = K.primes_above(3)[0]
-        sage: c13_func(SUK,phi_finite)
+        sage: c13_func(SUK, phi_finite)
         Traceback (most recent call last):
         ...
         TypeError: Place must be infinite
@@ -452,9 +450,9 @@ def c13_func(SUK, v, prec=106):
     except AttributeError:
         raise TypeError('Place must be infinite')
     if is_real_place(v):
-        return c3_func(SUK,prec)
+        return c3_func(SUK, prec)
     else:
-        return c3_func(SUK,prec)/2
+        return c3_func(SUK, prec)/2
 
 def K0_func(SUK, A, prec=106):
     r"""
@@ -474,10 +472,10 @@ def K0_func(SUK, A, prec=106):
 
         sage: from sage.rings.number_field.S_unit_solver import K0_func
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
         sage: A = K.roots_of_unity()
 
-        sage: K0_func(SUK,A) # abs tol 1e-29
+        sage: K0_func(SUK, A) # abs tol 1e-29
         9.475576673109275443280257946929e17
 
     REFERENCES:
@@ -487,19 +485,19 @@ def K0_func(SUK, A, prec=106):
     R = RealField(prec)
 
     def c5_func(SUK, v, R):
-        return R(c3_func(SUK, R.precision())/(v.residue_class_degree()*R(v.smallest_integer()).log()*v.ramification_index()))
+        return c3_func(SUK, R.precision()) / (v.residue_class_degree()*R(v.smallest_integer()).log()*v.ramification_index())
 
     def c6_func(SUK, v, A, R):
-        return R(R(c4_func(SUK, v, A, R.precision())).log()/(v.residue_class_degree()*R(v.smallest_integer()).log()*v.ramification_index()))
+        return c4_func(SUK, v, A, R.precision()).log() / (v.residue_class_degree()*R(v.smallest_integer()).log()*v.ramification_index())
 
     def c7_func(SUK, v, A, R):
-        return R(R(c4_func(SUK, v, A, R.precision())).log()/c3_func(SUK, R.precision()))
+        return (c4_func(SUK, v, A, R.precision())).log() / c3_func(SUK, R.precision())
 
     def c10_func(SUK, v, A, R):
         # [Sma1995]_ p. 824
         e_h = v.ramification_index()
         c_8, c_9 = c8_c9_func(SUK, v, A, R.precision())
-        return R((2/(e_h*c5_func(SUK, v, R)))*(e_h*c6_func(SUK, v, A, R) + c_9 + c_8 * R( c_8/(e_h*c5_func(SUK, v, R))).log()))
+        return (2/(e_h*c5_func(SUK, v, R))) * (e_h*c6_func(SUK, v, A, R)+c_9+c_8*(c_8/(e_h*c5_func(SUK, v, R))).log())
 
     return R(max([c10_func(SUK,v, A, R) for v in SUK.primes()] + [c7_func(SUK,v,A,R) for v in SUK.primes()]))
 
@@ -522,15 +520,15 @@ def K1_func(SUK, v, A, prec=106):
 
         sage: from sage.rings.number_field.S_unit_solver import K1_func
         sage: K.<xi> = NumberField(x^3-3)
-        sage: SUK = UnitGroup(K,S=tuple(K.primes_above(3)))
+        sage: SUK = UnitGroup(K, S=tuple(K.primes_above(3)))
         sage: phi_real = K.places()[0]
         sage: phi_complex = K.places()[1]
         sage: A = K.roots_of_unity()
 
-        sage: K1_func(SUK,phi_real,A)
+        sage: K1_func(SUK, phi_real, A)
         4.396386097852707394927181864635e16
 
-        sage: K1_func(SUK,phi_complex,A)
+        sage: K1_func(SUK, phi_complex, A)
         2.034870098399844430207420286581e17
 
     REFERENCES:
@@ -542,29 +540,29 @@ def K1_func(SUK, v, A, prec=106):
 
     #[Sma1995]_ p. 825
     if is_real_place(v):
-        c11 = R(R(4*c4_func(SUK, v, A, prec)).log()/(c3_func(SUK, prec)))
+        c11 = R(4*c4_func(SUK, v, A, prec)).log() / c3_func(SUK, prec)
     else:
-        c11 = R(2*(R(4*(c4_func(SUK,v, A, prec)).sqrt()).log())/(c3_func(SUK, prec)))
+        c11 = 2*( R(4*(c4_func(SUK,v, A, prec)).sqrt()).log() ) / c3_func(SUK, prec)
 
     #[Sma1995]_ p. 825
     if is_real_place(v):
-        c12 = R(2*c4_func(SUK, v, A, prec))
+        c12 = R(2 * c4_func(SUK, v, A, prec))
     else:
-        c12 = R(2*(c4_func(SUK,v, A, prec)).sqrt())
+        c12 = R(2 * c4_func(SUK, v, A, prec).sqrt())
 
     #[Sma1998]_ p. 225, Theorem A.1
     d = SUK.number_field().degree()
     t = SUK.rank()
-    Baker_C = R( 18 * factorial(t+2) * (t+1)**(t+2) * (32*d)**(t + 3) * R( 2*(t+1) * d).log() )
+    Baker_C = R(18 * factorial(t+2) * (t+1)**(t+2) * (32*d)**(t+3) * R(2*(t+1) * d).log())
 
     def hprime(SUK, alpha, v):
         #[Sma1998]_ p. 225
-        return R(max(alpha.global_height(), 1/SUK.number_field().degree(), (v(alpha)).log().abs()/SUK.number_field().degree()))
+        return R(max(alpha.global_height(), 1/SUK.number_field().degree(), v(alpha).log().abs()/SUK.number_field().degree()))
 
     #[Sma1995]_ p. 825 and [Sma1998]_ p. 225, Theorem A.1
     c14 = Baker_C * prod([hprime(SUK, alpha, v) for alpha in SUK.gens_values()])
 
     #[Sma1995]_ p. 825
-    c15 = R(2*((c12).log()+c14*R((SUK.rank()+1)*c14/c13_func(SUK, v, prec)).log())/c13_func(SUK, v, prec))
+    c15 = 2 * (c12.log()+c14*((SUK.rank()+1)*c14/c13_func(SUK, v, prec)).log()) / c13_func(SUK, v, prec)
 
     return max([c11, c15])

cd92ad29210f366f57728e8c1bfe782ef8f21c3c
Xavier Caruso <xavier.caruso@univ-rennes1.fr>
2018-07-24 16:01:52 -0500

    More doctests

--- src/sage/rings/padics/relative_ramified_CA.pyx
+++ src/sage/rings/padics/relative_ramified_CA.pyx
@@ -3,4 +3,28 @@ include "CA_template.pxi"
 
 cdef class RelativeRamifiedCappedAbsoluteElement(CAElement):
     def _poly_rep(self):
+        """
+        Return the underlying polynomial representation of this element 
+        (which is used for computations).
+
+        For debug purpose.
+
+        EXAMPLES::
+
+            sage: K.<a> = ZqCA(125)
+            sage: S.<x> = PolynomialRing(K)
+            sage: W.<w> = K.extension(x^3 - 25*x^2 - 5*a*x + 5)
+            sage: w._poly_rep()
+            x
+            sage: P = W(5)._poly_rep(); P
+            5
+
+        The coefficients of P are floating point p-adics::
+
+            sage: ring = P.parent().base_ring() 
+            sage: ring
+            5-adic Unramified Extension Ring in a defined by x^3 + 3*x + 3
+            sage: ring._prec_type()
+            'floating-point'
+        """
         return self.value

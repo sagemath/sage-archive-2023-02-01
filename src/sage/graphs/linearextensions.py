80a5766d6d2c288d003ab520b45ddcd147ff8fad
Martin Rubey <martin.rubey@tuwien.ac.at>
2018-07-24 15:02:38 +0200

    remove CombinatorialClass, prepend underscores to internal methods and variables, etc.

--- src/sage/graphs/linearextensions.py
+++ src/sage/graphs/linearextensions.py
@@ -50,14 +50,11 @@ the graph, the linear extensions are known as topological sorts ::
 # Distributed  under  the  terms  of  the  GNU  General  Public  License (GPL)
 #                         http://www.gnu.org/licenses/
 #*****************************************************************************
-from __future__ import print_function
-
-import sys
 from copy import copy
-from sage.combinat.combinat import CombinatorialClass
+from sage.structure.parent import Parent
+from sage.categories.finite_enumerated_sets import FiniteEnumeratedSets
 
-
-class LinearExtensions(CombinatorialClass):
+class LinearExtensions(Parent):
     def __init__(self, dag):
         r"""
         Creates an object representing the class of all linear extensions
@@ -77,15 +74,27 @@ class LinearExtensions(CombinatorialClass):
             [[]]
 
         """
-        self.dag = dag
-        self._name = "Linear extensions of %s"%dag
+        self._dag = copy(dag)
+        Parent.__init__(self, category = FiniteEnumeratedSets())
+
+    def _repr_(self):
+        """
+        TESTS::
+
+            sage: from sage.graphs.linearextensions import LinearExtensions
+            sage: D = DiGraph({ 0:[1,2], 1:[3], 2:[3,4] })
+            sage: LinearExtensions(D)
+            Linear extensions of Digraph on 5 vertices
+
+        """
+        return "Linear extensions of %s"%self._dag
 
     def _prepare(self):
         r"""
         The preprocessing routine in Figure 7 of "Generating Linear
         Extensions Fast" by Preusse and Ruskey.
 
-        This is an in-place algorithm and the list self.le keeps track
+        This is an in-place algorithm and the list self._le keeps track
         of the current linear extensions.  The boolean variable self.is_plus
         keeps track of the "sign".
 
@@ -98,20 +107,17 @@ class LinearExtensions(CombinatorialClass):
             ValueError: The digraph must be acyclic to have linear extensions.
 
         """
-        dag_copy = copy(self.dag)
+        dag_copy = copy(self._dag)
         le = []
-        a  = []
-        b  = []
+        a = []
+        b = []
 
-        #The preprocessing routine found in Figure 7 of
-        #"Generating Linear Extensions Fast" by
-        #Pruesse and Ruskey
+        # the preprocessing routine found in Figure 7 of
+        # "Generating Linear Extensions Fast" by
+        # Pruesse and Ruskey
         while dag_copy.num_verts() != 0:
-            #Find all the minimal elements of dag_copy
-            minimal_elements = []
-            for node in dag_copy.vertices():
-                if len(dag_copy.incoming_edges(node)) == 0:
-                    minimal_elements.append(node)
+            # find all the minimal elements of dag_copy
+            minimal_elements = dag_copy.sources()
             if not minimal_elements:
                 raise ValueError("The digraph must be acyclic to have linear extensions.")
             elif len(minimal_elements) == 1:
@@ -126,22 +132,20 @@ class LinearExtensions(CombinatorialClass):
                 le.append(bp)
                 dag_copy.delete_vertex(ap)
                 dag_copy.delete_vertex(bp)
-        self.max_pair = len(a) - 1
 
-        self.le = le
-        self.a  = a
-        self.b  = b
-        self.mrb = 0
-        self.mra = 0
-        self.is_plus = True
+        self._max_pair = len(a) - 1
+        self._le = le
+        self._a = a
+        self._b = b
+        self._is_plus = True
 
-    def switch(self, i):
+    def _switch(self, i):
         """
         This implements the Switch procedure described on page 7
         of "Generating Linear Extensions Fast" by Pruesse and Ruskey.
 
-        If i == -1, then the sign is changed.  If i > 0, then self.a[i]
-        and self.b[i] are transposed.
+        If i == -1, then the sign is changed.  If i > 0, then self._a[i]
+        and self._b[i] are transposed.
 
         Note that this meant to be called by the generate_linear_extensions
         method and is not meant to be used directly.
@@ -152,42 +156,42 @@ class LinearExtensions(CombinatorialClass):
             sage: D = DiGraph({ 0:[1,2], 1:[3], 2:[3,4] })
             sage: l = LinearExtensions(D)
             sage: _ = l.list()
-            sage: l.le = [0, 1, 2, 3, 4]
-            sage: l.is_plus
+            sage: l._le = [0, 1, 2, 3, 4]
+            sage: l._is_plus
             True
-            sage: [e for e in l.switch(-1)]
+            sage: [e for e in l._switch(-1)]
             []
-            sage: l.is_plus
+            sage: l._is_plus
             False
-            sage: l.a
+            sage: l._a
             [1, 4]
-            sage: l.b
+            sage: l._b
             [2, 3]
-            sage: [e for e in l.switch(0)]
+            sage: [e for e in l._switch(0)]
             []
-            sage: l.le
+            sage: l._le
             [0, 2, 1, 3, 4]
-            sage: l.a
+            sage: l._a
             [2, 4]
-            sage: l.b
+            sage: l._b
             [1, 3]
 
         """
         if i == -1:
-            self.is_plus = not self.is_plus
+            self._is_plus = not self._is_plus
         if i >= 0:
-            a_index = self.le.index(self.a[i])
-            b_index = self.le.index(self.b[i])
-            self.le[a_index] = self.b[i]
-            self.le[b_index] = self.a[i]
+            a, b = self._a[i], self._b[i]
+            a_index = self._le.index(a)
+            b_index = self._le.index(b)
+            self._le[a_index] = b
+            self._le[b_index] = a
 
-            self.b[i], self.a[i] = self.a[i], self.b[i]
+            self._b[i], self._a[i] = a, b
 
-        if self.is_plus:
-            yield self.le[:]
+        if self._is_plus:
+            yield self._le[:]
 
-
-    def move(self, element, direction):
+    def _move(self, element, direction):
         """
         This implements the Move procedure described on page 7
         of "Generating Linear Extensions Fast" by Pruesse and Ruskey.
@@ -205,39 +209,38 @@ class LinearExtensions(CombinatorialClass):
             sage: D = DiGraph({ 0:[1,2], 1:[3], 2:[3,4] })
             sage: l = LinearExtensions(D)
             sage: _ = l.list()
-            sage: l.le = [0, 1, 2, 3, 4]
-            sage: [e for e in l.move(1, "left")]
+            sage: l._le = [0, 1, 2, 3, 4]
+            sage: [e for e in l._move(1, "left")]
             [[1, 0, 2, 3, 4]]
-            sage: l.le
+            sage: l._le
             [1, 0, 2, 3, 4]
-            sage: [e for e in l.move(1, "right")]
+            sage: [e for e in l._move(1, "right")]
             [[0, 1, 2, 3, 4]]
-            sage: l.le
+            sage: l._le
             [0, 1, 2, 3, 4]
 
         """
-        index = self.le.index(element)
+        index = self._le.index(element)
         if direction == "right":
-            self.le[index] = self.le[index+1]
-            self.le[index+1] = element
+            self._le[index] = self._le[index+1]
+            self._le[index+1] = element
         elif direction == "left":
-            self.le[index] = self.le[index-1]
-            self.le[index-1] = element
+            self._le[index] = self._le[index-1]
+            self._le[index-1] = element
         else:
-            print("Bad direction!")
-            sys.exit()
-        if self.is_plus:
-            yield self.le[:]
+            raise ValueError("bad direction, it should be 'right'or 'left'")
+        if self._is_plus:
+            yield self._le[:]
 
 
-    def right(self, i, letter):
+    def _right(self, i, letter):
         """
         If letter =="b", then this returns True if and only if
-        self.b[i] is incomparable with the elements to its right
-        in self.le.  If letter == "a", then it returns True if
-        and only if self.a[i] is incomparable with the element to its
-        right in self.le and the element to the right is not
-        self.b[i]
+        self._b[i] is incomparable with the elements to its right
+        in self._le.  If letter == "a", then it returns True if
+        and only if self._a[i] is incomparable with the element to its
+        right in self._le and the element to the right is not
+        self._b[i]
 
         This is the Right function described on page 8 of
         "Generating Linear Extensions Fast" by Pruesse and Ruskey.
@@ -251,41 +254,41 @@ class LinearExtensions(CombinatorialClass):
             sage: D = DiGraph({ 0:[1,2], 1:[3], 2:[3,4] })
             sage: l = LinearExtensions(D)
             sage: _ = l.list()
-            sage: l.le
+            sage: l._le
             [0, 1, 2, 4, 3]
-            sage: l.a
+            sage: l._a
             [1, 4]
-            sage: l.b
+            sage: l._b
             [2, 3]
-            sage: l.right(0, "a")
+            sage: l._right(0, "a")
             False
-            sage: l.right(1, "a")
+            sage: l._right(1, "a")
             False
-            sage: l.right(0, "b")
+            sage: l._right(0, "b")
             False
-            sage: l.right(1, "b")
+            sage: l._right(1, "b")
             False
 
         """
         if letter == "a":
-            x = self.a[i]
-            yindex = self.le.index(x) + 1
-            if yindex >= len(self.le):
+            x = self._a[i]
+            yindex = self._le.index(x) + 1
+            if yindex >= len(self._le):
                 return False
-            y = self.le[ yindex ]
-            return self.incomparable(x,y) and y != self.b[i]
+            y = self._le[yindex]
+            return y != self._b[i] and self._incomparable(x, y)
 
         elif letter == "b":
-            x = self.b[i]
-            yindex = self.le.index(x) + 1
-            if yindex >= len(self.le):
+            x = self._b[i]
+            yindex = self._le.index(x) + 1
+            if yindex >= len(self._le):
                 return False
-            y = self.le[ yindex ]
-            return self.incomparable(x,y)
+            y = self._le[yindex]
+            return self._incomparable(x, y)
         else:
-            raise ValueError("Bad letter!")
+            raise ValueError("bad letter, it should be 'a' or 'b'")
 
-    def generate_linear_extensions(self, i):
+    def _generate_linear_extensions(self, i):
         """
         This a Python version of the GenLE routine found in Figure 8
         of "Generating Linear Extensions Fast" by Pruesse and Ruskey.
@@ -299,59 +302,59 @@ class LinearExtensions(CombinatorialClass):
             sage: D = DiGraph({ 0:[1,2], 1:[3], 2:[3,4] })
             sage: l = LinearExtensions(D)
             sage: l._prepare()
-            sage: [e for e in l.generate_linear_extensions(l.max_pair)]
+            sage: [e for e in l._generate_linear_extensions(l._max_pair)]
             [[0, 2, 1, 3, 4]]
 
         """
         if i >= 0:
-            for e in self.generate_linear_extensions(i-1):
+            for e in self._generate_linear_extensions(i-1):
                 yield e
             mrb = 0
             typical = False
-            while self.right(i, "b"):
+            while self._right(i, "b"):
                 mrb += 1
-                for e in self.move(self.b[i], "right"):
+                for e in self._move(self._b[i], "right"):
                     yield e
-                for e in self.generate_linear_extensions(i-1):
+                for e in self._generate_linear_extensions(i-1):
                     yield e
                 mra = 0
-                if self.right(i, "a"):
+                if self._right(i, "a"):
                     typical = True
                     cont = True
                     while cont:
                         mra += 1
-                        for e in self.move(self.a[i], "right"):
+                        for e in self._move(self._a[i], "right"):
                             yield e
-                        for e in self.generate_linear_extensions(i-1):
+                        for e in self._generate_linear_extensions(i-1):
                             yield e
-                        cont = self.right(i, "a")
+                        cont = self._right(i, "a")
                 if typical:
-                    for e in self.switch(i-1):
+                    for e in self._switch(i-1):
                         yield e
-                    for e in self.generate_linear_extensions(i-1):
+                    for e in self._generate_linear_extensions(i-1):
                         yield e
                     if mrb % 2 == 1:
                         mla = mra -1
                     else:
                         mla = mra + 1
-                    for x in range(mla):
-                        for e in self.move(self.a[i], "left"):
+                    for _ in range(mla):
+                        for e in self._move(self._a[i], "left"):
                             yield e
-                        for e in self.generate_linear_extensions(i-1):
+                        for e in self._generate_linear_extensions(i-1):
                             yield e
 
             if typical and (mrb % 2 == 1):
-                for e in self.move(self.a[i], "left"):
+                for e in self._move(self._a[i], "left"):
                     yield e
             else:
-                for e in self.switch(i-1):
+                for e in self._switch(i-1):
                     yield e
-            for e in self.generate_linear_extensions(i-1):
+            for e in self._generate_linear_extensions(i-1):
                 yield e
-            for x in range(mrb):
-                for e in self.move(self.b[i], "left"):
+            for _ in range(mrb):
+                for e in self._move(self._b[i], "left"):
                     yield e
-                for e in self.generate_linear_extensions(i-1):
+                for e in self._generate_linear_extensions(i-1):
                     yield e
 
     def __iter__(self):
@@ -370,16 +373,15 @@ class LinearExtensions(CombinatorialClass):
              [0, 2, 4, 1, 3]]
         """
         self._prepare()
-        yield self.le[:]
-        for e in self.generate_linear_extensions(self.max_pair):
+        yield self._le[:]
+        for e in self._generate_linear_extensions(self._max_pair):
             yield e
-        for e in self.switch(self.max_pair):
+        for e in self._switch(self._max_pair):
             yield e
-        for e in self.generate_linear_extensions(self.max_pair):
+        for e in self._generate_linear_extensions(self._max_pair):
             yield e
 
-
-    def incomparable(self, x, y):
+    def _incomparable(self, x, y):
         """
         Returns True if vertices x and y are incomparable in the directed
         acyclic graph when thought of as a poset.
@@ -388,10 +390,10 @@ class LinearExtensions(CombinatorialClass):
 
             sage: from sage.graphs.linearextensions import LinearExtensions
             sage: D = DiGraph({ 0:[1,2], 1:[3], 2:[3,4] })
-            sage: l = LinearExtensions(D)
-            sage: l.incomparable(0,1)
+            sage: l = LinearExtensions(D); l._prepare()
+            sage: l._incomparable(0,1)
             False
-            sage: l.incomparable(1,2)
+            sage: l._incomparable(1,2)
             True
         """
-        return (not self.dag.shortest_path(x, y)) and (not self.dag.shortest_path(y, x))
+        return (not self._dag.shortest_path(x, y)) and (not self._dag.shortest_path(y, x))

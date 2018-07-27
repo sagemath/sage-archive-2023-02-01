3d2f309cbe680bf1004fa83f363e06aca68be00b
Ben Salisbury <ben.salisbury@cmich.edu>
2018-07-27 15:55:49 -0400

    git added all.py and __init__.py

---
+++ src/sage/combinat/pathtableau/__init__.py
@@ -0,0 +1,7 @@
+r"""
+Path Tableaux
+=============
+
+- :ref:`sage.combinat.pathtableau.pathtableaux`
+- :ref:`sage.combinat.pathtableau.catalan`
+"""
---
+++ src/sage/combinat/pathtableau/all.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+from .pathtableaux import PathTableau, PathTableaux
+from .catalan import CatalanTableau, CatalanTableaux

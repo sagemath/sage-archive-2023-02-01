4a6ff4e6c396231908bd58758e46a9f0bb24dae2
John H. Palmieri <jhpalmieri64@gmail.com>
2018-07-30 15:16:47 -0700

    trac 25978: add comment explaining the change.

--- src/sage/rings/polynomial/multi_polynomial_ring_base.pyx
+++ src/sage/rings/polynomial/multi_polynomial_ring_base.pyx
@@ -938,6 +938,9 @@ cdef class MPolynomialRing_base(sage.rings.ring.CommutativeRing):
 
         counts, total = self._precomp_counts(n, degree)
 
+        # Note that 'terms' could be None while 'total' is a
+        # nonnegative integer, so the comparison 'terms > total' could
+        # fail in Python 3.
         if terms and terms > total:
             terms = total
 

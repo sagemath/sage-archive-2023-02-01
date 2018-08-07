40c91b3e077c8f623f34e59295bd8bacdf424fc4
Erik M. Bray <erik.bray@lri.fr>
2018-08-07 11:29:43 +0200

    rewrite this test to not depend on relatively arbitrary bounds
    instead test the expected semantics--that there are (as expected) fewer
    matches for 'tree' with whole_word=True, and that all the returned lines
    actually contain [tT]ree as a whole word match (using a simple regexp to
    check)

--- src/sage/misc/sagedoc.py
+++ src/sage/misc/sagedoc.py
@@ -1171,9 +1171,13 @@ def search_doc(string, extra1='', extra2='', extra3='', extra4='',
     counting the length of ``search_doc('tree',
     interact=False).splitlines()`` gives the number of matches. ::
 
-        sage: len(search_doc('tree', interact=False).splitlines()) > 4000  # optional - dochtml, long time
+        sage: N = len(search_doc('tree', interact=False).splitlines())  # optional - dochtml, long time
+        sage: L = search_doc('tree', whole_word=True, interact=False).splitlines()  # optional - dochtml, long time
+        sage: len(L) < N  # long time
         True
-        sage: len(search_doc('tree', whole_word=True, interact=False).splitlines()) < 2000  # optional - dochtml, long time
+        sage: import re
+        sage: tree_re = re.compile(r'(^|\W)tree(\W|$)', re.I)
+        sage: all(tree_re.search(l) for l in L)  # long time
         True
     """
     return _search_src_or_doc('doc', string, extra1=extra1, extra2=extra2,
@@ -1300,7 +1304,7 @@ def my_getsource(obj, oname=''):
     - ``oname`` -- str (optional). A name under which the object is
       known. Currently ignored by Sage.
 
-    OUTPUT: 
+    OUTPUT:
 
     Its documentation (string)
 

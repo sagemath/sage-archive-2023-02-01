eafa8b6e66fad59630d023ebdf5dc61af5f73dbe
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2018-08-19 17:23:56 +0200

    fix hash for 32bit

--- src/sage/schemes/projective/projective_point.py
+++ src/sage/schemes/projective/projective_point.py
@@ -364,7 +364,7 @@ class SchemeMorphism_point_projective_ring(SchemeMorphism_point):
 
     def __hash__(self):
         """
-        Computes the hash value of this point.
+        Compute the hash value of this point.
 
         If the base ring has a fraction field, normalize the point in
         the fraction field and then hash so that equal points have
@@ -400,7 +400,7 @@ class SchemeMorphism_point_projective_ring(SchemeMorphism_point):
 
             sage: P.<x,y> = ProjectiveSpace(Zmod(10), 1)
             sage: hash(P([2, 5]))
-            -479010389                     # 32-bit
+            -2047536788                    # 32-bit
             3339499987291935084            # 64-bit
         """
         R = self.codomain().base_ring()
@@ -414,7 +414,7 @@ class SchemeMorphism_point_projective_ring(SchemeMorphism_point):
         #a constant value
         return hash(self.codomain())
 
-    def scale_by(self,t):
+    def scale_by(self, t):
         """
         Scale the coordinates of the point by ``t``.
 

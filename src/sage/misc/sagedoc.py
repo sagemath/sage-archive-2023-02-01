691cca71282ee91e92d5d07bd49deaa447f03d10
John H. Palmieri <jhpalmieri64@gmail.com>
2018-08-27 10:36:34 -0700

    trac 26017: fix optional tags on doctests

--- src/sage/misc/sagedoc.py
+++ src/sage/misc/sagedoc.py
@@ -1173,11 +1173,11 @@ def search_doc(string, extra1='', extra2='', extra3='', extra4='',
 
         sage: N = len(search_doc('tree', interact=False).splitlines())  # optional - dochtml, long time
         sage: L = search_doc('tree', whole_word=True, interact=False).splitlines()  # optional - dochtml, long time
-        sage: len(L) < N  # long time
+        sage: len(L) < N  # optional - dochtml, long time
         True
         sage: import re
         sage: tree_re = re.compile(r'(^|\W)tree(\W|$)', re.I)
-        sage: all(tree_re.search(l) for l in L)  # long time
+        sage: all(tree_re.search(l) for l in L) # optional - dochtml, long time
         True
     """
     return _search_src_or_doc('doc', string, extra1=extra1, extra2=extra2,

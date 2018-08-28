66c72ec028bf708c094371ef6ad4ae2f79d6f516
raghukul01 <raghukul.raman01@gmail.com>
2018-08-28 21:12:11 +0530

    25692: Added type check for infinite permutation group

--- src/sage/groups/additive_abelian/additive_abelian_group.py
+++ src/sage/groups/additive_abelian/additive_abelian_group.py
@@ -452,7 +452,20 @@ class AdditiveAbelianGroup_fixed_gens(AdditiveAbelianGroup_class):
             sage: G = AdditiveAbelianGroup([2, 3])
             sage: G.permutation_group()
             Permutation Group with generators [(3,4,5), (1,2)]
+
+        TESTS:
+
+        Check that :trac:`25692` is fixed::
+
+            sage: G = AdditiveAbelianGroup([0])
+            sage: G.permutation_group()
+            Traceback (most recent call last):
+            ...
+            TypeError: Additive Abelian group must be finite
         """
+        # GAP does not support infinite permutation groups
+        if not self.is_finite(): 
+            raise TypeError('Additive Abelian group must be finite')
         from sage.groups.perm_gps.permgroup import PermutationGroup
         s = 'Image(IsomorphismPermGroup(AbelianGroup(%s)))'%(list(self.invariants()),)
         return PermutationGroup(gap_group=s)

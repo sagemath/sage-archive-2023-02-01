f03e4766fa8b6ad78f7f054e32f6be65f3a518c1
Bruce Westbury <bruce.westbury@gmail.com>
2018-08-31 14:28:43 +0100

    Ref

--- src/sage/combinat/pathtableau/pathtableaux.py
+++ src/sage/combinat/pathtableau/pathtableaux.py
@@ -15,9 +15,9 @@ by jeu-de-taquin on rectangular tableaux but in general they are different.
 
 REFERENCES:
 
-    .. [Wes2017] Bruce Westbury.
-    *Coboundary categories and local rules*,
-    :arxiv:'1705.07141'
+.. [Wes2017] Bruce Westbury.
+*Coboundary categories and local rules*,
+:arxiv:'1705.07141'
 
 AUTHORS:
 

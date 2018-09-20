8f958cfb2e583cec9e990564d240e97656997a17
Aaron Lauve <lauve@math.luc.edu>
2018-09-20 16:09:33 -0500

    Revert "modify getitem to allow for use of packed words as indices"
    This reverts commit 481b0f0c46b68e9f6d8c09044dc1755661ff891e.

--- src/sage/combinat/chas/wqsym.py
+++ src/sage/combinat/chas/wqsym.py
@@ -45,7 +45,7 @@ from sage.structure.global_options import GlobalOptions
 from sage.categories.hopf_algebras import HopfAlgebras
 from sage.categories.realizations import Category_realization_of_parent
 from sage.combinat.free_module import CombinatorialFreeModule
-from sage.combinat.set_partition_ordered import OrderedSetPartition, OrderedSetPartitions
+from sage.combinat.set_partition_ordered import OrderedSetPartitions
 from sage.combinat.shuffle import ShuffleProduct_overlapping, ShuffleProduct
 
 class WQSymBasis_abstract(CombinatorialFreeModule, BindableClass):
@@ -1902,10 +1902,9 @@ class WQSymBases(Category_realization_of_parent):
                 M[{2}, {1, 4}, {3, 5}]
             """
             try:
-                indx = OrderedSetPartition(p)
+                return self.monomial(self._indices(p))
             except TypeError:
-                indx = OrderedSetPartition([p])
-            return self.monomial(self._indices(indx))
+                return self.monomial(self._indices([p]))
 
         def is_field(self, proof=True):
             """

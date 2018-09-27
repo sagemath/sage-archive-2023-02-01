eec759470e1f089eb05b130790ce8e9864fd4304
Eero Hakavuori <eero.hakavuori@gmail.com>
2018-09-27 09:36:36 +0300

    trac #26352: Lie subalgebra leading term ordering handling fix

--- src/sage/algebras/lie_algebras/quotient.py
+++ src/sage/algebras/lie_algebras/quotient.py
@@ -119,7 +119,7 @@ class LieQuotient_finite_dimensional_with_basis(LieAlgebraWithStructureCoefficie
         Defining Y_1, Y_2, Y_3, Y_4, Y_5
         sage: lcs = Q.lower_central_series()
         sage: [I.basis().list() for I in lcs]
-        [[Y_1, Y_2, Y_3, Y_4, Y_5], [Y_3, Y_4, Y_5], [Y_4, Y_5], [Y_5], []]
+        [[Y_1, Y_2, Y_3, Y_4, Y_5], [Y_5, Y_4, Y_3], [Y_5, Y_4], [Y_5], []]
         sage: Y_2.bracket(Y_3)
         Y_5
 
@@ -152,6 +152,17 @@ class LieQuotient_finite_dimensional_with_basis(LieAlgebraWithStructureCoefficie
         ....:     lifts.append(X)
         sage: all([X.parent() is L for X, L in zip(lifts,quots)])
         True
+
+    Verify a quotient construction when the basis ordering and indices ordering
+    are different, see trac #26352::
+
+        sage: L.<c,b,a> = LieAlgebra(QQ, abelian=True)
+        sage: I2 = L.ideal([a+b, a+c])
+        sage: I2.basis()
+        Family (b + a, c + a)
+        sage: Q = L.quotient(I2)
+        sage: Q.basis()
+        Finite family {'a': a}
 
     A test suite::
 
@@ -201,7 +212,7 @@ class LieQuotient_finite_dimensional_with_basis(LieAlgebraWithStructureCoefficie
                                       "not implemented")
 
         # extract an index set from a complementary basis to the ideal
-        I_supp = [I.lift(X).leading_support() for X in I.basis()]
+        I_supp = [X.leading_support() for X in I.leading_monomials()]
         inv = ambient.basis().inverse_family()
         sorted_indices = [inv[X] for X in ambient.basis()]
         index_set = [i for i in sorted_indices if i not in I_supp]

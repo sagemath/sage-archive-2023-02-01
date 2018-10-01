11dc3f1d69671d5c61fd1493246f96bab99c26ec
Jesper Noordsij <jesper.noordsij@gmail.com>
2018-10-01 12:15:17 +0200

    Modified imports for new file location.

---
+++ src/sage/rings/invariants/all.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from sage.misc.lazy_import import lazy_import
+lazy_import('sage.rings.invariants.invariant_theory', 'invariant_theory')
+lazy_import('sage.rings.invariants.reconstruction', 'binary_form_from_invariants')

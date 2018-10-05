1e243e762a84a60cace49079c6fb13375e4d6f86
Dima Pasechnik <dimpase@gmail.com>
2018-10-05 11:11:39 +0100

    fix formatting to be able to build docs

--- src/sage/graphs/generators/families.py
+++ src/sage/graphs/generators/families.py
@@ -129,45 +129,43 @@ def KneserGraph(n,k):
     return g
 
 from sage.graphs.graph import Graph
-  
+
 def FurerGadget(k, prefix=None):
     r"""
     Return a Furer gadget of order ``k`` and their coloring.
-    
-    Construct the Furer gadget described for the first time in 
-    [CFI1992]_, a graph composed by a middle layer of `2^(k-1)` nodes
-    and two sets of nodes `(a_0, ... , a_{k-1})` and 
+
+    Construct the Furer gadget described in [CFI1992]_,
+    a graph composed by a middle layer of `2^(k-1)` nodes
+    and two sets of nodes `(a_0, ... , a_{k-1})` and
     `(b_0, ... , b_{k-1})`.
     Each node in the middle is connected to either `a_i` or `b_i`,
     for each i in [0,k[.
-    To read about the complete construction, see the paper mentioned
-    above.
+    To read about the complete construction, see [CFI1992]_.
     The returned coloring colors the middle section with one color, and
     then each pair `(a_i, b_i)` with another color.
     Since this method is mainly used to create Furer gadgets for the
     Cai-Furer-Immerman construction, returning gadgets that don't
     always have the same vertex labels is important, that's why there is
-    a parameter to manually set a prefix to be appended to each vertex 
-    label.
-    
+    a parameter to manually set a prefix to be appended to each vertex label.
+
     INPUT:
 
-    - ``k`` -- The order of the returned FurerGadget, greater than 0
+    - ``k``      -- The order of the returned Furer gadget, greater than 0.
 
-    - ``prefix`` -- Prefix of to be appended to each vertex label, 
+    - ``prefix`` -- Prefix of to be appended to each vertex label,
                     so as to individualise the returned Furer gadget.
                     Must be comparable for equality and hashable.
 
     OUTPUT:
-    
-    - ``G`` -- The Furer gadget of order ``k``
-    
-    - ``coloring`` -- A list of list of vertices, representing the 
-                      partition induced by the coloring of ``G``'s 
+
+    - ``G``        -- The Furer gadget of order ``k``
+
+    - ``coloring`` -- A list of list of vertices, representing the
+                      partition induced by the coloring of ``G``'s
                       vertices
-                      
+
     EXAMPLES:
-    
+
     Furer gadget of order 3, without any prefix. ::
         sage: G, p = graphs.FurerGadget(3)
         sage: G.vertices()
@@ -180,9 +178,9 @@ def FurerGadget(k, prefix=None):
          ((0, 2), (0, 'a'), None), ((0, 2), (1, 'b'), None),
          ((0, 2), (2, 'a'), None), ((0, 'b'), (1, 2), None),
          ((1, 2), (1, 'a'), None), ((1, 2), (2, 'a'), None)]
-    
+
     Furer gadget of order 3, with a prefix. ::
-        
+
         sage: G, p = graphs.FurerGadget(3, 'Prefix')
         sage: G.vertices()
         [('Prefix', ()), ('Prefix', (0, 1)), ('Prefix', (0, 2)),
@@ -236,48 +234,48 @@ def CaiFurerImmermanGraph(G, twisted=False):
     one, and a partition of its nodes.
 
     A Cai-Furer-Immerman graph from/on `G` is a graph created by
-    applying the transformation described in [CFI1992]_ on a graph 
+    applying the transformation described in [CFI1992]_ on a graph
     `G`, that is substituting every vertex v in `G` with a
-    Furer gadget `F(v)` of order d equal to the degree of the vertex, 
-    and then substituting every edge `(v,u)` in `G` 
-    with a pair of edges, one connecting the two "a" nodes of 
+    Furer gadget `F(v)` of order d equal to the degree of the vertex,
+    and then substituting every edge `(v,u)` in `G`
+    with a pair of edges, one connecting the two "a" nodes of
     `F(v)` and `F(u)` and the other their two "b" nodes.
-    The returned coloring of the vertices is made by the union of the 
-    colorings of each single Furer gadget, individualised for each 
+    The returned coloring of the vertices is made by the union of the
+    colorings of each single Furer gadget, individualised for each
     vertex of `G`.
-    To understand better what these "a" and "b" nodes are, see the 
+    To understand better what these "a" and "b" nodes are, see the
     documentation on  Furer gadgets.
-    
-    Furthermore, this method can apply what is described in the paper 
+
+    Furthermore, this method can apply what is described in the paper
     mentioned above as a "twist" on an edge, that is taking only one of
     the pairs of edges introduced in the new graph and swap two of their
-    extremes, making each edge go from an "a" node to a "b" node. 
+    extremes, making each edge go from an "a" node to a "b" node.
     This is only doable if the original graph G is connected.
-    
-    A CaiFurerImmerman graph on a graph with no balanced vertex 
+
+    A CaiFurerImmerman graph on a graph with no balanced vertex
     separators smaller than s and its twisted version
     cannot be distinguished by k-WL for any k < s.
 
     INPUT:
 
-    - ``G`` -- An undirected graph on which to construct the 
-               Cai-Furer-Immerman graph
+    - ``G``       -- An undirected graph on which to construct the
+                     Cai-Furer-Immerman graph
 
-    - ``twisted`` -- A boolean indicating if the version to construct 
+    - ``twisted`` -- A boolean indicating if the version to construct
                      is a twisted one or not
-    
+
     OUTPUT:
-    
-    - ``H`` -- The Cai-Furer-Immerman graph on ``G``
-    
-    - ``coloring`` -- A list of list of vertices, representing the 
+
+    - ``H``        -- The Cai-Furer-Immerman graph on ``G``
+
+    - ``coloring`` -- A list of list of vertices, representing the
                       partition induced by the coloring on ``H``
-    
+
     EXAMPLES:
-    
+
     CaiFurerImmerman graph with no balanced vertex separator smaller
     than 2 ::
-    
+
         sage: G = graphs.CycleGraph(4)
         sage: CFI, p = graphs.CaiFurerImmermanGraph(G)
         sage: CFI.vertices()
@@ -346,27 +344,27 @@ def CaiFurerImmermanGraph(G, twisted=False):
         s = ""
     newG.name("CaiFurerImmerman" + s + " graph constructed from a " + G.name())
     return newG, total_partition
-            
-  
+
+
 def EgawaGraph(p, s):
     r"""
     Return the Egawa graph with parameters `p`, `s`.
 
-    Egawa graphs are a peculiar family of graphs devised by Yoshimi 
+    Egawa graphs are a peculiar family of graphs devised by Yoshimi
     Egawa in  [Ega1981]_ .
-    The Shrikhande graph is a special case of this family of graphs, 
+    The Shrikhande graph is a special case of this family of graphs,
     with parameters `(1,0)`.
     All the graphs in this family are not recognizable by 1-WL
     (Weisfeiler Lehamn algorithm of the first order) and 2-WL, that is
     their orbits are not correctly returned by k-WL for k lower than 3.
-    
+
     Furthermore, all the graphs in this family are distance-regular, but
     they are not distance-transitive if `p \neq 0`.
-    
+
     The Egawa graph with parameters `(0, s)` is isomorphic to the
-    Hamming graph with parameters `(s, 4)`, when the underlying 
+    Hamming graph with parameters `(s, 4)`, when the underlying
     set of the Hamming graph is `[0,1,2,3]`
-    
+
     INPUT:
 
     - ``p`` -- power to which the graph named `Y` in the reference
@@ -374,9 +372,9 @@ def EgawaGraph(p, s):
 
     - ``s`` -- power to which the graph named `X` in the reference
                provided above will be raised
-    
+
     OUTPUT:
-    
+
     - ``G`` -- The Egawa graph with parameters (p,s)
 
     EXAMPLES:
@@ -420,15 +418,15 @@ def HammingGraph(n, q, X=None):
     r"""
     Returns the Hamming graph with parameters ``n``, ``q`` over ``X``.
 
-    Hamming graphs are graphs over the cartesian product of n copies 
-    of ``X``, where `q = |X|`, where the vertices, labelled with the 
+    Hamming graphs are graphs over the cartesian product of n copies
+    of ``X``, where `q = |X|`, where the vertices, labelled with the
     corresponding tuple in `X^n`, are connected if the Hamming distance
-    between their labels is 1. All Hamming graphs are regular, 
+    between their labels is 1. All Hamming graphs are regular,
     vertex-transitive and distance-regular.
-    
-    Hamming graphs with parameters `(1,q)` represent the complete graph 
+
+    Hamming graphs with parameters `(1,q)` represent the complete graph
     with q vertices over the set ``X``.
-    
+
     INPUT:
 
     - ``n`` -- power to which ``X`` will be raised to provide vertices
@@ -436,13 +434,13 @@ def HammingGraph(n, q, X=None):
 
     - ``q`` -- cardinality of ``X``
 
-    - ``X`` -- list of labels representing the vertices of the 
+    - ``X`` -- list of labels representing the vertices of the
                 underlying graph the Hamming graph will be based on; if
                 ``None`` (or left unused), the list `[0, ... , q-1]`
                 will be used
-    
+
     OUTPUT:
-    
+
     - ``G`` -- The Hamming graph with parameters `(n,q,X)`
 
     EXAMPLES:
@@ -453,7 +451,7 @@ def HammingGraph(n, q, X=None):
         sage: g = graphs.HammingGraph(3, 7)
         sage: g.is_distance_regular()
         True
-        sage: g.is_regular() 
+        sage: g.is_regular()
         True
         sage: g.is_vertex_transitive()
         True
@@ -464,18 +462,18 @@ def HammingGraph(n, q, X=None):
         sage: g = graphs.HammingGraph(1, 23)
         sage: g.is_isomorphic(graphs.CompleteGraph(23))
         True
-    
-    If a parameter ``q`` is provided which is not equal to ``X``'s 
+
+    If a parameter ``q`` is provided which is not equal to ``X``'s
     cardinality, an exception is raised. ::
-    
+
         sage: X = ['a','b','c','d','e']
         sage: g = graphs.HammingGraph(2, 3, X)
         Traceback (most recent call last):
         ...
         ValueError: q must be the cardinality of X
-        
+
     REFERENCES:
-    
+
     For a more accurate description, see the following wikipedia page:
     :wikipedia:`Hamming_graph`
     """
@@ -494,7 +492,8 @@ def HammingGraph(n, q, X=None):
                 if el == v[i]: continue
                 u = prefix + (el,) + suffix
                 g.add_edge(v,u)
-    return g   
+    return g
+
 def BalancedTree(r, h):
     r"""
     Returns the perfectly balanced tree of height `h \geq 1`,

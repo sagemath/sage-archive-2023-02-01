a8aa3ac0a3ed4da3ee58b2fa9bfd69cbd6f2d0d3
Travis Scrimshaw <tcscrims at gmail.com>
2018-10-08 09:42:19 +1000

    Reviewer changes and some bug fixes to manifolds catalog.

--- src/sage/manifolds/catalog.py
+++ src/sage/manifolds/catalog.py
@@ -1,15 +1,12 @@
 r"""
-Manifold Generator
+Manifolds Catalog
 
-the class :class:`ManifoldGenerator` implements some shortcuts to rapidly
-create various simple manifolds.
+A catalog of manifolds to rapidly create various simple manifolds.
 
 AUTHORS:
 
 - Florentin Jaffredo (2018) : initial version
-
 """
-
 
 # *****************************************************************************
 #  Copyright (C) 2018 Florentin Jaffredo <florentin.jaffredo@polytechnique.edu>
@@ -21,7 +18,9 @@ AUTHORS:
 #                  http://www.gnu.org/licenses/
 # *****************************************************************************
 
-def Minkowski(names=None, sgn=1):
+from sage.manifolds.differentiable.euclidean import EuclideanSpace as Euclidean
+
+def Minkowski(positive_spacelike=True, names=None):
     """
     Generate a Minkowski space of dimension 4.
 
@@ -32,44 +31,46 @@ def Minkowski(names=None, sgn=1):
 
     INPUT:
 
+    - ``positive_spacelike`` -- (default: ``True``) if ``False``, then
+      the spacelike vectors yield a negative sign (i.e., the signature
+      is `(+ - - - )`)
     - ``names`` -- (default: ``None``) name of the coordinates,
-      automatically set by the shortcut operator.
-    - ``sgn`` -- (default: ``1``) set to `-1` to switch sign convention. A
-      length 4 tuple is also accepted.
+      automatically set by the shortcut operator
 
     OUTPUT:
 
-    - pseudo-Riemannian manifold of dimension 4 with Minkowskian metric.
+    - pseudo-Riemannian manifold of dimension 4 with Minkowskian metric
 
     EXAMPLES::
 
-        sage: M.<t, x, y, z> = manifolds.Minkowski(sgn=(-1, 1, 1, 1))
+        sage: M.<t, x, y, z> = manifolds.Minkowski()
         sage: M.metric()[:]
         [-1  0  0  0]
         [ 0  1  0  0]
         [ 0  0  1  0]
         [ 0  0  0  1]
 
+        sage: M.<t, x, y, z> = manifolds.Minkowski(False)
+        sage: M.metric()[:]
+        [ 1  0  0  0]
+        [ 0 -1  0  0]
+        [ 0  0 -1  0]
+        [ 0  0  0 -1]
     """
     from sage.manifolds.manifold import Manifold
-    M = Manifold(4, 'M', structure = 'pseudo-Riemannian')
+    M = Manifold(4, 'M', structure='Lorentzian')
     if names is None:
         names = ("t", "x", "y", "z")
     C = M.chart(names=names)
     M._first_ngens = C._first_ngens
-    g = M.metric('g')
 
-    if hasattr(sgn, '__len__'):
-        if len(sgn)!=4:
-            raise ValueError("'sgn' must be a tuple of length 4")
-        for i in range(4):
-            g[i, i] = sgn[i]
-    else:
-        g[0,0] = -sgn
-        g[1,1], g[2,2], g[3,3] = sgn, sgn, sgn
+    g = M.metric('g')
+    sgn = 1 if positive_spacelike else -1
+    g[0,0] = -sgn
+    g[1,1], g[2,2], g[3,3] = sgn, sgn, sgn
     return M
 
-def Sphere(dim=2, radius=1, names=None, stereo2d=False, stereo_lim=None):
+def Sphere(dim=None, radius=1, names=None, stereo2d=False, stereo_lim=None):
     """
     Generate a sphere embedded in Euclidean space.
 
@@ -77,44 +78,61 @@ def Sphere(dim=2, radius=1, names=None, stereo2d=False, stereo_lim=None):
 
     INPUT:
 
-    - ``dim`` -- (default: ``2``) dimension of the sphere
+    - ``dim`` -- (optional) the dimension of the sphere; if not specified,
+      equals to the number of coordinate names
     - ``radius`` -- (default: ``1``) radius of the sphere
     - ``names`` -- (default: ``None``) name of the coordinates,
-      automatically set by the shortcut operator.
-    - ``stereo2d`` -- (default: ``False``) if True, defines only the
-      stereographic charts, only implemented in 2d.
-    - ``stereo_lim`` -- (default: ``None``) Parameter used to restrict the span
-      of the stereographic charts, so that they don't cover the whole sphere.
-      Valid domain will be `x**2 + y**2 < stereo_lim**2`.
-
+      automatically set by the shortcut operator
+    - ``stereo2d`` -- (default: ``False``) if ``True``, defines only the
+      stereographic charts, only implemented in 2d
+    - ``stereo_lim`` -- (default: ``None``) parameter used to restrict the
+      span of the stereographic charts, so that they don't cover the whole
+      sphere; valid domain will be ``x**2 + y**2 < stereo_lim**2``
 
     OUTPUT:
 
-    - Riemannian manifold.
+    - Riemannian manifold
 
     EXAMPLES::
 
         sage: S.<th, ph> = manifolds.Sphere()
-        sage: S.plot({},srange(-pi,pi*21/20,pi/20),
-        ....:   srange(-pi/2,pi/2*21/20,pi/20), viewer = 'threejs').show()
+        sage: S
+        2-dimensional pseudo-Riemannian submanifold S embedded in 
+         3-dimensional differentiable manifold E^3
+        sage: S.atlas()
+        [Chart (S, (th, ph))]
+        sage: S.metric().display()
+        gamma = dth*dth + sin(th)^2 dph*dph
 
+        sage: S = manifolds.Sphere(2, stereo2d=True)  # long time
+        sage: S  # long time
+        2-dimensional pseudo-Riemannian submanifold S embedded in
+         3-dimensional differentiable manifold E^3
+        sage: S.metric().display()  # long time
+        gamma = 4/(x^4 + y^4 + 2*(x^2 + 1)*y^2 + 2*x^2 + 1) dx*dx
+         + 4/(x^4 + y^4 + 2*(x^2 + 1)*y^2 + 2*x^2 + 1) dy*dy
     """
     from sage.manifolds.manifold import Manifold
+    from sage.manifolds.differentiable.euclidean import EuclideanSpace
     from functools import reduce
     from sage.functions.trig import cos, sin, atan, atan2
     from sage.functions.other import sqrt
     from sage.symbolic.constants import pi
-    import operator
+    from sage.misc.misc_c import prod
 
-    def prod(iterable):
-        return reduce(operator.mul, iterable, 1)
+    if dim is None:
+        if names is None:
+            raise ValueError("either the names or the dimension must be specified")
+        dim = len(names)
+    else:
+        if names is not None and dim != len(names):
+            raise ValueError("the number of coordinates does not match the dimension")
 
     if stereo2d:
-        if dim!=2:
+        if dim != 2:
             raise NotImplementedError("stereographic charts only "
                                       "implemented for 2d spheres")
-        xnames = ("X","Y","Z")
-        E = Euclidean(names=xnames)
+        E = Euclidean(names=("X", "Y", "Z"))
         S2 = Manifold(dim, 'S', ambient=E, structure='Riemannian')
         U = S2.open_subset('U')
         V = S2.open_subset('V')
@@ -123,8 +141,8 @@ def Sphere(dim=2, radius=1, names=None, stereo2d=False, stereo_lim=None):
         stereoS = V.chart(names=("xp", "yp"))
         xp, yp = stereoS[:]
         if stereo_lim is not None:
-            stereoN.add_restrictions(x**2+y**2<stereo_lim**2)
-            stereoS.add_restrictions(xp**2+yp**2<stereo_lim**2)
+            stereoN.add_restrictions(x**2+y**2 < stereo_lim**2)
+            stereoS.add_restrictions(xp**2+yp**2 < stereo_lim**2)
 
         stereoN_to_S = stereoN.transition_map(stereoS,
           (x / (x**2 + y**2), y / (x**2 + y**2)), intersection_name='W',
@@ -141,16 +159,15 @@ def Sphere(dim=2, radius=1, names=None, stereo2d=False, stereo_lim=None):
                                           stereoS_W: (yp != 0, xp < 0)})
         stereoN_A = stereoN_W.restrict(A)
         if names is None:
-            names = tuple(
-                ["phi_{}:(0,pi)".format(i) for i in range(dim - 1)] +
-                ["phi_{}:(-pi,pi)".format(dim - 1)])
+            names = tuple(["phi_{}:(0,pi)".format(i) for i in range(dim - 1)] +
+                          ["phi_{}:(-pi,pi)".format(dim - 1)])
         else:
             names = tuple([names[i] + ":(0,pi)" for i in range(dim - 1)] +
                           [names[dim - 1] + ":(-pi,pi)"])
         spher = A.chart(names=names)
         th, ph = spher[:]
-        spher_to_stereoN = spher.transition_map(stereoN_A, (sin(th)*cos(ph)
-                /(1-cos(th)), sin(th)*sin(ph)/(1-cos(th))))
+        spher_to_stereoN = spher.transition_map(stereoN_A, (sin(th)*cos(ph) / (1-cos(th)),
+                                                            sin(th)*sin(ph) / (1-cos(th))))
         spher_to_stereoN.set_inverse(2*atan(1/sqrt(x**2+y**2)), atan2(-y, -x)+pi)
         stereoN_to_S_A = stereoN_to_S.restrict(A)
         spher_to_stereoS = stereoN_to_S_A * spher_to_stereoN
@@ -168,37 +185,34 @@ def Sphere(dim=2, radius=1, names=None, stereo2d=False, stereo_lim=None):
 
         return S2
 
+    if dim != 2:
+        raise NotImplementedError("only implemented for 2 dimensional spheres")
 
-    if names is not None:
-        dim = len(names)
-
-    xnames = tuple(["x_{}".format(i) for i in range(dim+1)])
-    E = Euclidean(names=xnames)
+    E = EuclideanSpace(3, symbols='X Y Z')
     M = Manifold(dim, 'S', ambient=E, structure='Riemannian')
     if names is None:
-        names = tuple(
-            ["phi_{}:(0,pi)".format(i) for i in range(dim-1)] +
-            ["phi_{}:(-pi,pi)".format(dim-1)])
+        names = tuple(["phi_{}:(0,pi)".format(i) for i in range(dim-1)] +
+                      ["phi_{}:(-pi,pi)".format(dim-1)])
     else:
-        names = tuple([names[i]+":(0,pi)"for i in range(dim - 1)]+
+        names = tuple([names[i]+":(0,pi)"for i in range(dim - 1)] +
                       [names[dim-1]+":(-pi,pi)"])
     C = M.chart(names=names)
     M._first_ngens = C._first_ngens
     phi = M._first_ngens(dim)[:]
-    coordfunc = [radius * prod(sin(phi[j]) for j in range(dim))] +\
-                [radius * cos(phi[i]) * prod(sin(phi[j]) for j in range(i))
-                 for i in range(dim)]
+    coordfunc = ([radius * prod(sin(phi[j]) for j in range(dim))] +
+                 [radius * cos(phi[i]) * prod(sin(phi[j]) for j in range(i))
+                  for i in range(dim)])
     imm = M.diff_map(E, coordfunc)
     M.set_embedding(imm)
     M.induced_metric()
     return M
 
-def Kerr(m=1, a=0, names=None, coordinates="BL"):
+def Kerr(m=1, a=0, coordinates="BL", names=None):
     """
     Generate a Kerr spacetime.
 
     A Kerr spacetime is a 4 dimensional manifold describing a rotating black
-    hole. Two coordinates system are implemented : Boyer-Lindquist and ADM.
+    hole. Two coordinates system are implemented: Boyer-Lindquist and ADM.
     The first only has a single non-diagonal term in he metric, making its
     Ricci tensor faster to compute, but is divergent on the event horizon.
     The second is only divergent at the center.
@@ -207,23 +221,63 @@ def Kerr(m=1, a=0, names=None, coordinates="BL"):
 
     INPUT:
 
-    - ``m`` -- (default: ``1``) Mass of the black hole in natural units
+    - ``m`` -- (default: ``1``) mass of the black hole in natural units
       (`c=1`, `G=1`)
-    - ``a`` -- (default: ``0``) angular momentum in natural units. If set to
-      0, the resulting spacetime corresponds to a Schwarzschild black hole.
+    - ``a`` -- (default: ``0``) angular momentum in natural units; if set to
+      ``0``, the resulting spacetime corresponds to a Schwarzschild black hole
+    - ``coordinates`` -- (default: ``"BL"``) either ``"BL"`` or ``"ADM"``
     - ``names`` -- (default: ``None``) name of the coordinates,
-      automatically set by the shortcut operator.
-    - ``coordinates`` -- (default: ``"BL"``) either ``"BL"`` or ``"ADM"``.
+      automatically set by the shortcut operator
 
     OUTPUT:
 
-    - Lorentzian manifold.
+    - Lorentzian manifold
 
     EXAMPLES::
 
         sage: m, a = var('m, a')
         sage: K = manifolds.Kerr(m, a)
+        sage: K
+        4-dimensional Lorentzian manifold M
+        sage: K.atlas()
+        [Chart (M, (t, r, th, ph))]
+        sage: K.metric().display()
+        g = (2*m*r/(a^2*cos(th)^2 + r^2) - 1) dt*dt 
+         + 2*a*m*r*sin(th)^2/(a^2*cos(th)^2 + r^2) dt*dph 
+         + (a^2*cos(th)^2 + r^2)/(a^2 - 2*m*r + r^2) dr*dr 
+         + (a^2*cos(th)^2 + r^2) dth*dth 
+         + 2*a*m*r*sin(th)^2/(a^2*cos(th)^2 + r^2) dph*dt
+         + (2*a^2*m*r*sin(th)^2/(a^2*cos(th)^2 + r^2) + a^2 + r^2)*sin(th)^2 dph*dph
 
+        sage: K.<t, r, th, ph> = manifolds.Kerr()
+        sage: K
+        4-dimensional Lorentzian manifold M
+        sage: K.metric().display()
+        g = (2/r - 1) dt*dt + r^2/(r^2 - 2*r) dr*dr
+         + r^2 dth*dth + r^2*sin(th)^2 dph*dph
+        sage: K.default_chart().coord_range()
+        t: (-oo, +oo); r: (0, +oo); th: (0, pi); ph: (-pi, pi)
+
+        sage: m, a = var('m, a')
+        sage: K.<t, r, th, ph> = manifolds.Kerr(m, a, coordinates="ADM")
+        sage: K
+        4-dimensional Lorentzian manifold M
+        sage: K.atlas()
+        [Chart (M, (t, r, th, ph))]
+        sage: K.metric().display()
+        g = (2*m*r/(a^2*cos(th)^2 + r^2) - 1) dt*dt
+         + 2*m*r/(a^2*cos(th)^2 + r^2) dt*dr
+         - 2*a*m*r*sin(th)^2/(a^2*cos(th)^2 + r^2) dt*dph
+         + 2*m*r/(a^2*cos(th)^2 + r^2) dr*dt
+         + (2*m*r/(a^2*cos(th)^2 + r^2) + 1) dr*dr
+         - a*(2*m*r/(a^2*cos(th)^2 + r^2) + 1)*sin(th)^2 dr*dph
+         + (a^2*cos(th)^2 + r^2) dth*dth
+         - 2*a*m*r*sin(th)^2/(a^2*cos(th)^2 + r^2) dph*dt
+         - a*(2*m*r/(a^2*cos(th)^2 + r^2) + 1)*sin(th)^2 dph*dr
+         + (2*a^2*m*r*sin(th)^2/(a^2*cos(th)^2 + r^2)
+         + a^2 + r^2)*sin(th)^2 dph*dph
+        sage: K.default_chart().coord_range()
+        t: (-oo, +oo); r: (0, +oo); th: (0, pi); ph: (-pi, pi)
     """
     from sage.manifolds.manifold import Manifold
     from sage.functions.other import sqrt
@@ -232,6 +286,9 @@ def Kerr(m=1, a=0, names=None, coordinates="BL"):
     if coordinates == "ADM":
         if names is None:
             names = (r't:(-oo,+oo)', r'r:(0,+oo)', r'th:(0,pi):\theta', r'ph:(-pi,pi):\phi')
+        else:
+            names = (names[0]+r':(-oo,+oo)', names[1]+r':(0,+oo)',
+                     names[2]+r':(0,pi):\theta', names[3]+r':(-pi,pi):\phi')
         C = M.chart(names=names)
         M._first_ngens = C._first_ngens
         g = M.metric('g')
@@ -243,9 +300,13 @@ def Kerr(m=1, a=0, names=None, coordinates="BL"):
         g[0, 3] = -2*a*m*r/rho**2*sin(th)**2
         g[1, 3] = -a*sin(th)**2*(1+2*m*r/rho**2)
         return M
+
     if coordinates == "BL":
         if names is None:
             names = (r't:(-oo,+oo)', r'r:(0,+oo)', r'th:(0,pi):\theta', r'ph:(-pi,pi):\phi')
+        else:
+            names = (names[0]+r':(-oo,+oo)', names[1]+r':(0,+oo)',
+                     names[2]+r':(0,pi):\theta', names[3]+r':(-pi,pi):\phi')
         C = M.chart(names=names)
         M._first_ngens = C._first_ngens
         g = M.metric('g')
@@ -256,26 +317,9 @@ def Kerr(m=1, a=0, names=None, coordinates="BL"):
             (r**2+a**2+2*m*r*a**2/rho**2*sin(th)**2)*sin(th)**2
         g[0, 3] = 2*m*r*a*sin(th)**2/rho**2
         return M
-    raise NotImplementedError("Coordinates system not implemented, see help"
+
+    raise NotImplementedError("coordinates system not implemented, see help"
                               " for details")
-
-def Euclidean(*args, **kwargs):
-    """
-    Generate a Euclidean space.
-
-    Alias of :func:`~sage.manifolds.differentiable.euclidean.EuclideanSpace`.
-
-    OUTPUT:
-
-    - Riemannian manifold.
-
-    EXAMPLES::
-
-        sage: E.<x, y, z> = manifolds.Euclidean()
-
-    """
-    from sage.manifolds.differentiable.euclidean import EuclideanSpace
-    return EuclideanSpace(*args, **kwargs)
 
 def Torus(R=2, r=1, names=None):
     """
@@ -285,27 +329,37 @@ def Torus(R=2, r=1, names=None):
 
     INPUT:
 
-    - ``R`` -- (default: ``2``) Distance form the center to the center of
-      the tube
+    - ``R`` -- (default: ``2``) distance form the center to the
+      center of the tube
     - ``r`` -- (default: ``1``) radius of the tube
     - ``names`` -- (default: ``None``) name of the coordinates,
-      automatically set by the shortcut operator.
+      automatically set by the shortcut operator
 
     OUTPUT:
 
-    - Riemannian manifold.
+    - Riemannian manifold
 
     EXAMPLES::
 
-        sage: T.<theta, phi> = manifolds.Torus(3,1)
-        sage: T.plot({},srange(-pi,pi*21/20,pi/20),
-        ....:   srange(-pi,pi*21/20,pi/20), viewer = 'threejs').show()
-
+        sage: T.<theta, phi> = manifolds.Torus(3, 1)
+        sage: T
+        2-dimensional pseudo-Riemannian submanifold M embedded in 
+         3-dimensional differentiable manifold E^3
+        sage: T.atlas()
+        [Chart (M, (theta, phi))]
+        sage: T.embedding().display()
+        M --> E^3
+           (theta, phi) |--> (X, Y, Z) = ((cos(theta) + 3)*cos(phi), 
+                                          (cos(theta) + 3)*sin(phi), 
+                                          sin(theta))
+        sage: T.metric().display()
+        gamma = dtheta*dtheta + (cos(theta)^2 + 6*cos(theta) + 9) dphi*dphi
     """
     from sage.manifolds.manifold import Manifold
-    from sage.functions.all import cos, sin
-    xnames = tuple(["x_{}".format(i) for i in range(3)])
-    E = Euclidean(names=xnames)
+    from sage.functions.trig import cos, sin
+    from sage.manifolds.differentiable.euclidean import EuclideanSpace
+
+    E = EuclideanSpace(3, symbols='X Y Z')
     M = Manifold(2, 'M', ambient=E, structure="Riemannian")
     if names is None:
         names = ("th", "ph")

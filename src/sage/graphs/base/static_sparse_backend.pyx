b82d8ed9c195b7a786053d2730c79028fc560656
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2018-10-11 09:20:26 +0200

    trac 26447 sorting when possible

--- src/sage/graphs/base/static_sparse_backend.pyx
+++ src/sage/graphs/base/static_sparse_backend.pyx
@@ -431,6 +431,10 @@ cdef class StaticSparseBackend(CGraphBackend):
             True
         """
         vertices = list(G)
+        try:
+            vertices.sort()
+        except TypeError:
+            pass
         cdef StaticSparseCGraph cg = <StaticSparseCGraph> StaticSparseCGraph(G, vertices)
         self._cg = cg
 

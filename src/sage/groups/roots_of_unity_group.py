3b7b75c2ef435a44c4c10c4bfc0397850645d43a
Daniel Krenn <git@danielkrenn.at>
2018-10-28 19:44:30 +0100

    U: major restructuring of classes (factor out AbstractArgumentGroup)

--- src/sage/groups/roots_of_unity_group.py
+++ src/sage/groups/roots_of_unity_group.py
@@ -26,28 +26,30 @@ from sage.structure.unique_representation import UniqueRepresentation
 from sage.structure.richcmp import richcmp_by_eq_and_lt
 
 
-class UnitCirclePoint(MultiplicativeGroupElement):
+class AbstractArgument(MultiplicativeGroupElement):
     r"""
-    An element of :class:`UnitCircleGroup`
-    which is `e^{2\pi\mathit{exponent}}`.
+    An element of :class:`AbstractArgumentGroup`. This abstract class
+    encapsulates an element of the parent's base, i.e. it can be seen
+    as a wrapper class.
 
     INPUT:
 
     - ``parent`` -- a SageMath parent
 
-    - ``exponent`` -- a number (of a subset of the reals)
+    - ``element`` -- an element of parent's base
 
-    - ``normalize`` -- a boolean
+    - ``normalize`` -- a boolean (default: ``True``)
     """
-    def __init__(self, parent, exponent, normalize=True):
+
+    def __init__(self, parent, element, normalize=True):
         r"""
-        See :class:`UnitCirclePoint` for more information.
+        See :class:`AbstractArgument` for more information.
 
         TESTS::
 
             sage: from sage.groups.roots_of_unity_group import UnitCircleGroup, RootsOfUnityGroup
             sage: C = UnitCircleGroup(RR)
-            sage: C(exponent=1/2)
+            sage: C(exponent=1/2)  # indirect doctest
             e^(2*pi*0.500000000000000)
             sage: C(exponent=3/2)
             e^(2*pi*0.500000000000000)
@@ -62,29 +64,39 @@ class UnitCirclePoint(MultiplicativeGroupElement):
         """
         if parent is None:
             raise ValueError('parent must be provided')
-        super(UnitCirclePoint, self).__init__(parent=parent)
+        super(AbstractArgument, self).__init__(parent=parent)
 
-        exponent = parent.base()(exponent)
+        element = parent.base()(element)
         if normalize:
-            exponent = exponent - exponent.floor()
-        self._exponent_ = exponent
+            element = self._normalize_(element)
+        self._element_ = element
 
-    def _repr_(self):
+    @staticmethod
+    def _normalize_(element):
         r"""
-        Return a representation string of this point on the unit circle.
+        Normalizes the given element.
+
+        INPUT:
+
+        - ``element`` -- an element of the parent's base
+
+        OUTPUT:
+
+        An element.
 
         TESTS::
 
-            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
-            sage: C = UnitCircleGroup(RR)
-            sage: C(exponent=1/3)
-            e^(2*pi*0.333333333333333)
+            sage: from sage.groups.roots_of_unity_group import AbstractArgument
+            sage: AbstractArgument._normalize_(3/2)
+            Traceback (most recent call last):
+            ...
+            NotImplementedError: only implemented in concrete realizations
         """
-        return 'e^(2*pi*{})'.format(self._exponent_)
+        raise NotImplementedError('only implemented in concrete realizations')
 
     def __hash__(self):
         r"""
-        Return a hash value of this point on the unit circle.
+        Return a hash value of this argument.
 
         TESTS::
 
@@ -93,67 +105,13 @@ class UnitCirclePoint(MultiplicativeGroupElement):
             sage: hash(C(exponent=1/3))  # indirect doctest, random
             42
         """
-        return hash((self.parent(), self._exponent_))
-
-    def _mul_(self, other):
-        r"""
-        Return the product of this point on the unit circle and ``other``.
-
-        TESTS::
-
-            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
-            sage: C = UnitCircleGroup(RR)
-            sage: C(exponent=0.3) * C(exponent=0.4)
-            e^(2*pi*0.700000000000000)
-        """
-        P = self.parent()
-        return P.element_class(P, self._exponent_ + other._exponent_)
-
-    def __pow__(self, exponent):
-        r"""
-        Return the power of this point on the unit circle
-        to the given ``exponent``.
-
-        TESTS::
-
-            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup, RootsOfUnityGroup
-
-            sage: C = UnitCircleGroup(RR)
-            sage: C(exponent=0.1)^2
-            e^(2*pi*0.200000000000000)
-            sage: C(exponent=0.1)^QQ(2/1)
-            e^(2*pi*0.200000000000000)
-
-            sage: U = RootsOfUnityGroup()
-            sage: a = U(exponent=1/7); a
-            zeta7
-            sage: a^(7/3)
-            zeta3
-        """
-        P = self.parent()
-        return P.element_class(P, self._exponent_ * exponent)
-
-    def __invert__(self):
-        r"""
-        Return the inverse of this point on the unit circle.
-
-        TESTS::
-
-            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
-            sage: C = UnitCircleGroup(RR)
-            sage: ~C(exponent=0.4)
-            e^(2*pi*0.600000000000000)
-            sage: C(1) / C(exponent=0.4)
-            e^(2*pi*0.600000000000000)
-        """
-        P = self.parent()
-        return P.element_class(P, -self._exponent_)
+        return hash((self.parent(), self._element_))
 
     _richcmp_ = richcmp_by_eq_and_lt("_eq_", "_lt_")
 
     def _eq_(self, other):
         r"""
-        Return whether this point on the unit circle equals ``other``.
+        Return whether this argument equals ``other``.
 
         TESTS::
 
@@ -165,8 +123,21 @@ class UnitCirclePoint(MultiplicativeGroupElement):
             True
             sage: U(exponent=2/3) == U(exponent=-2/3)
             False
+
+        ::
+
+            sage: from sage.groups.roots_of_unity_group import ArgumentByElementGroup
+            sage: C = ArgumentByElementGroup(CC)
+            sage: C(I) == C(I)
+            True
+
+        As we do not have normalization in :class:`ArgumentByElement`,
+        then following, although equal, is not equal::
+
+            sage: C(I) == C(2*I)
+            False
         """
-        return self._exponent_ == other._exponent_
+        return self._element_ == other._element_
 
     def _lt_(self, other):
         r"""
@@ -193,67 +164,23 @@ class UnitCirclePoint(MultiplicativeGroupElement):
                            "for the roots of unity "
                            "{} and {}".format(self, other))
 
-    def is_one(self):
-        r"""
-        Return whether this point on the unit circle is `1`.
-
-        EXAMPLES:
-
-            sage: from sage.groups.roots_of_unity_group import UnitCircleGroug
-            sage: C = UnitCircleGroup(QQ)
-            sage: C(exponent=0).is_one()
-            True
-            sage: C(exponent=1/2).is_one()
-            False
-            sage: C(exponent=2/3).is_one()
-            False
-        """
-        return self._exponent_ == 0
-
-    def is_minus_one(self):
-        r"""
-        Return whether this point on the unit circle is `-1`.
-
-        EXAMPLES:
-
-            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
-            sage: C = UnitCircleGroup(QQ)
-            sage: C(exponent=0).is_minus_one()
-            False
-            sage: C(exponent=1/2).is_minus_one()
-            True
-            sage: C(exponent=2/3).is_minus_one()
-            False
-        """
-        from sage.rings.rational_field import QQ
-        return self._exponent_ == QQ(1)/QQ(2)
-
-
-class UnitCircleGroup(UniqueRepresentation, Parent):
+class AbstractArgumentGroup(UniqueRepresentation, Parent):
     r"""
-    A group for points on the unit circle.
+    A group whose elements represent (complex) arguments.
 
     INPUT:
 
-    - ``base`` -- a SageMath parent representing a subset of the reals
+    - ``base`` -- a SageMath parent
 
     - ``category`` -- a category
-
-    EXAMPLES::
-
-        sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
-        sage: UnitCircleGroup(RR)
-        Unit Circle Group with Exponents in Real Field with 53 bits of precision modulo ZZ
-        sage: UnitCircleGroup(QQ)
-        Unit Circle Group with Exponents in Rational Field modulo ZZ
     """
 
-    Element = UnitCirclePoint
+    Element = AbstractArgument
 
     @staticmethod
     def __classcall__(cls, base, category=None):
         r"""
-        See :class:`UnitCircleGroup` for more information.
+        See :class:`AbstractArgumentGroup` for more information.
 
         TESTS:
 
@@ -262,13 +189,13 @@ class UnitCircleGroup(UniqueRepresentation, Parent):
             Category of commutative groups
         """
         category = cls._determine_category_(category)
-        return super(UnitCircleGroup, cls).__classcall__(
+        return super(AbstractArgumentGroup, cls).__classcall__(
             cls, base, category)
 
     @staticmethod
     def _determine_category_(category):
         r"""
-        Return the category of this unit circle group.
+        Return the category of this argument group.
 
         INPUT:
 
@@ -292,7 +219,7 @@ class UnitCircleGroup(UniqueRepresentation, Parent):
 
     def __init__(self, base, category):
         r"""
-        See :class:`UnitCircleGroup` for more information.
+        See :class:`AbstractArgumentGroup` for more information.
 
         TESTS:
 
@@ -300,8 +227,213 @@ class UnitCircleGroup(UniqueRepresentation, Parent):
             sage: UnitCircleGroup(RR).base()  # indirect doctest
             Real Field with 53 bits of precision
         """
-        super(UnitCircleGroup, self).__init__(category=category,
+        super(AbstractArgumentGroup, self).__init__(category=category,
                                               base=base)
+
+    def __hash__(self):
+        r"""
+        Return a hash value of this argument group.
+
+        TESTS::
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+            sage: hash(UnitCircleGroup(RR))  # indirect doctest, random
+            42
+        """
+        return hash((self.__class__, self.base()))
+
+    def _an_element_(self):
+        r"""
+        Return an element of this argument group.
+
+        TESTS::
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+            sage: UnitCircleGroup(RR).an_element()  # indirect doctest
+            e^(2*pi*0.000000000000000)
+        """
+        return self.element_class(self, self.base().an_element())
+
+
+class UnitCirclePoint(AbstractArgument):
+    r"""
+    An element of :class:`UnitCircleGroup`
+    which is `e^{2\pi\mathit{exponent}}`.
+
+    INPUT:
+
+    - ``parent`` -- a SageMath parent
+
+    - ``exponent`` -- a number (of a subset of the reals)
+
+    - ``normalize`` -- a boolean (default: ``True``)
+    """
+
+    @staticmethod
+    def _normalize_(exponent):
+        r"""
+        Normalizes the given exponent so that it is in `[0,1)`.
+
+        INPUT:
+
+        - ``exponent`` -- an element of the parent's base
+
+        OUTPUT:
+
+        An element.
+
+        TESTS::
+
+            sage: from sage.groups.roots_of_unity_group import UnitCirclePoint
+            sage: UnitCirclePoint._normalize_(3/2)
+            1/2
+        """
+        return exponent - exponent.floor()
+
+    @property
+    def exponent(self):
+        r"""
+        The exponent of this point on the unit circle.
+
+        EXAMPLES:
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+            sage: C = UnitCircleGroup(RR)
+            sage: C(exponent=4/3).exponent
+            0.333333333333333
+        """
+        return self._element_
+
+    def _repr_(self):
+        r"""
+        Return a representation string of this point on the unit circle.
+
+        TESTS::
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+            sage: C = UnitCircleGroup(RR)
+            sage: C(exponent=1/3)
+            e^(2*pi*0.333333333333333)
+        """
+        return 'e^(2*pi*{})'.format(self.exponent)
+
+    def _mul_(self, other):
+        r"""
+        Return the product of this point on the unit circle and ``other``.
+
+        TESTS::
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+            sage: C = UnitCircleGroup(RR)
+            sage: C(exponent=0.3) * C(exponent=0.4)
+            e^(2*pi*0.700000000000000)
+        """
+        P = self.parent()
+        return P.element_class(P, self.exponent + other.exponent)
+
+    def __pow__(self, exponent):
+        r"""
+        Return the power of this point on the unit circle
+        to the given ``exponent``.
+
+        TESTS::
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup, RootsOfUnityGroup
+
+            sage: C = UnitCircleGroup(RR)
+            sage: C(exponent=0.1)^2
+            e^(2*pi*0.200000000000000)
+            sage: C(exponent=0.1)^QQ(2/1)
+            e^(2*pi*0.200000000000000)
+
+            sage: U = RootsOfUnityGroup()
+            sage: a = U(exponent=1/7); a
+            zeta7
+            sage: a^(7/3)
+            zeta3
+        """
+        P = self.parent()
+        return P.element_class(P, self.exponent * exponent)
+
+    def __invert__(self):
+        r"""
+        Return the inverse of this point on the unit circle.
+
+        TESTS::
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+            sage: C = UnitCircleGroup(RR)
+            sage: ~C(exponent=0.4)
+            e^(2*pi*0.600000000000000)
+            sage: C(1) / C(exponent=0.4)
+            e^(2*pi*0.600000000000000)
+        """
+        P = self.parent()
+        return P.element_class(P, -self.exponent)
+
+    def is_one(self):
+        r"""
+        Return whether this point on the unit circle is `1`.
+
+        EXAMPLES:
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+            sage: C = UnitCircleGroup(QQ)
+            sage: C(exponent=0).is_one()
+            True
+            sage: C(exponent=1/2).is_one()
+            False
+            sage: C(exponent=2/3).is_one()
+            False
+        """
+        return self.exponent == 0
+
+    def is_minus_one(self):
+        r"""
+        Return whether this point on the unit circle is `-1`.
+
+        EXAMPLES:
+
+            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+            sage: C = UnitCircleGroup(QQ)
+            sage: C(exponent=0).is_minus_one()
+            False
+            sage: C(exponent=1/2).is_minus_one()
+            True
+            sage: C(exponent=2/3).is_minus_one()
+            False
+        """
+        from sage.rings.rational_field import QQ
+        return self.exponent == QQ(1)/QQ(2)
+
+
+class UnitCircleGroup(AbstractArgumentGroup):
+    r"""
+    A group of points on the unit circle. These points are
+    represented by `e^{2\pi\mathit{exponent}}`.
+
+    INPUT:
+
+    - ``base`` -- a SageMath parent representing a subset of the reals
+
+    - ``category`` -- a category
+
+    EXAMPLES::
+
+        sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
+
+        sage: R = UnitCircleGroup(RR); R
+        Unit Circle Group with Exponents in Real Field with 53 bits of precision modulo ZZ
+        sage: R(exponent=2.42)
+        e^(2*pi*0.420000000000000)
+
+        sage: Q = UnitCircleGroup(QQ); Q
+        Unit Circle Group with Exponents in Rational Field modulo ZZ
+        sage: Q(exponent=6/5)
+        e^(2*pi*1/5)
+    """
+
+    Element = UnitCirclePoint
 
     def _repr_(self):
         r"""
@@ -330,30 +462,6 @@ class UnitCircleGroup(UniqueRepresentation, Parent):
         if ' ' in s:
             s = '({})'.format(s)
         return 'U_{}'.format(s)
-
-    def __hash__(self):
-        r"""
-        Return a hash value of this unit circle group.
-
-        TESTS::
-
-            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
-            sage: hash(UnitCircleGroup(RR))  # indirect doctest, random
-            42
-        """
-        return hash((self.__class__, self.base()))
-
-    def _an_element_(self):
-        r"""
-        Return an element of this unit circle group.
-
-        TESTS::
-
-            sage: from sage.groups.roots_of_unity_group import UnitCircleGroup
-            sage: UnitCircleGroup(RR).an_element()  # indirect doctest
-            e^(2*pi*0.000000000000000)
-        """
-        return self.element_class(self, self.base().an_element())
 
     def _element_constructor_(self, data, exponent=None):
         r"""
@@ -413,7 +521,7 @@ class UnitCircleGroup(UniqueRepresentation, Parent):
             elif isinstance(data, self.element_class):
                 if data.parent() == self:
                     return data
-                exponent = data._exponent_
+                exponent = data.exponent
 
             elif data == 1 or data == '1':
                 exponent = 0
@@ -467,7 +575,7 @@ class RootOfUnity(UnitCirclePoint):
             sage: a.exponent_numerator()
             2
         """
-        return self._exponent_.numerator()
+        return self.exponent.numerator()
 
     def exponent_denominator(self):
         r"""
@@ -483,7 +591,7 @@ class RootOfUnity(UnitCirclePoint):
             sage: a.exponent_denominator()
             3
         """
-        return self._exponent_.denominator()
+        return self.exponent.denominator()
 
     def _repr_(self):
         r"""
@@ -507,13 +615,13 @@ class RootOfUnity(UnitCirclePoint):
             zeta3^2
         """
         from sage.rings.rational_field import QQ
-        if self._exponent_ == 0:
+        if self.exponent == 0:
             return '1'
-        if self._exponent_ == QQ(1)/QQ(2):
+        if self.exponent == QQ(1)/QQ(2):
             return '-1'
-        if self._exponent_ == QQ(1)/QQ(4):
+        if self.exponent == QQ(1)/QQ(4):
             return 'I'
-        if self._exponent_ == QQ(3)/QQ(4):
+        if self.exponent == QQ(3)/QQ(4):
             return '-I'
         num = self.exponent_numerator()
         den = self.exponent_denominator()
@@ -532,6 +640,14 @@ class RootsOfUnityGroup(UnitCircleGroup):
     - ``category`` -- a category
 
     This is a specialized :class:`UnitCircleGroup` with base `\QQ`.
+
+    EXAMPLES::
+
+        sage: from sage.groups.roots_of_unity_group import RootsOfUnityGroup
+        sage: U = RootsOfUnityGroup(); U
+        Group of Roots of Unity
+        sage: U(exponent=1/4)
+        I
     """
 
     Element = RootOfUnity
@@ -548,7 +664,7 @@ class RootsOfUnityGroup(UnitCircleGroup):
             Category of commutative groups
         """
         category = cls._determine_category_(category)
-        return super(UnitCircleGroup, cls).__classcall__(
+        return super(AbstractArgumentGroup, cls).__classcall__(
             cls, category)
 
     def __init__(self, category):

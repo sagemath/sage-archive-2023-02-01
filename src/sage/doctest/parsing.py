724bc7188dcdb95388bf996b86733f18112a82d7
David Roe <roed.math@gmail.com>
2018-10-28 00:21:22 +0000

    Allow ... at beginning of doctest output

--- src/sage/doctest/parsing.py
+++ src/sage/doctest/parsing.py
@@ -42,6 +42,8 @@ float_regex = re.compile(r'\s*([+-]?\s*((\d*\.?\d+)|(\d+\.?))([eE][+-]?\d+)?)')
 optional_regex = re.compile(r'(py2|py3|long time|not implemented|not tested|known bug)|([^ a-z]\s*optional\s*[:-]*((\s|\w)*))')
 find_sage_prompt = re.compile(r"^(\s*)sage: ", re.M)
 find_sage_continuation = re.compile(r"^(\s*)\.\.\.\.:", re.M)
+python_prompt = re.compile(r"^(\s*)>>>", re.M)
+ellipsis_tag = "<ELLIPSIS_TAG>" # Used to allow ... at the beginning of output
 random_marker = re.compile('.*random', re.I)
 tolerance_pattern = re.compile(r'\b((?:abs(?:olute)?)|(?:rel(?:ative)?))? *?tol(?:erance)?\b( +[0-9.e+-]+)?')
 backslash_replacer = re.compile(r"""(\s*)sage:(.*)\\\ *
@@ -755,6 +757,16 @@ class SageDocTestParser(doctest.DocTestParser):
             4321
             sage: print(m)
             87654321
+
+        Test that :trac:`26575` is resolved::
+
+            sage: example3 = 'sage: Zp(5,4,print_mode="digits")(5)\n...00010'
+            sage: parsed3 = DTP.parse(example3)
+            sage: dte = parsed3[1]
+            sage: dte.sage_source
+            'Zp(5,4,print_mode="digits")(5)\n'
+            sage: dte.want
+            '...00010\n'
         """
         # Hack for non-standard backslash line escapes accepted by the current
         # doctest system.
@@ -769,6 +781,11 @@ class SageDocTestParser(doctest.DocTestParser):
             string = string[:m.start()] + g[0] + "sage:" + g[1] + future
             m = backslash_replacer.search(string,m.start())
 
+        replace_ellipsis = not python_prompt.search(string)
+        if replace_ellipsis:
+            # There are no >>> prompts, so we need to allow ... to begin the output
+            # We do so by replacing ellipses with a special tag, the putting them back after parsing
+            string = string.replace("...", ellipsis_tag)
         string = find_sage_prompt.sub(r"\1>>> sage: ", string)
         string = find_sage_continuation.sub(r"\1...", string)
         res = doctest.DocTestParser.parse(self, string, *args)
@@ -798,6 +815,8 @@ class SageDocTestParser(doctest.DocTestParser):
                 elif self.optional_only:
                     self.optionals['sage'] += 1
                     continue
+                if replace_ellipsis:
+                    item.want = item.want.replace(ellipsis_tag, "...")
                 item.want = parse_tolerance(item.source, item.want)
                 if item.source.startswith("sage: "):
                     item.sage_source = item.source[6:]

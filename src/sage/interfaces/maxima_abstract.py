b1dc567be39ef5d67b5b6e588937d803adcff84c
Antonio Rojas <arojas@archlinux.org>
2018-11-03 18:18:23 +0100

    Update maxima to 5.42.1

--- src/sage/interfaces/maxima_abstract.py
+++ src/sage/interfaces/maxima_abstract.py
@@ -168,7 +168,7 @@ class MaximaAbstract(ExtraTabCompletion, Interface):
             -- Function: gcd (<p_1>, <p_2>, <x_1>, ...)
             ...
         """
-        cmd = 'maxima --very-quiet -r "%s(%s);" '%(command, s)
+        cmd = 'maxima --very-quiet --batch-string="%s(%s);" '%(command, s)
         if sage.server.support.EMBEDDED_MODE:
             cmd += '< /dev/null'
 
@@ -178,9 +178,9 @@ class MaximaAbstract(ExtraTabCompletion, Interface):
         if redirect:
             res = bytes_to_str(subprocess.check_output(cmd, shell=True,
                                                        env=env))
-            # We get 4 lines of commented verbosity
-            # every time Maxima starts, so we need to get rid of them
-            for _ in range(4):
+            # We get 4 lines of commented verbosity every time Maxima starts
+            # and the input is echoed, so we need to get rid of them
+            for _ in range(5):
                 res = res[res.find('\n')+1:]
 
             return AsciiArtString(res)

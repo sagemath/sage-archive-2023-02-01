151ac1caa4956bc51db4c227c38b79585165a8ee
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2018-11-06 15:17:59 +0100

    py3: fix __repr__ of callable dict

--- src/sage/misc/callable_dict.pyx
+++ src/sage/misc/callable_dict.pyx
@@ -2,14 +2,15 @@
 """
 Callable dictionaries
 """
-#*****************************************************************************
+# ****************************************************************************
 #       Copyright (C) 2015 Nicolas M. Thiéry <nthiery at users.sf.net>
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
 #  as published by the Free Software Foundation; either version 2 of
 #  the License, or (at your option) any later version.
-#                  http://www.gnu.org/licenses/
-#*****************************************************************************
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
 
 cdef class CallableDict(dict):
     r"""
@@ -74,4 +75,18 @@ cdef class CallableDict(dict):
         try:
             return self[key]
         except KeyError:
-            raise ValueError(repr(key)+" is not in dict")
+            raise ValueError(repr(key) + " is not in dict")
+
+    def __repr__(self):
+        """
+        Return the string representation of ``self``.
+
+        EXAMPLES::
+
+            sage: from sage.misc.callable_dict import CallableDict
+            sage: from sage.combinat.words.morphism import CallableDict
+            sage: d = CallableDict({1: 'a', 3: 'b', 2: 'c'}); d
+            {1: 'a', 2: 'c', 3: 'b'}
+        """
+        from pprint import pformat
+        return pformat(dict(self))

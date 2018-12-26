171f9885ff150378e69de4cdfeea2fb1d6b107ea
Martin Rubey <martin.rubey@tuwien.ac.at>
2018-12-26 18:53:23 +0100

    same for inequality

--- src/sage/combinat/set_partition.py
+++ src/sage/combinat/set_partition.py
@@ -145,6 +145,14 @@ class AbstractSetPartition(ClonableArray):
             sage: D = P([[1], [2, 4], [3]])
             sage: A != D
             True
+
+        Note that this may give incorrect answers if the base set is not totally ordered::
+
+            sage: a,b = frozenset([0,1]), frozenset([2,3])
+            sage: p1 = SetPartition([[a], [b]])
+            sage: p2 = SetPartition([[b], [a]])
+            sage: p1 != p2
+            True
         """
         return not (self == y)
 

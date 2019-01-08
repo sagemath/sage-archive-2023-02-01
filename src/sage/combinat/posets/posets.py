7c05a839e2d1f7f93f449d4072130004efe1ad88
Jori Mäntysalo <jori.mantysalo@uta.fi>
2019-01-08 09:42:00 +0200

    Add alias name to doc.

--- src/sage/combinat/posets/posets.py
+++ src/sage/combinat/posets/posets.py
@@ -3384,7 +3384,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         or the rank of the poset if ``element`` is ``None``.
 
         (The rank of a poset is the length of the longest chain of
-        elements of the poset.)
+        elements of the poset. This is sometimes called length of poset.)
 
         EXAMPLES::
 

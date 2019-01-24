bd4d4c6eecc31c8b8e2de70a1dbd3fb0f66982a4
Frédéric Chapoton <chapoton@unistra.fr>
2019-01-24 16:55:15 +0100

    fix import

--- src/sage/combinat/rigged_configurations/all.py
+++ src/sage/combinat/rigged_configurations/all.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 
 from sage.misc.lazy_import import lazy_import
 
-lazy_import('sage.combinatrigged_configurations.rigged_configurations',
+lazy_import('sage.combinat.rigged_configurations.rigged_configurations',
             'RiggedConfigurations')
 
 del absolute_import

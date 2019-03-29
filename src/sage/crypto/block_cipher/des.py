004a039c550b071ebee0ef47704de738d3ba4a52
Lukas Stennes <lukas@stennes.org>
2019-03-29 20:18:50 +0100

    initial des commit

---
+++ src/sage/crypto/block_cipher/des.py
@@ -0,0 +1,17 @@
+r"""
+DES
+
+AUTHORS:
+
+- Lukas Stennes (2019-03-29): initial version
+"""
+
+# ****************************************************************************
+#       Copyright (C) 2013 Lukas Stennes <lukas.stennes@rub.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************

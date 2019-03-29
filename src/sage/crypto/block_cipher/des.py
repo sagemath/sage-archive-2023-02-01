423b261aea63c60f9820b53db03733abe470e6ca
Lukas Stennes <lukas@stennes.org>
2019-03-29 20:52:38 +0100

    implement des __init__ and __call__

--- src/sage/crypto/block_cipher/des.py
+++ src/sage/crypto/block_cipher/des.py
@@ -23,17 +23,55 @@ class DES(SageObject):
     This class implements DES described in [TODO: ADD REF]_.
     """
 
-    def __init__(self):
+    def __init__(self, rounds=None, keySchedule=False):
         r"""
         Construct an instance of DES.
+
+        INPUT:
+
+        - ``rounds``  -- integer (default: ``None``); the number of rounds. If
+          ``None`` the number of rounds of the key schedule is used.
+
+        - ``keySchedule`` -- (default: ``None``); the key schedule that will be
+          used for encryption and decryption. If ``None`` the default DES key
+          schedule is used.
+
         """
-        raise NotImplementedError
+        if keySchedule is None:
+            self._keySchedule = DES_KS()
+        else:
+            self._keySchedule = keySchedule
+        if rounds is None:
+            self._rounds = self._keySchedule._rounds
+        elif rounds <= self._keySchedule._rounds:
+            self._rounds = rounds
+        else:
+            raise ValueError('number of rounds must be less or equal to the '
+                             'number of rounds of the key schedule')
+        self._blocksize = 64
 
     def __call__(self, B, K, algorithm='encrypt'):
         r"""
         Apply DES encryption or decryption on ``B`` using the key ``K``.
         The flag ``algorithm`` controls what action is to be performed on
         ``B``.
+
+        INPUT:
+
+        - ``B`` -- integer or bit list-like; the plaintext or ciphertext
+
+        - ``K`` -- integer or bit list-like; the key
+
+        - ``algorithm`` -- string (default: ``'encrypt'``); a flag to signify
+          whether encryption or decryption is to be applied to ``B``. The
+          encryption flag is ``'encrypt'`` and the decryption flag is
+          ``'decrypt'``
+
+        OUTPUT:
+
+        - The plaintext or ciphertext corresponding to ``B``, obtained using
+          the key ``K``. If ``B`` is an integer the output will be too. If
+          ``B`` is list-like the output will be a bit vector.
         """
         if algorithm == 'encrypt':
             return self.encrypt(B, K)
@@ -73,3 +111,7 @@ class DES(SageObject):
         using DES decryption with key ``K``.
         """
         raise NotImplementedError
+
+
+class DES_KS(SageObject):
+    raise NotImplementedError

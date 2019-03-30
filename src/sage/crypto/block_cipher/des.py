ee31fae60a327dfdc0443407c6b320d29c9d57a2
Lukas Stennes <lukas@stennes.org>
2019-03-30 15:29:13 +0100

    added pc1 and pc2

--- src/sage/crypto/block_cipher/des.py
+++ src/sage/crypto/block_cipher/des.py
@@ -145,6 +145,10 @@ class DES_KS(SageObject):
             pass a ``master_key`` value on initialisation. Otherwise you can
             omit ``master_key`` and pass a key when you call the object.
         """
+        if len(K) == 56:
+            # insert 'parity' bits
+            for i in range(7, 64, 8):
+                K.insert(i, 0)
         raise NotImplementedError
 
     def __eq__(self, other):
@@ -198,17 +202,35 @@ class DES_KS(SageObject):
             raise ValueError('Key not set during initialisation')
         return iter(self(self._master_key))
 
-    def _pc1(self, key):
+    def _pc1(self, K):
         r"""
         Compute Permuted Choice 1 of ``key``.
         """
-        raise NotImplementedError
+        orderC = [57, 49, 41, 33, 25, 17,  9,
+                   1, 58, 50, 42, 34, 26, 18,
+                  10,  2, 59, 51, 43, 35, 27,
+                  19, 11,  3, 60, 52, 44, 46]
+        orderD = [63, 55, 47, 39, 31, 23, 15,
+                   7, 62, 54, 46, 38, 30, 22,
+                  14,  6, 61, 53, 45, 37, 29,
+                  21, 13,  5, 28, 20, 12,  4]
+        C = vector(GF(2), 28, [K[i-1] for i in orderC])
+        D = vector(GF(2), 28, [K[i-1] for i in orderD])
+        return C, D
 
-    def _pc2(self, key):
+    def _pc2(self, K):
         r"""
         Compute Permuted Choice 2 of ``key``.
         """
-        raise NotImplementedError
+        order = [14, 17, 11, 24,  1,  5,
+                  3, 28, 15,  6, 21, 10,
+                 23, 19, 12,  4, 26,  8,
+                 16,  7, 27, 20, 13,  2,
+                 41, 52, 31, 37, 47, 55,
+                 30, 40, 51, 45, 33, 48,
+                 44, 49, 39, 56, 34, 53,
+                 46, 42, 50, 36, 29, 32]
+        return vector(GF(2), 48, [K[i-1] for i in order])
 
     def _left_shift(self, half, i):
         r"""

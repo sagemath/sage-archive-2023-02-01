d32a3463751f975cf575c6b2a47898a5fb5bd676
Lukas Stennes <lukas@stennes.org>
2019-04-05 19:19:30 +0200

    implemented helper functions and encryption

--- src/sage/crypto/block_cipher/des.py
+++ src/sage/crypto/block_cipher/des.py
@@ -110,13 +110,22 @@ class DES(SageObject):
 
             sage: from sage.crypto.block_cipher.des import DES
             sage: des = DES()
-            sage: K = 0x0101010101010101
-            sage: P = 0x95F8A5E5DD31D900
-            sage: C = 0x8000000000000000
-            sage: des.encrypt(P, K).hex()
-            '8000000000000000'
+            sage: K = vector(GF(2), ZZ(0x133457799BBCDFF1).digits(2, padto=64)[::-1])
+            sage: P = vector(GF(2), ZZ(0x0123456789ABCDEF).digits(2, padto=64)[::-1])
+            sage: C = vector(GF(2), ZZ(0x85E813540F0AB405).digits(2, padto=64)[::-1])
+            sage: des.encrypt(P, K) == C
+            True
         """
-        raise NotImplementedError
+        state, inputType = _convert_to_vector(P, 64)
+        K, _ = _convert_to_vector(K, 64)
+        roundKeys = self._keySchedule(K)
+        state = self._ip(state)
+        L, R = state[0:32], state[32:64]
+        for k in roundKeys[:self._rounds]:
+            L, R = R, L + self._f(R, k)
+        state = vector(GF(2), 64, list(R)+list(L))
+        state = self._inv_ip(state)
+        return state if inputType == 'vector' else ZZ(list(state), 2)
 
     def decrypt(self, C, K):
         r"""
@@ -126,13 +135,57 @@ class DES(SageObject):
         raise NotImplementedError
 
     def _ip(self, B):
-        pass
+        r"""
+        EXAMPLES::
+
+            sage: from sage.crypto.block_cipher.des import DES
+            sage: des = DES()
+            sage: B = vector(GF(2), 64, [0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,1,0,1,0,0,0,1,0,1,0,1,1,0,0,1,1,1,1,0,0,0,1,0,0,1,1,0,1,0,1,0,1,1,1,1,0,0,1,1,0,1,1,1,1,0,1,1,1,1])
+            sage: des._ip(B)
+            (1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0)
+        """
+        IP = [58, 50, 42, 34, 26, 18, 10, 2,
+              60, 52, 44, 36, 28, 20, 12, 4,
+              62, 54, 46, 38, 30, 22, 14, 6,
+              64, 56, 48, 40, 32, 24, 16, 8,
+              57, 49, 41, 33, 25, 17,  9, 1,
+              59, 51, 43, 35, 27, 19, 11, 3,
+              61, 53, 45, 37, 29, 21, 13, 5,
+              63, 55, 47, 39, 31, 23, 15, 7]
+        return vector(GF(2), 64, [B[i-1] for i in IP])
 
     def _f(self, R, K):
-        pass
+        r"""
+        EXAMPLES::
+
+            sage: from sage.crypto.block_cipher.des import DES
+            sage: des = DES()
+            sage: R = vector(GF(2), 32, [1,1,1,1,0,0,0,0,1,0,1,0,1,0,1,0,1,1,1,1,0,0,0,0,1,0,1,0,1,0,1,0])
+            sage: K = vector(GF(2), 48, [0,0,0,1,1,0,1,1,0,0,0,0,0,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,0,0,0,0,0,1,1,1,0,0,1,0])
+            sage: des._f(R, K)
+            (0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1)
+        """
+        return self._permutaion(self._sboxes(self._expand(R)+K))
 
     def _expand(self, R):
-        pass
+        r"""
+        EXAMPLES::
+
+            sage: from sage.crypto.block_cipher.des import DES
+            sage: des = DES()
+            sage: R = vector(GF(2), 32, [1,1,1,1,0,0,0,0,1,0,1,0,1,0,1,0,1,1,1,1,0,0,0,0,1,0,1,0,1,0,1,0])
+            sage: des._expand(R)
+            (0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1)
+        """
+        E = [32,  1,  2,  3,  4,  5,
+              4,  5,  6,  7,  8,  9,
+              8,  9, 10, 11, 12, 13,
+             12, 13, 14, 15, 16, 17,
+             16, 17, 18, 19, 20, 21,
+             20, 21, 22, 23, 24, 25,
+             24, 25, 26, 27, 28, 29,
+             28, 29, 30, 31, 32,  1]
+        return vector(GF(2), 48, [R[i-1] for i in E])
 
     def _sboxes(self, B):
         r"""
@@ -171,10 +224,45 @@ class DES(SageObject):
         return vector(GF(2), 32, B)
 
     def _permutaion(self, B):
-        pass
+        r"""
+        EXAMPLES::
 
-    def _invIp(self, B):
-        pass
+            sage: from sage.crypto.block_cipher.des import DES
+            sage: des = DES()
+            sage: B = vector(GF(2), 32, [0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1])
+            sage: des._permutaion(B)
+            (0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1)
+        """
+        P = [16,  7, 20, 21,
+             29, 12, 28, 17,
+              1, 15, 23, 26,
+              5, 18, 31, 10,
+              2,  8, 24, 14,
+             32, 27,  3,  9,
+             19, 13, 30,  6,
+             22, 11,  4, 25,]
+        return vector(GF(2), 32, [B[i-1] for i in P])
+
+    def _inv_ip(self, B):
+        r"""
+        EXAMPLES::
+
+            sage: from sage.crypto.block_cipher.des import DES
+            sage: des = DES()
+            sage: B = vector(GF(2), 64, [0,0,0,0,1,0,1,0,0,1,0,0,1,1,0,0,1,1,0,1,1,0,0,1,1,0,0,1,0,1,0,1,0,1,0,0,0,0,1,1,0,1,0,0,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,1,0,0])
+            sage: des._inv_ip(B)
+            (1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1)
+        """
+        invIP = [40, 8, 48, 16, 56, 24, 64, 32,
+                 39, 7, 47, 15, 55, 23, 63, 31,
+                 38, 6, 46, 14, 54, 22, 62, 30,
+                 37, 5, 45, 13, 53, 21, 61, 29,
+                 36, 4, 44, 12, 52, 20, 60, 28,
+                 35, 3, 43, 11, 51, 19, 59, 27,
+                 34, 2, 42, 10, 50, 18, 58, 26,
+                 33, 1, 41,  9, 49, 17, 57, 25]
+        return vector(GF(2), 64, [B[i-1] for i in invIP])
+
 
 
 class DES_KS(SageObject):
@@ -312,16 +400,16 @@ class DES_KS(SageObject):
             sage: D
             (0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1)
         """
-        orderC = [57, 49, 41, 33, 25, 17,  9,
-                   1, 58, 50, 42, 34, 26, 18,
-                  10,  2, 59, 51, 43, 35, 27,
-                  19, 11,  3, 60, 52, 44, 46]
-        orderD = [63, 55, 47, 39, 31, 23, 15,
-                   7, 62, 54, 46, 38, 30, 22,
-                  14,  6, 61, 53, 45, 37, 29,
-                  21, 13,  5, 28, 20, 12,  4]
-        C = vector(GF(2), 28, [K[i-1] for i in orderC])
-        D = vector(GF(2), 28, [K[i-1] for i in orderD])
+        PC1_C = [57, 49, 41, 33, 25, 17,  9,
+                  1, 58, 50, 42, 34, 26, 18,
+                 10,  2, 59, 51, 43, 35, 27,
+                 19, 11,  3, 60, 52, 44, 46]
+        PC1_D = [63, 55, 47, 39, 31, 23, 15,
+                  7, 62, 54, 46, 38, 30, 22,
+                 14,  6, 61, 53, 45, 37, 29,
+                 21, 13,  5, 28, 20, 12,  4]
+        C = vector(GF(2), 28, [K[i-1] for i in PC1_C])
+        D = vector(GF(2), 28, [K[i-1] for i in PC1_D])
         return C, D
 
     def _pc2(self, K):
@@ -336,15 +424,15 @@ class DES_KS(SageObject):
             sage: ks._pc2(K)
             (0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0)
         """
-        order = [14, 17, 11, 24,  1,  5,
-                  3, 28, 15,  6, 21, 10,
-                 23, 19, 12,  4, 26,  8,
-                 16,  7, 27, 20, 13,  2,
-                 41, 52, 31, 37, 47, 55,
-                 30, 40, 51, 45, 33, 48,
-                 44, 49, 39, 56, 34, 53,
-                 46, 42, 50, 36, 29, 32]
-        return vector(GF(2), 48, [K[i-1] for i in order])
+        PC2 = [14, 17, 11, 24,  1,  5,
+                3, 28, 15,  6, 21, 10,
+               23, 19, 12,  4, 26,  8,
+               16,  7, 27, 20, 13,  2,
+               41, 52, 31, 37, 47, 55,
+               30, 40, 51, 45, 33, 48,
+               44, 49, 39, 56, 34, 53,
+               46, 42, 50, 36, 29, 32]
+        return vector(GF(2), 48, [K[i-1] for i in PC2])
 
     def _left_shift(self, half, i):
         r"""

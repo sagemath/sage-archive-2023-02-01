43534451e63a3d90cf5ae34285db96f1eddf7ae3
Jean-Florent Raymond <raymond@tu-berlin.de>
2019-04-10 19:36:11 +0200

    Major changes (mostly in cand_ext_enum) resulting in code that seems to work now. Positively tested on all graphs up to 8 vertices.

--- src/sage/graphs/domination.py
+++ src/sage/graphs/domination.py
@@ -145,117 +145,104 @@ def _peel(G, A):
     peeling.reverse()
     return peeling
 
-
-def _cand_ext_enum(G, dom, u_next, V_next):
+def _cand_ext_enum(G, dom_par, u_next, V_next):
     r'''
-    Return an iterator over the candidate extensions of ``dom`` to ``V_next``.
+    Return an iterator over the candidate extensions of ``dom_par`` to ``V_next``.
 
     For internal use. (See description below.)
     
     INPUT:
 
     - `G` -- a graph
-    - `dom` -- an iterable over some vertices of `G`
+    - `dom_par` -- an iterable over some vertices of `G`
     - `u_next` -- a vertex of `G`
     - `V_next` -- an iterable over some vertices of `G`
     
     OUTPUT:
 
     An iterator over all sets `X` that dominate
-    $N(u_next) \cap V_next \setminus N[dom]$ and are inclusion-wise
-    minimal (hereafter called candidate extensions of ``dom`` to ``V_next``).
+    $N(u_next) \cap V_next \setminus N[dom_par]$ and are inclusion-wise
+    minimal (hereafter called candidate extensions of ``dom_par`` to ``V_next``).
 
-    Intuitively, we have a partial (irredundant) dominating set `dom` and we
+    Intuitively, we have a partial (irredundant) dominating set `dom_par` and we
     want to extend it to `V_next`. For this we enumerate all the minimal
-    dominating sets of $N(u_next) \cap V_next \setminus N[dom]$,
-    which will contain all the extensions of `dom` into a minimal dominating
+    dominating sets of $N(u_next) \cap V_next \setminus N[dom_par]$,
+    which will contain all the extensions of `dom_par` into a minimal dominating
     set of `V_next`. (Note that by doing so we also enumerate sets `X` such
-    that $dom \cup X$ is dominating `V_next` but is not minimal.)
+    that $dom_par \cup X$ is dominating `V_next` but is not minimal.)
     '''
 
-    def _aux_with_rep(G, dom, u_next, V_next):
-        # Auxilliary routine. Does the same as _cand_ext_enum,
-        # except that the same set may be output several times
-        # (up to |G| times).
+    def _aux_with_rep(H, to_dom, u_next):
+        # Auxilliary routine.
+        # returns the minimal dominating sets of to_dom, with the
+        # assumption that to_som is included in the neighborhood
+        # of u_next.
+        # WARNING: the same output may be output several times
+        # (up to |H| times).
         #
         # In order to later remove duplicates, we here output pairs
-        # (dom,i) where dom is the output candidate extension and i counts
+        # (ext,i) where ext is the output candidate extension and i counts
         # how many elements have already been output.
         #
 
-        dominated_by_dom = set().union(
-            (G.neighbor_iterator(u, closed=True) for u in dom))
-        # S = neighbors of u_next in V_next that are not yet dominated:
-        S = set.intersection(set(G.neighbor_iterator(u_next)),
-                             V_next - dominated_by_dom)
-        del dominated_by_dom
-
-        # True iff u_next is dominated by dom:
-        u_next_dom_by_dom = any(
-            v in dom for v in G.neighbor_iterator(u_next, closed=True))
-
-        if u_next_dom_by_dom:
-            # In this case, u_next is already dominated by dom,
+        if u_next not in to_dom:
+            # In this case, u_next is already dominated by dom_par,
             # so only S has to be dominated.
-            # We enumerate the minimal DSs of the bicolored graph G(S):
+            # We enumerate the minimal DSs of the subset to_dom in H,
+            # which is a smaller instance as it excludes u_next:
 
             cand_ext_index = 0
-            for X in minimal_dominating_sets(G, S):
-                yield (X, cand_ext_index)
+
+            for ext in minimal_dominating_sets(H, to_dom):
+                yield (ext, cand_ext_index)
                 cand_ext_index += 1
 
-        elif not S:
+        elif to_dom == {u_next}:
             # In this case, only u_next has to be dominated
             cand_ext_index = 0
-            for w in G.neighbor_iterator(u_next, closed=True):
+            for w in H.neighbor_iterator(u_next, closed=True):
                 # Notice that the case w = u_next is included
                 yield ({w}, cand_ext_index)
                 cand_ext_index += 1
 
         else:
-            # In this case, both u_next and S have to be dominated
-
-            S_plus = copy(S)
-            S_plus.add(u_next)  # S_plus = S + {u_next}
-
-            yield ({u_next}, 0)  # The trivial extension
+            # In this case, both u_next and to_dom-u_next have to be dominated
+            yield ({u_next}, 0)  # The trivial extension (as to_dom is subset of N(u_next))
             # Start from 1 because we already output the 0-th elt:
             cand_ext_index = 1
 
-            for w in G.neighbor_iterator(u_next):
-                # Note that w never belongs to dom,
-                # as we are not in the first case of the if statement
+            # When u_next is not in the DS, one of its neighbors w should be:
+            for w in H.neighbor_iterator(u_next):
 
-                S_minus = S
-                S_minus.difference_update(G.neighbor_iterator(w, closed=True))
-                # S_minus: vertices of S that still need to be
-                # dominated, assuming w is included in the DS
-
-                for Q in minimal_dominating_sets(G, S_minus):
-                    # Closed neighborhood of Q:
-                    NQ = set().union(
-                        (G.neighbor_iterator(u, closed=True) for u in Q))
-                    Nw_minus = set.intersection(
-                        set(G.neighbor_iterator(w, closed=True)), S_plus)
-                    if not NQ >= Nw_minus:
-                        # If Nw_minus is not included in i.e. if w has
-                        # a private neighbor in V_next wrt Q + {w}:
-                        sQ = set(Q)
-                        sQ.add(w)
-                        yield (sQ, cand_ext_index)
+                remains_to_dom = set(to_dom)
+                remains_to_dom.difference_update(H.neighbor_iterator(w, closed=True))
+                # Here again we recurse on a smaller instance at it excludes u_next (and w)
+                for Q in minimal_dominating_sets(H, remains_to_dom):
+                    ext = set(Q)
+                    ext.add(w)
+                    # By construction w dominates u_next and Q dominates to_dom - N[w],
+                    # so ext dominates to_dom: it is a candidate extension
+                    # if it is not redundant
+                    if not H.is_redundant(ext):
+                        yield (ext, cand_ext_index)
                         cand_ext_index += 1
     #
     # End of aux_with_rep routine
 
+    # V_next - <what dom_par dominates> is what we have to dominate
+    to_dom = V_next - set().union(
+        *(G.neighbor_iterator(vert, closed=True)
+          for vert in dom_par))
+
     # Here we use aux_with_rep twice to enumerate the minimal
     # dominating sets while avoiding repeated outputs
-    for (X, i) in _aux_with_rep(G, dom, u_next, V_next):
-        for (Y, j) in _aux_with_rep(G, dom, u_next, V_next):
+    for (X, i) in _aux_with_rep(G, to_dom, u_next):
+        for (Y, j) in _aux_with_rep(G, to_dom, u_next):
             if j >= i:
                 # This is the first time we meet X: we output it
                 yield X
                 break
-            elif set(Y) == set(X):
+            elif Y == X: # These are sets
                 # X has already been output in the past: we ignore it
                 break
 
@@ -288,38 +275,40 @@ def minimal_dominating_sets(G, to_dominate=None):
         sage: sorted(list(minimal_dominating_sets(G, [4])))
         [{4}, {0}, {1}, {2}, {3}]
 
-        sage: sorted(list(minimal_dominating_sets(graphs.PetersenGraph())))
-        [{0, 2, 6},
-        {0, 9, 3},
-        {0, 8, 7},
-        {1, 3, 7},
-        {1, 4, 5},
-        {8, 1, 9},
-        {8, 2, 4},
-        {9, 2, 5},
-        {3, 5, 6},
-        {4, 6, 7},
-        {0, 8, 2, 9},
-        {0, 3, 6, 7},
-        {1, 3, 5, 9},
-        {8, 1, 4, 7},
-        {2, 4, 5, 6},
-        {0, 1, 2, 3, 4},
-        {0, 1, 2, 5, 7},
-        {0, 1, 4, 6, 9},
-        {0, 1, 5, 6, 8},
-        {0, 3, 4, 5, 8},
-        {0, 4, 5, 7, 9},
-        {1, 2, 3, 6, 8},
-        {1, 2, 6, 7, 9},
-        {2, 3, 4, 7, 9},
-        {2, 3, 5, 7, 8},
-        {3, 4, 6, 8, 9},
-        {5, 6, 7, 8, 9}]
+        sage: ll = list(minimal_dominating_sets(graphs.PetersenGraph()))
+        sage: pp = [{0, 2, 6},
+        ....: {0, 9, 3},
+        ....: {0, 8, 7},
+        ....: {1, 3, 7},
+        ....: {1, 4, 5},
+        ....: {8, 1, 9},
+        ....: {8, 2, 4},
+        ....: {9, 2, 5},
+        ....: {3, 5, 6},
+        ....: {4, 6, 7},
+        ....: {0, 8, 2, 9},
+        ....: {0, 3, 6, 7},
+        ....: {1, 3, 5, 9},
+        ....: {8, 1, 4, 7},
+        ....: {2, 4, 5, 6},
+        ....: {0, 1, 2, 3, 4},
+        ....: {0, 1, 2, 5, 7},
+        ....: {0, 1, 4, 6, 9},
+        ....: {0, 1, 5, 6, 8},
+        ....: {0, 8, 3, 4, 5},
+        ....: {0, 9, 4, 5, 7},
+        ....: {8, 1, 2, 3, 6},
+        ....: {1, 2, 9, 6, 7},
+        ....: {9, 2, 3, 4, 7},
+        ....: {8, 2, 3, 5, 7},
+        ....: {8, 9, 3, 4, 6},
+        ....: {8, 9, 5, 6, 7}]
+        sage: len(pp) == len(pp) and all(x in pp for x in ll) and all(x in ll for x in pp)
+        True 
 
     TESTS:
 
-        sage: list(minimal_dominating_sets(Graph())):
+        sage: list(minimal_dominating_sets(Graph()))
         [set()]
 
     ::
@@ -328,8 +317,14 @@ def minimal_dominating_sets(G, to_dominate=None):
         sage: def minimal_dominating_sets_naive(G):
         ....:     return (S for S in Subsets(G.vertices())
         ....:             if not(G.is_redundant(S)) and G.is_dominating(S))
-        sage: G = graphs.RandomGNP(5, 0.5)
-        sage: sorted(list(minimal_dominating_sets(G))) == sorted(list(minimal_dominating_sets_naive(G))) # long time
+        sage: def big_check(n):
+        ....:     for G in graphs(n):
+        ....:         ll = list(minimal_dominating_sets(G))
+        ....:         pp = list(minimal_dominating_sets_naive(G))
+        ....:         if len(pp) != len(pp) or any(x not in pp for x in ll) or any(x not in ll for x in pp):
+        ....:             return False
+        ....:     return True
+        sage: big_check(6) # long time
         True
 
     ::
@@ -346,6 +341,12 @@ def minimal_dominating_sets(G, to_dominate=None):
         # Internal routine
         # Recursively generates the leaves descendant of (dom,i)
         # H: graph; plng: peeling
+        # dom: minimal dominating set of plng[i][1]
+        # recursively generate the minimal dominating sets of plng[i+1][1] that are descendant
+        # (wrt the `parent` function) of the minimal DS `dom` of plng[i][1]
+
+        assert H.is_dominating(dom, plng[i][1]) #debug
+        assert i<=len(plng) -2 #debug
 
         if i == len(plng) - 2:
             # we reached a leaf, i.e. dom is a minimal DS of vertices_to_dominate
@@ -354,7 +355,7 @@ def minimal_dominating_sets(G, to_dominate=None):
             yield dom
             return
 
-        u_next, V_next = plng[i+1]
+        u_next, V_next = plng[i + 1]
 
         if H.is_dominating(dom, V_next):  # if dom dominates V_next
             # then dom is its unique extension: we recurse on it
@@ -362,13 +363,15 @@ def minimal_dominating_sets(G, to_dominate=None):
                 yield Di
             return
 
-        # For every candidate extension
+        # Otherwise, for every candidate extension
         for can_ext in _cand_ext_enum(H, dom, u_next, V_next):
 
             # We complete dom with can_ext -> canD
-            canD = set.union(set(can_ext), set(dom))
+            canD = set().union(can_ext, dom)
 
             if (not H.is_redundant(canD, V_next)) and set(dom) == set(_parent(H, canD, plng[i][1])):
+                # By construction, can_ext is a dominating set of `V_next - N[dom]`, so
+                # canD dominates V_next
                 # If canD is a legitimate child of dom and is not
                 # redundant, we recurse on it:
                 for Di in tree_search(H, plng, canD, i + 1):
@@ -390,4 +393,3 @@ def minimal_dominating_sets(G, to_dominate=None):
     for dom in tree_search(G, peeling, set(), 0):
         # we generate the leaves of the search tree that are descendant of (empty set, 0)
         yield dom
-

172096d81dd331a428dce9e8ff48de65784afb36
Samuel Lelièvre <samuel.lelievre@gmail.com>
2019-04-17 10:34:24 +0200

    27662: primecount 4.7 with a py3 fix

--- src/sage/interfaces/primecount.pyx
+++ src/sage/interfaces/primecount.pyx
@@ -110,7 +110,7 @@ cpdef prime_pi_128(n):
         sage: nth_prime_128(2**65)   # not tested
         ?
     """
-    cdef cppstring s = str(n)
+    cdef cppstring s = bytes(n)
     cdef bytes ans
     sig_on()
     ans = primecount.pi(s)

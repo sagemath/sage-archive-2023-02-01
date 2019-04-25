13d8c44c9f73ac3f7033fc819781a6e349f1637a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2019-04-25 16:13:29 +0200

    docstring fix

--- src/sage/geometry/polyhedron/library.py
+++ src/sage/geometry/polyhedron/library.py
@@ -933,7 +933,7 @@ class Polytopes():
 
         INPUT:
 
-        - ``exact`` - (boolean, default ``False``) if ``True`` use exact
+        - ``exact`` -- (boolean, default ``False``) if ``True`` use exact
           coordinates instead of floating point approximations
 
         - ``base_ring`` -- the field to use. If ``None`` (the default), construct

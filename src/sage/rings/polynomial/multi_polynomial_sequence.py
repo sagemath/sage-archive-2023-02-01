a9c97fdb9e7f2ea2a956ede2036a6e94df3fd821
Markus Wageringel <markus.wageringel+gh@gmail.com>
2019-04-26 21:02:49 +0200

    fix pyflakes errors in multi_polynomial_sequence

--- src/sage/rings/polynomial/multi_polynomial_sequence.py
+++ src/sage/rings/polynomial/multi_polynomial_sequence.py
@@ -158,19 +158,16 @@ from six.moves import range
 
 from sage.misc.cachefunc import cached_method
 
-from types import GeneratorType
 from sage.misc.converting_dict import KeyConvertingDict
 
-from sage.structure.sequence import Sequence, Sequence_generic
+from sage.structure.sequence import Sequence_generic
 
 from sage.rings.infinity import Infinity
 from sage.rings.finite_rings.finite_field_constructor import FiniteField as GF
 from sage.rings.finite_rings.finite_field_base import FiniteField
 from sage.rings.polynomial.multi_polynomial_ring import is_MPolynomialRing
 from sage.rings.quotient_ring import is_QuotientRing
-from sage.rings.quotient_ring_element import QuotientRingElement
 from sage.rings.polynomial.multi_polynomial_ideal import MPolynomialIdeal
-from sage.rings.polynomial.multi_polynomial import is_MPolynomial
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 
 from sage.interfaces.singular import singular_gb_standard_options
@@ -287,10 +284,9 @@ def PolynomialSequence(arg1, arg2=None, immutable=False, cr=False, cr_str=None):
         [x, y, z]
     """
     from sage.structure.element import is_Matrix
-    from sage.rings.polynomial.pbori import BooleanMonomialMonoid, BooleanMonomial
+    from sage.rings.polynomial.pbori import BooleanMonomialMonoid
 
     is_ring = lambda r: is_MPolynomialRing(r) or isinstance(r, BooleanMonomialMonoid) or (is_QuotientRing(r) and is_MPolynomialRing(r.cover_ring()))
-    is_poly = lambda f: is_MPolynomial(f) or isinstance(f, QuotientRingElement) or isinstance(f, BooleanMonomial)
 
     if is_ring(arg1):
         ring, gens = arg1, arg2

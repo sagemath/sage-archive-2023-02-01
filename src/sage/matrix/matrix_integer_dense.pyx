8c43ba044760d63e7c11188368343cc872458c6a
black-stones <black.stones@protonmail.com>
2019-05-12 13:57:34 -0400

    Check matrix is square

--- src/sage/matrix/matrix_integer_dense.pyx
+++ src/sage/matrix/matrix_integer_dense.pyx
@@ -5023,8 +5023,17 @@ cdef class Matrix_integer_dense(Matrix_dense):
 
 
         -  ``matrix`` - the Hermite normal form of self.
+
+        A ValueError is raised if the matrix is not square, fixing :trac:`5548`::
+
+            sage: random_matrix(ZZ,16,4)._hnf_mod(100)
+            Traceback (most recent call last):
+            ...
+            ValueError: matrix is not square
         """
         t = verbose('hermite mod %s'%D, caller_name='matrix_integer_dense')
+        if self._nrows != self._ncols:
+            raise ValueError("matrix is not square")
         cdef Matrix_integer_dense res = self._new(self._nrows,self._ncols)
         self._hnf_modn(res, D)
         verbose('finished hnf mod', t, caller_name='matrix_integer_dense')

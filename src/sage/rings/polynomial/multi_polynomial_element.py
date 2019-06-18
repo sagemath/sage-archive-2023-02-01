edafd35e391834cef0d3d5d563d4d735903636f4
atowsley <atowsley42@gmail.com>
2019-06-18 14:42:01 -0400

    Added new documentation.

--- src/sage/rings/polynomial/multi_polynomial_element.py
+++ src/sage/rings/polynomial/multi_polynomial_element.py
@@ -921,6 +921,16 @@ class MPolynomial_polydict(Polynomial_singular_repr, MPolynomial_element):
                 return [tuple(e) for e in self.__exponents]
 
     def inverse_of_unit(self):
+        """
+        Return the inverse of a unit in a ring.
+
+        TESTS::
+
+            sage: R.<c> = QQ[]
+            sage: l = R(2)
+            sage: l.inverse_of_unit().parent()
+            Univariate Polynomial Ring in c over Rational Field
+        """
         d = self.element().dict()
         k = list(d)
         if self.is_unit():

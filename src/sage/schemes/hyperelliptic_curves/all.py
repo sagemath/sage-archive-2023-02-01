c04c12f9038c49bde0e40077db480f5b919092d1
Frédéric Chapoton <chapoton@unistra.fr>
2019-06-26 16:50:57 +0200

    trac 28064 deprecation of some functions about invariants in global namespace

--- src/sage/schemes/hyperelliptic_curves/all.py
+++ src/sage/schemes/hyperelliptic_curves/all.py
@@ -1,9 +1,13 @@
 from __future__ import absolute_import
+from sage.misc.lazy_import import lazy_import
+
 from .constructor import HyperellipticCurve
 from .kummer_surface import KummerSurface
-from .invariants import (igusa_clebsch_invariants,
-                        absolute_igusa_invariants_kohel,
-                        absolute_igusa_invariants_wamelen,
-                        clebsch_invariants)
+lazy_import('sage.schemes.hyperelliptic_curves.invariants',
+            ['igusa_clebsch_invariants', 'absolute_igusa_invariants_kohel',
+             'absolute_igusa_invariants_wamelen', 'clebsch_invariants'],
+            deprecation=28064)
 from .mestre import (Mestre_conic, HyperellipticCurve_from_invariants)
 from . import monsky_washnitzer
+
+del absolute_import

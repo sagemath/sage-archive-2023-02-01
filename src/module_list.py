39a70997144faad3c3656770fb17de1e6c4805eb
Jonathan Kliem <jonathan.kliem@fu-berlin.de>
2019-06-27 21:00:34 +0200

    added compile argument -std=c++11

--- src/module_list.py
+++ src/module_list.py
@@ -300,7 +300,8 @@ ext_modules = [
               sources = ['sage/geometry/polyhedron/combinatorial_polyhedron/list_of_faces.pyx']),
 
     Extension('sage.geometry.polyhedron.combinatorial_polyhedron.bit_vector_operations.cc',
-              sources = ['sage/geometry/polyhedron/combinatorial_polyhedron/bit_vector_operations.cc']),
+              sources = ['sage/geometry/polyhedron/combinatorial_polyhedron/bit_vector_operations.cc'],
+              extra_compile_args=['-std=c++11']),
 
     Extension('sage.geometry.polyhedron.combinatorial_polyhedron.face_iterator',
               sources = ['sage/geometry/polyhedron/combinatorial_polyhedron/face_iterator.pyx']),

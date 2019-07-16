5eec95a03774ea9f2b8e5f4075554f9a3e1a0a05
Isuru Fernando <isuruf@gmail.com>
2019-07-16 09:34:00 -0500

    Fix some pyflakes issues

--- src/doc/pt/a_tour_of_sage/conf.py
+++ src/doc/pt/a_tour_of_sage/conf.py
@@ -11,8 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
-from sage.docs.conf import *
+from sage.docs.conf import release
+from sage.docs.conf import *  # NOQA
 
 # General information about the project.
 project = u'Uma Turnê pelo Sage'

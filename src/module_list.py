fd6d3a29df59bdb627e5c66687bd37cd8e83b761
Georgios Giapitzakis Tzintanos <giorgosgiapis@mail.com>
2019-07-31 18:46:08 +0300

    revert

--- src/module_list.py
+++ src/module_list.py
@@ -351,9 +351,6 @@ ext_modules = [
     Extension('sage.graphs.base.static_sparse_backend',
               sources = ['sage/graphs/base/static_sparse_backend.pyx']),
 
-    Extension('sage.graphs.graph_coloring',
-              sources = ['sage/graphs/graph_coloring.pyx']),
-
     Extension('sage.graphs.line_graph',
               sources = ['sage/graphs/line_graph.pyx']),
 

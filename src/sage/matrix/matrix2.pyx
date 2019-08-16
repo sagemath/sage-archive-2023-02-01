268d9dbe8d6557964cadcc4810f0965bf3b8191a
Kiran Kedlaya <kskedl@gmail.com>
2019-08-15 18:43:30 -0700

    Add algorithm scaled_partial_pivoting_valuation

--- src/sage/matrix/matrix2.pyx
+++ src/sage/matrix/matrix2.pyx
@@ -6613,6 +6613,9 @@ cdef class Matrix(Matrix1):
           - ``'scaled_partial_pivoting'``: Gauss elimination, using scaled
             partial pivoting (if base ring has absolute value)
 
+          - ``'scaled_partial_pivoting_valuation'``: Gauss elimination, using scaled
+            partial pivoting (if base ring has valuation)
+
           - ``'strassen'``: use a Strassen divide and conquer
             algorithm (if available)
 
@@ -6736,12 +6739,12 @@ cdef class Matrix(Matrix1):
             # but this should be done by introducing a category of general valuation rings and fields,
             # which we don't have at the moment
             elif self.base_ring() in DiscreteValuationFields():
-                algorithm = 'scaled_partial_pivoting'
+                algorithm = 'scaled_partial_pivoting_valuation'
             else:
                 algorithm = 'classical'
         try:
             if self.base_ring().is_field():
-                if algorithm in ['classical', 'partial_pivoting', 'scaled_partial_pivoting']:
+                if algorithm in ['classical', 'partial_pivoting', 'scaled_partial_pivoting', 'scaled_partial_pivoting_valuation']:
                     self._echelon_in_place(algorithm)
                 elif algorithm == 'strassen':
                     self._echelon_strassen(cutoff)
@@ -6779,6 +6782,9 @@ cdef class Matrix(Matrix1):
             (if base ring has absolute value)
 
           - ``'scaled_partial_pivoting'``: Gauss elimination, using scaled
+            partial pivoting (if base ring has absolute value)
+
+          - ``'scaled_partial_pivoting_valuation'``: Gauss elimination, using scaled
             partial pivoting (if base ring has absolute value)
 
           - ``'strassen'``: use a Strassen divide and conquer
@@ -7025,10 +7031,21 @@ cdef class Matrix(Matrix1):
                         if abs_val > scale_factor:
                             scale_factor = abs_val
                     scale_factors.append(scale_factor)
+            elif algorithm == 'scaled_partial_pivoting_valuation':
+                for r in range(nr):
+                    scale_factor = None
+                    for c in range(nc):
+                        valuation = A.get_unsafe(r, c).valuation()
+                        if scale_factor is None or valuation < scale_factor:
+                            scale_factor = valuation
+                    scale_factors.append(scale_factor)
 
             for c in range(nc):
                 sig_check()
-                max_abs_val = 0
+                if algorithm == 'scaled_partial_pivoting_valuation':
+                    max_abs_val = None
+                else:
+                    max_abs_val = 0
                 best_r = start_row - 1
 
                 if algorithm == 'partial_pivoting':
@@ -7037,21 +7054,28 @@ cdef class Matrix(Matrix1):
                         if abs_val > max_abs_val:
                             max_abs_val = abs_val
                             best_r = r
-                else: # algorithm == 'scaled_partial_pivoting':
+                elif algorithm == 'scaled_partial_pivoting':
                     for r in range(start_row, nr):
                         if scale_factors[r]:
                             abs_val = A.get_unsafe(r,c).abs() / scale_factors[r]
                             if abs_val > max_abs_val:
                                 max_abs_val = abs_val
                                 best_r = r
+                else: # algorithm == 'scaled_partial_pivoting_valuation':
+                    for r in range(start_row, nr):
+                        if scale_factors[r] is not None:
+                            abs_val = scale_factors[r] - A.get_unsafe(r,c).valuation()
+                            if abs_val > max_abs_val:
+                                max_abs_val = abs_val
+                                best_r = r
 
-                if max_abs_val:
+                if max_abs_val or (algorithm == 'scaled_partial_pivoting_valuation' and max_abs_val is not None):
                     pivots.append(c)
                     a_inverse = ~A.get_unsafe(best_r, c)
                     A.rescale_row(best_r, a_inverse, c)
                     A.swap_rows(best_r, start_row)
 
-                    if algorithm == 'scaled_partial_pivoting':
+                    if algorithm == 'scaled_partial_pivoting' or algorithm == 'scaled_partial_pivoting_valuation':
                         scale_factors[best_r], scale_factors[start_row] = scale_factors[start_row], scale_factors[best_r]
 
                     for i in range(nr):
@@ -8882,6 +8906,15 @@ cdef class Matrix(Matrix1):
 
             sage: matrix().inverse()
             []
+
+        Test :trac:`27473`::
+
+            sage: F.<t> = LaurentSeriesRing(GF(2))
+            sage: M = Matrix([[t,1],[0,t]])
+            sage: ~M
+            [t^-1 t^-2]
+            [   0 t^-1]
+
         """
         return ~self
 

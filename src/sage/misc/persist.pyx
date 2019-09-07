bfd64bc6d887951329ca75a0b17b98af6a4c4ca2
Simon King <simon.king@uni-jena.de>
2019-09-07 19:19:47 +0200

    Fix keyword for py3-only test

--- src/sage/misc/persist.pyx
+++ src/sage/misc/persist.pyx
@@ -832,7 +832,7 @@ class SagePickler(_BasePickler):
 
     The following line demonstrates what would happen without :trac:`28444`::
 
-        sage: loads(b'x\x9ck`J\x8e\x8f\xcfM\xcc\xcc\x8b\x8f\xe7r\xcb\xcf\xe7*d\x0cej`/dj\r*d\xd6\x03\x00\x89\xc5\x08{', encoding='ASCII') # optional: python3
+        sage: loads(b'x\x9ck`J\x8e\x8f\xcfM\xcc\xcc\x8b\x8f\xe7r\xcb\xcf\xe7*d\x0cej`/dj\r*d\xd6\x03\x00\x89\xc5\x08{', encoding='ASCII') #py3
         Traceback (most recent call last):
         ...
         UnicodeDecodeError: 'ascii' codec can't decode byte 0x80 in position 0: ordinal not in range(128)
@@ -1022,7 +1022,7 @@ def loads(s, compress=True, **kwargs):
 
     The following line demonstrates what would happen without :trac:`28444`::
 
-        sage: loads(b'x\x9ck`J\x8e\x8f\xcfM\xcc\xcc\x8b\x8f\xe7r\xcb\xcf\xe7*d\x0cej`/dj\r*d\xd6\x03\x00\x89\xc5\x08{', encoding='ASCII') # optional: python3
+        sage: loads(b'x\x9ck`J\x8e\x8f\xcfM\xcc\xcc\x8b\x8f\xe7r\xcb\xcf\xe7*d\x0cej`/dj\r*d\xd6\x03\x00\x89\xc5\x08{', encoding='ASCII') #py3
         Traceback (most recent call last):
         ...
         UnicodeDecodeError: 'ascii' codec can't decode byte 0x80 in position 0: ordinal not in range(128)

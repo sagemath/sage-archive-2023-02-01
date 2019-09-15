f4581b69a1ee8b3069db26b522d823ccd607cef6
Simon King <simon.king@uni-jena.de>
2019-09-15 13:36:39 +0200

    Add a comment regarding the expected data type for an unpickle helper

--- src/sage/matrix/matrix_gfpn_dense.pyx
+++ src/sage/matrix/matrix_gfpn_dense.pyx
@@ -1906,6 +1906,11 @@ def mtx_unpickle(f, int nr, int nc, data, bint m):
         sage: mtx_unpickle(MatrixSpace(GF(19),0,5), 0, 5, b'', True) # optional: meataxe
         []
     """
+    # The expeced input type is bytes. However, Python-2 legacy pickles do
+    # not distinguish between str and bytes. If such pickle is unpickled
+    # in Python-3, Sage will receive a str in in `latin1` encoding. Therefore,
+    # in the following line, we use a helper function that would return bytes,
+    # regardless whether the input is bytes or str.
     cdef bytes Data = str_to_bytes(data, encoding='latin1')
     if isinstance(f, (int, long)):
         # This is for old pickles created with the group cohomology spkg

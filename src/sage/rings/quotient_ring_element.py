9e2715908929efc3fde80fb9af5870008ded4225
Frédéric Chapoton <chapoton@unistra.fr>
2019-10-12 17:14:05 +0200

    converting some _cmp_ to _richcmp_

--- src/sage/rings/quotient_ring_element.py
+++ src/sage/rings/quotient_ring_element.py
@@ -6,15 +6,15 @@ AUTHORS:
 - William Stein
 """
 
-#*****************************************************************************
+# ****************************************************************************
 #       Copyright (C) 2005 William Stein <wstein@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
-#                  http://www.gnu.org/licenses/
-#*****************************************************************************
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
 
 
 from sage.structure.element import RingElement
@@ -646,7 +646,7 @@ class QuotientRingElement(RingElement):
         # elements of different degrees. The whole quotient stuff relies
         # in I.reduce(x) returning a normal form of x with respect to I.
         # Hence, we will not use more than that.
-        #return cmp(self.__rep, other.__rep)
+
         # Since we have to compute normal forms anyway, it makes sense
         # to use it for comparison in the case of an inequality as well.
         if self.__rep == other.__rep: # Use a shortpath, so that we

fe5c890e66a7dd37395720b6c8e08a3837e5a74d
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-01-18 12:10:09 -0500

    29038: Python package sage_conf: Provides configuration information for sagelib

---
+++ build/pkgs/sage_conf/src/sage_conf.py
@@ -0,0 +1,8 @@
+# build/pkgs/sage_conf/src/sage_conf.py.  Generated from sage_conf.py.in by configure.
+
+MAXIMA = "/Users/mkoeppe/s/sage/sage-rebasing/worktree-algebraic-2018-spring/local/bin/maxima"
+
+# The following must not be used during build to determine source or installation
+# location of sagelib.  See comments in SAGE_ROOT/src/Makefile.in
+SAGE_LOCAL = "/Users/mkoeppe/s/sage/sage-rebasing/worktree-algebraic-2018-spring/local"
+SAGE_ROOT = "/Users/mkoeppe/s/sage/sage-rebasing/worktree-algebraic-2018-spring"

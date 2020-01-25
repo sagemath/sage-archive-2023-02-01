238b198a7e14e754b8bda45d52f938b532a277de
Frédéric Chapoton <chapoton@unistra.fr>
2020-01-25 10:40:29 +0100

    get rid of all calls to six.iterkeys

--- src/sage/rings/polynomial/multi_polynomial_ring.py
+++ src/sage/rings/polynomial/multi_polynomial_ring.py
@@ -50,18 +50,17 @@ TESTS::
     (Multivariate Polynomial Ring in x, y, z over Finite Field of size 5,
     (x, y, z))
 """
-#*****************************************************************************
+# ****************************************************************************
 #       Copyright (C) 2005 William Stein <wstein@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
-#                  http://www.gnu.org/licenses/
-#*****************************************************************************
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
 from __future__ import absolute_import
 from six.moves import range
-from six import iteritems, iterkeys, itervalues
 
 from sage.rings.ring import IntegralDomain
 import sage.rings.fraction_field_element as fraction_field_element
@@ -446,7 +445,7 @@ class MPolynomialRing_polydict( MPolynomialRing_macaulay2_repr, PolynomialRing_s
                 # no guarantees that this is mathematically solid."
                 K = self.base_ring()
                 D = x.element().dict()
-                for i, a in iteritems(D):
+                for i, a in D.items():
                     D[i] = K(a)
                 return MPolynomial_polydict(self, D)
             elif set(P.variable_names()).issubset(set(self.variable_names())) and self.base_ring().has_coerce_map_from(P.base_ring()):
@@ -470,7 +469,7 @@ class MPolynomialRing_polydict( MPolynomialRing_macaulay2_repr, PolynomialRing_s
                 # no guarantees that this is mathematically solid."
                 K = self.base_ring()
                 D = x.dict()
-                for i, a in iteritems(D):
+                for i, a in D.items():
                     D[i] = K(a)
                 return MPolynomial_polydict(self, D)
             elif set(P.variable_names()).issubset(set(self.variable_names())) and self.base_ring().has_coerce_map_from(P.base_ring()):
@@ -487,7 +486,7 @@ class MPolynomialRing_polydict( MPolynomialRing_macaulay2_repr, PolynomialRing_s
 
         elif isinstance(x, dict):
             K = self.base_ring()
-            return MPolynomial_polydict(self, {i: K(a) for i, a in iteritems(x)})
+            return MPolynomial_polydict(self, {i: K(a) for i, a in x.items()})
 
         elif isinstance(x, fraction_field_element.FractionFieldElement) and x.parent().ring() == self:
             if x.denominator() == 1:
@@ -631,10 +630,12 @@ class MPolynomialRing_polydict( MPolynomialRing_macaulay2_repr, PolynomialRing_s
         if not coeff:
             coeff = self.base_ring().one()
         else:
-            coeff = self.base_ring()(next(itervalues(f.dict())) /  next(itervalues(g.dict())))
+            nf = next(iter(f.dict().values()))
+            ng = next(iter(g.dict().values()))                    
+            coeff = self.base_ring()(nf / ng)
 
-        f = next(iterkeys(f.dict()))
-        g = next(iterkeys(g.dict()))
+        f = next(iter(f.dict()))
+        g = next(iter(g.dict()))
 
         res = f.esub(g)
 

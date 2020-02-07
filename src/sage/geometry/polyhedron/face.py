7278f23b2bd3178fd399d05d7fd171d808c9f834
Jean-Philippe Labbé <labbe@math.fu-berlin.de>
2020-02-07 16:30:29 +0100

    fixed doctest

--- src/sage/geometry/polyhedron/face.py
+++ src/sage/geometry/polyhedron/face.py
@@ -753,7 +753,7 @@ class PolyhedronFace(SageObject):
             else:  # 'inner'
                 rays += [facet.A()]
         parent = self.polyhedron().parent()
-        origin = self.ambient_space().zero()
+        origin = self.polyhedron().ambient_space().zero()
         return parent.element_class(parent, [[origin], rays, lines], None)
 
 

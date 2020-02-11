8be072b7244b342c6b70737cc36409121b70b41b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-02-11 16:01:05 -0500

    build/bin/write-dockerfile.sh: Copy src/doc/bootstrap early

--- build/bin/write-dockerfile.sh
+++ build/bin/write-dockerfile.sh
@@ -132,6 +132,7 @@ cat <<EOF
 RUN mkdir -p /sage
 WORKDIR /sage
 ADD Makefile VERSION.txt README.md bootstrap configure.ac sage ./
+ADD src/doc/bootstrap src/doc/bootstrap
 ADD m4 ./m4
 ADD build ./build
 ADD src/bin/sage-version.sh src/bin/sage-version.sh

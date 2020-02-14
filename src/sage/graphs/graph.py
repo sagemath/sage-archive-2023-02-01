c4749fa073c7e6e0f279f054b0ec9c3a07844345
vipul79321 <vipul.gupta73921@gmail.com>
2020-02-14 22:27:06 +0530

    minor changes removed from graph.py

--- src/sage/graphs/graph.py
+++ src/sage/graphs/graph.py
@@ -419,8 +419,6 @@ from copy import copy
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 from sage.rings.integer import Integer
 from sage.rings.integer_ring import ZZ
-from sage.graphs.independent_sets import IndependentSets
-from sage.combinat.combinatorial_map import combinatorial_map
 import sage.graphs.generic_graph_pyx as generic_graph_pyx
 from sage.graphs.generic_graph import GenericGraph
 from sage.graphs.digraph import DiGraph

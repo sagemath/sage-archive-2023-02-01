725582982f126b850c8beb041aa6ba29f41a4379
Markus Wageringel <markus.wageringel+gh@gmail.com>
2020-02-22 21:12:43 +0100

    29204: fix iterator for python 2

--- src/sage/typeset/character_art.py
+++ src/sage/typeset/character_art.py
@@ -28,6 +28,7 @@ from __future__ import print_function
 
 import os
 import sys
+import six
 from sage.structure.sage_object import SageObject
 
 
@@ -310,7 +311,7 @@ class CharacterArt(SageObject):
         # We implement a custom iterator instead of repeatedly using
         # itertools.chain to prepend elements in order to avoid quadratic time
         # complexity
-        class PrependIterator:
+        class PrependIterator(six.Iterator):
             """
             Iterator with support for prepending of elements.
             """

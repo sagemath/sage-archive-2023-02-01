139c77461f7f67b7f9059d674d8ad315e0d6611e
vipul79321 <vipul.gupta73921@gmail.com>
2020-03-04 19:16:41 +0530

    issues merged

--- src/sage/graphs/graph_input.py
+++ src/sage/graphs/graph_input.py
@@ -383,7 +383,7 @@ def from_oriented_incidence_matrix(G, M, loops=False, multiedges=False, weighted
         ...
         ValueError: each column represents an edge: -1 goes to 1
     
-    Handle incidence matrix containing a column with only zeros (:trac:`29276`)::
+    Handle incidence matrix containing a column with only zeros (:trac:`29275`)::
 
         sage: m = Matrix([[0,1],[0,-1],[0,0]])
         sage: m

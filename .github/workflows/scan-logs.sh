f9aca59f5fa10573179570f9e31d14715dad0060
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-04-12 10:31:30 -0700

    .github/workflows/scan-logs.sh: Fixup regexp for scanning ptest.log

--- .github/workflows/scan-logs.sh
+++ .github/workflows/scan-logs.sh
@@ -2,9 +2,12 @@
 # The markup in the output is a GitHub Actions logging command
 # https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions
 LOGS=${1-logs}
-find "$LOGS" -type f -name "*.log" -exec sh -c '\
-    if tail -100 "{}" 2>/dev/null | grep "^Error" >/dev/null; then \
-        echo :":"error file={}:":" ==== ERROR IN LOG FILE {} ====; cat "{}" ; \
-    elif tail -20 "{}" 2>/dev/null | grep -E "^(Warning: Error testing|make: .*test.*Error)" >/dev/null; then \
-        echo :":"warning file={}:":" ==== TESTSUITE FAILURE IN LOG FILE {} ====; cat "{}"; \
-    fi' \;
+for a in $(find "$LOGS" -type f -name "*.log"); do
+    if tail -100 "$a" 2>/dev/null | grep "^Error" >/dev/null; then
+        echo :":"error file=$a:":" ==== ERROR IN LOG FILE $a ====
+        cat "$a"
+    elif tail -20 "$a" 2>/dev/null | grep -E "^(Warning: Error testing|^sage .*doctest.*failed)" >/dev/null; then
+        echo :":"warning file=$a:":" ==== TESTSUITE FAILURE IN LOG FILE $a ====
+        cat "$a"
+    fi
+done

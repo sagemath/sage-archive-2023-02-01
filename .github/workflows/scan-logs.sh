b32e1bf451eceb45ef874ddbbb4cc102fb22a396
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-04-12 10:37:48 -0700

    .github/workflows/scan-logs.sh: Also catch errors of the form ImportError

--- .github/workflows/scan-logs.sh
+++ .github/workflows/scan-logs.sh
@@ -3,7 +3,7 @@
 # https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions
 LOGS=${1-logs}
 for a in $(find "$LOGS" -type f -name "*.log"); do
-    if tail -100 "$a" 2>/dev/null | grep "^Error" >/dev/null; then
+    if tail -100 "$a" 2>/dev/null | grep "^[A-Za-z]*Error" >/dev/null; then
         echo :":"error file=$a:":" ==== ERROR IN LOG FILE $a ====
         cat "$a"
     elif tail -20 "$a" 2>/dev/null | grep -E "^(Warning: Error testing|^sage .*doctest.*failed)" >/dev/null; then

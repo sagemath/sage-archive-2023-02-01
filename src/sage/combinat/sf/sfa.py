c37c530505daaa7e701cdfacf498007457c12b07
Darij Grinberg <darijgrinberg@gmail.com>
2020-04-25 22:05:23 +0200

    test battery for principal_specialization

--- src/sage/combinat/sf/sfa.py
+++ src/sage/combinat/sf/sfa.py
@@ -5569,9 +5569,96 @@ class SymmetricFunctionAlgebra_generic_Element(CombinatorialFreeModule.Element):
             {-3}
 
             sage: S = SymmetricFunctions(GF(3))
-            sage: B = [S.p(), S.m(), S.e(), S.h(), S.s(), S.f()]
+            sage: B = [S.m(), S.e(), S.h(), S.s(), S.f()]
             sage: m = S.m(); x = m[3,2,1]
             sage: set([b(x).principal_specialization(n=4, q=GF(3)(2)) for b in B])
+            {1}
+
+            sage: S = SymmetricFunctions(Zmod(4))
+            sage: B = [S.m(), S.e(), S.h(), S.s(), S.f()]
+            sage: m = S.m(); x = m[3,2,1]
+            sage: set([b(x).principal_specialization(n=4, q=Zmod(4)(2)) for b in B])
+            {0}
+            sage: y = m[3,1]
+            sage: set([b(y).principal_specialization(n=4, q=Zmod(4)(2)) for b in B])
+            {2}
+
+        Check that parents are correct over `\mathbb{F}_3`::
+
+            sage: S = SymmetricFunctions(GF(3))
+            sage: B = [S.m(), S.e(), S.h(), S.s(), S.f()]
+            sage: lams = [Partition([]), Partition([1]), Partition([2,1])]
+            sage: set(b[lam].principal_specialization(n=2, q=GF(3)(0)).parent() for b in B for lam in lams)
+            {Finite Field of size 3}
+            sage: set(b[lam].principal_specialization(n=2, q=GF(3)(1)).parent() for b in B for lam in lams)
+            {Finite Field of size 3}
+            sage: set(b[lam].principal_specialization(n=2, q=GF(3)(2)).parent() for b in B for lam in lams)
+            {Finite Field of size 3}
+            sage: set(b[lam].principal_specialization(n=2).parent() for b in B for lam in lams)
+            {Univariate Polynomial Ring in q over Finite Field of size 3}
+            sage: set(b[lam].principal_specialization().parent() for b in B for lam in lams)
+            {Fraction Field of Univariate Polynomial Ring in q over Finite Field of size 3}
+
+            sage: a = S.e()[2,1].principal_specialization(n=2, q=GF(3)(2)); a
+            0
+            sage: a = S.e()[1,1,1].principal_specialization(n=2); a
+            q^3 + 1
+            
+            sage: set(b.one().principal_specialization(n=2, q=GF(3)(2)) for b in B)
+            {1}
+            sage: set(b.one().principal_specialization(n=2, q=GF(3)(1)) for b in B)
+            {1}
+            sage: set(b.one().principal_specialization(n=2) for b in B)
+            {1}
+            sage: set(b.one().principal_specialization() for b in B)
+            {1}
+
+        Check that parents are correct over the integer ring::
+
+            sage: S = SymmetricFunctions(ZZ)
+            sage: B = [S.m(), S.e(), S.h(), S.s(), S.f()]
+            sage: lams = [Partition([]), Partition([1]), Partition([2,1])]
+            sage: set(b[lam].principal_specialization(n=2, q=0).parent() for b in B for lam in lams)
+            {Integer Ring}
+            sage: set(b[lam].principal_specialization(n=2, q=1).parent() for b in B for lam in lams)
+            {Integer Ring}
+            sage: set(b[lam].principal_specialization(n=2, q=2).parent() for b in B for lam in lams)
+            {Integer Ring}
+            sage: set(b[lam].principal_specialization(n=2).parent() for b in B for lam in lams)
+            {Univariate Polynomial Ring in q over Integer Ring}
+            sage: set(b[lam].principal_specialization().parent() for b in B for lam in lams)
+            {Fraction Field of Univariate Polynomial Ring in q over Integer Ring}
+
+        Check that parents are correct over a polynomial ring::
+
+            sage: P = PolynomialRing(ZZ, "q")
+            sage: q = P.gen()
+            sage: S = SymmetricFunctions(P)
+            sage: B = [S.m(), S.e(), S.h(), S.s(), S.f()]
+            sage: lams = [Partition([]), Partition([1]), Partition([2,1])]
+            sage: set(b[lam].principal_specialization(n=2, q=P(0)).parent() for b in B for lam in lams)
+            {Univariate Polynomial Ring in q over Integer Ring}
+            sage: set(b[lam].principal_specialization(n=2, q=P(1)).parent() for b in B for lam in lams)
+            {Univariate Polynomial Ring in q over Integer Ring}
+            sage: set(b[lam].principal_specialization(n=2, q=P(2)).parent() for b in B for lam in lams)
+            {Univariate Polynomial Ring in q over Integer Ring}
+            sage: set(b[lam].principal_specialization(n=2, q=q).parent() for b in B for lam in lams)
+            {Univariate Polynomial Ring in q over Integer Ring}
+            sage: set(b[lam].principal_specialization(q=q).parent() for b in B for lam in lams)
+            {Fraction Field of Univariate Polynomial Ring in q over Integer Ring}
+
+            sage: a = S.e()[2,1].principal_specialization(n=2, q=2); a
+            6
+            sage: a = S.e()[2,1].principal_specialization(n=2, q=q); a
+            q**2 + q
+            
+            sage: set(b.one().principal_specialization(n=2, q=P(2)) for b in B)
+            {1}
+            sage: set(b.one().principal_specialization(n=2, q=P(1)) for b in B)
+            {1}
+            sage: set(b.one().principal_specialization(n=2, q=q) for b in B)
+            {1}
+            sage: set(b.one().principal_specialization(q=q) for b in B)
             {1}
 
         """

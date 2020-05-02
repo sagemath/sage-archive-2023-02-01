ea92c284b5204560d65478e84c60806cc3a8c2ea
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-05-02 12:54:55 -0700

    Factor out .github/workflows/extra-sage-local.sh from ci-cygwin*.yml, remove archives after extracting

---
+++ .github/workflows/extract-sage-local.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# to be run from $SAGE_ROOT, with arguments sage-local-${{ env.PREVIOUS_STAGES }}.tar
+
+# Show all tar files from the sage-local artifact
+ls -l sage-local-*.tar
+
+mkdir -p
+
+# We specifically use the cygwin tar so that symlinks are saved/restored correctly on Windows.
+for a in $*; do
+    echo Extracting $a
+    tar xf $a
+    rm -f $a
+done
+
+# Also get rid of the stages that were not extracted
+rm -f sage-local-*.tar
+
+# We set the installation records to the same mtime so that no rebuilds due to dependencies
+# among these packages are triggered.
+(cd local/var/lib/sage/installed/ && touch .dummy && touch --reference=.dummy *)
+
+# Show what has been built already.
+ls -l local local/var/lib/sage/installed/
+
+# Rebase!
+src/bin/sage-rebase.sh local

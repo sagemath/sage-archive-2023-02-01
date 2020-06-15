b9314d4da2107ff79d1b4d62d2837c3a747484bb
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-06-15 16:52:31 -0700

    sage.misc.call: Add standard header information, add to reference manual

--- src/sage/misc/call.py
+++ src/sage/misc/call.py
@@ -1,3 +1,19 @@
+"""
+Attribute and method calling
+"""
+
+# ****************************************************************************
+#       Copyright (C) 2008 Mike Hansen
+#       Copyright (C) 2010, 2013 Nicolas M. Thiery
+#       Copyright (C) 2018 Frédéric Chapoton
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
 #############################################
 # Operators
 #############################################

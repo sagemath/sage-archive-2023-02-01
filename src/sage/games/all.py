188e71c47d9c6f487a00c29ad2eed5e086d4cc29
Frédéric Chapoton <chapoton@unistra.fr>
2020-06-17 21:44:52 +0200

    remove deprecated imports from 27066

--- src/sage/games/all.py
+++ src/sage/games/all.py
@@ -1,20 +1,2 @@
-"""
-Test for deprecations of imports into global namespace::
-
-    sage: backtrack_all
-    doctest:warning...:
-    DeprecationWarning:
-    Importing backtrack_all from here is deprecated. If you need to use it, please import it directly from sage.games.sudoku_backtrack
-    See https://trac.sagemath.org/27066 for details.
-    ...
-"""
-from __future__ import absolute_import
-
-from sage.misc.lazy_import import lazy_import
-
-lazy_import("sage.games.sudoku_backtrack", 'backtrack_all', deprecation=27066)
-
 from .sudoku import Sudoku, sudoku
 from .hexad import Minimog
-
-del absolute_import

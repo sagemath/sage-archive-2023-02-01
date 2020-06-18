8a41546d3ceffe39842f7d654049043ab5424cb0
Kiran Kedlaya <kskedl@gmail.com>
2020-06-17 21:53:28 -0700

    Speed up hgm_coeffs using symmetry

--- src/sage/modular/hypergeometric_misc.pyx
+++ src/sage/modular/hypergeometric_misc.pyx
@@ -3,6 +3,7 @@ Some utility routines for the hypergeometric motives package that benefit
 significantly from Cythonization.
 """
 from cpython cimport array
+from cysignals.signals cimport sig_on, sig_off
 
 cpdef hgm_coeffs(long long p, int f, int prec, gamma, m, int D,
                  gtable, int gtable_prec, bint use_longs):
@@ -36,7 +37,7 @@ cpdef hgm_coeffs(long long p, int f, int prec, gamma, m, int D,
     gl = len(gamma)
     cdef array.array gamma_array1 = array.array('i', gamma.keys())
     cdef array.array gamma_array2 = array.array('i', gamma.values())
-    cdef array.array r_array = array.array('i', [0]) * gl
+    r_array = [0] * gl
     cdef array.array digit_count = array.array('i', [0]) * q1
     cdef array.array gtab2
 
@@ -62,13 +63,18 @@ cpdef hgm_coeffs(long long p, int f, int prec, gamma, m, int D,
     else:
         for r in range(q1):
             r1 = r
-            digit_count[r] = 0
+            w = 0
             for i in range(f):
-                digit_count[r] += r1 % p
+                w += r1 % p
                 r1 //= p
+            digit_count[r] = w
     Rz = R.zero()
+    ans = [None] * q1
     for r in range(q1):
-        # First determine whether this term is forced to be zero
+        # Skip this coefficient if we already have it by symmetry.
+        if ans[r] is not None:
+            continue
+        # Determine whether this term is forced to be zero
         # for divisibility reasons. If so, skip the p-adic arithmetic.
         i = 0
         for k in range(gl):
@@ -80,34 +86,47 @@ cpdef hgm_coeffs(long long p, int f, int prec, gamma, m, int D,
         i //= (p - 1)
         l = i + f * (D + m[0] - m[r])
         if l >= prec:
-            ans.append(Rz)
-            continue
-        # Keep numerator and denominator separate for efficiency.
-        if use_longs:
-            w = 1
-            w1 = 1
+            ans[r] = Rz
         else:
-            u = R.one()
-            u1 = R.one()
-        for k in range(gl):
-            gv = gamma_array2[k]
-            r1 = r_array[k]
-            if flip:
-                gv = -gv
+            # Keep numerator and denominator separate for efficiency.
             if use_longs:
-                w2 = gtab2[r1] # cast to long long to avoid overflow
-                if gv > 0: 
-                    for j in range(gv): w = w * w2 % q2
-                else:
-                    for j in range(-gv): w1 = w1 * w2 % q2
+                w = 1
+                w1 = 1
             else:
-                if gv > 0:
-                    for j in range(gv): u *= gtable[r1]
+                u = R.one()
+                u1 = R.one()
+            for k in range(gl):
+                gv = gamma_array2[k]
+                r1 = r_array[k]
+                if flip:
+                    gv = -gv
+                if use_longs:
+                    w2 = gtab2[r1] # cast to long long to avoid overflow
+                    if gv > 0: 
+                        for j in range(gv):
+                            w = w * w2 % q2
+                    else:
+                        for j in range(-gv):
+                            w1 = w1 * w2 % q2
                 else:
-                    for j in range(-gv): u1 *= gtable[r1]
-        if use_longs:
-            u = R(w)
-            u1 = R(w1)
-        if i % 2: u = -u
-        ans.append((u / u1) << l)
+                    w2 = gtable[r1]
+                    if gv > 0:
+                        for j in range(gv):
+                            u *= w2
+                    else:
+                        for j in range(-gv):
+                            u1 *= w2
+            if use_longs:
+                u = R(w)
+                u1 = R(w1)
+            if i % 2: u = -u
+            ans[r] = (u / u1) << l
+        if f > 1:
+            r1 = r
+            for j in range(f-1):
+                r1 = r1 * p % q1
+                if ans[r1] is not None:
+                    break
+                ans[r1] = ans[r]
     return ans
+

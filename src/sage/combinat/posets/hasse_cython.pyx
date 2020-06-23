75d6b1bbb0a99bc526413c954eff7ed3a22e7f67
Frédéric Chapoton <chapoton@unistra.fr>
2020-06-23 20:32:03 +0200

    some simplifications in Cython code for Moebius + Coxeter matrices

--- src/sage/combinat/posets/hasse_cython.pyx
+++ src/sage/combinat/posets/hasse_cython.pyx
@@ -12,7 +12,7 @@ Some fast computations for finite posets
 #                  https://www.gnu.org/licenses/
 # ****************************************************************************
 
-from cysignals.signals cimport sig_str, sig_off
+from cysignals.signals cimport sig_str, sig_off, sig_check
 
 from sage.libs.flint.fmpz cimport *
 from sage.libs.flint.fmpz_mat cimport *
@@ -20,6 +20,7 @@ from sage.libs.flint.fmpz_mat cimport *
 from sage.rings.integer_ring import ZZ
 from sage.matrix.matrix_integer_dense cimport Matrix_integer_dense
 from sage.matrix.constructor import Matrix
+from sage.matrix.matrix_space import MatrixSpace
 
 
 cpdef Matrix_integer_dense moebius_matrix_fast(list positions):
@@ -48,25 +49,23 @@ cpdef Matrix_integer_dense moebius_matrix_fast(list positions):
         sage: moebius_matrix_fast(D)
         42 x 42 dense matrix over Integer Ring (...)
     """
-    cdef fmpz_mat_t M
     cdef Matrix_integer_dense A
     cdef Py_ssize_t n = len(positions)
     cdef Py_ssize_t i
     cdef int j, k
+    A = Matrix_integer_dense.__new__(Matrix_integer_dense,
+                                     MatrixSpace(ZZ, n, n), None, None, None)
     sig_str("FLINT exception")
-    fmpz_mat_init(M, n, n)
     sig_off()
-    fmpz_mat_one(M)
+    fmpz_mat_one(A._matrix)
     for i in range(n - 1, -1, -1):
+        sig_check()
         for j in positions[i]:
             if j != i:
                 for k in positions[j]:
-                    fmpz_sub(fmpz_mat_entry(M, i, k),
-                             fmpz_mat_entry(M, i, k),
-                             fmpz_mat_entry(M, j, k))
-    A = Matrix(ZZ, n, n)
-    fmpz_mat_set(A._matrix, M)
-    fmpz_mat_clear(M)
+                    fmpz_sub(fmpz_mat_entry(A._matrix, i, k),
+                             fmpz_mat_entry(A._matrix, i, k),
+                             fmpz_mat_entry(A._matrix, j, k))
     return A
 
 
@@ -96,31 +95,28 @@ cpdef Matrix_integer_dense coxeter_matrix_fast(list positions):
         sage: coxeter_matrix_fast(D)
         42 x 42 dense matrix over Integer Ring (...)
     """
-    cdef fmpz_mat_t M
     cdef Matrix_integer_dense A
     cdef Py_ssize_t n = len(positions)
     cdef Py_ssize_t i
     cdef int j, k
-    sig_str("FLINT exception")
-    fmpz_mat_init(M, n, n)
-    sig_off()
-    fmpz_mat_one(M)
+    A = Matrix_integer_dense.__new__(Matrix_integer_dense,
+                                     MatrixSpace(ZZ, n, n), None, None, None)
+    fmpz_mat_one(A._matrix)
     for i in range(n - 1, -1, -1):
+        sig_check()
         for j in positions[i]:
             if j != i:
                 for k in positions[j]:
-                    fmpz_sub(fmpz_mat_entry(M, k, i),
-                             fmpz_mat_entry(M, k, i),
-                             fmpz_mat_entry(M, k, j))
+                    fmpz_sub(fmpz_mat_entry(A._matrix, k, i),
+                             fmpz_mat_entry(A._matrix, k, i),
+                             fmpz_mat_entry(A._matrix, k, j))
     for i in range(n):
+        sig_check()
         for j in positions[i]:
             if j != i:
                 for k in range(n):
-                    fmpz_add(fmpz_mat_entry(M, i, k),
-                             fmpz_mat_entry(M, i, k),
-                             fmpz_mat_entry(M, j, k))
-    fmpz_mat_scalar_mul_si(M, M, -1)
-    A = Matrix(ZZ, n, n)
-    fmpz_mat_set(A._matrix, M)
-    fmpz_mat_clear(M)
+                    fmpz_add(fmpz_mat_entry(A._matrix, i, k),
+                             fmpz_mat_entry(A._matrix, i, k),
+                             fmpz_mat_entry(A._matrix, j, k))
+    fmpz_mat_scalar_mul_si(A._matrix, A._matrix, -1)
     return A

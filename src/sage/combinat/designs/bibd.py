d2d340e040d05aa8be6326913576bdf7cda44d23
Ivo Maffei <ivomaffei@gmail.com>
2020-06-29 12:34:58 +0200

    forgot to delete old import

--- src/sage/combinat/designs/bibd.py
+++ src/sage/combinat/designs/bibd.py
@@ -56,7 +56,6 @@ from __future__ import absolute_import
 from sage.categories.sets_cat import EmptySetError
 from sage.misc.unknown import Unknown
 from .design_catalog import transversal_design
-from .block_design import BlockDesign
 from sage.arith.all import binomial, is_prime_power
 from .group_divisible_designs import GroupDivisibleDesign
 from .designs_pyx import is_pairwise_balanced_design

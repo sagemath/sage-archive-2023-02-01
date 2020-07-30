e7d3935481e00af24ac21ffaa9043500cc1309f9
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-07-29 22:41:06 -0700

    Same for vector spaces with basis

--- src/sage/categories/vector_spaces.py
+++ src/sage/categories/vector_spaces.py
@@ -222,6 +222,25 @@ class VectorSpaces(Category_module):
                 """
                 return [self.base_category()]
 
+        class FiniteDimensional(CategoryWithAxiom_over_base_ring):
+
+            class TensorProducts(TensorProductsCategory):
+
+                def extra_super_categories(self):
+                    """
+                    Implement the fact that a (finite) tensor product of
+                    finite dimensional vector spaces is a finite dimensional vector space.
+
+                    EXAMPLES::
+
+                        sage: VectorSpaces(QQ).WithBasis().FiniteDimensional().TensorProducts().extra_super_categories()
+                        [Category of finite dimensional vector spaces with basis over Rational Field]
+                        sage: VectorSpaces(QQ).WithBasis().FiniteDimensional().TensorProducts().FiniteDimensional()
+                        Category of tensor products of finite dimensional vector spaces with basis over Rational Field
+
+                    """
+                    return [self.base_category()]
+
         class Graded(GradedModulesCategory):
             """
             Category of graded vector spaces with basis.
@@ -271,7 +290,7 @@ class VectorSpaces(Category_module):
             def extra_super_categories(self):
                 """
                 Implement the fact that a (finite) tensor product of
-                finite dimensional modules is a finite dimensional module.
+                finite dimensional vector spaces is a finite dimensional vector space.
 
                 EXAMPLES::
 

7c91fb4a9a46c5a52cafab012ff7342421e73781
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-07-30 13:53:52 -0700

    src/setup.py: Remove sage-clone-source, sage-sdist from list of installed sagelib scripts

--- src/setup.py
+++ src/setup.py
@@ -131,9 +131,7 @@ code = setup(name = 'sage',
                  'bin/sage-cleaner',
                  ## Only makes sense in sage-the-distribution. TODO: Move to another installation script.
                  'bin/sage-list-packages',
-                 'bin/sage-clone-source',
                  'bin/sage-download-upstream',
-                 'bin/sage-sdist',
                  'bin/sage-location',
                  ## Uncategorized scripts in alphabetical order
                  'bin/math-readline',

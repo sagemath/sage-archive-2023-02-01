07291564d5dd3d19b6a8b115981523e6b7b84c74
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-08-03 17:36:51 -0700

    TensorFreeSubmodule_comp: Factor out _is_symmetry_coarsening_of, add _element_constructor_

--- src/sage/tensor/modules/tensor_free_submodule.py
+++ src/sage/tensor/modules/tensor_free_submodule.py
@@ -35,6 +35,7 @@ class TensorFreeSubmodule_comp(TensorFreeModule):
 
         sage: from sage.tensor.modules.tensor_free_submodule import TensorFreeSubmodule_comp
         sage: M = FiniteRankFreeModule(ZZ, 3, name='M')
+        sage: e = M.basis('e')
         sage: T60M = M.tensor_module(6, 0)
         sage: Sym0123x45M = TensorFreeSubmodule_comp(M, (6, 0), sym=((0, 1, 2, 3), (4, 5)))
         sage: Sym012x345M = TensorFreeSubmodule_comp(M, (6, 0), sym=((0, 1, 2), (3, 4, 5)))
@@ -43,6 +44,11 @@ class TensorFreeSubmodule_comp(TensorFreeModule):
         True
         sage: T60M.has_coerce_map_from(Sym0123x45M)
         True
+        sage: t = e[0]^6
+        sage: t.parent()
+        FIXME
+        sage: Sym012345M(t) is t
+        FIXME
 
     TESTS::
 
@@ -86,6 +92,73 @@ class TensorFreeSubmodule_comp(TensorFreeModule):
         return "Free module of type-({},{}) tensors with {} on the {}".format(
             self._tensor_type[0], self._tensor_type[1], self._comp, self._fmodule)
 
+    def _is_symmetry_coarsening_of(self, coarser_comp, finer_comp):
+        self_tensor_type = self.tensor_type()
+
+        def sym_antisym(comp):
+            if isinstance(comp, tuple):
+                sym, antisym = tuple
+                if sym is None:
+                    sym = []
+                if antisym is None:
+                    antisym = []
+                return sym, antisym
+            # Similar code is in Component.contract, should refactor.
+            try:
+                return comp._sym, comp._antisym
+            except AttributeError:
+                return [], []
+
+        def is_coarsening_of(self_sym_list, other_sym_list):
+            # Use the union-find data structure
+            S = DisjointSet(self_tensor_type[0] + self_tensor_type[1])
+            for index_set in self_sym_list:
+                i = index_set[0]
+                for j in index_set[1:]:
+                    S.union(i, j)
+            for index_set in other_sym_list:
+                i = S.find(index_set[0])
+                for j in index_set[1:]:
+                    if S.find(j) != i:
+                        return False
+            return True
+
+        finer_sym, finer_antisym = sym_antisym(finer_comp)
+        if not finer_sym and not finer_antisym:
+            return True
+        coarser_sym, coarser_antisym = sym_antisym(coarser_comp)
+        if not is_coarsening_of(self._comp._sym, other_sym):
+            return False
+        if not is_coarsening_of(self._comp._antisym, other_antisym):
+            return False
+        return True
+
+    def _element_constructor_(self, comp=[], basis=None, name=None,
+                              latex_name=None, sym=None, antisym=None):
+        if sym is not None or antisym is not None:
+            # Refuse to create a tensor with finer symmetries
+            # than those defining the subspace
+            if not self._is_symmetry_coarsening_of((sym, antisym), self._comp):
+                raise ValueError("cannot create a tensor with symmetries {} as an element of {}".
+                                 format((sym, antisym), self))
+        try:
+            comp_parent = comp.parent()
+        except AttributeError:
+            comp_parent = None
+        if comp_parent == self.ambient_module():
+            resu = comp
+            # comp is already a tensor.  If its declared symmetries are coarser
+            # than the symmetries defining self, we can use it directly.
+            if self._is_symmetry_coarsening_of(resu, self._comp):
+                return resu
+        if sym is None:
+            sym = self._comp._sym
+        if antisym is None:
+            sym = self._comp._antisym
+        resu = super()._element_constructor_(comp=comp, basis=basis, name=name,
+                                             latex_name=latex_name,
+                                             sym=sym, antisym=antisym)
+        return resu
 
     def is_submodule(self, other):
         r"""
@@ -111,7 +184,7 @@ class TensorFreeSubmodule_comp(TensorFreeModule):
             True
 
         """
-        if self == other:
+        if super().is_submodule(other):
             return True
         self_base_module = self.base_module()
         self_tensor_type = self.tensor_type()
@@ -124,28 +197,10 @@ class TensorFreeSubmodule_comp(TensorFreeModule):
             return False
         if self_tensor_type != other_tensor_type:
             return False
-        # Use the union-find data structure
-        def is_coarsening_of(self_sym_list, other_sym_list):
-            S = DisjointSet(self_tensor_type[0] + self_tensor_type[1])
-            for index_set in self_sym_list:
-                i = index_set[0]
-                for j in index_set[1:]:
-                    S.union(i, j)
-            for index_set in other_sym_list:
-                i = S.find(index_set[0])
-                for j in index_set[1:]:
-                    if S.find(j) != i:
-                        return False
-            return True
-        # Similar code is in Component.contract, should refactor.
+
         try:
-            other_sym = other._comp._sym
-            other_antisym = other._comp._antisym
+            other_comp = other._comp
         except AttributeError:
             # other is full tensor module (no symmetry)
             return True
-        if not is_coarsening_of(self._comp._sym, other_sym):
-            return False
-        if not is_coarsening_of(self._comp._antisym, other._comp._antisym):
-            return False
-        return True
+        return self._is_symmetry_coarsening_of(self._comp, other_comp)

4165a9c2bb9a4b887c379a5d0255022d90212e21
Daniel Bump <bump@match.stanford.edu>
2020-08-05 08:44:59 -0700

    pyflakes and pycodestyle requested revisions

--- src/sage/combinat/root_system/fusion_ring.py
+++ src/sage/combinat/root_system/fusion_ring.py
@@ -780,7 +780,6 @@ class FusionRing(WeylCharacterRing):
         if self.Nk_ij(i,j,k) == 0:
             return 0
         rho = self.space().rho()
-        v = [self._nf*x.weight().inner_product(x.weight()+2*rho) for x in [i,j,k]]
         r = self.root_of_unity((k.twist(reduce=False)-i.twist(reduce=False)-j.twist(reduce=False))/2)
         if i != j:
             return r
@@ -951,7 +950,7 @@ class FusionRing(WeylCharacterRing):
                 return twist
 
         def ribbon(self):
-            """
+            r"""
             Returns the twist or ribbon element of ``self``. An additive 
             version of this is available as :meth:`twist`. If `h` is the
             rational number modulo 2 produced by ``self.twist()``, this 

c83f1bed40cd9425e66396b56b1e83e39bda17b4
Stefan Grosser <stefan.grosser1@gmail.com>
2020-08-17 14:14:05 -0400

    changed from ``degree`` to ``property`` and patch ``is_path`` documentation

--- src/sage/graphs/generic_graph.py
+++ src/sage/graphs/generic_graph.py
@@ -10417,7 +10417,7 @@ class GenericGraph(GenericGraph_pyx):
 
         return {v: self._assoc.get(v, None) for v in verts}
 
-    def vertex_iterator(self, vertices=None, degree=None):
+    def vertex_iterator(self, vertices=None, property=None):
         """
         Return an iterator over the given vertices.
 
@@ -10430,8 +10430,9 @@ class GenericGraph(GenericGraph_pyx):
         - ``vertices`` -- iterated vertices are these intersected with the
           vertices of the (di)graph
 
-        - ``degree`` -- a nonnegative integer (default: ``None``) that specifies
-          vertices with the given degree
+        - ``property`` -- function (default: ``None``); a function that inputs
+          a vertex and outputs a boolean value, i.e., a vertex ``v`` is kept if
+          ``property(v) == True``
 
         EXAMPLES::
 
@@ -10472,8 +10473,10 @@ class GenericGraph(GenericGraph_pyx):
             sage: timeit V = list(P.vertex_iterator())      # not tested
             100000 loops, best of 3: 5.74 [micro]s per loop
         """
-        if degree:
-            return [v for v, d in self.degree_iterator(labels=True) if d == degree]
+        if property is not None:
+            for v in self._backend.iterator_verts(vertices):
+                if property(v):
+                    yield v
 
         return self._backend.iterator_verts(vertices)
 
@@ -10551,7 +10554,7 @@ class GenericGraph(GenericGraph_pyx):
         for u in self._backend.iterator_nbrs(vertex):
             yield u
 
-    def vertices(self, sort=True, key=None, degree=None):
+    def vertices(self, sort=True, key=None, property=None):
         r"""
         Return a list of the vertices.
 
@@ -10564,8 +10567,9 @@ class GenericGraph(GenericGraph_pyx):
           vertex as its one argument and returns a value that can be used for
           comparisons in the sorting algorithm (we must have ``sort=True``)
 
-        - ``degree`` -- a nonnegative integer (default: ``None``); an integer
-          that specifies vertices only with the given degree
+        - ``property`` -- function (default: ``None``); a function that inputs
+          a vertex and outputs a boolean value, i.e., a vertex ``v`` is kept if
+          ``property(v) == True``
 
         OUTPUT:
 
@@ -10644,8 +10648,8 @@ class GenericGraph(GenericGraph_pyx):
         if (not sort) and key:
             raise ValueError('sort keyword is False, yet a key function is given')
         if sort:
-            return sorted(self.vertex_iterator(degree=degree), key=key)
-        return list(self.vertex_iterator(degree=degree))
+            return sorted(self.vertex_iterator(property=property), key=key)
+        return list(self.vertex_iterator(property=property))
 
     def neighbors(self, vertex, closed=False):
         """

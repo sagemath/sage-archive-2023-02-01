ae30cf302ded1d18087bf156f535596555befdeb
François Bissey <frp.bissey@gmail.com>
2020-08-19 11:34:50 +1200

    Basic conversion to sage doctrings/doctesting. Remove original copyright/license functions.

--- src/sage/rings/polynomial/pbori/frontend.py
+++ src/sage/rings/polynomial/pbori/frontend.py
@@ -3,29 +3,30 @@ r"""
 This module defines an initial ring, and patches the declare_ring to use
 a given context.
 
+EXAMPLES::
 
-sage: x(0)
-x(0)
-sage: x(0)*x(0)
-x(0)
-sage: x(0) + x(0)
-0
-sage: x(9999)
-x(9999)
-sage: x(9999)*x(9999)
-x(9999)
-sage: x(9999) + x(9999)
-0
+    sage: x(0)
+    x(0)
+    sage: x(0)*x(0)
+    x(0)
+    sage: x(0) + x(0)
+    0
+    sage: x(9999)
+    x(9999)
+    sage: x(9999)*x(9999)
+    x(9999)
+    sage: x(9999) + x(9999)
+    0
 
-sage: from sage.rings.polynomial.pbori.brial.frontend import *
-sage: context = dict(globals())
-sage: polybori_start(context) # doctest: +ELLIPSIS
-ipbori...
-sage: r = context['declare_ring']('abc')
-sage: context['a']
-a
-sage: r.variable(0)
-a
+    sage: from sage.rings.polynomial.pbori.brial.frontend import *
+    sage: context = dict(globals())
+    sage: polybori_start(context)
+    ipbori...
+    sage: r = context['declare_ring']('abc')
+    sage: context['a']
+    a
+    sage: r.variable(0)
+    a
 """
 
 from __future__ import print_function
@@ -36,7 +37,9 @@ from os import environ as env, path as os_path
 
 
 def block_scheme_names(blocks):
-    """Helper for Singular interface."""
+    r"""
+    Helper for Singular interface.
+    """
 
     context = dict()
     from .blocks import declare_block_scheme
@@ -45,29 +48,6 @@ def block_scheme_names(blocks):
     return list(context.keys())
 
 ipbname = 'ipbori'
-
-
-def polybori_copyright():
-    print("""Copyright (c) 2007-2011 by The PolyBoRi Team.
-  Michael Brickenstein (MFO)  brickenstein@mfo.de
-  Alexander Dreyer (ITWM) alexander.dreyer@itwm.fraunhofer.de
-
-The PolyBoRi Team is a joint project of
-  Mathematisches Forschungsinstitut Oberwolfach (MFO), Germany
-  Department of Mathematics, University of Kaiserslautern, Germany, and
-  Fraunhofer Institute for Industrial Mathematics (ITWM), Kaiserslautern, Germany.
-
-PolyBoRi incorporates the following works:
-  The CU Decision Diagrams Package  Release 2.4.1 (CUDD) by Fabio Somenzi,
-    Copyright (c) 1995-2004, Regents of the University of Colorado. All Rights Reserved.
-  The M4RI Library - http://m4ri.sagemath.org
-    Copyright (C) 2007-2010, Martin Albrecht, Gregory Bard, and The M4RI Team""")
-
-
-def polybori_license():
-    print("""ipbori and the PolyBoRi framework are licensed under the terms of
-the GNU General Public License (GPL) version 2 or later.
-See http://www.gnu.org/licenses/ for details.""")
 
 
 def polybori_start(global_context):
@@ -79,8 +59,7 @@ def polybori_start(global_context):
     declare_ring.__doc__ = orig_declare_ring.__doc__
     global_context["declare_ring"] = declare_ring
 
-    print(ipbname + """ -- The interactive command line tool of PolyBoRi %s
-Type "polybori_copyright()" or "polybori_license()" for more information.
+    print(ipbname + """ -- The interactive command line tool of PolyBoRi/BRiAL %s
 """ % global_context.get("polybori_version", ''))
 
 # Here come the defaults

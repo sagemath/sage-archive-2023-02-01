aabb6202fb4b5531ddcabaf4a17756eb89c33f7c
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-09-08 17:54:40 -0700

    src/sage/rings/ideal.py: Fix a pycodestyle warning

--- src/sage/rings/ideal.py
+++ src/sage/rings/ideal.py
@@ -259,7 +259,8 @@ class Ideal_generic(MonoidElement):
             gens = [ring(x) for x in gens]
 
         gens = tuple(gens)
-        if len(gens)==0: gens=(ring.zero(),)
+        if len(gens) == 0:
+            gens = (ring.zero(),)
         self.__gens = gens
         MonoidElement.__init__(self, ring.ideal_monoid())
 

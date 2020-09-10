7c0f5504d48a910d46ffa1dcad68c068894f5b63
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-09-10 13:21:40 -0700

    src/sage_setup/library_order.py: Move giac before ecm

--- src/sage_setup/library_order.py
+++ src/sage_setup/library_order.py
@@ -78,8 +78,8 @@ aliases = cython_aliases()
 
 arb_dylib_name = aliases["ARB_LIBRARY"]
 library_order_list = aliases["SINGULAR_LIBRARIES"] + [
-    "ec", "ecm",
-    "giac", "intl", "curl"
+    "giac", "intl", "curl",
+    "ec", "ecm"
 ] + aliases["LINBOX_LIBRARIES"] + aliases["FFLASFFPACK_LIBRARIES"] + aliases["GSL_LIBRARIES"] + [
     "pari", "flint", "ratpoints", "ecl", "glpk", "ppl",
     arb_dylib_name, "mpfi", "mpfr", "mpc", "ntl", "gmp", "gmpxx",

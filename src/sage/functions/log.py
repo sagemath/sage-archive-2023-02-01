e44937e6f76a1f16258096a2298eb3e5baed8653
gh-bmlivin <togtdyalttai@live.com>
2020-09-13 13:35:53 -0700

    adding empty line and changing track to trac in doctest of sage.functions.log.log

--- src/sage/functions/log.py
+++ src/sage/functions/log.py
@@ -425,7 +425,8 @@ def log(*args, **kwds):
         ...
         TypeError: Symbolic function log takes at most 2 arguments (3 given)
     
-    Check if :track:`29164` is fixed::
+    Check if :trac:`29164` is fixed::
+        
         sage: log(0, 2)
         -Infinity
     """

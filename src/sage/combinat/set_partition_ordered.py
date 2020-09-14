0b9e229f87d4c8a1693334e950f4201d28f7b060
Frédéric Chapoton <chapoton@unistra.fr>
2020-09-14 21:13:06 +0200

    number of inversions for ordered set partitions

--- src/sage/combinat/set_partition_ordered.py
+++ src/sage/combinat/set_partition_ordered.py
@@ -810,6 +810,35 @@ class OrderedSetPartition(ClonableArray,
                 out[letter] = i
         return Words()([out[letter] + 1 for letter in X])
 
+    def number_of_inversions(self):
+        r"""
+        Return the number of inversions in ``self``.
+
+        An inversion of an ordered set partition with blocks `[B_1,B_2,...,B_k]`
+        is a pair of letters `i` and `j` with `i < j` such that `i` is minimal
+        in `B_m`, `j ∈ B_l`, and `l < m`.
+
+        REFERENCES:
+
+        .. A. T. Wilson.
+        *An extension of MacMahon’s Equidistribution Theorem to ordered multiset partitions*.
+        Electron. J. Combin., **23** (1) (2016), P1.5.
+
+        EXAMPLES::
+
+            sage: OrderedSetPartition([{2,5},{4,6},{1,3}]).number_of_inversions()
+            5
+            sage: OrderedSetPartition([{1,3,8},{2,4},{5,6,7}]).number_of_inversions()
+            3
+        """
+        num_invs = 0
+        for i in range(len(self)):
+            for j in range(i + 1, len(self)):
+                for a in self[i]:
+                    if a > min(self[j]):
+                        num_invs += 1
+        return num_invs
+
 
 class OrderedSetPartitions(UniqueRepresentation, Parent):
     """

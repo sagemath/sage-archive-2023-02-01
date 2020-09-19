fa10a901256c0c133b5957a48cc57fd5a8a9832f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-09-19 12:01:56 -0700

    src/sage/features/__init__.py: Add doctests

--- src/sage/features/__init__.py
+++ src/sage/features/__init__.py
@@ -389,18 +389,33 @@ class PackageSystem(Feature):
     EXAMPLES::
 
         sage: from sage.features import PackageSystem
-        sage: homebrew = PackageSystem('homebrew')
-        sage: homebrew.spkg_installation_hint('openblas')  # optional - SAGE_ROOT
-        'To install openblas using the homebrew package manager, you can try to run:\n!brew install openblas'
-
+        sage: PackageSystem('conda')
+        Feature('conda')
     """
     def _is_present(self):
         """
         Test whether ``self`` appears in the list of available package systems.
+
+        EXAMPLES::
+
+            sage: from sage.features import PackageSystem
+            sage: debian = PackageSystem('debian')
+            sage: debian.is_present()  # indirect doctest, random
+            True
         """
         return self in package_systems()
 
     def spkg_installation_hint(self, spkgs, *, prompt="  !", feature=None):
+        """
+        Return a string that explains how to install ``feature``.
+
+        EXAMPLES::
+
+            sage: from sage.features import PackageSystem
+            sage: homebrew = PackageSystem('homebrew')
+            sage: homebrew.spkg_installation_hint('openblas')  # optional - SAGE_ROOT
+            'To install openblas using the homebrew package manager, you can try to run:\n!brew install openblas'
+        """
         if isinstance(spkgs, (tuple, list)):
             spkgs = ' '.join(spkgs)
         if feature is None:
@@ -408,6 +423,18 @@ class PackageSystem(Feature):
         return self._spkg_installation_hint(spkgs, prompt, feature)
 
     def _spkg_installation_hint(self, spkgs, prompt, feature):
+        """
+        Return a string that explains how to install ``feature``.
+
+        Override this method in derived classes.
+
+        EXAMPLES::
+
+            sage: from sage.features import PackageSystem
+            sage: fedora = PackageSystem('fedora')
+            sage: fedora.spkg_installation_hint('openblas')  # optional - SAGE_ROOT
+            'To install openblas using the fedora package manager, you can try to run:\n!sudo yum install openblas-devel'
+        """
         from subprocess import run, CalledProcessError
         lines = []
         system = self.name
@@ -430,17 +457,35 @@ class PackageSystem(Feature):
 class SagePackageSystem(PackageSystem):
     r"""
     The feature describing the Sage package manager.
+
+    EXAMPLES::
+
+        sage: from sage.features import SagePackageSystem
+        sage: SagePackageSystem()
+        Feature('sage-spkg')
     """
     @staticmethod
     def __classcall__(cls):
         """
         Normalize initargs.
+
+        TESTS::
+
+            sage: from sage.features import SagePackageSystem
+            sage: SagePackageSystem() is SagePackageSystem()  # indirect doctest
+            True
         """
         return PackageSystem.__classcall__(cls, "sage-spkg")
 
     def _is_present(self):
         """
         Test whether ``sage-spkg`` is available.
+
+        EXAMPLES::
+
+            sage: from sage.features import SagePackageSystem
+            sage: SagePackageSystem().is_present()  # indirect doctest, optional - SAGE_ROOT
+            True
         """
         from subprocess import run, DEVNULL, CalledProcessError
         try:
@@ -451,6 +496,16 @@ class SagePackageSystem(PackageSystem):
             return False
 
     def _spkg_installation_hint(self, spkgs, prompt, feature):
+        """
+        Return a string that explains how to install ``feature``.
+
+        EXAMPLES::
+
+            sage: from sage.features import SagePackageSystem
+            sage: print(SagePackageSystem().spkg_installation_hint(['foo', 'bar'], prompt="### ", feature='foobarability'))  # indirect doctest
+            To install foobarability using the Sage package manager, you can try to run:
+            ### sage -i foo bar
+        """
         lines = []
         lines.append(f'To install {feature} using the Sage package manager, you can try to run:')
         lines.append(f'{prompt}sage -i {spkgs}')
@@ -459,17 +514,35 @@ class SagePackageSystem(PackageSystem):
 class PipPackageSystem(PackageSystem):
     r"""
     The feature describing the Pip package manager.
+
+    EXAMPLES::
+
+        sage: from sage.features import PipPackageSystem
+        sage: PipPackageSystem()
+        Feature('pip')
     """
     @staticmethod
     def __classcall__(cls):
         """
         Normalize initargs.
+
+        TESTS::
+
+            sage: from sage.features import PipPackageSystem
+            sage: PipPackageSystem() is PipPackageSystem()  # indirect doctest
+            True
         """
         return PackageSystem.__classcall__(cls, "pip")
 
     def _is_present(self):
         """
         Test whether ``pip`` is available.
+
+        EXAMPLES::
+
+            sage: from sage.features import PipPackageSystem
+            sage: bool(PipPackageSystem().is_present())    # indirect doctest
+            True
         """
         from subprocess import run, DEVNULL, CalledProcessError
         try:

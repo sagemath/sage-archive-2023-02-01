a854438114e06147248cd413b483e5f11b4e2aff
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-09-23 04:45:15 -0700

    DocTestController.__init__: Only add package systems when 'optional' is used

--- src/sage/doctest/control.py
+++ src/sage/doctest/control.py
@@ -363,8 +363,8 @@ class DocTestController(SageObject):
                         if pkg['installed'] and pkg['installed_version'] == pkg['remote_version']:
                             options.optional.add(pkg['name'])
 
-                from sage.features import package_systems
-                options.optional.update(system.name for system in package_systems())
+                    from sage.features import package_systems
+                    options.optional.update(system.name for system in package_systems())
 
                 # Check that all tags are valid
                 for o in options.optional:

18f03643e854a4f322f17bbe079a68784379c44a
gh-bmlivin <togtdyalttai@live.com>
2020-10-18 16:47:43 -0700

    actually check if result of division fits in long instead of in int

--- src/sage/symbolic/ginac/numeric.cpp
+++ src/sage/symbolic/ginac/numeric.cpp
@@ -3241,8 +3241,8 @@ long numeric::to_long() const {
                 mpz_init(bigint);
                 mpz_fdiv_q(bigint, mpq_numref(v._bigrat),
                                 mpq_denref(v._bigrat));
-                if (mpz_fits_sint_p(v._bigint)) {
-                        long n = mpz_get_si(bigint);
+                if (mpz_fits_slong_p(v._bigint)) {
+                        long n = (long int)mpz_get_si(bigint);
                         mpz_clear(bigint);
                         return n;
                 }

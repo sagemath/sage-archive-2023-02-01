532ad6ed0fb3682a9e1a8714415eccbb4a393713
Marc Mezzarobba <marc@mezzarobba.net>
2020-11-03 18:47:42 +0100

    #30857 additional compatibility wrappers after #24483

--- src/sage/rings/complex_field.py
+++ src/sage/rings/complex_field.py
@@ -1,18 +1,17 @@
 r"""
-Deprecated import from :trac:`24483`.
+Deprecated in favor of :mod:`sage.rings.complex_mpfr`
 
 TESTS::
 
     sage: from sage.rings.complex_field import ComplexField
-    sage: ComplexField()
     doctest:warning
     ...
-    Importing ComplexField from here is deprecated. If you need to use it, please import it directly from sage.rings.complex_mpfr
+    DeprecationWarning: the complex_field module is deprecated, please use sage.rings.complex_mpfr
     See http://trac.sagemath.org/24483 for details.
+    sage: ComplexField()
     Complex Field with 53 bits of precision
 """
 
-from sage.misc.lazy_import import lazy_import
-
-lazy_import('sage.rings.complex_mpfr', 'ComplexField', deprecation=24483)
-
+from sage.misc.superseded import deprecation
+from sage.rings.complex_mpfr import *
+deprecation(24483, "the complex_field module is deprecated, please use sage.rings.complex_mpfr")

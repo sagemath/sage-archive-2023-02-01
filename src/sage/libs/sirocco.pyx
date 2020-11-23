98c2ffd75c16e5bc8266405911f532141165feb3
Miguel Marco <mmarco@unizar.es>
2020-11-23 14:12:24 +0100

    Add interface to new sirocco functions

--- src/sage/libs/sirocco.pyx
+++ src/sage/libs/sirocco.pyx
@@ -23,7 +23,8 @@ from sage.rings.real_mpfr import RealField
 cdef extern from "sirocco.h":
     mpfr_t* homotopyPath_mp(int degree, mpfr_t *_coef, mpfr_t _y0R, mpfr_t _y0I, int prec)
     double* homotopyPath(int degree, double *_coef, double _y0R, double _y0I)
-
+    mpfr_t* homotopyPath_mp_comps(int degree, mpfr_t *_coef, mpfr_t _y0R, mpfr_t _y0I, int prec, int nothercomps, int *degreescomps, mpfr_t *_coefscomps)
+    double* homotopyPath_comps(int degree, double *_coef, double _y0R, double _y0I, int nothercomps, int *degreescomps, double *_coefscomps)
 
 
 cpdef list[list] contpath_mp(int deg, list values, RealNumber y0r, RealNumber y0i, int prec):
@@ -76,6 +77,71 @@ cpdef list[list] contpath_mp(int deg, list values, RealNumber y0r, RealNumber y0
     free(rop)
     return l
 
+cpdef list[list] contpath_mp_comps(int deg, list values, RealNumber y0r, RealNumber y0i, int prec, list otherdegs, list othercoefs):
+    """
+    Mimics :func:`contpath`, but with the following differences:
+
+    - The floating point numbers can be arbitrary precision RealNumbers.
+
+    - A extra argument is needed, indicating the bits of precision used
+      in the computations.
+    """
+    cdef mpfr_t* cvalues = <mpfr_t*> check_allocarray(len(values), sizeof(mpfr_t))
+    cdef mpfr_t* cothercoefs = <mpfr_t*> check_allocarray(len(othercoefs), sizeof(mpfr_t))
+    cdef int* cotherdegs = <int*> check_allocarray(len(otherdegs), sizeof(int))
+    cdef mpfr_t* rop
+    cdef int i, j
+    cdef mpfr_t y0R
+    cdef mpfr_t y0I
+
+    for j in range(len(values)):
+        mpfr_init2(cvalues[j], prec)
+        mpfr_set(cvalues[j], (<RealNumber>values[j]).value, MPFR_RNDN)
+
+    for j in range(len(othercoefs)):
+        mpfr_init2(cothercoefs[j], prec)
+        mpfr_set(cothercoefs[j], (<RealNumber>othercoefs[j]).value, MPFR_RNDN)
+
+    for j in range(len(otherdegs)):
+        cotherdegs[j] = int(otherdegs[j])
+
+
+    sig_on()
+    mpfr_init2(y0R, prec)
+    mpfr_set(y0R, (<RealNumber>y0r).value, MPFR_RNDN)
+    mpfr_init2(y0I, prec)
+    mpfr_set(y0I, (<RealNumber>y0i).value, MPFR_RNDN)
+    rop = homotopyPath_mp_comps(deg, cvalues, y0R, y0I, prec, int(len(otherdegs)), cotherdegs, cothercoefs)
+    sig_off()
+
+    for j in range(len(values)):
+        mpfr_clear(cvalues[j])
+    free(cvalues)
+
+    for j in range(len(othercoefs)):
+        mpfr_clear(cothercoefs[j])
+    free(cothercoefs)
+    free(cotherdegs)
+    if rop == NULL:
+        raise ValueError("libsirocco could not guarantee one step")
+
+    cdef int n = mpfr_get_si(rop[0], MPFR_RNDN)
+    cdef list l = []
+    cdef list inner
+    cdef RealNumber RN
+    field = RealField(prec)
+    for i in range(n):
+        inner = []
+        for j in range(3*i+1, 3*(i+1)+1):
+            RN = <RealNumber> RealNumber.__new__(RealNumber, field)
+            mpfr_set(RN.value, rop[j], MPFR_RNDN)
+            mpfr_clear(rop[j])
+            inner.append(RN)
+        l.append(tuple(inner))
+    free(rop)
+    return l
+
+
 cpdef list[list] contpath(int deg, list values, double y0r, double y0i):
     """
     INPUT:
@@ -119,3 +185,55 @@ cpdef list[list] contpath(int deg, list values, double y0r, double y0i):
     free(c_values)
     return l
 
+cpdef list[list] contpath_comps(int deg, list values, double y0r, double y0i, list otherdegrees, list othercoefs):
+    """
+    INPUT:
+
+    - An integer, representing the degree of the polynomial
+
+    - A list of floating point numbers. Each four consecutive elements
+      of this list represent the interval corresponding to a coefficient.
+      Coefficients are listed in increasing deglex order, and inside each
+      coefficients, the four numbers represent the lower real, upper real,
+      lower imaginary and real imaginary limits of the interval.
+
+    - A float representing the real part of the initial root approximation
+
+    - A float representing the imaginary part of the initial root.
+
+    OUTPUT:
+
+    A list of tuples. Each tuple represents the `x` value (between 0 and 1)
+    and the real and imaginary parts of the `y` value of a vertex in
+    the piecewise linear approximation of the path tracked by the root.
+    """
+    cdef double* rop
+    cdef double* c_values = <double*> check_allocarray(len(values), sizeof(double))
+    cdef int* c_otherdegrees = <int*> check_allocarray(len(otherdegrees), sizeof(int))
+    cdef double* c_othercoefs = <double*> check_allocarray(len(othercoefs), sizeof(double))
+    cdef int clen = <int> len(values)
+    cdef int i
+    for i,v in enumerate(values):
+        c_values[i] = values[i]
+
+    for i,v in enumerate(otherdegrees):
+        c_otherdegrees[i] = otherdegrees[i]
+    for i,v in enumerate(othercoefs):
+        c_othercoefs[i] = othercoefs[i]
+
+    cdef double y0R = y0r
+    cdef double y0I = y0i
+    sig_on()
+    rop = homotopyPath_comps(deg, c_values, y0R, y0I, int(len(otherdegrees)), c_otherdegrees, c_othercoefs)
+    sig_off()
+    if rop == NULL:
+        raise ValueError("libsirocco could not guarantee one step")
+    cdef int n = int(rop[0])
+    cdef list l = [0] * n
+    for i in range(n):
+        l[i] = (rop[3*i+1], rop[3*i+2], rop[3*i+3])
+    free(rop)
+    free(c_values)
+    free(c_otherdegrees)
+    free(c_othercoefs)
+    return l

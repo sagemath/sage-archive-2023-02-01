234388bc73118522487d0189c72c3e2a3d9776ee
Tobias Diez <code@tobiasdiez.com>
2020-12-04 12:33:27 +0100

    Add minimal pytest config

---
+++ src/conftest.py
@@ -0,0 +1,22 @@
+from typing import Any
+from __future__ import annotations
+import pytest
+
+# Ignore a few test files that are (not yet) using pytest
+collect_ignore = [
+    "sage/libs/gap/test_long.py",
+    "sage/structure/test_factory.py",
+    "sage/misc/nested_class_test.py",
+    "sage/repl/rich_output/backend_test.py"
+]
+
+
+@pytest.fixture(autouse=True)
+def add_imports(doctest_namespace: dict[str, Any]):
+    """
+    Add global imports for doctests.
+
+    See `pytest documentation <https://docs.pytest.org/en/stable/doctest.html#doctest-namespace-fixture>`.
+    """
+    import sage.all  # type: ignore # implicitly used below by calling locals()
+    doctest_namespace.update(**locals())

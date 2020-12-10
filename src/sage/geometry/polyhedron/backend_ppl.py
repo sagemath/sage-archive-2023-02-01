8bd41ea2580fc7bf3c5d65ccd6cdcec68d9ef5c4
Jonathan Kliem <jonathan.kliem@fu-berlin.de>
2020-12-10 11:19:17 +0100

    lazy import ppl in backend_ppl

--- src/sage/geometry/polyhedron/backend_ppl.py
+++ src/sage/geometry/polyhedron/backend_ppl.py
@@ -6,11 +6,10 @@ from sage.rings.all import ZZ
 from sage.rings.integer import Integer
 from sage.arith.functions import LCM_list
 from sage.misc.functional import denominator
-from ppl import (
-    C_Polyhedron, Constraint_System, Generator_System,
-    Linear_Expression,
-    line, ray, point
-)
+from sage.features import PythonModule
+lazy_import('ppl', ['C_Polyhedron', 'Generator_System', 'Constraint_System',
+                    'Linear_Expression', 'line', 'ray', 'point'],
+                    feature=PythonModule("ppl", spkg="pplpy"))
 from .base import Polyhedron_base
 from .base_QQ import Polyhedron_QQ
 from .base_ZZ import Polyhedron_ZZ

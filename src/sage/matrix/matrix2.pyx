813fbb2dc73923662deb78ebbafe3c394ddb86d5
Xavier Caruso <xavier.caruso@normalesup.org>
2020-12-10 23:23:25 +0100

    move _charpoly_over_number_field to the NumberField class

--- src/sage/matrix/matrix2.pyx
+++ src/sage/matrix/matrix2.pyx
@@ -2872,11 +2872,7 @@ cdef class Matrix(Matrix1):
             if hasattr(R, '_matrix_charpoly'):
                 f = R._matrix_charpoly(self, var)
             if f is None:
-                if is_NumberField(R):
-                    f = self._charpoly_over_number_field(var)
-                elif is_IntegerModRing(R):
-                    f = self.lift().charpoly(var).change_ring(R)
-                elif R in _Fields and R.is_exact():
+                if R in _Fields and R.is_exact():
                     f = self._charpoly_hessenberg(var)
                 else:
                     f = self._charpoly_df(var)
@@ -3054,53 +3050,6 @@ cdef class Matrix(Matrix1):
         f = X ** n + S(list(reversed(F)))
 
         return f
-
-    def _charpoly_over_number_field(self, var='x'):
-        r"""
-        Use PARI to compute the characteristic polynomial of self as a
-        polynomial over the base ring.
-
-        EXAMPLES::
-
-            sage: x = QQ['x'].gen()
-            sage: K.<a> = NumberField(x^2 - 2)
-            sage: m = matrix(K, [[a-1, 2], [a, a+1]])
-            sage: m._charpoly_over_number_field('Z')
-            Z^2 - 2*a*Z - 2*a + 1
-            sage: m._charpoly_over_number_field('a')(m) == 0
-            True
-            sage: m = matrix(K, [[0, a, 0], [-a, 0, 0], [0, 0, 0]])
-            sage: m._charpoly_over_number_field('Z')
-            Z^3 + 2*Z
-
-        The remaining tests are indirect::
-
-            sage: L.<b> = K.extension(x^3 - a)
-            sage: m = matrix(L, [[b+a, 1], [a, b^2-2]])
-            sage: m.charpoly('Z')
-            Z^2 + (-b^2 - b - a + 2)*Z + a*b^2 - 2*b - 2*a
-            sage: m.charpoly('a')
-            a^2 + (-b^2 - b - a + 2)*a + a*b^2 - 2*b - 2*a
-            sage: m.charpoly('a')(m) == 0
-            True
-
-        ::
-
-            sage: M.<c> = L.extension(x^2 - a*x + b)
-            sage: m = matrix(M, [[a+b+c, 0, b], [0, c, 1], [a-1, b^2+1, 2]])
-            sage: f = m.charpoly('Z'); f
-            Z^3 + (-2*c - b - a - 2)*Z^2 + ((b + 2*a + 4)*c - b^2 + (-a + 2)*b + 2*a - 1)*Z + (b^2 + (a - 3)*b - 4*a + 1)*c + a*b^2 + 3*b + 2*a
-            sage: f(m) == 0
-            True
-            sage: f.base_ring() is M
-            True
-        """
-        K = self.base_ring()
-        if not is_NumberField(K):
-            raise ValueError("_charpoly_over_number_field called with base ring (%s) not a number field" % K)
-
-        paripoly = self.__pari__().charpoly()
-        return K[var](paripoly)
 
     def fcp(self, var='x'):
         """

f4a0c4a19b7cb07df3cb95295fc5d3b794e0c7d5
Tobias Diez <code@tobiasdiez.com>
2020-12-26 21:17:24 +0100

    Create SageObJectTest

--- src/sage/numerical/backends/generic_backend_test.py
+++ src/sage/numerical/backends/generic_backend_test.py
@@ -1,16 +1,22 @@
 import pytest
 from .generic_backend import GenericBackend
+from sage.structure.sage_object import SageObject
+from sage.structure.sage_object_test import SageObjectTests
 
-class GenericBackendTests:
+class GenericBackendTests(SageObjectTests):
 
     @pytest.fixture
-    def backend(self) -> GenericBackend:
+    def backend(self, *args, **kwargs) -> GenericBackend:
         raise NotImplementedError
+
+    @pytest.fixture
+    def sage_object(self, backend) -> SageObject:
+        return backend
 
     def test_ncols_nonnegative(self, backend: GenericBackend):
         assert backend.ncols() >= 0
 
-    def test_sage_unittest_testsuite(self, backend: GenericBackend):
+    def test_sage_unittest_testsuite(self, sage_object: SageObject):
         # TODO: Remove this test as soon as all old test methods are migrated
         from sage.misc.sage_unittest import TestSuite
-        TestSuite(backend).run(verbose=True, raise_on_failure=True, skip="_test_pickling")
+        TestSuite(sage_object).run(verbose=True, raise_on_failure=True, skip="_test_pickling")

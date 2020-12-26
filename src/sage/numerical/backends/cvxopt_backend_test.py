d1f14f011c23a1a218400af7400edb7e1c613c0b
Tobias Diez <code@tobiasdiez.com>
2020-12-26 20:27:41 +0100

    Incorporate feedback

--- src/sage/numerical/backends/cvxopt_backend_test.py
+++ src/sage/numerical/backends/cvxopt_backend_test.py
@@ -9,7 +9,7 @@ class TestCVXOPTBackend(GenericBackendTests):
     def backend(self) -> GenericBackend:
         return MixedIntegerLinearProgram(solver="CVXOPT").get_backend() 
 
-    def test_old_testsuite(self, backend: GenericBackend):
+    def test_sage_unittest_testsuite(self, backend: GenericBackend):
         # TODO: Remove this test as soon as all old test methods are migrated
         from sage.misc.sage_unittest import TestSuite
         TestSuite(backend).run(verbose=True, raise_on_failure=True, skip=("_test_pickling","_test_solve","_test_solve_trac_18572"))
--- src/sage/numerical/backends/generic_backend_test.py
+++ src/sage/numerical/backends/generic_backend_test.py
@@ -10,7 +10,7 @@ class GenericBackendTests:
     def test_ncols_nonnegative(self, backend: GenericBackend):
         assert backend.ncols() >= 0
 
-    def test_old_testsuite(self, backend: GenericBackend):
+    def test_sage_unittest_testsuite(self, backend: GenericBackend):
         # TODO: Remove this test as soon as all old test methods are migrated
         from sage.misc.sage_unittest import TestSuite
         TestSuite(backend).run(verbose=True, raise_on_failure=True, skip="_test_pickling")

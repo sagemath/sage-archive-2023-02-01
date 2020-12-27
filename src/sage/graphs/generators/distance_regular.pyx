74da7a90e5a04384837a9732fd456190a0b2bad8
Dima Pasechnik <dima@pasechnik.info>
2020-12-27 23:21:27 +0000

    another missing optional tag added

--- src/sage/graphs/generators/distance_regular.pyx
+++ src/sage/graphs/generators/distance_regular.pyx
@@ -2095,7 +2095,7 @@ def graph_with_classical_parameters(int d, int b, alpha_in, beta_in, int gamma):
         Symplectic Dual Polar Graph DSp(6, 2): Graph on 135 vertices
         sage: graph_with_classical_parameters(3, 2, 2, 14, 7)
         Grassmann graph J_2(6, 3): Graph on 1395 vertices
-        sage: graph_with_classical_parameters(3, -2, -2, 6, 6)
+        sage: graph_with_classical_parameters(3, -2, -2, 6, 6) # optional - gap_packages internet
         Generalised hexagon of order (2, 8): Graph on 819 vertices
     """
     from sage.rings.rational import Rational

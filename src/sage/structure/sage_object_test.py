3c06234e703737f5ddaeb09caa4161ab26578ba7
Tobias Diez <code@tobiasdiez.com>
2020-12-27 10:44:00 +0100

    Add docstring

--- src/sage/structure/sage_object_test.py
+++ src/sage/structure/sage_object_test.py
@@ -9,6 +9,9 @@ class SageObjectTests:
         raise NotImplementedError
 
     def test_sage_unittest_testsuite(self, sage_object: SageObject):
+        """
+        Subclasses should override this method if they need to skip some tests.
+        """
         # TODO: Remove this test as soon as all old test methods are migrated
         from sage.misc.sage_unittest import TestSuite
         TestSuite(sage_object).run(verbose=True, raise_on_failure=True)

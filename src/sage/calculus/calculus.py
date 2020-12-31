5101846c85fbd2064023db7ded02900d7ff2ab19
Dave Witte Morris <Dave.Morris@uleth.ca>
2020-12-30 22:50:19 -0700

    another doctest for trac 14821

--- src/sage/calculus/calculus.py
+++ src/sage/calculus/calculus.py
@@ -399,6 +399,8 @@ To check that :trac:`14821` is fixed::
     sage: H = exp(-1.0 * x)
     sage: H.integral(x, 0, 1)
     0.6321205588285577
+    sage: integral(exp(-300.0/(-0.064*x+14.0)),x,0.0,120.0)
+    4.62770039817000e-9
 """
 
 import re

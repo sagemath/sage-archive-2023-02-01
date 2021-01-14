78f7af381e3bd01f621001e1947057844bd8e276
Michael Jung <m.jung@vu.nl>
2021-01-14 09:58:56 +0100

    Trac #31233: category of homset docstring adapted

--- src/sage/manifolds/differentiable/integrated_curve.py
+++ src/sage/manifolds/differentiable/integrated_curve.py
@@ -657,9 +657,8 @@ class IntegratedCurve(DifferentiableCurve):
             sage: c.__reduce__()
             (<class 'sage.manifolds.differentiable.manifold_homset.IntegratedCurveSet_with_category.element_class'>,
              (Set of Morphisms from Real interval (0, 5) to
-              3-dimensional differentiable manifold M in Category of
-              homsets of subobjects of sets and topological spaces which
-              actually are integrated curves,
+              3-dimensional differentiable manifold M in Category of homsets of
+              topological spaces which actually are integrated curves,
               [B_0*Dx2*q*t*e^(-(x1^2 + x2^2)/L^2)/(T*m),
                -B_0*Dx1*q*t*e^(-(x1^2 + x2^2)/L^2)/(T*m),
                0],
@@ -3537,10 +3536,9 @@ class IntegratedAutoparallelCurve(IntegratedCurve):
             sage: c.__reduce__()
             (<class 'sage.manifolds.differentiable.manifold_homset.IntegratedAutoparallelCurveSet_with_category.element_class'>,
              (Set of Morphisms from Real interval (0, 5) to
-              3-dimensional differentiable manifold M in Category of
-              homsets of subobjects of sets and topological spaces which
-              actually are integrated autoparallel curves with respect to
-              a certain affine connection,
+              3-dimensional differentiable manifold M in Category of homsets of
+              topological spaces which actually are integrated autoparallel
+              curves with respect to a certain affine connection,
               Affine connection nabla on the 3-dimensional
               differentiable manifold M,
               t,
@@ -3917,10 +3915,9 @@ class IntegratedGeodesic(IntegratedAutoparallelCurve):
             sage: c.__reduce__()
             (<...IntegratedGeodesicSet_with_category.element_class'>,
              (Set of Morphisms from Real interval (0, pi) to
-              2-dimensional Riemannian manifold S^2 in Category of
-              homsets of subobjects of sets and topological spaces which
-              actually are integrated geodesics with respect to a certain
-              metric,
+              2-dimensional Riemannian manifold S^2 in Category of homsets of
+              topological spaces which actually are integrated geodesics with
+              respect to a certain metric,
               Riemannian metric g on the 2-dimensional Riemannian
               manifold S^2,
               t,

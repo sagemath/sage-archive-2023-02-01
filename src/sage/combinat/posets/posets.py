a4354a645daee99506e76dee77565e2949b80895
Frédéric Chapoton <chapoton@unistra.fr>
2021-01-15 11:36:25 +0100

    some details in posets.py

--- src/sage/combinat/posets/posets.py
+++ src/sage/combinat/posets/posets.py
@@ -861,6 +861,19 @@ class FinitePoset(UniqueRepresentation, Parent):
         sage: parent(QP[0]) is QP
         True
 
+    Conversion to some other software is possible::
+
+        sage: P = posets.TamariLattice(3)
+        sage: libgap(P)  # optional - gap_packages
+        <A poset on 5 points>
+
+        sage: P = Poset({1:[2],2:[]})
+        sage: macaulay2('needsPackage "Posets"')   # optional - macaulay2
+        Posets
+        sage: macaulay2(P)   # optional - macaulay2
+        Relation Matrix: | 1 1 |
+                         | 0 1 |
+
     .. NOTE::
 
        A class that inherits from this class needs to define
@@ -1776,7 +1789,7 @@ class FinitePoset(UniqueRepresentation, Parent):
                 new_a_spec[-1] += k_val * a_spec[i - 1] * n_lin_exts
         return new_a_spec
 
-    def is_linear_extension(self, l):
+    def is_linear_extension(self, l) -> bool:
         """
         Return whether ``l`` is a linear extension of ``self``.
 
@@ -2245,7 +2258,7 @@ class FinitePoset(UniqueRepresentation, Parent):
                     w
 
         Thus each edge represents a cover relation in the Hasse diagram.
-        We represent his as the tuple `(w, x, y, z)`.
+        We represent this as the tuple `(w, x, y, z)`.
 
         OUTPUT:
 
@@ -2281,7 +2294,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         vertices = list(map(self._element_to_vertex, elmts))
         return list(map(self._vertex_to_element, self._hasse_diagram.common_upper_covers(vertices)))
 
-    def is_d_complete(self):
+    def is_d_complete(self) -> bool:
         r"""
         Return ``True`` if a poset is d-complete and ``False`` otherwise.
 
@@ -2549,7 +2562,7 @@ class FinitePoset(UniqueRepresentation, Parent):
     # Maybe this should also be deprecated.
     intervals_number = relations_number
 
-    def is_incomparable_chain_free(self, m, n=None):
+    def is_incomparable_chain_free(self, m, n=None) -> bool:
         r"""
         Return ``True`` if the poset is `(m+n)`-free, and ``False`` otherwise.
 
@@ -2666,7 +2679,7 @@ class FinitePoset(UniqueRepresentation, Parent):
                 return False
         return True
 
-    def is_lequal(self, x, y):
+    def is_lequal(self, x, y) -> bool:
         """
         Return ``True`` if `x` is less than or equal to `y` in the poset, and
         ``False`` otherwise.
@@ -2691,7 +2704,7 @@ class FinitePoset(UniqueRepresentation, Parent):
 
     le = is_lequal
 
-    def is_less_than(self, x, y):
+    def is_less_than(self, x, y) -> bool:
         """
         Return ``True`` if `x` is less than but not equal to `y` in the poset,
         and ``False`` otherwise.
@@ -2720,7 +2733,7 @@ class FinitePoset(UniqueRepresentation, Parent):
 
     lt = is_less_than
 
-    def is_gequal(self, x, y):
+    def is_gequal(self, x, y) -> bool:
         """
         Return ``True`` if `x` is greater than or equal to `y` in the poset,
         and ``False`` otherwise.
@@ -2743,7 +2756,7 @@ class FinitePoset(UniqueRepresentation, Parent):
 
     ge = is_gequal
 
-    def is_greater_than(self, x, y):
+    def is_greater_than(self, x, y) -> bool:
         """
         Return ``True`` if `x` is greater than but not equal to `y` in the
         poset, and ``False`` otherwise.
@@ -3024,7 +3037,7 @@ class FinitePoset(UniqueRepresentation, Parent):
             return False
         return True
 
-    def is_bounded(self):
+    def is_bounded(self) -> bool:
         """
         Return ``True`` if the poset is bounded, and ``False`` otherwise.
 
@@ -3057,7 +3070,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         """
         return self._hasse_diagram.is_bounded()
 
-    def is_chain(self):
+    def is_chain(self) -> bool:
         """
         Return ``True`` if the poset is totally ordered ("chain"), and
         ``False`` otherwise.
@@ -3083,7 +3096,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         """
         return self._hasse_diagram.is_chain()
 
-    def is_chain_of_poset(self, elms, ordered=False):
+    def is_chain_of_poset(self, elms, ordered=False) -> bool:
         """
         Return ``True`` if ``elms`` is a chain of the poset,
         and ``False`` otherwise.
@@ -3180,7 +3193,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         elms_H = [self._element_to_vertex(e) for e in elms]
         return self._hasse_diagram.is_antichain_of_poset(elms_H)
 
-    def is_connected(self):
+    def is_connected(self) -> bool:
         """
         Return ``True`` if the poset is connected, and ``False`` otherwise.
 
@@ -3209,7 +3222,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         """
         return self._hasse_diagram.is_connected()
 
-    def is_series_parallel(self):
+    def is_series_parallel(self) -> bool:
         """
         Return ``True`` if the poset is series-parallel, and ``False``
         otherwise.
@@ -3737,7 +3750,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         else:
             raise ValueError("the poset is not ranked")
 
-    def is_ranked(self):
+    def is_ranked(self) -> bool:
         r"""
         Return ``True`` if the poset is ranked, and ``False`` otherwise.
 
@@ -3766,7 +3779,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         """
         return bool(self.rank_function())
 
-    def is_graded(self):
+    def is_graded(self) -> bool:
         r"""
         Return ``True`` if the poset is graded, and ``False`` otherwise.
 
@@ -7538,7 +7551,7 @@ class FinitePoset(UniqueRepresentation, Parent):
         """
         return self.linear_extension().evacuation().to_poset()
 
-    def is_rank_symmetric(self):
+    def is_rank_symmetric(self) -> bool:
         r"""
         Return ``True`` if the poset is rank symmetric, and ``False``
         otherwise.

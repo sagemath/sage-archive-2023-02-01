62b1c6177607b85a8d2cc5136bc23d660f0d9931
Tobias Diez <code@tobiasdiez.com>
2021-01-23 22:19:18 +0100

    Igonre deprecation_test as well

--- src/conftest.py
+++ src/conftest.py
@@ -15,7 +15,8 @@ import pytest
 # Ignore a few test files that are (not yet) using pytest
 collect_ignore = [
     "sage/misc/nested_class_test.py",
-    "sage/repl/rich_output/backend_test.py"
+    "sage/repl/rich_output/backend_test.py",
+    "sage/tests/deprecation_test.py"
 ]
 
 

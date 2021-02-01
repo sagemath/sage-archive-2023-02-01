0592226745971878657539a740fa54b065c9d0bb
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-01-31 19:26:31 -0800

    src/sage/numerical/backends/generic_backend.pyx: Restore _test_ncols_nonnegative, with comment

--- src/sage/numerical/backends/generic_backend.pyx
+++ src/sage/numerical/backends/generic_backend.pyx
@@ -867,6 +867,14 @@ cdef class GenericBackend:
         """
 
         raise NotImplementedError()
+
+    def _test_ncols_nonnegative(self, **options):
+        # Trac #31103: This method has already been migrated to pytest (generic_backend_test)
+        # and should be removed as soon as the external sage_numerical_backends packages
+        # are updated to invoke pytest as part of their testsuite.
+        tester = self._tester(**options)
+        p = self
+        tester.assertGreaterEqual(self.ncols(), 0)
     
     cpdef int nrows(self):
         """

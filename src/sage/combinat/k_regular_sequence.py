73f80a3c4620ae33f8a8b6421ac222e1e57bbe24
Gabriel Lipnik <gabriel.lipnik@tugraz.at>
2021-02-17 13:14:50 +0100

    add n1 to recursion_rules

--- src/sage/combinat/k_regular_sequence.py
+++ src/sage/combinat/k_regular_sequence.py
@@ -754,6 +754,7 @@ class kRegularSequenceSpace(RecognizableSeriesSpace):
         u = max(indices_right)
         ll = (floor((l*k**(M-m) - k**M + 1)/(k**(M-m) - 1)) + 1)*(l < 0)
         uu = max([ceil((u*k**(M-m) + k**M - k**m)/(k**(M-m) - 1)) - 1, k**m - 1])
+        n1 = n0 - floor(ll/q^M)
         dim = (k**M - 1)/(k - 1) + (M - m)*(uu - ll - k**m + 1)
 
         recursion_rules = namedtuple('recursion_rules',
@@ -761,8 +762,8 @@ class kRegularSequenceSpace(RecognizableSeriesSpace):
                                       'll', 'uu', 'n_start', 'dim',
                                       'coeffs', 'start_values'])
 
-        return recursion_rules(M=M, m=m, l=l, u=u, ll=ll, uu=uu, n_start=n_start,
-                               dim=dim, coeffs=coeffs, start_values=start_values)
+        return recursion_rules(M=M, m=m, l=l, u=u, ll=ll, uu=uu, dim=dim,
+                               coeffs=coeffs, initial_values=initial_values, n0=n0, n1=n1)
 
 
     def _get_matrix_from_recursions_(self, recursion_rules, rem, function, var):

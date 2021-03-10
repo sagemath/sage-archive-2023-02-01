b713cc1fe9183c4ba559faaabada38cc6de779a0
Jonathan Kliem <jonathan.kliem@fu-berlin.de>
2021-03-10 11:05:49 +0100

    correct module for lazy import

--- src/sage/sandpiles/all.py
+++ src/sage/sandpiles/all.py
@@ -1,12 +1,12 @@
 from sage.misc.lazy_import import lazy_import
 
-lazy_import('sage.sandpile', 'Sandpile')
-lazy_import('sage.sandpile', 'SandpileDivisor')
-lazy_import('sage.sandpile', 'SandpileConfig')
-lazy_import('sage.sandpile', 'random_DAG')
-lazy_import('sage.sandpile', 'firing_graph')
-lazy_import('sage.sandpile', 'parallel_firing_graph')
-lazy_import('sage.sandpile', 'wilmes_algorithm')
-lazy_import('sage.sandpile', 'triangle_sandpile')
+lazy_import('sage.sandpiles.sandpile', 'Sandpile')
+lazy_import('sage.sandpiles.sandpile', 'SandpileDivisor')
+lazy_import('sage.sandpiles.sandpile', 'SandpileConfig')
+lazy_import('sage.sandpiles.sandpile', 'random_DAG')
+lazy_import('sage.sandpiles.sandpile', 'firing_graph')
+lazy_import('sage.sandpiles.sandpile', 'parallel_firing_graph')
+lazy_import('sage.sandpiles.sandpile', 'wilmes_algorithm')
+lazy_import('sage.sandpiles.sandpile', 'triangle_sandpile')
 
 lazy_import('sage.sandpiles.examples', 'sandpiles')

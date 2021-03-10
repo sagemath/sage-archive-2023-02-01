44cacd27e04015750d97216b9d9dfb9f12538425
Jonathan Kliem <jonathan.kliem@fu-berlin.de>
2021-03-10 10:54:04 +0100

    fix lazy imports at startup

--- src/sage/sandpiles/all.py
+++ src/sage/sandpiles/all.py
@@ -1,12 +1,12 @@
 from sage.misc.lazy_import import lazy_import
 
-from .sandpile import (Sandpile,
-                       SandpileDivisor,
-                       SandpileConfig,
-                       random_DAG,
-                       firing_graph,
-                       parallel_firing_graph,
-                       wilmes_algorithm,
-                       triangle_sandpile)
+lazy_import('sage.sandpile', 'Sandpile')
+lazy_import('sage.sandpile', 'SandpileDivisor')
+lazy_import('sage.sandpile', 'SandpileConfig')
+lazy_import('sage.sandpile', 'random_DAG')
+lazy_import('sage.sandpile', 'firing_graph')
+lazy_import('sage.sandpile', 'parallel_firing_graph')
+lazy_import('sage.sandpile', 'wilmes_algorithm')
+lazy_import('sage.sandpile', 'triangle_sandpile')
 
 lazy_import('sage.sandpiles.examples', 'sandpiles')

a3aa94a209d319a5d923af84cd28cf26acaced66
Vincent Delecroix <20100.delecroix@gmail.com>
2021-04-27 09:47:27 +0200

    31700: dummy extend argument to MatrixSymbolicDense.eigenvalues

--- src/sage/matrix/matrix_symbolic_dense.pyx
+++ src/sage/matrix/matrix_symbolic_dense.pyx
@@ -166,7 +166,7 @@ cdef maxima
 from sage.calculus.calculus import symbolic_expression_from_maxima_string, maxima
 
 cdef class Matrix_symbolic_dense(Matrix_generic_dense):
-    def eigenvalues(self):
+    def eigenvalues(self, extend=True):
         """
         Compute the eigenvalues by solving the characteristic
         polynomial in maxima.
@@ -177,6 +177,15 @@ cdef class Matrix_symbolic_dense(Matrix_generic_dense):
             sage: a.eigenvalues()
             [-1/2*sqrt(33) + 5/2, 1/2*sqrt(33) + 5/2]
 
+        TESTS:
+
+        Check for :trac:`31700`::
+
+            sage: m = matrix([[cos(pi/5), sin(pi/5)], [-sin(pi/5), cos(pi/5)]])
+            sage: t = linear_transformation(m)
+            sage: t.eigenvalues()
+            [1/4*sqrt(5) - 1/4*sqrt(2*sqrt(5) - 10) + 1/4,
+             1/4*sqrt(5) + 1/4*sqrt(2*sqrt(5) - 10) + 1/4]
         """
         maxima_evals = self._maxima_(maxima).eigenvalues()._sage_()
         if not len(maxima_evals):

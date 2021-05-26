a7635a92e0117f19eb7fcdf3005c04559ae1f4bc
Guillaume Rousseau <=>
2021-05-26 21:46:19 +0200

    added copyright

--- src/sage/persistent_homology/filtered_complex.py
+++ src/sage/persistent_homology/filtered_complex.py
@@ -52,8 +52,17 @@ filtration value -1::
     sage: X(Simplex(['baba']))
     -1
 
-
 """
+
+# ****************************************************************************
+#       Copyright (C) 2013 GUILLAUME ROUSSEAU <guillaume.rousseau@ens-lyon.fr>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
 
 from sage.structure.sage_object import SageObject
 from sage.homology.simplicial_complex import Simplex

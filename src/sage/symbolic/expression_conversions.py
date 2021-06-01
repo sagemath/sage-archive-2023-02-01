c92aec8046b9060eb6c30d99520430ca2f96745e
Martin Rubey <martin.rubey@tuwien.ac.at>
2021-06-01 08:30:23 +0200

    remove superfluous import of I

--- src/sage/symbolic/expression_conversions.py
+++ src/sage/symbolic/expression_conversions.py
@@ -18,7 +18,6 @@ overridden by subclasses.
 import operator as _operator
 from sage.rings.rational_field import QQ
 from sage.symbolic.ring import SR
-from sage.symbolic.constants import I
 from sage.functions.all import exp
 from sage.symbolic.operators import arithmetic_operators, relation_operators, FDerivativeOperator, add_vararg, mul_vararg
 from sage.rings.number_field.number_field_element_quadratic import NumberFieldElement_gaussian

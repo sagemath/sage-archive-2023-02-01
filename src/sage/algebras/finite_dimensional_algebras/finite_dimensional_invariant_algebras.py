1c4e871beace0f0150774dfbeca84ff17e3419bd
Trevor K. Karn <karnx018@umn.edu>
2021-06-15 17:55:04 -0500

    Initial commit of invariant algebra

---
+++ src/sage/algebras/finite_dimensional_algebras/finite_dimensional_invariant_algebras.py
@@ -0,0 +1,40 @@
+r"""
+Invariant modules
+"""
+
+# ****************************************************************************
+#       Copyright (C) 2021 Trevor K. Karn <karnx018 at umn.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
+from sage.modules.with_basis.invariant import FiniteDimensionalInvariantModule
+
+class FiniteDimensionalInvariantAlgebra(FiniteDimensionalInvariantModule):
+
+	def __init__(self, R, *args, **kwargs):
+		"""
+        INPUTS::
+
+        - ``R`` -- an instance of a ``Representation`` of a semigroup `S`
+                   acting on the algebra `A`.
+
+        OUTPUTS::
+
+        - ``AS`` -- the invariant algebra of the semigroup action of `S` on `A`, or
+                    equivalently, the isotypic component of the representation of
+                    `S` carried by `A` corresponding to the trivial character.
+
+        EXAMPLES::
+        
+            
+
+        TESTS::
+
+        """
+
+        pass

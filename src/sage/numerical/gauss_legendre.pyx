af7de168802757e09796e40a064668bca5c8a50e
Linden Disney-Hogg (Work) <disneyhogg@gmail.com>
2021-06-21 11:46:07 +0100

    Fixed error by ammending typo.
    The line `degree==1` should have instead been `degree==3`, as this is the output that it is
    providing, and degree=1 is not a valid input.

--- src/sage/numerical/gauss_legendre.pyx
+++ src/sage/numerical/gauss_legendre.pyx
@@ -86,7 +86,7 @@ def nodes(degree,prec):
     TWO = 2*ONE
     rnd = R.rnd
     epsilon = R(1)>>(prec+8)
-    if degree == 1:
+    if degree == 3:
         x = (R(3)/5).sqrt()
         w = R(5)/18
         nodes = [((1-x)/2,w),(HALF,R(4)/9),((1+x)/2,w)]

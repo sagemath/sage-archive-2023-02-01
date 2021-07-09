d54ac2a52fad8b4627afd5693f80456022ab0c8c
DavidAyotte <davidayotte94@outlook.com>
2021-07-09 16:58:24 -0400

    fix some redundancy

--- src/sage/modular/modform/space.py
+++ src/sage/modular/modform/space.py
@@ -1091,7 +1091,7 @@ class ModularFormsSpace(hecke.HeckeModule_generic):
             sage: M(M([1, 2, 3, 4, 5]), check=True)
             4 + 6*q + 47*q^2 + 143*q^3 + 358*q^4 + 630*q^5 + O(q^6)
         """
-        if isinstance(x, (self.element_class, ModularFormElement)):
+        if isinstance(x, ModularFormElement):
             if x.parent() is self:
                 return x
 

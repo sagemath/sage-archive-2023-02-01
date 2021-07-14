53c7b65f42044639b59015c87ba00b3645422bf4
Travis Scrimshaw <tcscrims@gmail.com>
2021-07-14 12:02:29 +1000

    Rewriting doctests to test the new format.

--- src/sage/game_theory/parser.py
+++ src/sage/game_theory/parser.py
@@ -1,4 +1,17 @@
+"""
+Parser For gambit And lrs Nash Equilibria
+"""
 
+# ****************************************************************************
+#       Copyright (C) 2014 James Campbell james.campbell@tanti.org.uk
+#                     2015 Vincent Knight
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
 
 class Parser():
     r"""
@@ -11,128 +24,33 @@ class Parser():
 
     def __init__(self, raw_string):
         """
-        Initialise a Parser instance by storing a raw_string
+        Initialise a Parser instance by storing a ``raw_string``
         (currently only used with H representation of a game).
 
-        TESTS:
+        EXAMPLES::
 
-        Simply checking that we have the correct string output
-        for the H representation (which is the format required
-        for the ``'lrs'`` algorithm)::
-
+            sage: from sage.cpython.string import bytes_to_str
             sage: from sage.game_theory.parser import Parser
-            sage: A = matrix([[1, 2], [3, 2]])
-            sage: g = NormalFormGame([A])
-            sage: raw_string = g._Hrepresentation(A, -A)
-            doctest:warning...
-            DeprecationWarning: NormalFormGame._Hrepresentation is deprecated as it
-            creates the legacy input format. Use NormalFormGame._lrs_nash_format instead
-            See https://trac.sagemath.org/27745 for details.
-            sage: P = Parser(raw_string)
-            sage: print(P.raw_string[0])
-            H-representation
-            linearity 1 5
-            begin
-            5 4 rational
-            0 1 0 0
-            0 0 1 0
-            0 1 3  1
-            0 2 2  1
-            -1 1 1 0
-            end
-            <BLANKLINE>
-
-            sage: print(P.raw_string[1])
-            H-representation
-            linearity 1 5
-            begin
-            5 4 rational
-            0 -1 -2  1
-            0 -3 -2  1
-            0 1 0 0
-            0 0 1 0
-            -1 1 1 0
-            end
-            <BLANKLINE>
-
-        The specific case of a game with 1 strategy for each player::
-
+            sage: from subprocess import Popen, PIPE
             sage: A = matrix([[1]])
             sage: B = matrix([[5]])
             sage: g = NormalFormGame([A,B])
-            sage: raw_string = g._Hrepresentation(A, B)
-            sage: P = Parser(raw_string)
-            sage: print(P.raw_string[0])
-            H-representation
-            linearity 1 3
-            begin
-            3 3 rational
-            0 1 0
-            0 -5  1
-            -1 1 0
-            end
-            <BLANKLINE>
+            sage: raw_string = g._lrs_nash_format(A, B)
+            sage: game_name = tmp_filename()
+            sage: with open(game_name, 'w') as game_file:
+            ....:     _ = game_file.write(game_str)
+            sage: process = Popen(['lrsnash', game_name], stdout=PIPE, stderr=PIPE)  # optional - lrslib
+            sage: lrs_output = [bytes_to_str(row) for row in process.stdout]         # optional - lrslib
 
-            sage: print(P.raw_string[1])
-            H-representation
-            linearity 1 3
-            begin
-            3 3 rational
-            0 -1  1
-            0 1 0
-            -1 1 0
-            end
-            <BLANKLINE>
+            sage: Parser(lrs_output).format_lrs()                                    # optional - lrslib
 
-        Another test::
-
-            sage: from sage.game_theory.parser import Parser
-            sage: A = matrix([[-7, -5, 5],
-            ....:             [5, 5, 3],
-            ....:             [1, -6, 1]])
-            sage: B = matrix([[-9, 7, 9],
-            ....:             [6, -2, -3],
-            ....:             [-4, 6, -10]])
-            sage: g = NormalFormGame([A, B])
-            sage: raw_string = g._Hrepresentation(A, B)
-            sage: P = Parser(raw_string)
-            sage: print(P.raw_string[0])
-            H-representation
-            linearity 1 7
-            begin
-            7 5 rational
-            0 1 0 0 0
-            0 0 1 0 0
-            0 0 0 1 0
-            0  9 -6  4  1
-            0 -7  2 -6  1
-            0 -9  3  10  1
-            -1 1 1 1 0
-            end
-            <BLANKLINE>
-
-            sage: print(P.raw_string[1])
-            H-representation
-            linearity 1 7
-            begin
-            7 5 rational
-            0 7 5 -5  1
-            0 -5 -5 -3  1
-            0 -1 6 -1  1
-            0 1 0 0 0
-            0 0 1 0 0
-            0 0 0 1 0
-            -1 1 1 1 0
-            end
-            <BLANKLINE>
-
-        This class is also used to parse the output of algorithms from the gambit
-        python interface using the `format_gambit` function.
+        This class is also used to parse the output of algorithms from
+        the gambit python interface using :meth:`format_gambit()`.
         """
         self.raw_string = raw_string
 
     def format_lrs(self, legacy_format=False):
-        """
+        r"""
         Parses the output of lrs so as to return vectors
         corresponding to equilibria.
 
@@ -143,45 +61,35 @@ class Parser():
             sage: from subprocess import Popen, PIPE
             sage: A = matrix([[1, 2], [3, 2]])
             sage: g = NormalFormGame([A])
-            sage: game1_str, game2_str = g._Hrepresentation(A, -A)
-            doctest:warning...
-            DeprecationWarning: NormalFormGame._Hrepresentation is deprecated as it
-            creates the legacy input format. Use NormalFormGame._lrs_nash_format instead
-            See https://trac.sagemath.org/27745 for details.
-            sage: g1_name = tmp_filename()
-            sage: g2_name = tmp_filename()
-            sage: g1_file = open(g1_name, 'w')
-            sage: g2_file = open(g2_name, 'w')
-            sage: _ = g1_file.write(game1_str)
-            sage: g1_file.close()
-            sage: _ = g2_file.write(game2_str)
-            sage: g2_file.close()
-            sage: process = Popen(['lrsnash', g1_name, g2_name], stdout=PIPE, stderr=PIPE)  # not tested, optional - lrslib
-            sage: lrs_output = [bytes_to_str(row) for row in process.stdout]                # not tested, optional - lrslib
+            sage: game_str = g._lrs_nash_format(A, -A)
+            sage: game_name = tmp_filename()
+            sage: with open(game_name, 'w') as game_file:
+            ....:     _ = game_file.write(game_str)
+            sage: process = Popen(['lrsnash', game_name], stdout=PIPE, stderr=PIPE)  # optional - lrslib
+            sage: lrs_output = [bytes_to_str(row) for row in process.stdout]         # optional - lrslib
 
-        The above creates a game, writes the H representation to
-        temporary files, calls lrs and stores the output in `lrs_output`
+        The above creates a game, writes the H representations to
+        temporary files, calls lrs and stores the output in ``lrs_output``
         (here slicing to get rid of some system parameters that get returned)::
 
-            sage: lrs_output[:20]                                                           # not tested, optional - lrslib
+            sage: lrs_output[:-2]                                                    # optional - lrslib
             [...,
-             '***** 4 4 rational\n',
              '2  0  1  2 \n',
              '1  1/2  1/2 -2 \n',
              '\n',
              '2  0  1  2 \n',
              '1  0  1 -2 \n',
              '\n',
-             '\n',
              '*Number of equilibria found: 2\n',
              '*Player 1: vertices=3 bases=3 pivots=5\n',
-             ...]
+             '*Player 2: vertices=2 bases=1 pivots=6\n',
+             '\n']
 
         The above is pretty messy, here is the output when we put it through
         the parser::
 
-            sage: nasheq = Parser(lrs_output).format_lrs(legacy_format=True)                # not tested, optional - lrslib
-            sage: nasheq                                                                    # not tested, optional - lrslib
+            sage: nasheq = Parser(lrs_output).format_lrs()                           # optional - lrslib
+            sage: nasheq                                                             # optional - lrslib
             [[(1/2, 1/2), (0, 1)], [(0, 1), (0, 1)]]
 
         Another game::
@@ -193,22 +101,16 @@ class Parser():
             ....:             [6, -2, -3],
             ....:             [-4, 6, -10]])
             sage: g = NormalFormGame([A, B])
-            sage: game1_str, game2_str = g._Hrepresentation(A, B)
-            sage: g1_name = tmp_filename()
-            sage: g2_name = tmp_filename()
-            sage: g1_file = open(g1_name, 'w')
-            sage: g2_file = open(g2_name, 'w')
-            sage: _ = g1_file.write(game1_str)
-            sage: g1_file.close()
-            sage: _ = g2_file.write(game2_str)
-            sage: g2_file.close()
-            sage: process = Popen(['lrsnash', g1_name, g2_name], stdout=PIPE, stderr=PIPE)  # not tested, optional - lrslib
-            sage: lrs_output = [bytes_to_str(row) for row in process.stdout]                # not tested, optional - lrslib
-            sage: print(lrs_output[:25])                                                    # not tested, optional - lrslib
+            sage: game_str = g._lrs_nash_format(A, B)
+            sage: game_name = tmp_filename()
+            sage: with open(game_name, 'w') as game_file:
+            ....:     _ = game_file.write(game_str)
+            sage: process = Popen(['lrsnash', game_name], stdout=PIPE, stderr=PIPE)  # optional - lrslib
+            sage: lrs_output = [bytes_to_str(row) for row in process.stdout]         # optional - lrslib
+            sage: print(lrs_output[:-2])                                             # optional - lrslib
             [...,
-             '***** 5 5 rational\n',
-             '2  1/7  0  6/7  23/7 \n',
              '2  0  1/6  5/6  10/3 \n',
+             '2  1/7  0  6/7  23/7 \n',
              '1  1/3  2/3  0  1 \n',
              '\n',
              '2  0  0  1  5 \n',
@@ -217,17 +119,43 @@ class Parser():
              '2  1  0  0  5 \n',
              '1  0  1  0  6 \n',
              '\n',
-             '\n',
              '*Number of equilibria found: 4\n',
              '*Player 1: vertices=6 bases=7 pivots=10\n',
-             ...]
+             '*Player 2: vertices=4 bases=2 pivots=14\n',
+             '\n']
 
-            sage: nasheq = Parser(lrs_output).format_lrs(legacy_format=True)                # not tested, optional - lrslib
-            sage: sorted(nasheq)                                                            # not tested, optional - lrslib
+            sage: nasheq = Parser(lrs_output).format_lrs()                           # optional - lrslib
+            sage: sorted(nasheq)                                                     # optional - lrslib
             [[(0, 1, 0), (1, 0, 0)],
              [(1/3, 2/3, 0), (0, 1/6, 5/6)],
              [(1/3, 2/3, 0), (1/7, 0, 6/7)],
              [(1, 0, 0), (0, 0, 1)]]
+
+        TESTS:
+
+        An example with the legacy format::
+
+            sage: from sage.cpython.string import bytes_to_str
+            sage: from sage.game_theory.parser import Parser
+            sage: from subprocess import Popen, PIPE
+            sage: A = matrix([[1, 2], [3, 2]])
+            sage: g = NormalFormGame([A])
+            sage: game1_str, game2_str = g._Hrepresentation(A, -A)
+            doctest:warning...
+            DeprecationWarning: NormalFormGame._Hrepresentation is deprecated as it
+            creates the legacy input format. Use NormalFormGame._lrs_nash_format instead
+            See https://trac.sagemath.org/27745 for details.
+            sage: g1_name, g2_name = tmp_filename(), tmp_filename()
+            sage: g1_file, g2_file = open(g1_name, 'w'), open(g2_name, 'w')
+            sage: _ = g1_file.write(game1_str)
+            sage: g1_file.close()
+            sage: _ = g2_file.write(game2_str)
+            sage: g2_file.close()
+            sage: process = Popen(['lrsnash', g1_name, g2_name], stdout=PIPE, stderr=PIPE)  # not tested, optional - lrslib
+            sage: lrs_output = [bytes_to_str(row) for row in process.stdout]                # not tested, optional - lrslib
+            sage: nasheq = Parser(lrs_output).format_lrs(legacy_format=True)                # not tested, optional - lrslib
+            sage: nasheq                                                                    # not tested, optional - lrslib
+            [[(1/2, 1/2), (0, 1)], [(0, 1), (0, 1)]]
         """
         equilibria = []
         from sage.misc.sage_eval import sage_eval
@@ -362,3 +290,4 @@ class Parser():
             nice_stuff.append(profile)
 
         return nice_stuff
+

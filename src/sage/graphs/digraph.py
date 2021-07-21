fcc3e759862ecaf81d409b133507665549b4e13f
Michael Orlitzky <michael@orlitzky.com>
2021-07-21 08:41:15 -0400

    Trac #32188: re-enable test fixed in trac 32095.

--- src/sage/graphs/digraph.py
+++ src/sage/graphs/digraph.py
@@ -2474,7 +2474,7 @@ class DiGraph(GenericGraph):
             sage: G = graphs.RandomGNP(40, 0.4).to_directed()
             sage: d1 = G.diameter(algorithm='DiFUB', by_weight=True)
             sage: d2 = max(G.eccentricity(algorithm='Dijkstra_Boost', by_weight=True))
-            sage: d1 == d2  # not tested, known bug (see :trac:`32095`)
+            sage: d1 == d2
             True
             sage: G = digraphs.Path(5)
             sage: G.diameter(algorithm = 'DiFUB')

0847a9d23689daa06b5683f6818de90622965865
Antonio Rojas <arojas@archlinux.org>
2021-07-29 23:08:27 +0200

    Cast parameters to int in RandomBarabasiAlbert

--- src/sage/graphs/digraph.py
+++ src/sage/graphs/digraph.py
@@ -3158,7 +3158,7 @@ class DiGraph(GenericGraph):
         Using the NetworkX implementation ::
 
             sage: list(D.topological_sort(implementation="NetworkX"))
-            [4, 5, 6, 9, 0, 3, 2, 7, 1, 8, 10]
+            [0, 4, 1, 3, 2, 5, 6, 9, 7, 8, 10]
 
         ::
 

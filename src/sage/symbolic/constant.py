5b557c68dd722ae8063334b23a595f9b1482a5d7
Jonathan Kliem <jonathan.kliem@fu-berlin.de>
2021-08-23 11:23:13 +0200

    deprecation warnings

---
+++ src/sage/symbolic/constant.py
@@ -0,0 +1,2 @@
+from sage.misc.lazy_import import lazy_import
+lazy_import('sage.symbolic.expression', 'PynacConstant', deprecation=32386)
---
+++ src/sage/symbolic/series.py
@@ -0,0 +1,2 @@
+from sage.misc.lazy_import import lazy_import
+lazy_import('sage.symbolic.expression', 'SymbolicSeries', deprecation=32386)

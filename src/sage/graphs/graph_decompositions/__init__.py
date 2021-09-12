8efece0a0ed470f57905a57e138e036a99e7b4da
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-09-11 22:54:37 -0700

    sage.graphs, sage.graphs.graph_decompositions: Remove unnecessary import of modules

--- src/sage/graphs/graph_decompositions/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file is not empty !
-import sage.graphs.graph_decompositions.vertex_separation
-import sage.graphs.graph_decompositions.rankwidth
-import sage.graphs.graph_decompositions.bandwidth
-import sage.graphs.graph_decompositions.cutwidth

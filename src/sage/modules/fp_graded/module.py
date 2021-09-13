fab508ad23b72e96a9e367956325f24c100bb18b
John H. Palmieri <jhpalmieri64@gmail.com>
2021-09-13 13:54:52 -0700

    trac 32505: clear out __init__.py

--- src/sage/modules/fp_graded/module.py
+++ src/sage/modules/fp_graded/module.py
@@ -1,3 +1,14 @@
+# TODO:
+#
+# 1. Why do we need to define __bool__ and __eq__ in element.py? They should be taken care of automatically, once we define __nonzero__.
+# 2. inhheritance: free modules, elements, etc., should perhaps inherit from fp versions, or maybe both should inherit from generic classes, to consolidate some methods (like degree, _repr_, others?)
+# 3. Test with graded modules over other graded rings. (Should be okay, but add some doctests.)
+#
+# In __classcall__/__init__ for FP_Modules, allow as input a free module or a morphism of free modules?
+
+
+
+
 r"""
 Finitely presented graded modules
 

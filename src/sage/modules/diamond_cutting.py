848cf86999bec0e3a7058417fe02b6b35d2ff8c9
Dima Pasechnik <dima@pasechnik.info>
2021-10-02 20:42:19 +0100

    use inner_product and sqrtrem

--- src/sage/modules/diamond_cutting.py
+++ src/sage/modules/diamond_cutting.py
@@ -258,7 +258,10 @@ def calculate_voronoi_cell(basis, radius=None, verbose=False):
     artificial_length = None
     if dim[0] < dim[1]:
         # introduce "artificial" basis points (representing infinity)
-        artificial_length = max(v.norm(1) for v in basis).ceil() * 2
+        def approx_norm(v):
+            r,r1 = (v.inner_product(v)).sqrtrem()
+            return r + (r1 > 0)
+        artificial_length = max(approx_norm(v) for v in basis) * 2
         additional_vectors = identity_matrix(dim[1]) * artificial_length
         basis = basis.stack(additional_vectors)
         # LLL-reduce to get quadratic matrix
@@ -277,7 +280,7 @@ def calculate_voronoi_cell(basis, radius=None, verbose=False):
 
     # twice the length of longest vertex in Q is a safe choice
     if radius is None:
-        radius = 2 * max(v.norm(1) ** 2 for v in basis)
+        radius = 2 * max(v.inner_product(v) for v in basis)
 
     V = diamond_cut(Q, basis, radius, verbose=verbose)
 

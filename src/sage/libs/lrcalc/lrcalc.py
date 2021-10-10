2cb45110329e9afb89ec892e56ab244124b34049
Antonio Rojas <arojas@archlinux.org>
2021-10-10 13:26:37 +0200

    Implement tscrim's suggestions, fix test output

--- src/sage/libs/lrcalc/lrcalc.py
+++ src/sage/libs/lrcalc/lrcalc.py
@@ -191,12 +191,10 @@ from sage.combinat.partition import _Partitions
 from sage.combinat.permutation import Permutation
 from sage.combinat.skew_tableau import SkewTableaux
 from sage.combinat.skew_partition import SkewPartition
-from sage.combinat.words.word import Word
 from sage.rings.integer import Integer
 import lrcalc
 
 def _lrcalc_dict_to_sage(result):
-    return dict({_Partitions(i):Integer(k) for i,k in result.items()})
     r"""
     Translate from lrcalc output format to Sage expected format.
 
@@ -204,8 +202,9 @@ def _lrcalc_dict_to_sage(result):
 
         sage: from sage.libs.lrcalc.lrcalc import mult
         sage: mult([2,1],[3,2,1],3) # indirect doctest
-        {[3, 3, 3]: 1, [4, 3, 2]: 1, [4, 4, 1]: 1, [5, 2, 2]: 1, [5, 3, 1]: 1}
+        {[3, 3, 3]: 1, [4, 3, 2]: 2, [4, 4, 1]: 1, [5, 2, 2]: 1, [5, 3, 1]: 1}
     """
+    return dict({_Partitions(la):Integer(k) for la,k in result.items()})
 
 def lrcoef_unsafe(outer, inner1, inner2):
     r"""
@@ -356,7 +355,8 @@ def mult(part1, part2, maxrows=None, level=None, quantum=None):
     P = quantum.parent()
     output = {}
     for i,k in result.items():
-        output[_Partitions(i[0])] = output.get(_Partitions(i[0]), P.zero()) + k*quantum**(i[1])
+        la = _Partitions(i[0])
+        output[la] = output.get(la, P.zero()) + k*quantum**(i[1])
     return output
 
 
@@ -416,7 +416,7 @@ def coprod(part, all=0):
         [(([1, 1], [1]), 1), (([2], [1]), 1), (([2, 1], []), 1)]
     """
     result = lrcalc.coprod(part, all)
-    return dict({tuple(_Partitions(j) for j in i):Integer(k) for i,k in result.items()})
+    return {tuple(_Partitions(mu) for mu in la):Integer(k) for la,k in result.items()}
 
 
 def mult_schubert(w1, w2, rank=0):
@@ -449,7 +449,7 @@ def mult_schubert(w1, w2, rank=0):
          ([7, 3, 4, 1, 2, 5, 6], 1), ([7, 4, 2, 1, 3, 5, 6], 1)]
     """
     result = lrcalc.schubmult(w1, w2, rank)
-    return dict({Permutation(list(i)):Integer(k) for i,k in result.items()})
+    return {Permutation(list(la)):Integer(k) for la,k in result.items()}
 
 
 def lrskew(outer, inner, weight=None, maxrows=-1):
@@ -503,19 +503,18 @@ def lrskew(outer, inner, weight=None, maxrows=-1):
     shape = SkewPartition([outer, inner])
 
     if weight is None:
-        while True:
-            try:
-                word = Word([i+1 for i in next(iterator)])
-                yield SkewTableaux().from_shape_and_word(shape, word)
-            except StopIteration:
-                break
+        ST = SkewTableaux()
+        for data in iterator:
+            yield ST.from_shape_and_word(shape, [i+1 for i in data])
     else:
         wt = _Partitions(weight)
-        while True:
-            try:
-                word = Word([i+1 for i in next(iterator)])
-                r = SkewTableaux().from_shape_and_word(shape, word)
-                if r.weight() == wt:
-                    yield r
-            except StopIteration:
-                break
+        ST = SkewTableaux()
+        for data in iterator:
+            sdata = [i+1 for i in data]
+            m = max(sdata)
+            w = [0] * m
+            for j in sdata:
+                w[j-1] += 1
+            if w == wt:
+                r = ST.from_shape_and_word(shape, sdata)
+                yield r

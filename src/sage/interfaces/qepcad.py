c9861d16af2e435985ab77239f6eb5b0c775fa6b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-10-17 10:05:46 -0700

    src/sage/interfaces/qepcad.py: Remove use of is_SymbolicEquation

--- src/sage/interfaces/qepcad.py
+++ src/sage/interfaces/qepcad.py
@@ -1924,11 +1924,9 @@ class qepcad_formula_factory:
         if isinstance(lhs, qformula):
             return lhs
 
-        from sage.symbolic.expression import is_SymbolicEquation
-        if is_SymbolicEquation(lhs):
-            rhs = lhs.rhs()
-            op = lhs.operator()
-            lhs = lhs.lhs()
+        from sage.structure.element import Expression
+        if isinstance(lhs, Expression) and lhs.is_relational():
+            lhs, op, rhs = lhs.lhs(), lhs.operator(), lhs.rhs()
 
         op = self._normalize_op(op)
 

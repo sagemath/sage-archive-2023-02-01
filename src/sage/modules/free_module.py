810e3e6d58c5206db09838b5612be17e0a8e12a7
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-10-26 11:49:55 -0700

    src/sage/modules/free_module.py: Separate long if/elif chain from the following code by a newline

--- src/sage/modules/free_module.py
+++ src/sage/modules/free_module.py
@@ -7462,6 +7462,7 @@ def element_class(R, is_sparse):
     elif isinstance(R, sage.rings.abc.SymbolicRing) and not is_sparse:
         import sage.modules.vector_symbolic_dense
         return sage.modules.vector_symbolic_dense.Vector_symbolic_dense
+
     if is_sparse:
         return free_module_element.FreeModuleElement_generic_sparse
     else:

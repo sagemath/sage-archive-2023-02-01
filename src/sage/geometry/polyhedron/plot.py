c1b1d9338fc3142f533bf37f2c13defc496d3335
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-10-26 22:10:32 -0700

    src/sage/geometry/polyhedron/plot.py: Add more # optional - sage.plot

--- src/sage/geometry/polyhedron/plot.py
+++ src/sage/geometry/polyhedron/plot.py
@@ -881,8 +881,8 @@ class Projection(SageObject):
 
             sage: cube1 = polytopes.hypercube(1)
             sage: proj = cube1.projection()
-            sage: points = proj.render_points_1d()
-            sage: points._objects
+            sage: points = proj.render_points_1d()  # optional - sage.plot
+            sage: points._objects                   # optional - sage.plot
             [Point set defined by 2 point(s)]
         """
         return point2d([c + [0] for c in self.coordinates_of(self.points)], **kwds)
@@ -902,8 +902,8 @@ class Projection(SageObject):
 
         EXAMPLES::
 
-            sage: outline = polytopes.hypercube(1).projection().render_line_1d()
-            sage: outline._objects[0]
+            sage: outline = polytopes.hypercube(1).projection().render_line_1d()  # optional - sage.plot
+            sage: outline._objects[0]                                             # optional - sage.plot
             Line defined by 2 points
         """
         if len(self.lines) == 0:
@@ -922,8 +922,8 @@ class Projection(SageObject):
 
             sage: hex = polytopes.regular_polygon(6)
             sage: proj = hex.projection()
-            sage: hex_points = proj.render_points_2d()
-            sage: hex_points._objects
+            sage: hex_points = proj.render_points_2d()  # optional - sage.plot
+            sage: hex_points._objects                   # optional - sage.plot
             [Point set defined by 6 point(s)]
         """
         return point2d(self.coordinates_of(self.points), **kwds)
@@ -935,8 +935,8 @@ class Projection(SageObject):
         EXAMPLES::
 
             sage: penta = polytopes.regular_polygon(5)
-            sage: outline = penta.projection().render_outline_2d()
-            sage: outline._objects[0]
+            sage: outline = penta.projection().render_outline_2d()  # optional - sage.plot
+            sage: outline._objects[0]                               # optional - sage.plot
             Line defined by 2 points
         """
         wireframe = []
@@ -957,8 +957,8 @@ class Projection(SageObject):
             sage: cps = [i^3 for i in srange(-2,2,1/5)]
             sage: p = Polyhedron(vertices = [[(t^2-1)/(t^2+1),2*t/(t^2+1)] for t in cps])
             sage: proj = p.projection()
-            sage: filled_poly = proj.render_fill_2d()
-            sage: filled_poly.axes_width()
+            sage: filled_poly = proj.render_fill_2d()  # optional - sage.plot
+            sage: filled_poly.axes_width()             # optional - sage.plot
             0.8
         """
         poly = [polygon2d(self.coordinates_of(p), **kwds)
@@ -973,8 +973,8 @@ class Projection(SageObject):
 
             sage: p = polytopes.cross_polytope(3)
             sage: proj = p.projection()
-            sage: verts = proj.render_vertices_3d()
-            sage: verts.bounding_box()
+            sage: verts = proj.render_vertices_3d()  # optional - sage.plot
+            sage: verts.bounding_box()               # optional - sage.plot
             ((-1.0, -1.0, -1.0), (1.0, 1.0, 1.0))
         """
         return point3d(self.coordinates_of(self.points), **kwds)
@@ -987,8 +987,8 @@ class Projection(SageObject):
 
             sage: cube = polytopes.hypercube(3)
             sage: cube_proj = cube.projection()
-            sage: wire = cube_proj.render_wireframe_3d()
-            sage: print(wire.tachyon().split('\n')[77])  # for testing
+            sage: wire = cube_proj.render_wireframe_3d()                # optional - sage.plot
+            sage: print(wire.tachyon().split('\n')[77])  # for testing  # optional - sage.plot
             FCylinder base 1.0 1.0 -1.0 apex -1.0 1.0 -1.0 rad 0.005 texture...
         """
         wireframe = []
@@ -1007,8 +1007,8 @@ class Projection(SageObject):
         EXAMPLES::
 
             sage: p = polytopes.hypercube(3).projection()
-            sage: p_solid = p.render_solid_3d(opacity = .7)
-            sage: type(p_solid)
+            sage: p_solid = p.render_solid_3d(opacity=.7)  # optional - sage.plot
+            sage: type(p_solid)                            # optional - sage.plot
             <class 'sage.plot.plot3d.index_face_set.IndexFaceSet'>
         """
         polys = self.polygons
@@ -1031,9 +1031,9 @@ class Projection(SageObject):
 
         EXAMPLES::
 
-            sage: print(Polyhedron([]).projection().render_0d().description())
+            sage: print(Polyhedron([]).projection().render_0d().description())           # optional - sage.plot
             <BLANKLINE>
-            sage: print(Polyhedron(ieqs=[(1,)]).projection().render_0d().description())
+            sage: print(Polyhedron(ieqs=[(1,)]).projection().render_0d().description())  # optional - sage.plot
             Point set defined by 1 point(s):    [(0.0, 0.0)]
         """
         if point_opts is None:
@@ -1062,7 +1062,7 @@ class Projection(SageObject):
 
         EXAMPLES::
 
-            sage: Polyhedron([(0,), (1,)]).projection().render_1d()
+            sage: Polyhedron([(0,), (1,)]).projection().render_1d()  # optional - sage.plot
             Graphics object consisting of 2 graphics primitives
         """
         plt = Graphics()

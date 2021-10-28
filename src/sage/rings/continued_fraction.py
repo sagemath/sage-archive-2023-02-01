e1664fff95fb671e91bc41737518f7442dd4690f
Jonathan Kliem <jonathan.kliem@fu-berlin.de>
2021-10-28 08:59:20 +0200

    use different precisions in doctest

--- src/sage/rings/continued_fraction.py
+++ src/sage/rings/continued_fraction.py
@@ -263,7 +263,7 @@ def rat_interval_cf_list(r1, r2):
         sage: rat_interval_cf_list(257/113, 5224/2297)
         [2, 3, 1, 1, 1, 4]
         sage: for prec in range(10,54):
-        ....:     R = RealIntervalField(20)
+        ....:     R = RealIntervalField(prec)
         ....:     for _ in range(100):
         ....:         x = R.random_element() * R.random_element() + R.random_element() / 100
         ....:         l = x.lower().exact_rational()

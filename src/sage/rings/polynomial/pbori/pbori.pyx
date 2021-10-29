b0f3ebc9ad1dbe2c3d2fdb9e24e31009b0e733a5
Frédéric Chapoton <chapoton@unistra.fr>
2021-10-29 21:17:58 +0200

    removing another bunch of py2/py3 tags

--- src/sage/rings/polynomial/pbori/pbori.pyx
+++ src/sage/rings/polynomial/pbori/pbori.pyx
@@ -1378,9 +1378,7 @@ cdef class BooleanPolynomialRing(MPolynomialRing_base):
         EXAMPLES::
 
             sage: P.<x,y,z> = BooleanPolynomialRing(3)
-            sage: [P._random_monomial_dfirst(3, (0,1,2)) for _ in range(10)]  # py2
-            [x*y*z, x*y*z, x*y*z, y*z, x*z, z, z, y*z, x*y*z, 1]
-            sage: [P._random_monomial_dfirst(3, (0,1,2)) for _ in range(10)]  # py3 random
+            sage: [P._random_monomial_dfirst(3, (0,1,2)) for _ in range(10)]  # random
             [x*y*z, x*y*z, x*y*z, x*y, x*z, x, x, y*z, x*y*z, 1]
         """
         from sage.rings.integer_ring import ZZ

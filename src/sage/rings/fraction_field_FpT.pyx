a113340357198f69c4e26a7656e05cac431dad24
Michael Orlitzky <michael@orlitzky.com>
2021-11-08 06:37:37 -0500

    Trac #32841: remove zn_poly from distutils "libraries" fields.

--- src/sage/rings/fraction_field_FpT.pyx
+++ src/sage/rings/fraction_field_FpT.pyx
@@ -1,4 +1,4 @@
-# distutils: libraries = gmp NTL_LIBRARIES zn_poly
+# distutils: libraries = gmp NTL_LIBRARIES
 # distutils: extra_compile_args = NTL_CFLAGS
 # distutils: include_dirs = NTL_INCDIR
 # distutils: library_dirs = NTL_LIBDIR

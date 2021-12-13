787ddd9bc5cff4be18ea082a453cce4c32d6d471
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-12-12 20:10:25 -0800

    src/sage/repl/ipython_extension.py: Fall back to importing sage.all__sagemath_repl if sage.all is not available

--- src/sage/repl/ipython_extension.py
+++ src/sage/repl/ipython_extension.py
@@ -432,7 +432,10 @@ class SageCustomizations(object):
         self.init_inspector()
         self.init_line_transforms()
 
-        import sage.all # until sage's import hell is fixed
+        try:
+            import sage.all # until sage's import hell is fixed
+        except ImportError:
+            import sage.all__sagemath_repl
 
         self.shell.verbose_quit = True
 

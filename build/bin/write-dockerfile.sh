fad8942c93702e6c098fe6b9cd2d68704b256334
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-01-01 10:58:09 -0800

    build/bin/write-dockerfile.sh: Use relative workdir 'sage' (again)

--- build/bin/write-dockerfile.sh
+++ build/bin/write-dockerfile.sh
@@ -191,8 +191,8 @@ cat <<EOF
 
 FROM with-system-packages as bootstrapped
 #:bootstrapping:
-RUN mkdir -p /sage
-WORKDIR /sage
+RUN mkdir -p sage
+WORKDIR sage
 $ADD Makefile VERSION.txt COPYING.txt condarc.yml README.md bootstrap configure.ac sage .homebrew-build-env tox.ini Pipfile.m4 ./
 $ADD src/doc/bootstrap src/doc/bootstrap
 $ADD src/bin src/bin

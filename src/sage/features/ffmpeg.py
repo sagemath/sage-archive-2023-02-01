df0a2d63c125a1201d14b466b5d2900f1f452a64
Sébastien Labbé <sebastien.labbe@labri.fr>
2022-01-05 12:16:03 +0100

    33092: adding is_functional method to feature ffmpeg

--- src/sage/features/ffmpeg.py
+++ src/sage/features/ffmpeg.py
@@ -12,8 +12,7 @@ Feature for testing the presence of ``ffmpeg``
 #                  https://www.gnu.org/licenses/
 # ****************************************************************************
 
-from . import Executable
-
+from . import Executable, FeatureTestResult
 
 class FFmpeg(Executable):
     r"""
@@ -36,6 +35,65 @@ class FFmpeg(Executable):
         Executable.__init__(self, "ffmpeg", executable="ffmpeg",
                             spkg="ffmpeg",
                             url="https://www.ffmpeg.org/")
+
+    def is_functional(self):
+        r"""
+        Return whether command ``ffmpeg`` in the path is functional.
+
+        EXAMPLES::
+
+            sage: from sage.features.ffmpeg import FFmpeg
+            sage: FFmpeg().is_functional()   # optional - ffmpeg
+            FeatureTestResult('ffmpeg', True)
+
+        """
+        # Create the content of 1-pixel png file
+        content = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x00\x00\x00\x00:~\x9bU\x00\x00\x00\nIDATx\x9cc`\x00\x00\x00\x02\x00\x01H\xaf\xa4q\x00\x00\x00\x00IEND\xaeB`\x82'
+
+        # NOTE:
+        #
+        # This is how the above content of a 1 pixel PNG was created::
+        #
+        #    sage: import numpy as np
+        #    sage: from PIL import Image
+        #    sage: image = Image.fromarray(np.array([[100]], dtype=np.uint8))
+        #    sage: image.save('file.png')
+        #    sage: with open('file.png', 'rb') as f:
+        #    ....:     content = f.read()
+
+        # create a png file with the content
+        from sage.misc.temporary_file import tmp_filename
+        base_filename_png = tmp_filename(ext='.png')
+        with open(base_filename_png, 'wb') as f:
+            f.write(content)
+
+        # Set up filenames
+        import os
+        base, filename_png = os.path.split(base_filename_png)
+        filename, _png = os.path.splitext(filename_png)
+        filename_gif = filename + '.gif'
+
+        # running the command (taken from sage/plot/animate.py)
+        from subprocess import run
+        cmd = ['ffmpeg', '-y', '-f', 'image2', '-r', '5', '-i',
+               filename_png, '-pix_fmt', 'rgb24', '-loop', '0',
+               filename_gif]
+        result = run(cmd, cwd=base, capture_output=True, text=True)
+
+        # If an error occured, return False
+        if result.returncode:
+            return FeatureTestResult(self, False, reason='Running command "{}" '
+                        'returned non-zero exit status "{}" with stderr '
+                        '"{}" and stdout "{}".'.format(result.args,
+                                                       result.returncode,
+                                                       result.stderr.strip(),
+                                                       result.stdout.strip()))
+
+        # If necessary, run more tests here
+        # ...
+
+        # The command seems functional
+        return FeatureTestResult(self, True)
 
 
 def all_features():

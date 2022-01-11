79234bbe4ecf97d9b396c9d9331274d469ad3d6b
Antonio Rojas <arojas@archlinux.org>
2022-01-11 20:26:10 +0100

    Filter distutils warnings form more modules

--- src/sage/all.py
+++ src/sage/all.py
@@ -98,7 +98,7 @@ warnings.filterwarnings('ignore', category=DeprecationWarning,
 
 warnings.filterwarnings('ignore', category=DeprecationWarning,
                         message='The distutils(.sysconfig module| package) is deprecated',
-                        module='numpy|sage.env|sage.features')
+                        module='Cython|distutils|numpy|sage.env|sage.features')
 
 ################ end setup warnings ###############################
 

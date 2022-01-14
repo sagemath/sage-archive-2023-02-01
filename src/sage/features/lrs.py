986ed5305d3b0827da4c6699928fae87a706ae5c
Sébastien Labbé <sebastien.labbe@labri.fr>
2022-01-14 12:43:52 +0100

    33167: simplification of check #1 in is_functional method of lrs feature

--- src/sage/features/lrs.py
+++ src/sage/features/lrs.py
@@ -3,11 +3,9 @@ r"""
 Feature for testing the presence of ``lrslib``
 """
 
-import os
 import subprocess
 
 from . import Executable, FeatureTestResult
-from sage.cpython.string import str_to_bytes, bytes_to_str
 
 
 class Lrs(Executable):
@@ -46,19 +44,17 @@ class Lrs(Executable):
 
         # Check #1
         tf_name = tmp_filename()
-        with open(tf_name, 'wb') as tf:
-            tf.write(str_to_bytes("V-representation\nbegin\n 1 1 rational\n 1 \nend\nvolume"))
-        devnull = open(os.devnull, 'wb')
+        with open(tf_name, 'w') as tf:
+            tf.write("V-representation\nbegin\n 1 1 rational\n 1 \nend\nvolume")
         command = ['lrs', tf_name]
-        try:
-            lines = bytes_to_str(subprocess.check_output(command, stderr=devnull))
-        except subprocess.CalledProcessError as e:
+        result = subprocess.run(command, capture_output=True, text=True)
+        if result.returncode:
             return FeatureTestResult(self, False,
-                reason="Call to `{command}` failed with exit code {e.returncode}.".format(command=" ".join(command), e=e))
+                reason="Call to `{command}` failed with exit code {result.returncode}.".format(command=" ".join(command), result=result))
 
         expected_list = ["Volume= 1", "Volume=1"]
-        if all(lines.find(expected) == -1 for expected in expected_list):
-            print(lines)
+        if all(result.stdout.find(expected) == -1 for expected in expected_list):
+            print(result.stdout)
             return FeatureTestResult(self, False,
                 reason="Output of `{command}` did not contain the expected result {expected}.".format(
                     command=" ".join(command),

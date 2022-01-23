4f5a56d484be776adc09a725091cf0d361da1659
Michael Orlitzky <michael@orlitzky.com>
2022-01-22 23:04:28 -0500

    Trac #8784: free the integer pool when sage exits.
    The sage.rings.integer module uses a global pool of integer objects
    for performance reasons. The (empty) pool gets creates when the module
    is imported, and is currently freed only when quit_sage() is called.
    Freeing memory before termination is not strictly necessary, but is
    a good practice and can aid tools like valgrind.
    
    In this commit we move the cleanup operation out of quit_sage(), which
    must be called manually, and into an atexit hook. This will ensure
    that the pool is freed even when running a *.sage script or using the
    sage library from python code.
    
    This is a step towards making quit_sage() obsolete, and eventually
    removing it from sage.all.

--- src/sage/all.py
+++ src/sage/all.py
@@ -268,9 +268,6 @@ def quit_sage(verbose=True):
     from sage.interfaces.quit import expect_quitall
     expect_quitall(verbose=verbose)
 
-    # Free globally allocated gmp integers.
-    import sage.rings.integer
-    sage.rings.integer.free_integer_pool()
     import sage.algebras.quatalg.quaternion_algebra_element
     sage.algebras.quatalg.quaternion_algebra_element._clear_globals()
 

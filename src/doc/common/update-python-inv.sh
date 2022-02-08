b4f1a9bd24012cee614a7aed71326fa4aaae9928
Kwankyu Lee <ekwankyu@gmail.com>
2022-02-08 11:27:05 +0900

    Edit update-python-inv.sh

--- src/doc/common/update-python-inv.sh
+++ src/doc/common/update-python-inv.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
-# The files python2.inv and python3.inv contain the database of Sphinx hyperlink targets
-# used by the intersphinx extension. See
+# The file python3.inv contains the database of Sphinx hyperlink targets used by
+# the intersphinx extension. See
+#
 #    http://sphinx-doc.org/ext/intersphinx.html
+#
 # To be able to compile Sage without accessing the net, we use a local copy of
 # this database. Here is how to update it:
 
 if command -v wget > /dev/null 2>&1 ; then
-    rm -f python.inv python2.inv python3.inv
+    rm -f python3.inv
     wget https://docs.python.org/release/`sage --python3 --version 2>&1 | cut -d " " -f 2`/objects.inv -O - > python3.inv
 elif command -v curl > /dev/null 2>&1 ; then
     # On OS X, curl is installed by default, but not wget.
-    rm -f python.inv python2.inv python3.inv
+    rm -f python3.inv
     curl https://docs.python.org/release/`sage --python3 --version 2>&1 | cut -d " " -f 2`/objects.inv > python3.inv
 else
     echo "Error: neither wget nor curl is installed."

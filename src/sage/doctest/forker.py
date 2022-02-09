66cf8bd767aed982c0fdffcce22f39be0f78bda8
Michael Orlitzky <michael@orlitzky.com>
2022-02-09 08:01:40 -0500

    Trac #32981: report verbose doctest success in CPU time.
    Since we're now warning about tests that take too much CPU time (as
    opposed to too much wall time), I think it makes sense to report the
    time taken in the same units.

--- src/sage/doctest/forker.py
+++ src/sage/doctest/forker.py
@@ -1261,13 +1261,13 @@ class SageDocTestRunner(doctest.DocTestRunner, object):
             sage: FDS = FileDocTestSource(filename,DD)
             sage: doctests, extras = FDS.create_doctests(globals())
             sage: ex = doctests[0].examples[0]
-            sage: ex.walltime = 0.0
+            sage: ex.cputime = 0.0
             sage: DTR.report_success(sys.stdout.write, doctests[0], ex, '1764')
             ok [0.00 s]
         """
         # We completely replace doctest.DocTestRunner.report_success so that we can include time taken for the test
         if self._verbose:
-            out("ok [%.2f s]\n" % (example.walltime + check_duration))
+            out("ok [%.2f s]\n" % (example.cputime + check_duration))
 
     def report_failure(self, out, test, example, got, globs):
         r"""

818410f0f4c4b14e14741e0c7b4fe2aabac8ba61
Michael Orlitzky <michael@orlitzky.com>
2022-02-13 16:11:29 -0500

    Trac #33213: replace SAGE_TMP in the isgci database download routine.
    ...and make it work on python3. The database download requires
    internet access and isn't tested by default. As a result, it lay
    broken for a while -- the local file needs to be opened in binary mode
    to write the bytes returned by urlopen().

--- src/sage/graphs/isgci.py
+++ src/sage/graphs/isgci.py
@@ -826,22 +826,20 @@ class GraphClasses(UniqueRepresentation):
 
             sage: graph_classes._download_db() # Not tested -- requires internet
         """
-        from sage.misc.misc import SAGE_TMP
+        import tempfile
         u = urlopen('https://www.graphclasses.org/data.zip',
-                    context=default_context())
-        localFile = open(os.path.join(SAGE_TMP, 'isgci.zip'), 'w')
-        localFile.write(u.read())
-        localFile.close()
-        z = zipfile.ZipFile(os.path.join(SAGE_TMP, 'isgci.zip'))
+                    context=SSLContext())
+        with tempfile.NamedTemporaryFile(suffix=".zip") as f:
+            f.write(u.read())
+            z = zipfile.ZipFile(f.name)
 
-        # Save a systemwide updated copy whenever possible
-
-        try:
-            z.extract(_XML_FILE, GRAPHS_DATA_DIR)
-            z.extract(_SMALLGRAPHS_FILE, GRAPHS_DATA_DIR)
-        except IOError:
-            z.extract(_XML_FILE, SAGE_TMP)
-            z.extract(_SMALLGRAPHS_FILE, GRAPHS_DATA_DIR)
+            # Save a systemwide updated copy whenever possible
+            try:
+                z.extract(_XML_FILE, GRAPHS_DATA_DIR)
+                z.extract(_SMALLGRAPHS_FILE, GRAPHS_DATA_DIR)
+            except IOError:
+                z.extract(_XML_FILE, d)
+                z.extract(_SMALLGRAPHS_FILE, GRAPHS_DATA_DIR)
 
     def _parse_db(self, directory):
         r"""

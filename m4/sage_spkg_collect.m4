tobiasdiez/sagetrac-mirror

Enumerate packages in ./bootstrap; only warn about missing 'type' files; use the generated list in m4/sage_spkg_collect.m4 instead of globbing

m4/sage_spkg_collect.m4
@@ -66,12 +66,21 @@ AS_VAR_COPY([$1], [pkg_cv_][$1])
 AS_VAR_IF([$1], [""], [$5], [$4])dnl
 ])dnl PKG_CHECK_VAR
 ])
+
 dnl ==========================================================================
-
 AC_DEFUN_ONCE([SAGE_SPKG_COLLECT], [
-# Configure all spkgs with configure-time checks
+dnl The m4/sage_spkg_configures.m4 file is generated by bootstrap.
+dnl It contains:
+dnl - "m4_sinclude"s for the build/pkgs/SPKG/spkg-configure.m4 files
+dnl - calls to SAGE_SPKG_CONFIGURE_* macros defined there
+dnl - calls to SAGE_SPKG for all packages
+dnl - calls to SAGE_SPKG_ENABLE for optional/experimental packages
 m4_include([m4/sage_spkg_configures.m4])
+])
 
+dnl ==========================================================================
+AC_DEFUN([SAGE_SPKG_INIT], [
+dnl Intialize the collection variables.
 # To deal with ABI incompatibilities when gcc is upgraded, every package
 # (except gcc) should depend on gcc if gcc is already installed.
 # See https://trac.sagemath.org/ticket/24703
@@ -105,9 +114,11 @@ SAGE_DUMMY_PACKAGES='\
 SAGE_STANDARD_PACKAGES='\
 '
 # List of all installed and to-be-installed optional packages - filled in SAGE_SPKG_ENABLE
-#SAGE_OPTIONAL_INSTALLED_PACKAGES
+SAGE_OPTIONAL_INSTALLED_PACKAGES='\
+'
 # List of optional packages to be uninstalled - filled in SAGE_SPKG_ENABLE
-#SAGE_OPTIONAL_CLEANED_PACKAGES
+SAGE_OPTIONAL_CLEANED_PACKAGES='\
+'
 
 # List of all packages that should be downloaded
 SAGE_SDIST_PACKAGES='\
@@ -123,14 +134,33 @@ SAGE_PIP_PACKAGES='\
 SAGE_SCRIPT_PACKAGES='\
 '
 
-# for each package in pkgs/, add them to the SAGE_PACKAGE_VERSIONS and
-# SAGE_PACKAGE_DEPENDENCIES lists, and to one or more of the above variables
-# depending on the package type and other criteria (such as whether or not it
-# needs to be installed)
+AC_SUBST([SAGE_PACKAGE_VERSIONS])
+AC_SUBST([SAGE_PACKAGE_DEPENDENCIES])
+AC_SUBST([SAGE_NORMAL_PACKAGES])
+AC_SUBST([SAGE_PIP_PACKAGES])
+AC_SUBST([SAGE_SCRIPT_PACKAGES])
+AC_SUBST([SAGE_BUILT_PACKAGES])
+AC_SUBST([SAGE_DUMMY_PACKAGES])
+AC_SUBST([SAGE_STANDARD_PACKAGES])
+AC_SUBST([SAGE_OPTIONAL_INSTALLED_PACKAGES])
+AC_SUBST([SAGE_OPTIONAL_CLEANED_PACKAGES])
+AC_SUBST([SAGE_SDIST_PACKAGES])
+])
 
-for DIR in $SAGE_ROOT/build/pkgs/*; do
-    test -d "$DIR" || continue
 
+dnl ==========================================================================
+AC_DEFUN([SAGE_SPKG], [
+    AC_REQUIRE([SAGE_SPKG_INIT])
+    SPKG_NAME=$1
+    dnl add SPKG_NAME to the SAGE_PACKAGE_VERSIONS and
+    dnl SAGE_PACKAGE_DEPENDENCIES lists, and to one or more of the above variables
+    dnl depending on the package type and other criteria (such as whether or not it
+    dnl needs to be installed)
+
+    DIR="$SAGE_ROOT"/build/pkgs/$SPKG_NAME
+    AS_IF([test ! -d "$DIR"], [
+        AC_MSG_ERROR([Directory $DIR is missing. Re-run bootstrap.])
+    ])
     SPKG_TYPE_FILE="$DIR/type"
     if test -f "$SPKG_TYPE_FILE"; then
         SPKG_TYPE=`cat $SPKG_TYPE_FILE`
@@ -138,7 +168,6 @@ for DIR in $SAGE_ROOT/build/pkgs/*; do
         AC_MSG_WARN(["$SPKG_TYPE_FILE" is missing.  Leftovers from another branch?])
     fi
 
-    SPKG_NAME=$(basename $DIR)
     SPKG_VERSION=$(newest_version $SPKG_NAME)
 
     in_sdist=no
@@ -224,17 +253,4 @@ for DIR in $SAGE_ROOT/build/pkgs/*; do
         SAGE_NORMAL_PACKAGES+="    $SPKG_NAME \\"$'\n'
         ;;
     esac
-done
-
-AC_SUBST([SAGE_PACKAGE_VERSIONS])
-AC_SUBST([SAGE_PACKAGE_DEPENDENCIES])
-AC_SUBST([SAGE_NORMAL_PACKAGES])
-AC_SUBST([SAGE_PIP_PACKAGES])
-AC_SUBST([SAGE_SCRIPT_PACKAGES])
-AC_SUBST([SAGE_BUILT_PACKAGES])
-AC_SUBST([SAGE_DUMMY_PACKAGES])
-AC_SUBST([SAGE_STANDARD_PACKAGES])
-AC_SUBST([SAGE_OPTIONAL_INSTALLED_PACKAGES])
-AC_SUBST([SAGE_OPTIONAL_CLEANED_PACKAGES])
-AC_SUBST([SAGE_SDIST_PACKAGES])
 ])

sagemath/sage-archive-2023-02-01

m4/sage_spkg_collect.m4
@@ -64,12 +64,21 @@ AS_VAR_COPY([$1], [pkg_cv_][$1])
 AS_VAR_IF([$1], [""], [$5], [$4])dnl
 ])dnl PKG_CHECK_VAR
 ])
+
 dnl ==========================================================================
-
 AC_DEFUN_ONCE([SAGE_SPKG_COLLECT], [
-# Configure all spkgs with configure-time checks
+dnl The m4/sage_spkg_configures.m4 file is generated by bootstrap.
+dnl It contains:
+dnl - "m4_sinclude"s for the build/pkgs/SPKG/spkg-configure.m4 files
+dnl - calls to SAGE_SPKG_CONFIGURE_* macros defined there
+dnl - calls to SAGE_SPKG for all packages
+dnl - calls to SAGE_SPKG_ENABLE for optional/experimental packages
 m4_include([m4/sage_spkg_configures.m4])
+])
 
+dnl ==========================================================================
+AC_DEFUN([SAGE_SPKG_INIT], [
+dnl Intialize the collection variables.
 # To deal with ABI incompatibilities when gcc is upgraded, every package
 # (except gcc) should depend on gcc if gcc is already installed.
 # See https://trac.sagemath.org/ticket/24703
@@ -125,14 +134,33 @@ SAGE_SCRIPT_PACKAGES=''
 SAGE_NEED_SYSTEM_PACKAGES=""
 SAGE_NEED_SYSTEM_PACKAGES_OPTIONAL=""
 
-# for each package in pkgs/, add them to the SAGE_PACKAGE_VERSIONS and
-# SAGE_PACKAGE_DEPENDENCIES lists, and to one or more of the above variables
-# depending on the package type and other criteria (such as whether or not it
-# needs to be installed)
+AC_SUBST([SAGE_PACKAGE_VERSIONS])
+AC_SUBST([SAGE_PACKAGE_DEPENDENCIES])
+AC_SUBST([SAGE_PACKAGE_TREES])
+AC_SUBST([SAGE_NORMAL_PACKAGES])
+AC_SUBST([SAGE_PIP_PACKAGES])
+AC_SUBST([SAGE_SCRIPT_PACKAGES])
+AC_SUBST([SAGE_BUILT_PACKAGES])
+AC_SUBST([SAGE_DUMMY_PACKAGES])
+AC_SUBST([SAGE_OPTIONAL_INSTALLED_PACKAGES])
+AC_SUBST([SAGE_OPTIONAL_UNINSTALLED_PACKAGES])
+AC_SUBST([SAGE_SDIST_PACKAGES])
+])
 
-for DIR in $SAGE_ROOT/build/pkgs/*; do
-    test -d "$DIR" || continue
 
+dnl ==========================================================================
+AC_DEFUN([SAGE_SPKG], [
+    AC_REQUIRE([SAGE_SPKG_INIT])
+    SPKG_NAME=$1
+    dnl add SPKG_NAME to the SAGE_PACKAGE_VERSIONS and
+    dnl SAGE_PACKAGE_DEPENDENCIES lists, and to one or more of the above variables
+    dnl depending on the package type and other criteria (such as whether or not it
+    dnl needs to be installed)
+
+    DIR="$SAGE_ROOT"/build/pkgs/$SPKG_NAME
+    AS_IF([test ! -d "$DIR"], [
+        AC_MSG_ERROR([Directory $DIR is missing. Re-run bootstrap.])
+    ])
     SPKG_TYPE_FILE="$DIR/type"
     if test -f "$SPKG_TYPE_FILE"; then
         SPKG_TYPE=`cat $SPKG_TYPE_FILE`
@@ -141,7 +169,6 @@ for DIR in $SAGE_ROOT/build/pkgs/*; do
         continue
     fi
 
-    SPKG_NAME=$(basename $DIR)
     SPKG_VERSION=$(newest_version $SPKG_NAME)
 
     in_sdist=yes
@@ -365,19 +392,6 @@ for DIR in $SAGE_ROOT/build/pkgs/*; do
         SAGE_NORMAL_PACKAGES="${SAGE_NORMAL_PACKAGES} \\$(printf '\n    ')${SPKG_NAME}"
         ;;
     esac
-done
-
-AC_SUBST([SAGE_PACKAGE_VERSIONS])
-AC_SUBST([SAGE_PACKAGE_DEPENDENCIES])
-AC_SUBST([SAGE_PACKAGE_TREES])
-AC_SUBST([SAGE_NORMAL_PACKAGES])
-AC_SUBST([SAGE_PIP_PACKAGES])
-AC_SUBST([SAGE_SCRIPT_PACKAGES])
-AC_SUBST([SAGE_BUILT_PACKAGES])
-AC_SUBST([SAGE_DUMMY_PACKAGES])
-AC_SUBST([SAGE_OPTIONAL_INSTALLED_PACKAGES])
-AC_SUBST([SAGE_OPTIONAL_UNINSTALLED_PACKAGES])
-AC_SUBST([SAGE_SDIST_PACKAGES])
 ])
 
 AC_DEFUN([SAGE_SYSTEM_PACKAGE_NOTICE], [
2511e3e18995b5768fc12a75c69a2b2988937bcd
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-02-27 15:12:39 -0800

    src/sage/groups/matrix_gps/coxeter_group.py: Do not use sage.symbolic for easy cases of cos(pi/x) (fixup)

--- src/sage/groups/matrix_gps/coxeter_group.py
+++ src/sage/groups/matrix_gps/coxeter_group.py
@@ -292,6 +292,8 @@ class CoxeterMatrixGroup(UniqueRepresentation, FinitelyGeneratedMatrixGroup_gene
             def val(x):
                 if x == -1:
                     return 2
+                elif x == 1:
+                    return -2
                 elif x == 2:
                     return 0
                 elif x == 3:

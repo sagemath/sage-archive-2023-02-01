7b6433475e9e5b4cf15aa9723ad2b138c2f8b456
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-03-20 16:00:01 -0700

    src/tox.ini (pyright): Use pypi package pyright

--- src/.pip-js-install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh -x
-set -e
-python -m pip install "$@"
-if [ -n "$JS_DEPS" ]; then
-    nodeenv --python-virtualenv --verbose
-    npm install -g --no-package-lock --no-save --verbose $JS_DEPS
-fi

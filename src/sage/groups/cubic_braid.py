a134d26b31f59bdbec2be9d80398dcc35cff6a18
Sebastian Oehms <seb.oehms@gmail.com>
2022-03-27 20:49:09 +0200

    33567: fix typo in docstring

--- src/sage/groups/cubic_braid.py
+++ src/sage/groups/cubic_braid.py
@@ -887,7 +887,7 @@ class CubicBraidGroup(FinitelyPresentedGroup):
 
     def codegrees(self):
         """
-        Return the degrees of ``self``.
+        Return the codegrees of ``self``.
 
         This only makes sense when ``self`` is a finite reflection group.
 

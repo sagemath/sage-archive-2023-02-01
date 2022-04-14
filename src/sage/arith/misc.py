adab07b6807cb3a06f8c2a2f6914e18f236827ba
Frédéric Chapoton <chapoton@unistra.fr>
2022-04-14 21:57:31 +0200

    adjust error messages in arith/

--- src/sage/arith/misc.py
+++ src/sage/arith/misc.py
@@ -1407,7 +1407,7 @@ def random_prime(n, proof=None, lbound=2):
                     else:
                         smallest_prime = ZZ(lbound-1).next_probable_prime()
                     if smallest_prime > n:
-                        raise ValueError("There are no primes between %s and %s (inclusive)" % (lbound, n))
+                        raise ValueError("there are no primes between %s and %s (inclusive)" % (lbound, n))
 
     if proof:
         prime_test = is_prime
@@ -2050,7 +2050,7 @@ def xkcd(n=""):
             data = f.read()
     except HTTPError as error:
         if error.getcode() == 400:  # this error occurs when asking for a non valid comic number
-            raise RuntimeError("Could not obtain comic data from {}. Maybe you should enable time travel!".format(url))
+            raise RuntimeError("could not obtain comic data from {}".format(url))
     except URLError:
         pass
 
@@ -2196,7 +2196,7 @@ def power_mod(a, n, m):
         mpz(285)
     """
     if m == 0:
-        raise ZeroDivisionError("modulus must be nonzero.")
+        raise ZeroDivisionError("modulus must be nonzero")
     if m == 1:
         return 0
     if n < 0:
@@ -2204,7 +2204,7 @@ def power_mod(a, n, m):
         return power_mod(ainv, -n, m)
     if n == 0:
         if a == 0:
-            raise ArithmeticError("0^0 is undefined.")
+            raise ArithmeticError("0^0 is undefined")
         return 1
 
     apow = a % m
@@ -3201,7 +3201,7 @@ def crt(a, b, m=None, n=None):
     g, alpha, beta = XGCD(m, n)
     q, r = f(g)
     if r != 0:
-        raise ValueError("No solution to crt problem since gcd(%s,%s) does not divide %s-%s" % (m, n, a, b))
+        raise ValueError("no solution to crt problem since gcd(%s,%s) does not divide %s-%s" % (m, n, a, b))
     from sage.arith.functions import lcm
 
     x = a + q*alpha*py_scalar_to_element(m)
@@ -3278,9 +3278,9 @@ def CRT_list(v, moduli):
         23
     """
     if not isinstance(v, list) or not isinstance(moduli, list):
-        raise ValueError("Arguments to CRT_list should be lists")
+        raise ValueError("arguments to CRT_list should be lists")
     if len(v) != len(moduli):
-        raise ValueError("Arguments to CRT_list should be lists of the same length")
+        raise ValueError("arguments to CRT_list should be lists of the same length")
     if not v:
         return ZZ.zero()
     if len(v) == 1:
@@ -4611,10 +4611,10 @@ def hilbert_symbol(a, b, p, algorithm="pari"):
         ans_pari = hilbert_symbol(a,b,p,algorithm='pari')
         ans_direct = hilbert_symbol(a,b,p,algorithm='direct')
         if ans_pari != ans_direct:
-            raise RuntimeError("There is a bug in hilbert_symbol; two ways of computing the Hilbert symbol (%s,%s)_%s disagree" % (a,b,p))
+            raise RuntimeError("there is a bug in hilbert_symbol; two ways of computing the Hilbert symbol (%s,%s)_%s disagree" % (a,b,p))
         return ans_pari
     else:
-        raise ValueError("Algorithm %s not defined" % algorithm)
+        raise ValueError(f"algorithm {algorithm} not defined")
 
 
 def hilbert_conductor(a, b):

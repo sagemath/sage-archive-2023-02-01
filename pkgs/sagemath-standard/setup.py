278cc0da5aa3f11a687c5a311ba9e8c09e484778
François Bissey <frp.bissey@gmail.com>
2022-05-12 12:11:06 +1200

    Remove sage_build which is only used to generate wrappers and move the generation to setup.py in all cases.

--- pkgs/sagemath-standard/setup.py
+++ pkgs/sagemath-standard/setup.py
@@ -44,13 +44,11 @@ else:
     from sage_setup.setenv import setenv
     setenv()
 
-    from sage_setup.command.sage_build import sage_build
     from sage_setup.command.sage_build_cython import sage_build_cython
     from sage_setup.command.sage_build_ext import sage_build_ext
     from sage_setup.command.sage_install import sage_install_and_clean
 
-    cmdclass = dict(build=sage_build,
-                    build_cython=sage_build_cython,
+    cmdclass = dict(build_cython=sage_build_cython,
                     build_ext=sage_build_ext,
                     install=sage_install_and_clean)
 
@@ -75,6 +73,10 @@ if sdist:
     python_modules = []
     cython_modules = []
 else:
+    log.info("Generating auto-generated sources")
+    from sage_setup.autogen import autogen_all
+    autogen_all()
+
     # TODO: This should be quiet by default
     print("Discovering Python/Cython source code....")
     t = time.time()

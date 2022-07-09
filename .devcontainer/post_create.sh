e12ca6cf399c6898873de5ebbc363a7e7e442d0e
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-07-08 17:41:06 -0700

    .devcontainer: Install git

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,9 +3,7 @@
 export PATH=$(pwd)/build/bin:$PATH
 SYSTEM=$(sage-guess-package-system)
 eval $(sage-print-system-package-command $SYSTEM "$@" update)
-if [ -n "$EXTRA_SAGE_PACKAGES" ]; then
-    eval $(sage-print-system-package-command $SYSTEM --yes "$@" --spkg install _prereq $EXTRA_SAGE_PACKAGES)
-fi
+eval $(sage-print-system-package-command $SYSTEM --yes "$@" --spkg install _prereq git $EXTRA_SAGE_PACKAGES)
 if [ -n "$EXTRA_SYSTEM_PACKAGES" ]; then
     eval $(sage-print-system-package-command $SYSTEM --yes "$@" install $EXTRA_SYSTEM_PACKAGES)
 fi

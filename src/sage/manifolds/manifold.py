b531034fb9fec3db0b82cb4db00f2be57eae41c9
Frédéric Chapoton <chapoton@unistra.fr>
2022-07-13 10:54:22 +0200

    rst fixes in manifolds

--- src/sage/manifolds/manifold.py
+++ src/sage/manifolds/manifold.py
@@ -904,10 +904,10 @@ class TopologicalManifold(ManifoldSubset):
 
         INPUT:
 
-        - ``resu`` -- an instance of ``:class:`TopologicalManifold` or
+        - ``resu`` -- an instance of :class:`TopologicalManifold` or
           a subclass.
 
-        - ``coord_def`` -- (default: {}) definition of the subset in
+        - ``coord_def`` -- (default: ``{}``) definition of the subset in
           terms of coordinates; ``coord_def`` must a be dictionary with keys
           charts on the manifold and values the symbolic expressions formed
           by the coordinates to define the subset

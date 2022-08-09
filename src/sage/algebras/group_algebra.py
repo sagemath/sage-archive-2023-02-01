383d2733753c531d29005ba331548a0fdede4ca3
Trevor K. Karn <karnx018@umn.edu>
2022-08-09 15:52:16 -0700

    Change copy and fix _coerce_map_from

--- src/sage/algebras/group_algebra.py
+++ src/sage/algebras/group_algebra.py
@@ -156,9 +156,16 @@ class GroupAlgebra_class(CombinatorialFreeModule):
             sage: QG = G.algebra(QQ)
             sage: ZG = G.algebra(ZZ)
             sage: ZG.coerce_map_from(H)
-            Coercion map:
+            Composite map:
               From: Cyclic group of order 3 as a permutation group
               To:   Algebra of Dihedral group of order 6 as a permutation group over Integer Ring
+              Defn:   Coercion map:
+                      From: Cyclic group of order 3 as a permutation group
+                      To:   Dihedral group of order 6 as a permutation group
+                    then
+                      Coercion map:
+                      From: Dihedral group of order 6 as a permutation group
+                      To:   Algebra of Dihedral group of order 6 as a permutation group over Integer Ring
             sage: QG.coerce_map_from(ZG)
             Generic morphism:
               From: Algebra of Dihedral group of order 6 as a permutation group over Integer Ring
@@ -192,17 +199,20 @@ class GroupAlgebra_class(CombinatorialFreeModule):
             sage: ZG = G.algebra(ZZ, category=AdditiveMagmas())
             sage: ZG.has_coerce_map_from(G)
             False
-
-
         """
         G = self.basis().keys()
         K = self.base_ring()
 
-        if G.has_coerce_map_from(S):
+        G_coercion = G.coerce_map_from(S)
+        if G_coercion is not None:
             from sage.categories.groups import Groups
             # No coercion for additive groups because of ambiguity of +
             #   being the group action or addition of a new term.
-            return self.category().is_subcategory(Groups().Algebras(K))
+            if not self.category().is_subcategory(Groups().Algebras(K)):
+                return None
+            if S is G:
+                return True
+            return self.coerce_map_from(G) * G_coercion
 
         if S in Sets.Algebras:
             S_K = S.base_ring()

869f6e2eec454da74899394e5feb0f5222311a5c
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-08-11 10:36:19 -0700

    .devcontainer/portability-post_start.sh: If upstream is a symlink, remove it; keep logs in container if possible

--- .devcontainer/portability-post_start.sh
+++ .devcontainer/portability-post_start.sh
@@ -4,12 +4,17 @@
 # The script assumes that it is run from SAGE_ROOT.
 #
 # If "config.log" or "logs" are symlinks (for example, created by 'tox -e local-...',
-# or after https://trac.sagemath.org/ticket/33262), remove them.
-for f in config.log logs; do
+# or after https://trac.sagemath.org/ticket/33262), they might point outside of
+# the devcontainer, so remove them. Likewise for upstream.
+for f in config.log logs upstream; do
     if [ -L $f ]; then
         rm -f $f
     fi
 done
+# If possible (ensured after https://trac.sagemath.org/ticket/33262), keep the logs in the container.
+if [ ! -f logs ]; then
+    ln -s /sage/logs logs
+fi
 # Bootstrap, configure, and build the Sage distribution, reusing the Sage installation from the prebuilt image.
 set -e
 set -x

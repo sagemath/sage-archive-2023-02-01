c8d987723dfca75236c0a45654c86e6fb40f92a6
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-08-28 10:36:45 -0700

    TensorFreeSubmodule_comp._is_symmetry_coarsening_of: Add docstring

--- src/sage/tensor/modules/tensor_free_submodule.py
+++ src/sage/tensor/modules/tensor_free_submodule.py
@@ -217,6 +217,33 @@ class TensorFreeSubmodule_comp(TensorFreeModule):
             prefix.lower(), self._tensor_type[0], self._tensor_type[1], self._fmodule, suffix)
 
     def _is_symmetry_coarsening_of(self, coarser_comp, finer_comp):
+        r"""
+        Return whether ``coarser_comp`` has coarser symmetry than ``finer_comp``.
+
+        INPUT:
+
+        - ``coarser_comp``, ``finer_comp``: :class:`~sage.tensor.modules.comp.Components`
+
+        EXAMPLES::
+
+            sage: from sage.tensor.modules.tensor_free_submodule import TensorFreeSubmodule_comp
+            sage: M = FiniteRankFreeModule(ZZ, 3, name='M')
+            sage: e = M.basis('e')
+            sage: T60M = M.tensor_module(6, 0)
+            sage: Sym0123x45M = TensorFreeSubmodule_comp(M, (6, 0), sym=((0, 1, 2, 3), (4, 5)))
+            sage: com0123x45M = Sym0123x45M.an_element()._components[e]; com0123x45M
+            6-indices components w.r.t. Basis (e_0,e_1,e_2) on the Rank-3 free module M over the Integer Ring
+            sage: Sym012x345M = TensorFreeSubmodule_comp(M, (6, 0), sym=((0, 1, 2), (3, 4, 5)))
+            sage: com012x345M = Sym012x345M.an_element()._components[e]; com012x345M
+            6-indices components w.r.t. Basis (e_0,e_1,e_2) on the Rank-3 free module M over the Integer Ring
+            sage: Sym012345M  = TensorFreeSubmodule_comp(M, (6, 0), sym=((0, 1, 2, 3, 4, 5)))
+            sage: com012345M  = Sym012345M.an_element()._components[e]; com012345M
+            6-indices components w.r.t. Basis (e_0,e_1,e_2) on the Rank-3 free module M over the Integer Ring
+            sage: Sym0123x45M._is_symmetry_coarsening_of(com0123x45M, com012x345M)
+            True
+            sage: Sym0123x45M._is_symmetry_coarsening_of(com012345M, com012x345M)
+            True
+        """
         self_tensor_type = self.tensor_type()
 
         def sym_antisym(comp):

c42118a437ccfd7f7975869925ef5b56d503c1bf
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-08-28 14:06:48 -0700

    src/sage/manifolds/differentiable/tangent_space.py: Add example: isomorphism with inner product space

--- src/sage/manifolds/differentiable/tangent_space.py
+++ src/sage/manifolds/differentiable/tangent_space.py
@@ -162,6 +162,58 @@ class TangentSpace(FiniteRankFreeModule):
         sage: p2 == p
         True
 
+    An isomorphism of the tangent space with an inner product space with distinguished basis::
+
+        sage: g = M.metric('g')
+        sage: g[:] = ((1, 0), (0, 1))
+        sage: Q_Tp_xy = g[c_xy.frame(),:](*p.coordinates(c_xy)); Q_Tp_xy
+        [1 0]
+        [0 1]
+        sage: W_Tp_xy = VectorSpace(SR, 2, inner_product_matrix=Q_Tp_xy)
+        sage: Tp.bases()[0]
+        Basis (∂/∂x,∂/∂y) on the Tangent space at Point p on the 2-dimensional differentiable manifold M
+        sage: phi_Tp_xy = Tp.isomorphism_with_fixed_basis(Tp.bases()[0], codomain=W_Tp_xy); phi_Tp_xy
+        Generic morphism:
+        From: Tangent space at Point p on the 2-dimensional differentiable manifold M
+        To:   Ambient quadratic space of dimension 2 over Symbolic Ring
+        Inner product matrix:
+        [1 0]
+        [0 1]
+
+        sage: Q_Tp_uv = g[c_uv.frame(),:](*p.coordinates(c_uv)); Q_Tp_uv
+        [1/2   0]
+        [  0 1/2]
+        sage: W_Tp_uv = VectorSpace(SR, 2, inner_product_matrix=Q_Tp_uv)
+        sage: Tp.bases()[1]
+        Basis (∂/∂u,∂/∂v) on the Tangent space at Point p on the 2-dimensional differentiable manifold M
+        sage: phi_Tp_uv = Tp.isomorphism_with_fixed_basis(Tp.bases()[1], codomain=W_Tp_uv); phi_Tp_uv
+        Generic morphism:
+        From: Tangent space at Point p on the 2-dimensional differentiable manifold M
+        To:   Ambient quadratic space of dimension 2 over Symbolic Ring
+        Inner product matrix:
+        [1/2   0]
+        [  0 1/2]
+
+        sage: t1, t2 = Tp.tensor((1,0)), Tp.tensor((1,0))
+        sage: t1[:] = (8, 15)
+        sage: t2[:] = (47, 11)
+        sage: t1[Tp.bases()[0],:]
+        [8, 15]
+        sage: phi_Tp_xy(t1), phi_Tp_xy(t2)
+        ((8, 15), (47, 11))
+        sage: phi_Tp_xy(t1).inner_product(phi_Tp_xy(t2))
+        541
+
+        sage: Tp_xy_to_uv = M.change_of_frame(c_xy.frame(), c_uv.frame()).at(p); Tp_xy_to_uv
+        Automorphism of the Tangent space at Point p on the 2-dimensional differentiable manifold M
+        sage: Tp.set_change_of_basis(Tp.bases()[0], Tp.bases()[1], Tp_xy_to_uv)
+        sage: t1[Tp.bases()[1],:]
+        [23, -7]
+        sage: phi_Tp_uv(t1), phi_Tp_uv(t2)
+        ((23, -7), (58, 36))
+        sage: phi_Tp_uv(t1).inner_product(phi_Tp_uv(t2))
+        541
+
     .. SEEALSO::
 
         :class:`~sage.tensor.modules.finite_rank_free_module.FiniteRankFreeModule`

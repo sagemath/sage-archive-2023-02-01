be985d4bf3ce69adc01ed79c191c3455ded48b33
Kwankyu Lee <ekwankyu@gmail.com>
2022-08-31 13:51:08 +0900

    uniformize the headline

--- src/sage/rings/finite_rings/element_givaro.pyx
+++ src/sage/rings/finite_rings/element_givaro.pyx
@@ -1,7 +1,7 @@
 # distutils: libraries = givaro gmp m
 # distutils: language = c++
 r"""
-Givaro Field Elements
+Givaro finite field flements
 
 Sage includes the Givaro finite field library, for highly optimized
 arithmetic in finite fields.

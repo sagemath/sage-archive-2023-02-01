bfd6eee17d7f6105e8f13d4c3796ff74edfe8a51
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-09-05 15:29:26 -0700

    src/sage/modules/with_basis/invariant.py: Update copyright

--- src/sage/modules/with_basis/invariant.py
+++ src/sage/modules/with_basis/invariant.py
@@ -4,7 +4,8 @@ Invariant modules
 
 # ****************************************************************************
 #       Copyright (C) 2021 Trevor K. Karn <karnx018 at umn.edu>
-#                          Travis Scrimshaw
+#                     2021 Travis Scrimshaw
+#                     2022 Matthias Koeppe
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

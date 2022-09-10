82d8b62091f58f3ff68a8287be7e30515000f61f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-09-10 16:41:35 -0700

    src/sage/structure/support_view.py: Add tests

--- src/sage/structure/support_view.py
+++ src/sage/structure/support_view.py
@@ -6,8 +6,56 @@ from collections.abc import MappingView, Sequence, Set
 
 
 class SupportView(MappingView, Sequence, Set):
+    r"""
+    Dynamic view of the set of keys of a dictionary that are associated with nonzero values
+
+    It behaves like the objects returned by the :meth:`keys`, :meth:`values`,
+    :meth:`items` of a dictionary (or other :class:`collections.abc.Mapping`
+    classes).
+
+    INPUT:
+
+    - ``mapping`` -- a :class:`dict` or another :class:`collections.abc.Mapping`.
+
+    - ``zero`` -- (optional) test for zeroness by comparing with this value.
+
+    EXAMPLES::
+
+        sage: d = {'a': 47, 'b': 0, 'c': 11}
+        sage: from sage.structure.support_view import SupportView
+        sage: supp = SupportView(d); supp
+        SupportView({'a': 47, 'b': 0, 'c': 11})
+        sage: 'a' in supp, 'b' in supp, 'z' in supp
+        (True, False, False)
+        sage: len(supp)
+        2
+        sage: list(supp)
+        ['a', 'c']
+        sage: supp[0], supp[1]
+        ('a', 'c')
+        sage: supp[-1]
+        'c'
+        sage: supp[:]
+        ('a', 'c')
+        sage: supp[2]
+        Traceback (most recent call last):
+        ...
+        IndexError
+
+        sage: d['b'] = 815
+        sage: len(supp)
+        3
+    """
 
     def __init__(self, mapping, *, zero=None):
+        r"""
+        TESTS::
+
+            sage: from sage.structure.support_view import SupportView
+            sage: supp = SupportView({'a': 'b', 'c': ''}, zero='')
+            sage: len(supp)
+            1
+        """
         self._mapping = mapping
         self._zero = zero
 
@@ -18,6 +66,10 @@ class SupportView(MappingView, Sequence, Set):
         return length
 
     def __getitem__(self, index):
+        if isinstance(index, slice):
+            return tuple(self)[index]
+        if index < 0:
+            return tuple(self)[index]
         for i, key in enumerate(self):
             if i == index:
                 return key

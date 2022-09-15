fa77041c3b64c493295361258aa7970cf210e597
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-09-15 12:39:17 -0700

    src/sage/rings/polynomial/msolve.py: Use absolute filename of msolve

--- src/sage/rings/polynomial/msolve.py
+++ src/sage/rings/polynomial/msolve.py
@@ -46,13 +46,11 @@ def _run_msolve(ideal, options):
 
     # Run msolve
 
-    msolve().require()
-
     drlpolring = ideal.ring().change_ring(order='degrevlex')
     polys = ideal.change_ring(drlpolring).gens()
     msolve_in = tempfile.NamedTemporaryFile(mode='w',
                                             encoding='ascii', delete=False)
-    command = ["msolve", "-f", msolve_in.name] + options
+    command = [msolve().absolute_filename(), "-f", msolve_in.name] + options
     try:
         print(",".join(drlpolring.variable_names()), file=msolve_in)
         print(base.characteristic(), file=msolve_in)

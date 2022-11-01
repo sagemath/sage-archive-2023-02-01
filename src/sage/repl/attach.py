7b6fa565f426e28e14be3b22c202301f9d530e9e
Gonzalo Tornaría <tornaria@cmat.edu.uy>
2022-10-31 22:13:13 -0300

    doctests: message added more info in python 3.11

--- src/sage/repl/attach.py
+++ src/sage/repl/attach.py
@@ -54,6 +54,7 @@ character-by-character::
         exec(code, globals)
       File ".../foobar.sage....py", line ..., in <module>
         raise ValueError("third")   # this should appear in the source snippet
+    ...
     ValueError: third
     sage: detach(src)
 """

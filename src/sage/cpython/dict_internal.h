76040803c8ae150baef449edce67ebdafb2ee896
Gonzalo Tornaría <tornaria@cmat.edu.uy>
2022-11-06 11:53:24 -0300

    dict_del_by_value: add internal definitions for python 3.11

--- src/sage/cpython/dict_internal.h
+++ src/sage/cpython/dict_internal.h
@@ -5,6 +5,8 @@
  * changes.
  * */
 
+#if PY_VERSION_HEX < 0x30b0000
+
 /************************************************************/
 /* Copied verbatim from cpython 3.8 (Objects/dict-common.h) */
 /************************************************************/
@@ -163,3 +165,78 @@ dictkeys_set_index(PyDictKeysObject *keys, Py_ssize_t i, Py_ssize_t ix)
 }
 
 /************************************************************/
+
+#else   /* Python >= 3.11 */
+
+#define Py_BUILD_CORE
+#include <internal/pycore_dict.h>
+
+/************************************************************/
+/* Copied verbatim from cpython 3.11 (Objects/dictobject.c) */
+/************************************************************/
+
+#define PERTURB_SHIFT 5
+#define DK_MASK(dk) (DK_SIZE(dk)-1)
+
+/* lookup indices.  returns DKIX_EMPTY, DKIX_DUMMY, or ix >=0 */
+static inline Py_ssize_t
+dictkeys_get_index(const PyDictKeysObject *keys, Py_ssize_t i)
+{
+    int log2size = DK_LOG_SIZE(keys);
+    Py_ssize_t ix;
+
+    if (log2size < 8) {
+        const int8_t *indices = (const int8_t*)(keys->dk_indices);
+        ix = indices[i];
+    }
+    else if (log2size < 16) {
+        const int16_t *indices = (const int16_t*)(keys->dk_indices);
+        ix = indices[i];
+    }
+#if SIZEOF_VOID_P > 4
+    else if (log2size >= 32) {
+        const int64_t *indices = (const int64_t*)(keys->dk_indices);
+        ix = indices[i];
+    }
+#endif
+    else {
+        const int32_t *indices = (const int32_t*)(keys->dk_indices);
+        ix = indices[i];
+    }
+    assert(ix >= DKIX_DUMMY);
+    return ix;
+}
+
+/* write to indices. */
+static inline void
+dictkeys_set_index(PyDictKeysObject *keys, Py_ssize_t i, Py_ssize_t ix)
+{
+    int log2size = DK_LOG_SIZE(keys);
+
+    assert(ix >= DKIX_DUMMY);
+    assert(keys->dk_version == 0);
+
+    if (log2size < 8) {
+        int8_t *indices = (int8_t*)(keys->dk_indices);
+        assert(ix <= 0x7f);
+        indices[i] = (char)ix;
+    }
+    else if (log2size < 16) {
+        int16_t *indices = (int16_t*)(keys->dk_indices);
+        assert(ix <= 0x7fff);
+        indices[i] = (int16_t)ix;
+    }
+#if SIZEOF_VOID_P > 4
+    else if (log2size >= 32) {
+        int64_t *indices = (int64_t*)(keys->dk_indices);
+        indices[i] = ix;
+    }
+#endif
+    else {
+        int32_t *indices = (int32_t*)(keys->dk_indices);
+        assert(ix <= 0x7fffffff);
+        indices[i] = (int32_t)ix;
+    }
+}
+
+#endif

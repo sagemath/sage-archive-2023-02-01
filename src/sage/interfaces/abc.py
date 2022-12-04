2604174d0ad2a7825d356ce8c219033cbf96eb96
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2022-12-04 15:40:48 -0800

    src/sage/interfaces/abc.py: Add doc

--- src/sage/interfaces/abc.py
+++ src/sage/interfaces/abc.py
@@ -1,18 +1,71 @@
-class GapElement:
+r"""
+Abstract base classes for interface elements
+"""
 
+
+class GapElement:
+    r"""
+    Abstract base class for :class:`~sage.interfaces.gap.GapElement`.
+
+    This class is defined for the purpose of ``isinstance`` tests.  It should not be
+    instantiated.
+
+    EXAMPLES:
+
+    By design, there is a unique direct subclass::
+
+        sage: len(sage.interfaces.abc.GapElement.__subclasses__()) <= 1
+        True
+    """
     pass
 
 
 class GpElement:
+    r"""
+    Abstract base class for :class:`~sage.interfaces.gp.GpElement`.
 
+    This class is defined for the purpose of ``isinstance`` tests.  It should not be
+    instantiated.
+
+    EXAMPLES:
+
+    By design, there is a unique direct subclass::
+
+        sage: len(sage.interfaces.abc.GpElement.__subclasses__()) <= 1
+        True
+    """
     pass
 
 
 class Macaulay2Element:
+    r"""
+    Abstract base class for :class:`~sage.interfaces.macaulay2.Macaulay2Element`.
 
+    This class is defined for the purpose of ``isinstance`` tests.  It should not be
+    instantiated.
+
+    EXAMPLES:
+
+    By design, there is a unique direct subclass::
+
+        sage: len(sage.interfaces.abc.Macaulay2Element.__subclasses__()) <= 1
+        True
+    """
     pass
 
 
 class SingularElement:
+    r"""
+    Abstract base class for :class:`~sage.interfaces.singular.SingularElement`.
 
+    This class is defined for the purpose of ``isinstance`` tests.  It should not be
+    instantiated.
+
+    EXAMPLES:
+
+    By design, there is a unique direct subclass::
+
+        sage: len(sage.interfaces.abc.SingularElement.__subclasses__()) <= 1
+        True
+    """
     pass

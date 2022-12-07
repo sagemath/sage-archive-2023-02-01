Furisto/sagetrac-mirror

Trac #32841: use zlib instead of zn_poly in package list examples.
We have a few doctests that list all sage packages, and expect zn_poly
to be the last one listed. Having removed zn_poly, however, zlib is now
the terminal package. Here we update all such tests.

src/sage/misc/package.py
@@ -26,8 +26,7 @@ command inside Sage::
      'alabaster',
      'arb',
      ...
-     'zlib',
-     'zn_poly']
+     'zlib']
 
 Functions
 ---------
@@ -263,7 +262,7 @@ def list_packages(*pkg_types: str, pkg_sources: List[str] = ['normal', 'pip', 's
          'arb',
          'babel',
          ...
-         'zn_poly']
+         'zlib']
         sage: sage_conf_info = L['sage_conf']  # optional - build
         sage: sage_conf_info.type              # optional - build
         'standard'
@@ -473,8 +472,8 @@ def package_versions(package_type, local=False):
         sage: std = package_versions('standard', local=True)  # optional - build
         sage: 'gap' in std  # optional - build
         True
-        sage: std['zn_poly']  # optional - build, random
-        ('0.9.p12', '0.9.p12')
+        sage: std['zlib']  # optional - build, random
+        ('1.2.11.p0', '1.2.11.p0')
     """
     return {pkg.name: (pkg.installed_version, pkg.remote_version) for pkg in list_packages(package_type, local=local).values()}
 

sagemath/sage-archive-2023-02-01

src/sage/misc/package.py
@@ -26,8 +26,7 @@ command inside Sage::
      'alabaster',
      'arb',
      ...
-     'zlib',
-     'zn_poly']
+     'zlib']
 
 Functions
 ---------
@@ -265,7 +264,7 @@ def list_packages(*pkg_types: str, pkg_sources: List[str] = ['normal', 'pip', 's
          'arb',
          'babel',
          ...
-         'zn_poly']
+         'zlib']
         sage: sage_conf_info = L['sage_conf']  # optional - sage_spkg
         sage: sage_conf_info.type              # optional - sage_spkg
         'standard'
@@ -498,8 +497,8 @@ def package_versions(package_type, local=False):
         sage: std = package_versions('standard', local=True)  # optional - sage_spkg
         sage: 'gap' in std  # optional - sage_spkg
         True
-        sage: std['zn_poly']  # optional - sage_spkg, random
-        ('0.9.p12', '0.9.p12')
+        sage: std['zlib']  # optional - sage_spkg, random
+        ('1.2.11.p0', '1.2.11.p0')
     """
     return {pkg.name: (pkg.installed_version, pkg.remote_version) for pkg in list_packages(package_type, local=local).values()}
 